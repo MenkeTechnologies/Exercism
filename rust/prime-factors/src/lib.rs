d2106f5c1a31e98aaf2e65129b116b1f0e380f3c
MenkeTechnologies <linux.dev25@gmail.com>
2021-01-20 10:00:51 -0500

    prime

---
+++ rust/prime-factors/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn factors(n: u64) -> Vec<u64> {
+
+    let mut factors: Vec<u64> = vec![];
+    let mut p_factors: Vec<u64> = vec![];
+
+
+    for num in 1..=n {
+        if n % num == 0 {
+            factors.push(num);
+        }
+    }
+
+    let mut copy = n;
+    for x in factors {
+        while copy % x == 0 && x != 1 {
+            copy /= x;
+            p_factors.push(x)
+        }
+    }
+
+    p_factors
+
+}
