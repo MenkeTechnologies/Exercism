fe0c16d548d43b34c92380171d93b32268802f82
MenkeTechnologies <linux.dev25@gmail.com>
2021-01-22 11:42:48 -0500

    high scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub struct HighScores{
+    vec:Vec<u32>
+}
+
+impl HighScores {
+    pub fn new(scores: &[u32]) -> Self {
+
+        HighScores{
+          vec: scores.to_vec()
+        }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.vec.as_slice()
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.vec.last().cloned()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.vec.iter().max().cloned()
+
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut copy = self.vec.clone();
+        copy.sort_by(|a, b| b.cmp(a));
+        copy.iter().take(3).map(|a| *a).collect::<Vec<u32>>()
+    }
+}
