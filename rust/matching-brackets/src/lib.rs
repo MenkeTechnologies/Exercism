125c1829888430b558bf5adfae6bb3969b0c7161
MenkeTechnologies <linux.dev25@gmail.com>
2021-01-24 03:01:50 -0500

    match

---
+++ rust/matching-brackets/src/lib.rs
@@ -0,0 +1,41 @@
+use std::net::Shutdown::Read;
+
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut v = vec![];
+
+    for char in string.chars() {
+
+        match char {
+
+            '[' | '{' | '(' => {
+                v.push(char)
+            },
+            ']' | '}' | ')' => {
+                if v.is_empty() {
+                    return false;
+                }
+                let last_char = v.pop().unwrap();
+                if char == ']' && last_char != '[' {
+                    return false;
+                }
+                if char == ')' && last_char != '(' {
+                    return false;
+                }
+                if char == '}' && last_char != '{' {
+                    return false;
+                }
+
+
+            }
+
+            _ => {}
+        }
+    }
+
+    if v.is_empty() {
+        return true;
+    }
+
+    false
+
+}
