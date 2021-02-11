3879589ef1ce5f137f2c2103e4f2ea9d7c809159
MenkeTechnologies <linux.dev25@gmail.com>
2021-02-11 09:27:05 -0500

    collatz

---
+++ rust/collatz-conjecture/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn collatz(n: u64) -> Option<u64> {
+    let mut cnt = 0;
+    let mut cpy = n;
+    loop {
+        if cpy < 1  {
+            return None;
+        }
+        if cpy == 1 {
+            return Some(cnt);
+        }
+
+        if cpy % 2 == 00 {
+           cpy /= 2;
+        } else {
+            cpy = 3 * cpy + 1;
+        }
+
+        cnt+=1;
+
+    }
+
+}
