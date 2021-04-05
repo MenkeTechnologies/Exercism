8030822d9265f45d9544efde8f3066764fa14098
MenkeTechnologies <linux.dev25@gmail.com>
2021-04-05 14:01:32 -0400

    rot

---
+++ rust/rotational-cipher/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn rot(first: char, offset: i8, transform: char) -> char {
+    let rem = transform as i16 - first as i16 + offset as i16;
+    let rem_off = rem.rem_euclid(26) as u8;
+    (rem_off + first as u8) as char
+}
+
+pub fn rotate(input: &str, offset: i8) -> String {
+    input.chars()
+        .map(|ch| match ch {
+            'a'..='z' => rot('a', offset, ch),
+            'A'..='Z' => rot('A', offset, ch),
+            _ => ch
+        })
+        .collect::<String>()
+}
