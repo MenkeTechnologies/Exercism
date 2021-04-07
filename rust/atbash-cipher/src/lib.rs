2fa576a947651eae92252b96995ae6afa32c44b4
MenkeTechnologies <linux.dev25@gmail.com>
2021-04-07 01:07:42 -0400

    atbbash

---
+++ rust/atbash-cipher/src/lib.rs
@@ -0,0 +1,33 @@
+fn rotate(c: char) -> char {
+    if c.is_ascii_digit() {
+        c
+    } else {
+        (b'z' - (c as u8 - b'a')) as char
+    }
+}
+
+/// "Encipher" with the Atbash cipher.
+pub fn encode(plain: &str) -> String {
+    let chars = plain
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .map(rotate)
+        .collect::<Vec<char>>();
+
+    let mut v: Vec<String> = vec![];
+    chars.chunks(5).for_each(|x| {
+        v.push(x.iter().cloned().collect());
+    });
+
+    v.join(" ")
+}
+
+/// "Decipher" with the Atbash cipher.
+pub fn decode(cipher: &str) -> String {
+    cipher
+        .chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .map(rotate)
+        .collect()
+}
