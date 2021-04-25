38a0731458d4fc0dfbf56b79846aefeef99c2e00
MenkeTechnologies <linux.dev25@gmail.com>
2021-04-24 20:51:14 -0400

    sg

--- rust/scale-generator/src/lib.rs
+++ rust/scale-generator/src/lib.rs
@@ -1,32 +1,81 @@
-// You should change this.
-//
-// Depending on your implementation, there are a variety of potential errors
-// which might occur. They aren't checked by the test suite in order to
-// allow the greatest freedom of implementation, but real libraries should
-// provide useful, descriptive errors so that downstream code can react
-// appropriately.
-//
-// One common idiom is to define an Error enum which wraps all potential
-// errors. Another common idiom is to use a helper type such as failure::Error
-// which does more or less the same thing but automatically.
 pub type Error = ();
 
-pub struct Scale;
+pub struct Scale {
+    tonic: String,
+    tonic_uc: String,
+    intervals: Vec<usize>,
+}
 
 impl Scale {
+    const SHARPS: [&'static str; 12] = [
+        "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
+    ];
+    const FLATS: [&'static str; 12] = [
+        "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab",
+    ];
+    const USE_SHARP: [&'static str; 14] = [
+        "C", "G", "D", "A", "a", "E", "B", "F#", "e", "b", "f#", "c#", "g#", "d#",
+    ];
+
     pub fn new(tonic: &str, intervals: &str) -> Result<Scale, Error> {
-        unimplemented!(
-            "Construct a new scale with tonic {} and intervals {}",
-            tonic,
-            intervals
-        )
+        let tonic_uc = Scale::getuc(tonic);
+
+        Ok(Self {
+            tonic: tonic.to_string(),
+            tonic_uc,
+            intervals: intervals
+                .chars()
+                .map(|c| match c {
+                    'm' => 1,
+                    'M' => 2,
+                    _ => 999,
+                })
+                .collect(),
+        })
+    }
+
+    fn getuc(tonic: &str) -> String {
+        let mut v: Vec<char> = tonic.chars().collect();
+        v[0].make_ascii_uppercase();
+
+        let mut tonic_uc = String::from(v[0]);
+        if v.len() == 2 {
+            tonic_uc.push(v[1]);
+        }
+        tonic_uc
     }
 
     pub fn chromatic(tonic: &str) -> Result<Scale, Error> {
-        unimplemented!("Construct a new chromatic scale with tonic {}", tonic)
+        let tonic_uc = Scale::getuc(tonic);
+        Ok(Self {
+            tonic: tonic.to_string(),
+            tonic_uc,
+            intervals: vec![1; 12],
+        })
     }
 
     pub fn enumerate(&self) -> Vec<String> {
-        unimplemented!()
+        let scale = if Self::is_sharp(self.tonic.as_str()) {
+            Self::SHARPS
+        } else {
+            Self::FLATS
+        };
+
+        let mut index = scale.iter().position(|&r| r == self.tonic_uc).unwrap();
+
+        let mut v = vec![];
+
+        for i in &self.intervals {
+            let i1 = index % scale.len();
+            let note = scale[i1];
+            v.push(note.to_string());
+            index += *i as usize;
+        }
+
+        v
+    }
+
+    fn is_sharp(tonic: &str) -> bool {
+        Self::USE_SHARP.contains(&tonic)
     }
 }
