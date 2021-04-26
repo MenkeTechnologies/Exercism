484003089d8a43d4765113a76f242d15f1765be7
MenkeTechnologies <linux.dev25@gmail.com>
2021-04-26 10:56:04 -0400

    pp

--- rust/palindrome-products/src/lib.rs
+++ rust/palindrome-products/src/lib.rs
@@ -1,26 +1,57 @@
 #[derive(Debug, PartialEq, Eq)]
 pub struct Palindrome {
-    // implement your palindrome type here
+    num: u64,
 }
 
 impl Palindrome {
     pub fn new(a: u64, b: u64) -> Palindrome {
-        unimplemented!("create a palindrome with factors ({}, {})", a, b)
+        Self { num: a * b }
     }
 
     pub fn value(&self) -> u64 {
-        unimplemented!("return the value of this palindrome")
+        self.num
     }
 
     pub fn insert(&mut self, a: u64, b: u64) {
-        unimplemented!("insert new factors ({}, {}) into this palindrome", a, b)
+        self.num = a * b
     }
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    unimplemented!(
-        "Find the min and max palindromic numbers which are products of numbers in the inclusive range ({}..{})",
-        min,
-        max
-    )
+    let mut v = vec![];
+    for i in min..=max {
+        for j in i..=max {
+            if is_palindrome(i * j) {
+                v.push(i * j)
+            }
+        }
+    }
+
+    if v.is_empty() { return None; }
+
+    let mut min = v[0];
+    let mut max = v[0];
+    v.into_iter().for_each(|n| {
+        if n > max {
+            max = n;
+        }
+        if n < min {
+            min = n;
+        }
+    });
+
+    Some((Palindrome { num: min }, Palindrome { num: max }))
+}
+
+fn is_palindrome(num: u64) -> bool {
+    let mut n = num;
+    let mut v = vec![];
+    while n != 0 {
+        v.push(n % 10);
+        n /= 10;
+    }
+    let o = v.clone();
+    v.reverse();
+
+    o == v
 }
