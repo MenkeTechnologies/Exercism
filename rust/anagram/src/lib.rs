0285d2dc5766c1d377153f1e21c9a5caf2830cb2
MenkeTechnologies <linux.dev25@gmail.com>
2021-04-27 11:37:20 -0400

    ana

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,24 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+fn get_map(word: String) -> HashMap<char, u32> {
+    word.chars().fold(HashMap::new(), |mut map, c| {
+        let v = map.entry(c).or_insert(0);
+        *v += 1;
+        map
+    })
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let cnt = word.len();
+    let map = get_map(word.to_lowercase());
+
+    possible_anagrams
+        .clone()
+        .into_iter()
+        .filter(|&&p| {
+            let pmap = get_map(p.to_lowercase());
+            cnt == p.len() && word.to_lowercase() != p.to_lowercase() && map == pmap
+        })
+        .map(|p| *p)
+        .collect::<HashSet<&str>>()
 }
--- rust/bowling/src/lib.rs
+++ rust/bowling/src/lib.rs
@@ -1,27 +1,163 @@
+const MAX_PINS: u16 = 10;
+const MAX_FRAMES: usize = 10;
+
 #[derive(Debug, PartialEq)]
 pub enum Error {
     NotEnoughPinsLeft,
     GameComplete,
 }
 
+#[derive(Debug)]
+pub struct Frame {
+    index: usize,
+    rolls: Vec<u16>,
+    bonus: Vec<u16>,
+}
+
+impl Frame {
+    fn new(index: usize, pins: u16) -> Self {
+        Frame {
+            index,
+            rolls: vec![pins],
+            bonus: vec![],
+        }
+    }
+
+    fn is_open(&self) -> bool {
+        match (self.rolls.len(), self.rolls.first()) {
+            (1, Some(&MAX_PINS)) => false,
+            (2, _) => false,
+            _ => true,
+        }
+    }
+
+    fn is_strike(&self) -> bool {
+        match self.rolls.first() {
+            Some(&MAX_PINS) => true,
+            _ => false,
+        }
+    }
+
+    fn is_spare(&self) -> bool {
+        match (self.rolls.len(), self.rolls.iter().sum()) {
+            (2, MAX_PINS) => true,
+            _ => false,
+        }
+    }
+
+    fn is_open_for_bonus(&self) -> bool {
+        match (self.is_strike(), self.is_spare(), self.bonus.len()) {
+            (true, _, n) if n <= 1 => true,
+            (false, true, 0) => true,
+            _ => false,
+        }
+    }
+
+    fn is_complete(&self) -> bool {
+        !self.is_open() && !self.is_open_for_bonus()
+    }
+
+    fn add_roll(&mut self, pins: u16) -> Result<(), Error> {
+        if self.score() + pins > MAX_PINS {
+            return Err(Error::NotEnoughPinsLeft);
+        }
+        self.rolls.push(pins);
+        Ok(())
+    }
+
+    fn add_bonus(&mut self, pins: u16) -> Result<(), Error> {
+        if self.is_open_for_bonus() {
+            match (self.index, self.bonus.first()) {
+                (9, Some(n)) if *n < MAX_PINS && n + pins > MAX_PINS => {
+                    return Err(Error::NotEnoughPinsLeft);
+                }
+                _ => {
+                    self.bonus.push(pins);
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn score(&self) -> u16 {
+        let pin_score: u16 = self.rolls.iter().sum();
+        let bonus: u16 = self.bonus.iter().sum();
+        pin_score + bonus
+    }
+}
+
+#[derive(Default)]
 pub struct BowlingGame {
-    frames: Vec<u16>,
-    roll: bool,
+    current_frame: Option<Frame>,
+    frames: Vec<Frame>,
 }
 
 impl BowlingGame {
     pub fn new() -> Self {
         BowlingGame {
-            frames: vec![],
-            roll: false,
+            current_frame: None,
+            frames: Vec::with_capacity(MAX_FRAMES)
         }
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        unimplemented!("Record that {} pins have been scored", pins);
+        if pins > MAX_PINS {
+            return Err(Error::NotEnoughPinsLeft);
+        }
+
+        if self.is_completed() {
+            return Err(Error::GameComplete);
+        }
+
+        for frame in self.frames.iter_mut() {
+            frame.add_bonus(pins)?;
+        }
+
+        if self.frames.len() < MAX_FRAMES {
+            match &mut self.current_frame {
+                Some(frame) => {
+                    frame.add_roll(pins)?;
+
+                    if !frame.is_open() {
+                        self.end_frame()
+                    }
+                }
+                _ => {
+                    if pins == MAX_PINS {
+                        self.frames.push(self.new_frame(pins))
+                    } else {
+                        self.current_frame = Some(self.new_frame(pins));
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn new_frame(&self, pins: u16) -> Frame {
+        Frame::new(self.frames.len(), pins)
+    }
+
+    fn end_frame(&mut self) {
+        let frame = std::mem::replace(&mut self.current_frame, None).unwrap();
+        self.frames.push(frame);
+    }
+
+    fn all_frames_completed(&self) -> bool {
+        self.frames.iter().all(Frame::is_complete)
+    }
+
+    fn is_completed(&self) -> bool {
+        self.frames.len() == MAX_FRAMES && self.all_frames_completed()
     }
 
     pub fn score(&self) -> Option<u16> {
-        unimplemented!("Return the score if the game is complete, or None if not.");
+        if self.is_completed() {
+            Some(self.frames.iter().map(|x| x.score()).sum())
+        } else {
+            None
+        }
     }
 }
