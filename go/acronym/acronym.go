eed646cc4aa9a2568e21706e609ed474ec969a1a
MenkeTechnologies <linux.dev25@gmail.com>
2021-04-27 23:05:21 -0400

    acro

---
+++ go/acronym/acronym.go
@@ -0,0 +1,31 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package acronym should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package acronym
+
+import (
+    "strings"
+    "unicode"
+)
+
+func Split(r rune) bool {
+    return r == ' ' || r == '-'
+}
+
+// Abbreviate should have a comment documenting it.
+func Abbreviate(s string) string {
+    s3 := ""
+    for _, i := range strings.FieldsFunc(strings.ToUpper(s), Split) {
+
+        for _, j := range []rune(i) {
+
+            if unicode.IsLetter(j) {
+                s3 += string(j)
+                break
+            }
+        }
+    }
+    return s3
+}
