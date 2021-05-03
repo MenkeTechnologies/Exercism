6c77b21d9d648c6eb9433b3f058cbf578df4563a
MenkeTechnologies <linux.dev25@gmail.com>
2021-05-02 22:28:57 -0400

    ss

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,27 @@
+package scrabble
+
+import "unicode"
+
+var table = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
+}
+
+func Score(input string) int {
+
+	var r = []rune(input)
+	var sc = 0
+
+	for _, r2 := range r {
+
+		sc += table[unicode.ToUpper(r2)]
+
+	}
+
+	return sc
+}
