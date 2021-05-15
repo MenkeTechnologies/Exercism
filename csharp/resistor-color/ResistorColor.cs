68c01c37401cb6115a170d703d5354c6e15936f6
MenkeTechnologies <linux.dev25@gmail.com>
2021-05-15 17:37:59 -0400

    rc

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+public static class ResistorColor
+{
+    private static Dictionary<string, int> map;
+
+    static ResistorColor()
+    {
+        map = new Dictionary<string, int>
+        {
+            {"black", 0},
+            {"brown", 1},
+            {"red", 2},
+            {"orange", 3},
+            {"yellow", 4},
+            {"green", 5},
+            {"blue", 6},
+            {"violet", 7},
+            {"grey", 8},
+            {"white", 9},
+        };
+    }
+
+
+    public static int ColorCode(string color)
+    {
+
+        return map[color.ToLower()];
+
+    }
+
+    public static string[] Colors()
+    {
+        return map.Keys.ToArray();
+    }
+}
