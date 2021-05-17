586c020689860cd8ad1ec39907e560968e7d4156
MenkeTechnologies <linux.dev25@gmail.com>
2021-05-17 12:15:50 -0400

    hamming

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -4,6 +4,25 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        
+
+
+        if (firstStrand.Length != secondStrand.Length)
+        {
+            throw new ArgumentException("bad args");
+        }
+        int d = 0;
+
+        for (int i = 0; i < firstStrand.Length; i++)
+        {
+            var c = firstStrand[i];
+            var c2 = secondStrand[i];
+
+            if (c != c2)
+            {
+                ++d;
+            }
+        }
+
+        return d;
     }
 }
