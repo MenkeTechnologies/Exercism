42ce744b42bf2df427d31337433100ad8d8205f4
MenkeTechnologies <linux.dev25@gmail.com>
2021-05-18 15:08:54 -0400

    bob cj

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,24 @@
-(ns bob)
+(ns bob
+  (:require [clojure.string :as str]))
+
+(defn question?
+  "docstring"
+  [str]
+  (str/ends-with? (str/trim str) "?")
+  )
+
+(defn shout?
+  "docstring"
+  [str]
+  (and (= (str/upper-case str) str) (re-seq #"[a-zA-Z]" str))
+  )
 
 (defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
+  (cond
+    (str/blank? s) "Fine. Be that way!"
+    (and (question? s) (shout? s)) "Calm down, I know what I'm doing!"
+    (question? s) "Sure."
+    (shout? s) "Whoa, chill out!"
+    :else "Whatever."
+    )
 )
--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,5 +1,6 @@
-(ns reverse-string)
+(ns reverse-string
+  (:require [clojure.string :as str]))
 
 (defn reverse-string [s] ;; <- arglist goes here
-  ;; your code goes here
+  (str/reverse s)
 )
