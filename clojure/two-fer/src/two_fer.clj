35ee1d93fb5638ebf31163417c5ceded83e2fc6f
MenkeTechnologies <linux.dev25@gmail.com>
2021-05-18 14:59:12 -0400

    two-fer clj

--- clojure/two-fer/src/two_fer.clj
+++ clojure/two-fer/src/two_fer.clj
@@ -1,5 +1,5 @@
 (ns two-fer)
 
-(defn two-fer [name] ;; <- arglist goes here
-  ;; your code goes here
+(defn two-fer [& name] ;; <- arglist goes here
+  (str "One for " (or (first name) "you") ", one for me.")
 )
