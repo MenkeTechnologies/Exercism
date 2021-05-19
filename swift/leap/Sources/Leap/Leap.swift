c17990ad92da839c7d4b9814341d131524bbad5f
MenkeTechnologies <linux.dev25@gmail.com>
2021-05-19 18:46:20 -0400

    leap

--- swift/leap/Sources/Leap/Leap.swift
+++ swift/leap/Sources/Leap/Leap.swift
@@ -1,1 +1,26 @@
 //Solution goes in Sources
+
+public class Year {
+
+    public let isLeapYear: Bool
+
+    public init(calendarYear:Int)
+    {
+        if calendarYear % 4 == 0 {
+            if calendarYear % 100 == 0{
+
+                if calendarYear % 400 == 0{
+
+                    isLeapYear = true;
+                    return
+                }
+
+            } else {
+                isLeapYear = true;
+                return
+            }
+        }
+        isLeapYear = false;
+    }
+
+}
