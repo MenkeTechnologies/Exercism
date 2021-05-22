0fce619cd4563df4bd401c65a8aa7173d48192bd
MenkeTechnologies <linux.dev25@gmail.com>
2021-05-22 12:49:29 -0400

    gs

--- swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
+++ swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
@@ -1,1 +1,32 @@
 //Solution goes in Sources
+
+import Foundation
+
+public class Gigasecond {
+
+    let date: Date?
+    let df: DateFormatter?
+
+    public var description: String {
+
+        if var fD = date {
+            fD.addTimeInterval(1e9)
+            return df!.string(from: fD)
+        }
+
+        return "error!"
+    }
+
+
+    public init?(from: String) {
+        let df = DateFormatter()
+        df.timeZone = TimeZone(abbreviation: "GMT")
+        df.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+
+        guard let date = df.date(from: from) else { return nil }
+
+        self.df = df
+        self.date = date
+    }
+
+}
