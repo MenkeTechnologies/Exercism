00ad87c828c5291b7f055bfe792aadc03ab1345c
MenkeTechnologies <linux.dev25@gmail.com>
2021-05-24 04:33:19 -0400

    leap kt

--- kotlin/leap/src/main/kotlin/Leap.kt
+++ kotlin/leap/src/main/kotlin/Leap.kt
@@ -1,6 +1,4 @@
-data class Year(val todo: Nothing) {
+data class Year(val year: Int) {
 
-    // TODO: Implement proper constructor
-
-    val isLeap: Boolean = TODO("Implement this getter to complete the task")
+    val isLeap: Boolean = year % 4 == 0 && year % 100 != 0 || year % 400 == 0
 }
