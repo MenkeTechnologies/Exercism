d0850174011a74cb6dac18c7cb5dfd07479db665
MenkeTechnologies <linux.dev25@gmail.com>
2021-05-26 09:38:12 -0400

    space age

--- groovy/space-age/src/main/groovy/SpaceAge.groovy
+++ groovy/space-age/src/main/groovy/SpaceAge.groovy
@@ -1,6 +1,26 @@
+import java.math.RoundingMode
+
 class SpaceAge {
 
+    private static final double yearToSeconds = 31557600
+
+    private static final Map<String, Double> planetMapping = [
+            "Mercury": 0.2408467,
+            "Venus"  : 0.61519726,
+            "Earth"  : 1.0,
+            "Mars"   : 1.8808158,
+            "Jupiter": 11.862615,
+            "Saturn" : 29.447498,
+            "Uranus" : 84.016846,
+            "Neptune": 164.79132,
+
+    ]
+
+
     static double age(String planet, int seconds) {
-        throw new UnsupportedOperationException('method not implemented.')
+
+        def decimal = seconds / yearToSeconds / planetMapping.get(planet)
+
+        decimal.round(2)
     }
 }
--- groovy/space-age/src/test/groovy/SpaceAgeSpec.groovy
+++ groovy/space-age/src/test/groovy/SpaceAgeSpec.groovy
@@ -11,7 +11,6 @@ class SpaceAgeSpec extends Specification {
         'Earth' | 1000000000 || 31.69
     }
 
-    @Ignore
     def "Age on Mercury"() {
         expect:
         SpaceAge.age(planet, seconds) == expected
@@ -21,7 +20,6 @@ class SpaceAgeSpec extends Specification {
         'Mercury' | 2134835688 || 280.88
     }
 
-    @Ignore
     def "Age on Venus"() {
         expect:
         SpaceAge.age(planet, seconds) == expected
@@ -31,7 +29,6 @@ class SpaceAgeSpec extends Specification {
         'Venus' | 189839836 || 9.78
     }
 
-    @Ignore
     def "Age on Mars"() {
         expect:
         SpaceAge.age(planet, seconds) == expected
@@ -41,7 +38,6 @@ class SpaceAgeSpec extends Specification {
         'Mars' | 2129871239 || 35.88
     }
 
-    @Ignore
     def "Age on Jupiter"() {
         expect:
         SpaceAge.age(planet, seconds) == expected
@@ -51,7 +47,6 @@ class SpaceAgeSpec extends Specification {
         'Jupiter' | 901876382 || 2.41
     }
 
-    @Ignore
     def "Age on Saturn"() {
         expect:
         SpaceAge.age(planet, seconds) == expected
@@ -61,7 +56,6 @@ class SpaceAgeSpec extends Specification {
         'Saturn' | 2000000000 || 2.15
     }
 
-    @Ignore
     def "Age on Uranus"() {
         expect:
         SpaceAge.age(planet, seconds) == expected
@@ -71,7 +65,6 @@ class SpaceAgeSpec extends Specification {
         'Uranus' | 1210123456 || 0.46
     }
 
-    @Ignore
     def "Age on Neptune"() {
         expect:
         SpaceAge.age(planet, seconds) == expected
