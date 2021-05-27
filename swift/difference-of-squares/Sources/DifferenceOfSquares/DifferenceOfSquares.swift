3424a1f08f19d7103d3edaf2dbbaeed313f1f179
MenkeTechnologies <linux.dev25@gmail.com>
2021-05-27 09:44:36 -0400

    dos

--- swift/difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
+++ swift/difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
@@ -1,1 +1,26 @@
 //Solution goes in Sources
+
+class Squares {
+
+    let square: Int
+
+    init(_ square: Int) {
+        self.square = square
+    }
+
+    public var squareOfSum: Int {
+        let s = (0...square).reduce(0, +)
+        return s * s
+    }
+
+    public var sumOfSquares: Int {
+        (0...square).reduce(0, { $0 + $1 * $1 })
+
+    }
+
+    public var differenceOfSquares: Int {
+        squareOfSum - sumOfSquares
+    }
+
+
+}
