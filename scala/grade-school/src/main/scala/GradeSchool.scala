288a517120867ba4418c9c4006b8880e2bae0737
MenkeTechnologies <linux.dev25@gmail.com>
2021-05-30 14:41:26 -0400

    gs

--- scala/grade-school/src/main/scala/GradeSchool.scala
+++ scala/grade-school/src/main/scala/GradeSchool.scala
@@ -1,12 +1,22 @@
 class School {
   type DB = Map[Int, Seq[String]]
 
-  def add(name: String, g: Int) = ???
+  var roster: DB = Map()
 
-  def db: DB = ???
+  def add(name: String, g: Int) = {
+    var names: Seq[String] = roster.getOrElse(g, Seq.empty)
+    names ++= Seq(name)
+    roster += (g -> names)
+  }
 
-  def grade(g: Int): Seq[String] = ???
+  def db: DB = roster
 
-  def sorted: DB = ???
+  def grade(g: Int): Seq[String] = {
+    roster.getOrElse(g, Seq.empty)
+  }
+
+  def sorted: DB = {
+    roster.map(m => m._1 -> m._2.sorted).toSeq.sortBy(_._1).toMap
+  }
 }
 
