85567e362adf64392e16665e15bb6cdb63bcc93e
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-01 09:59:22 -0400

    leap

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,5 +1,4 @@
-function isLeapYear(/* Parameters go here */) {
-  // Your code here
+function isLeapYear(number: number) {
+    return number % 4 == 0 && number % 100 != 0 || number % 400 == 0
 }
-
 export default isLeapYear
