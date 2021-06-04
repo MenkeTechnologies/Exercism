27be64cc7f1b9151d44a9403f3615261647d7e6f
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-04 10:50:14 -0400

    rna

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,7 +1,28 @@
 class Transcriptor {
-  toRna(/* Parameters go here */) {
-    // Your code here
-  }
+
+    private readonly map = new Map([
+        ["G", "C"],
+        ["C", "G"],
+        ["T", "A"],
+        ["A", "U"]
+    ])
+
+    toRna(str: string): string {
+
+        let translated = "";
+
+        for (let ch of str) {
+            let mapped = this.map.get(ch);
+            if (mapped == undefined) {
+                throw new Error(`Invalid input DNA.`)
+            }
+            translated += mapped;
+        }
+
+        return translated;
+
+
+    }
 }
 
 export default Transcriptor
