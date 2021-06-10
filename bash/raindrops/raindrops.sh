35657cb9112dbc756cca66a71489aecc7397ea02
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-10 09:34:09 -0400

    rrd

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+if (( $1 % 3 == 0)); then
+    r="Pling"
+fi
+
+if (( $1 % 5 == 0)); then
+    r="${r}Plang"
+fi
+
+if (( $1 % 7 == 0)); then
+    r="${r}Plong"
+fi
+
+if [[ -z "$r" ]]; then
+    r="$1"
+fi
+
+echo "$r"
