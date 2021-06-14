71d7912e9c9ef8f67b8e04fd252d3f864d6659ae
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-14 10:20:53 -0400

    robot name

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,17 +1,38 @@
 using System;
+using System.Collections.Generic;
+
 
 public class Robot
 {
-    public string Name
+    public Robot() => Name = gen();
+
+    private static Dictionary<string, int> dict = new Dictionary<string, int>();
+    public string Name { get; private set; }
+
+    private static string gen()
     {
-        get
+        while (true)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            var random = new Random();
+            var a = (char)('A' + random.Next() % 26);
+            var b = (char)('A' + random.Next() % 26);
+            var num1 = random.Next() % 10;
+            var num2 = random.Next() % 10;
+            var num3 = random.Next() % 10;
+            var s = $"{a}{b}{num1}{num2}{num3}";
+
+            if (dict.ContainsKey(s))
+            {
+                continue;
+            }
+
+            dict[s] = 1;
+            return s;
         }
     }
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Name = gen();
     }
 }
