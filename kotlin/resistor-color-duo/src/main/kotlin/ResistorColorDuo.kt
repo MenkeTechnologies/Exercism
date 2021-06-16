26631cc6f959281e5373a419cd7d4ff4900db7c0
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-16 11:47:55 -0400

    color

--- kotlin/resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
+++ kotlin/resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
@@ -1,6 +1,19 @@
 object ResistorColorDuo {
 
     fun value(vararg colors: Color): Int {
-        TODO("Implement this to complete the task")
+
+        var str = ""
+        var cnt = 0
+
+        for (color in colors) {
+            str += color.ordinal
+
+            if (++cnt == 2) {
+                break
+            }
+        }
+
+        return str.toInt()
+
     }
 }
