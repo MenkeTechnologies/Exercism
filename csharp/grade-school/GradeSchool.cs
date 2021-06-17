481234193b97e56eacb77f96d30fa076f1b22339
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-17 09:03:45 -0700

    gs

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -1,20 +1,47 @@
-using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 public class GradeSchool
 {
+    private IDictionary<int, List<string>> map = new Dictionary<int, List<string>>();
+
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!map.ContainsKey(grade))
+        {
+            map[grade] = new List<string>();
+            map[grade].Add(student);
+        }
+        else
+        {
+            map[grade].Add(student);
+        }
     }
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<string> list = new List<string>();
+        var immutableSortedSet = map.Keys.ToImmutableSortedSet();
+
+        foreach (var i in immutableSortedSet)
+        {
+            map[i].Sort();
+            list.AddRange(map[i]);
+        }
+
+        return list;
     }
 
     public IEnumerable<string> Grade(int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!map.ContainsKey(grade))
+        {
+            return new List<string>();
+        }
+        else
+        {
+            map[grade].Sort();
+            return map[grade];
+        }
     }
 }
