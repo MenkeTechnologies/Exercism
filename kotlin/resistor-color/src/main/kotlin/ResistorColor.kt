06a688511c255db7060e0c7586255c82efb51730
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-18 16:46:57 -0700

    rc

--- kotlin/resistor-color/src/main/kotlin/ResistorColor.kt
+++ kotlin/resistor-color/src/main/kotlin/ResistorColor.kt
@@ -1,11 +1,24 @@
 object ResistorColor {
 
+    private val map = mapOf(
+        "black" to 0,
+        "brown" to 1,
+        "red" to 2,
+        "orange" to 3,
+        "yellow" to 4,
+        "green" to 5,
+        "blue" to 6,
+        "violet" to 7,
+        "grey" to 8,
+        "white" to 9
+    )
+
     fun colorCode(input: String): Int {
-        TODO("Implement this to complete the task")
+        return map[input.toLowerCase()] ?: -1
     }
 
     fun colors(): List<String> {
-        TODO("Implement this to complete the task")
+        return map.toList().sortedBy { (k, v) -> v }.map { (k, v) -> k }.toList()
     }
 
 }
