4a2b97d14d5ceff5d3168d4a89c23ecb055d6e16
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-18 08:31:33 -0700

    2fer

--- groovy/two-fer/src/main/groovy/TwoFer.groovy
+++ groovy/two-fer/src/main/groovy/TwoFer.groovy
@@ -1,6 +1,7 @@
 class TwoFer {
 
-    static String twoFer(String name) {
-        throw new UnsupportedOperationException('Method is not implemented')
+    static String twoFer(String name = "you") {
+        if (name == "") name = "you"
+        "One for $name, one for me."
     }
 }
