678e3e4f32dec6682b6056f42f2911ec600fd2fc
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-19 12:06:15 -0700

    rds

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -4,6 +4,27 @@ public static class Raindrops
 {
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string res = "";
+        if (number % 3 == 0)
+        {
+            res += "Pling";
+        }
+
+        if (number % 5 == 0)
+        {
+            res += "Plang";
+        }
+
+        if (number % 7 == 0)
+        {
+            res += "Plong";
+        }
+
+        if (res == "")
+        {
+            res += number;
+        }
+
+        return res;
     }
 }
