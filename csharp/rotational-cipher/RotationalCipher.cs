a763e0e2ce03cdb4d2f3f6d580fe8f48751d5048
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-19 11:14:24 -0700

    rot

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -4,6 +4,28 @@ public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string res = "";
+        foreach (var c in text.ToCharArray())
+        {
+            if (char.IsLetter(c))
+            {
+                if (char.IsUpper(c))
+                {
+                    var offset = (c - 'A' + shiftKey) % 26;
+                    res += (char)('A' + offset);
+                }
+                else
+                {
+                    var offset = (c - 'a' + shiftKey) % 26;
+                    res += (char)('a' + offset);
+                }
+            }
+            else
+            {
+                res += c;
+            }
+        }
+
+        return res;
     }
 }
