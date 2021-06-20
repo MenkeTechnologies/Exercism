2879e9c1fbff7acf244f3c346df435fd5c290079
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-20 09:53:53 -0700

    rna transcrip

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,23 @@
-fun transcribeToRna(dna: String): String = ""
+fun transcribeToRna(dna: String): String {
+
+    var res = ""
+    for (char in dna.toCharArray()) {
+        res += xform(char)
+    }
+
+    return res
+}
+
+private fun xform(char: Char): Char {
+    if (char == 'G') {
+        return 'C'
+    } else if (char == 'C') {
+        return 'G'
+    } else if (char == 'T') {
+        return 'A'
+    } else if (char == 'A') {
+        return 'U'
+    } else {
+        throw RuntimeException("invalid dna $char")
+    }
+}
