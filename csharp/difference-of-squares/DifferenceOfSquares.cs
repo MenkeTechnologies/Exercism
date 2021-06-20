344cbc95281b41f67651cc402264b28879cabc60
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-20 09:46:01 -0700

    ds

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -4,16 +4,29 @@ public static class DifferenceOfSquares
 {
     public static int CalculateSquareOfSum(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int sum = 0;
+        for (int i = 1; i <= max; i++)
+        {
+            sum += i;
+        }
+
+        return (int) Math.Pow(sum, 2);
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int sum = 0;
+        
+        for (int i = 1; i <= max; i++)
+        {
+            sum += (int) Math.Pow(i, 2);
+            
+        }
+        return sum;
     }
 
     public static int CalculateDifferenceOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
     }
 }
