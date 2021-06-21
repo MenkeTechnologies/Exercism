8fcb066ac7cff61c04b40fbc36ffe697f981fab0
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-21 11:59:12 -0700

    darts

--- kotlin/darts/src/main/kotlin/Darts.kt
+++ kotlin/darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,19 @@
+import kotlin.math.ceil
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 object Darts {
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
+    fun score(x: Number, y: Number): Int {
+
+        val dist = ceil(sqrt(x.toDouble().pow(2) + y.toDouble().pow(2)))
+
+        return when (dist.toInt()) {
+            in 0..1 -> 10
+            in 2..5 -> 5
+            in 6..10 -> 1
+            else -> 0
+        }
+
     }
 }
