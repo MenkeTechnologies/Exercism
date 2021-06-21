6c352f2ade20d0f96312010089bec9e1ea89966d
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-21 12:12:16 -0700

    sum mult

--- swift/sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
+++ swift/sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
@@ -1,1 +1,16 @@
 //Solution goes in Sources
+
+class SumOfMultiples {
+
+    static func toLimit(_ limit: Int, inMultiples: [Int]) -> Int {
+
+        (0..<limit).filter {
+            for item in inMultiples.filter { $0 != 0 } {
+                if $0 % item == 0 {
+                    return true
+                }
+            }
+            return false
+        }.reduce(0, +)
+    }
+}
