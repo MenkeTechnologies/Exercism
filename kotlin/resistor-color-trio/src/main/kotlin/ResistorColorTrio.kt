aff63a1ab8a74bc9de6f65e9fe4399ea5ecf0b0b
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-21 11:59:12 -0700

    darts

--- kotlin/resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
+++ kotlin/resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
@@ -1,6 +1,18 @@
 object ResistorColorTrio {
 
     fun text(vararg input: Color): String {
-        TODO("Implement this to complete the task")
+        var res = ""
+        var cnt = 0
+
+        for (color in input) {
+            if (cnt < 2) {
+                res += color
+            } else {
+
+            }
+            ++cnt
+        }
+
+        return res
     }
 }
