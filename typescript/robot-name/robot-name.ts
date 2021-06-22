9be9a628d4f3108c0b8840fe32c8f22547dc242f
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-22 09:46:54 -0700

    robot name

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,16 +1,45 @@
 export default class Robot {
+    private static map: Map<string, boolean> = new Map();
+    private nam: string;
+
     constructor() {
+        this.nam = ""
     }
 
     public get name(): string {
-        throw new Error('Implement Robot#name')
+        if (this.nam == "") {
+
+            while (true) {
+
+                const n = this.genName();
+
+                if (!Robot.map.get(n)) {
+                    Robot.map.set(n, true)
+                    this.nam = n;
+                    return this.nam;
+                }
+            }
+        } else {
+            return this.nam;
+        }
+    }
+
+    private genName() {
+        let name = String.fromCharCode(65 + Math.round(Math.random() * 26));
+
+        name += String.fromCharCode(65 + Math.round(Math.random() * 26));
+        name += Math.round(Math.random() * 10);
+        name += Math.round(Math.random() * 10);
+        name += Math.round(Math.random() * 10);
+
+        return name;
     }
 
     public static releaseNames(): void {
-        throw new Error('Implement Robot.releaseNames')
+        this.map.clear();
     }
 
     public resetName(): void {
-        throw new Error('Implement Robot#resetName')
+        this.nam = ""
     }
 }
