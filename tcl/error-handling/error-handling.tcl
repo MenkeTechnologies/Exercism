a2ce1cfc5d0995851bb3020eca5c1278f499eb21
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-26 10:17:02 -0700

    error

--- tcl/error-handling/error-handling.tcl
+++ tcl/error-handling/error-handling.tcl
@@ -1,7 +1,29 @@
 proc custom_error_message {message} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    error "$message"
 }
 
+proc map_error {message} {
+    set errormap {
+      "divide by zero" "division by zero"
+      "*no such file or directory" "file does not exist" 
+      "invalid command name*" "proc does not exist"
+    }
+
+    foreach {k v} $errormap {
+        if { [string match $k $message]} {
+            return $v
+        }
+    }
+
+    return $message
+}
+
+
 proc handle_error {script} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    if { [catch $script err] } {
+        return [map_error $err]
+    } else {
+        return "success"
+
+    }
 }
