b782711e9e3bdb615704f3637ed1ec2575730f1f
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-26 10:52:41 -0700

    darts

--- groovy/darts/src/main/groovy/Darts.groovy
+++ groovy/darts/src/main/groovy/Darts.groovy
@@ -1,6 +1,16 @@
 class Darts {
 
     static int score(x, y) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+        def d = Math.pow(Math.pow(x - 0, 2) + Math.pow(y - 0, 2), 0.5)
+        
+        if (d <= 1) {
+            return 10
+        } else if (d <= 5) {
+            return 5
+        } else if (d <= 10) {
+            return 1
+        }
+
+        return 0
     }
 }
