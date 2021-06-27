4bb462e97a1c97883c1afc936d68d0ae9d34530a
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-27 12:39:06 -0700

    prot

--- groovy/protein-translation/src/main/groovy/ProteinTranslation.groovy
+++ groovy/protein-translation/src/main/groovy/ProteinTranslation.groovy
@@ -1,6 +1,36 @@
 class ProteinTranslation {
 
-    static proteins(strand) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    static def rna = [AUG: 'Methionine',
+                      UUU: 'Phenylalanine',
+                      UUC: 'Phenylalanine',
+                      UUA: 'Leucine',
+                      UUG: 'Leucine',
+                      UCU: 'Serine',
+                      UCC: 'Serine',
+                      UCA: 'Serine',
+                      UCG: 'Serine',
+                      UAU: 'Tyrosine',
+                      UAC: 'Tyrosine',
+                      UGU: 'Cysteine',
+                      UGC: 'Cysteine',
+                      UGG: 'Tryptophan',
+                      UAA: 'STOP',
+                      UAG: 'STOP',
+                      UGA: 'STOP'
+    ]
+
+    static def proteins(String strand) {
+
+        return strand.collect()
+                .collate(3)
+                .collect {
+                    def codon = it.join()
+                    if (!rna.containsKey(codon)) {
+                        throw new IllegalArgumentException("unknown codon $codon")
+                    }
+                    rna[codon]
+                }
+                .takeWhile { it != "STOP" }
+
     }
 }
