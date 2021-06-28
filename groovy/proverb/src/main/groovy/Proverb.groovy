7754fd1c3f403ab639c7b204a7b074de7633db17
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-28 12:01:40 -0700

    proverb

--- groovy/proverb/src/main/groovy/Proverb.groovy
+++ groovy/proverb/src/main/groovy/Proverb.groovy
@@ -1,6 +1,25 @@
 class Proverb {
 
-    static recite(strings) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    static def recite(List<String> strings) {
+
+        def fin = ""
+
+        if (strings.size() == 0) {
+            return fin
+        }
+
+        for (int i = 0; i < strings.size() - 1; ++i) {
+
+            def cur = strings.get(i)
+            def next = strings.get(i + 1)
+            def res = "For want of a $cur the $next was lost.\n"
+            fin += res
+
+
+        }
+        fin += "And all for the want of a ${strings.get(0)}."
+
+        fin
+
     }
 }
