fb0a1b226ad168465fb683347b85a88a449fb7c4
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-28 11:15:25 -0700

    series

--- tcl/series/series.tcl
+++ tcl/series/series.tcl
@@ -1,3 +1,26 @@
 proc slices {series length} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    if {$length < 1} {
+        return -code error "slice length cannot be less than one"
+
+    }
+
+    set offset [string length $series]
+
+    if {$offset == 0} {
+        return -code error "series cannot be empty"
+
+    }
+
+    if {$length > $offset} {
+        return -code error "slice length cannot be greater than series length"
+
+    }
+
+    set res {}
+
+    for {set i 0; set j $length} {$j <= $offset} {incr i; incr j} {
+        lappend res [string range $series $i $j-1]
+    }
+
+    return $res
 }
