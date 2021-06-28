d56aa6cb7fad69642568c54b4ca4bc29ddd77e73
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-28 11:41:55 -0700

    wc

--- tcl/word-count/word-count.tcl
+++ tcl/word-count/word-count.tcl
@@ -1,5 +1,23 @@
 proc countWords {sentence} {
     set counts [dict create]
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+
+    set sentence [string tolower $sentence]
+    regsub -all {[^a-z'0-9]+} $sentence " " sentence
+    set sentence [string trim $sentence]
+
+    foreach word [split $sentence] {
+        regsub {^'(.*)'$} $word {\1} word
+
+        if {[dict exists $counts $word]} {
+            set cnt [dict get $counts $word]
+        } else {
+            set cnt 0
+        }
+        dict set counts $word [expr $cnt + 1]
+    }
+
+
     return $counts
 }
+
+
