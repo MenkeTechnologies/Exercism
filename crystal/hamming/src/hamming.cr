92bb4d9565df8768ebfe5f39070f76f45d79ce0b
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-29 12:19:52 -0400

    ham

--- crystal/hamming/src/hamming.cr
+++ crystal/hamming/src/hamming.cr
@@ -1,1 +1,15 @@
 # Please implement your solution to hamming in this file
+
+module Hamming
+
+    def self.distance(left, right)
+
+        if left.size != right.size
+            raise ArgumentError.new("size must be equal")
+        end
+
+        left.chars.zip(right.chars).count {|a,b| a != b }
+
+    end
+
+end
