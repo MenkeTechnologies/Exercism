d6d0e40e368c14c19143d1c3d3d0cc3aaab33ef2
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-09 09:49:38 -0400

    leap perl

--- perl5/leap/Leap.pm
+++ perl5/leap/Leap.pm
@@ -7,7 +7,7 @@ our @EXPORT_OK = qw<is_leap_year>;
 
 sub is_leap_year {
   my ($year) = @_;
-  return undef;    # Replace this with your own code to pass the tests.
+  return $year % 4 == 0 && ($year % 100 != 0 || $year % 400 == 0)
 }
 
 1;
