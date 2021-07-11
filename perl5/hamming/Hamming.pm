f9103e44a85c0be582f047c561a52471c32476b0
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-11 11:37:02 -0400

    hams

--- perl5/hamming/Hamming.pm
+++ perl5/hamming/Hamming.pm
@@ -5,8 +5,23 @@ use Exporter 'import';
 our @EXPORT_OK = qw(hamming_distance);
 
 sub hamming_distance {
-  my ( $strand1, $strand2 ) = @_;
-  return undef;
+    my ($strand1, $strand2) = @_;
+
+    if (length($strand1) != length($strand2)) {
+        die("left and right strands must be of equal length");
+    }
+
+    my $h = 0;
+    for (my $i = 0; $i < length($strand1); $i++) {
+        my $c1 = substr($strand1, $i, 1);
+        my $c2 = substr($strand2, $i, 1);
+
+        if ($c1 ne $c2) {
+            ++$h;
+        }
+    }
+
+    return $h;
 }
 
 1;
