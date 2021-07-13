359377505782fae0993d9af6388d0fbdf981f698
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-13 10:19:55 -0400

    arm

--- bash/armstrong-numbers/armstrong_numbers.sh
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+num="$1"
+sum=0
+cnt=${#num}
+
+for (( i = 0; i < cnt; i++ )); do
+    ch=${num:$i:1}
+    (( sum += ch ** cnt ))
+done
+
+if (( sum == num )); then
+    echo "true"
+else
+    echo "false"
+fi
