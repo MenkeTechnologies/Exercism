3323359c79a0c5de9dd31db6e9c67bf773251730
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-15 10:50:28 -0400

    sum mult

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,4 +1,12 @@
+
+
 object SumOfMultiples {
-  def sum(factors: Set[Int], limit: Int): Int = ???
+  def sum(factors: Set[Int], limit: Int): Int = {
+
+    factors.flatMap(f => {
+      (1 until limit).takeWhile(_ * f < limit).map(_ * f)
+    }).sum
+
+  }
 }
 
