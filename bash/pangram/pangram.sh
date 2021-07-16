5f9bd2f6328616b68f679954f9db22d61aef3d74
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-16 14:00:01 -0400

    pangram

--- bash/pangram/pangram.sh
+++ bash/pangram/pangram.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+res=$(echo "$1" | perl -ne '$_=lc;$_=~s@[^[:alpha:]]@@g;print "$_\n" for split //' | sort | uniq | perl -pe 's@\n@@')
+
+
+[[ $res == abcdefghijklmnopqrstuvwxyz ]] && echo true || echo false
