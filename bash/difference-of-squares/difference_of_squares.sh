b755685128cde7296cb54247d448d3d7d1861f9f
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-19 10:37:10 -0400

    diff sq

--- bash/difference-of-squares/difference_of_squares.sh
+++ bash/difference-of-squares/difference_of_squares.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+sqsum=0
+sumsq=0
+
+for (( i = 1; i <= "$2"; i++ )); do
+    (( sqsum += $i ))
+    (( sumsq += ($i ** 2) ))
+done
+
+(( sqsum = sqsum ** 2 ))
+
+
+if [[ $1 == 'square_of_sum' ]]; then
+    echo $sqsum
+elif [[ $1 == 'sum_of_squares' ]]; then
+    echo $sumsq
+else
+    echo $((sqsum - sumsq))
+fi
