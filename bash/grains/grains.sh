14095f09ee733ee354332c1ff2dca987244e3c59
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-21 03:18:18 -0400

    gr

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+if [[ $1 == total ]]; then
+    echo "1 * (1 - 2 ^ 64) / (1 - 2)" | bc
+else
+    if (( $1 < 1 || $1 > 64 )); then
+        echo "Error: invalid input"
+        exit 1
+    fi
+    echo "2 ^ $(( $1 - 1 ))" | bc
+fi
+
+
