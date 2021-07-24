024e681fe6a6e4c199aae87e5e517947386b37c0
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-24 14:24:30 -0400

    sm

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Enumerable.Range(1, max-1)
+            .Where(i =>
+            multiples.Any(m => m != 0 && i % m == 0))
+            .Sum();
     }
 }
