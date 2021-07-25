dfbee0acb7c049a51d46d12cd44c9f76aed5d356
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-25 10:32:06 -0400

    gr

--- perl5/grains/Grains.pm
+++ perl5/grains/Grains.pm
@@ -1,16 +1,20 @@
 package Grains;
 use strict;
 use warnings;
+use bignum;
 use Exporter 'import';
 our @EXPORT_OK = qw(grains_on_square total_grains);
 
 sub grains_on_square {
   my ($square) = @_;
-  return undef;
+  if ($square < 1 or $square > 64) {
+      die "square must be between 1 and 64";
+  }
+  return 2 ** ($square - 1);
 }
 
 sub total_grains {
-  return undef;
+  return 1 * (1 - 2 ** 64) / ( 1 - 2);
 }
 
 1;
