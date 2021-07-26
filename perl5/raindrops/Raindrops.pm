d89bd8a7d43fa26625722b696b02dd32bf2d085d
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-26 09:10:35 -0400

    rd

--- perl5/raindrops/Raindrops.pm
+++ perl5/raindrops/Raindrops.pm
@@ -5,8 +5,24 @@ use Exporter 'import';
 our @EXPORT_OK = qw(raindrop);
 
 sub raindrop {
-  my ($number) = @_;
-  return undef;
+    my ($number) = @_;
+
+    my $res = "";
+
+    if ($number % 3 == 0) {
+        $res .= "Pling";
+    }
+    if ($number % 5 == 0) {
+        $res .= "Plang";
+    }
+    if ($number % 7 == 0) {
+        $res .= "Plong";
+    }
+    if ($res eq "") {
+        $res = "$number";
+    }
+
+    return $res;
 }
 
 1;
