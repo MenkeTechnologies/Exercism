9e804d4696bb6a7bfa868601409f788573976519
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-28 13:39:07 -0400

    rna

--- crystal/rna-transcription/src/rna_transcription.cr
+++ crystal/rna-transcription/src/rna_transcription.cr
@@ -1,1 +1,15 @@
 # Please implement your solution to rna-transcription in this file
+class RnaComplement
+
+    @@map = {
+        'G' => 'C',
+        'C' => 'G',
+        'T' => 'A',
+        'A' => 'U',
+
+    }
+
+    def self.of_dna (dna)
+        dna.chars.map {|ch| @@map[ch]}.join
+    end
+end
