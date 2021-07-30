76c96d9f6f6e9a459307c214062fa284a3c0c1da
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-30 09:25:39 -0400

    ss

--- perl5/scrabble-score/ScrabbleScore.pm
+++ perl5/scrabble-score/ScrabbleScore.pm
@@ -4,9 +4,27 @@ use warnings;
 use Exporter 'import';
 our @EXPORT_OK = qw(score);
 
+my %points = (
+    (map { $_ => 1 } qw(A E I O U L N R S T)),
+    (map { $_ => 2 } qw(D G)),
+    (map { $_ => 3 } qw(B C M P)),
+    (map { $_ => 4 } qw(F H V W Y)),
+    (map { $_ => 5 } qw(K)),
+    (map { $_ => 8 } qw(J X)),
+    (map { $_ => 10 } qw(Q Z))
+);
+
+
 sub score {
-  my ( $word, %extensions ) = @_;
-  return undef;
+  my ($word) = @_;
+
+  my @ch = split //, uc $word;
+
+  my $sc = 0;
+
+  $sc += $points{$_} for @ch;
+
+  return $sc;
 }
 
 1;
