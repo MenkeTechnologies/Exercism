0ba6c9af8fa5558b55f4d3f9e65403d65af762b5
MenkeTechnologies <linux.dev25@gmail.com>
2021-07-31 12:10:12 -0400

    leap

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+if [[ -z $1 ]] || [[ -n $2 ]] || ! [[ $1 =~ ^[0-9]+$ ]]; then
+    echo 'Usage: leap.sh <year>'
+    exit 1
+fi
+
+if (( $1 % 4 == 0 && $1 % 100 != 0 || $1 % 400 == 0 )); then
+    echo true
+else
+    echo false
+fi
+
