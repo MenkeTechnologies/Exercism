5dc983e2a8fe5aadb73cc1c1f7e751205afbfcf2
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-01 01:04:43 -0400

    rd

--- crystal/raindrops/src/raindrops.cr
+++ crystal/raindrops/src/raindrops.cr
@@ -1,1 +1,25 @@
 # Please implement your solution to raindrops in this file
+
+class Raindrops
+
+    def self.convert(num)
+        str = ""
+
+        if num % 3 == 0
+            str += "Pling"
+        end
+        if num % 5 == 0
+            str += "Plang"
+        end
+        if num % 7 == 0
+            str += "Plong"
+        end
+
+        if str == ""
+            str = "#{num}"
+        end
+
+        return str
+    end
+    
+end
