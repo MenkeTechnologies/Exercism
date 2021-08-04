e6c8de5a815fceb5412d55367c0624efc10d02da
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-04 09:37:40 -0400

    ds

--- crystal/difference-of-squares/src/difference_of_squares.cr
+++ crystal/difference-of-squares/src/difference_of_squares.cr
@@ -1,1 +1,21 @@
 # Please implement your solution to difference-of-squares in this file
+    class Squares
+
+        def self.square_of_sum(num)
+
+            (1..num).sum ** 2
+
+        end
+
+        def self.sum_of_squares(num)
+
+            (1..num).map {|x| x ** 2}.sum
+
+        end
+
+        def self.difference_of_squares(num)
+
+            square_of_sum(num) - sum_of_squares(num)
+
+        end
+    end
