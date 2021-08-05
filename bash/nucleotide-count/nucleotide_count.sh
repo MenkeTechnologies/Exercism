4d07bfdf3106530d80bebc40b33e8113e08a8e3b
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-05 10:32:00 -0400

    nc

--- bash/nucleotide-count/nucleotide_count.sh
+++ bash/nucleotide-count/nucleotide_count.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+declare -A map
+
+map[A]=0
+map[C]=0
+map[G]=0
+map[T]=0
+
+while read -n 1; do
+    case $REPLY in
+        A|C|G|T )
+            (( ++map[$REPLY] ))
+            ;;
+        *)
+            echo "Invalid nucleotide in strand" >&2
+            exit 1
+            ;;
+    esac
+done < <(printf '%s' "${1^^}")
+
+echo "A: ${map[A]}
+C: ${map[C]}
+G: ${map[G]}
+T: ${map[T]}"
