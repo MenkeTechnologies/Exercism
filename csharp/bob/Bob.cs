8090bd1f7840179afeeedf3c499ddd88d96143bf
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-12 13:37:04 -0400

    bob

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Text.RegularExpressions;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var input = Regex.Replace(statement, @"\s+", "");
+
+        if (input.Length == 0)
+        {
+            return "Fine. Be that way!";
+        }
+
+        if (Regex.IsMatch(input, @"[A-Z]+") && !Regex.IsMatch(input, @"[a-z]+"))
+        {
+            if (Regex.IsMatch(input, @"\?$"))
+            {
+                return "Calm down, I know what I'm doing!";
+            }
+
+            return "Whoa, chill out!";
+        }
+
+        if (Regex.IsMatch(input, @"\?$"))
+        {
+            return "Sure.";
+        }
+
+        return "Whatever.";
     }
 }
