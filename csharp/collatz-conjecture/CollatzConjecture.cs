2ec2de702b8f7e3893acc9d0a650efb82c517e90
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-13 07:45:44 -0400

    collatz

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -4,6 +4,35 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        int cnt = 0;
+
+        if (number <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(number));
+        }
+
+
+        while (number != 1)
+        {
+
+            if (number % 2 == 0)
+            {
+                number /= 2;
+            }
+            else
+            {
+
+                number = 3 * number + 1;
+
+            }
+
+
+            ++cnt;
+        }
+
+
+        return cnt;
+
+
     }
 }
