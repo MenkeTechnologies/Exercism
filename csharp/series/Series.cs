7e2c66ac704ca3621512fd7d13c76c9dcdcf8e52
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-14 23:30:50 -0400

    series

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Linq;
 
 public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (numbers.Length <= 0 || sliceLength <= 0 || sliceLength > numbers.Length)
+        {
+            throw new ArgumentException();
+        }
+
+        return Enumerable.Range(0, numbers.Length - sliceLength + 1)
+            .Select(p => numbers.Substring(p, sliceLength)).ToArray();
     }
 }
