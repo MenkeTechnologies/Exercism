62c3fae9161ad645b17cd521581df65a6674bda6
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-15 21:16:04 -0400

    bob

--- groovy/bob/src/main/groovy/Bob.groovy
+++ groovy/bob/src/main/groovy/Bob.groovy
@@ -1,5 +1,25 @@
 class Bob {
-    static response(input) {
-        throw new UnsupportedOperationException('method not implemented.')
+    static response(String input) {
+
+        input = input.replaceAll('\\s+', '')
+
+        if (input.isEmpty()) {
+            return "Fine. Be that way!"
+        }
+
+        if (input.matches('.*[A-Z]+.*') && !input.matches('.*[a-z]+.*')) {
+            if (input.matches('.*\\?$')) {
+                return "Calm down, I know what I'm doing!"
+
+            }
+
+            return "Whoa, chill out!"
+
+        }
+        if (input.matches('.*\\?$')) {
+            return 'Sure.'
+        }
+
+        'Whatever.'
     }
 }
