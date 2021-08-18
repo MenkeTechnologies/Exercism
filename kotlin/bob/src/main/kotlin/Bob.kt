6bff40b5305ca4ed2eeaf529b660357b4235c081
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-18 09:11:20 -0400

    bob

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -1,5 +1,34 @@
 object Bob {
     fun hey(input: String): String {
-        TODO("Implement the function to complete the task")
+
+        val trimmed = input.replace("""\s+""".toRegex(), "")
+
+        if (trimmed.isEmpty()) {
+            return "Fine. Be that way!"
+        }
+
+
+        if ("""[A-Z]""".toRegex().containsMatchIn(trimmed) &&
+            !"""[a-z]""".toRegex().containsMatchIn(trimmed)
+        ) {
+
+
+            if ("""\?$""".toRegex().containsMatchIn(trimmed)) {
+                return "Calm down, I know what I'm doing!"
+            }
+
+            return "Whoa, chill out!"
+
+        }
+
+
+        if ("""\?$""".toRegex().containsMatchIn(trimmed)) {
+            return "Sure."
+        }
+
+
+        return "Whatever."
+
+
     }
 }
