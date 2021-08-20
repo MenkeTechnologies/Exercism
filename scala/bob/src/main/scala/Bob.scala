bd78a7ad7a266121249d670613b34fa041dc5602
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-20 10:41:33 -0400

    bob

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,3 +1,26 @@
 object Bob {
-  def response(statement: String): String = ???
+  def response(statement: String): String = {
+
+    val noSpace = statement.replaceAll(raw"\s+", "")
+
+    if (noSpace.isEmpty) {
+      return "Fine. Be that way!"
+    }
+
+    if (noSpace.matches(raw".*[A-Z].*") && !noSpace.matches(raw".*[a-z].*")) {
+
+      if (noSpace.matches(raw".*\?$$")) {
+        return "Calm down, I know what I'm doing!"
+      }
+
+      return "Whoa, chill out!"
+    }
+
+    if (noSpace.matches(raw".*\?$$")) {
+      return "Sure."
+    }
+
+
+    "Whatever."
+  }
 }
