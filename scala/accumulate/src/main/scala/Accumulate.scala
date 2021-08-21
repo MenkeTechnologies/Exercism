d70a13865a06b917653b12d55d3415def38f7a8f
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-21 17:16:32 -0400

    acc

--- scala/accumulate/src/main/scala/Accumulate.scala
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -1,3 +1,10 @@
 class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = ???
+  def accumulate[A, B](f: (A) => B, list: List[A]): List[B] = {
+    var res: List[B] = List()
+    for (elem <- list) {
+      res ++= List(f(elem))
+    }
+
+    res
+  }
 }
