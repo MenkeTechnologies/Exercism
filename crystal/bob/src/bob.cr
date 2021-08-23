563ee5cd9afa68df954db619f56a08726ac850e0
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-23 09:45:54 -0400

    bob

--- crystal/bob/src/bob.cr
+++ crystal/bob/src/bob.cr
@@ -1,1 +1,27 @@
 # Please implement your solution to bob in this file
+
+class Bob
+    def self.hey(s)
+
+        if s.blank?
+            return "Fine. Be that way!"
+        end
+
+        if /[A-Z]/ =~ s && /[a-z]/ !~ s
+
+            if /\?$/ =~ s
+                return "Calm down, I know what I'm doing!"
+            end
+
+            return "Whoa, chill out!"
+        end
+
+        if /\?$/ =~ s
+            return "Sure."
+        end
+
+        "Whatever."
+
+
+    end
+end
