5eabbc4edb204211ef17ed420be4e571e48b0087
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-27 07:59:16 -0400

    rcd

--- groovy/resistor-color-duo/src/main/groovy/ResistorColorDuo.groovy
+++ groovy/resistor-color-duo/src/main/groovy/ResistorColorDuo.groovy
@@ -1,6 +1,22 @@
 class ResistorColorDuo {
 
+    static def map = [
+            'black': 0,
+            'brown': 1,
+            'red': 2,
+            'orange': 3,
+            'yellow': 4,
+            'green': 5,
+            'blue': 6,
+            'violet': 7,
+            'grey': 8,
+    ]
+
     static int value(List<String> colorsInput) {
-        throw new UnsupportedOperationException('method not implemented.')
+        def res = ''
+        res += map[colorsInput[0].toLowerCase()]
+        res += map[colorsInput[1].toLowerCase()]
+
+        Integer.valueOf(res)
     }
 }
