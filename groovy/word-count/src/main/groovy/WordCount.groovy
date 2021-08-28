22e8ebe0187ef517e073363ca85c198bcfaa58cb
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-28 10:42:56 -0400

    wc

--- groovy/word-count/src/main/groovy/WordCount.groovy
+++ groovy/word-count/src/main/groovy/WordCount.groovy
@@ -1,10 +1,24 @@
 class WordCount {
 
-    WordCount(s) {
-        // YOUR CODE HERE
+    def map = [:]
+    def l = []
+
+    WordCount(String s) {
+        l = s.toLowerCase().replaceAll("(?!\\w|'t\\b).", " ").split()
+
+        l.each {
+            if (map[it]) {
+                map[it] += 1
+            } else {
+                map[it] = 1
+            }
+        }
+
     }
 
     def countWords() {
-        // YOUR CODE HERE
+        map.sort { -it.value }.each {
+            println("$it.key: $it.value")
+        }
     }
 }
