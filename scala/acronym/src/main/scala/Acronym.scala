2c274b53574784317c00a7b45eb1ad4bc550fcaa
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-30 12:31:37 -0400

    acro

--- scala/acronym/src/main/scala/Acronym.scala
+++ scala/acronym/src/main/scala/Acronym.scala
@@ -1,3 +1,6 @@
 object Acronym {
-  def abbreviate(phrase: String): String = ???
+  def abbreviate(phrase: String): String = {
+    phrase.toUpperCase.replaceAll("-", " ").replaceAll("[,_]", "").split(raw"\s+").map(_.head).mkString
+
+  }
 }
