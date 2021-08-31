a251cb219a3ba8bdea3582f9ed1c35e0360764ab
MenkeTechnologies <linux.dev25@gmail.com>
2021-08-31 12:08:43 -0400

    acro

--- crystal/acronym/src/acronym.cr
+++ crystal/acronym/src/acronym.cr
@@ -1,1 +1,8 @@
 # Please implement your solution to acronym in this file
+
+class Acronym
+    def self.abbreviate(str)
+
+        str.upcase.gsub(/[_,]/, "").gsub(/-/, " ").split.map{|s| s[0]}.join
+    end
+end
