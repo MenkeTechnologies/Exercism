206749b1177acade2a41e81b08d85d6c5c03a262
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-01 13:57:36 -0400

    col

--- crystal/collatz-conjecture/src/collatz_conjecture.cr
+++ crystal/collatz-conjecture/src/collatz_conjecture.cr
@@ -1,1 +1,26 @@
 # Please implement your solution to collatz-conjecture in this file
+
+module CollatzConjecture
+
+    def self.steps(n)
+
+        if n <= 0
+            raise ArgumentError.new
+        end
+
+        cnt=0
+
+        while n != 1
+            if n % 2 == 0
+                n /= 2
+            else
+                n = 3 *n + 1
+            end
+            cnt += 1
+
+        end
+
+        cnt
+    end
+
+end
