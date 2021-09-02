d1012104f6c454c1b6caaa6eff2c69869bf50063
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-02 10:20:30 -0400

    d

---
+++ scala/darts/src/main/scala/Darts.scala
@@ -0,0 +1,16 @@
+import scala.math.pow
+
+object Darts {
+  def score(x: Double, y: Double): Int = {
+
+    val d = pow(pow(x, 2) + pow(y, 2), 0.5)
+
+    d match {
+      case d if d <= 1 => 10
+      case d if d <= 5 => 5
+      case d if d <= 10 => 1
+      case _ => 0
+    }
+  }
+
+}
