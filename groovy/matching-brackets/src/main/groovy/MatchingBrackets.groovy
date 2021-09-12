1bb38ad939e0c6d80a30797ca61e60d2ba763b48
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-12 11:33:44 -0400

    mb

--- groovy/matching-brackets/src/main/groovy/MatchingBrackets.groovy
+++ groovy/matching-brackets/src/main/groovy/MatchingBrackets.groovy
@@ -1,6 +1,25 @@
 class MatchingBrackets {
 
-    static isPaired(value) {
-        throw new UnsupportedOperationException('Method is not implemented')
+    static Boolean isPaired(String value) {
+
+        def stack = new Stack()
+        def map = ["[": "]", "{": "}", "(": ")"]
+
+
+        for (it in value.split("")) {
+
+            if (map.containsKey(it)) {
+                stack.push(it)
+            } else if (map.values().contains(it)) {
+                if (stack.isEmpty() || map[stack.pop()] != it) {
+                    return false
+                }
+            }
+
+        }
+
+        stack.isEmpty()
+
+
     }
 }
