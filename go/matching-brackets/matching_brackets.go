8741dfe242a69a9fe941505ef53935879f6508dc
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-13 11:21:17 -0400

    go mb

--- go/matching-brackets/matching_brackets.go
+++ go/matching-brackets/matching_brackets.go
@@ -1,1 +1,32 @@
 package brackets
+
+var m = map[rune]rune{
+	'(': ')',
+	'[': ']',
+	'{': '}',
+}
+
+func Bracket(input string) bool {
+
+	var stack []rune
+
+	for _, r := range input {
+
+		for k, v := range m {
+			if k == r {
+				stack = append(stack, r)
+				break
+			} else if v == r {
+				if len(stack) == 0 || stack[len(stack)-1] != k {
+					return false
+				}
+				stack = stack[:len(stack)-1]
+				break
+			}
+		}
+
+	}
+
+	return len(stack) == 0
+
+}
