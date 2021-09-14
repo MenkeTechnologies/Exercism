35c913a45b0f7d7905b0936a40417886ec7782fa
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-14 10:12:48 -0400

    acc

---
+++ perl5/accumulate/Accumulate.pm
@@ -0,0 +1,15 @@
+package Accumulate;
+use strict;
+use warnings FATAL => 'all';
+
+sub accumulate {
+    my ($lst, $fn) = @_;
+    my @res = ();
+
+    push @res, &{$fn}($_) for @{$lst};
+
+    \@res
+
+}
+
+1;
