23590a8386166a1f40cd0756f92f8b595452d638
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-17 09:43:38 -0400

    nc

--- perl5/nucleotide-count/NucleotideCount.pm
+++ perl5/nucleotide-count/NucleotideCount.pm
@@ -5,8 +5,15 @@ use Exporter 'import';
 our @EXPORT_OK = qw(count_nucleotides);
 
 sub count_nucleotides {
-  my ($strand) = @_;
-  return undef;
+    my ($strand) = @_;
+    my %map = ("A", 0, "G", 0, "C", 0, "T", 0);
+
+    for (split //, $strand) {
+        die "Invalid nucleotide in strand" if !/[AGCT]/;
+        ++$map{$_}
+    }
+
+    \%map
 }
 
-1;
+1
