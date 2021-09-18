51fffcd3e230a0a06f321a47c4ce6e0390da385c
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-18 10:36:48 -0400

    gs

--- perl5/gigasecond/Gigasecond.pm
+++ perl5/gigasecond/Gigasecond.pm
@@ -2,11 +2,19 @@ package Gigasecond;
 use strict;
 use warnings;
 use Exporter 'import';
+use Time::Piece;
+use Time::Seconds;
+
 our @EXPORT_OK = qw(add_gigasecond);
+use constant FORMAT => "%Y-%m-%dT%H:%M:%S";
+use constant GIGA => 1e9;
 
 sub add_gigasecond {
-  my ($time) = @_;
-  return undef;
+    my ($time) = @_;
+
+    my $tp = Time::Piece->strptime($time, FORMAT) + GIGA;
+    $tp->strftime(FORMAT)
+
 }
 
-1;
+1
