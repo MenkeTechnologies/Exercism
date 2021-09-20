ea51ed354bd6c9b7fc1bb885ec37a080ffcfdb1a
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-20 11:18:16 -0400

    allergies

--- perl5/allergies/Allergies.pm
+++ perl5/allergies/Allergies.pm
@@ -4,14 +4,29 @@ use warnings;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<allergic_to list_allergies>;
 
+my %m = ("eggs", 1, "peanuts", 2, "shellfish", 4, "strawberries", 8, "tomatoes", 16, "chocolate", 32, "pollen", 64, "cats", 128);
+
 sub allergic_to {
-  my ($input) = @_;
-  return undef;
+    my ($input) = @_;
+    my ($item, $score) = ($$input{item}, $$input{score});
+
+    ($m{$item} & $score) > 0
+
 }
 
 sub list_allergies {
-  my ($score) = @_;
-  return undef;
+    my ($score) = @_;
+
+    my @l;
+
+    for (keys %m) {
+        push @l, $_ if allergic_to {
+            item  => $_,
+            score => $score
+        }
+    }
+
+    \@l
 }
 
-1;
+1
