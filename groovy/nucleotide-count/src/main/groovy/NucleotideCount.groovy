e934649bae012c0626d614d96c60227956bfb628
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-26 14:13:24 -0400

    nc

--- groovy/nucleotide-count/src/main/groovy/NucleotideCount.groovy
+++ groovy/nucleotide-count/src/main/groovy/NucleotideCount.groovy
@@ -1,6 +1,15 @@
 class NucleotideCount {
 
-    static count(strand) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    static count(String strand) {
+        def map = [
+                A: 0,
+                G: 0,
+                C: 0,
+                T: 0
+        ]
+
+        strand.chars.each { map[it as String]++ }
+
+        map
     }
 }
