8a504238d2576da71ca92b2bd55575dd4629517e
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-27 09:41:24 -0400

    rna

--- groovy/rna-transcription/src/main/groovy/RnaTranscription.groovy
+++ groovy/rna-transcription/src/main/groovy/RnaTranscription.groovy
@@ -1,6 +1,14 @@
 class RnaTranscription {
 
+    static def map = [
+            A: "U",
+            G: "C",
+            C: "G",
+            T: "A"
+    ]
+
     static String toRna(String strand) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+        strand.toUpperCase().chars.collect { map[it as String] }.join()
+
     }
 }
