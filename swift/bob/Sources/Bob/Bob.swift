dab4b1c1787cbbe7c60287f6a5c3527f939a0554
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-27 17:22:07 -0400

    bob

--- swift/bob/Sources/Bob/Bob.swift
+++ swift/bob/Sources/Bob/Bob.swift
@@ -1,1 +1,32 @@
 //Solution goes in Sources
+
+class Bob {
+
+    public class func hey(_ input: String) -> String {
+
+        let nospace = input.filter {
+            !$0.isWhitespace
+        }
+
+        if nospace.isEmpty {
+            return "Fine. Be that way!"
+        }
+
+        if !nospace.filter{ $0.isUppercase }.isEmpty && nospace.filter{ $0.isLowercase }.isEmpty {
+
+//            if nospace.last! == "?" {
+//                return "Calm down, I know what I'm doing!"
+//            }
+
+            return "Whoa, chill out!"
+
+        }
+        if nospace.last! == "?" {
+            return "Sure."
+        }
+
+
+        return "Whatever."
+
+    }
+}
