2876838579394cabc7879ec029f8aad7c567b745
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-29 13:14:14 -0400

    sh

--- groovy/secret-handshake/src/main/groovy/SecretHandshake.groovy
+++ groovy/secret-handshake/src/main/groovy/SecretHandshake.groovy
@@ -1,6 +1,23 @@
 class SecretHandshake {
 
+    static def MAP = [
+            0b1   : "wink",
+            0b10  : "double blink",
+            0b100 : "close your eyes",
+            0b1000: "jump",
+    ]
+    static def RVS = 0b10000
+
     static List<String> commands(int number) {
-        throw new UnsupportedOperationException('method not implemented.')
+
+        def cmds = []
+
+        MAP.each { if (it.key & number) cmds << it.value }
+
+        if (number & RVS) {
+            cmds = cmds.reverse()
+        }
+
+        cmds
     }
 }
