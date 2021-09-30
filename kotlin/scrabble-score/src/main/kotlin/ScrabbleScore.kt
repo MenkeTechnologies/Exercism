6f9886bae2051ed191cefbc9d98b85ac9eae4c67
MenkeTechnologies <linux.dev25@gmail.com>
2021-09-30 13:12:16 -0400

    ss

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,10 +1,18 @@
 object ScrabbleScore {
 
-    fun scoreLetter(c: Char): Int {
-        TODO("Implement this function to complete the task")
+
+    private val MAP = mapOf('A' to 1, 'B' to 3, 'C' to 3, 'D' to 2, 'E' to 1, 'F' to 4,
+            'G' to 2, 'H' to 4, 'I' to 1, 'J' to 8, 'K' to 5, 'L' to 1,
+            'M' to 3, 'N' to 1, 'O' to 1, 'P' to 3, 'Q' to 10, 'R' to 1,
+            'S' to 1, 'T' to 1, 'U' to 1, 'V' to 4, 'W' to 4, 'X' to 8,
+            'Y' to 4, 'Z' to 10)
+
+    private fun scoreLetter(c: Char): Int {
+
+        return MAP[c]!!
     }
 
     fun scoreWord(word: String): Int {
-        TODO("Implement this function to complete the task")
+        return word.toUpperCase().toCharArray().map { scoreLetter(it) }.sum()
     }
 }
