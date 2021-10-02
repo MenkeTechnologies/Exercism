e1ad169b92d664221df25576c788d354f20bbadb
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-02 02:33:50 -0400

    sh kot

--- kotlin/secret-handshake/src/main/kotlin/SecretHandshake.kt
+++ kotlin/secret-handshake/src/main/kotlin/SecretHandshake.kt
@@ -1,5 +1,19 @@
+import kotlin.math.pow
+
 object HandshakeCalculator {
+    infix fun Int.`**`(exponent: Int): Int = toDouble().pow(exponent).toInt()
+
     fun calculateHandshake(number: Int): List<Signal> {
-        TODO("Implement the function to complete the task")
+        var toList = Signal.values().filter {
+            it.ordinal == 0 && number and 1 > 0 ||
+                    2 `**` it.ordinal and number > 0
+        }.toList()
+
+        if (number and 16 > 0) {
+            toList = toList.reversed()
+        }
+
+        return toList
+
     }
 }
