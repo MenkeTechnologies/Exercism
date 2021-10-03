e7571211e2297988e1613a80baaf7317982bdc0c
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-03 02:13:02 -0400

    wc

--- kotlin/word-count/src/main/kotlin/WordCount.kt
+++ kotlin/word-count/src/main/kotlin/WordCount.kt
@@ -1,6 +1,16 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        TODO("Implement this function to complete the task")
+
+        val m = mutableMapOf<String, Int>()
+
+        phrase.toLowerCase()
+            .replace(Regex("""(?!\w|'t\b)."""), " ")
+            .trim().split(Regex("""\s+"""))
+            .forEach { m[it] = m.getOrDefault(it, 0) + 1 }
+
+        return m
+
+
     }
 }
