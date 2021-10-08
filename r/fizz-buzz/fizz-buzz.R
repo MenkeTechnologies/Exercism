d54755ea345614c4f364f871fc8f7641c5d7a4d2
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-08 11:26:29 -0400

    fb

--- r/fizz-buzz/fizz-buzz.R
+++ r/fizz-buzz/fizz-buzz.R
@@ -1,2 +1,20 @@
 fizz_buzz <- function(input) {
+  v = vector()
+  for (i in 1:input) {
+    
+    if (i %% 15 == 0){
+      v[i] = "Fizz Buzz"
+    } else if (i %% 3 == 0){
+      v[i] = "Fizz"
+    }
+    else if (i %% 5 == 0){
+      v[i] = "Buzz"
+    } else {
+      v[i] = i
+    }
+    
+  }
+  
+  v
+  
 }
