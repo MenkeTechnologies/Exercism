79f33440323628c10dbbd14e68fc661fcce50ede
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-09 15:43:43 -0400

    acro

--- r/acronym/acronym.R
+++ r/acronym/acronym.R
@@ -1,3 +1,5 @@
-acronym <- function(input) {
-    
+acronym = function(input) {
+    cl = gsub("[_,]", "", gsub("[-]", " ", toupper(input)))
+    v = unlist(strsplit(cl, " "))
+    paste(sub(r"{^(\w)\S*}", r"{\1}", v), collapse = '')
 }
