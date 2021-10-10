13893eed19bc3083f608721b1fac7dff8f7f6b3e
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-10 13:04:42 -0400

    ham

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -1,4 +1,28 @@
 # This is a stub function to take two strings
 # and calculate the hamming distance
-hamming <- function(strand1, strand2) {
+hamming =function(strand1, strand2) {
+  
+  
+  if (nchar(strand1) != nchar(strand2)) {
+    stop("This is an error message")
+  }
+  if (nchar(strand1) == 0 || nchar(strand2) == 0) {
+    return(0)
+  }
+  cnt = 0
+  i=1
+  
+  v1 = unlist(strsplit(strand1, ""))
+  v2 = unlist(strsplit(strand2, ""))
+  
+  for (ch1 in v1){
+    ch2 = v2[i]
+    if (ch1 != ch2 ){
+      cnt = cnt + 1
+    }
+    i = i + 1
+  }
+  
+  
+  cnt
 }
