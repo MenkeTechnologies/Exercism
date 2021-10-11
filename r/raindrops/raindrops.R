a2111e163c1054cd37d8f58d41b7d96f9cfffaee
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-11 12:35:52 -0400

    rd

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -1,3 +1,19 @@
-raindrops <- function(number) {
+raindrops = function(n) {
+
+  str = ""
   
+  if (n %% 3 == 0) {
+    str = paste(str, "Pling", sep="")
+  }
+  if (n %% 5 == 0) {
+    str = paste(str, "Plang", sep="")
+  }
+  if (n %% 7 == 0) {
+    str = paste(str, "Plong", sep="")
+  }
+  if (nchar(str) == 0 ){
+    str = toString(n)
+  }
+  
+  str
 }
