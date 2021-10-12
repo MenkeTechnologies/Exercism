c41d1e421fa706be000481fc710fde90c2b867fd
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-12 11:19:11 -0400

    wc

--- r/word-count/word-count.R
+++ r/word-count/word-count.R
@@ -1,3 +1,6 @@
-word_count <- function(input) {
+word_count = function(input) {
+ str = trimws(gsub(r"{(?!\w|'\w+\b).}", " ", tolower(input), perl=T))
+ w = strsplit(str, r"{\s+}")
 
+ as.list(table(w))
 }
