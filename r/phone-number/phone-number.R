0978d723fd106c35c2be2061bd68bc665db9a656
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-13 12:41:49 -0400

    pn

--- r/phone-number/phone-number.R
+++ r/phone-number/phone-number.R
@@ -1,3 +1,19 @@
-parse_phone_number <- function(number_string) {
+parse_phone_number = function(number_string) {
+  str = gsub(r"{[^\d]}", "", number_string, perl=T)
+  if (nchar(str) <= 9 || nchar(str) >= 12) {
+    return(NULL)
+  }
+  if (nchar(str) == 11) {
+    if (!grepl(r"{^1\d+}",str, perl=T)){
+      return(NULL)
+    }
+    str=sub(".", "", str)
+  }
+  
+  if (!grepl("[2-9]\\d{2}[2-9]\\d{6}", str, perl=T)){
+    return(NULL)
+  }
+  
+  str
   
 }
