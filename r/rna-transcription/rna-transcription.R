dfa1ab119e51b35ad25ad34689431a3407087b84
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-14 13:23:46 -0400

    rna

--- r/rna-transcription/rna-transcription.R
+++ r/rna-transcription/rna-transcription.R
@@ -1,3 +1,13 @@
-to_rna <- function(dna) {
+coll = 
+
+to_rna = function(dna) {
+  map = list(G = "C", C = "G", T = "A", A = "U")
+  v = unlist(strsplit(toupper(dna), ""))
   
+  paste(lapply(v, FUN = function(x) {
+    if (is.null(map[[x]])){
+      stop("invalid dna strand")
+    }
+    map[[x]]
+  }), collapse="")
 }
