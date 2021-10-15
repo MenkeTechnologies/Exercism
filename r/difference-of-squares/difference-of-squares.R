cc28d9110943d93ffab11ddc5519a3bc86241307
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-15 08:34:34 -0400

    ds

--- r/difference-of-squares/difference-of-squares.R
+++ r/difference-of-squares/difference-of-squares.R
@@ -1,5 +1,18 @@
 # this is a stub function that takes a natural_number
 # and should return the difference-of-squares as described
 # in the README.md
-difference_of_squares <- function(natural_number) {
+difference_of_squares = function(natural_number) {
+  
+  sumsq = 0
+  sqsum = 0
+  
+  for (i in 1:natural_number){
+    sumsq = sumsq + i ** 2
+    sqsum = sqsum + i
+  }
+  
+  sqsum = sqsum ** 2
+  
+  sqsum - sumsq
+  
 }
