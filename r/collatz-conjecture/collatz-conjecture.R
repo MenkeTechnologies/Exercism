f3bf3187bede46ef516140a336a6d04fd86f1155
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-16 13:54:31 -0400

    col

--- r/collatz-conjecture/collatz-conjecture.R
+++ r/collatz-conjecture/collatz-conjecture.R
@@ -1,3 +1,21 @@
-collatz_step_counter <- function(num) {
+collatz_step_counter = Vectorize(function(num) {
   
-}
+  if (num <= 0) {
+    stop("bad input num")
+  }
+  
+  cnt = 0
+  
+  while (num != 1){
+    
+  if (num %% 2 == 0){
+    num = num / 2
+  } else {
+    num = 3 * num + 1
+  }
+cnt = cnt + 1
+  }
+  
+  cnt
+  
+})
