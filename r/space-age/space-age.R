5b0ef42f46603c0f2f823133d49256be2b49cf3f
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-17 11:47:00 -0400

    ssa

--- r/space-age/space-age.R
+++ r/space-age/space-age.R
@@ -1,3 +1,19 @@
-space_age <- function(seconds, planet) {
-
+space_age = function(seconds, planet) {
+  spy = 3600 * 24 * 365.25
+  ey = seconds / spy
+  
+  factor = switch(
+    planet,
+    "mercury" = 0.2408467,
+    "venus"   = 0.61519726,
+    "earth"   = 1,
+    "mars"    = 1.8808158,
+    "jupiter" = 11.862615,
+    "saturn"  = 29.447498,
+    "uranus"  = 84.016846,
+    "neptune" = 164.79132
+  )
+  
+  
+  round(ey / factor, 2)
 }
