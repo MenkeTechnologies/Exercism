6dc968bd701bada3f5fc4acdeab65f5d79d8c547
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-18 12:32:03 -0400

    ana

--- r/anagram/anagram.R
+++ r/anagram/anagram.R
@@ -1,3 +1,18 @@
-anagram <- function(subject, candidates) {
+anagram = function(s, candidates) {
 
+  s = tolower(s)
+  sorted = paste(sort(unlist(strsplit(s, ""))), collapse = "")
+  vec = c()
+  
+  for (s2 in candidates) {
+    
+    s2l = tolower(s2)
+    sorted2 = paste(sort(unlist(strsplit(s2l, ""))), collapse = "")
+    if (sorted == sorted2 && s != s2l) {
+      vec = append(vec, s2)
+    }
+  }
+  
+  vec
+  
 }
