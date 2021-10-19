216ad5f0fc8ad0c963f8f9010866e4446f916f07
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-19 13:13:24 -0400

    r rot

--- r/rotational-cipher/rotational-cipher.R
+++ r/rotational-cipher/rotational-cipher.R
@@ -1,3 +1,19 @@
-rotate <- function(text, key) {
+
+
+rotate = function(text, key) {
+  
+  chars = unlist(strsplit(text, ""))
+  
+  paste(lapply(chars, 
+               function(c) {
+                 if (c %in% letters) {
+                   base = utf8ToInt("a")
+                 } else if (c %in% LETTERS) {
+                   base = utf8ToInt("A")
+                 } else {
+                   return(c)
+                 }
+                 intToUtf8((utf8ToInt(c) - base + key) %% 26 + base)
+               }), collapse="")
   
 }
