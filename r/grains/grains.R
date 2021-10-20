26f4bf846cc64d0715f9f998ae1600e5c92f6fa5
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-20 12:41:53 -0400

    gr

--- r/grains/grains.R
+++ r/grains/grains.R
@@ -1,7 +1,17 @@
-square <- function(n) {
+square = function(n) {
+  
+  if (n > 64 || n < 1) {
+    stop("error with n")
+  }
 
+  2 ** (n - 1)
 }
 
-total <- function() {
-
+total = function() {
+  sum = 0
+  for (i in 1:64){
+    sum = sum + square(i)
+  }
+  
+  sum
 }
