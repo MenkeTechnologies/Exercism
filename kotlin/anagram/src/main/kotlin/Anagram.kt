987765ccb82a43efa41fab5698eaffca440d2b17
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-23 13:43:58 -0400

    ana

--- kotlin/anagram/src/main/kotlin/Anagram.kt
+++ kotlin/anagram/src/main/kotlin/Anagram.kt
@@ -1,7 +1,19 @@
-class Anagram {
-    // TODO: implement proper constructor to complete the task
+class Anagram(word: String) {
+    private val lc = word.toLowerCase()
 
     fun match(anagrams: Collection<String>): Set<String> {
-        TODO("Implement the function to complete the task")
+
+        val s = mutableSetOf<String>()
+
+        anagrams.forEach {
+            val lc2 = it.toLowerCase()
+            if (lc.toList().sorted() == lc2.toList().sorted() && lc != lc2) {
+                s += it
+
+            }
+        }
+
+        return s
+
     }
 }
