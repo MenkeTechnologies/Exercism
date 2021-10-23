39a451af8e3735d15b4e833ab577bd55cdb4764f
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-23 13:43:58 -0400

    ana

--- kotlin/raindrops/src/main/kotlin/Raindrops.kt
+++ kotlin/raindrops/src/main/kotlin/Raindrops.kt
@@ -1,6 +1,14 @@
 object Raindrops {
 
-    fun convert(n: Int): String {
-        TODO("Implement this function to complete the task")
+    private val sounds = mapOf(3 to "Pling", 5 to "Plang", 7 to "Plong")
+
+    fun convert(n: Int): String = buildString {
+
+        sounds.forEach{
+            if (it.key % n == 0) {
+                append(it.value)
+            }
+        }
+        if (isEmpty()) append(n)
     }
 }
