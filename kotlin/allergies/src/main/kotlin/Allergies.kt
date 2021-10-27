dd73d6d5183ff63ff70bebec7dc5035616b345a4
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-27 11:56:37 -0400

    allergen

--- kotlin/allergies/src/main/kotlin/Allergies.kt
+++ kotlin/allergies/src/main/kotlin/Allergies.kt
@@ -1,11 +1,6 @@
-class Allergies {
-    // TODO: implement proper constructor to complete the task
+class Allergies(private val score: Int) {
 
-    fun getList(): List<Allergen> {
-        TODO("Implement the function to complete the task")
-    }
+    fun getList() = Allergen.values().flatMap { if (isAllergicTo(it)) listOf(it) else listOf() }.toList()
 
-    fun isAllergicTo(allergen: Allergen) {
-        TODO("Implement the function to complete the task")
-    }
+    fun isAllergicTo(allergen: Allergen) = score and allergen.score > 0
 }
