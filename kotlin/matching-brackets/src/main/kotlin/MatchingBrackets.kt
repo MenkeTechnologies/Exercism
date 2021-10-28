c14fead2ac23b1f3a998cb12ed339fd7d564d1ae
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-28 13:48:01 -0400

    dw

--- kotlin/matching-brackets/src/main/kotlin/MatchingBrackets.kt
+++ kotlin/matching-brackets/src/main/kotlin/MatchingBrackets.kt
@@ -1,6 +1,23 @@
 object MatchingBrackets {
 
+    val map = mapOf("[" to "]", "{" to "}", "(" to ")")
+
     fun isValid(input: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val stk = mutableListOf<String>()
+
+        val remain = input.split("")
+            .dropWhile {
+                if (map.containsKey(it)) {
+                    stk += it
+                } else if (map.values.contains(it)) {
+                    if (stk.isEmpty() || map[stk.removeAt(stk.lastIndex)] != it) {
+                        return@dropWhile false
+                    }
+                }
+                return@dropWhile true
+            }.toList()
+
+        return stk.isEmpty() && remain.isEmpty()
+
     }
 }
