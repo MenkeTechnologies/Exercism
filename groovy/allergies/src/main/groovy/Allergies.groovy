b2aa03b4ef72569e55403cb16e05e571d136e4ea
MenkeTechnologies <linux.dev25@gmail.com>
2021-10-31 13:30:02 -0400

    all

--- groovy/allergies/src/main/groovy/Allergies.groovy
+++ groovy/allergies/src/main/groovy/Allergies.groovy
@@ -1,7 +1,37 @@
-class Allergies {
+enum Allergen {
+    EGGS(1),
+    PEANUTS(2),
+    SHELLFISH(4),
+    STRAWBERRIES(8),
+    TOMATOES(16),
+    CHOCOLATE(32),
+    POLLEN(64),
+    CATS(128)
 
-    Allergies(int score) {
-        throw new UnsupportedOperationException('method not implemented.')
+    int raw = 0
+
+    Allergen(int raw) {
+        this.raw = raw
+    }
+}
+
+class Allergies {
+    private int mask
+
+    Allergies(int mask) {
+        this.mask = mask
     }
 
+    def allergicTo(String str) {
+        def allergen = Allergen.valueOf(str.toUpperCase())
+        allergicTo(allergen)
+    }
+
+    def allergicTo(Allergen allergen) {
+        (mask & allergen.raw) > 0
+    }
+
+    ArrayList<String> list() {
+        Allergen.values().findAll { allergicTo it }.collect { it.toString().toLowerCase() }.toList()
+    }
 }
