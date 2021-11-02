a088915433b5f6f1d39f4e226ad46e3c9bd3132c
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-02 12:20:36 -0400

    pn

--- groovy/phone-number/src/main/groovy/PhoneNumber.groovy
+++ groovy/phone-number/src/main/groovy/PhoneNumber.groovy
@@ -1,5 +1,13 @@
 class PhoneNumber {
+    static final def REGEX = /^\s*(\+?1?\s*)?\(?([2-9]\d{2})\)?[-.\s]*([2-9]\d{2})[-.\s]*(\d{4})\s*$/
+
     static String clean(String input) {
-        throw new UnsupportedOperationException('method not implemented.')
+
+        def m = input =~ REGEX
+
+        if (!m) throw new Exception()
+
+        m[0][2] + m[0][3] + m[0][4]
+
     }
 }
