8bd148c4c7aaf5ed050cad491bc823997e4bef38
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-04 12:08:40 -0400

    coll

--- swift/collatz-conjecture/Sources/CollatzConjecture/CollatzConjecture.swift
+++ swift/collatz-conjecture/Sources/CollatzConjecture/CollatzConjecture.swift
@@ -1,1 +1,26 @@
 //Solution goes in Sources
+extension String: Error {
+}
+
+class CollatzConjecture {
+
+    public class func steps(_ i: Int) throws -> Int {
+
+        if i < 1 {
+            throw "err"
+        }
+        var cnt = 0
+        var j = i
+
+        while j != 1 {
+            if j % 2 == 0 {
+                j /= 2
+            } else {
+                j = 3 * j + 1
+
+            }
+            cnt += 1
+        }
+        return cnt
+    }
+}
