61fd45d5bb422e12376d11af2a7a037180f50c01
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-05 12:28:53 -0400

    all

--- swift/allergies/Sources/Allergies/Allergies.swift
+++ swift/allergies/Sources/Allergies/Allergies.swift
@@ -1,1 +1,23 @@
-//Solution goes in Sources
+enum Allergy: UInt, CaseIterable {
+    case eggs = 1
+    case peanuts = 2
+    case shellfish = 4
+    case strawberries = 8
+    case tomatoes = 16
+    case chocolate = 32
+    case pollen = 64
+    case cats = 128
+}
+
+class Allergies {
+
+    let mask: UInt
+
+    public init(_ mask: UInt) {
+        self.mask = mask
+    }
+
+    public func hasAllergy(_ allergy: Allergy) -> Bool {
+        allergy.rawValue & mask > 0
+    }
+}
