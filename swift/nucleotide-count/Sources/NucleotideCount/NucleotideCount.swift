30113fb6ec732539b3ae77badd6a52a6a84d8b72
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-06 12:41:18 -0400

    nc

--- swift/nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
+++ swift/nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
@@ -1,1 +1,21 @@
-//Solution goes in Sources
+class DNA {
+
+    private var map = ["T": 0, "A": 0, "C": 0, "G": 0]
+
+    public init?(strand: String) {
+
+        if strand.contains(where: { map[String($0)] == nil }) {
+            return nil
+        }
+
+        strand.map { String($0) }.forEach { map[$0]! += 1 }
+    }
+
+    public func count(_ s: String) -> Int {
+        map[s]!
+    }
+
+    public func counts() -> [String: Int] {
+        map
+    }
+}
