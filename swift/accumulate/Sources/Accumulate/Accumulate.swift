e3837c19530e2062dfb5aa8545ac4df9579aba29
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-07 12:55:56 -0500

    acc

--- swift/accumulate/Sources/Accumulate/Accumulate.swift
+++ swift/accumulate/Sources/Accumulate/Accumulate.swift
@@ -1,1 +1,16 @@
 //Solution goes in Sources
+
+extension Array {
+
+    func accumulate<T>(_ clo: (Element) -> T) -> [T] {
+
+        var r: [T] = []
+
+        forEach {
+            r.append(clo($0))
+        }
+
+        return r
+
+    }
+}
