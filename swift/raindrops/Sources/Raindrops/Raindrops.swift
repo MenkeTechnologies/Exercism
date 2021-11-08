6b2eb6705563648c40d1bedfd29e9b8bc8547108
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-08 10:28:31 -0500

    rd

--- swift/raindrops/Sources/Raindrops/Raindrops.swift
+++ swift/raindrops/Sources/Raindrops/Raindrops.swift
@@ -1,1 +1,31 @@
 //Solution goes in Sources
+
+class Raindrops {
+
+    public let sounds: String
+
+    public init(_ i: Int) {
+
+        var r = ""
+        if i % 3 == 0 {
+            r += "Pling"
+        }
+
+        if i % 5 == 0 {
+            r += "Plang"
+        }
+
+        if i % 7 == 0 {
+            r += "Plong"
+        }
+
+        if r.isEmpty {
+            r = "\(i)"
+        }
+
+        sounds = r
+
+    }
+
+
+}
