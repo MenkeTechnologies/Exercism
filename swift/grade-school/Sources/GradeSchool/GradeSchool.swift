9ac10d3d4d4ddfe23bb36b7ffba5d25ed008634a
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-09 11:14:03 -0500

    gs

--- swift/grade-school/Sources/GradeSchool/GradeSchool.swift
+++ swift/grade-school/Sources/GradeSchool/GradeSchool.swift
@@ -1,1 +1,19 @@
-//Solution goes in Sources
+class GradeSchool {
+
+    var roster: [Int: [String]] = [:]
+
+    public var sortedRoster: [Int: [String]] {
+        roster.reduce(into: [:]) {
+            $0[$1.key] = $1.value.sorted()
+        }
+    }
+
+    public func addStudent(_ name: String, grade: Int) {
+        roster[grade, default: []].append(name)
+    }
+
+    public func studentsInGrade(_ grade: Int) -> [String] {
+
+        roster[grade, default: []]
+    }
+}
