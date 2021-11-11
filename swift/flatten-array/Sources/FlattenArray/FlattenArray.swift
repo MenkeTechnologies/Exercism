1aeb0c6e449e0a5a5d9aeb5204c0601ee7f358d5
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-11 10:58:50 -0500

    fa

--- swift/flatten-array/Sources/FlattenArray/FlattenArray.swift
+++ swift/flatten-array/Sources/FlattenArray/FlattenArray.swift
@@ -1,1 +1,6 @@
-//Solution goes in Sources
+func flattenArray<T>(_ array: [Any?]) -> [T] {
+
+    array.flatMap{($0 as? [Any]).map{flattenArray($0)} ?? [$0]}
+    .filter{$0 as? T != nil}.map{$0 as! T}
+}
+
