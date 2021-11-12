ef36afed91561590bca84c5a1282d3b371ab8e2d
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-12 14:45:06 -0500

    acro

--- swift/acronym/Sources/Acronym/Acronym.swift
+++ swift/acronym/Sources/Acronym/Acronym.swift
@@ -1,1 +1,16 @@
-//Solution goes in Sources
+import Foundation
+
+class Acronym {
+
+    public class func abbreviate(_ text: String) -> String {
+
+        try! NSRegularExpression(pattern: #"\b\w|[A-Z][a-z]"#)
+                .matches(in: text, range: NSRange(text.startIndex..., in: text))
+                .compactMap { Range($0.range, in: text) }
+                .compactMap { text[$0].first }
+                .map { String($0) }
+                .joined().uppercased()
+
+
+    }
+}
