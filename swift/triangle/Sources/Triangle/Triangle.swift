4b0bee26926807f7e304dbbcd18929dfea6b21d2
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-13 06:17:54 -0500

    tri

--- swift/triangle/Sources/Triangle/Triangle.swift
+++ swift/triangle/Sources/Triangle/Triangle.swift
@@ -1,1 +1,33 @@
-//Solution goes in Sources
+class Triangle {
+
+    public var kind: String = ""
+
+    public init(_ i: Double, _ i2: Double, _ i3: Double) {
+        let doubles = [i, i2, i3].sorted()
+
+        doubles.forEach {
+            if $0 <= 0 {
+                kind = "ErrorKind"
+            }
+        }
+
+        if doubles[0] + doubles[1] < doubles[2] {
+            kind = "ErrorKind"
+        }
+
+        if !kind.isEmpty {
+            return
+        }
+
+        let s = Set(doubles)
+
+        if s.count == 1 {
+            kind = "Equilateral"
+        } else if s.count == 3 {
+            kind = "Scalene"
+        } else {
+            kind = "Isosceles"
+        }
+
+    }
+}
