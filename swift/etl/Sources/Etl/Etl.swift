192255692ce8762441dd8e88b32bf687038ac3b1
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-15 09:46:41 -0500

    etl

--- swift/etl/Sources/Etl/Etl.swift
+++ swift/etl/Sources/Etl/Etl.swift
@@ -1,1 +1,15 @@
-//Solution goes in Sources
+class ETL {
+
+    public class func transform(_ old: [Int: [String]]) -> [String: Int] {
+
+        var new: [String: Int] = [:]
+
+        old.forEach {
+            let key = $0.key
+            $0.value.forEach {
+                new[$0.lowercased()] = key
+            }
+        }
+        return new
+    }
+}
