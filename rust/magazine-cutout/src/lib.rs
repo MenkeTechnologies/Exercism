bbac8b132cfeca09872e648ab9682f7f5b115be4
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-18 10:22:33 -0500

    rpn

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -5,5 +5,25 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut map: HashMap<&str, i32> = HashMap::new();
+
+    for &word in magazine {
+        map.insert(word, *map.get(word).get_or_insert(&0) + 1);
+    }
+
+    for &word in note {
+
+        match map.get_mut(word) {
+            None => return false,
+            Some(&mut v) => {
+                if v == 0 {
+                    return false
+                }
+
+                map.insert(word, v - 1);
+            }
+        }
+    }
+
+    true
 }
--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -7,9 +7,44 @@ pub enum CalculatorInput {
     Value(i32),
 }
 
+pub fn do_op(op: &CalculatorInput, stk: &mut Vec<i32>) -> Option<i32> {
+    match stk.pop() {
+        None => None,
+        Some(n1) => {
+            match stk.pop() {
+                None => None,
+                Some(n2) => {
+                    match op {
+                        CalculatorInput::Add => Some(n2 + n1),
+                        CalculatorInput::Subtract => Some(n2 - n1),
+                        CalculatorInput::Multiply => Some(n2 * n1),
+                        CalculatorInput::Divide => Some(n2 / n1),
+                        CalculatorInput::Value(_v) => None
+                    }
+                }
+            }
+        }
+    }
+}
+
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    unimplemented!(
-		"Given the inputs: {:?}, evaluate them as though they were a Reverse Polish notation expression",
-		inputs,
-	);
+    let mut stk = vec![];
+
+    for it in inputs {
+        match it {
+            CalculatorInput::Value(v) => stk.push(*v),
+            _ => {
+                match do_op(it, &mut stk) {
+                    Some(r) => stk.push(r),
+                    None => return None
+                }
+            }
+        }
+    }
+
+    if stk.len() > 1 {
+        None
+    } else {
+        stk.pop()
+    }
 }
