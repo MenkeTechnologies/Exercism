fee7793d784815c23a080331a17196a32146421f
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-19 10:40:05 -0500

    las

--- swift/lasagna/Sources/Lasagna/Lasagna.swift
+++ swift/lasagna/Sources/Lasagna/Lasagna.swift
@@ -1,7 +1,14 @@
-// TODO: define the 'expectedMinutesInOven' constant
+let expectedMinutesInOven = 40
 
-// TODO: define the 'remainingMinutesInOven' function
+func remainingMinutesInOven(elapsedMinutes: Int) -> Int {
+    expectedMinutesInOven - elapsedMinutes
+}
 
-// TODO: define the 'preparationTimeInMinutes' function
+func preparationTimeInMinutes(layers: Int) -> Int {
+    layers * 2
+}
 
-// TODO: define the 'totalTimeInMinutes' function
+func totalTimeInMinutes(layers: Int, elapsedMinutes: Int) -> Int {
+    preparationTimeInMinutes(layers: layers) + elapsedMinutes
+
+}
--- swift/lasagna/Tests/LasagnaTests/LasagnaTests.swift
+++ swift/lasagna/Tests/LasagnaTests/LasagnaTests.swift
@@ -1,6 +1,7 @@
 import XCTest
 @testable import Lasagna
 
+
 final class LasagnaTests: XCTestCase {
   let runAll = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
 
@@ -9,17 +10,15 @@ final class LasagnaTests: XCTestCase {
   }
 
   func testRemainingMinutes() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
     XCTAssertEqual(remainingMinutesInOven(elapsedMinutes: 13), 27)
   }
 
+
   func testPreperationMinutes() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
     XCTAssertEqual(preparationTimeInMinutes(layers: 6), 12)
   }
 
   func testTotalMinutes() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
     XCTAssertEqual(totalTimeInMinutes(layers: 6, elapsedMinutes: 13), 25)
   }
 
