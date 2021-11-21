a53c58222f8ef65eab3eceaf6b3f1bb42d4557ec
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-21 13:22:54 -0500

    fr

--- swift/freelancer-rates/Tests/FreelancerRatesTests/FreelancerRatesTests.swift
+++ swift/freelancer-rates/Tests/FreelancerRatesTests/FreelancerRatesTests.swift
@@ -10,23 +10,19 @@ final class FreelancerRatesTests: XCTestCase {
   }
 
   func testmonthlyRoundDown() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
     XCTAssertEqual(monthlyRateFrom(hourlyRate: 77, withDiscount: 10.5), 12129, accuracy: 0.001)
   }
 
   func testmonthlyRoundUp() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
     XCTAssertEqual(monthlyRateFrom(hourlyRate: 80, withDiscount: 10.5), 12602, accuracy: 0.001)
   }
 
   func testworkdaysIn() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
     XCTAssertEqual(
       workdaysIn(budget: 20_000, hourlyRate: 80, withDiscount: 11), 35.0, accuracy: 0.001)
   }
 
   func testworkdaysShouldRoundDown() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
     XCTAssertEqual(
       workdaysIn(budget: 25_000, hourlyRate: 80, withDiscount: 11), 43, accuracy: 0.001)
   }
