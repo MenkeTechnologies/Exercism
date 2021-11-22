c9cd06689eb46b55dcd1f51a6c8d3e8d0280bcf3
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-22 11:19:25 -0500

    vp

--- swift/vehicle-purchase/Sources/VehiclePurchase/VehiclePurchase.swift
+++ swift/vehicle-purchase/Sources/VehiclePurchase/VehiclePurchase.swift
@@ -1,11 +1,43 @@
 func canIBuy(vehicle: String, price: Double, monthlyBudget: Double) -> String {
-  fatalError("Please implement the canIBuy(vehicle:price:monthlyBudget:) function")
+    let regular = monthlyBudget * 5 * 12
+    let within10 = monthlyBudget * (5 * 12 + 10)
+    if price > regular {
+        if price > within10 {
+            return "Darn! No \(vehicle) for me"
+        }
+        return "I'll have to be frugal if I want a \(vehicle)"
+    } else {
+
+        return "Yes! I'm getting a \(vehicle)"
+    }
 }
 
 func licenseType(numberOfWheels wheels: Int) -> String {
-  fatalError("Please implement licenseType(numberOfWheels:) function")
+
+    switch wheels {
+    case 2, 3:
+        return "You will need a motorcycle license for your vehicle"
+    case 4, 6:
+        return "You will need an automobile license for your vehicle"
+    case 18:
+        return "You will need a commercial trucking license for your vehicle"
+    default:
+        return "We do not issue licenses for those types of vehicles"
+    }
+
 }
 
 func registrationFee(msrp: Int, yearsOld: Int) -> Int {
-  fatalError("Please implement registrationFee(msrp:yearsOld:) function")
+    if yearsOld >= 10 {
+        return 25
+    }
+
+    var base = Double(msrp > 25000 ? msrp : 25000)
+
+    base -= Double(yearsOld) * base / 10
+
+    base /= 100
+
+    return Int(base.rounded(.down))
+
 }
