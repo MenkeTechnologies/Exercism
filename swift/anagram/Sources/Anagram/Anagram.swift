73ef49c58dd12b65c582c2c2f004c27098237e3d
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-23 10:58:19 -0500

    ana

--- swift/anagram/Sources/Anagram/Anagram.swift
+++ swift/anagram/Sources/Anagram/Anagram.swift
@@ -1,1 +1,16 @@
-//Solution goes in Sources
+class Anagram {
+
+    var lc: String
+    var sorted: [Character]
+
+    init(word: String) {
+        lc = word.lowercased()
+        sorted = lc.sorted()
+    }
+
+    public func match(_ strings: [String]) -> [String] {
+        strings.filter {
+            lc != $0.lowercased() && sorted == $0.lowercased().sorted()
+        }
+    }
+}
