e149aa74955976e0b9c94cdf983b0a9ee3b76d86
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-24 12:56:18 -0500

    rn

--- swift/robot-name/Sources/RobotName/RobotName.swift
+++ swift/robot-name/Sources/RobotName/RobotName.swift
@@ -1,1 +1,34 @@
-//Solution goes in Sources
+class Robot {
+
+    var name = ""
+    let ltrs = UInt8(ascii: "A")...UInt8(ascii: "Z")
+    let nums = UInt8(ascii: "0")...UInt8(ascii: "9")
+
+    init() {
+        name = genName()
+    }
+
+    func randomLet() -> String {
+
+        String(UnicodeScalar(ltrs.randomElement()!))
+
+    }
+
+    func randomNum() -> String {
+
+        String(UnicodeScalar(nums.randomElement()!))
+
+    }
+
+    func genName() -> String {
+        ((0..<2).map { _ in
+            randomLet()
+        } + (0..<3).map { _ in
+            randomNum()
+        }).joined()
+    }
+
+    public func resetName() {
+        name = genName()
+    }
+}
