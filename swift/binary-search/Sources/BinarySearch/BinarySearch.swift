08aff832261372b8990c9031ba76fbf4fec19e78
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-25 15:11:32 -0500

    bs

--- swift/binary-search/Sources/BinarySearch/BinarySearch.swift
+++ swift/binary-search/Sources/BinarySearch/BinarySearch.swift
@@ -1,1 +1,55 @@
 //Solution goes in Sources
+
+enum BinarySearchError: Error {
+
+    case unsorted
+}
+
+class BinarySearch {
+
+    public var list: [Int]
+
+    public var middle: Int
+
+    public init(_ ary: [Int]) throws {
+
+        if ary != ary.sorted() {
+            throw BinarySearchError.unsorted
+        }
+
+        list = ary
+
+        middle = list.count / 2
+
+
+    }
+
+    public func searchFor(_ search: Int) -> Int? {
+
+        if list.isEmpty {
+            return nil
+        }
+
+        var left = 0
+        var right = list.count - 1
+
+        while true {
+
+            if left > right {
+                return nil
+            }
+
+            let mid = (left + right) / 2
+
+            if search == list[mid] {
+                return mid
+            } else if search < list[mid] {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+
+        }
+
+    }
+}
