55b241127ef23c86000005285bd9d27b4f8a8b09
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-26 10:13:32 -0500

    pn

--- swift/perfect-numbers/Sources/PerfectNumbers/PerfectNumbers.swift
+++ swift/perfect-numbers/Sources/PerfectNumbers/PerfectNumbers.swift
@@ -1,1 +1,26 @@
-//Solution goes in Sources
+enum NumberType {
+    case perfect
+    case deficient
+    case abundant
+}
+
+class NumberClassifier {
+
+    public var classification: NumberType
+
+    public init(number: Int) {
+
+        let sum = (1..<number).filter {
+            number % $0 == 0
+        }.reduce(0, +)
+
+        if sum < number {
+            classification = .deficient
+        } else if sum > number {
+            classification = .abundant
+        } else {
+            classification = .perfect
+        }
+
+    }
+}
