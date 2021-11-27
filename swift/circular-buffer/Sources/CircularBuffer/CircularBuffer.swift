17f8b6ffc7a79ee0efdea978c4fc7590f4637863
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-27 06:41:45 -0500

    cb

--- swift/circular-buffer/Sources/CircularBuffer/CircularBuffer.swift
+++ swift/circular-buffer/Sources/CircularBuffer/CircularBuffer.swift
@@ -1,1 +1,53 @@
-//Solution goes in Sources
+enum CircularBufferError: Error {
+    case bufferEmpty
+    case bufferFull
+}
+
+class CircularBuffer<T> {
+
+    let capacity: Int
+    var array: [T] = []
+
+
+    var full: Bool {
+        array.count == capacity
+    }
+
+    init(capacity: Int) {
+        self.capacity = capacity
+    }
+
+    public func read() throws -> T {
+
+        if array.isEmpty {
+            throw CircularBufferError.bufferEmpty
+        }
+
+        return array.removeFirst()
+
+    }
+
+
+    public func write(_ itm: T) throws {
+
+        if full {
+            throw CircularBufferError.bufferFull
+        }
+
+        array.append(itm)
+
+    }
+
+    public func overwrite(_ itm: T) {
+
+        if full {
+            array.remove(at: 0)
+        }
+        try? write(itm)
+
+    }
+
+    public func clear() {
+        array.removeAll(keepingCapacity: true)
+    }
+}
