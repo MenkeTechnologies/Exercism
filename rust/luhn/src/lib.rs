918f1d40eaef5d43637a378249eff6b62583a08c
MenkeTechnologies <linux.dev25@gmail.com>
2021-11-30 12:41:25 -0500

    luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,39 +1,28 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let code_no_space: String = code.chars().filter(|c| !c.is_whitespace()).collect();
-    if code_no_space.len() <= 1 {
+    let invalid: Vec<char> = code.chars().filter(|ch| !(ch.is_whitespace() || ch.is_numeric())).collect();
+
+    if !invalid.is_empty() {
+        return false;
+    }
+    let no_ws: String = code.chars().filter(|ch| !ch.is_whitespace()).collect();
+
+    if no_ws.len() < 2 {
         return false;
     }
 
-    let mut str = String::new();
-    let mut cnt = 1;
-    for ch in code_no_space.chars().rev() {
-        if !ch.is_numeric() {
-            return false;
-        }
-        if cnt % 2 == 0 {
-            let mut doubled = ch.to_digit(10).unwrap() * 2;
+    let sum = no_ws.chars().rev().map(|ch| ch.to_digit(10).unwrap()).enumerate().fold(0, |mut sum, (cnt, int)| {
+        if cnt % 2 == 1 {
+            let mut doubled = int * 2;
             if doubled > 9 {
                 doubled -= 9;
             }
-
-            let i3 = (doubled + 48) as u8;
-            let doubled_ch = char::from(i3);
-            str.push(doubled_ch);
+            sum += doubled
         } else {
-            str.push(ch)
+            sum += int
         }
+        sum
+    });
 
-        cnt += 1
-    }
-
-    let sum = str
-        .chars()
-        .fold(0, |sum, ch| sum + ch.to_digit(10).unwrap());
-
-    if sum % 10 != 0 {
-        return false;
-    }
-
-    true
+    sum % 10 == 0
 }
