433801ef60ca13523ca05fe88d61fb33dc582a63
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-01 09:36:18 -0500

    clok

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -7,10 +7,13 @@ pub struct Clock {
 }
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut fin_h = hours;
-        let mut fin_m = minutes;
+    pub fn new(hr: i32, min: i32) -> Self {
+        let (hours, minutes) = Self::adjust(hr, min);
 
+        Clock { hours, minutes }
+    }
+
+    fn adjust(mut fin_h: i32, mut fin_m: i32) -> (i32, i32) {
         while fin_m >= 60 {
             fin_h += 1;
             fin_m -= 60;
@@ -26,10 +29,7 @@ impl Clock {
             fin_h += 24
         }
 
-        Clock {
-            hours: fin_h,
-            minutes: fin_m,
-        }
+        (fin_h, fin_m)
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
