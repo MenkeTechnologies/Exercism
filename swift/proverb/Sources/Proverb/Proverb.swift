b97b51d72245ebf93596ccbaaa65054032d9c466
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-03 10:30:00 -0500

    pro

--- swift/proverb/Sources/Proverb/Proverb.swift
+++ swift/proverb/Sources/Proverb/Proverb.swift
@@ -1,1 +1,19 @@
 //Solution goes in Sources
+extension Array where Element == String {
+
+    func recite() -> String {
+
+        indices.reduce("") {
+
+            if ($1 != count - 1) {
+                return $0 + "For want of a \(self[$1]) the \(self[$1 + 1]) was lost.\n"
+
+            } else {
+                return $0 + "And all for the want of a \(first!)."
+            }
+
+        }
+
+    }
+
+}
