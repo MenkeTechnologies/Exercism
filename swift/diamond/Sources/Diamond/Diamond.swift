88f58aa01af700d8a6f8adb04620b64d7b5316d9
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-04 17:19:57 -0500

    diamond

--- swift/diamond/Sources/Diamond/Diamond.swift
+++ swift/diamond/Sources/Diamond/Diamond.swift
@@ -1,1 +1,25 @@
-//Solution goes in Sources
+class Diamond {
+
+    public class func makeDiamond(letter: Character) -> [String] {
+
+        let letters = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        let SPACE = " "
+
+        guard let top = letters.firstIndex(of: letter) else {
+            return [""]
+        }
+
+        let asc = 0...top
+
+        return (asc + asc.reversed().dropFirst()).map{ letterIdx in
+            (asc.reversed() + asc.dropFirst()).reduce("") { str, midlineOffset in
+                if midlineOffset == letterIdx {
+                    return str + String(letters[letterIdx])
+                } else  {
+                    return str + SPACE
+                }
+            }
+        }
+
+    }
+}
