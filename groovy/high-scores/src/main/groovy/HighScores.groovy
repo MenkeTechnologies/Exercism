c6f7b316929c81a180f60dc920ead40327bc7120
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-08 11:47:03 -0500

    hs

--- groovy/high-scores/src/main/groovy/HighScores.groovy
+++ groovy/high-scores/src/main/groovy/HighScores.groovy
@@ -1,6 +1,21 @@
 class HighScores {
 
-    HighScores() {
-        throw new UnsupportedOperationException('method not implemented.')
+    ArrayList<Integer> scores
+
+    HighScores(integers) {
+        scores = integers
+    }
+
+    def personalTopThree() {
+        scores.toSorted().reverse().take(3)
+    }
+
+    def personalBest() {
+        scores.toSorted().last()
+    }
+
+    def latest() {
+        scores.last()
+
     }
 }
