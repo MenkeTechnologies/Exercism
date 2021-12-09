c122fb5655fb63161ce6a1239dbedaf51f343588
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-09 12:19:27 -0500

    pn

--- groovy/perfect-numbers/src/main/groovy/PerfectNumbers.groovy
+++ groovy/perfect-numbers/src/main/groovy/PerfectNumbers.groovy
@@ -1,6 +1,26 @@
 class PerfectNumbers {
 
     static Classification classify(int num) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+
+        if (num == 1) {
+            return Classification.DEFICIENT
+        }
+        if (num <= 0) {
+            throw new ArithmeticException()
+        }
+
+        def upper = num - 1
+        def sum = (1..upper).findAll { num % it == 0 }.sum()
+
+        if (sum == num) {
+            Classification.PERFECT
+        } else if (sum < num) {
+            Classification.DEFICIENT
+
+        } else {
+            Classification.ABUNDANT
+
+        }
+
     }
 }
