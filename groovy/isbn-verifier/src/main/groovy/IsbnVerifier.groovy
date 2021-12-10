96480bfc6e6af32ddc7ddf52428314c544482058
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-10 02:48:37 -0500

    iv

--- groovy/isbn-verifier/src/main/groovy/IsbnVerifier.groovy
+++ groovy/isbn-verifier/src/main/groovy/IsbnVerifier.groovy
@@ -1,5 +1,10 @@
 class IsbnVerifier {
     static boolean isValid(String isbn) {
-        throw new UnsupportedOperationException('method not implemented.')
+        if (!isbn.matches(/^\d-?\d{3}-?\d{5}-?[\dX]$/)) {
+            return false
+        }
+        isbn.reverse().collect{it as char}.findAll { it.letterOrDigit }.withIndex().collect { ch, i ->
+            ch == 'X' ? 10 : Character.getNumericValue(ch) * (i + 1)
+        }.sum() % 11 == 0
     }
 }
