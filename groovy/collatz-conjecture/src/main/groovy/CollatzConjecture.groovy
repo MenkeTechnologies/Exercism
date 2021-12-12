3db8a87c2f83c1b01d1641af00dbc6bf6148c14d
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-12 14:16:30 -0500

    cc

--- groovy/collatz-conjecture/src/main/groovy/CollatzConjecture.groovy
+++ groovy/collatz-conjecture/src/main/groovy/CollatzConjecture.groovy
@@ -1,6 +1,24 @@
 class CollatzConjecture {
 
     static int steps(int number) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+
+        if (number <= 0) {
+            throw new ArithmeticException()
+        }
+
+        int steps = 0
+
+        while (number != 1) {
+            if (number % 2 == 0) {
+                number /= 2
+            } else {
+                number = 3 * number + 1
+            }
+
+            ++steps
+        }
+
+        steps
+
     }
 }
