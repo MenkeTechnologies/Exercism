53b9900a399f13d9e5c37018502cbb6b24ff73b7
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-17 07:58:31 -0500

    elixir hw

---
+++ elixir/lasagna/lib/lasagna.ex
@@ -0,0 +1,21 @@
+defmodule Lasagna do
+  def expected_minutes_in_oven do
+    40
+  end
+
+  def remaining_minutes_in_oven(time) do
+    expected_minutes_in_oven() - time
+  end
+
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
+
+  def total_time_in_minutes(layers, oven_time) do
+    preparation_time_in_minutes(layers) + oven_time
+  end
+
+  def alarm do
+    "Ding!"
+  end
+end
