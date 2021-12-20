3e59f3171bf730fca59d7b32978997ef25f9597e
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-19 19:41:51 -0500

    iso

--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -1,6 +1,8 @@
 object Isogram {
 
     fun isIsogram(input: String): Boolean {
-        TODO("Implement this function to complete the task")
+        return input.toLowerCase().filter { it.isLetter() }.groupBy { it }.values.none { it.size > 1 }
     }
+
+
 }
