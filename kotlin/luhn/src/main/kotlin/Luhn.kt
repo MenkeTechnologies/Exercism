c8cf4b4c8a80654bff66debdc77de2d52fd9a0c8
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-21 10:08:55 -0500

    luhn

--- kotlin/luhn/src/main/kotlin/Luhn.kt
+++ kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -1,6 +1,18 @@
 object Luhn {
 
     fun isValid(candidate: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val digits = candidate.filter { !it.isWhitespace() }.toList()
+
+        if (digits.size < 2 || digits.any { !it.isDigit() }) {
+            return false
+        }
+
+        return digits.reversed().map { Character.getNumericValue(it) }
+                .withIndex().map {
+                    val dbl = if (it.index % 2 == 1) it.value * 2 else it.value
+
+                    if (dbl > 9) dbl - 9 else dbl
+                }.sum() % 10 == 0
+
     }
 }
