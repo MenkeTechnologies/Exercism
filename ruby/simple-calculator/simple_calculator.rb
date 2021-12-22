18e01d276a593a933e29f32d81791c7057948b25
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-22 10:06:32 -0500

    sc

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -1,7 +1,20 @@
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
+  class UnsupportedOperation < StandardError
+  end
+
   def self.calculate(first_operand, second_operand, operation)
-    raise 'Please implement the SimpleCalculator.calculate method'
+
+    raise ArgumentError unless first_operand.is_a?(Integer) && second_operand.is_a?(Integer)
+
+    raise UnsupportedOperation unless ALLOWED_OPERATIONS.include? operation
+
+    return 'Division by zero is not allowed.' if second_operand.zero? && operation == '/'
+
+    answer = first_operand.send(operation, second_operand)
+
+    "#{first_operand} #{operation} #{second_operand} = #{answer}"
+
   end
 end
