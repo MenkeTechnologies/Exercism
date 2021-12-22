223de9557992f033adbf9455d7912745a8b4b3c3
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-22 09:43:29 -0500

    pn

--- kotlin/perfect-numbers/src/main/kotlin/PerfectNumbers.kt
+++ kotlin/perfect-numbers/src/main/kotlin/PerfectNumbers.kt
@@ -1,8 +1,19 @@
-
 enum class Classification {
     DEFICIENT, PERFECT, ABUNDANT
 }
 
 fun classify(naturalNumber: Int): Classification {
-    return Classification.DEFICIENT
+
+    require(naturalNumber > 0)
+
+    val su = (1..naturalNumber / 2).filter { naturalNumber % it == 0 }.sum()
+
+    return if (su == naturalNumber) {
+        Classification.PERFECT
+    } else if (su < naturalNumber) {
+        Classification.DEFICIENT
+    } else {
+        Classification.ABUNDANT
+    }
+
 }
