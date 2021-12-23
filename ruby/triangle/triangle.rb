2fbd8056d81ac5067ab4a6722191a2e4ca355019
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-23 11:24:35 -0500

    unlocked ruby

---
+++ ruby/triangle/triangle.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Triangle' exercise in this file. Make the tests in
+`triangle_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/triangle` directory.
+=end
