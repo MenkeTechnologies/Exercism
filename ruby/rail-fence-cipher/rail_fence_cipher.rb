2175544da9634d1233c5f8ff8e612fc83b95c739
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-23 11:30:58 -0500

    log

---
+++ ruby/rail-fence-cipher/rail_fence_cipher.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Rail Fence Cipher' exercise in this file. Make the tests in
+`rail_fence_cipher_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/rail-fence-cipher` directory.
+=end
