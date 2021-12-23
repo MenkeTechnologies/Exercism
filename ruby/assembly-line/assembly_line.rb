7cc460ed3a9b9cd76337049c6e2161fd596c3301
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-23 11:20:38 -0500

    asy

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -4,10 +4,21 @@ class AssemblyLine
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    fact = 1
+    if @speed >= 5 && @speed <= 8
+      fact = 0.9
+    elsif @speed == 9
+      fact = 0.8
+    elsif @speed == 10
+      fact = 0.77
+    end
+
+    fact * 221 * @speed
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+
+    (production_rate_per_hour / 60).floor
+
   end
 end
