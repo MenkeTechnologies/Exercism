36d12c86c086487a80f35715ac94041492132305
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-23 20:17:29 -0500

    remove skip

--- ruby/grep/grep_test.rb
+++ ruby/grep/grep_test.rb
@@ -57,7 +57,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_one_match_print_line_numbers_flag
-    skip
+
     pattern = "Forbidden"
     flags = ["-n"]
     files = ["paradise-lost.txt"]
@@ -69,7 +69,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_one_match_case_insensitive_flag
-    skip
+
     pattern = "FORBIDDEN"
     flags = ["-i"]
     files = ["paradise-lost.txt"]
@@ -81,7 +81,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_one_match_print_file_names_flag
-    skip
+
     pattern = "Forbidden"
     flags = ["-l"]
     files = ["paradise-lost.txt"]
@@ -93,7 +93,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_one_match_match_entire_lines_flag
-    skip
+
     pattern = "With loss of Eden, till one greater Man"
     flags = ["-x"]
     files = ["paradise-lost.txt"]
@@ -105,7 +105,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_one_match_multiple_flags
-    skip
+
     pattern = "OF ATREUS, Agamemnon, KIng of MEN."
     flags = ["-n", "-i", "-x"]
     files = ["iliad.txt"]
@@ -117,7 +117,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_several_matches_no_flags
-    skip
+
     pattern = "may"
     flags = []
     files = ["midsummer-night.txt"]
@@ -131,7 +131,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_several_matches_print_line_numbers_flag
-    skip
+
     pattern = "may"
     flags = ["-n"]
     files = ["midsummer-night.txt"]
@@ -145,7 +145,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_several_matches_match_entire_lines_flag
-    skip
+
     pattern = "may"
     flags = ["-x"]
     files = ["midsummer-night.txt"]
@@ -157,7 +157,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_several_matches_case_insensitive_flag
-    skip
+
     pattern = "ACHILLES"
     flags = ["-i"]
     files = ["iliad.txt"]
@@ -170,7 +170,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_several_matches_inverted_flag
-    skip
+
     pattern = "Of"
     flags = ["-v"]
     files = ["paradise-lost.txt"]
@@ -186,7 +186,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_no_matches_various_flags
-    skip
+
     pattern = "Gandalf"
     flags = ["-n", "-l", "-x", "-i"]
     files = ["iliad.txt"]
@@ -198,7 +198,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_one_match_file_flag_takes_precedence_over_line_flag
-    skip
+
     pattern = "ten"
     flags = ["-n", "-l"]
     files = ["iliad.txt"]
@@ -210,7 +210,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_several_matches_inverted_and_match_entire_lines_flags
-    skip
+
     pattern = "Illustrious into Ades premature,"
     flags = ["-x", "-v"]
     files = ["iliad.txt"]
@@ -229,7 +229,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_multiple_files_one_match_no_flags
-    skip
+
     pattern = "Agamemnon"
     flags = []
     files = ["iliad.txt", "midsummer-night.txt", "paradise-lost.txt"]
@@ -241,7 +241,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_multiple_files_several_matches_no_flags
-    skip
+
     pattern = "may"
     flags = []
     files = ["iliad.txt", "midsummer-night.txt", "paradise-lost.txt"]
@@ -255,7 +255,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_multiple_files_several_matches_print_line_numbers_flag
-    skip
+
     pattern = "that"
     flags = ["-n"]
     files = ["iliad.txt", "midsummer-night.txt", "paradise-lost.txt"]
@@ -270,7 +270,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_multiple_files_one_match_print_file_names_flag
-    skip
+
     pattern = "who"
     flags = ["-l"]
     files = ["iliad.txt", "midsummer-night.txt", "paradise-lost.txt"]
@@ -283,7 +283,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_multiple_files_several_matches_case_insensitive_flag
-    skip
+
     pattern = "TO"
     flags = ["-i"]
     files = ["iliad.txt", "midsummer-night.txt", "paradise-lost.txt"]
@@ -304,7 +304,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_multiple_files_several_matches_inverted_flag
-    skip
+
     pattern = "a"
     flags = ["-v"]
     files = ["iliad.txt", "midsummer-night.txt", "paradise-lost.txt"]
@@ -318,7 +318,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_multiple_files_one_match_match_entire_lines_flag
-    skip
+
     pattern = "But I beseech your grace that I may know"
     flags = ["-x"]
     files = ["iliad.txt", "midsummer-night.txt", "paradise-lost.txt"]
@@ -330,7 +330,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_multiple_files_one_match_multiple_flags
-    skip
+
     pattern = "WITH LOSS OF EDEN, TILL ONE GREATER MAN"
     flags = ["-n", "-i", "-x"]
     files = ["iliad.txt", "midsummer-night.txt", "paradise-lost.txt"]
@@ -342,7 +342,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_multiple_files_no_matches_various_flags
-    skip
+
     pattern = "Frodo"
     flags = ["-n", "-l", "-x", "-i"]
     files = ["iliad.txt", "midsummer-night.txt", "paradise-lost.txt"]
@@ -354,7 +354,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_multiple_files_several_matches_file_flag_takes_precedence_over_line_number_flag
-    skip
+
     pattern = "who"
     flags = ["-n", "-l"]
     files = ["iliad.txt", "midsummer-night.txt", "paradise-lost.txt"]
@@ -367,7 +367,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_multiple_files_several_matches_inverted_and_match_entire_lines_flags
-    skip
+
     pattern = "Illustrious into Ades premature,"
     flags = ["-x", "-v"]
     files = ["iliad.txt", "midsummer-night.txt", "paradise-lost.txt"]
--- ruby/kindergarten-garden/kindergarten_garden_test.rb
+++ ruby/kindergarten-garden/kindergarten_garden_test.rb
@@ -8,19 +8,19 @@ class GardenTest < Minitest::Test
   end
 
   def test_different_garden_for_alice
-    skip
+
     garden = Garden.new("VC\nRC")
     assert_equal %i[violets clover radishes clover], garden.alice
   end
 
   def test_bobs_garden
-    skip
+
     garden = Garden.new("VVCG\nVVRC")
     assert_equal %i[clover grass radishes clover], garden.bob
   end
 
   def test_bob_and_charlies_gardens
-    skip
+
     garden = Garden.new("VVCCGG\nVVCCGG")
     assert_equal %i[clover clover clover clover], garden.bob
     assert_equal %i[grass grass grass grass], garden.charlie
@@ -29,7 +29,7 @@ end
 
 class TestFullGarden < Minitest::Test
   def setup
-    skip
+
     diagram = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV"
     @garden = Garden.new(diagram)
   end
@@ -37,69 +37,69 @@ class TestFullGarden < Minitest::Test
   attr_reader :garden
 
   def test_alice
-    skip
+
     assert_equal %i[violets radishes violets radishes], garden.alice
   end
 
   def test_bob
-    skip
+
     assert_equal %i[clover grass clover clover], garden.bob
   end
 
   def test_charlie
-    skip
+
     assert_equal %i[violets violets clover grass], garden.charlie
   end
 
   def test_david
-    skip
+
     assert_equal %i[radishes violets clover radishes], garden.david
   end
 
   def test_eve
-    skip
+
     assert_equal %i[clover grass radishes grass], garden.eve
   end
 
   def test_fred
-    skip
+
     assert_equal %i[grass clover violets clover], garden.fred
   end
 
   def test_ginny
-    skip
+
     assert_equal %i[clover grass grass clover], garden.ginny
   end
 
   def test_harriet
-    skip
+
     assert_equal %i[violets radishes radishes violets], garden.harriet
   end
 
   def test_ileana
-    skip
+
     assert_equal %i[grass clover violets clover], garden.ileana
   end
 
   def test_joseph
-    skip
+
     assert_equal %i[violets clover violets grass], garden.joseph
   end
 
   def test_kincaid
-    skip
+
     assert_equal %i[grass clover clover grass], garden.kincaid
   end
 
   def test_larry
-    skip
+
     assert_equal %i[grass violets clover violets], garden.larry
   end
 end
 
 class DisorderedTest < Minitest::Test
   def setup
-    skip
+
     diagram = "VCRRGVRG\nRVGCCGCV"
     students = %w[Samantha Patricia Xander Roger]
     @garden = Garden.new(diagram, students)
@@ -108,22 +108,22 @@ class DisorderedTest < Minitest::Test
   attr_reader :garden
 
   def test_patricia
-    skip
+
     assert_equal %i[violets clover radishes violets], garden.patricia
   end
 
   def test_roger
-    skip
+
     assert_equal %i[radishes radishes grass clover], garden.roger
   end
 
   def test_samantha
-    skip
+
     assert_equal %i[grass violets clover grass], garden.samantha
   end
 
   def test_xander
-    skip
+
     assert_equal %i[radishes grass clover violets], garden.xander
   end
 end
@@ -142,7 +142,7 @@ class TwoGardensDifferentStudents < Minitest::Test
   end
 
   def test_bob_and_charlie_per_garden
-    skip
+
     assert_equal %i[radishes radishes grass clover], garden1.bob
     assert_equal %i[violets clover radishes violets], garden2.bob
     assert_equal %i[grass violets clover grass], garden1.charlie
