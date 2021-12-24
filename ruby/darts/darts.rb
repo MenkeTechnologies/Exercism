f794241af500fe1de9f574348569d9d6cccc5298
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-23 19:42:50 -0500

    darts

--- ruby/darts/darts.rb
+++ ruby/darts/darts.rb
@@ -5,3 +5,23 @@ Write your code for the 'Darts' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/darts` directory.
 =end
+
+class Darts
+  def initialize x, y
+    @x = x
+    @y = y
+    @dist = ((@x - 0)**2 + (@y - 0)**2)**0.5
+  end
+
+  def score
+    if @dist > 10
+      0
+    elsif @dist > 5
+      1
+    elsif @dist > 1
+      5
+    else
+      10
+    end
+  end
+end
