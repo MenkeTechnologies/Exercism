56eb4302fb22d70494e7e41d43a798c0ff2b2d28
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-25 04:30:45 -0500

    more rb

---
+++ ruby/resistor-color/resistor_color.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Resistor Color' exercise in this file. Make the tests in
+`resistor_color_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/resistor-color` directory.
+=end
