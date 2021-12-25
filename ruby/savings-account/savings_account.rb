22b57f579b3473d945902a526a4d83d57db7a93b
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-25 04:17:15 -0500

    sa

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -1,13 +1,27 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    if balance >= 5000
+      2.475
+    elsif balance >= 1000
+      1.621
+    elsif balance >= 0
+      0.5
+    else
+      -3.213
+    end
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    var = balance.abs * interest_rate(balance) / 100
+    balance + var
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    cnt = 0
+    while current_balance < desired_balance
+      current_balance = annual_balance_update(current_balance)
+      cnt += 1
+    end
+    cnt
   end
 end
