fbb9e1cf1324af0cab0b2ae563b5dfffe891481b
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-24 19:32:16 -0500

    iso

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -5,3 +5,12 @@ Write your code for the 'Isogram' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/isogram` directory.
 =end
+
+class Isogram
+
+  def self.isogram?(input)
+
+    input.downcase.chars.reject { _1 =~ /[-\s]/ }.group_by { _1 }.values.none? { _1.size > 1 }
+
+  end
+end
