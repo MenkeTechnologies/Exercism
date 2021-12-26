9c6c2c491b215a98e6eb091712f453569b565193
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-26 12:31:34 -0500

    mg

--- ruby/moviegoer/moviegoer.rb
+++ ruby/moviegoer/moviegoer.rb
@@ -11,15 +11,15 @@ class Moviegoer
   end
 
   def ticket_price
-    raise 'Please implement the Moviegoer#ticket_price method'
+    @age < 60 ? 15 : 10
   end
 
   def watch_scary_movie?
-    raise 'Please implement the Moviegoer#watch_scary_movie method'
+    @age >= 18
   end
 
   # Popcorn is 🍿
   def claim_free_popcorn!
-    raise 'Please implement the Moviegoer#claim_free_popcorn method'
+    @member ? '🍿' : (raise NotMovieClubMemberError)
   end
 end
