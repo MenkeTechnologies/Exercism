11267e45e417619bb4e9e6c751fda3053db4dc9f
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-27 12:46:33 -0500

    sa

--- java/space-age/src/main/java/SpaceAge.java
+++ java/space-age/src/main/java/SpaceAge.java
@@ -1,43 +1,46 @@
 class SpaceAge {
 
+    Double seconds;
+    private final Double SEC_PER_Y = 31557600.;
+
     SpaceAge(double seconds) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.seconds = seconds;
     }
 
     double getSeconds() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.seconds;
     }
 
     double onEarth() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.seconds / SEC_PER_Y;
     }
 
     double onMercury() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 0.2408467;
     }
 
     double onVenus() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 0.61519726;
     }
 
     double onMars() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 1.8808158;
     }
 
     double onJupiter() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 11.862615;
     }
 
     double onSaturn() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 29.447498;
     }
 
     double onUranus() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 84.016846;
     }
 
     double onNeptune() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 164.79132;
     }
 
 }
