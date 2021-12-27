d46bb20aa185f5b0a8e73a5f3316ef41c9500d4c
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-27 13:18:59 -0500

    pf

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -5,3 +5,25 @@ Write your code for the 'Prime Factors' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/prime-factors` directory.
 =end
+
+class PrimeFactors
+
+  def self.of(start)
+
+    fac = []
+    divisor = 2
+
+    while start > 1
+      while (start % divisor).zero?
+        fac << divisor
+        start /= divisor
+      end
+
+      divisor += 1
+    end
+
+    fac
+
+  end
+
+end
