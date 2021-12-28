d5929020709fafded44601b85197074f6473f0d0
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-27 22:08:43 -0500

    reformat

--- kotlin/pangram/src/main/kotlin/Pangram.kt
+++ kotlin/pangram/src/main/kotlin/Pangram.kt
@@ -3,6 +3,6 @@ object Pangram {
     fun isPangram(input: String): Boolean {
         val lc = input.toLowerCase()
 
-        return ('a'..'z').all{lc.contains(it)};
+        return ('a'..'z').all { lc.contains(it) }
     }
 }
