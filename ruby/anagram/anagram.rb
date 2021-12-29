04b556d6682b254c551331bc8c7de66a29e014ee
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-29 13:41:07 -0500

    ana

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -5,3 +5,18 @@ Write your code for the 'Anagram' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/anagram` directory.
 =end
+
+class Anagram
+
+  def initialize(str)
+
+    @str = str.downcase
+    @letters = @str.chars.sort
+
+  end
+
+  def match(ary)
+    ary.filter { _1.downcase.chars.sort == @letters && @str != _1.downcase }
+  end
+
+end
