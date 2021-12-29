7cf421489f4649266e79b71e912641d684c56d6a
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-29 15:24:12 -0500

    wc

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -5,3 +5,13 @@ Write your code for the 'Word Count' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
+
+class Phrase
+  def initialize(str)
+    @w = str.downcase.scan(/\b[\w']+\b/).group_by { _1 }.map { [_1, _2.size] }.to_h
+  end
+
+  def word_count
+    @w
+  end
+end
