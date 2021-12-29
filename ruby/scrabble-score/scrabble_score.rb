e1b7bf3304f493c07193fd5f6328d4710c094c88
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-29 15:48:24 -0500

    ss

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,7 +1,36 @@
-=begin
-Write your code for the 'Scrabble Score' exercise in this file. Make the tests in
-`scrabble_score_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/scrabble-score` directory.
-=end
+# Write your code for the 'Scrabble Score' exercise in this file. Make the tests in
+# `scrabble_score_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/scrabble-score` directory.
+
+class Scrabble
+  SCORES = {
+    'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1, 'N' => 1,
+    'R' => 1, 'S' => 1, 'T' => 1,
+    'D' => 2, 'G' => 2,
+    'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3,
+    'F' => 4, 'H' => 4, 'V' => 4, 'W' => 4, 'Y' => 4,
+    'K' => 5,
+    'J' => 8, 'X' => 8,
+    'Q' => 10, 'Z' => 10
+  }.freeze
+
+  def initialize(str)
+    @str = str.nil? ? '' : str
+  end
+
+  def score
+    return 0 if @str.nil?
+
+    @str.upcase.chars.map { SCORES[_1] || 0 }.sum
+  end
+
+  def self.score(word)
+    Scrabble.new(word).score
+
+  end
+
+end
