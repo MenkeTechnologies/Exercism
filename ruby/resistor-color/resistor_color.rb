0084454f839441f0b5d00095898673db39c65780
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-29 22:35:01 -0500

    rc

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -5,3 +5,27 @@ Write your code for the 'Resistor Color' exercise in this file. Make the tests i
 To get started with TDD, see the `README.md` file in your
 `ruby/resistor-color` directory.
 =end
+
+class ResistorColor
+
+  COLOR_RULES = {
+    'black' => '0',
+    'brown' => '1',
+    'red' => '2',
+    'orange' => '3',
+    'yellow' => '4',
+    'green' => '5',
+    'blue' => '6',
+    'violet' => '7',
+    'grey' => '8',
+    'white' => '9'
+  }.freeze
+
+  COLORS = COLOR_RULES.keys
+
+  def self.color_code(str)
+
+    COLOR_RULES[str].to_i
+
+  end
+end
