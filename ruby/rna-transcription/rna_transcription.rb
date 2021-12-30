01fb8a2579dbd6809a8267affe4e59da638755d3
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-30 10:15:05 -0500

    rna

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -5,3 +5,19 @@ Write your code for the 'Rna Transcription' exercise in this file. Make the test
 To get started with TDD, see the `README.md` file in your
 `ruby/rna-transcription` directory.
 =end
+
+class Complement
+
+  RNA_MAP = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }.freeze
+
+  def self.of_dna(dna)
+
+    dna.chars.map { RNA_MAP[_1] }.join
+
+  end
+end
