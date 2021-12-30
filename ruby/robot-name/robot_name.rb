8416e2e6406704118ed8d723334cfb7914c7c1e2
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-30 13:02:31 -0500

    rn

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -5,3 +5,33 @@ Write your code for the 'Robot Name' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/robot-name` directory.
 =end
+class Robot
+  ALL_NAMES = {}
+
+  def initialize
+    @alpha = ('A'..'Z').to_a
+    reset
+  end
+
+  def reset
+    loop do
+      @name = (1..5).map {
+        if _1 < 3
+          @alpha[rand(26)]
+        else
+          rand(10)
+        end
+      }.join
+      if ALL_NAMES[@name].nil?
+        ALL_NAMES[@name] = 1
+        break
+      end
+    end
+  end
+
+  attr_reader :name
+
+  def self.forget
+    ALL_NAMES.clear
+  end
+end
