80b832253207629b9e15f1468747f11f3aa8192b
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-29 22:59:30 -0500

    grai

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -5,3 +5,16 @@ Write your code for the 'Grains' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/grains` directory.
 =end
+class Grains
+
+  def self.square(i)
+
+    throw ArgumentError if i < 1 || i > 64
+    2 ** (i - 1)
+  end
+
+  def self.total
+    (1..64).map { square _1 }.sum
+
+  end
+end
