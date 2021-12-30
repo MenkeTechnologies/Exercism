d517709a1c9d39a31474b2ab9f427a347be2bedd
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-30 10:12:01 -0500

    leap

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -5,3 +5,10 @@ Write your code for the 'Leap' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/leap` directory.
 =end
+
+class Year
+
+  def self.leap?(year)
+    (year % 4).zero? && year % 100 != 0 || (year % 400).zero?
+  end
+end
