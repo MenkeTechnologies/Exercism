73c5e1d239d3432d1bb4d0897efb69bb565dcc18
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-30 19:40:37 -0500

    sm

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -5,3 +5,15 @@ Write your code for the 'Sum Of Multiples' exercise in this file. Make the tests
 To get started with TDD, see the `README.md` file in your
 `ruby/sum-of-multiples` directory.
 =end
+
+class SumOfMultiples
+
+  def initialize(*factors)
+    @factors = factors
+  end
+
+  def to(limit)
+    (1...limit).filter { |f| @factors.any? { _1 != 0 && (f % _1).zero? } }.sum
+  end
+
+end
