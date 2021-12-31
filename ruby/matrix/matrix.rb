8cc47f656b81e3db6ea90ee171fe1d9d26bafb3e
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-31 12:16:56 -0500

    matr

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,7 +1,22 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
+# frozen_string_literal: true
+# Write your code for the 'Matrix' exercise in this file. Make the tests in
+# `matrix_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/matrix` directory.
+class Matrix
 
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
+  def initialize(str)
+    @rows = str.split("\n").map { _1.split(' ').map(&:to_i) }
+    @columns = str.split("\n").reduce([]) { |acc, n|
+      n.split(' ').each_with_index {
+        (acc[_2] ||= []) << _1.to_i
+      }
+      acc
+    }
+
+  end
+
+  attr_reader :rows, :columns
+
+end
