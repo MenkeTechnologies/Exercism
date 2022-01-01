bf3fccfa566f8e2ac6a457627aa6520df5db06e2
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-01 17:27:29 -0500

    clo

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,7 +1,50 @@
-=begin
-Write your code for the 'Clock' exercise in this file. Make the tests in
-`clock_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/clock` directory.
-=end
+# Write your code for the 'Clock' exercise in this file. Make the tests in
+# `clock_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/clock` directory.
+
+class Clock
+  def initialize(hour: 0, minute: 0)
+    hour1 = hour
+    minute1 = minute
+    while minute1 >= 60
+      minute1 -= 60
+      hour1 += 1
+    end
+
+    while minute1.negative?
+      minute1 += 60
+      hour1 -= 1
+    end
+
+    hour1 %= 24
+
+    hour1 += 24 if hour1.negative?
+
+    @hour = hour1
+    @minute = minute1
+  end
+
+  attr_accessor :hour, :minute
+
+  def +(other)
+    Clock.new(hour: hour + other.hour, minute: minute + other.minute)
+
+  end
+
+  def -(other)
+    Clock.new(hour: hour - other.hour, minute: minute - other.minute)
+  end
+
+  def ==(other)
+    hour == other.hour && minute == other.minute
+  end
+
+  def to_s
+    format('%02d:%02d', @hour, @minute)
+  end
+
+end
