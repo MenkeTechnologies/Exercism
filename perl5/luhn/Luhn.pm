feb680f2908ab24b514819b1878333494caefd95
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-01 11:45:27 -0500

    luhn

--- perl5/luhn/Luhn.pm
+++ perl5/luhn/Luhn.pm
@@ -5,8 +5,21 @@ use Exporter 'import';
 our @EXPORT_OK = qw(is_luhn_valid);
 
 sub is_luhn_valid {
-  my ($input) = @_;
-  return undef;
+    my ($input) = @_;
+
+    $input =~ s@\s+@@g;
+
+    return 0 if length($input) < 2 || $input =~ /\D/;
+
+    my $cnt = 0;
+    my $sum = 0;
+
+    do {
+        $_ *= 2 if $cnt++ % 2 == 1;
+        $sum += $_ > 9 ? $_ - 9 : $_
+    } for split('', reverse($input));
+
+    $sum % 10 == 0
 }
 
 1;
