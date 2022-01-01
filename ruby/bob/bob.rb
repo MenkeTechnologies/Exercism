6a64cb83856c343e4210e5b5e5b5f91807e4a350
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-01 11:56:19 -0500

    bob

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,7 +1,27 @@
-=begin
-Write your code for the 'Bob' exercise in this file. Make the tests in
-`bob_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/bob` directory.
-=end
+# Write your code for the 'Bob' exercise in this file. Make the tests in
+# `bob_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/bob` directory.
+
+class Bob
+  def self.hey(remark)
+    remark.gsub!(/\s+/, '')
+
+    return 'Fine. Be that way!' if remark.empty?
+
+    if remark !~ /[a-z]/ && remark =~ /[A-Z]/
+
+      return "Calm down, I know what I'm doing!" if remark =~ /\?$/
+
+      return 'Whoa, chill out!'
+
+    end
+
+    return 'Sure.' if remark =~ /\?$/
+
+    'Whatever.'
+  end
+end
