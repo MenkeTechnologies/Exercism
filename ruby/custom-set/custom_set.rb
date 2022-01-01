5e68d6c97b99c941bb7ca8668493b0273f9bf192
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-31 20:55:01 -0500

    cs

--- ruby/custom-set/custom_set.rb
+++ ruby/custom-set/custom_set.rb
@@ -5,3 +5,51 @@ Write your code for the 'Custom Set' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/custom-set` directory.
 =end
+
+class CustomSet
+  def initialize(ary)
+    @ary = ary.uniq
+  end
+
+  def empty?
+    @ary.empty?
+  end
+
+  def member?(elem)
+    @ary.member?(elem)
+  end
+
+  def subset?(set)
+    @ary.size.zero? || @ary.all? { set.member? _1 }
+  end
+
+  def disjoint?(set)
+    @ary.none? { set.member? _1 }
+  end
+
+  def add(elem)
+    @ary << elem
+    @ary.uniq!
+    @ary
+  end
+
+  def size
+    @ary.size
+  end
+
+  def difference(set)
+    @ary.reject { set.member?(_1) }
+  end
+
+  def intersection(set)
+    @ary.filter { set.member?(_1) }
+  end
+
+  def union(set)
+    @ary += set.difference(self)
+  end
+
+  def ==(other)
+    @ary.size == other.size && @ary.all? { other.member? _1 }
+  end
+end
