2dd7d5eb30adf964e54d231a7979579292618117
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-31 19:32:55 -0500

    coll

--- ruby/collatz-conjecture/collatz_conjecture.rb
+++ ruby/collatz-conjecture/collatz_conjecture.rb
@@ -1,7 +1,27 @@
-=begin
-Write your code for the 'Collatz Conjecture' exercise in this file. Make the tests in
-`collatz_conjecture_test.rb` pass.
+# frozen_string_literal: true
+# Write your code for the 'Collatz Conjecture' exercise in this file. Make the tests in
+# `collatz_conjecture_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/collatz-conjecture` directory.
+class CollatzConjecture
 
-To get started with TDD, see the `README.md` file in your
-`ruby/collatz-conjecture` directory.
-=end
+  def self.steps(limit)
+
+    throw ArgumentError if limit < 1
+
+    cnt = 0
+
+    while limit != 1
+      if limit.even?
+        limit /= 2
+      else
+        limit = 3 * limit + 1
+      end
+      cnt += 1
+    end
+
+    cnt
+
+  end
+end
