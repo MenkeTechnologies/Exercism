e3e0b016aaa61d10d3fdf7856a2dc5383ad58e8f
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-31 20:32:58 -0500

    nt

--- ruby/nucleotide-count/nucleotide_count.rb
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -5,3 +5,34 @@ Write your code for the 'Nucleotide Count' exercise in this file. Make the tests
 To get started with TDD, see the `README.md` file in your
 `ruby/nucleotide-count` directory.
 =end
+
+class Nucleotide
+
+  def initialize(str)
+    @h = {
+      'A' => 0,
+      'C' => 0,
+      'G' => 0,
+      'T' => 0
+    }
+
+    str.chars.each do
+      throw ArgumentError if @h[_1].nil?
+      @h[_1] += 1
+    end
+
+  end
+
+  def count(nt)
+    @h[nt]
+  end
+
+  def histogram
+    @h
+  end
+
+  def self.from_dna(str)
+    new(str)
+  end
+
+end
