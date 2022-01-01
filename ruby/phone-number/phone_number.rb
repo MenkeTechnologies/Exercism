7a2ee891954345a7f62238dc641ae2768ba2dea4
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-31 19:55:41 -0500

    pn

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -5,3 +5,15 @@ Write your code for the 'Phone Number' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/phone-number` directory.
 =end
+
+class PhoneNumber
+
+  def self.clean(num)
+
+    if num =~ /^\s*(\+?1?\s*)?\(?([2-9]\d{2})\)?[-.\s]*([2-9]\d{2})[-.\s]*(\d{4})\s*$/
+      "#{$2}#{$3}#{$4}"
+    end
+  end
+
+
+end
