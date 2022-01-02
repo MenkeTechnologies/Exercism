0a93a0be2a99b11457a32c87fdeb2c2c0d0364ce
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-02 12:27:45 -0500

    dia

--- ruby/diamond/diamond.rb
+++ ruby/diamond/diamond.rb
@@ -1,7 +1,24 @@
-=begin
-Write your code for the 'Diamond' exercise in this file. Make the tests in
-`diamond_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/diamond` directory.
-=end
+# Write your code for the 'Diamond' exercise in this file. Make the tests in
+# `diamond_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/diamond` directory.
+
+class Diamond
+  def self.make_diamond(letter)
+    letters = ('A'..'Z').to_a
+
+    limit = letters.index(letter)
+
+    row_rng = (0..limit).to_a + (0..limit - 1).to_a.reverse
+    col_rng = (0..limit).to_a.reverse + (1..limit).to_a
+
+    row_rng.reduce('') do |acr, r|
+      acr + col_rng.reduce('') do |acc, c|
+        acc + (r == c ? (letters[c]).to_s : ' ')
+      end + "\n"
+    end
+  end
+end
