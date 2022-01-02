4aaf246625b1096d890ff488b2526a0cf7594a8e
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-01 22:16:08 -0500

    rc

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,28 +1,36 @@
-#[derive(Debug, PartialEq)]
+use int_enum::IntEnum;
+use enum_iterator::IntoEnumIterator;
+
+#[repr(usize)]
+#[derive(Debug, PartialEq, Copy, Clone, IntEnum, IntoEnumIterator)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9
 }
 
 pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+    _color.int_value()
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    match ResistorColor::from_int(value) {
+        Ok(success) => {
+            format!("{:?}", success)
+        }
+        Err(_) => {
+            "value out of range".to_string()
+        }
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    ResistorColor::into_enum_iter().collect()
 }
