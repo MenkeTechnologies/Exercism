2edc2e088d6e49cd70fefa8f5038a411e1382d8b
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-01 22:16:08 -0500

    rc

--- rust/phone-number/src/lib.rs
+++ rust/phone-number/src/lib.rs
@@ -1,6 +1,17 @@
+use regex::Regex;
+
 pub fn number(user_number: &str) -> Option<String> {
-    unimplemented!(
-        "Given the number entered by user '{}', convert it into SMS-friendly format. If the entered number is not a valid NANP number, return None.",
-        user_number
-    );
+    let regex =
+        Regex::new(r"^\s*(\+?1?\s*)?\(?([2-9]\d{2})\)?[-.\s]*([2-9]\d{2})[-.\s]*(\d{4})\s*$")
+            .unwrap();
+
+    match regex.captures(user_number) {
+        Some(captures) => Some(format!(
+            "{}{}{}",
+            captures.get(2).unwrap().as_str(),
+            captures.get(3).unwrap().as_str(),
+            captures.get(4).unwrap().as_str(),
+        )),
+        _ => None,
+    }
 }
