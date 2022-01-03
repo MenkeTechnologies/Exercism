c9543a2c9eaddb84c4bf26b469e6ebfb20509a80
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-02 20:46:36 -0500

    ps

--- swift/pizza-slices/Sources/PizzaSlices/PizzaSlices.swift
+++ swift/pizza-slices/Sources/PizzaSlices/PizzaSlices.swift
@@ -1,10 +1,35 @@
 func sliceSize(diameter: Double?, slices: Int?) -> Double? {
-  fatalError("Please implement the sliceSize(diameter:slices:) function")
+    if let d = diameter, d >= 0, let s = slices, s > 0 {
+        let r = d / 2
+        return (Double.pi * (r * r)) / Double(s)
+    }
+
+    return nil
 }
 
 func biggestSlice(
-  diameterA: String, slicesA: String,
-  diameterB: String, slicesB: String
+        diameterA: String, slicesA: String,
+        diameterB: String, slicesB: String
 ) -> String {
-  fatalError("Please implement the biggestSlice(diameterA:slicesA:diameterB:slicesB:) function")
+    let d1 = Double(diameterA)
+    let s1 = Int(slicesA)
+    let d2 = Double(diameterB)
+    let s2 = Int(slicesB)
+
+    let sizeA = sliceSize(diameter: d1, slices: s1)
+    let sizeB = sliceSize(diameter: d2, slices: s2)
+
+    switch (sizeA, sizeB) {
+        case (nil, nil): return "Neither slice is bigger"
+        case (.some(_), nil): return "Slice A is bigger"
+        case (nil, .some(_)): return "Slice B is bigger"
+        case let (a?, b?): if a > b {
+            return "Slice A is bigger"
+        } else if a < b {
+            return "Slice B is bigger"
+        } else {
+            return "Neither slice is bigger"
+        }
+    }
+
 }
