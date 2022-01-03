4e4a05a22c86d25d10db09c060a01d12f0cd12e7
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-03 09:01:29 -0500

    acc

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -5,3 +5,9 @@ Write your code for the 'Accumulate' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/accumulate` directory.
 =end
+
+class Array
+  def accumulate(&clo)
+    reduce([]) { _1 << clo.call(_2) }
+  end
+end
