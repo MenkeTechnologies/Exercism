3bbfbd8c055e46fc9cdcc6e05536cef849080d06
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-03 08:47:41 -0500

    rct

--- ruby/resistor-color-trio/resistor_color_trio.rb
+++ ruby/resistor-color-trio/resistor_color_trio.rb
@@ -1,7 +1,44 @@
-=begin
-Write your code for the 'Resistor Color Trio' exercise in this file. Make the tests in
-`resistor_color_trio_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-trio` directory.
-=end
+# Write your code for the 'Resistor Color Trio' exercise in this file. Make the tests in
+# `resistor_color_trio_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/resistor-color-trio` directory.
+
+class ResistorColorTrio
+  COLOR_RULES = {
+    'black' => '0',
+    'brown' => '1',
+    'red' => '2',
+    'orange' => '3',
+    'yellow' => '4',
+    'green' => '5',
+    'blue' => '6',
+    'violet' => '7',
+    'grey' => '8',
+    'white' => '9'
+  }.freeze
+
+  def initialize(ary)
+    @first = ary.take(2).map { color_val(_1) }.join
+    @val = @first + ('0' * color_val(ary.last).to_i)
+  end
+
+  RESISTOR_VAL = 'Resistor value: '
+
+  UNITS = %w[ohms kiloohms megaohms gigaohms].freeze
+
+  def label
+    @prefix = @val.chars.reverse.drop_while { _1 == '0' }.reverse.join
+    @zc = @val.chars.reverse.take_while { _1 == '0' }.length
+
+    "#{RESISTOR_VAL}#{@prefix}#{'0' * (@zc % 3)} #{UNITS[@zc / 3]}"
+  end
+
+  private
+
+  def color_val(var1)
+    COLOR_RULES[var1] || (raise ArgumentError)
+  end
+end
