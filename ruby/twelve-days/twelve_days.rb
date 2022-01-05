b926ce52972aa9ef50310b3d0235114fec47c9a7
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-05 13:02:58 -0500

    twelve

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -1,7 +1,38 @@
-=begin
-Write your code for the 'Twelve Days' exercise in this file. Make the tests in
-`twelve_days_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/twelve-days` directory.
-=end
+# Write your code for the 'Twelve Days' exercise in this file. Make the tests in
+# `twelve_days_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/twelve-days` directory.
+
+class TwelveDays
+  DAYS = %w[first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth].freeze
+  GIFTS = [
+    'a Partridge in a Pear Tree.',
+    'two Turtle Doves',
+    'three French Hens',
+    'four Calling Birds',
+    'five Gold Rings',
+    'six Geese-a-Laying',
+    'seven Swans-a-Swimming',
+    'eight Maids-a-Milking',
+    'nine Ladies Dancing',
+    'ten Lords-a-Leaping',
+    'eleven Pipers Piping',
+    'twelve Drummers Drumming'
+  ].freeze
+
+  def self.song
+    DAYS.map.with_index do
+      rng = 0.._2
+      gstr = if rng.size > 1
+               GIFTS[1.._2].reverse.join(', ') + ", and #{GIFTS.first}"
+             else
+               GIFTS[rng].reverse.join
+             end
+
+      "On the #{_1} day of Christmas my true love gave to me: #{gstr}\n" + (_2 < 11 ? "\n" : '')
+    end.join
+  end
+end
