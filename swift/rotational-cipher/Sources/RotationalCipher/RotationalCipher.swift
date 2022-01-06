3f4860e15fd0a315e2058eb88f8ea0a737024631
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-06 10:35:08 -0500

    rot

--- swift/rotational-cipher/Sources/RotationalCipher/RotationalCipher.swift
+++ swift/rotational-cipher/Sources/RotationalCipher/RotationalCipher.swift
@@ -1,1 +1,15 @@
 //Solution goes in Sources
+
+class RotationalCipher {
+
+    public class func rotate(_ s: String, ROT: Int) -> String {
+
+        s.map { $0.isUppercase ? rot($0, ROT, "A") : $0.isLowercase ?
+                            rot($0, ROT, "a") : String($0)
+                } .joined()
+    }
+
+    private static func rot(_ arg: Character, _ ROT: Int, _ base: String) -> String {
+        String(Character(UnicodeScalar(((arg.asciiValue! - Character(base).asciiValue! + UInt8(ROT)) % 26) + Character(base).asciiValue!)))
+    }
+}
