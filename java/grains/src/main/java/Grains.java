fa864d7c7a3b6fa66066c036cbed8bd1dfa2f0de
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-06 12:06:39 -0500

    grains

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -1,13 +1,17 @@
 import java.math.BigInteger;
+import java.util.stream.IntStream;
 
 class Grains {
-
     BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        if (square < 1 || square > 64) {
+            throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+        return BigInteger.TWO.pow(square - 1);
     }
 
     BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
 
+        return IntStream.rangeClosed(1, 64).mapToObj(this::grainsOnSquare).reduce(BigInteger.ZERO, BigInteger::add);
+    }
 }
