ce32c027545d974aed8e428cc25eb4dd6283e0a2
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-06 10:04:42 -0500

    rot

--- ruby/rotational-cipher/rotational_cipher.rb
+++ ruby/rotational-cipher/rotational_cipher.rb
@@ -5,3 +5,23 @@ Write your code for the 'Rotational Cipher' exercise in this file. Make the test
 To get started with TDD, see the `README.md` file in your
 `ruby/rotational-cipher` directory.
 =end
+
+class RotationalCipher
+
+  def self.rotate(str, amount)
+
+    str.chars.map do
+
+      case _1
+      when /[a-z]/
+        ((_1.ord - 'a'.ord + amount) % 26) + 'a'.ord
+      when /[A-Z]/
+        ((_1.ord - 'A'.ord + amount) % 26) + 'A'.ord
+      else
+        _1
+      end.chr
+
+    end.join
+
+  end
+end
