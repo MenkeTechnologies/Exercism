0e3389f0542b5e5385227a05bb34a27c0bbe5356
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-07 14:39:51 -0500

    track

--- clojure/tracks-on-tracks-on-tracks/src/tracks_on_tracks_on_tracks.clj
+++ clojure/tracks-on-tracks-on-tracks/src/tracks_on_tracks_on_tracks.clj
@@ -3,26 +3,31 @@
 (defn new-list
   "Creates an empty list of languages to practice."
   []
+  '()
   )
 
 (defn add-language
   "Adds a language to the list."
   [lang lang-list]
+  (conj lang-list lang)
   )
 
 (defn first-language
   "Returns the first language on the list."
   [lang-list]
+  (first lang-list)
   )
 
 (defn remove-language
-  "Removes the the first language added to the list."
+  "Removes the first language added to the list."
   [lang-list]
+  (rest lang-list)
   )
 
 (defn count-languages
   "Returns the total number of languages on the list."
   [lang-list]
+  (count lang-list)
   )
 
 (defn learning-list
@@ -30,4 +35,12 @@
   Java and JavaScript, then finally returns a count of the total number
   of languages."
   []
+  (->> (new-list)
+       (add-language "Clojure")
+       (add-language "Lisp")
+       remove-language
+       (add-language "Java")
+       (add-language "JavaScript")
+       count-languages
+       )
   )
