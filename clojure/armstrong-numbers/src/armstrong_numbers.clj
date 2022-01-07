e5a25fc87a753ed87c77c2ab76d5c98d2756ce59
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-07 13:03:36 -0500

    arm

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,5 +1,11 @@
 (ns armstrong-numbers)
 
-(defn armstrong? [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn armstrong? [num]
+
+  (let [digits (map #(Character/getNumericValue %) (str num))
+        count (count digits)
+        pow-digits (map #(apply * (repeat count %)) digits)]
+    (= num (apply + pow-digits))
+    )
+
+  )
