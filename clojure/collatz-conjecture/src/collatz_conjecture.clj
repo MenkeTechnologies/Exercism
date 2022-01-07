80670da8eeb6771b19a2edbf3d3e3cdc6d8ac2c6
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-07 14:59:12 -0500

    coll

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -1,5 +1,17 @@
 (ns collatz-conjecture)
 
-(defn collatz [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn work [num]
+  (if (even? num)
+    (/ num 2)
+    (inc (* num 3)))
+  )
+
+(defn collatz [num]
+
+  (if (<= num 0) (throw "Input cannot be <= 0")
+
+    (loop [v num cnt 0]
+      (if (= 1 v) cnt
+      (recur (work v) (inc cnt))
+      )))
+  )
