5367669ebaf426b4274ddfeed631b5dd6afb6428
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-06 21:59:16 -0500

    dnd

--- kotlin/dnd-character/src/main/kotlin/DndCharacter.kt
+++ kotlin/dnd-character/src/main/kotlin/DndCharacter.kt
@@ -1,22 +1,23 @@
+import kotlin.math.floor
+import kotlin.random.Random
+
 class DndCharacter {
 
-    val strength: Int = TODO("Initialize value to complete the task")
-    val dexterity: Int = TODO("Initialize value to complete the task")
-    val constitution: Int = TODO("Initialize value to complete the task")
-    val intelligence: Int = TODO("Initialize value to complete the task")
-    val wisdom: Int = TODO("Initialize value to complete the task")
-    val charisma: Int = TODO("Initialize value to complete the task")
-    val hitpoints: Int = TODO("Initialize value to complete the task")
+    val strength: Int = ability()
+    val dexterity: Int = ability()
+    val constitution: Int = ability()
+    val intelligence: Int = ability()
+    val wisdom: Int = ability()
+    val charisma: Int = ability()
+    val hitpoints: Int = 10 + modifier(constitution)
 
     companion object {
 
-        fun ability(): Int {
-            TODO("Implement the function to complete the task")
-        }
+        fun ability(): Int =
+            List(4) { Random.nextInt(1, 7) }.sorted().drop(1).sum()
 
-        fun modifier(score: Int): Int {
-            TODO("Implement the function to complete the task")
-        }
+
+        fun modifier(score: Int): Int = floor((score - 10.0) / 2).toInt()
     }
 
 }
