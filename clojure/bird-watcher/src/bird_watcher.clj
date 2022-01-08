d7352326ab056550c72c1e2c8407deed10d28346
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-08 11:03:23 -0500

    bw

--- clojure/bird-watcher/src/bird_watcher.clj
+++ clojure/bird-watcher/src/bird_watcher.clj
@@ -1,22 +1,46 @@
 (ns bird-watcher)
 
-(def last-week 
+(def last-week
+  [0 2 5 3 7 8 4]
   )
 
 (defn today [birds]
+  (last birds)
   )
 
 (defn inc-bird [birds]
+  (-> birds
+      pop
+      (conj (-> birds today inc))
+      )
   )
 
 (defn day-without-birds? [birds]
+  (->> birds
+       (every? #(pos? %))
+       (not)
+       )
+
   )
 
 (defn n-days-count [birds n]
+  (->> birds
+       (take n)
+       (reduce +))
   )
 
 (defn busy-days [birds]
+  (->> birds
+       (remove #(< % 5))
+       count)
   )
 
 (defn odd-week? [birds]
+  (let [odds (take-nth 2 birds)
+        evens (take-nth 2 (rest birds))
+        ]
+
+    (and (every? #(= 1 %) odds)
+         (every? #(= 0 %) evens))
+    )
   )
