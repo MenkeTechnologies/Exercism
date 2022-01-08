374be79c63ea70c3753c41d208b3f10b0bf8692d
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-08 10:08:06 -0500

    ca

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -1,12 +1,27 @@
 (ns cars-assemble)
 
+(def cars-per-hour 221)
+
 (defn production-rate
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
+  (let [rate
+        (condp # speed
+          10 0.77
+          9 0.8
+          5 0.9
+          1 1.0
+          0 0.0
+          )
+        ]
+    (* speed cars-per-hour rate)
+    )
   )
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
   [speed]
+  (int (/ (production-rate speed) 60))
+
   )
