cb65de48459394ae7ead075dc474e7d4add1b8ad
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-08 12:58:44 -0500

    rd

--- scala/raindrops/src/main/scala/Raindrops.scala
+++ scala/raindrops/src/main/scala/Raindrops.scala
@@ -1,4 +1,8 @@
 object Raindrops {
-  def convert(n: Int): String = ???
+  val MAP = Map(3 -> "Pling", 5 -> "Plang", 7 -> "Plong", 1 -> null)
+
+  def convert(num: Int): String =
+    MAP.filterKeys(num % _ == 0).foldLeft(""){ (acc, n) => if (n._1 != 1) acc + n._2 else if (acc.isEmpty) s"$num" else acc }
+
 }
 
