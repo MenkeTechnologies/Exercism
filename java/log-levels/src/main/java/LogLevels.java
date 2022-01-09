b1f59c4bca7381cf212c2683d7c8169609a87331
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-09 12:24:07 -0500

    ll

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,25 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LogLevels {
+    static Pattern regex = Pattern.compile("^\\[(.*)\\]:\\s+(.*\\S+)\\s*$");
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+        Matcher matcher = regex.matcher(logLine);
+        matcher.matches();
+
+        return matcher.group(2);
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+
+        Matcher matcher = regex.matcher(logLine);
+        matcher.matches();
+
+        return matcher.group(1).toLowerCase();
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+        return String.format("%s (%s)", message(logLine), logLevel(logLine));
     }
 }
