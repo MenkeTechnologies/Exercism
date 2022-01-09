d38591aa9caf7e362802a8f2262e71b0228dc6e1
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-09 12:59:02 -0500

    trans

--- kotlin/transpose/src/main/kotlin/Transpose.kt
+++ kotlin/transpose/src/main/kotlin/Transpose.kt
@@ -1,6 +1,8 @@
 object Transpose {
 
-    fun transpose(input: List<String>): List<String> {
-        TODO("Implement this to complete the task")
-    }
+    fun transpose(input: List<String>) =
+        (0 until (input.map { it.length }.max() ?: 0)).map { i ->
+            input.map { it.getOrNull(i) }.dropLastWhile { it == null }
+                .map { it ?: ' ' }.joinToString("")
+        }
 }
