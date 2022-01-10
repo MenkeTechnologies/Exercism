44f23b3b41ce6435daf17dd92abe8ec75031f765
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-10 10:53:36 -0500

    ss

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,44 @@
+import java.util.Map;
+
+import static java.util.Map.entry;
+
 class Scrabble {
+    static final Map<Character, Integer> letterToScore = Map.ofEntries(
+            entry('A', 1),
+            entry('E', 1),
+            entry('I', 1),
+            entry('O', 1),
+            entry('U', 1),
+            entry('L', 1),
+            entry('N', 1),
+            entry('R', 1),
+            entry('S', 1),
+            entry('T', 1),
+            entry('D', 2),
+            entry('G', 2),
+            entry('B', 3),
+            entry('C', 3),
+            entry('M', 3),
+            entry('P', 3),
+            entry('F', 4),
+            entry('H', 4),
+            entry('V', 4),
+            entry('W', 4),
+            entry('Y', 4),
+            entry('K', 5),
+            entry('J', 8),
+            entry('X', 8),
+            entry('Q', 10),
+            entry('Z', 10)
+    );
+    String w;
 
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        w = word;
     }
 
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
 
+        return w.toUpperCase().chars().map(c -> letterToScore.get((char) c)).sum();
+    }
 }
