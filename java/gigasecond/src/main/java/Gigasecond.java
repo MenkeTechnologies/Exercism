2dd7c88c8050527b714856a4acdbf76878c01bc0
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-10 10:48:33 -0500

    gs

--- java/gigasecond/src/main/java/Gigasecond.java
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -1,16 +1,19 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Gigasecond {
+    LocalDateTime ldt;
+
     public Gigasecond(LocalDate moment) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        ldt = moment.atStartOfDay();
     }
 
     public Gigasecond(LocalDateTime moment) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        ldt = moment;
     }
 
     public LocalDateTime getDateTime() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return ldt.plus((long) 1e9, ChronoUnit.SECONDS);
     }
 }
