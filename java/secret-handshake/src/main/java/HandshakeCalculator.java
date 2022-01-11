be0b6a43f4ef3eeffd832a33596586e78285d354
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-11 09:54:35 -0500

    hs

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class HandshakeCalculator {
-
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
 
+        List<Signal> li = Arrays.stream(Signal.values())
+                .filter(s -> (number & ((int) (Math.pow(2, s.ordinal())))) > 0).collect(Collectors.toList());
+
+        if ((number & 10_000) > 0) {
+            Collections.reverse(li);
+        }
+
+        return li;
+    }
 }
