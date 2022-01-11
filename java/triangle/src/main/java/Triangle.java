dff7a79a82e34b01e36ac5d76d0075e800bcf505
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-11 10:04:42 -0500

    tri

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -1,19 +1,32 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
 class Triangle {
+    private HashSet<Double> doubles;
 
     Triangle(double side1, double side2, double side3) throws TriangleException {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        List<Double> sides = Arrays.asList(side1, side2, side3);
+        Collections.sort(sides);
+
+        if (sides.stream().anyMatch(s -> s <= 0)) throw new TriangleException();
+
+        if (sides.get(0) + sides.get(1) < sides.get(2)) throw new TriangleException();
+
+        doubles = new HashSet<>(sides);
     }
 
     boolean isEquilateral() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return doubles.size() == 1;
     }
 
     boolean isIsosceles() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return doubles.size() <= 2;
     }
 
     boolean isScalene() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return doubles.size() == 3;
     }
-
 }
