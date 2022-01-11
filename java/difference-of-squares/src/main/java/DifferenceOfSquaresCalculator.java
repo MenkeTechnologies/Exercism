d626155ea72ca15117a49a8eb9a3e0877e404098
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-11 09:41:23 -0500

    dos

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,19 @@
+import java.util.stream.IntStream;
+
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+       return (int) Math.pow(IntStream.rangeClosed(1, input)
+                .sum(), 2);
     }
 
     int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return (int) IntStream.rangeClosed(1, input).mapToDouble(i->Math.pow(i, 2))
+                .sum();
     }
 
     int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
     }
 
 }
