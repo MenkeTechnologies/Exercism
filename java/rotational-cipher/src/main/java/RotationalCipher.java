68e445f3fa5be806c7f0f5a98a9771d8d58f77a8
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-12 11:12:27 -0500

    rc

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,11 +1,22 @@
+import java.util.stream.Collectors;
+
 class RotationalCipher {
+    Integer sc;
 
     RotationalCipher(int shiftKey) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        sc = shiftKey;
     }
 
     String rotate(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        return data.chars().map(this::rot).mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining(""));
     }
 
+    private int rot(int c) {
+        if (Character.isLetter(c)) {
+            return c >= 'a' ? (c - 'a' + sc) % 26 + 'a' :
+                    (c - 'A' + sc) % 26 + 'A';
+        }
+        return c;
+    }
 }
