6b43fdb8467fab5ba63085cfc55415dc8d883c05
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-14 10:03:36 -0500

    rob

--- java/robot-name/src/main/java/Robot.java
+++ java/robot-name/src/main/java/Robot.java
@@ -1,10 +1,40 @@
-/*
+import java.util.HashMap;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
 
-Please remove this comment when submitting your solution.
+class Robot {
+    static HashMap<String, Integer> MAP = new HashMap<>();
 
-*/
+    String name;
+
+    public Robot() {
+
+        name = gen();
+    }
+
+    private String gen() {
+
+        Random random = new Random();
+
+        String name = IntStream.range(0, 5)
+                .map(i -> i < 2 ? random.nextInt(26) + 'A' : random.nextInt(10) + '0').mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining(""));
+
+        if (MAP.containsKey(name)) {
+            return gen();
+        } else {
+            MAP.put(name, 1);
+            return name;
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void reset() {
+        name = gen();
+    }
+}
