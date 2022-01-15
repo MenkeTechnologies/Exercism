f3216e07cf53b0a7ea96d7209e4fede4d169cca2
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-15 00:32:07 -0500

    pasc

--- ruby/pascals-triangle/pascals_triangle.rb
+++ ruby/pascals-triangle/pascals_triangle.rb
@@ -5,3 +5,17 @@ Write your code for the 'Pascals Triangle' exercise in this file. Make the tests
 To get started with TDD, see the `README.md` file in your
 `ruby/pascals-triangle` directory.
 =end
+
+class Triangle
+
+  def initialize(n)
+    @n = n
+  end
+
+  def rows
+    (2..@n).reduce([[1]]) do |acc, _|
+      acc << ([1] + acc.last[0..-2].each_with_index.map { _1 + acc.last[_2 + 1] } + [1])
+    end
+  end
+
+end
