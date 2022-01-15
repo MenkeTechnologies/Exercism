4acb5de140d5bac0deffc65ef85bc14ae307a2b3
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-15 01:48:43 -0500

    pasc

--- kotlin/pascals-triangle/src/main/kotlin/PascalsTriangle.kt
+++ kotlin/pascals-triangle/src/main/kotlin/PascalsTriangle.kt
@@ -1,6 +1,9 @@
 object PascalsTriangle {
 
-    fun computeTriangle(rows: Int): List<List<Int>> {
-        TODO("Implement this function to complete the task")
-    }
+    fun computeTriangle(rows: Int): List<List<Int>> =
+        if (rows <= 0) { listOf() } else
+        (2..rows).fold(listOf(listOf(1))) { acc, _ ->
+            acc + listOf( listOf(1) + acc.last().dropLast(1).withIndex()
+                    .map { it.value + acc.last()[it.index + 1] } + listOf(1))
+        }
 }
