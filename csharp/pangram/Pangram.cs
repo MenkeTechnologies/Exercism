3cd0aa89c1c73b7cc4b2b6506a061824aea81c05
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-16 18:59:38 -0500

    pan

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using System.Linq;
 
 public static class Pangram
 {
-    public static bool IsPangram(string input)
-    {
-        throw new NotImplementedException();
-    }
+    public static bool IsPangram(string input) =>
+        Enumerable.Range('a', 26).All(i => input.ToLower().Contains((char)i));
 }
