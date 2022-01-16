5023f698566036897b1ebd1108a2bf0e455193a4
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-16 13:02:31 -0500

    mb

--- bash/matching-brackets/matching_brackets.sh
+++ bash/matching-brackets/matching_brackets.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+declare -A map=
+map['[']=']'
+map['{']='}'
+map['(']=')'
+declare -A close
+close[']']='['
+close['}']='{'
+close[')']='('
+
+stack=()
+
+for (( i = 0; i < "${#1}"; i++ )); do
+    ch="${1:$i:1}"
+
+    if [[ -n ${map[$ch]} ]]; then
+        stack+=( "$ch" )
+    elif [[ -n ${close[$ch]} ]]; then
+        if (( ${#stack} == 0 )) || [[ ${stack[-1]} != ${close[$ch]} ]]; then
+            echo false
+            exit 0
+        else
+            unset stack[-1]
+        fi
+    fi
+done
+
+if [[ ${#stack} == 0 ]]; then
+    echo true
+else
+    echo false
+fi
