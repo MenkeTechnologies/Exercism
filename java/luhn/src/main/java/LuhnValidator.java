d695dffe8e88a6ea31163edc7479ea471c526e4f
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-16 13:15:19 -0500

    lv

--- java/luhn/src/main/java/LuhnValidator.java
+++ java/luhn/src/main/java/LuhnValidator.java
@@ -1,7 +1,20 @@
+import java.util.stream.IntStream;
+
 class LuhnValidator {
-
     boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        String ns = new StringBuilder(candidate.replaceAll("\\s", "")).reverse().toString();
 
+        if (ns.length() < 2 || ns.matches("\\D")) {
+            return false;
+        }
+
+        return IntStream.range(0, ns.length())
+                .map(i -> {
+                    int d = Character.getNumericValue(ns.toCharArray()[i]);
+                    if (i % 2 == 1) {
+                        d *= 2;
+                    }
+                    return d > 9 ? d - 9 : d;
+                }).sum() % 10 == 0;
+    }
 }
