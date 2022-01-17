913c889ba4e450a6c45fffd5fe6515037eb78f1a
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-16 19:31:25 -0500

    ana

--- csharp/anagram/Anagram.cs
+++ csharp/anagram/Anagram.cs
@@ -1,14 +1,19 @@
 using System;
+using System.Linq;
 
 public class Anagram
 {
+    private readonly String _lc;
+    private readonly char[] _sort;
+
     public Anagram(string baseWord)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        _lc = baseWord.ToLower();
+        _sort = _lc.OrderBy(c => c).ToArray();
     }
 
-    public string[] FindAnagrams(string[] potentialMatches)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public string[] FindAnagrams(string[] potentialMatches) =>
+        potentialMatches.ToList().FindAll(w =>
+            w.ToLower().OrderBy(c => c).ToArray().SequenceEqual(_sort)
+            && !w.ToLower().Equals(_lc)).ToArray();
 }
