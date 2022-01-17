d5e9bf5a3dbbffd23212ece842741d9c0715487b
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-17 00:37:39 -0500

    grains

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -1,18 +1,12 @@
-/**
- * You can use the bigint type and BigInt global object to support numbers below
- * Number.MIN_SAFE_INTEGER and above NUMBER.MAX_SAFE_INTEGER.
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
- */
+export const square = (n) => {
 
-//
-// This is only a SKELETON file for the 'Grains' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-export const square = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+  if (n < 1 || n > 64) {
+    throw new Error('square must be between 1 and 64')
+  }
+  return BigInt(Math.pow(2, n - 1));
 
-export const total = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+}
+
+export const total = () =>
+  [...Array(64).keys()]
+    .map(n => square(n + 1)).reduce((acc, n) => acc + n, 0n)
