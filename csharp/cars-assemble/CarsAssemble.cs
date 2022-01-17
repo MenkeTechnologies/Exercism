43ddf4d0fcdc23f71170f9574fdb36430ba6c36b
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-17 14:00:49 -0500

    ca

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -2,18 +2,18 @@ using System;
 
 static class AssemblyLine
 {
-    public static double SuccessRate(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
-    }
-    
-    public static double ProductionRatePerHour(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
-    }
+    public static double SuccessRate(int speed) =>
+        speed switch
+        {
+            < 1 => 0.0,
+            <= 4 => 1.0,
+            <= 8 => 0.9,
+            9 => 0.8,
+            10 => 0.77,
+            _ => 0.0
+        };
 
-    public static int WorkingItemsPerMinute(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
-    }
+    public static double ProductionRatePerHour(int speed) => SuccessRate(speed) * 221 * speed;
+
+    public static int WorkingItemsPerMinute(int speed) => (int)(ProductionRatePerHour(speed) / 60);
 }
