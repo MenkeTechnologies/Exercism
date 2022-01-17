0e6b240f7fe294532ded748bd64e461231b344ef
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-17 10:38:08 -0500

    ana

--- csharp/annalyns-infiltration/AnnalynsInfiltration.cs
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -2,23 +2,21 @@ using System;
 
 static class QuestLogic
 {
-    public static bool CanFastAttack(bool knightIsAwake)
-    {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFastAttack() method");
-    }
+    public static bool CanFastAttack(bool knightIsAwake) => !knightIsAwake;
 
-    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
-    {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSpy() method");
-    }
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake) => knightIsAwake || archerIsAwake || prisonerIsAwake;
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
-    {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSignalPrisoner() method");
-    }
+    =>
+        prisonerIsAwake && !archerIsAwake;
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
+        if (petDogIsPresent)
+        {
+            return !archerIsAwake;
+        }
+
+        return prisonerIsAwake && !archerIsAwake && !knightIsAwake;
     }
 }
