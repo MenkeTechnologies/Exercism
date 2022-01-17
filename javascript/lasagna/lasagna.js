790763a7ef00d4b7689ecc9aad7f90e028d54e01
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-17 00:18:15 -0500

    js

---
+++ javascript/lasagna/lasagna.js
@@ -0,0 +1,33 @@
+/**
+ * The number of minutes it takes to prepare a single layer.
+ */
+export const PREPARATION_MINUTES_PER_LAYER = 2;
+export const EXPECTED_MINUTES_IN_OVEN = 40
+
+/**
+ * Determines the number of minutes the lasagna still needs to remain in the
+ * oven to be properly prepared.
+ *
+ * @param {number} actualMinutesInOven
+ * @returns {number} the number of minutes remaining
+ */
+export const remainingMinutesInOven = (actualMinutesInOven) =>
+  EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+
+/**
+ * Given a number of layers, determines the total preparation time.
+ *
+ * @param {number} numberOfLayers
+ * @returns {number} the total preparation time
+ */
+export const preparationTimeInMinutes = (numberOfLayers) => numberOfLayers * PREPARATION_MINUTES_PER_LAYER;
+
+/**
+ * Calculates the total working time. That is, the time to prepare all the layers
+ * of lasagna, and the time already spent in the oven.
+ *
+ * @param {number} numberOfLayers
+ * @param {number} actualMinutesInOven
+ * @returns {number} the total working time
+ */
+export const totalTimeInMinutes = (numberOfLayers, actualMinutesInOven) => preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
