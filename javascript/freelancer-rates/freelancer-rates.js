5dbb670a73ba2fff9f35ae0a2c4b966391252710
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-17 10:27:57 -0500

    free

--- javascript/freelancer-rates/freelancer-rates.js
+++ javascript/freelancer-rates/freelancer-rates.js
@@ -1,33 +1,10 @@
-// @ts-check
-//
-// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion on the web
-// and supported IDEs when implementing this exercise. You don't need to
-// understand types, JSDoc, or TypeScript in order to complete this JavaScript
-// exercise, and can completely ignore this comment block and directive.
-
-// 👋🏽 Hi again!
-//
-// A quick reminder about exercise stubs:
-//
-// 💡 You're allowed to completely clear any stub before you get started. Often
-// we recommend using the stub, because they are already set-up correctly to
-// work with the tests, which you can find in ./freelancer-rates.spec.js.
-//
-// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
-// in idiomatic JavaScript, but some companies and style-guides do enforce them.
-//
-// Get those rates calculated!
-
 /**
  * The day rate, given a rate per hour
  *
  * @param {number} ratePerHour
  * @returns {number} the rate per day
  */
-export function dayRate(ratePerHour) {
-  throw new Error('Implement the dayRate function');
-}
+export const dayRate = (ratePerHour) => ratePerHour * 8
 
 /**
  * Calculates the number of days in a budget, rounded down
@@ -36,9 +13,8 @@ export function dayRate(ratePerHour) {
  * @param {number} ratePerHour: the rate per hour
  * @returns {number} the number of days
  */
-export function daysInBudget(budget, ratePerHour) {
-  throw new Error('Implement the daysInBudget function');
-}
+export const daysInBudget = (budget, ratePerHour) =>
+  Math.floor(budget / dayRate(ratePerHour))
 
 /**
  * Calculates the discounted rate for large projects, rounded up
@@ -48,6 +24,11 @@ export function daysInBudget(budget, ratePerHour) {
  * @param {number} discount: for example 20% written as 0.2
  * @returns {number} the rounded up discounted rate
  */
-export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-  throw new Error('Implement the priceWithMonthlyDiscount function');
+export const priceWithMonthlyDiscount = (ratePerHour, numDays, discount) => {
+
+  let discountDays = Math.floor(numDays / 22) * 22;
+  let fullDays = numDays - discountDays
+  let rate = dayRate(ratePerHour);
+  let disc = 1 - discount;
+  return Math.ceil(rate * discountDays * disc + rate * fullDays)
 }
