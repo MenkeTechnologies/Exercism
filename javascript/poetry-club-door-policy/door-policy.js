f66c9cbdab5840f0917ee9bd95c94578d0ee074a
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-18 09:26:53 -0500

    dp

--- javascript/poetry-club-door-policy/door-policy.js
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -1,24 +1,3 @@
-// @ts-check
-//
-// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion on the web
-// and supported IDEs when implementing this exercise. You don't need to
-// understand types, JSDoc, or TypeScript in order to complete this JavaScript
-// exercise, and can completely ignore this comment block and directive.
-//
-// 👋🏽 Hi again!
-//
-// A quick reminder about exercise stubs:
-//
-// 💡 You're allowed to completely clear any stub before you get started. Often
-// we recommend using the stub, because they are already set-up correctly to
-// work with the tests, which you can find in ./door-policy.spec.js.
-//
-// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
-// in idiomatic JavaScript, but some companies and style-guides do enforce them.
-//
-// Good luck with that door policy!
-
 /**
  * Respond with the correct character, given the line of the
  * poem, if this were said at the front door.
@@ -26,9 +5,7 @@
  * @param {string} line
  * @returns {string}
  */
-export function frontDoorResponse(line) {
-  throw new Error('Implement the frontDoorResponse function');
-}
+export const frontDoorResponse = line => line.trim().substr(0, 1);
 
 /**
  * Format the password for the front-door, given the response
@@ -37,9 +14,7 @@ export function frontDoorResponse(line) {
  * @param {string} word the letters you responded with before
  * @returns {string} the front door password
  */
-export function frontDoorPassword(word) {
-  throw new Error('Implement the frontDoorPassword function');
-}
+export const frontDoorPassword = word => word.toUpperCase().substr(0, 1) + word.toLowerCase().substr(1);
 
 /**
  * Respond with the correct character, given the line of the
@@ -48,9 +23,8 @@ export function frontDoorPassword(word) {
  * @param {string} line
  * @returns {string}
  */
-export function backDoorResponse(line) {
-  throw new Error('Implement the backDoorResponse function');
-}
+export const backDoorResponse = line =>
+  line.trim().substr(line.trim().length-1,1)
 
 /**
  * Format the password for the back door, given the response
@@ -59,6 +33,4 @@ export function backDoorResponse(line) {
  * @param {string} word the letters you responded with before
  * @returns {string} the back door password
  */
-export function backDoorPassword(word) {
-  throw new Error('Implement the backDoorPassword function');
-}
+export const backDoorPassword = word => `${frontDoorPassword(word)}, please`;
