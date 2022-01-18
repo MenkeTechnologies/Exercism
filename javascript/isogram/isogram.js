de57b32479c8088f1e7c9e36e5053c3a88a20e86
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-18 09:40:28 -0500

    iso

---
+++ javascript/isogram/isogram.js
@@ -0,0 +1,6 @@
+//
+// This is only a SKELETON file for the 'Isogram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isIsogram = (word) => !/(\w).*\1/i.test(word);
