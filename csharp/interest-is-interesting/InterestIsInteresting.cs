af74b8415d3a58fbd900f03e33825e13662934c1
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-17 21:36:16 -0500

    int

--- csharp/interest-is-interesting/InterestIsInteresting.cs
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -2,23 +2,28 @@ using System;
 
 static class SavingsAccount
 {
-    public static float InterestRate(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
-    }
+    public static float InterestRate(decimal balance) =>
+        balance switch
+        {
+            < 0 => 3.213f,
+            < 1000 => 0.5f,
+            >= 5000 => 2.475f,
+            >= 1000 => 1.621f
+        };
 
-    public static decimal Interest(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
-    }
+    public static decimal Interest(decimal balance) => balance * (decimal)(InterestRate(balance) / 100);
 
-    public static decimal AnnualBalanceUpdate(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
-    }
+    public static decimal AnnualBalanceUpdate(decimal balance) => balance + Interest(balance);
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        var y = 0;
+        while (balance < targetBalance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            ++y;
+        }
+
+        return y;
     }
 }
