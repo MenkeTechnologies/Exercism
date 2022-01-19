e9d61e5ec35744a2c2d537e24b8290e6ae4a12b8
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-19 10:24:26 -0500

    mb

--- csharp/matching-brackets/MatchingBrackets.cs
+++ csharp/matching-brackets/MatchingBrackets.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class MatchingBrackets
 {
+    private static Dictionary<char, char> MAP = new()
+    {
+        { '{', '}' },
+        { '[', ']' },
+        { '(', ')' }
+    };
+
     public static bool IsPaired(string input)
     {
-        throw new NotImplementedException();
+        var stk = new Stack<char>();
+
+        return input.ToList().All(c =>
+            {
+                if (MAP.ContainsKey(c))
+                {
+                    stk.Push(c);
+                }
+                else if (MAP.ContainsValue(c))
+                {
+                    if (stk.Count == 0 || MAP[stk.Pop()] != c)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        ) && stk.Count == 0;
     }
 }
--- ruby/matching-brackets/matching_brackets.rb
+++ ruby/matching-brackets/matching_brackets.rb
@@ -5,3 +5,28 @@ Write your code for the 'Matching Brackets' exercise in this file. Make the test
 To get started with TDD, see the `README.md` file in your
 `ruby/matching-brackets` directory.
 =end
+
+class Brackets
+
+  MAP = {
+    '[' => ']',
+    '(' => ')',
+    '{' => '}'
+  }.freeze
+
+  def self.paired?(str)
+
+    stk = []
+
+    str.chars.each do
+      if MAP[_1]
+        stk << _1
+      elsif MAP.value?(_1)
+        return false if stk.empty? || MAP[stk.pop] != _1
+      end
+    end
+
+    stk.empty?
+
+  end
+end
