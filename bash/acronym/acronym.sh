76128c9f77af0ad9c82133da5b94a5268937cd56
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-19 12:59:05 -0500

    acro

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
-
-
-in="$*"
-
-in=${in^^}
-in=${in//-/ }
-in=${in//[[:punct:]]/}
-
-res=''
+in="${*^^}"
+in="${in//[^[:alpha:]\']/ }"
 
 for w in $in; do
     res+="${w:0:1}"
 done
 
-echo "$res"
-
+echo $res
