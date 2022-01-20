1f59a6e8abdb0276f99c27db6fbe1db6a56984f1
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-20 12:32:44 -0500

    ex

---
+++ python/grains/grains.py
@@ -0,0 +1,9 @@
+def square(number):
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+
+    return 2 ** (number - 1)
+
+
+def total():
+    return sum(map(lambda x: square(x), range(1, 65)))
