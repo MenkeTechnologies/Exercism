5fa696771c09a0283d430cef070fb2445353ed34
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-19 22:38:37 -0500

    acc

--- rust/accumulate/src/lib.rs
+++ rust/accumulate/src/lib.rs
@@ -1,7 +1,9 @@
 /// What should the type of _function be?
 pub fn map<F, T, U>(input: Vec<T>, mut _function: F) -> Vec<U>
-where
-    F: FnMut(T) -> U,
+    where
+        F: FnMut(T) -> U,
 {
-    input.into_iter().map(_function).collect()
+    input.into_iter().fold(Vec::new(), |mut acc, n| {
+        acc.push(_function(n));acc
+    })
 }
