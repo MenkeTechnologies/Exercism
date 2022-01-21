7593c080f657def9509c9b9d805004c5b9aa1677
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-20 22:52:43 -0500

    darts

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,13 @@
 def score(x, y):
-    pass
+    dist = pow(pow(x, 2) + pow(y, 2), 0.5)
+
+    if dist <= 1:
+        return 10
+
+    if dist <= 5:
+        return 5
+
+    if dist <= 10:
+        return 1
+
+    return 0
