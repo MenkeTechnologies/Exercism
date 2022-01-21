1ca63e15a4539768bbc5d455345846555eab10f5
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-21 11:17:04 -0500

    pn

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -3,6 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-export const classify = () => {
-  throw new Error('Remove this statement and implement this function');
+export const classify = (num) => {
+
+  if (num <= 0) {
+    throw new Error('Classification is only possible for natural numbers.')
+  }
+
+  let sum = [...Array(num-1).keys()].map(x=>x + 1).filter(n => num % n === 0).reduce((acc, n) => acc + n, 0)
+
+
+  if (sum === num) {
+      return 'perfect'
+  }
+
+  if (sum < num) {
+    return 'deficient'
+  }
+
+  if (sum > num) {
+    return 'abundant'
+  }
+
 };
