6f0c9d69660b1170de5ccf9403df4c839bab44a2
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-22 00:24:46 -0500

    rna

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,4 +1,18 @@
 package strand
 
+var MAP = map[rune]rune{
+	'G': 'C',
+	'C': 'G',
+	'T': 'A',
+	'A': 'U',
+}
+
 func ToRNA(dna string) string {
+
+	out := make([]rune, len(dna))
+	for i, c := range dna {
+		out[i] = MAP[c]
+	}
+
+	return string(out)
 }
