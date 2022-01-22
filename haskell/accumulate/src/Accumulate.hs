110f3fcee92ecc8270f8065d16f4c68ebeffb74a
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-22 18:49:32 -0500

    acc

--- haskell/accumulate/src/Accumulate.hs
+++ haskell/accumulate/src/Accumulate.hs
@@ -1,4 +1,5 @@
 module Accumulate (accumulate) where
 
 accumulate :: (a -> b) -> [a] -> [b]
-accumulate f xs = error "You need to implement this function."
+accumulate _ [] = []
+accumulate f (x:xs) = f x : accumulate f xs
