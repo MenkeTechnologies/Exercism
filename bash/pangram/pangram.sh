b9c6e9e9c966d3434cc2ea174e54e32a12009dc8
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-24 13:25:17 -0500

    pangram

--- bash/pangram/pangram.sh
+++ bash/pangram/pangram.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-res=$(echo "$1" | perl -nE '$_=lc;s@[^[:alpha:]]@@g;say for split //' | sort | uniq | perl -pe 's@\n@@')
+res=$(echo "$1" | perl -MList::Util=uniq -nE '$_=lc;s@[^[:alpha:]]@@g;print for uniq sort split //')
 
 
 [[ $res == abcdefghijklmnopqrstuvwxyz ]] && echo true || echo false
