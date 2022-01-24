bfebfd21245ff26074efc980391c9298f7405502
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-24 15:52:01 -0500

    bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,13 @@
+import re
+
+
 def response(hey_bob):
-    pass
+    hey_bob = re.sub(r"\s+", '', hey_bob)
+
+    if len(hey_bob) == 0:
+        return 'Fine. Be that way!'
+
+    if re.search(r'[A-Z]', hey_bob) and not re.search(r'[a-z]', hey_bob):
+        return "Calm down, I know what I'm doing!" if re.search(r'\?$', hey_bob) else 'Whoa, chill out!'
+
+    return 'Sure.' if re.search(r'\?$', hey_bob) else 'Whatever.'
