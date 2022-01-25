e90b741276b454247a836a43c52db7b812cf75a2
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-25 09:58:30 -0500

    sc

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,19 @@
-using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Identifier
 {
-    public static string Clean(string identifier)
-    {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
-    }
+    private static bool isGreekLc(char c) => 'α' <= c && c <= 'ω';
+
+    public static string Clean(string identifier) =>
+        Regex.Replace(identifier, @"-(\D)", m => m.Groups[1].Value.ToUpper()).Select(c =>
+            c switch
+            {
+                ' ' => "_",
+                '\0' => "CTRL",
+                _ when isGreekLc(c) => "",
+                _ when char.IsLetter(c) => $"{c}",
+                _ => ""
+            }
+        ).Aggregate("", (acc, n) => acc + n);
 }
