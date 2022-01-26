810d347f6a4fc150fca2fbfa700897313bdbf303
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-26 12:19:35 -0500

    rc

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,19 @@
+color_dict = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+
 def color_code(color):
-    pass
+    return color_dict[color]
 
 
 def colors():
-    pass
+    return list(color_dict.keys())
