79394707c6719795768ce4ebe362aac9a700d722
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-27 14:12:26 -0500

    men's shoes test

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -2,10 +2,8 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
 class WordCount {
     public Map<String, Integer> phrase(String word) {
-
         return Arrays.stream(word.toLowerCase().replaceAll("(\\s'|'\\s|(?!\\w|'\\w+\\b).)", " ").trim().split("\\s+"))
                 .collect(Collectors.groupingBy(Function.identity()))
                 .entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().size()));
