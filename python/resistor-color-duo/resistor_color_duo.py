d0494e7faae29a5e5488b0a7a26cb454c398b533
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-27 14:28:12 -0500

    rcd

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,16 @@
+MAP = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9
+}
+
+
 def value(colors):
-    pass
+    return int("".join(map(lambda c: str(MAP[c]), colors[:2])))
