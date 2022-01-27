4b4f0921b8d892b4d2dc6aac940d426e2870d494
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-27 14:49:07 -0500

    squeaky

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+
+        Matcher matcher = Pattern.compile("-(\\D)").matcher(identifier);
+
+        if (matcher.find()) identifier = matcher.replaceAll(matcher.group(1).toUpperCase());
+
+        return identifier.chars()
+                .mapToObj(c -> c == ' ' ? "_" : Character.isISOControl(c) ? "CTRL" :
+                        c >= 'α' && c <= 'ω' ? "" : Character.isLetter(c) ?
+                                String.valueOf((char) c) : ""
+                ).collect(Collectors.joining(""));
     }
 }
