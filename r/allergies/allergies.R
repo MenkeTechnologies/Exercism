4189c5d89326e0b66b28b31e4dbe7c2740b32566
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-28 01:13:40 -0500

    allergies

--- r/allergies/allergies.R
+++ r/allergies/allergies.R
@@ -1,11 +1,13 @@
 allergy <- function(num) {
-  
+  allergens = c('eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats')
+  rng=2**(0:(length(allergens) - 1))
+  allergens[bitwAnd(rng, num) > 0]
 }
 
 allergic_to <- function(allergy_object, allergy) {
-  
+  allergy %in% allergy_object
 }
 
 list_allergies <- function(allergy_object) {
-  
+  allergy_object
 }
