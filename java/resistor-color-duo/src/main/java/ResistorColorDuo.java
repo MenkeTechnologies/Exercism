fe4501b11e4a5d6b4c79bb742a11c457434605c0
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-28 11:14:03 -0500

    rc

--- java/resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 class ResistorColorDuo {
+    enum Colours {
+        black,
+        brown,
+        red,
+        orange,
+        yellow,
+        green,
+        blue,
+        violet,
+        grey,
+        white
+    }
+
     int value(String[] colors) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Integer.parseInt(Arrays.stream(colors).limit(2).map(c -> Colours.valueOf(c).ordinal() + "").collect(Collectors.joining("")));
     }
 }
