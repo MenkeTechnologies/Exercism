86a2ce0a9a7733e8dddbb1353d56b2c14a91863f
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-28 21:26:28 -0500

    ca

--- fsharp/cars-assemble/CarsAssemble.fs
+++ fsharp/cars-assemble/CarsAssemble.fs
@@ -1,10 +1,13 @@
 module CarsAssemble
 
 let successRate (speed: int): float =
-    failwith "Please implement the 'successRate' function"
+    if speed >= 10 then 0.77
+    elif speed >= 9 then 0.8
+    elif speed >= 5 then 0.9
+    elif speed >= 1 then 1
+    else 0
 
-let productionRatePerHour (speed: int): float =
-    failwith "Please implement the 'productionRatePerHour' function"
+let productionRatePerHour (speed: int): float = float(speed * 221) * successRate speed
 
 let workingItemsPerMinute (speed: int): int =
-    failwith "Please implement the 'workingItemsPerMinute' function"
+    productionRatePerHour speed / 60.0 |> floor |> int
