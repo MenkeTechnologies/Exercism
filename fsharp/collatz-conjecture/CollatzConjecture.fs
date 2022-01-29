e3b58f5fcb3b14aa532890e1e34701bae577be56
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-29 13:50:18 -0500

    cc

--- fsharp/collatz-conjecture/CollatzConjecture.fs
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -1,3 +1,8 @@
 ﻿module CollatzConjecture
 
-let steps (number: int): int option = failwith "You need to implement this function."
+let rec steps (number: int) : int option =
+    match number with
+    | 1 -> Some 0
+    | number when number <= 0 -> None
+    | number when number % 2 = 0 -> number / 2 |> steps |> Option.map (fun v -> v + 1)
+    | number -> 3 * number + 1 |> steps |> Option.map (fun v -> v + 1)
