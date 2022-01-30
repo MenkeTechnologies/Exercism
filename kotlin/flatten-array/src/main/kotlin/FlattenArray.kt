1480595823a9027304ecb2788027d1d587fd3d9e
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-30 11:14:18 -0500

    lo

--- kotlin/flatten-array/src/main/kotlin/FlattenArray.kt
+++ kotlin/flatten-array/src/main/kotlin/FlattenArray.kt
@@ -1,8 +1,5 @@
 object Flattener {
-    fun flatten(source: Collection<Any?>): List<Any> {
-
-        return source.filterNotNull()
-            .flatMap { if (it is Collection<*>) flatten(it) else listOf(it) }
-            .toList()
-    }
+    fun flatten(source: Collection<Any?>): List<Any> = source.filterNotNull()
+        .flatMap { if (it is Collection<*>) flatten(it) else listOf(it) }
+        .toList()
 }
