78e009d57050d8fe5658007a2b436507e1a39fab
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-29 22:12:28 -0500

    isbn

--- java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,7 +1,20 @@
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class IsbnVerifier {
-
     boolean isValid(String stringToVerify) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
 
+        if (!Pattern.compile("^\\d-?\\d{3}-?\\d{5}-?[\\dX]$")
+                .matcher(stringToVerify).matches()) return false;
+
+        String rvs = new StringBuilder(stringToVerify).reverse().chars()
+                .filter(Character::isLetterOrDigit)
+                .mapToObj(n -> Character.valueOf((char) n).toString())
+                .collect(Collectors.joining());
+
+        return IntStream.range(0, rvs.length())
+                .map(n -> rvs.charAt(n) == 'X' ? 10 : Character.getNumericValue(rvs.charAt(n)) * (n + 1))
+                .sum() % 11 == 0;
+    }
 }
