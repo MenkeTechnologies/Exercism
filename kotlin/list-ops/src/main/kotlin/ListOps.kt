06b98e05b8fb68be6679ff880ef8f7c4ec2ad9b9
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-30 11:14:18 -0500

    lo

--- kotlin/list-ops/src/main/kotlin/ListOps.kt
+++ kotlin/list-ops/src/main/kotlin/ListOps.kt
@@ -1,29 +1,20 @@
-fun <T> List<T>.customAppend(list: List<T>): List<T> {
-    TODO("Implement this function to complete the task")
-}
+fun <T> List<T>.customAppend(list: List<T>): List<T> = listOf(this, list).flatten()
 
-fun List<Any>.customConcat(): List<Any> {
-    TODO("Implement this function to complete the task")
-}
+fun List<Any>.customConcat(): List<Any> = flattenRec(listOf(this))
 
-fun <T> List<T>.customFilter(predicate: (T) -> Boolean): List<T> {
-    TODO("Implement this function to complete the task")
-}
+fun flattenRec(source: Collection<Any?>): List<Any> = source.filterNotNull()
+        .flatMap { if (it is Collection<*>) flattenRec(it) else listOf(it) }
+        .toList()
 
-val List<Any>.customSize: Int get() = TODO("Implement this getter to complete the task")
+fun <T> List<T>.customFilter(predicate: (T) -> Boolean): List<T> =
+        fold(mutableListOf()) { acc, n -> if (predicate(n)) acc += n; acc }
 
-fun <T, U> List<T>.customMap(transform: (T) -> U): List<U> {
-    TODO("Implement this function to complete the task")
-}
+val List<Any>.customSize: Int get() = size
 
-fun <T, U> List<T>.customFoldLeft(initial: U, f: (U, T) -> U): U {
-    TODO("Implement this function to complete the task")
-}
+fun <T, U> List<T>.customMap(transform: (T) -> U): List<U> = fold(mutableListOf()) { acc, n -> acc += transform(n); acc }
 
-fun <T, U> List<T>.customFoldRight(initial: U, f: (T, U) -> U): U {
-    TODO("Implement this function to complete the task")
-}
+fun <T, U> List<T>.customFoldLeft(initial: U, f: (U, T) -> U): U = fold(initial, f)
 
-fun <T> List<T>.customReverse(): List<T> {
-    TODO("Implement this function to complete the task")
-}
+fun <T, U> List<T>.customFoldRight(initial: U, f: (T, U) -> U): U = foldRight(initial, f)
+
+fun <T> List<T>.customReverse(): List<T> = foldRight(mutableListOf()) { n, acc -> acc += n;acc }
