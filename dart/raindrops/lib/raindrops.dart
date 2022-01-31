e92d229864bb6ea675bf8934b062709d41b6f46e
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-31 11:44:26 -0500

    rd

--- dart/raindrops/lib/raindrops.dart
+++ dart/raindrops/lib/raindrops.dart
@@ -1,3 +1,9 @@
 class Raindrops {
-  // Put your code here
+  static const MAP = {3: "Pling", 5: "Plang", 7: "Plong"};
+
+  String convert(int i) => MAP.keys
+      .where((e) => i % e == 0)
+      .map((e) => MAP[e])
+      .join("")
+      .replaceAll(RegExp('^\$'), "$i");
 }
