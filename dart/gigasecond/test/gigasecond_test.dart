5be2ad616574d53376840234272d4409f66e2d81
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-30 23:08:17 -0500

    remove skip

--- dart/gigasecond/test/gigasecond_test.dart
+++ dart/gigasecond/test/gigasecond_test.dart
@@ -13,7 +13,7 @@ void gigasecondTests() {
       DateTime expectedDate = DateTime.utc(2043, DateTime.january, 1, 1, 46, 40);
 
       expect(result, equals(expectedDate));
-    }, skip: false);
+    });
 
     test('second test for date only specification of time', () {
       final DateTime birthDate = DateTime.utc(1977, DateTime.june, 13);
@@ -21,7 +21,7 @@ void gigasecondTests() {
       DateTime expectedDate = DateTime.utc(2009, DateTime.february, 19, 1, 46, 40);
 
       expect(result, equals(expectedDate));
-    }, skip: true);
+    });
 
     test('third test for date only specification of time', () {
       final DateTime birthDate = DateTime.utc(1959, DateTime.july, 19);
@@ -29,7 +29,7 @@ void gigasecondTests() {
       DateTime expectedDate = DateTime.utc(1991, DateTime.march, 27, 1, 46, 40);
 
       expect(result, equals(expectedDate));
-    }, skip: true);
+    });
 
     test('full time specified', () {
       final DateTime birthDate = DateTime.utc(2015, DateTime.january, 24, 22, 00, 00);
@@ -37,7 +37,7 @@ void gigasecondTests() {
       DateTime expectedDate = DateTime.utc(2046, DateTime.october, 2, 23, 46, 40);
 
       expect(result, equals(expectedDate));
-    }, skip: true);
+    });
 
     test('full time with day roll-over', () {
       final DateTime birthDate = DateTime.utc(2015, DateTime.january, 24, 23, 59, 59);
@@ -45,6 +45,6 @@ void gigasecondTests() {
       DateTime expectedDate = DateTime.utc(2046, DateTime.october, 3, 01, 46, 39);
 
       expect(result, equals(expectedDate));
-    }, skip: true);
+    });
   });
 }
