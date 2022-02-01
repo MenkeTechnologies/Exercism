cdcc39a3a4bc5259e1ebffe4ee557bad916fa916
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-01 11:16:49 -0500

    kl

--- java/karls-languages/src/main/java/LanguageList.java
+++ java/karls-languages/src/main/java/LanguageList.java
@@ -5,30 +5,31 @@ public class LanguageList {
     private final List<String> languages = new ArrayList<>();
 
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Please implement the isEmpty() method");
+        return languages.isEmpty();
     }
 
     public void addLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the addLanguage() method");
+        languages.add(language);
     }
 
     public void removeLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the removeLanguage() method");
+        languages.remove(language);
     }
 
     public String firstLanguage() {
-        throw new UnsupportedOperationException("Please implement the firstLanguage() method");
+        return languages.get(0);
     }
 
     public int count() {
-        throw new UnsupportedOperationException("Please implement the count() method");
+
+        return languages.size();
     }
 
     public boolean containsLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the containsLanguage() method");
+        return languages.stream().anyMatch(l -> l.equals(language));
     }
 
     public boolean isExciting() {
-        throw new UnsupportedOperationException("Please implement the isExciting() method");
+        return languages.stream().anyMatch(l -> l.equals("Java") || l.equals("Kotlin"));
     }
 }
