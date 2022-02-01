bcf7df0104b187b6c054577be136d2cc7ed49eed
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-01 00:38:40 -0500

    iso

--- lua/isogram/isogram.lua
+++ lua/isogram/isogram.lua
@@ -1,3 +1,12 @@
 return function(s)
+    local m = {}
+    for c in s:lower():gmatch('%a') do
+        if m[c] then
+            return false
+        end
 
+        m[c] = true
+    end
+
+    return true
 end
