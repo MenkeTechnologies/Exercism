e061ef59929af0dfc36c9f8a2a7265d1ce44be6a
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-01 18:32:07 -0500

    mb

--- dart/matching-brackets/lib/matching_brackets.dart
+++ dart/matching-brackets/lib/matching_brackets.dart
@@ -1,3 +1,17 @@
 class MatchingBrackets {
-  // Put your code here
+  static const MAP = {'[': ']', '(': ')', '{': '}'};
+
+  bool isPaired(String s) {
+    var stack = [];
+
+    return s.split("").every((e) {
+          if (MAP.containsKey(e)) {
+            stack.add(e);
+          } else if (MAP.containsValue(e)) {
+            if (stack.isEmpty || MAP[stack.removeLast()] != e) return false;
+          }
+          return true;
+        }) &&
+        stack.isEmpty;
+  }
 }
