97381f416e94a1bb536ac031d6361a0406e03d43
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-01 22:20:08 -0500

    wt

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,23 @@
 package techpalace
 
+import (
+	"fmt"
+	"strings"
+)
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	stars := strings.Repeat("*", numStarsPerLine)
+	return fmt.Sprintf("%s\n%s\n%s", stars, welcomeMsg, stars)
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	return strings.TrimSpace(strings.ReplaceAll(oldMsg, "*", ""))
+
 }
