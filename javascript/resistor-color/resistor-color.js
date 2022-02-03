80886c1c70852b114d4b141b3e663ac6474b6ea0
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-03 15:27:19 -0500

    rc

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,10 +1,16 @@
-//
-// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const MAP = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+}
 
-export const colorCode = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+export const colorCode = (color) => MAP[color]
 
-export const COLORS = undefined;
+export const COLORS = Object.keys(MAP)
