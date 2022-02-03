6fa193e78569052871166c6579bb3e43852a3a0b
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-03 10:08:16 -0500

    an

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -2,6 +2,8 @@
 class ArmstrongNumbers {
     boolean isArmstrongNumber(int numberToCheck) {
 
-        return numberToCheck == String.valueOf(numberToCheck).chars().map(Character::getNumericValue).mapToDouble(c -> Math.pow(c, String.valueOf(numberToCheck).length())).sum();
+        return numberToCheck == String.valueOf(numberToCheck).chars()
+                .map(Character::getNumericValue)
+                .mapToDouble(c -> Math.pow(c, String.valueOf(numberToCheck).length())).sum();
     }
 }
