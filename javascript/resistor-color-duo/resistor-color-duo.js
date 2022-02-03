181c18fbc5dbcabf8474c283eb5100e68e722084
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-03 15:29:37 -0500

    rcd

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -2,7 +2,17 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const MAP = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+}
 
-export const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+export const decodedValue = (codes) => parseInt(codes.slice(0, 2).map(c => MAP[c]).join(""));
