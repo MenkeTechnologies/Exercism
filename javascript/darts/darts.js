0d90d3d3543a9a1df2563186898f5598847db46b
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-03 17:49:28 -0500

    darts

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -1,8 +1,4 @@
-//
-// This is only a SKELETON file for the 'Darts' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const score = () => {
-  throw new Error('Remove this statement and implement this function');
+export const score = (x, y) => {
+  const d = Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5)
+  return d <= 1 ? 10 : d <= 5 ? 5 : d <= 10 ? 1 : 0;
 };
