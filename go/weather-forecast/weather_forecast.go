8ab2f66f3d5cb0509a3fffe84f1d8b215f11cd47
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-04 00:00:30 -0500

    wf

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather description.
 package weather
 
+// CurrentCondition string.
 var CurrentCondition string
+
+// CurrentLocation string.
 var CurrentLocation string
 
+// Forecast function.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
