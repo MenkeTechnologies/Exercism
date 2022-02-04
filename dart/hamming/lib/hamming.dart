d89330d7d422b2595cee11b8ebfc825c08d05c03
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-04 11:55:51 -0500

    ham

--- dart/hamming/lib/hamming.dart
+++ dart/hamming/lib/hamming.dart
@@ -1,3 +1,14 @@
 class Hamming {
-  // Put your code here
+  int validate(String l, String r) {
+    if (l.length != r.length) {
+      throw ArgumentError(l.isEmpty || r.isEmpty
+          ? 'no strand must be empty'
+          : 'left and right strands must be of equal length');
+    }
+
+    return l.length;
+  }
+
+  int distance(String l, String r) =>
+      Iterable<int>.generate(validate(l, r)).where((i) => l[i] != r[i]).length;
 }
