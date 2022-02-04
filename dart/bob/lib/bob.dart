c1f835af8105f48db2bff022fe6550e679620865
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-04 11:38:02 -0500

    bob

--- dart/bob/lib/bob.dart
+++ dart/bob/lib/bob.dart
@@ -1,3 +1,17 @@
 class Bob {
-  // Put your code here
+  String response(String s) {
+    s = s.replaceAll(RegExp(r'\s'), '');
+
+    if (s.isEmpty) {
+      return 'Fine. Be that way!';
+    }
+
+    if (RegExp('[A-Z]').hasMatch(s) && !RegExp('[a-z]').hasMatch(s)) {
+      return s.endsWith("?")
+          ? 'Calm down, I know what I\'m doing!'
+          : 'Whoa, chill out!';
+    }
+
+    return s.endsWith("?") ? 'Sure.' : 'Whatever.';
+  }
 }
