0e700a940e28bd599afb1b0692b78f4d90b612f8
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-04 11:45:25 -0500

    sa

--- dart/space-age/lib/space_age.dart
+++ dart/space-age/lib/space_age.dart
@@ -1,3 +1,19 @@
 class SpaceAge {
-  // Put your code here
+
+  static const SPY = 31557600;
+
+  final Map<String, double> orbitalPeriods = {
+    'Earth': 1,
+    'Mercury': 0.2408467,
+    'Venus': 0.61519726,
+    'Mars': 1.8808158,
+    'Jupiter': 11.862615,
+    'Saturn': 29.447498,
+    'Uranus': 84.016846,
+    'Neptune': 164.79132
+  };
+
+
+  num age({required String planet, required int seconds}) => num.parse((seconds / SPY / orbitalPeriods[planet]!).toStringAsFixed(2));
+
 }
