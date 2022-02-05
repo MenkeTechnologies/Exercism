c51dc3fb7fbea2572f31e7ac3ed8830df489246c
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-05 11:48:01 -0500

    rna

--- dart/rna-transcription/lib/rna_transcription.dart
+++ dart/rna-transcription/lib/rna_transcription.dart
@@ -1,3 +1,12 @@
 class RnaTranscription {
+
+  final Map<String, String> MAP = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U',
+  };
+
+  String toRna(String s) => s.split('').map((e) => MAP[e]).join('');
   // Put your code here
 }
