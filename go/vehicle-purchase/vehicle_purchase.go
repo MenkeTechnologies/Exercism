6c6dc50c0b7267981f0c1215616c3dd8d92ad3a2
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-05 02:01:23 -0500

    vp

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -2,15 +2,32 @@ package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+
+	if kind == "car" || kind == "truck" {
+		return true
+	}
+	return false
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in dictionary order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	r := option1
+	if option1 > option2 {
+		r = option2
+	}
+
+	return r + " is clearly the better choice."
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+
+	f := 0.5
+	if age < 3 {
+		f = 0.8
+	} else if age < 10 {
+		f = 0.7
+	}
+
+	return originalPrice * f
 }
