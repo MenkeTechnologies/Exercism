4b7c0676c53fa001b230793213fbb4669863bd00
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-06 12:10:36 -0500

    rc

--- dart/resistor-color/lib/resistor_color.dart
+++ dart/resistor-color/lib/resistor_color.dart
@@ -1,3 +1,19 @@
 class ResistorColor {
-  // Put your code here
+
+  static const MAP = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9
+  };
+
+  List<String> get colors => MAP.keys.toList();
+
+  int colorCode(String colo) => MAP[colo] ?? -1;
 }
