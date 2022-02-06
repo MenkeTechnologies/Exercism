dae8883e33750441b3b7d261b6fd14d58c2b2e5f
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-06 12:17:00 -0500

    rcd

--- dart/resistor-color-duo/lib/resistor_color_duo.dart
+++ dart/resistor-color-duo/lib/resistor_color_duo.dart
@@ -1,3 +1,16 @@
 class ResistorColorDuo {
-  // Put your code here
+  static const MAP = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9
+  };
+
+  int value(List<String> list) => int.parse(list.take(2).map((e) => MAP[e] ?? -1).join(''));
 }
