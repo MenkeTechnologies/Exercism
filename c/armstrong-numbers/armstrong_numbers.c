1d46ee9eba4a14c82b9c59a217c838a1356ef588
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-06 15:05:31 -0500

    arm

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,1 +1,16 @@
 #include "armstrong_numbers.h"
+#include <stdio.h>
+#include <math.h>
+
+bool is_armstrong_number(int candidate) {
+    char buf[10];
+
+    int sum = 0;
+    int len = sprintf(buf, "%d", candidate);
+
+    for (int i = 0; i < len; ++i) {
+        sum += pow(buf[i] - '0', len);
+    }
+
+    return sum == candidate;
+}
