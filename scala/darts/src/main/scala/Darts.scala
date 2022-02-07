9a40e612fe7704e54f9a640467fa02a8b78a3ba0
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-07 17:32:29 -0500

    darts

--- scala/darts/src/main/scala/Darts.scala
+++ scala/darts/src/main/scala/Darts.scala
@@ -1,16 +1,10 @@
 import scala.math.pow
 
 object Darts {
-  def score(x: Double, y: Double): Int = {
-
-    val d = pow(pow(x, 2) + pow(y, 2), 0.5)
-
-    d match {
-      case d if d <= 1 => 10
-      case d if d <= 5 => 5
-      case d if d <= 10 => 1
-      case _ => 0
-    }
+  def score(x: Double, y: Double) = pow(pow(x, 2) + pow(y, 2), 0.5) match {
+    case d if d <= 1 => 10
+    case d if d <= 5 => 5
+    case d if d <= 10 => 1
+    case _ => 0
   }
-
 }
