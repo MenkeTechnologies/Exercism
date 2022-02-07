c5b0ab69034ad79b992d652d0729045d6fb325d7
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-07 12:52:12 -0500

    cc

--- dart/collatz-conjecture/lib/collatz_conjecture.dart
+++ dart/collatz-conjecture/lib/collatz_conjecture.dart
@@ -1,3 +1,19 @@
 class CollatzConjecture {
-  // Put your code here
+  int steps(int limit) {
+    if (limit < 1) {
+      throw ArgumentError("Only positive numbers are allowed");
+    }
+    var cnt = 0;
+
+    while (limit > 1) {
+      if (limit % 2 == 0) {
+        limit ~/= 2;
+      } else {
+        limit = 3 * limit + 1;
+      }
+      ++cnt;
+    }
+
+    return cnt;
+  }
 }
