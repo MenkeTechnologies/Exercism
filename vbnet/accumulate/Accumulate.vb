b5f4b52ba685c1777e528133cd7f0096ec17eb8f
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-06 22:56:20 -0500

    acc

--- vbnet/accumulate/Accumulate.vb
+++ vbnet/accumulate/Accumulate.vb
@@ -1,5 +1,12 @@
 Imports System.Runtime.CompilerServices
 
 Module AccumulateExtensions
+    <Extension()>
+    Public Iterator Function Accumulate(Of t)(collection As IEnumerable(Of t), op as Func(Of t,t)) as IEnumerable(Of t)
+        For Each e in collection
+            Yield op(e)
+        Next
+
+    End Function
 
 End Module
