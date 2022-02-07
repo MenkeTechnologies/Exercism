655eca513f08098afc763ed2f144270428074064
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-06 23:35:47 -0500

    hamm

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,22 +1,6 @@
-# frozen_string_literal: true
-# Write your code for the 'Hamming' exercise in this file. Make the tests in
-# `hamming_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/hamming` directory.
 class Hamming
-
-  def self.compute(str1, str2)
-
-    raise ArgumentError if str1.size != str2.size
-
-    (0..str1.size - 1).reduce(0) do
-      if str1[_2] != str2[_2]
-        _1 + 1
-      else
-        _1
-      end
-    end
-
+  def self.compute(s1, s2)
+    raise ArgumentError if s1.size != s2.size
+    (0..s1.size - 1).reduce(0) { s1[_2] != s2[_2] ? _1 + 1 : _1 }
   end
 end
