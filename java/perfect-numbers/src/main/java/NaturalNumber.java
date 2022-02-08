862fec2f3c86240138ae7e4270ba42e65b73c495
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-08 11:55:18 -0500

    pn

--- java/perfect-numbers/src/main/java/NaturalNumber.java
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,5 +1,22 @@
+import java.util.stream.IntStream;
+
 class NaturalNumber {
+    Classification classification;
 
+    public NaturalNumber(int num) {
+        if (num <= 0) throw new IllegalArgumentException("You must supply a natural number (positive integer)");
 
+        int sum = IntStream.range(1, num).filter(n -> num % n == 0).sum();
+        if (sum < num) {
+            classification = Classification.DEFICIENT;
+        } else if (sum > num) {
+            classification = Classification.ABUNDANT;
+        } else {
+            classification = Classification.PERFECT;
+        }
+    }
 
+    public Classification getClassification() {
+        return classification;
+    }
 }
