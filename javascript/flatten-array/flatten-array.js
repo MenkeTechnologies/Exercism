d41964d701a95e8e90741e9185f6600814738380
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-09 13:31:56 -0500

    fa

--- javascript/flatten-array/flatten-array.js
+++ javascript/flatten-array/flatten-array.js
@@ -3,6 +3,5 @@
 // convenience to get you started writing code faster.
 //
 
-export const flatten = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+export const flatten = (ary) =>
+  ary.map(n => Array.isArray(n) ? flatten([...n]) : [n]).reduce((acc,n ) => [...acc, ...n], []).filter(n => n != null)
