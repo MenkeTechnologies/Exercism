9ff73f659714d81d0030ebb011a669d35a20f2ba
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-10 11:53:56 -0500

    isbn

--- dart/isbn-verifier/lib/isbn_verifier.dart
+++ dart/isbn-verifier/lib/isbn_verifier.dart
@@ -1,1 +1,15 @@
-// Put your code here
+bool isValid(String s) {
+  if (RegExp(r"^\d-?\d{3}-?\d{5}-?[\dX]$").hasMatch(s) == false) return false;
+
+  return s.replaceAll('-', '')
+              .split('')
+              .reversed
+              .toList()
+              .asMap()
+              .map(
+                  (k, v) => MapEntry(k, v == 'X' ? 10 : int.parse(v) * (k + 1)))
+              .values
+              .reduce((acc, n) => acc + n) %
+          11 ==
+      0;
+}
