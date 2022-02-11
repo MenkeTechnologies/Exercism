191670b37d5db0551f8e247f55be08f8dd857b76
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-11 17:00:50 -0500

    diamond

--- kotlin/diamond/src/main/kotlin/Diamond.kt
+++ kotlin/diamond/src/main/kotlin/Diamond.kt
@@ -1,27 +1,10 @@
 class DiamondPrinter {
-    fun printToList(c: Char): List<String> {
-
-        val alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-        val indexOf = alpha.indexOf(c)
-
-        if (indexOf < 0) {
-            return listOf()
-        }
-
-        val asc = 0..indexOf
-
-        return (asc + asc.reversed().drop(1)).map {
-            (asc.reversed() + asc.drop(1)).fold("") { acc, midOffset ->
-                if (midOffset == it) {
-                    acc + alpha[midOffset]
-                } else {
-                    "$acc "
-                }
-            }
-        }.toList()
-
-    }
-
+    fun printToList(c: Char) =
+            ((0..c - 'A') + (0..c - 'A').reversed().drop(1)).map {
+                ((0..c - 'A').reversed() + (0..c - 'A').drop(1))
+                        .fold("") { acc, midOffset ->
+                            acc + if (midOffset == it) 'A' + midOffset else " "
+                        }
+            }.toList()
 
 }
