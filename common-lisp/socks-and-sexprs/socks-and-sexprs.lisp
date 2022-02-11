e260c82b54c6624112cda2cfd669343e327fa4b9
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-11 11:53:36 -0500

    bird watch

--- common-lisp/socks-and-sexprs/socks-and-sexprs.lisp
+++ common-lisp/socks-and-sexprs/socks-and-sexprs.lisp
@@ -6,19 +6,19 @@
 (in-package :socks-and-sexprs)
 
 ;; Evaluates to some symbol (not a keyword)
-(defun lennys-favorite-food ())
+(defun lennys-favorite-food ( ) 'lasagna)
 
 ;; Evaluates to some keyword
-(defun lennys-secret-keyword ())
+(defun lennys-secret-keyword () :aliens-are-real)
 
 ;; Evaluates to T if THING is an atom, NIL otherwise
-(defun is-an-atom-p (thing))
+(defun is-an-atom-p (thing) (atom thing))
 
 ;; Evaluates to T if THING is a cons, NIL otherwise
-(defun is-a-cons-p (thing))
+(defun is-a-cons-p (thing) (consp thing))
 
 ;; Evaluates to the first part of CONS
-(defun first-thing (cons))
+(defun first-thing (cons) (car cons))
 
 ;; Evaluates to the 'rest' of the CONS
-(defun rest-of-it (cons))
+(defun rest-of-it (cons) (cdr cons))
