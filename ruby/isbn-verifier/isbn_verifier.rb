7b9cbf8df2c2ea82ddb95a79fd031961668ef4fa
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-11 11:41:17 -0500

    isbn

--- ruby/isbn-verifier/isbn_verifier.rb
+++ ruby/isbn-verifier/isbn_verifier.rb
@@ -1,6 +1,5 @@
 class IsbnVerifier
   def self.valid?(str)
-    return false if str !~ /^\d-?\d{3}-?\d{5}-?[\dX]$/
-    str.scan(/\w/).reverse.each_with_index.map { _1 == 'X' ? 10 : _1.to_i * (_2 + 1) }.sum % 11 == 0
+    str =~ /^\d-?\d{3}-?\d{5}-?[\dX]$/ && str.scan(/\w/).reverse.each_with_index.map { _1 == 'X' ? 10 : _1.to_i * (_2 + 1) }.sum % 11 == 0
   end
 end
