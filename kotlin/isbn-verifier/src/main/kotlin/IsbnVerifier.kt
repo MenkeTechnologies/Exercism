3ae2eb32b0095b8a7b4c711bc641ea682f09c2e1
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-11 11:52:11 -0500

    isbn

--- kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
+++ kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
@@ -1,18 +1,11 @@
 class IsbnVerifier {
 
-    fun isValid(number: String): Boolean {
-        val r = Regex("""^\d-?\d{3}-?\d{5}-?[\dX]$""")
-
-        if (!r.matches(number)) {
-            return false
-        }
-
-        return number.reversed()
-                .filter { it.isLetterOrDigit() }
-                .withIndex()
-                .map {
-                    if (it.value == 'X') 10 else Character.getNumericValue(it.value) * (it.index + 1)
-                }.sum() % 11 == 0
-
-    }
+    fun isValid(number: String) =
+            Regex("""^\d-?\d{3}-?\d{5}-?[\dX]$""").matches(number) &&
+                    number.reversed()
+                            .filter { it.isLetterOrDigit() }
+                            .withIndex()
+                            .map {
+                                if (it.value == 'X') 10 else Character.getNumericValue(it.value) * (it.index + 1)
+                            }.sum() % 11 == 0
 }
