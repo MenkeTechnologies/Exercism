7f2281921dfacf5ce9e6b94437effad0a518ccaf
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-11 14:24:56 -0500

    isbn

--- groovy/isbn-verifier/src/main/groovy/IsbnVerifier.groovy
+++ groovy/isbn-verifier/src/main/groovy/IsbnVerifier.groovy
@@ -1,8 +1,6 @@
 class IsbnVerifier {
     static boolean isValid(String isbn) {
-        if (!isbn.matches(/^\d-?\d{3}-?\d{5}-?[\dX]$/)) {
-            return false
-        }
+        isbn.matches(/^\d-?\d{3}-?\d{5}-?[\dX]$/) &&
         isbn.reverse().collect{it as char}.findAll { it.letterOrDigit }.withIndex().collect { ch, i ->
             ch == 'X' ? 10 : Character.getNumericValue(ch) * (i + 1)
         }.sum() % 11 == 0
