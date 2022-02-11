bbe96f756e520ab93f39cf7cb0fc574ea47407c5
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-10 23:15:50 -0500

    rd

--- erlang/raindrops/src/raindrops.erl
+++ erlang/raindrops/src/raindrops.erl
@@ -2,5 +2,14 @@
 
 -export([convert/1]).
 
+-define(Factors, [{3, "Pling"}, {5, "Plang"}, {7, "Plong"}]).
 
-convert(_Number) -> undefined.
+convert(Number) ->
+  case lists:foldl(fun({Factor, Sound}, Acc) ->
+    case Number rem Factor of
+      0 -> Acc ++ Sound;
+      _ -> Acc
+    end end, "", ?Factors) of
+    "" -> integer_to_list(Number);
+    Sound -> Sound
+  end.
