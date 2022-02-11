9dab23043a69598a1d327710c8f13d04b989aecd
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-11 11:28:22 -0500

    dp

--- java/diamond/src/main/java/DiamondPrinter.java
+++ java/diamond/src/main/java/DiamondPrinter.java
@@ -1,9 +1,23 @@
 import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.Comparator.reverseOrder;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.rangeClosed;
+import static java.util.stream.Stream.concat;
 
 class DiamondPrinter {
-
     List<String> printToList(char a) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
 
+        int limit = a - 'A';
+        return concat(rangeClosed(0, limit).boxed(),
+                IntStream.range(0, limit).boxed().sorted(reverseOrder()))
+                .map(r ->
+                        concat(rangeClosed(0, limit).boxed().sorted(reverseOrder()),
+                                rangeClosed(1, limit).boxed())
+                                .map(c -> r.equals(c) ? String.valueOf((char) (r + 'A')) : " ").collect(joining(""))
+
+                ).collect(toList());
+    }
 }
