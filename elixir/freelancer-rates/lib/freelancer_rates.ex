07cbc212efbd663d488e97ccc74765282462ca95
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-11 17:10:25 -0500

    freelancer

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,30 @@
 defmodule FreelancerRates do
+
+  @daily_rate 8.0
+  @monthly_billable_days 22
+
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    hourly_rate * @daily_rate
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount * (100 - discount) / 100
+  end
+
+  def daily_discounted(hourly_rate, discount) do
+    hourly_rate
+    |> daily_rate
+    |> apply_discount(discount)
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    daily_discounted(hourly_rate, discount) * @monthly_billable_days
+    |> ceil
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    budget / daily_discounted(hourly_rate, discount)
+    |> Float.floor(1)
+
   end
 end
