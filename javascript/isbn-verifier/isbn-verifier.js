e4512bd8e23c850493960359e52aa5faf62784c5
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-11 11:57:52 -0500

    bw

--- javascript/isbn-verifier/isbn-verifier.js
+++ javascript/isbn-verifier/isbn-verifier.js
@@ -1,4 +1,4 @@
-export const isValid = (str) =>
+export const isValid = str =>
   (str.match(/^\d-?\d{3}-?\d{5}-?[\dX]$/) || false) &&
     str.split('').filter(c => c.match(/[X0-9]/)).reverse()
       .map((n, i) => n === 'X' ? 10 : n * (i + 1))
