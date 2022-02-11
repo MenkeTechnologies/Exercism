8308b406fabcbe0565bd7442b173e1d7a952f657
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-11 11:53:36 -0500

    bird watch

---
+++ go/bird-watcher/bird_watcher.go
@@ -0,0 +1,17 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+}
