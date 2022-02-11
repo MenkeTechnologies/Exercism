bbbfd3e0076c74258e32b9b558d2a735ce452805
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-11 11:57:52 -0500

    bw

--- go/bird-watcher/bird_watcher.go
+++ go/bird-watcher/bird_watcher.go
@@ -3,15 +3,27 @@ package birdwatcher
 // TotalBirdCount return the total bird count by summing
 // the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
-
+	c := 0
+	for _, b := range birdsPerDay {
+		c += b
+	}
+	return c
 }
 
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
+	return TotalBirdCount(birdsPerDay[(week-1)*7 : week*7])
 }
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
 func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := range birdsPerDay {
+		if i%2 == 0 {
+			birdsPerDay[i]++
+
+		}
+	}
+	return birdsPerDay
 }
