66db863d257ad6fffb7b411fbe2491552e94c23d
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-12 15:52:53 -0500

    more exe

---
+++ common-lisp/grains/grains.lisp
@@ -0,0 +1,8 @@
+(defpackage :grains
+  (:use :cl)
+  (:export :square :total))
+(in-package :grains)
+
+(defun square (n) )
+
+(defun total () )
---
+++ common-lisp/lillys-lasagna/lillys-lasagna.lisp
@@ -0,0 +1,16 @@
+(defpackage :lillys-lasagna
+  (:use :cl)
+  (:export :expected-time-in-oven
+           :remaining-minutes-in-oven
+           :preparation-time-in-minutes
+           :elapsed-time-in-minutes))
+
+(in-package :lillys-lasagna)
+
+;; Define function expected-time-in-oven
+
+;; Define function remaining-minutes-in-oven
+
+;; Define function preparation-time-in-minutes
+
+;; Define function elapsed-time-in-minutes
