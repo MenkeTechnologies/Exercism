72c7fa7cb53014a225e5bf24c1f9c8c681582b50
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-11 20:57:40 -0500

    ll

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,9 +1,27 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+
+    case {level, legacy?} do
+      {0, false} -> :trace
+      {1, _} -> :debug
+      {2, _} -> :info
+      {3, _} -> :warning
+      {4, _} -> :error
+      {5, false} -> :fatal
+      _ -> :unknown
+    end
+
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+
+    lbl = to_label(level, legacy?)
+    cond do
+      lbl in [:error, :fatal] -> :ops
+      lbl == :unknown and legacy? -> :dev1
+      lbl == :unknown -> :dev2
+      true -> false
+    end
+
   end
 end
