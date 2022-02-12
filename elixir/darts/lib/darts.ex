223512f28c390566d2f68e0bab586284ed80648a
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-12 12:31:39 -0500

    darts

--- elixir/darts/lib/darts.ex
+++ elixir/darts/lib/darts.ex
@@ -1,3 +1,5 @@
+import Float, only: [pow: 2]
+
 defmodule Darts do
   @type position :: {number, number}
 
@@ -6,5 +8,14 @@ defmodule Darts do
   """
   @spec score(position :: position) :: integer
   def score({x, y}) do
+    d = pow(pow(x * 1.0, 2) + pow(y * 1.0, 2), 0.5)
+
+    cond do
+      d <= 1 -> 10
+      d <= 5 -> 5
+      d <= 10 -> 1
+      true -> 0
+    end
+
   end
 end
