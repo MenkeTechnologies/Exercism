fe7be1a9ae882845267d6c77faa6e6f57e572cf7
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-13 01:41:41 -0500

    pn

--- vbnet/perfect-numbers/PerfectNumbers.vb
+++ vbnet/perfect-numbers/PerfectNumbers.vb
@@ -1,9 +1,27 @@
 ﻿Imports System
-Imports System.Collections.Generic
 Imports System.Linq
 
+Public Enum Classification
+    Perfect
+    Abundant
+    Deficient
+End Enum
+
 Public Module PerfectNumbers
-    Public Function Classify(ByVal number As Integer) As Classification
-        Throw New NotImplementedException("You need to implement this function")
+    Public Function Classify(number As Integer) As Classification
+
+        If number < 1
+            Throw new ArgumentOutOfRangeException()
+        End If
+
+        Dim sum = Enumerable.Range(1, number/2).Where(function(i) number Mod i = 0).Sum()
+
+        If sum > number
+            Return Classification.Abundant
+        ElseIf sum < number
+            Return Classification.Deficient
+        Else
+            Return Classification.Perfect
+        End If
     End Function
 End Module
