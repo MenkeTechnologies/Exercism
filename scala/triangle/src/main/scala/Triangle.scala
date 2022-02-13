4e3db77e4d17ae61374c25b08ebdef87d4f58141
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-12 21:20:09 -0500

    tri

--- scala/triangle/src/main/scala/Triangle.scala
+++ scala/triangle/src/main/scala/Triangle.scala
@@ -1,27 +1,11 @@
 case class Triangle(s1: Double, s2: Double, s3: Double) {
 
   val l = List(s1, s2, s3).sorted
-  val s = scala.collection.mutable.Set[Double]()
-  s ++= l
+  val s = l.toSet
 
-  val isValid = {
-    !s.exists(_ <= 0) && l.head + l(1) >= l(2)
-  }
-
-  def scalene = {
-    isValid && s.size == 3
-  }
-
-  def isosceles = {
-
-    isValid && s.size == 2 || s.size == 1
-  }
-
-  def equilateral = {
-
-    isValid && s.size == 1
-  }
-
-
+  val isValid = !s.exists(_ <= 0) && l.head + l(1) >= l(2)
+  def scalene = isValid && s.size == 3
+  def isosceles = isValid && s.size <= 2
+  def equilateral = isValid && s.size == 1
 }
 
