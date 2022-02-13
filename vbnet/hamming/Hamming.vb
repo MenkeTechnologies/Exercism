c72e7ccdfe31bd258c0e6cce841b9dc679f0e703
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-13 12:11:53 -0500

    ham

--- vbnet/hamming/Hamming.vb
+++ vbnet/hamming/Hamming.vb
@@ -1,5 +1,11 @@
 Public Module Hamming
     Public Function Distance(ByVal firstStrand As String, ByVal secondStrand As String) As Integer
-        Throw New NotImplementedException("You need to implement this function")
+
+        if firstStrand.Length <> secondStrand.Length
+            Throw New ArgumentException("Strands have different length")
+        End If
+
+        Return firstStrand.Zip(secondStrand).Count(function(tuple) tuple.First <> tuple.Second)
+
     End Function
 End Module
