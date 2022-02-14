1c53cab2c5a2958f2c9f4cf8b9301833ce935df0
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-14 01:02:16 -0500

    pn

--- vbnet/phone-number/PhoneNumber.vb
+++ vbnet/phone-number/PhoneNumber.vb
@@ -1,5 +1,16 @@
+Imports System.Text.RegularExpressions
+
 Public Class PhoneNumber
-    Public Shared Function Clean(ByVal phoneNumber As String) As String
-        Throw New NotImplementedException("You need to implement this function")
+    Dim Shared ReadOnly re = New Regex("^\s*(\+?1?\s*)?\(?([2-9]\d{2})\)?[-.\s]*([2-9]\d{2})[-.\s]*(\d{4})\s*$")
+
+    Public Shared Function Clean(phoneNumber As String) As String
+
+        Dim match = re.Match(phoneNumber)
+
+        If Not match.Success Then
+            Throw New ArgumentException()
+        End If
+
+        Return $"{match.Groups(2)}{match.Groups(3)}{match.Groups(4)}"
     End Function
 End Class
