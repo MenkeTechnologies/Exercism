1498bc8fa54813687e5274a55d66ea16ed83b411
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-15 12:46:11 -0500

    rn

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,21 +1,5 @@
 object Hamming {
-  def distance(str: String, str1: String): Option[Int] = {
-
-    if (str.length != str1.length) {
-        return None
-    }
-
-    var sum = 0
-
-    for (i <- 0 until str.length) {
-      if (str.charAt(i) != str1.charAt(i)) {
-          sum += 1
-      }
-
-    }
-
-    Some(sum)
-
-  }
-
+  def distance(s1: String, s2: String) =
+    if (s1.length != s2.length) { None }
+    else Some(s1.zip(s2).count(t => t._1 != t._2))
 }
--- scala/robot-name/src/main/scala/RobotName.scala
+++ scala/robot-name/src/main/scala/RobotName.scala
@@ -1,19 +1,7 @@
 import scala.util.Random.nextInt
 
 class Robot {
-
-  private var _name = gen()
-
-  def reset() = {
-    _name = gen()
-  }
-
-  def gen() = {
-
-    val alpha = 'A' to 'Z'
-    (Stream.continually(alpha(nextInt(26))).take(2) ++ Stream.continually(nextInt(10)).take(3)).mkString
-  }
-
-  def name = _name
-
+  var name = gen()
+  def reset() = name = gen()
+  def gen() = (Stream.continually(('A' to 'Z')(nextInt(26))).take(2) ++ Stream.continually(nextInt(10)).take(3)).mkString
 }
