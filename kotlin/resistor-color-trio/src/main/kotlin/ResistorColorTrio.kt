42b4bc2c0b3855d37cdc0ced1378409449d0a512
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-17 15:43:24 -0500

    rct

--- kotlin/resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
+++ kotlin/resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
@@ -1,18 +1,10 @@
 object ResistorColorTrio {
 
     fun text(vararg input: Color): String {
-        var res = ""
+        val raw = input.withIndex().map { (i,n) -> if (i < 2) n.ordinal.toString() else "0".repeat(n.ordinal) }.joinToString("")
 
-        for ((cnt, color) in input.withIndex()) {
-            if (cnt < 2) {
-                res += color.ordinal
-            } else {
-                res += "0".repeat(color.ordinal)
-            }
-        }
-
-        var prefix = res.dropLastWhile { it == '0' }
-        val zeroCnt = res.takeLastWhile { it == '0' }.length
+        var prefix = raw.dropLastWhile { it == '0' }
+        val zeroCnt = raw.takeLastWhile { it == '0' }.length
         prefix += "0".repeat(zeroCnt % 3)
         val unit = Unit.values()[zeroCnt / 3].toString().toLowerCase()
 
