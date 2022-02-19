c490df44241ea4f549ee8b5fd66039e903caee23
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-19 15:07:29 -0500

    hs

--- elixir/high-score/lib/high_score.ex
+++ elixir/high-score/lib/high_score.ex
@@ -1,25 +1,25 @@
 defmodule HighScore do
+  @init 0
   def new() do
-    # Please implement the new/0 function
+    %{}
   end
 
-  def add_player(scores, name, score) do
-    # Please implement the add_player/3 function
+  def add_player(scores, name, score \\ @init) do
+    Map.put(scores, name, score)
   end
-
   def remove_player(scores, name) do
-    # Please implement the remove_player/2 function
+    Map.delete(scores, name)
   end
 
   def reset_score(scores, name) do
-    # Please implement the reset_score/2 function
+    Map.put(scores, name, @init)
   end
 
   def update_score(scores, name, score) do
-    # Please implement the update_score/3 function
+    Map.update(scores, name, score, &score + &1)
   end
 
   def get_players(scores) do
-    # Please implement the get_players/1 function
+    Map.keys(scores)
   end
 end
