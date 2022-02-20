48822f08ff843416955e9c1d3d74c1ac1f636679
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-20 15:17:41 -0500

    rc

--- elixir/resistor-color/lib/resistor_color.ex
+++ elixir/resistor-color/lib/resistor_color.ex
@@ -1,8 +1,8 @@
 defmodule ResistorColor do
-  @doc """
-  Return the value of a color band
-  """
-  @spec code(atom) :: integer()
-  def code(color) do
+
+  @colors %{black: 0, brown: 1, red: 2, orange: 3, yellow: 4, green: 5, blue: 6, violet: 7, grey: 8, white: 9}
+
+  def code(c) do
+    @colors[c]
   end
 end
