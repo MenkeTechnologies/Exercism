fdaee1e9dc6fa0c018684e74df6840090b85c1f1
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-21 10:23:02 -0500

    as

--- crystal/armstrong-numbers/src/armstrong-numbers.cr
+++ crystal/armstrong-numbers/src/armstrong-numbers.cr
@@ -1,1 +1,8 @@
 # Please implement your solution to armstrong-numbers in this file
+
+module ArmstrongNumbers
+
+  def self.armstrong_number?(num : Int)
+    num == num.digits.map { |d| d ** num.digits.size }.sum
+  end
+end
