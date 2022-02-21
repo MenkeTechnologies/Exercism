2cd38544f9ca12a32d0c6d1e8758fbdf9a29b70e
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-21 10:53:26 -0500

    arm

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,22 +1,5 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let mut digits = vec![];
-    let mut dup = num;
-    loop {
-        let digit = dup % 10;
-        digits.push(digit);
-        if dup < 10 {
-            break;
-        } else {
-            dup /= 10;
-        }
-    }
-
-    let len: u32 = digits.len() as u32;
-
-    let sum = digits
-        .iter()
-        .map(|a| u32::pow(*a, len))
-        .fold(0, |a, b| a + b);
-
-    sum == num
+    num == num.to_string().chars().map(|d| {
+        u32::pow(d.to_digit(10).unwrap(), num.to_string().chars().count() as u32)
+    }).sum()
 }
