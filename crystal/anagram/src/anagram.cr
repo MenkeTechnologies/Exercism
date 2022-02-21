f399d4771c5643e403a89bbd12ee7c665f4dd9eb
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-21 10:20:54 -0500

    ana

--- crystal/anagram/src/anagram.cr
+++ crystal/anagram/src/anagram.cr
@@ -1,1 +1,5 @@
-# Please implement your solution to anagram in this file
+module Anagram
+  def self.find(word : String, cand)
+    cand.select { |c| c.downcase.chars.sort == word.downcase.chars.sort && c.downcase != word.downcase }
+  end
+end
