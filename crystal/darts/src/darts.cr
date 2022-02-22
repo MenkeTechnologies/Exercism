9e40c775a48bb5507ac017eea736c170c5d4a69e
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-22 12:33:48 -0500

    darts

--- crystal/darts/src/darts.cr
+++ crystal/darts/src/darts.cr
@@ -1,1 +1,12 @@
-# Please implement your solution to darts in this file
+module Darts
+
+  def self.score(x,y)
+    case (x ** 2 + y ** 2) ** 0.5
+      when ..1 then 10
+      when ..5 then 5
+      when ..10 then 1
+      else 0
+    end
+  end
+
+end
