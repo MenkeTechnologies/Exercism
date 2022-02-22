fe0f5a08287a1859e57e3054d3cc5f31a11d111f
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-22 13:15:05 -0500

    allergies

--- crystal/allergies/src/allergies.cr
+++ crystal/allergies/src/allergies.cr
@@ -1,1 +1,22 @@
-# Please implement your solution to allergies in this file
+module Allergies
+
+  MAP = {
+  "eggs"         => 1 << 0,
+  "peanuts"      => 1 << 1,
+  "shellfish"    => 1 << 2,
+  "strawberries" => 1 << 3,
+  "tomatoes"     => 1 << 4,
+  "chocolate"    => 1 << 5,
+  "pollen"       => 1 << 6,
+  "cats"         => 1 << 7,
+  }
+
+  def self.allergic_to(allergy, m)
+    MAP[allergy] & m > 0
+
+  end
+
+  def self.list(m)
+    MAP.keys.select {|k| MAP[k] & m > 0}
+  end
+end
