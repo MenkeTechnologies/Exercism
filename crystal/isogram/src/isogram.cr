b6638cc0b6659d7f04caa30b3531dd1245fb7e7e
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-22 11:32:56 -0500

    iso

--- crystal/isogram/src/isogram.cr
+++ crystal/isogram/src/isogram.cr
@@ -1,1 +1,5 @@
-# Please implement your solution to isogram in this file
+module Isogram
+  def self.isogram?(str : String) : Bool
+    str.downcase.chars.select(&.letter?).group_by{|c| c}.values.all? {|v| v.size == 1}
+  end
+end
