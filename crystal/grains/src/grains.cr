a203d536dff470073b4bdb3012a4a3d055f923a4
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-22 15:32:22 -0500

    grains

--- crystal/grains/src/grains.cr
+++ crystal/grains/src/grains.cr
@@ -1,1 +1,13 @@
-# Please implement your solution to grains in this file
+module Grains
+
+  def self.square(n)
+    raise ArgumentError.new if n <1 || n > 64
+    2.to_u64 ** (n - 1)
+  end
+
+
+  def self.total
+    (1..64).map{|n| square(n)}.sum
+  end
+
+end
