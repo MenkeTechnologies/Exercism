7d93d630412e41a39b8b477630a33f190c545349
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-22 12:26:36 -0500

    rc

--- crystal/resistor-color/src/resistor_color.cr
+++ crystal/resistor-color/src/resistor_color.cr
@@ -1,1 +1,22 @@
-# Please implement your solution to resistor-color in this file
+module ResistorColor
+
+  MAP = {
+  "black"  => 0,
+  "brown"  => 1,
+  "red"    => 2,
+  "orange" => 3,
+  "yellow" => 4,
+  "green"  => 5,
+  "blue"   => 6,
+  "violet" => 7,
+  "grey"   => 8,
+  "white"  => 9
+  }
+  def self.color_code(c)
+    MAP[c]
+  end
+
+  def self.colors
+    MAP.keys
+  end
+end
