1b304d6de1a56e918fe8cb3b26fa2460378004a0
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-22 11:48:54 -0500

    etl

--- crystal/etl/src/etl.cr
+++ crystal/etl/src/etl.cr
@@ -1,1 +1,8 @@
-# Please implement your solution to etl in this file
+module ETL
+  def self.transform(input : Hash(String, Array(String)))
+    input.reduce(Hash(String, Int32).new) {|acc,n|
+      n[1].each {|v| acc[v.downcase] = n[0].to_i}
+      acc
+    }
+  end
+end
