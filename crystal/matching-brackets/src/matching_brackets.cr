f582438d24dbb5774e753def83c2ca7e6e05d29a
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-23 10:41:40 -0500

    mb

--- crystal/matching-brackets/src/matching_brackets.cr
+++ crystal/matching-brackets/src/matching_brackets.cr
@@ -1,1 +1,18 @@
-# Please implement your solution to matching-brackets in this file
+module MatchingBrackets
+
+  MAP = {'(' => ')', '[' => ']', '{' => '}'}
+
+  def self.valid?(s)
+    stk = [] of Char
+
+    s.chars.each do |c|
+      if MAP.has_key?(c)
+        stk << c
+        elsif MAP.has_value?(c)
+          return false if stk.empty? || MAP[stk.pop?] != c
+      end
+    end
+
+    stk.empty?
+  end
+end
