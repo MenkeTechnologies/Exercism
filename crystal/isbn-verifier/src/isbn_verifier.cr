97d1032ee18757d6b17fcdcc622a1c17bc775bed
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-23 16:57:00 -0500

    isbn

--- crystal/isbn-verifier/src/isbn_verifier.cr
+++ crystal/isbn-verifier/src/isbn_verifier.cr
@@ -1,1 +1,5 @@
-# Please implement your solution to isbn-verifier in this file
+module IsbnVerifier
+  def self.valid?(str)
+    str !~ /^\d-?\d{3}-?\d{5}-?[\dX]$/ ? false : str.scan(/\w/).map{|c|c[0]}.reverse.each_with_index.map { |c, i| c == "X" ? 10 : c.to_i * (i + 1) }.sum % 11 == 0
+  end
+end
--- crystal/strain/src/strain.cr
+++
@@ -1,1 +0,0 @@
-# Please implement your solution to strain in this file
