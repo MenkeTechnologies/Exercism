01a0b076d03d1ae70ad1f17de156353c04418a19
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-23 23:30:30 -0500

    luhn

--- crystal/luhn/src/luhn.cr
+++ crystal/luhn/src/luhn.cr
@@ -1,14 +1,10 @@
 module Luhn
   def self.valid?(s)
-
-    s = s.gsub(/\s/, "")
-
-    return false if s.size < 2 || s.chars.any? {|c| c.match?(/D/)}
-
-    str.chars.reverse.map { |c| c.to_i }.each.with_index.map { |n, i|
-      dbl = i.odd? ? n * n : n
-      dbl -= dbl > 9 ? 9: 0
+    str = s.gsub(/\s/, "")
+    str.size < 2 || str.chars.any? { |c| c.to_s !~ /\d/ } ? false :
+    str.chars.reverse.each.with_index.map { |c, i|
+      dbl = i.odd? ? c.to_i * 2 : c.to_i
+      dbl - (dbl > 9 ? 9 : 0)
     }.sum % 10 == 0
-
   end
 end
