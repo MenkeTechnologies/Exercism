135da0812cd951fb0d7e361cb18f6f153d1bd79a
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-24 11:18:41 -0500

    pf

--- crystal/prime-factors/src/prime_factors.cr
+++ crystal/prime-factors/src/prime_factors.cr
@@ -1,1 +1,19 @@
-# Please implement your solution to prime-factors in this file
+module PrimeFactors
+  def self.factors(init)
+    fac = [] of Int32
+    divisor = 2
+
+    while init > 1
+      while init % divisor == 0
+        fac << divisor
+        init /= divisor
+      end
+
+      divisor += 1
+
+    end
+
+    fac
+
+  end
+end
