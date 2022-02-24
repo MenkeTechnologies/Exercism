b9b52c9ed8b4789975a5afba8f5ac79a173bfe2e
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-24 11:14:55 -0500

    bin

--- crystal/binary/src/binary.cr
+++ crystal/binary/src/binary.cr
@@ -1,1 +1,9 @@
-# Please implement your solution to binary in this file
+class Binary
+  def self.to_decimal(s)
+    raise ArgumentError.new if s.match(/[^01]/)
+
+    s.chars.reduce(0) {|acc,n|
+      acc *  2 + n.to_i
+    }
+  end
+end
