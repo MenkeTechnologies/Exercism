7b87edb693c8d7d64baf2fe5023e0baba866a88f
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-24 21:46:33 -0500

    qa

--- java/queen-attack/src/main/java/QueenAttackCalculator.java
+++ java/queen-attack/src/main/java/QueenAttackCalculator.java
@@ -1,10 +1,35 @@
-/*
+class Queen {
+    final int x, y;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    public Queen(int x, int y) {
 
-Please remove this comment when submitting your solution.
+        if (x < 0) throw new IllegalArgumentException("Queen position must have positive row.");
+        if (x > 7) throw new IllegalArgumentException("Queen position must have row <= 7.");
+        if (y < 0) throw new IllegalArgumentException("Queen position must have positive column.");
+        if (y > 7) throw new IllegalArgumentException("Queen position must have column <= 7.");
 
-*/
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class QueenAttackCalculator {
+    private final int deltaX;
+    private final int deltaY;
+
+    public QueenAttackCalculator(Queen queen1, Queen queen2) {
+
+        if (queen1 == null || queen2 == null)
+            throw new IllegalArgumentException("You must supply valid positions for both Queens.");
+
+        if (queen1.x == queen2.x && queen1.y == queen2.y)
+            throw new IllegalArgumentException("Queens cannot occupy the same position.");
+
+        deltaX = Math.abs(queen1.x - queen2.x);
+        deltaY = Math.abs(queen1.y - queen2.y);
+    }
+
+    public boolean canQueensAttackOneAnother() {
+        return deltaX == 0 || deltaY == 0 || deltaX == deltaY;
+    }
+}
