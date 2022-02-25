54e3bdec9c2e18783cb8c935574f777b8183e004
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-25 16:30:10 -0500

    final

--- java/robot-name/src/main/java/Robot.java
+++ java/robot-name/src/main/java/Robot.java
@@ -5,19 +5,20 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Robot {
-    private static Map<String, Integer> MAP = new HashMap<>();
-    String name;
-    Random random = new Random();
+    private final static Map<String, Integer> MAP = new HashMap<>();
+    private String name;
+    private final Random random = new Random();
 
     public Robot() {
-        name = gen();
+        reset();
     }
 
     private String gen() {
 
         while (true) {
             String name = IntStream.range(0, 5)
-                    .map(i -> i < 2 ? random.nextInt(26) + 'A' : random.nextInt(10) + '0').mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining(""));
+                    .map(i -> i < 2 ? random.nextInt(26) + 'A' : random.nextInt(10) + '0')
+                    .mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining(""));
             if (!MAP.containsKey(name)) {
                 MAP.put(name, 1);
                 return name;
