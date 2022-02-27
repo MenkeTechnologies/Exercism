1a663c7159b4620d2be55a670bbf28783f2a2d73
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-26 21:24:15 -0500

    leap

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,7 +1,7 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 
 }
