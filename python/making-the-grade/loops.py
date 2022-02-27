38a8cd01bc02a3dbd9f3588a70692d7bec2d737f
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-27 15:53:34 -0500

    loops

---
+++ python/making-the-grade/loops.py
@@ -0,0 +1,70 @@
+def round_scores(student_scores):
+    """"Round all provided student scores.
+
+    :param student_scores: list of student exam scores as float or int.
+    :return: list of student scores *rounded* to nearest integer value.
+    """
+
+    return map(round, student_scores)
+
+
+def count_failed_students(student_scores):
+    """Count the number of failing students out of the group provided.
+
+    :param student_scores: list of integer student scores.
+    :return: integer count of student scores at or below 40.
+    """
+
+    return len([n for n in student_scores if n <= 40])
+
+
+def above_threshold(student_scores, threshold):
+    """Determine how many of the provided student scores were 'the best' based on the provided threshold.
+
+    :param student_scores: list of integer scores
+    :param threshold :  integer
+    :return: list of integer scores that are at or above the "best" threshold.
+    """
+
+    return [n for n in student_scores if n >= threshold]
+
+
+def letter_grades(highest):
+    """Create a list of grade thresholds based on the provided highest grade.
+
+    :param highest: integer of highest exam score.
+    :return: list of integer lower threshold scores for each D-A letter grade interval.
+             For example, where the highest score is 100, and failing is <= 40,
+             The result would be [41, 56, 71, 86]:
+
+             41 <= "D" <= 55
+             56 <= "C" <= 70
+             71 <= "B" <= 85
+             86 <= "A" <= 100
+    """
+    threshold = 41
+    return list(range(threshold, highest, round((highest - threshold) / 4)))
+
+
+
+def student_ranking(student_scores, student_names):
+    """Organize the student's rank, name, and grade information in ascending order.
+
+     :param student_scores: list of scores in descending order.
+     :param student_names: list of names in descending order by exam score.
+     :return: list of strings in format ["<rank>. <student name>: <score>"].
+     """
+
+    return [f"{index+1}. {name}: {score}" for index, (score, name) in enumerate(zip(student_scores, student_names))]
+
+
+
+def perfect_score(student_info):
+    """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
+
+    :param student_info: list of [<student name>, <score>] lists
+    :return: first `[<student name>, 100]` or `[]` if no student score of 100 is found.
+    """
+    ps = list(filter(lambda s: s[1] == 100, student_info))
+
+    return ps[0] if len(ps) > 0 else []
