6543b5b7e2a34cff08ce7fb6e0e3829f9b005cff
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-27 14:36:01 -0500

    pn

--- crystal/perfect-numbers/src/perfect_numbers.cr
+++ crystal/perfect-numbers/src/perfect_numbers.cr
@@ -1,1 +1,7 @@
-# Please implement your solution to perfect-numbers in this file
+module PerfectNumbers
+  def self.classify(num)
+      raise ArgumentError.new "n cannot be negative" if num < 1
+      sum = (1..num -1).select { |n| num % n == 0 }.sum
+      sum == num ? "perfect" : sum > num ? "abundant" : "deficient"
+  end
+end
