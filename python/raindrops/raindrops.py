7a45b736a0d0195660f75f1ac2634d58e8e2b1e5
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-28 08:49:48 -0500

    rain

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,6 @@
+drops = [(3, 'Pling'), (5, 'Plang'), (7, 'Plong')]
+
+
 def convert(number):
-    pass
+    sp = [s for f, s in drops if number % f == 0]
+    return "".join(sp) if sp else str(number)
