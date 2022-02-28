93bc682f4b5c0bd10f9bbb042e610993dd495bca
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-28 08:58:40 -0500

    ham

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,7 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+
+    return sum([0 if a == b else 1 for a,b in zip(strand_a, strand_b)])
+
+
