702eaaaae4a70db8789090771097d6e92e75c1cf
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-01 15:37:44 -0500

    cc

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,11 @@
 def steps(number):
-    pass
+    if number < 1:
+        raise ValueError("Only positive integers are allowed")
+    cnt = 0
+    while number > 1:
+        if number % 2 == 1:
+            number = 3 * number + 1
+        else:
+            number /= 2
+        cnt += 1
+    return cnt
