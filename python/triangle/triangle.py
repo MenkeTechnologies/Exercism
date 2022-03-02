c9e090da502584319a5e1bdd94405c0dac173562
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-02 13:29:59 -0500

    tri

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,18 @@
+def valid(sides):
+    sortt = sorted(sides)
+    return all(s > 0 for s in sortt) and sortt[0] + sortt[1] >= sortt[2]
+
+
 def equilateral(sides):
-    pass
+    sett = set(sides)
+    return valid(sides) and len(sett) == 1
 
 
 def isosceles(sides):
-    pass
+    sett = set(sides)
+    return valid(sides) and len(sett) <= 2
 
 
 def scalene(sides):
-    pass
+    sett = set(sides)
+    return valid(sides) and len(sett) == 3
