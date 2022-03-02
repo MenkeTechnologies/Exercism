baf17a676bb6c658a2ec15739df37b94db6c8a24
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-02 13:23:10 -0500

    luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,19 @@
+import re
+
+
 class Luhn:
     def __init__(self, card_num):
-        pass
+        self.num = "".join(str(card_num).split())
+
+    def acc(self, i, ch):
+        dbl = int(ch)
+        if i % 2 == 1:
+            dbl *= 2
+        if dbl > 9:
+            dbl -= 9
+        return dbl
 
     def valid(self):
-        pass
+        if len(self.num) < 2 or re.search(r"\D", self.num) != None:
+            return False
+        return sum(self.acc(i, ch) for i, ch in enumerate(self.num[::-1])) % 10 == 0
