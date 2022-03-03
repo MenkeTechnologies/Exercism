4060b14e0c3421d3e5c7edb3d90b24f765b76c09
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-03 12:48:31 -0500

    sh

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,2 +1,6 @@
-def commands(binary_str):
-    pass
+DICT = {'wink': 1, 'double blink': 2, 'close your eyes': 4, 'jump': 8}
+
+def commands(n):
+    n = int(n, 2)
+    l = [k for k,v in DICT.items() if n & v]
+    return l[::-1] if n & 16 else l
