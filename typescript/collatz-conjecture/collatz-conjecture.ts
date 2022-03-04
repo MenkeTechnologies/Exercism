1858c34781961840224c84f0e2c8f48afbac9e81
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-04 11:38:47 -0500

    coll

--- typescript/collatz-conjecture/collatz-conjecture.ts
+++ typescript/collatz-conjecture/collatz-conjecture.ts
@@ -1,3 +1,14 @@
-export function steps(count: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
-}
+export const steps = (n: number): number => {
+
+    if (n < 1) throw new Error("Only positive numbers are allowed");
+
+    let cnt = 0;
+
+    while (n > 1) {
+        n = n % 2 === 0 ? n / 2 : 3 * n + 1;
+        ++cnt;
+    }
+
+    return cnt;
+
+};
