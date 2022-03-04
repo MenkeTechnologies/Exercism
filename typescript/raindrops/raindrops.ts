16c6153e509a0188bf22da5e31ea6fbebe9b4d4a
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-04 15:18:27 -0500

    rd

--- typescript/raindrops/raindrops.ts
+++ typescript/raindrops/raindrops.ts
@@ -1,3 +1,4 @@
-export function convert() {
-  throw new Error('Remove this statement and implement this function')
-}
+const MAP = {3: "Pling", 5: "Plang", 7: "Plong"}
+
+export const convert = (n: number) =>
+    Object.entries(MAP).filter(([k]) => n % parseInt(k) === 0).map(([_, v]) => v).join("") || n.toString();
