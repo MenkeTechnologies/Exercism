9dd28cdf3cbea4969c9fd8810abdd3f71ccd5035
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-04 11:26:57 -0500

    bob

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,3 +1,11 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
-}
+export const hey = (message: string) => {
+  message = message.replace(/\s+/g, '')
+  if (message.length === 0){
+    return 'Fine. Be that way!'
+  }
+  if (message.match(/[A-Z]/) && !message.match(/[a-z]/)) {
+     return message.match(/\?$/) ? "Calm down, I know what I'm doing!" : 'Whoa, chill out!'
+  }
+  return message.match(/\?$/) ? 'Sure.' : 'Whatever.'
+};
+
