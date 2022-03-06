520bbce968d66997d4d5ffcff9d07dfa15318f82
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-06 16:37:30 -0500

    ana

--- typescript/anagram/anagram.ts
+++ typescript/anagram/anagram.ts
@@ -1,9 +1,11 @@
 export class Anagram {
-  constructor(input: unknown) {
-    throw new Error('Remove this statement and implement this function')
-  }
+    private input: string
+    private sorted: string
 
-  public matches(...potentials: unknown[]): unknown {
-    throw new Error('Remove this statement and implement this function')
-  }
+    constructor(input: string) {
+        this.input = input;
+        this.sorted = input.toLowerCase().split('').sort().join('');
+    }
+
+    public matches = (...cand: string[]): string[] => cand.filter(w => w.toLowerCase() !== this.input.toLowerCase() && w.toLowerCase().split('').sort().join('') === this.sorted);
 }
