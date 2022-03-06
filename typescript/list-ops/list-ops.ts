1a31d2d116e604c2e9450227971a8162b85cfe84
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-05 19:51:43 -0500

    lo

--- typescript/list-ops/list-ops.ts
+++ typescript/list-ops/list-ops.ts
@@ -1,10 +1,61 @@
-export class List {
-  public static create(...values: unknown[]): unknown {
-    // Do *not* construct any array literal ([]) in your solution.
-    // Do *not* construct any arrays through new Array in your solution.
-    // DO *not* use any of the Array.prototype methods in your solution.
+export class List<T> {
 
-    // You may use the destructuring and spreading (...) syntax from Iterable.
-    throw new Error('Remove this statement and implement this function')
-  }
+    constructor(public items: T[]) {
+    }
+
+    public static create<T>(...values: T[]): List<T> {
+        return new List(values);
+    }
+
+    public append(list: List<T>): List<T> {
+        this.items = [...this.items, ...list.items];
+        return this;
+    }
+
+    public concat(list: List<List<T>>): List<T> {
+        // @ts-ignore
+        return list.foldl((acc, n) => acc.append(n), new List(this.items));
+    }
+
+    filter<U>(f: (_: U) => boolean): List<U> {
+        // @ts-ignore
+        return new List(this.items.reduce((acc, n) => [...acc, ...(f(n) ? [n] : [])], []));
+
+    }
+
+    length(): number {
+        return this.items.length;
+    }
+
+    map<U>(f: (_: T) => U): List<U> {
+        // @ts-ignore
+        return List.create(...(this.items.reduce((acc, n) => [...acc, f(n)], [])));
+    }
+
+    foldl<U, V>(f: (_acc: V, _n: U) => V, init: V): V {
+        this.items.forEach(n => {
+            // @ts-ignore
+            init = f(init, n);
+        })
+        return init;
+    }
+
+    foldr<U, V>(f: (_acc: V, _n: U) => V, init: V): V {
+        this.reverse();
+
+        this.items.forEach(n => {
+            // @ts-ignore
+            init = f(init, n);
+        })
+        return init;
+    }
+
+    reverse(): List<T> {
+        this.items = this.items.reverse()
+        return this;
+    }
+
+    forEach(f: (_: T) => void): void {
+        this.items.forEach(f);
+    }
 }
