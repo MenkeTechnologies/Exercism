53f3b0ec601991550fe7785d23237117ad6982e0
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-06 16:26:29 -0500

    arm

--- typescript/armstrong-numbers/armstrong-numbers.ts
+++ typescript/armstrong-numbers/armstrong-numbers.ts
@@ -1,3 +1,1 @@
-export function isArmstrongNumber(number: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
-}
+export const isArmstrongNumber = (number: number) => number === number.toString().split('').map(d => parseInt(d) ** number.toString().split('').length).reduce((acc, n) => acc + n, 0);
