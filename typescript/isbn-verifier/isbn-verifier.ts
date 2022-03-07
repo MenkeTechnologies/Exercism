bf9943956d30774a0db5f0ecb554a2a3fe00ab70
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-07 12:25:09 -0500

    isbn

--- typescript/isbn-verifier/isbn-verifier.ts
+++ typescript/isbn-verifier/isbn-verifier.ts
@@ -1,3 +1,5 @@
-export function isValid(isbn: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
-}
+export const isValid = (str: string) =>
+    (str.match(/^\d-?\d{3}-?\d{5}-?[\dX]$/) || false) &&
+    str.split('').filter(c => c.match(/[X0-9]/)).reverse()
+        .map((n, i) => n === 'X' ? 10 : parseInt(n) * (i + 1))
+        .reduce((acc, n) => acc + n) % 11 === 0;
