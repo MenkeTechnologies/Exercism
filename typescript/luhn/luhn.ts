58adf7cde541d134a9bee449d262ce99a7ec0510
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-07 12:23:40 -0500

    luhn

--- typescript/luhn/luhn.ts
+++ typescript/luhn/luhn.ts
@@ -1,3 +1,8 @@
-export function valid(digitString: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export const valid = (str: string) => {
+  const nows = str.replace(/\s/g, '')
+  if (nows.length < 2 || nows.match(/\D/)) return false;
+  return nows.split('').map(c => parseInt(c)).reverse().map((c, i) => {
+    if (i % 2 === 1) c *= 2;
+    return c > 9 ? c - 9 : c
+  }).reduce((acc, n) => acc + n, 0) % 10 === 0;
 }
