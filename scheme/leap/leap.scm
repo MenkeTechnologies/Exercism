b9dc79f54db7470e17cf05649b219653b50db8b9
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-08 21:24:45 -0500

    leap

--- scheme/leap/leap.scm
+++ scheme/leap/leap.scm
@@ -1,5 +1,10 @@
 (import (rnrs))
 
 (define (leap-year? year)
-  'implement-me!)
+  (and (zero? (modulo year 4))
+       (or (not (zero? (modulo year 100)))
+           (zero? (modulo year 400))
+           )
+       )
 
+  )
