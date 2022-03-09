fd4578f06fa5652e17faab17a19a23c1e087749f
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-09 11:46:50 -0500

    allergies

--- lua/allergies/allergies.lua
+++ lua/allergies/allergies.lua
@@ -1,9 +1,27 @@
-local function list(score)
+local food = {
+    eggs=1,
+    peanuts=2,
+    shellfish=4,
+    strawberries=8,
+    tomatoes=16,
+    chocolate=32,
+    pollen=64,
+    cats=128,
+}
 
+local function allergic_to(mask, which)
+    return mask & food[which] > 0
 end
 
-local function allergic_to(score, which)
-
+local function list(score)
+    local map = {}
+    for k, v in pairs(food) do
+        if allergic_to(score, k) then
+            table.insert(map, k)
+        end
+    end
+    table.sort(map, function (a, b) return food[a] < food[b] end)
+    return map
 end
 
 return {
