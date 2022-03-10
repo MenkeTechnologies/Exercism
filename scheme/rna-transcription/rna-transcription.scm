df8b432128df233b5c8a16b7e80f4a8d67a19d7f
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-10 11:06:56 -0500

    rna

--- scheme/rna-transcription/rna-transcription.scm
+++ scheme/rna-transcription/rna-transcription.scm
@@ -1,5 +1,16 @@
 (import (rnrs))
 
+(define (conv c)
+(cond 
+ ((eq? c #\G) #\C)
+ ((eq? c #\C) #\G)
+ ((eq? c #\T) #\A)
+ ((eq? c #\A) #\U)
+ )
+ )
+
 (define (dna->rna dna)
-  'implement-me!)
+ (string-map conv dna)
+
+)
 
