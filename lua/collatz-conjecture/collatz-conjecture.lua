07014e0823b369ed8807b8bf9b52a686059c6132
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-12 15:25:50 -0500

    cc

--- lua/collatz-conjecture/collatz-conjecture.lua
+++ lua/collatz-conjecture/collatz-conjecture.lua
@@ -1,3 +1,21 @@
 return function(n)
 
+    if n < 1 then
+        error("Only positive numbers are allowed")
+    end
+
+    local cnt = 0
+
+    while n > 1 do
+        if n % 2 == 0 then
+            n = n / 2
+        else
+            n = 3 * n + 1
+        end
+        cnt = cnt + 1
+
+    end
+
+    return cnt
+
 end
