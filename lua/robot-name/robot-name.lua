f46f39672e5e9edd4a12fc915fca7a143f033ddc
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-12 15:20:53 -0500

    rn

--- lua/robot-name/robot-name.lua
+++ lua/robot-name/robot-name.lua
@@ -1,3 +1,19 @@
 local Robot = {}
 
+function Robot:new()
+    local r = {}
+    setmetatable(r, self)
+    self.__index = self
+    r:reset()
+    return r
+end
+
+function Robot:reset()
+    local A = 65
+    local Z = 90
+    self.name = utf8.char(math.random(A, Z)) .. utf8.char(math.random(A, Z)) ..
+            math.random(0, 9) .. math.random(0, 9) .. math.random(0, 9)
+    return self
+end
+
 return Robot
