b36fb8159d245b9589acf4f4526cf13be4f57a36
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-13 13:25:08 -0400

    gri

--- csharp/grains/Grains.cs
+++ csharp/grains/Grains.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Linq;
 
 public static class Grains
 {
     public static ulong Square(int n)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (n is <= 0 or > 64)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return 1UL << n - 1;
     }
 
-    public static ulong Total()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static ulong Total() => Enumerable.Range(1, 64).Select(Square).Aggregate(0UL, (acc, n) => acc + n);
 }
