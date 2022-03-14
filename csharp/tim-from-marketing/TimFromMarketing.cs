50fcdd71b8198702e7c8929be9dc0db975d20000
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-14 03:59:51 -0400

    tim

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -2,8 +2,5 @@ using System;
 
 static class Badge
 {
-    public static string Print(int? id, string name, string? department)
-    {
-        throw new NotImplementedException("Please implement the (static) Badge.Print() method");
-    }
+    public static string Print(int? id, string name, string? department) => (id.HasValue ? $"[{id.Value}] - " : "") + $"{name} - " + (department ?? "owner").ToUpper();
 }
