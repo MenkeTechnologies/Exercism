3ba24a03c93a624ab17f96e96013472fbeff7278
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-14 01:53:52 -0400

    darts

--- csharp/darts/Darts.cs
+++ csharp/darts/Darts.cs
@@ -2,8 +2,14 @@
 
 public static class Darts
 {
-    public static int Score(double x, double y)
-    {
-       throw new NotImplementedException("You need to implement this function");
-    }
+    public static int Score(double x, double y) =>
+        dist(x, y) switch
+        {
+            <= 1.0 => 10,
+            <= 5.0 => 5,
+            <= 10.0 => 1,
+            _ => 0
+        };
+
+    private static double dist(double x, double y) => Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
 }
