f23851fee42d6e057c07d20658e14656d6d6449f
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-16 10:49:32 -0400

    pna

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -1,14 +1,16 @@
 using System;
+using System.Text.RegularExpressions;
 
 public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        var match = Regex.Match(phoneNumber, @"^\s*(\+?1?\s*)?\(?([2-9]\d{2})\)?[-.\s]*([2-9]\d{2})[-.\s]*(\d{4})\s*$");
+
+        return (match.Groups[2].ToString() == "212", match
+            .Groups[3].ToString() == "555", match.Groups[4].ToString());
+
     }
 
-    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
-    {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
-    }
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) => phoneNumberInfo.IsFake;
 }
