5b3c8d7a33fbf39d43d31a06117c9cb5bedf9fc7
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-16 10:42:03 -0400

    pn

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Text.RegularExpressions;
 
 public class PhoneNumber
 {
     public static string Clean(string phoneNumber)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var match = Regex.Match(phoneNumber, @"^\s*(\+?1?\s*)?\(?([2-9]\d{2})\)?[-.\s]*([2-9]\d{2})[-.\s]*(\d{4})\s*$");
+        if (!match.Success)
+        {
+            throw new ArgumentException();
+        }
+
+        return match.Groups[2].ToString() + match.Groups[3] + match.Groups[4];
     }
 }
