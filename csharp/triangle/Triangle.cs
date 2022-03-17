a629baacbbde05f5caa9f89103a8e7d1e45cd849
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-17 11:16:31 -0400

    tri

--- csharp/triangle/Triangle.cs
+++ csharp/triangle/Triangle.cs
@@ -1,19 +1,37 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Triangle
 {
+    private static bool isValid(double[] ary)
+    {
+        Array.Sort(ary);
+        return ary.All(c => c > 0) && ary[0] + ary[1] >= ary[2];
+    }
+
     public static bool IsScalene(double side1, double side2, double side3)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var ary = new[] { side1, side2, side3 };
+        var set = new HashSet<double>(ary);
+
+        return isValid(ary) && set.Count == 3;
     }
 
-    public static bool IsIsosceles(double side1, double side2, double side3) 
+
+    public static bool IsIsosceles(double side1, double side2, double side3)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var ary = new[] { side1, side2, side3 };
+        var set = new HashSet<double>(ary);
+
+        return isValid(ary) && set.Count <= 2;
     }
 
-    public static bool IsEquilateral(double side1, double side2, double side3) 
+    public static bool IsEquilateral(double side1, double side2, double side3)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var ary = new[] { side1, side2, side3 };
+        var set = new HashSet<double>(ary);
+
+        return isValid(ary) && set.Count == 1;
     }
 }
