8ab0168f96399206a872bdfb3ddc7cc02a29ac99
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-17 11:03:41 -0400

    rcd

--- csharp/resistor-color-duo/ResistorColorDuo.cs
+++ csharp/resistor-color-duo/ResistorColorDuo.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class ResistorColorDuo
 {
-    public static int Value(string[] colors)
+    public static Dictionary<string, string> BANDS = new()
     {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        {"black", "0"},
+        {"brown", "1"},
+        {"red", "2"},
+        {"orange", "3"},
+        {"yellow", "4"},
+        {"green", "5"},
+        {"blue", "6"},
+        {"violet", "7"},
+        {"grey", "8"},
+        {"white", "9"}
+    };
+    public static int Value(string[] colors) => int.Parse(string.Join("", colors.Take(2).Select(c => BANDS[c])));
 }
