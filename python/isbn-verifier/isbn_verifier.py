669c616b7e7e905246d31b47272fd55fd9824176
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-18 10:57:25 -0400

    isbn

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,15 @@
+import re
+
+
 def is_valid(isbn):
-    pass
+    if not re.compile(r"^\d-?\d{3}-?\d{5}-?[\dX]$").match(isbn):
+        return False
+
+    sum = 0
+    for i, c in enumerate(isbn.replace("-", "")[::-1]):
+        if c == 'X':
+            sum += 10
+        else:
+            sum += int(c) * (i + 1)
+
+    return sum % 11 == 0
