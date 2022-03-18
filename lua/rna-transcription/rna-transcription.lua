fd76c816aa95590e08eef9f1e7e608d896c6045a
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-17 21:41:02 -0400

    rna

--- lua/rna-transcription/rna-transcription.lua
+++ lua/rna-transcription/rna-transcription.lua
@@ -1,3 +1,13 @@
+local mapping = { C = 'G', G = 'C', A = 'U', T = 'A' }
+
 return function(dna)
+
+    local rna = {}
+
+    for l in dna:gmatch(".") do
+        table.insert(rna, mapping[l])
+    end
+
+    return table.concat(rna, '')
 
 end
