b668cffcaf3fc1a5da9baa1e91c6d8b24dedc1b6
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-18 11:16:09 -0400

    pc

--- java/nth-prime/src/main/java/PrimeCalculator.java
+++ java/nth-prime/src/main/java/PrimeCalculator.java
@@ -1,7 +1,14 @@
+import java.util.stream.IntStream;
+
 class PrimeCalculator {
-
     int nth(int nth) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
 
+        return IntStream.concat(IntStream.of(2), IntStream.iterate(3, n -> n + 2))
+                .filter(n ->
+                        IntStream.range(2, n / 2).allMatch(e -> n % e > 0)
+                )
+                .limit(nth)
+                .max()
+                .orElseThrow(() -> new IllegalArgumentException("no prime in range " + nth));
+    }
 }
