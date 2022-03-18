0f507e3ed607b3d61441e534fecdb65507f46073
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-18 12:20:15 -0400

    strain

--- csharp/strain/Strain.cs
+++ csharp/strain/Strain.cs
@@ -5,11 +5,23 @@ public static class Strain
 {
     public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        foreach (var n in collection)
+        {
+            if (predicate(n))
+            {
+                yield return n;
+            }
+        }
     }
 
     public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        foreach (var n in collection)
+        {
+            if (!predicate(n))
+            {
+                yield return n;
+            }
+        }
     }
 }
