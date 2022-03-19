e2f01561d07519d43e0dc4b6b90bedeb2e2678db
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-19 11:40:48 -0400

    iso

--- tcl/isogram/isogram.tcl
+++ tcl/isogram/isogram.tcl
@@ -1,3 +1,13 @@
 proc isIsogram {input} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set d {}
+    set letters [regexp -inline -all {[a-z]} [string tolower $input]]
+    foreach l $letters {
+        dict incr d $l
+        if {[dict get $d $l] > 1} {
+            return false
+        }
+    }
+
+    return true
+
 }
