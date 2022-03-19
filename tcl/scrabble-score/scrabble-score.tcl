7590b3e8fd38d3fecdd9c7069460384c33adbd2b
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-19 11:46:16 -0400

    ss

--- tcl/scrabble-score/scrabble-score.tcl
+++ tcl/scrabble-score/scrabble-score.tcl
@@ -1,3 +1,19 @@
 proc scrabbleScore {word} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+      set points [dict create \
+        A 1 E 1 I 1 O 1 U 1 L 1 N 1 R 1 S 1 T 1 \
+        D 2 G 2 \
+        B 3 C 3 M 3 P 3 \
+        F 4 H 4 V 4 W 4 Y 4 \
+        K 5 \
+        J 8 X 8 \
+        Q 10 Z 10
+    ]
+
+    set r 0
+
+    foreach c [split [string toupper $word] {}] {
+        incr r [dict get $points $c]
+    }
+
+    return $r
 }
