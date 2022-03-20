3a3f590ccbb0cdfcbb7f452062cf3f2e654d3262
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-20 14:11:33 -0400

    interest

--- go/interest-is-interesting/interest_is_interesting.go
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -2,20 +2,34 @@ package interest
 
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	panic("Please implement the InterestRate function")
+
+	if balance >= 5000 {
+		return 2.475
+	} else if balance >= 1000 {
+		return 1.621
+	} else if balance >= 0 {
+		return 0.5
+	}
+	return 3.213
 }
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+	return balance * float64(InterestRate(balance)) / 100
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	return balance + Interest(balance)
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	var years int
+	for balance < targetBalance {
+		balance += Interest(balance)
+		years++
+	}
+
+	return years
 }
