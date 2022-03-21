5fae41d4a9e17878d2f415b6ade85312edf25e84
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-21 03:38:17 -0400

    gs

--- tcl/gigasecond/gigasecond.tcl
+++ tcl/gigasecond/gigasecond.tcl
@@ -1,3 +1,6 @@
 proc addGigasecond {datetime} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set fmt {%Y-%m-%dT%H:%M:%S}
+    set sec [clock scan $datetime -timezone :UTC]
+    set gs [expr int(pow(10,9))]
+    return [clock format [expr $sec + $gs] -format $fmt -timezone :UTC]
 }
