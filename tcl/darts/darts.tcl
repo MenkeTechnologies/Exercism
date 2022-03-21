5840339d4ecf1b31fdaea9623cad8d5b414939c0
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-21 03:25:27 -0400

    darts

--- tcl/darts/darts.tcl
+++ tcl/darts/darts.tcl
@@ -1,3 +1,9 @@
 proc score {x y} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set d [expr pow(pow($x,2) + pow($y,2), 0.5)]
+
+    if {$d <= 1} { return 10 }
+    if {$d <= 5} { return 5 }
+    if {$d <= 10} { return 1 }
+
+    return 0
 }
