1ea40d07b42dca1de956b5ce48899fa159228ead
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-21 03:52:22 -0400

    grains

--- tcl/grains/grains.tcl
+++ tcl/grains/grains.tcl
@@ -1,7 +1,17 @@
-proc square {square} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+namespace eval grains {
 
-proc total {} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    namespace export square total
+    namespace ensemble create
+
+    proc square {square} {
+        if {$square < 1 || $square > 64} {
+            throw 1 "square must be between 1 and 64"
+        }
+        expr 2 ** ($square - 1)
+    }
+
+    proc total {} {
+        expr 2 ** 64 - 1
+    }
+
 }
