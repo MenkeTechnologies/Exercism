e023d798a40f5a22299a30441f657854d0d8d534
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-22 09:52:59 -0400

    dos

--- tcl/difference-of-squares/difference-of-squares.tcl
+++ tcl/difference-of-squares/difference-of-squares.tcl
@@ -1,11 +1,22 @@
 proc squareOfSum {n} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+
+    set sum 0
+    for {set i 1} {$i <= $n} {incr i} {
+        incr sum $i
+    }
+
+    return [expr int(pow($sum,2))]
 }
 
 proc sumOfSquares {n} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set sum 0
+    for {set i 1} {$i <= $n} {incr i} {
+        incr sum [expr int(pow($i, 2))]
+    }
+
+    return $sum
 }
 
 proc differenceOfSquares {n} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    return [expr abs([sumOfSquares $n] - [squareOfSum $n])]
 }
