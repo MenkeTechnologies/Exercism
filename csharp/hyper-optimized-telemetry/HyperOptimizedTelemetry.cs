94af033b30dc42e9a58a32892973150b8c37e7e3
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-22 09:30:59 -0400

    luhn

--- csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -4,11 +4,9 @@ public static class TelemetryBuffer
 {
     public static byte[] ToBuffer(long reading)
     {
-        throw new NotImplementedException("Please implement the static TelemetryBuffer.ToBuffer() method");
     }
 
     public static long FromBuffer(byte[] buffer)
     {
-        throw new NotImplementedException("Please implement the static TelemetryBuffer.FromBuffer() method");
     }
 }
--- tcl/luhn/luhn.tcl
+++ tcl/luhn/luhn.tcl
@@ -1,4 +1,23 @@
 proc luhn {digits} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    regsub -all {\s+} $digits {} digits
+
+    if {[string length $digits] < 2 || [regexp {[^0-9]} $digits] } {
+        return false
+    }
+
+    set tot 0
+    set cnt 0
+
+    foreach d [split [string reverse $digits] ""] {
+
+        if {$cnt % 2 == 1} {
+            set d [expr {$d * 2}]
+        }
+        set d [expr $d > 9 ? [expr $d - 9] : $d]
+        incr tot $d
+        incr cnt
+    }
+
+   expr {$tot % 10 == 0}
 }
 
