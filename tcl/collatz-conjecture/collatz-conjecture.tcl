84b3b841cfba0b59ba11cecb03a3eaaa03a52415
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-23 10:26:00 -0400

    coll

--- tcl/collatz-conjecture/collatz-conjecture.tcl
+++ tcl/collatz-conjecture/collatz-conjecture.tcl
@@ -1,3 +1,19 @@
-proc steps {} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc steps {n} {
+    if {$n < 1} {
+        error "Only positive integers are allowed"
+    }
+
+    set cnt 0
+
+    while {$n > 1} {
+        if {$n % 2 == 0} {
+            set n [expr $n / 2]
+        } else {
+            set n [expr 3 * $n + 1]
+        }
+
+        incr cnt
+    }
+
+    return $cnt
 }
