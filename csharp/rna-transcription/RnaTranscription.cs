7a0bb20a92bd2ca06f230bf5e78929cae1508c56
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-25 10:49:54 -0400

    rna

--- csharp/rna-transcription/RnaTranscription.cs
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Linq;
 
 public static class RnaTranscription
 {
-    public static string ToRna(string nucleotide)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static string ToRna(string nucleotide) =>
+        new(nucleotide.Select(c => c switch
+        {
+            'G' => 'C',
+            'C' => 'G',
+            'T' => 'A',
+            'A' => 'U',
+            _ => throw new ArgumentException()
+        }).ToArray());
 }
