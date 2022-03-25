bcb85dac5d15414fe1f4843364952cfd96bfb34e
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-25 11:23:46 -0400

    list ops

--- csharp/list-ops/ListOps.cs
+++ csharp/list-ops/ListOps.cs
@@ -4,43 +4,61 @@ using System.Linq;
 
 public static class ListOps
 {
-    public static int Length<T>(List<T> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int Length<T>(List<T> input) => input.Count;
 
     public static List<T> Reverse<T>(List<T> input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<T> r = new List<T>();
+        for (int i = Length(input) - 1; i >= 0; i--) r.Add(input[i]);
+
+        return r;
     }
 
-    public static List<TOut> Map<TIn, TOut>(List<TIn> input, Func<TIn, TOut> map)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static List<TOut> Map<TIn, TOut>(List<TIn> input, Func<TIn, TOut> map) =>
+        input.Aggregate(new List<TOut>(), (acc, n) =>
+        {
+            acc.Add(map(n));
+            return acc;
+        });
 
-    public static List<T> Filter<T>(List<T> input, Func<T, bool> predicate)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static List<T> Filter<T>(List<T> input, Func<T, bool> predicate) =>
+        input.Aggregate(new List<T>(), (acc, n) =>
+        {
+            if (predicate(n))
+            {
+                acc.Add((n));
+            }
+
+            return acc;
+        });
 
     public static TOut Foldl<TIn, TOut>(List<TIn> input, TOut start, Func<TOut, TIn, TOut> func)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var acc = start;
+        foreach (var n in input) acc = func(acc, n);
+
+        return acc;
     }
 
     public static TOut Foldr<TIn, TOut>(List<TIn> input, TOut start, Func<TIn, TOut, TOut> func)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var acc = start;
+        for (var i = input.Count - 1; i >= 0; i--) acc = func(input[i], acc);
+        return acc;
     }
 
-    public static List<T> Concat<T>(List<List<T>> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static List<T> Concat<T>(List<List<T>> input) => input.Aggregate(new List<T>(), (acc, n) => Append(acc, n));
 
     public static List<T> Append<T>(List<T> left, List<T> right)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<T> r = new List<T>
+        {
+            Capacity = left.Count + right.Count
+        };
+
+        foreach (var n in left) r.Add(n);
+        foreach (var n in right) r.Add(n);
+
+        return r;
     }
 }
