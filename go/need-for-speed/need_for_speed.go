35a3a95843ea00fd5c4c583e6b0771400acc7912
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-25 11:36:29 -0400

    nfs

--- go/need-for-speed/need_for_speed.go
+++ go/need-for-speed/need_for_speed.go
@@ -1,26 +1,52 @@
 package speed
 
-// TODO: define the 'Car' type struct
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+type Track struct {
+	distance int
+}
 
 // NewCar creates a new remote controlled car with full battery and given specifications.
 func NewCar(speed, batteryDrain int) Car {
-	panic("Please implement the NewCar function")
+	return Car{
+		battery:      100,
+		batteryDrain: batteryDrain,
+		speed:        speed,
+		distance:     0,
+	}
 }
 
-// TODO: define the 'Track' type struct
-
-// NewTrack created a new track
 func NewTrack(distance int) Track {
-	panic("Please implement the NewTrack function")
+
+	return Track{
+		distance: distance,
+	}
 }
 
 // Drive drives the car one time. If there is not enough battery to drive on more time,
 // the car will not move.
 func Drive(car Car) Car {
-	panic("Please implement the Drive function")
+	if car.batteryDrain > car.battery {
+		return car
+	}
+
+	return Car{
+		speed:        car.speed,
+		batteryDrain: car.batteryDrain,
+		battery:      car.battery - car.batteryDrain,
+		distance:     car.speed + car.distance,
+	}
+
 }
 
 // CanFinish checks if a car is able to finish a certain track.
 func CanFinish(car Car, track Track) bool {
-	panic("Please implement the CanFinish function")
+
+	return car.battery >= (track.distance-car.distance)/car.speed*car.batteryDrain
+
 }
