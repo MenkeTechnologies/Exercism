399c27bded2f154c8cc81c345f450475f8e35897
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-26 13:48:39 -0400

    rcd

--- tcl/resistor-color-duo/resistor-color-duo.tcl
+++ tcl/resistor-color-duo/resistor-color-duo.tcl
@@ -1,5 +1,32 @@
 namespace eval ::resistorColor {
-    proc value {} {
-        throw {NOT_IMPLEMENTED} "Implement this procedure."
+
+    set codes {
+        black   0
+        brown   1
+        red     2
+        orange  3
+        yellow  4
+        green   5
+        blue    6
+        violet  7
+        grey    8
+        white   9
+    }
+
+    proc colorCode {colour} {
+        try {
+            return [dict get $resistorColor::codes $colour]
+        } on error {err opts} {
+            error "Invalid color: $colour"
+        }
+    }
+
+    proc value {args} {
+        set res {}
+        foreach color [lrange $args 0 1] {
+            lappend res [colorCode $color]
+        }
+
+        return [join $res ""]
     }
 }
