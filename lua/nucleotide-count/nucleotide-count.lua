fe3d584bd5b484657bc3e89de00dacdb14c262dd
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-26 14:17:39 -0400

    ntc

--- lua/nucleotide-count/nucleotide-count.lua
+++ lua/nucleotide-count/nucleotide-count.lua
@@ -1,3 +1,20 @@
 local DNA = {}
 
+function DNA:new(s)
+    self.nucleotideCounts = { A = 0, T = 0, G = 0, C = 0 }
+    s = string.upper(s)
+
+    for i = 1, #s do
+        local nt = s:sub(i,i)
+        self.nucleotideCounts[nt] = self.nucleotideCounts[nt] + 1
+    end
+
+    return self
+end
+
+function DNA:count(nt)
+    assert (self.nucleotideCounts[nt], 'Invalid Nucleotide')
+    return self.nucleotideCounts[nt]
+end
+
 return DNA
