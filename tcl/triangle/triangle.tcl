e43e89eae59c4ffc91a0ba1a3f1ac0a01ab17c4b
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-28 09:43:09 -0400

    tri

--- tcl/triangle/triangle.tcl
+++ tcl/triangle/triangle.tcl
@@ -1,5 +1,18 @@
 namespace eval triangle {
-    proc is {} {
-        throw {NOT_IMPLEMENTED} "Implement this procedure."
+    proc is {type sides} {
+        lassign [lsort -real $sides] a b c
+        if {!($a > 0 && $b > 0 && $c > 0 && $a + $b > $c)} {
+            return false
+        }
+
+        set nUniq [llength [lsort -real -unique $sides]]
+
+        switch -exact -- $type {
+            equilateral {return [expr $nUniq == 1]}
+            isosceles   {return [expr $nUniq <= 2]}
+            scalene     {return [expr $nUniq == 3]}
+            default {error "invalid type"}
+        }
+
     }
 }
