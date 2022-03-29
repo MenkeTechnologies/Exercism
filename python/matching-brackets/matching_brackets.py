5cfdf37e19f4358faa6b01bc25424ca03cdae4c6
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-29 11:12:12 -0400

    mb

--- python/matching-brackets/matching_brackets.py
+++ python/matching-brackets/matching_brackets.py
@@ -1,2 +1,12 @@
 def is_paired(input_string):
-    pass
+    pairs = {'{': '}', '[': ']', '(': ')'}
+    stk = []
+
+    for ch in input_string:
+        if ch in pairs:
+            stk.append(ch)
+        elif ch in pairs.values():
+            if len(stk) == 0 or ch != pairs[stk.pop()]:
+                return False
+
+    return len(stk) == 0
