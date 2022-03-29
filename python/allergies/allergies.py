5a8b44bb5fa362e4f5c8a0907de09c599101b456
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-29 11:04:34 -0400

    allergies

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,22 @@
 class Allergies:
 
-    def __init__(self, score):
-        pass
+    ALLERGENS = {
+        'eggs': 1,
+        'peanuts': 2,
+        'shellfish': 4,
+        'strawberries': 8,
+        'tomatoes': 16,
+        'chocolate': 32,
+        'pollen': 64,
+        'cats': 128,
+    }
+
+    def __init__(self, mask):
+        self.mask = mask
 
     def allergic_to(self, item):
-        pass
+        return self.mask & self.ALLERGENS[item] > 0
 
     @property
     def lst(self):
-        pass
+        return [allergen for allergen in self.ALLERGENS if self.allergic_to(allergen)]
