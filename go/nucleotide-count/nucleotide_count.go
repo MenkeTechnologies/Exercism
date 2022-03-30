dca8bb669b133be9cc23fca327ce680ac30b028c
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-30 10:14:59 -0400

    nt

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -1,11 +1,16 @@
 package dna
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Histogram is a mapping from nucleotide to its count in given DNA.
 // Choose a suitable data type.
-type Histogram
+type Histogram map[rune]uint
 
 // DNA is a list of nucleotides. Choose a suitable data type.
-type DNA
+type DNA string
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
@@ -14,6 +19,16 @@ type DNA
 // The receiver appears in its own argument list between the func keyword and the method name.
 // Here, the Counts method has a receiver of type DNA named d.
 func (d DNA) Counts() (Histogram, error) {
-	var h Histogram
+	d = DNA(strings.ToUpper(string(d)))
+	h := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
+	for _, nt := range d {
+		_, ok := h[nt]
+		if !ok {
+			return nil, fmt.Errorf("Invalid nucleotide %v", nt)
+		}
+		h[nt]++
+	}
+
 	return h, nil
+
 }
