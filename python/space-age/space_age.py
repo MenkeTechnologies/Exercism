646e3cb67bf5fc2da6161c7565f5e27118c911db
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-29 22:34:05 -0400

    sa

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,3 +1,16 @@
 class SpaceAge:
+    SPY = 31557600.0
+    years = {
+        'Earth': 1,
+        'Mercury': 0.2408467,
+        'Venus': 0.61519726,
+        'Mars': 1.8808158,
+        'Jupiter': 11.862615,
+        'Saturn': 29.447498,
+        'Uranus': 84.016846,
+        'Neptune': 164.79132,
+    }
+
     def __init__(self, seconds):
-        pass
+        for planet in self.years.keys():
+            setattr(self, f'on_{planet.lower()}', lambda arg=planet: round(seconds / self.SPY / self.years[arg], 2))
