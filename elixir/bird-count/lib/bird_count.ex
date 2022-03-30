dc076346e881e63e487fa4ad3cd3f583d45bde6c
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-30 10:10:31 -0400

    bc

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,25 @@
 defmodule BirdCount do
   def today(list) do
-    # Please implement the today/1 function
+    List.first(list)
   end
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
+  def increment_day_count([]) do
+    [1]
+  end
+
+  def increment_day_count([h | t]) do
+    [h + 1 | t]
   end
 
   def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+    Enum.member?(list, 0)
   end
 
   def total(list) do
-    # Please implement the total/1 function
+    Enum.sum(list)
   end
 
   def busy_days(list) do
-    # Please implement the busy_days/1 function
+    Enum.filter(list, fn x -> x >= 5 end) |> length()
   end
 end
