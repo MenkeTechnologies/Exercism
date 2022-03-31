d787f09bda44e0e17df8059ccfb51a552091dd20
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-31 13:34:59 -0400

    fa

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,8 @@
 def flatten(iterable):
-    pass
+    r = []
+    for i in iterable:
+        if not isinstance(i, str) and hasattr(i, '__iter__'):
+            r += flatten(i)
+        elif i is not None:
+            r.append(i)
+    return r
