789deda213d3328a3204a0e88aef12ce9abeb40c
MenkeTechnologies <linux.dev25@gmail.com>
2022-03-31 13:38:39 -0400

    etl

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,1 +1,16 @@
 package etl
+
+import "strings"
+
+func Transform(in map[int][]string) (out map[string]int) {
+	out = make(map[string]int)
+
+	for score, letters := range in {
+		for _, letter := range letters {
+			out[strings.ToLower(letter)] = score
+		}
+	}
+
+	return out
+
+}
