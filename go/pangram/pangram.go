fb5568d8dc17997ddbc4f152bd941d4a67bc8c56
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-01 10:23:54 -0400

    pan

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,1 +1,14 @@
 package pangram
+
+import "strings"
+
+func IsPangram(s string) bool {
+	s = strings.ToLower(s)
+
+	for c := 'a'; c <= 'z'; c++ {
+		if strings.IndexRune(s, c) == -1 {
+			return false
+		}
+	}
+	return true
+}
