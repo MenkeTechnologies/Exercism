43a547c88a9db87873315105496a8076fd9a6776
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-01 11:10:30 -0400

    ana

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,1 +1,25 @@
 package anagram
+
+import (
+	"sort"
+	"strings"
+)
+
+func lcSort(s string) string {
+	ch := strings.Split(strings.ToLower(s), "")
+	sort.Strings(ch)
+	return strings.Join(ch, "")
+}
+
+func Detect(s string, candidates []string) (anagrams []string) {
+
+	lc := lcSort(s)
+	for _, cand := range candidates {
+		lower := strings.ToLower(cand)
+		if lcSort(cand) == lc && strings.ToLower(s) != lower {
+			anagrams = append(anagrams, cand)
+		}
+	}
+	return
+
+}
