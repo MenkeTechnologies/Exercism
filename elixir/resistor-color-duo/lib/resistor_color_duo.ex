4cc29db6f6755266c98c178b8bffd4c8e2e4d8a7
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-02 13:46:39 -0400

    rcd

--- elixir/resistor-color-duo/lib/resistor_color_duo.ex
+++ elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -1,8 +1,26 @@
 defmodule ResistorColorDuo do
+  @resistance_chart %{
+    :black => 0,
+    :brown => 1,
+    :red => 2,
+    :orange => 3,
+    :yellow => 4,
+    :green => 5,
+    :blue => 6,
+    :violet => 7,
+    :grey => 8,
+    :white => 9
+  }
+
+
   @doc """
   Calculate a resistance value from two colors
   """
   @spec value(colors :: [atom]) :: integer
   def value(colors) do
+    colors
+    |> Enum.take(2)
+    |> Enum.map(& @resistance_chart[&1])
+    |> Integer.undigits
   end
 end
