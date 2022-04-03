c48d8161832a778e3b486435fd7fe05ba8937596
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-03 12:58:49 -0400

    lo

--- lua/list-ops/list-ops.lua
+++ lua/list-ops/list-ops.lua
@@ -1,17 +1,35 @@
 local function reduce(xs, value, f)
-
+    local init = value
+    for _, n in ipairs(xs) do
+        init = f(n, init)
+    end
+    return init
 end
 
 local function map(xs, f)
+    local r = {}
+    for _, n in ipairs(xs) do
+        table.insert(r, f(n))
+    end
+
+    return r
 
 end
 
 local function filter(xs, pred)
 
+    local r = {}
+    for _, n in ipairs(xs) do
+        if pred(n) then
+            table.insert(r, n)
+        end
+    end
+
+    return r
 end
 
 return {
-  map = map,
-  reduce = reduce,
-  filter = filter
+    map = map,
+    reduce = reduce,
+    filter = filter
 }
