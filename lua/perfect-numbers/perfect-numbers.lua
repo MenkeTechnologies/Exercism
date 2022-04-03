4e46228bfe089b3cff65ad937c8b8f1234fcae97
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-03 13:01:37 -0400

    pn

--- lua/perfect-numbers/perfect-numbers.lua
+++ lua/perfect-numbers/perfect-numbers.lua
@@ -1,12 +1,28 @@
 local function aliquot_sum(n)
+    r = 0
+    for i = 1, n // 2 do
+        if n % i == 0 then
+            r = r + i
+        end
+    end
+
+    return r
 
 end
 
 local function classify(n)
+    local sum = aliquot_sum(n)
+    if sum > n then
+        return "abundant"
+    elseif sum < n then
+        return "deficient"
+    else
+        return "perfect"
+    end
 
 end
 
 return {
-  aliquot_sum = aliquot_sum,
-  classify = classify
+    aliquot_sum = aliquot_sum,
+    classify = classify
 }
