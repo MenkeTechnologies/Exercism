7c1fc4dc34906861e4ee91bb3f56aa51de029fd3
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-04 10:13:11 -0400

    fa

--- lua/flatten-array/flatten-array.lua
+++ lua/flatten-array/flatten-array.lua
@@ -1,4 +1,16 @@
-local function flatten(input)
+local function flatten(input, r)
+    r = r or {}
+
+    for _,v in ipairs(input) do
+        if type(v) == "table" then
+            flatten(v, r)
+        elseif v then
+            r[#r + 1] = v
+
+        end
+    end
+
+    return r
 
 end
 
