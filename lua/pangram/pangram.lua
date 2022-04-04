2bcd8b881901b4b8036cf106ac66f9d7ba3ca247
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-04 10:11:02 -0400

    pan

--- lua/pangram/pangram.lua
+++ lua/pangram/pangram.lua
@@ -1,3 +1,11 @@
 return function(s)
+    local lc = s:lower()
+    for c in string.gmatch("abcdefghijklmnopqrstuvwxyz", ".") do
+        if not lc:find(c) then
+            return false
+        end
+    end
+
+    return true
 
 end
