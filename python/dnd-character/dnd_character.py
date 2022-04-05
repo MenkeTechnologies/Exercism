44e8361d3f3d19d87b0cc9107db9822aeca065d6
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-05 10:15:26 -0400

    dnd

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,3 +1,21 @@
+from math import floor
+from random import randrange
+
+
 class Character:
     def __init__(self):
-        pass
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def ability(self):
+        return sum(sorted([randrange(1, 7) for _ in range(4)])[1:])
+
+
+
+def modifier(ability):
+    return floor((ability - 10) / 2)
