8285bc7a279503d0b222a80437443829750ec82f
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-05 09:56:00 -0400

    ana

--- lua/anagram/anagram.lua
+++ lua/anagram/anagram.lua
@@ -1,3 +1,35 @@
 local Anagram = {}
 
+function Anagram:new(word)
+
+    self.orig = word
+    self.sorted = sortStr(word)
+
+    return self
+end
+
+function Anagram:match(candidates)
+
+    local r = {}
+    for _, str in ipairs(candidates) do
+        if str ~= self.orig and sortStr(str) == self.sorted then
+            table.insert(r, str)
+        end
+    end
+
+    return r
+end
+
+function sortStr(s)
+    local lett = {}
+    local lc = s:lower()
+    for ch in string.gmatch(lc, ".") do
+        table.insert(lett, ch)
+    end
+
+    table.sort(lett)
+
+    return table.concat(lett)
+end
+
 return Anagram
