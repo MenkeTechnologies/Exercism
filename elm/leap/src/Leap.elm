ce768d00b1b8236b50d5f8bd11617ef6d1b06a8d
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-06 10:55:12 -0400

    leap

--- elm/leap/src/Leap.elm
+++ elm/leap/src/Leap.elm
@@ -2,5 +2,4 @@ module Leap exposing (isLeapYear)
 
 
 isLeapYear : Int -> Bool
-isLeapYear year =
-    Debug.todo "Please implement this function"
+isLeapYear y = remainderBy 4 y == 0 && remainderBy 100 y /= 0 || remainderBy 400 y == 0
