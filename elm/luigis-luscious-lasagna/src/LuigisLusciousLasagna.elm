9915a875107ce2a955e80961f9f98f8866757e3b
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-06 17:02:52 -0400

    lasagna

--- elm/luigis-luscious-lasagna/src/LuigisLusciousLasagna.elm
+++ elm/luigis-luscious-lasagna/src/LuigisLusciousLasagna.elm
@@ -1,3 +1,10 @@
 module LuigisLusciousLasagna exposing (remainingTimeInMinutes)
 
--- TODO: define the remainingTimeInMinutes function
+expectedMinutesInOven = 40
+minutesPerLayer = 2
+
+remainingTimeInMinutes : Int -> Int -> Int
+remainingTimeInMinutes layers minutesElapsed =
+      let preparationTimeInMinutes numLayers = minutesPerLayer * numLayers
+      in preparationTimeInMinutes layers + expectedMinutesInOven - minutesElapsed
+
