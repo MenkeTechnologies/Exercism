47aad4b6c7f6379aef2da246b1ed2f9f2a8138ce
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-07 11:46:12 -0400

    ham

--- elm/hamming/src/Hamming.elm
+++ elm/hamming/src/Hamming.elm
@@ -1,6 +1,12 @@
 module Hamming exposing (distance)
 
+import String exposing (length, toList)
 
 distance : String -> String -> Result String Int
-distance left right =
-    Debug.todo "Please implement this function"
+distance l r =
+    if length l == length r then
+        List.map2 (/=) (toList l) (toList r)
+        |> List.filter (\n -> n == True)
+        |> List.length |> Ok
+    else
+        Err "left and right strands must be of equal length"
