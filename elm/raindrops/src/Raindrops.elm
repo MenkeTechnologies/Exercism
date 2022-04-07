3908e0afeddbd933eb15bc70623c344c4e2cc907
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-07 11:34:45 -0400

    rd

--- elm/raindrops/src/Raindrops.elm
+++ elm/raindrops/src/Raindrops.elm
@@ -1,6 +1,15 @@
 module Raindrops exposing (raindrops)
 
+drops = [ ( 3, "Pling" ), ( 5, "Plang" ), ( 7, "Plong" ) ]
+
+emptyOrDefault : Int -> String -> String
+emptyOrDefault n s =
+    if String.isEmpty s then
+        String.fromInt n
+    else
+        s
 
 raindrops : Int -> String
-raindrops number =
-    Debug.todo "Please implement this function"
+raindrops n = List.filterMap(\(divisor, s) ->
+    if modBy divisor n == 0 then Just s else Nothing) drops
+    |> String.concat |> emptyOrDefault n
