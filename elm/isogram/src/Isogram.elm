6e9a32068dbf7ea311743ee72c911b240b8f799e
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-08 10:59:15 -0400

    iso

--- elm/isogram/src/Isogram.elm
+++ elm/isogram/src/Isogram.elm
@@ -1,6 +1,9 @@
 module Isogram exposing (isIsogram)
-
+import Set
 
 isIsogram : String -> Bool
 isIsogram sentence =
-    Debug.todo "Please implement this function"
+    let
+        list = String.toList sentence |> List.filter Char.isAlpha |> List.map Char.toLower
+        set = list |> Set.fromList
+    in List.length list == Set.size set
