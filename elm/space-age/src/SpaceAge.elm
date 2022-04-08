904a0e75f980725030b8ed2080a02e21b126e46d
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-08 10:47:01 -0400

    sa

--- elm/space-age/src/SpaceAge.elm
+++ elm/space-age/src/SpaceAge.elm
@@ -11,7 +11,18 @@ type Planet
     | Uranus
     | Neptune
 
+spy = 31557600
 
 ageOn : Planet -> Float -> Float
 ageOn planet seconds =
-    Debug.todo "Please implement this function"
+    let earthYears = seconds / spy
+        earthYearsFactor fact = earthYears / fact
+    in case planet of
+        Mercury -> earthYearsFactor 0.2408467
+        Venus -> earthYearsFactor 0.61519726
+        Earth -> earthYears
+        Mars -> earthYearsFactor 1.8808158
+        Jupiter -> earthYearsFactor 11.862615
+        Saturn -> earthYearsFactor 29.447498
+        Uranus -> earthYearsFactor 84.016846
+        Neptune -> earthYearsFactor 164.79132
