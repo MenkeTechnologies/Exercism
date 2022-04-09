39944056a3fe3195d4c9f005c5286cf0322560d0
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-09 12:20:42 -0400

    bw

--- elm/bandwagoner/src/Bandwagoner.elm
+++ elm/bandwagoner/src/Bandwagoner.elm
@@ -1,22 +1,30 @@
 module Bandwagoner exposing (..)
 
--- TODO: please define a type alias for the 'Coach' record
--- TODO: please define the 'Stats' record
--- TODO: please define the 'Team' record
+type alias Coach =
+    {
+    name: String,
+    formerPlayer: Bool
+    }
+type alias Stats =
+    {
+    wins: Int,
+    losses: Int
+    }
+
+type alias Team =
+    { name : String,
+    coach : Coach,
+    stats : Stats
+    }
 
 
 replaceCoach newCoach team =
-    Debug.todo "Please implement this function"
+    { team | coach = newCoach }
 
 
 createTeam name stats coach =
-    Debug.todo "Please implement this function"
-
-
-
--- TODO: use an extensible record to accept any record that has a `stats: Stats` field
--- TODO: use pattern matching in the function parameter to get the `stats` field from the record
+   Team name coach stats
 
 
 rootForTeam team =
-    Debug.todo "Please implement this function, try to pattern match in the function parameter"
+    team.stats.wins > team.stats.losses
