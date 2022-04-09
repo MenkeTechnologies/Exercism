86e541c008bb59f950c5ed9a72b1eeabc3332106
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-09 12:49:05 -0400

    tou

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,7 +1,69 @@
-=begin
-Write your code for the 'Tournament' exercise in this file. Make the tests in
-`tournament_test.rb` pass.
+class Team
+  WIN_POINTS = 3
+  DRAW_POINTS = 1
+  attr_accessor :name, :wins, :losses, :draws
 
-To get started with TDD, see the `README.md` file in your
-`ruby/tournament` directory.
-=end
+  def initialize(name)
+    @name = name
+    @wins = 0
+    @losses = 0
+    @draws = 0
+  end
+
+  def matches
+    @wins + @losses + @draws
+  end
+
+  def points
+    @wins * WIN_POINTS + @draws * DRAW_POINTS
+  end
+
+  def info
+    [@name, matches, @wins, @draws, @losses, points]
+  end
+
+  def won
+    @wins += 1
+  end
+
+  def lost
+    @losses += 1
+  end
+
+  def tied
+    @draws += 1
+  end
+
+end
+
+class Tournament
+  REPORT_HEADER = %w[Team MP W D L P].freeze
+  REPORT_FORMAT = "%-31s| %2s |  %s |  %s |  %s |  %s\n".freeze
+
+  def self.tally(input)
+    @teams = Hash.new { _1[_2] = Team.new(_2) }
+    input.split("\n").each { parse(_1) }
+
+    REPORT_FORMAT % REPORT_HEADER +
+      @teams.values
+            .sort_by { [-_1.points, _1.name] }
+            .map { REPORT_FORMAT % _1.info }
+            .join
+  end
+
+  def self.parse(line)
+    t1, t2, r = line.split(';')
+    case r
+    when 'win'
+      @teams[t1].won
+      @teams[t2].lost
+    when 'loss'
+      @teams[t1].lost
+      @teams[t2].won
+    when 'draw'
+      @teams[t1].tied
+      @teams[t2].tied
+    end
+  end
+
+end
