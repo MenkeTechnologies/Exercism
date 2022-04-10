e2f9241652ca1fc193fef2c09142fedc70f56047
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-10 14:05:17 -0400

    etl

--- tcl/etl/etl.tcl
+++ tcl/etl/etl.tcl
@@ -1,3 +1,17 @@
-proc transform {} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc transform {raw} {
+
+    try {
+        set input [dict create {*}$raw]
+    } on error {} {
+        error "invalid input"
+    }
+
+    set r {}
+    dict for {value letters} $input {
+        foreach letter $letters {
+            dict set r [string tolower $letter] $value
+        }
+    }
+
+    return $r
 }
