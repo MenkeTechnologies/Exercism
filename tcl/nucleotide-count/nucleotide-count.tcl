cb4f2289787ec2f4b9803dbe8fce8c590b96ba43
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-10 13:56:19 -0400

    nt

--- tcl/nucleotide-count/nucleotide-count.tcl
+++ tcl/nucleotide-count/nucleotide-count.tcl
@@ -1,3 +1,11 @@
 proc nucleotideCounts {strand} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set counts {A 0 C 0 G 0 T 0}
+    foreach ch [split $strand ""] {
+        if {[dict exists $counts $ch]} {
+            dict incr counts $ch
+        } else {
+            error "Invalid nucleotide in strand"
+        }
+    }
+    return $counts
 }
