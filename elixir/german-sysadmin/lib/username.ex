6eb96bccedd666383cf9090977b7d808ab723bbf
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-10 14:51:21 -0400

    more exe

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,10 +1,19 @@
 defmodule Username do
   def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
-
-    # Please implement the sanitize/1 function
+    Enum.reduce(
+      username,
+      '',
+      fn c, acc ->
+        acc ++ case c do
+          ?_ -> '_'
+          ?ä -> 'ae'
+          ?ö -> 'oe'
+          ?ß -> 'ss'
+          ?ü -> 'ue'
+          c when c in ?a..?z -> [c]
+          _ -> ''
+        end
+      end
+    )
   end
 end
