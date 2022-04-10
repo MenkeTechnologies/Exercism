54c6ff4e09ecdc479602ae038a584f04458f440c
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-10 13:53:45 -0400

    mult

--- tcl/sum-of-multiples/sum-of-multiples.tcl
+++ tcl/sum-of-multiples/sum-of-multiples.tcl
@@ -1,3 +1,13 @@
 proc sumOfMultiples {factors limit} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set sum 0
+    for {set i 1} {$i < $limit} {incr i} {
+        foreach f $factors {
+            if {$f > 0 && $i % $f == 0} {
+                incr sum $i
+                break
+            }
+        }
+    }
+
+    return $sum
 }
