8e6bde9699f93de61a38947672e1410b3329c48d
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-10 13:18:52 -0400

    strain

--- erlang/strain/src/strain.erl
+++ erlang/strain/src/strain.erl
@@ -2,8 +2,13 @@
 
 -export([keep/2, discard/2]).
 
-keep(_Fn, _List) ->
-  undefined.
+keep(F, _List) -> filter(F, _List, []).
 
-discard(_Fn, _List) ->
-  undefined.
+discard(F, _List) -> filter(fun(X) -> not F(X) end, _List, []).
+
+filter(F, [H|T], Acc) ->
+  case F(H) of
+    true -> filter(F, T, [H|Acc]);
+    false -> filter(F, T, Acc)
+  end;
+filter(_, [], Acc) -> lists:reverse(Acc).
