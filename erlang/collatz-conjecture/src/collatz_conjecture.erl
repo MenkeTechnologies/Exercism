3fe361c585586d1dbd266d45c1d778731d4e4018
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-10 00:05:09 -0400

    coll

--- erlang/collatz-conjecture/src/collatz_conjecture.erl
+++ erlang/collatz-conjecture/src/collatz_conjecture.erl
@@ -3,4 +3,9 @@
 -export([steps/1]).
 
 
-steps(_N) -> undefined.
+steps(N) when is_integer(N), N > 0 -> steps(N, 0);
+steps(_) -> erlang:error(badarg).
+
+steps(1, Acc) -> Acc;
+steps(N, Acc) when N rem 2 == 0 -> steps(N div 2, Acc + 1);
+steps(N, Acc) -> steps(3 * N + 1, Acc + 1).
