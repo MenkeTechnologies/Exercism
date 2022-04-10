4f50e81b98cd8c27d1a4addaa6319ca776df7aaf
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-10 13:29:21 -0400

    darts

--- erlang/darts/src/darts.erl
+++ erlang/darts/src/darts.erl
@@ -2,5 +2,13 @@
 
 -export([score/2]).
 
+dist(X,Y) -> math:sqrt(X * X + Y * Y).
 
-score(_X, _Y) -> undefined.
+score(_X, _Y) ->
+  D = dist(_X, _Y),
+  if
+    D =< 1 -> 10;
+    D =< 5 -> 5;
+    D =< 10 -> 1;
+    true -> 0
+  end.
