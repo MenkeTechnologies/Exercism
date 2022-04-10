f805e050fd0f938a740c54681d6f9827290cf95c
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-10 14:44:28 -0400

    strain

--- tcl/strain/strain.tcl
+++ tcl/strain/strain.tcl
@@ -1,8 +1,22 @@
   
-proc keep {varname list condition} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc keep {varname list condition {keep 1}} {
+    set l {}
+    foreach $varname $list {
+        eval "set v $$varname"
+        if {[eval $condition]} {
+            if {$keep} {
+                lappend l $v
+            }
+        } else {
+            if {!$keep} {
+                lappend l $v
+            }
+        }
+
+    }
+    return $l
 }
 
 proc discard {varname list condition} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    keep $varname $list $condition 0
 }
