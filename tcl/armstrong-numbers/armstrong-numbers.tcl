4fad7527d437610bdb7110674bd75a5d42c81f04
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-10 13:58:54 -0400

    arm

--- tcl/armstrong-numbers/armstrong-numbers.tcl
+++ tcl/armstrong-numbers/armstrong-numbers.tcl
@@ -1,3 +1,8 @@
 proc isArmstrongNumber {number} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set len [string length $number]
+    set sum 0
+    foreach ch [split $number ""] {
+        incr sum [expr {$ch ** $len}]
+    }
+    expr $sum == $number
 }
