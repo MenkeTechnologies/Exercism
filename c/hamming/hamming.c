f023f11c4258a894f59fd9f1cc0684172eca7dd1
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-11 01:25:38 -0400

    ham

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -1,1 +1,15 @@
 #include "hamming.h"
+#include "string.h"
+
+int compute(const char *lhs, const char *rhs) {
+
+
+    if (strlen(lhs) != strlen(rhs)) return -1;
+    int diff = 0;
+
+    for (size_t i = 0; i < strlen(lhs); ++i) {
+        if (lhs[i] != rhs[i]) ++diff;
+    }
+
+    return diff;
+}
