738ba8bcd35961aa46be75a3962f3d84275b5eef
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-12 10:40:48 -0400

    resistor

--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -1,1 +1,14 @@
 #include "resistor_color.h"
+
+int color_code(resistor_band_t resistorBand) {
+    return resistorBand;
+}
+int color[TYPES];
+
+int *colors() {
+    for (int i = 0; i < TYPES; ++i) {
+        color[i] = i;
+    }
+
+    return color;
+}
--- c/resistor-color/resistor_color.h
+++ c/resistor-color/resistor_color.h
@@ -2,7 +2,20 @@
 #define RESISTOR_COLOR_H
 
 typedef enum {
-
+    BLACK = 0,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE,
+    TYPES
 } resistor_band_t;
+
+int color_code(resistor_band_t);
+int* colors();
 
 #endif
