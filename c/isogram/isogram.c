bd12d51f50389b9866b91cfb659dc259fab719fc
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-12 11:01:12 -0400

    iso

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,1 +1,23 @@
+#include <string.h>
+#include <ctype.h>
 #include "isogram.h"
+
+bool is_isogram(const char *phrase) {
+    bool seen[26];
+    memset(seen, 0, sizeof seen);
+
+    if (!phrase) {
+        return false;
+    }
+
+    for (const char *c = phrase; *c; ++c) {
+        if (isalpha(*c)) {
+            int i = tolower(*c) - 'a';
+            if (seen[i]) {
+                return false;
+            }
+            seen[i] = true;
+        }
+    }
+    return true;
+}
