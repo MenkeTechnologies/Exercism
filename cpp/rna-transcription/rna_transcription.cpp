7c876b38dbaf12184b39e8e3ee5666f72a49eb64
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-14 11:15:48 -0400

    rna

--- cpp/rna-transcription/rna_transcription.cpp
+++ cpp/rna-transcription/rna_transcription.cpp
@@ -1,5 +1,27 @@
 #include "rna_transcription.h"
 
+#include <map>
+
+using namespace std;
+
 namespace rna_transcription {
+    static const map<char, char> dna2rna = {
+            {'G', 'C'},
+            {'C', 'G'},
+            {'T', 'A'},
+            {'A', 'U'}
+    };
+
+    char to_rna(char ch) {
+        return dna2rna.at(ch);
+    }
+
+    string to_rna(string dna) {
+        string rna;
+        for (char ch: dna) {
+            rna += to_rna(ch);
+        }
+        return rna;
+    }
 
 }  // namespace rna_transcription
--- cpp/rna-transcription/rna_transcription.h
+++ cpp/rna-transcription/rna_transcription.h
@@ -1,7 +1,15 @@
 #if !defined(RNA_TRANSCRIPTION_H)
 #define RNA_TRANSCRIPTION_H
 
+#include <map>
+#include <string>
+
+using namespace std;
+
 namespace rna_transcription {
+
+    char to_rna(char);
+    string to_rna(string);
 
 }  // namespace rna_transcription
 
