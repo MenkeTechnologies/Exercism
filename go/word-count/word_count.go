5d8d42ef2110df70135ff3d5b89c3d85e2c7e298
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-15 08:49:40 -0400

    wc

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,1 +1,29 @@
 package wordcount
+
+import (
+	"strings"
+	"unicode"
+)
+
+type Frequency map[string]int
+
+func WordCount(s string) (dict Frequency) {
+
+	dict = Frequency{}
+
+	words := strings.FieldsFunc(s, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsDigit(r) && r != '\''
+	})
+
+	for _, w := range words {
+		w = strings.Trim(strings.ToLower(w), "'")
+		if _, ok := dict[w]; !ok {
+			dict[w] = 1
+		} else {
+			dict[w]++
+		}
+	}
+
+	return
+
+}
