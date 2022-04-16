71e505fe670bf64e74067ce3af7fe89c6d814738
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-16 13:13:30 -0400

    dos

--- cpp/difference-of-squares/difference_of_squares.cpp
+++ cpp/difference-of-squares/difference_of_squares.cpp
@@ -2,4 +2,28 @@
 
 namespace difference_of_squares {
 
-}  // namespace difference_of_squares
+    int sum_of_squares(int n) {
+
+        int sum = 0;
+        for (int i = 1; i <= n; ++i) {
+            sum += pow(i, 2);
+        }
+
+        return sum;
+    }
+
+    int square_of_sum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; ++i) {
+            sum += i;
+        }
+
+        return pow(sum, 2);
+    }
+
+    int difference(int n) {
+        return square_of_sum(n) - sum_of_squares(n);
+    }
+
+
+}
