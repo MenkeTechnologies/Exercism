5fcc6ebae42007218d75ddb4d6b1e099770bf09e
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-17 11:31:44 -0400

    ham

--- scheme/hamming/hamming.scm
+++ scheme/hamming/hamming.scm
@@ -1,5 +1,5 @@
 (import (rnrs))
 
-(define (hamming-distance strand-a strand-b)
-  'implement-me!)
+(define (hamming-distance s1 s2)
+  (length (filter not (map char=? (string->list s1) (string->list s2)))))
 
