62081570b5a9d92e0df2a73df78b5e7416bb2df0
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-17 11:39:55 -0400

    ana

--- scheme/anagram/anagram.scm
+++ scheme/anagram/anagram.scm
@@ -1,5 +1,16 @@
 (import (rnrs))
 
 (define (anagram target words)
-  'implement-me!)
+ (filter (lambda (word) (is-anagram? target word))
+  (filter (lambda (word) (not (string=? (downcase target) (downcase word)))) words)
+  )
+  )
 
+(define (is-anagram? target word)
+ (string=? (sorted (downcase target)) (sorted (downcase word))))
+
+(define (sorted word)
+ (list->string (list-sort char<? (string->list word))))
+
+(define (downcase word)
+ (list->string (map char-downcase (string->list word))))
