ab3dc1cb8f7bde6a60d74167163b5c9ffa1cc8ba
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-18 00:11:19 -0400

    dos

--- racket/difference-of-squares/difference-of-squares.rkt
+++ racket/difference-of-squares/difference-of-squares.rkt
@@ -1,3 +1,14 @@
 #lang racket
 
 (provide sum-of-squares square-of-sum difference)
+
+(define (sum-of-squares n)
+    (for/sum ([s (in-range 1 (add1 n))]) (sqr s))
+)
+
+(define (square-of-sum n)
+    (sqr (for/sum ([s (in-range 1 (add1 n))]) s))
+)
+
+(define (difference n)
+    (- (square-of-sum n) (sum-of-squares n)))
