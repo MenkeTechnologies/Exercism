88b5483c09fe316ee8bf82e24361eb8e067a6bea
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-19 11:07:51 -0400

    wc

--- racket/word-count/word-count.rkt
+++ racket/word-count/word-count.rkt
@@ -2,3 +2,12 @@
 
 (provide word-count)
 
+(define (word-count sentence)
+ (define regex #px"(\\w+'\\w|\\d+|\\w+)")
+ (for/fold
+    ([wc (hash)])
+    ([w (in-list (map string-downcase (regexp-match* regex sentence)))])
+    (hash-set wc w (add1 (hash-ref wc w 0)))
+    )
+ )
+
