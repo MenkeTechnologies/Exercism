857c1388453cda81345db7bb9b9731607c706d95
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-18 23:19:31 -0400

    coll

--- racket/collatz-conjecture/collatz-conjecture.rkt
+++ racket/collatz-conjecture/collatz-conjecture.rkt
@@ -2,5 +2,8 @@
 
 (provide collatz)
 
-  
-
+(define (collatz n)
+ (if (not (exact-positive-integer? n)) (error "Invalid input")
+  (if (= n 1) 0
+   (if (zero? (modulo n 2)) (add1 (collatz (/ n 2)))
+    (add1 (collatz (add1 (* 3 n))))))))
