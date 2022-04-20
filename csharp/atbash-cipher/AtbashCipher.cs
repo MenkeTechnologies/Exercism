751e6b045eca02297a5227efa2a2adf70dffccc6
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-20 12:16:43 -0400

    atbash

--- csharp/atbash-cipher/AtbashCipher.cs
+++ csharp/atbash-cipher/AtbashCipher.cs
@@ -1,14 +1,17 @@
 using System;
+using System.Linq;
 
 public static class AtbashCipher
 {
-    public static string Encode(string plainValue)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static string Encode(string plainValue) =>
+        plainValue.Where(char.IsLetterOrDigit)
+            .Select(char.ToLower)
+            .Select(c => char.IsLetter(c) ? (char)('a' + 'z' - c) : c)
+            .SelectMany((c, index) => index > 0 && index % 5 == 0 ? $" {c}" : $"{c}")
+            .Aggregate("", (acc, n) => acc + n);
 
-    public static string Decode(string encodedValue)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static string Decode(string encodedValue) =>
+        encodedValue
+            .Where(char.IsLetterOrDigit)
+            .Select(c => char.IsLetter(c) ? (char)('a' + 'z' - c) : c).Aggregate("", (acc, n) => acc + n);
 }
