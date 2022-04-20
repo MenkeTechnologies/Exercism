b653da46bf5ff6ac8473a1a55d5bc6a69f6bfd87
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-20 17:13:26 -0400

    dos

--- emacs-lisp/difference-of-squares/difference-of-squares.el
+++ emacs-lisp/difference-of-squares/difference-of-squares.el
@@ -7,3 +7,17 @@
 
 (provide 'difference-of-squares)
 ;;; difference-of-squares.el ends here
+
+(defun square-of-sum (n)
+ (expt (apply '+ (number-sequence 1 n)) 2)
+ )
+
+(defun sum-of-squares (n)
+ (apply '+ (mapcar (lambda (nn) (expt nn 2)) (number-sequence 1 n)))
+ )
+
+(defun difference (n)
+    (-  (square-of-sum n)
+        (sum-of-squares n)
+    )
+)
