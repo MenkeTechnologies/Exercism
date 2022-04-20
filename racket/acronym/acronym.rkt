7260ba2315a56980dd4bc86ff43c894183d96ac0
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-20 01:47:48 -0400

    acro

--- racket/acronym/acronym.rkt
+++ racket/acronym/acronym.rkt
@@ -1,3 +1,14 @@
 #lang racket
 
 (provide acronym)
+
+(require threading)
+
+(define (acronym s)
+ (~>> s
+    (string-split _ #px"[-\\s_]+")
+    (map (curryr string-ref 0) )
+    (map char-upcase)
+    (apply string)
+  )
+)
