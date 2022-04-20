11361cc6e194b7d121eb0eb01d0cc6fd5aec91e7
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-20 11:45:30 -0400

    ana

--- racket/anagram/anagram.rkt
+++ racket/anagram/anagram.rkt
@@ -1,3 +1,13 @@
 #lang racket
 
 (provide anagrams-for)
+
+(define (sort-letters s)
+ (sort (string->list (string-downcase s)) char<?)
+ )
+
+(define (anagrams-for word cands)
+ (let ([sorted (sort-letters word)])
+  (filter (lambda (cand) (and (equal? (sort-letters cand) sorted) (not (equal? cand word)))) cands)
+  )
+ )
