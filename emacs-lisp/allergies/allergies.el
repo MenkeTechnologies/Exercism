1cbb0d42238edce628688fc622d7e84e70ececf5
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-21 14:42:33 -0400

    all

--- emacs-lisp/allergies/allergies.el
+++ emacs-lisp/allergies/allergies.el
@@ -1,10 +1,19 @@
-;;; allergies.el --- Allergies Exercise (exercism)
-
-;;; Commentary:
-
-;;; Code:
-
-
-
 (provide 'allergies)
-;;; allergies.el ends here
+
+(require 'seq)
+
+(defvar allergen-names '("eggs"
+                         "peanuts"
+                         "shellfish"
+                         "strawberries"
+                         "tomatoes"
+                         "chocolate"
+                         "pollen"
+                         "cats"))
+
+(defun allergen-list (mask)
+ (seq-filter (lambda (a) (allergic-to-p mask a)) allergen-names))
+
+(defun allergic-to-p (mask a)
+ (< 0 (logand mask (expt 2 (seq-position allergen-names a))))
+ )
