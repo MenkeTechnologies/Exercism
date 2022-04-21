1778c127ca43bbe18e3d76f76e3311b3f3b1928f
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-21 00:42:29 -0400

    ham

--- emacs-lisp/hamming/hamming.el
+++ emacs-lisp/hamming/hamming.el
@@ -1,9 +1,7 @@
-;;; hamming.el --- Hamming (exercism)
-
-;;; Commentary:
-
-;;; Code:
-
-
 (provide 'hamming)
-;;; hamming.el ends here
+(require 'seq)
+
+(defun hamming-distance (s1 s2)
+ (if (= (length s1) (length s2))
+  (seq-count #'not (seq-mapn #'= s1 s2))
+   (error "DNA strands are of unequal lengths")))
