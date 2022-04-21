de8f348c6b091100a6ed2b42ce1801a1048fdc86
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-21 11:36:40 -0400

    rna

--- emacs-lisp/rna-transcription/rna-transcription.el
+++ emacs-lisp/rna-transcription/rna-transcription.el
@@ -1,12 +1,15 @@
-;;; rna-transcription.el -- RNA Transcription (exercism)
-
-;;; Commentary:
-
-
-
-;;; Code:
-
-
 
 (provide 'rna-transcription)
-;;; rna-transcription.el ends here
+
+
+(defun to-rna (s)
+ (mapconcat (lambda (c)
+             (pcase c
+                (?G "C")
+                (?C "G")
+                (?T "A")
+                (?A "U")
+                (_ (error "Unknown character %s" c))
+              ))
+  s ""
+  ))
