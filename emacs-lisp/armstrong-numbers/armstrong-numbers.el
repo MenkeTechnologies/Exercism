a5a422ee12c8f42ee4647c42b463e507a6c8519c
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-20 23:06:38 -0400

    arm

--- emacs-lisp/armstrong-numbers/armstrong-numbers.el
+++ emacs-lisp/armstrong-numbers/armstrong-numbers.el
@@ -1,8 +1,8 @@
-;;; armstrong-numbers.el --- armstrong-numbers Exercise (exercism)
-
-;;; Commentary:
-
-;;; Code:
+(defun armstrong-p (n)
+ (eq n
+  (apply '+
+   (mapcar
+    (lambda (x) (expt (cl-digit-char-p x) (length (number-to-string n))))
+    (prin1-to-string n)))))
 
 (provide 'armstrong-numbers)
-;;; armstrong-numbers.el ends here
