9415372c9485bb399979383bd8a293e971487c99
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-20 21:29:34 -0400

    grains

--- emacs-lisp/grains/grains.el
+++ emacs-lisp/grains/grains.el
@@ -1,10 +1,10 @@
 ;;; grains.el --- Grains exercise (exercism)
 
-;;; Commentary:
-
-;;; Code:
-
-
-
 (provide 'grains)
 ;;; grains.el ends here
+
+(defun square (num)
+ (expt 2.0 (1- num)))
+
+(defun total ()
+ (apply #'+ (mapcar #'square (number-sequence 1 64))))
