b393542b2e04556f6848afdaff5bcbeadabf513c
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-22 14:52:46 -0400

    pn

--- emacs-lisp/perfect-numbers/perfect-numbers.el
+++ emacs-lisp/perfect-numbers/perfect-numbers.el
@@ -1,11 +1,18 @@
-;;; perfect-numbers.el --- perfect-numbers Exercise (exercism)
+(require 'seq)
 
-;;; Commentary:
+(defun aliquot (n)
+  (apply '+ 
+   (seq-filter (lambda (m) (zerop (% n m)))
+    (number-sequence 1 (/ n 2)))))
 
-;;; Code:
+(defun classify (n)
+ (let ((sum (aliquot n)))
+    (cond ((> 0 n) (error "Classification is only possible for natural numbers"))
+        ((> sum n) 'abundant)
+        ((< sum n) 'deficient)
+        (t 'perfect)
 
-(defun classify (number)
-  "Do something with NUMBER here.")
+  ))
+ )
 
 (provide 'perfect-numbers)
-;;; perfect-numbers.el ends here
