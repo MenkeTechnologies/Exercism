178e6f8d23534fd57d3a8a09c173b897d9bae854
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-22 17:21:33 -0400

    allerg

--- clojurescript/allergies/src/allergies.cljs
+++ clojurescript/allergies/src/allergies.cljs
@@ -1,5 +1,20 @@
 (ns allergies)
 
-(defn allergies [])
+(def allergen->score
+  {:eggs         1
+   :peanuts      2
+   :shellfish    4
+   :strawberries 8
+   :tomatoes     16
+   :chocolate    32
+   :pollen       64
+   :cats         128})
 
-(defn allergic-to? [])
+(defn allergic-to? [mask allergen]
+      (pos?
+        (bit-and mask(allergen allergen->score))))
+
+
+(defn allergies [mask]
+      (filter #(allergic-to? mask %)
+              (keys allergen->score)))
