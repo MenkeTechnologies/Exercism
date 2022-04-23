7f6b86135536194b8e743fb6078d70c49cadcba2
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-23 09:58:17 -0400

    nt

--- clojurescript/nucleotide-count/src/nucleotide_count.cljs
+++ clojurescript/nucleotide-count/src/nucleotide_count.cljs
@@ -1,5 +1,16 @@
 (ns nucleotide-count)
 
-(defn count-of-nucleotide-in-strand [nucleotide dna-strand])
+(def nucleotides {"A" 0 "T" 0 "C" 0 "G" 0})
 
-(defn nucleotide-counts [dna-strand])
+(defn nucleotide-counts [dna-strand]
+  (->> dna-strand
+       (seq)
+       (frequencies)
+       (#(conj nucleotides %))
+       ))
+
+(defn count-of-nucleotide-in-strand [nucleotide dna-strand]
+  (if (nucleotides nucleotide)
+    ((nucleotide-counts dna-strand) nucleotide)
+    (throw (js/Error "Bad nucleotide")))
+  )
