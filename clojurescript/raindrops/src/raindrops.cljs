15457f882fcae4d18035e6432601782ef140bf64
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-23 00:43:14 -0400

    rd

--- clojurescript/raindrops/src/raindrops.cljs
+++ clojurescript/raindrops/src/raindrops.cljs
@@ -1,3 +1,14 @@
 (ns raindrops)
 
-(defn convert [n])
+(def sounds {3 "Pling"
+             5 "Plang"
+             7 "Plong"})
+
+(defn convert [n]
+  (as-> sounds _
+       (filter #(zero? (rem n (key %))) _)
+       (vals _)
+       (or _ [n])
+       (apply str _)
+        )
+  )
