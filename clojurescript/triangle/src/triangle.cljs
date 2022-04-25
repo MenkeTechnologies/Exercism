2a5de633fa33b2b7c4c3765ca46ad25887433a53
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-25 10:00:55 -0400

    tri

--- clojurescript/triangle/src/triangle.cljs
+++ clojurescript/triangle/src/triangle.cljs
@@ -1,9 +1,24 @@
 (ns triangle)
 
-(defn is-valid? [])
+(defn is-valid? [a b c]
+  (let [sides (sort-by #(* -1 %) [a b c])]
+    (and (every? #(not= 0 %) sides)
+         (<= (first sides) (apply + (rest sides))))))
 
-(defn equilateral? [])
+(defn- uniq-sides [a b c]
+  (count (distinct [a b c])))
 
-(defn isosceles? [])
+(defn equilateral? [a b c]
+  (and (is-valid? a b c)
+       (= 1 (uniq-sides a b c)))
+  )
 
-(defn scalene? [])
+(defn isosceles? [a b c]
+  (and (is-valid? a b c)
+       (>= 2 (uniq-sides a b c)))
+  )
+
+(defn scalene? [a b c]
+  (and (is-valid? a b c)
+       (= 3 (uniq-sides a b c)))
+  )
