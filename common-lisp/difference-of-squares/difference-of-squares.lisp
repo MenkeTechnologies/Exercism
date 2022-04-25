f961ec3c3458120482638bd0e8a675a0d261cc9d
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-25 15:58:26 -0400

    dos

--- common-lisp/difference-of-squares/difference-of-squares.lisp
+++ common-lisp/difference-of-squares/difference-of-squares.lisp
@@ -8,12 +8,15 @@
 
 (defun square-of-sum (n)
   "Calculates the square of the sum for a given number."
+  (expt (reduce #'+ (loop for i from 1 to n collecting i))2)
   )
 
 (defun sum-of-squares (n)
   "Calculates the sum of squares for a given number."
+  (reduce #'+ (loop for i from 1 to n collecting (expt i 2)))
   )
 
 (defun difference (n)
   "Finds the diff. between the square of the sum and the sum of the squares."
+  (- (square-of-sum n) (sum-of-squares n))
   )
