66e8f81a32e76d3b6ae631e9f1fb6fd4b2535059
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-26 15:22:29 -0400

    etl

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -1,5 +1,12 @@
 (ns etl)
 
-(defn transform [source] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn partial-transform [[num letters]]
+  (let [letters (map clojure.string/lower-case letters)]
+    (apply merge (for [letter letters] (assoc {} letter num))))
+  )
+
+(defn transform [source]
+  (->> (into [] source)
+       (map partial-transform)
+       (apply merge))
+  )
