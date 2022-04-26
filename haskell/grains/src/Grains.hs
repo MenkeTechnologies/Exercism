9ed490465122ed4c5e5e483f81944efbe10b1697
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-26 11:15:40 -0400

    grains

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -1,7 +1,9 @@
 module Grains (square, total) where
+  
+import Data.Maybe (fromMaybe)
 
 square :: Integer -> Maybe Integer
-square n = error "You need to implement this function."
+square n = if elem n [1..64] then Just $ 2^(n - 1) else Nothing
 
 total :: Integer
-total = error "You need to implement this function."
+total = sum (map ((fromMaybe 0) . square) [1..64])
