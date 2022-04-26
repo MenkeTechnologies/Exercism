a1b4ab84cb68ba7f4bdff4d91211356c145dff02
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-26 13:53:07 -0400

    rna

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,5 +1,15 @@
 (ns rna-transcription)
 
-(defn to-rna [dna] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def nucleotide-map {\G \C \C \G \T \A \A \U})
+
+(defn- get-nt [k]
+  (let [v (get nucleotide-map k)]
+    (if (nil? v)
+      (throw (AssertionError. "invalid"))
+      v
+      )))
+
+(defn to-rna [dna]
+  (->> dna
+       (map #(get-nt %))
+       (apply str)))
