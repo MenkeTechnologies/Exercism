bb23a38e5bbc4618565924078a5c551e40a3c750
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-26 11:22:57 -0400

    ham

--- clojure/hamming/src/hamming.clj
+++ clojure/hamming/src/hamming.clj
@@ -1,5 +1,6 @@
 (ns hamming)
 
-(defn distance [strand1 strand2] ; <- arglist goes here
-  ;; your code goes here
+(defn distance [strand1 strand2]
+  (when (= (count strand1) (count strand2))
+    (count (filter false? (map = strand1 strand2))))
 )
