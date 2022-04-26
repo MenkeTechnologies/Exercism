10285c9a02b98487290bb408bb8e309553192a33
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-26 19:28:29 -0400

    qa

--- perl5/queen-attack/Queens.pm
+++ perl5/queen-attack/Queens.pm
@@ -1,11 +1,36 @@
 package Queens;
 use strict;
-use warnings FATAL => 'all';
+use warnings;
 
 sub new {
-    my ($class, %data) = @_;
-    bless \%data, $class
+  my ( $class, %data ) = @_;
 
+  return undef;
+}
+
+sub white {
+  my ($self) = @_;
+  
+  return undef;
+}
+
+sub black {
+  my ($self) = @_;
+  
+  return undef;
+}
+
+sub to_string {
+  my ($self) = @_;
+  
+  return undef;
+}
+
+sub can_attack {
+  my ($self) = @_;
+
+  return undef;
 }
 
 1;
+
