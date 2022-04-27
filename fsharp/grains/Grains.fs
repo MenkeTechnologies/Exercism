b3d583f9f55588c61589e96c115bfe8c385cba51
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-27 09:09:34 -0400

    grains

--- fsharp/grains/Grains.fs
+++ fsharp/grains/Grains.fs
@@ -1,4 +1,17 @@
 ﻿module Grains
 
-let square (n: int): Result<uint64,string> = failwith "You need to implement this function."
-let total: Result<uint64,string> = failwith "You need to implement this function."
+open System
+
+let square (n: int) : Result<uint64, string> =
+    if n > 0 && n < 65 then
+        Math.Pow(2, float (n - 1)) |> uint64 |> Ok
+    else
+        Error "square must be between 1 and 64"
+
+let total: Result<uint64, string> =
+    List.map square [ 1..64 ]
+    |> List.reduce (fun acc n ->
+        match acc, n with
+        | Ok acc, Ok v -> Ok(acc + v)
+        | _ -> Error "bad square"
+        )
