253aa8a91f6deefc8685ad45c64c4e4448b193d6
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-27 16:40:02 -0400

    darts

--- c/darts/darts.c
+++ c/darts/darts.c
@@ -1,1 +1,16 @@
+#include <math.h>
 #include "darts.h"
+
+
+const int SCORE_INNER_CIRCLE = 10;
+const int SCORE_MIDDLE_CIRCLE = 5;
+const int SCORE_OUTER_CIRCLE = 1;
+const int SCORE_OUT_OF_BOUNDS = 0;
+
+
+uint8_t score(coordinate_t coordinate) {
+    float d = sqrtf(pow(coordinate.x, 2) + pow(coordinate.y, 2));
+
+    return d <= 1 ? SCORE_INNER_CIRCLE : d <= 5 ? SCORE_MIDDLE_CIRCLE
+    : d <= 10 ? SCORE_OUTER_CIRCLE : SCORE_OUT_OF_BOUNDS;
+}
--- c/darts/darts.h
+++ c/darts/darts.h
@@ -1,4 +1,14 @@
 #ifndef DARTS_H
 #define DARTS_H
 
+#include <stdint.h>
+
+typedef struct coordanate
+{
+    float x;
+    float y;
+} coordinate_t;
+
+uint8_t score(coordinate_t);
+
 #endif
