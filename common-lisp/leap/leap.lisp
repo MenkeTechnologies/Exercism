9ac4ec324b50cb6a8bc5ba3c41a87e1d9ba82b2f
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-28 11:16:19 -0400

    leap

--- common-lisp/leap/leap.lisp
+++ common-lisp/leap/leap.lisp
@@ -3,5 +3,12 @@
   (:export :leap-year-p))
 (in-package :leap)
 
-(defun leap-year-p (year)
+(defun divisible (a b)
+ (zerop (mod a b)))
+
+(defun leap-year-p (y)
+ (or
+    (and (divisible y 4)
+        (not (divisible y 100)))
+    (divisible y 400))
   )
