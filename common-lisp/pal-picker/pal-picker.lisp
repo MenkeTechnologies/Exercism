729dd37714b79b4f8ec752f4a615c0a2fcf74239
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-27 22:05:13 -0400

    pp

--- common-lisp/pal-picker/pal-picker.lisp
+++ common-lisp/pal-picker/pal-picker.lisp
@@ -5,12 +5,36 @@
 
 (in-package :pal-picker)
 
-(defun pal-picker (personality))
+(defun pal-picker (personality)
+ (case personality
+   (:lazy "Cat")
+    (:energetic "Dog")
+    (:quiet "Fish")
+    (:hungry "Rabbit")
+    (:talkative "Bird")
+    (otherwise "I don't know... A dragon?")
+  ))
 
-(defun habitat-fitter (weight))
+(defun habitat-fitter (weight)
+ (cond
+    ((>= weight 40) :massive)
+    ((>= weight 20) :large)
+    ((>= weight 10) :medium)
+    ((>= weight 1)  :small)
+    (T :just-your-imagination)
+  ))
 
-(defun feeding-time-p (fullness))
+(defun feeding-time-p (fullness)
+ (if (> fullness 20)
+    "All is well."
+    "It's feeding time!"
+  ))
 
-(defun pet (pet))
+(defun pet (pet)
+ (if (string= pet "Fish")
+  "Maybe not with this pet..."))
 
-(defun play-fetch (pet))
+(defun play-fetch (pet)
+ (if (not (string= pet "Dog"))
+  "Maybe not with this pet..."
+  ))
