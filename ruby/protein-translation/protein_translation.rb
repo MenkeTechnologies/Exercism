de746796303e953c9370a18ea0dd23ac4bbd9129
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-29 11:47:28 -0400

    prot

--- ruby/protein-translation/protein_translation.rb
+++ ruby/protein-translation/protein_translation.rb
@@ -4,26 +4,34 @@ end
 class Translation
 
   CODONS = {
-    %w(AUG) => 'Methionine',
-    %w(UUU UUC) => 'Phenylalanine',
-    %w(UUA UUG) => 'Leucine',
-    %w(UCU UCC UCA UCG) => 'Serine',
-    %w(UAU UAC) => 'Tyrosine',
-    %w(UGU UGC) => 'Cysteine',
-    %w(UGG) => 'Tryptophan',
-    %w(UAA UAG UGA) => 'STOP'
-  }.freeze
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UGG" => "Tryptophan",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
 
   def self.of_codon(codon)
     CODONS[codon] || (raise InvalidCodonError)
   end
 
   def self.of_rna(strand)
-    strand.chars.slice(3)
-          .map{CODONS[_1]}
-          .take_while {_1 != "STOP" }
-    
-  end
-    
+    strand.chars.each_slice(3)
+          .map { of_codon(_1.join) }
+          .take_while { _1 != "STOP" }
 
   end
+
+end
