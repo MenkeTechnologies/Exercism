a6cdcb083abfc58c1fe226c8cfc4f7fdbea5ae2d
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-28 21:47:27 -0400

    nth

--- crystal/nth-prime/src/nth_prime.cr
+++ crystal/nth-prime/src/nth_prime.cr
@@ -1,1 +1,17 @@
-# Please implement your solution to nth-prime in this file
+module NthPrime
+  extend self
+  def prime(n)
+    raise ArgumentError.new "n must be positive" if n < 1
+    primes = [2]
+    curr = 2
+    while primes.size < n
+      curr += 1
+      primes << curr if prime?(curr)
+    end
+    primes[-1]
+  end
+  
+  def prime?(n)
+    (2..Math.sqrt(n)).none? { |i| n % i == 0 }
+  end
+end
