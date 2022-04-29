9b7ababcdce942dc0b99d1958b441713fd9aba8a
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-29 10:44:39 -0400

    dnd

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,9 +1,14 @@
 export class DnDCharacter {
-  public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
-  }
+  public static generateAbilityScore = () => Math.floor(Math.random() * 16) + 3;
 
-  public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this statement and implement this function')
-  }
+  public static getModifierFor = (abilityValue: number) => Math.floor((abilityValue - 10) / 2);
+  
+  readonly strength = DnDCharacter.generateAbilityScore()
+  readonly dexterity = DnDCharacter.generateAbilityScore()
+  readonly constitution = DnDCharacter.generateAbilityScore()
+  readonly intelligence = DnDCharacter.generateAbilityScore()
+  readonly wisdom = DnDCharacter.generateAbilityScore()
+  readonly charisma = DnDCharacter.generateAbilityScore()
+  readonly hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution)
+ 
 }
