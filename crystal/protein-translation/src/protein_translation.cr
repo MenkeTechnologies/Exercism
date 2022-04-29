b51b400cc354df47fc06a6709b60942825324357
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-29 11:30:15 -0400

    prot

--- crystal/protein-translation/src/protein_translation.cr
+++ crystal/protein-translation/src/protein_translation.cr
@@ -1,1 +1,30 @@
-# Please implement your solution to protein-translation in this file
+class ProteinTranslation
+
+  @@protein_map = {
+  "AUG" => "Methionine",
+  "UUU" => "Phenylalanine",
+  "UUC" => "Phenylalanine",
+  "UUA" => "Leucine",
+  "UUG" => "Leucine",
+  "UCU" => "Serine",
+  "UCC" => "Serine",
+  "UCA" => "Serine",
+  "UCG" => "Serine",
+  "UAU" => "Tyrosine",
+  "UAC" => "Tyrosine",
+  "UGU" => "Cysteine",
+  "UGC" => "Cysteine",
+  "UGG" => "Tryptophan",
+  "UAA" => "STOP",
+  "UAG" => "STOP",
+  "UGA" => "STOP"}
+
+
+  def self.proteins(rna : String)
+    rna.chars.in_groups_of(3).map{|c| c.join}
+    .map{|c| @@protein_map[c]}
+    .take_while{ |p| p != "STOP" }
+  end
+
+
+end
