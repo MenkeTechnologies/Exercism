782e0129c445fbed21ab3537a82bddf3d423d342
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-30 11:27:26 -0400

    perf

--- r/perfect-numbers/perfect-numbers.R
+++ r/perfect-numbers/perfect-numbers.R
@@ -1,3 +1,19 @@
 number_type <- function(n){
+  if (n < 1) {
+    stop("n must be natural number")
+  } else if (n == 1) {
+    return("deficient")
+  }
+  
+  m = n / 2
+  aliquot = sum((1:m)[(n %% 1:m) == 0])
+  
+  if (aliquot == n) {
+    "perfect"
+  } else if (aliquot > n) {
+    "abundant"
+  } else {
+    "deficient"
+  }
   
 }
