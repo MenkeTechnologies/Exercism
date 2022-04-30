5e41c956c410ba6c716372d1db50bbabcc324062
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-30 11:30:50 -0400

    acro

--- common-lisp/acronym/acronym.lisp
+++ common-lisp/acronym/acronym.lisp
@@ -4,6 +4,8 @@
 
 (in-package :acronym)
 
-(defun acronym (str)
+(defun acronym (s)
   "Returns the acronym for a noun of tech jargon."
+  (let ((title (string-capitalize s)))
+   (remove-if-not #'upper-case-p title))
   )
