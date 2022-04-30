f9ea094b02e2317eb6e5ab015673c59e6ab26342
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-29 21:09:16 -0400

    etl

--- common-lisp/etl/etl.lisp
+++ common-lisp/etl/etl.lisp
@@ -6,4 +6,11 @@
 
 (defun transform (data)
   "Transforms hash values into keys with their keys as their values."
+  (let ((scores (make-hash-table)))
+   (maphash #'(lambda (s letters)
+               (dolist (l letters)
+                (setf (gethash (char-downcase l) scores) s)))
+    data)
+   scores
+   )
   )
