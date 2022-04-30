83644a4d1c18f7be54222c04aeed6e083e1728d0
MenkeTechnologies <linux.dev25@gmail.com>
2022-04-30 11:37:58 -0400

    cs

--- common-lisp/character-study/character-study.lisp
+++ common-lisp/character-study/character-study.lisp
@@ -7,10 +7,32 @@
    :type-of-char))
 (in-package :character-study)
 
-(defun compare-chars (char1 char2))
+(defun compare-chars (ch1 ch2)
+ (cond
+  ((char= ch1 ch2) :equal-to)
+  ((char> ch1 ch2) :greater-than)
+  ((char< ch1 ch2) :less-than)
+  ))
 
-(defun size-of-char (char))
+(defun size-of-char (ch)
+ (cond
+  ((upper-case-p ch) :big)
+  ((lower-case-p ch) :small)
+  (t :no-size)
+  ))
 
-(defun change-size-of-char (char wanted-size))
+(defun change-size-of-char (ch wanted-size)
+ (cond
+  ((string= wanted-size :big) (char-upcase ch))
+  ((string= wanted-size :small) (char-downcase ch))
+  ))
 
-(defun type-of-char (char))
+(defun type-of-char (ch)
+ (cond
+  ((alpha-char-p ch) :alpha)
+  ((digit-char-p ch) :numeric)
+  ((char= ch #\Space) :space)
+  ((char= ch #\Newline) :newline)
+  (t :unknown)
+  )
+ )
