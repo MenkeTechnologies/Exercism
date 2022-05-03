1d655a877645b227464685b124c608a54f45ef37
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-03 16:02:23 -0400

    hs

---
+++ nim/high-scores/high_scores.nim
@@ -0,0 +1,8 @@
+import algorithm
+
+proc latest*(scores: openArray[int]): int = scores[^1]
+
+proc personalBest*(scores: openArray[int]): int = scores.max
+
+proc personalTopThree*(scores: openArray[int]): seq[int] = scores.sorted.reversed[0..min(2, scores.high)]
+
