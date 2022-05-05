5eaad6d9ff06a50105b64c96540a391c0c107669
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-05 10:32:29 -0400

    pangram

--- c/pangram/pangram.c
+++ c/pangram/pangram.c
@@ -1,1 +1,24 @@
 #include "pangram.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+bool is_pangram(const char *sentence) {
+
+    if (!sentence) {
+        return false;
+    }
+    char *lc = (char *) malloc(strlen(sentence) + 1);
+    strcpy(lc, sentence);
+    for (int i = 0; sentence[i]; i++) {
+        lc[i] = tolower(sentence[i]);
+    }
+
+    for (char i = 'a'; i <= 'z'; ++i) {
+        if (!strchr(lc, i)) {
+            return false;
+        }
+    }
+    return true;
+
+}
