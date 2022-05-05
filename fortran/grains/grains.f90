6e4bdc33760024339b89b65496067fbcba88c6f8
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-05 15:52:43 -0400

    grains

--- fortran/grains/grains.f90
+++ fortran/grains/grains.f90
@@ -8,10 +8,13 @@ contains
   double precision function square(n)
     integer :: n
     square = -1.d0
+    if (n > 0 .and. n <= 64) then
+        square = 2.d0 ** (n - 1)
+    end if
   end function
 
   double precision function total()
-    total = -1.d0
+    total = 2.d0 ** 64
   end function
 
 end module
