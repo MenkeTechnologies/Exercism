ad73f2ce77038a7909bc17497b77dfad66816ab7
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-07 11:05:46 -0400

    grains

--- prolog/grains/grains.pl
+++ prolog/grains/grains.pl
@@ -1,3 +1,8 @@
-square(SquareNumber, Value).
+square(SquareNumber, Value) :-
+    between(1, 64, SquareNumber),
+    Value is 2^(SquareNumber - 1).
 
-total(Value).
+total(Value) :-
+    numlist(1,64, Squares),
+    maplist(square, Squares, Grains),
+    foldl(plus, Grains, 0, Value).
