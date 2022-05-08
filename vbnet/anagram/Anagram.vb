ab9dcafad41b42aba14043577b0073802fb799d1
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-08 12:27:07 -0400

    ana

--- vbnet/anagram/Anagram.vb
+++ vbnet/anagram/Anagram.vb
@@ -1,3 +1,19 @@
 Public Class Anagram
+    Private ReadOnly _word As String
 
+    Public Sub New(word As String)
+        _word = word
+    End Sub
+
+    Public Function Match(candidates As String()) As IEnumerable(Of String)
+        Return candidates.Where(AddressOf IsAnagram).OrderBy(Function(w) w).ToArray()
+    End Function
+
+    Private Function IsAnagram(candidate As String) As Boolean
+        Return SortedLowerCase(candidate) = SortedLowerCase(_word) And candidate.ToLower() <> _word.ToLower()
+    End Function
+
+    Private Function SortedLowerCase(word As String) As String
+        Return new String(word.ToLower().OrderBy(Function(c)c).ToArray())
+    End Function
 End Class
--- vbnet/collatz-conjecture/CollatzConjecture.vb
+++ vbnet/collatz-conjecture/CollatzConjecture.vb
@@ -1,7 +1,13 @@
 Imports System
 
 Public Module CollatzConjecture
-    Public Function Steps(ByVal number As Integer) As Integer
-       Throw New NotImplementedException("Delete this statement and write your own implementation here")        
+    Public Function Steps(ByVal n As Integer) As Integer
+       if n < 1 then Throw new ArgumentOutOfRangeException
+        Dim cnt = 0
+        While n > 1
+            If n Mod  2 then n = 3 * n + 1 else n \= 2
+            cnt += 1
+        End While
+        Return cnt
     End Function
 End Module
