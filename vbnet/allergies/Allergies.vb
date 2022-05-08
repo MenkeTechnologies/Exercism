02b16c710aced8f37a44b90cccdd414478644b9e
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-08 12:14:30 -0400

    allergies

--- vbnet/allergies/Allergies.vb
+++ vbnet/allergies/Allergies.vb
@@ -1,3 +1,25 @@
 Public Class Allergies
+    Private ReadOnly score As Integer
+    Private Shared ReadOnly AvailableAllergies As New Dictionary(Of String, Integer) From {
+        {"eggs", 1},
+        {"peanuts", 2},
+        {"shellfish", 4},
+        {"strawberries", 8},
+        {"tomatoes", 16},
+        {"chocolate", 32},
+        {"pollen", 64},
+        {"cats", 128}
+        }
 
+    Public Sub New(score As Integer)
+        Me.score = score
+    End Sub
+
+    Public Function List() As List(Of String)
+        Return AvailableAllergies.Where(Function(it) AllergicTo(it.Key)).Select(Function(it) it.Key).ToList()
+    End Function
+
+    Public Function AllergicTo(allergen As String) As Boolean
+        Return (score and AvailableAllergies(allergen)) > 0
+    End Function
 End Class
