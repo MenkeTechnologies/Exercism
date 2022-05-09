586802d0b797b1a4e29b62fb3e248ffff3535378
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-09 00:10:17 -0400

    iso

--- dart/isogram/lib/isogram.dart
+++ dart/isogram/lib/isogram.dart
@@ -1,3 +1,6 @@
 class Isogram {
-  // Put your code here
-}
+  bool isIsogram(String s) {
+    String chars = s.toLowerCase().replaceAll(new RegExp(r"\W"), "");
+    return chars.split("").toSet().length == chars.length;
+  }
+  }
