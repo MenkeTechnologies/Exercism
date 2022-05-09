2e3e85eabc47a657bb197032924b5952c6cb611a
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-09 00:13:11 -0400

    darts

--- dart/darts/lib/darts.dart
+++ dart/darts/lib/darts.dart
@@ -1,3 +1,16 @@
+import 'dart:math';
+
 class Darts {
-  // Put your code here
+  int score(double x, double y) {
+    double distance = sqrt(x * x + y * y);
+
+    if (distance <= 1) {
+      return 10;
+    } else if (distance <= 5) {
+      return 5;
+    } else if (distance <= 10) {
+      return 1;
+    }
+    return 0;
+  }
 }
