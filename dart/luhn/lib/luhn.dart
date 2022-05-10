de23406897dd6b6facc716a8b8657d76f7983150
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-10 10:09:11 -0400

    luhn

--- dart/luhn/lib/luhn.dart
+++ dart/luhn/lib/luhn.dart
@@ -1,3 +1,22 @@
 class Luhn {
-  // Put your code here
+  bool valid(String value) {
+    String nows = value.replaceAll(new RegExp(r'\s'), '');
+    if (nows.length < 2 || nows.contains(new RegExp(r'\D'))) {
+      return false;
+    }
+
+    int sum = 0;
+
+    nows
+        .split('')
+        .reversed
+        .map(int.parse)
+        .toList()
+        .asMap()
+        .forEach((index, number) {
+      int dbl = index.isOdd ? number * 2 : number;
+      sum += dbl > 9 ? dbl - 9 : dbl;
+    });
+    return sum % 10 == 0;
+  }
 }
