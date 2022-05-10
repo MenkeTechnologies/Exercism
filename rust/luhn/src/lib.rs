87d9a96b7502049fa948c1a21451fb27f1069e9b
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-10 19:48:10 -0400

    cleaner luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,28 +1,20 @@
-/// Check a Luhn checksum.
-pub fn is_valid(code: &str) -> bool {
-    let invalid: Vec<char> = code.chars().filter(|ch| !(ch.is_whitespace() || ch.is_numeric())).collect();
+use regex::Regex;
 
-    if !invalid.is_empty() {
-        return false;
-    }
+pub fn is_valid(code: &str) -> bool {
     let no_ws: String = code.chars().filter(|ch| !ch.is_whitespace()).collect();
 
-    if no_ws.len() < 2 {
-        return false;
-    }
-
-    let sum = no_ws.chars().rev().map(|ch| ch.to_digit(10).unwrap()).enumerate().fold(0, |mut sum, (cnt, int)| {
-        if cnt % 2 == 1 {
-            let mut doubled = int * 2;
+    if no_ws.len() < 2 || Regex::new(r"\D").unwrap().is_match(no_ws.as_str()) {
+        false
+    } else {
+        no_ws.chars().rev().map(|ch| ch.to_digit(10).unwrap()).enumerate().map(|(i, n)| {
+            let mut doubled = n;
+            if i % 2 == 1 {
+                doubled *= 2
+            }
             if doubled > 9 {
                 doubled -= 9;
             }
-            sum += doubled
-        } else {
-            sum += int
-        }
-        sum
-    });
-
-    sum % 10 == 0
+            doubled
+        }).sum::<u32>() % 10 == 0
+    }
 }
