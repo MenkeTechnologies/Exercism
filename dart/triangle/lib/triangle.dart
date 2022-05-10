6b0558c626487096bb5c7386f7bf308581053f88
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-10 09:59:30 -0400

    tri

--- dart/triangle/lib/triangle.dart
+++ dart/triangle/lib/triangle.dart
@@ -1,3 +1,22 @@
 class Triangle {
-  // Put your code here
+  bool valid(List<double> sorted) {
+    sorted.sort();
+    return sorted.every((element) => element > 0) &&
+        sorted[0] + sorted[1] >= sorted[2];
+  }
+
+  bool equilateral(double a, double b, double c) {
+    List<double> list = [a, b, c];
+    return valid(list) && list.toSet().length == 1;
+  }
+
+  bool isosceles(double a, double b, double c) {
+    List<double> list = [a, b, c];
+    return valid(list) && list.toSet().length < 3;
+  }
+
+  bool scalene(double a, double b, double c) {
+    List<double> list = [a, b, c];
+    return valid(list) && list.toSet().length == 3;
+  }
 }
