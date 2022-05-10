3bc577d97250a5434b7f1945ef2001267442b4e5
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-10 09:32:31 -0400

    pan

--- dart/pangram/lib/pangram.dart
+++ dart/pangram/lib/pangram.dart
@@ -1,3 +1,5 @@
 class Pangram {
-  // Put your code here
+  bool isPangram(String sentence) =>
+      Iterable.generate(26, (i) => String.fromCharCode('A'.runes.first + i))
+          .every((element) => sentence.toUpperCase().contains(element));
 }
