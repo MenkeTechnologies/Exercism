91bfe8fd373b90851772ba7f79438582e1735b7f
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-14 13:35:49 -0400

    pan

--- scheme/pangram/pangram.scm
+++ scheme/pangram/pangram.scm
@@ -1,5 +1,18 @@
 (import (rnrs))
 
 (define (pangram? phrase)
-  'implement-me!)
+  (= 26 (length
+    (letters
+      (uniq (string->list
+             (string-downcase phrase)))))))
 
+(define (letters li)
+  (filter
+    (lambda (c) (char<=? #\a c #\z))
+    li))
+
+(define (uniq li)
+  (fold-left
+    (lambda (acc c) (if (member c acc) acc (cons c acc)))
+    '()
+    li))
