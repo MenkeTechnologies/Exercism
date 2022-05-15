f7c7930cc22ba2d908a70e406e590d4f3cb8265b
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-15 00:17:02 -0400

    rd

--- fsharp/raindrops/Raindrops.fs
+++ fsharp/raindrops/Raindrops.fs
@@ -1,3 +1,7 @@
 module Raindrops
 
-let convert (number: int): string = failwith "You need to implement this function."
+let convert (number: int): string =
+    let str = [(3,"Pling");(5,"Plang");(7,"Plong")]
+            |> Seq.map (fun (k, v) -> if number % k = 0 then v else "")
+            |> String.concat ""
+    if str <> "" then str else number.ToString()
