eefcb3f8a19392ab52d872a23d5a7ce0ea595a24
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-15 00:00:30 -0400

    rvs

--- fsharp/reverse-string/ReverseString.fs
+++ fsharp/reverse-string/ReverseString.fs
@@ -1,3 +1,3 @@
 module ReverseString
 
-let reverse (input: string): string = failwith "You need to implement this function."
+let reverse (input: string): string = Seq.rev input |> System.String.Concat
