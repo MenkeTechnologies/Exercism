ad1129635ca112feef2c1c41f7d6e263c668a7ee
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-15 00:09:10 -0400

    hamm

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,3 +1,8 @@
 module Hamming
 
-let distance (strand1: string) (strand2: string): int option = failwith "You need to implement this function."
+let distance (s1: string) (s2: string): int option =
+    if s1.Length = s2.Length then
+        (s1, s2) ||> Seq.map2(<>)
+        |> Seq.filter id
+        |> Seq.length |> Some
+    else None
