636ce21397f4e56bcb086dc5659fea113bd14795
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-15 00:03:01 -0400

    darts

--- fsharp/darts/Darts.fs
+++ fsharp/darts/Darts.fs
@@ -1,3 +1,9 @@
 module Darts
 
-let score (x: double) (y: double): int = failwith "You need to implement this function."
+open System
+let score (x: double) (y: double): int =
+    match sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) with
+    | r when r <= 1 -> 10
+    | r when r <= 5 -> 5
+    | r when r <= 10 -> 1
+    | _ -> 0
