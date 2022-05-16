b0dce568cbf1733b1c89b1e47ca97eeb0a0627fd
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-16 11:24:38 -0400

    iso

--- fsharp/isogram/Isogram.fs
+++ fsharp/isogram/Isogram.fs
@@ -1,3 +1,7 @@
 module Isogram
 
-let isIsogram str = failwith "You need to implement this function."
+open System
+let isIsogram str =
+    str |> Seq.filter Char.IsLetter
+    |> Seq.countBy Char.ToLower
+    |> Seq.forall (fun pair -> snd pair = 1)
