2a80e74e16c4b7989b4111e14bad95c47254e7a2
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-16 11:08:01 -0400

    ss

--- fsharp/scrabble-score/ScrabbleScore.fs
+++ fsharp/scrabble-score/ScrabbleScore.fs
@@ -1,3 +1,15 @@
 module ScrabbleScore
 
-let score word = failwith "You need to implement this function."
+open System
+
+let private letterToScore =
+    function
+    | 'D' | 'G' -> 2
+    | 'B' | 'C' | 'M' | 'P' -> 3
+    | 'F' | 'H' | 'V' | 'W' | 'Y' -> 4
+    | 'K' -> 5
+    | 'J' | 'X' -> 8
+    | 'Q' | 'Z' -> 10
+    | _ -> 1
+
+let score word = word |> Seq.map (fun c -> c |> Char.ToUpper |> letterToScore) |> Seq.sum
