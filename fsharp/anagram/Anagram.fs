af25f95a00dae839bb88c80ea259e663abf62988
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-17 09:58:46 -0400

    ana

--- fsharp/anagram/Anagram.fs
+++ fsharp/anagram/Anagram.fs
@@ -1,3 +1,12 @@
 module Anagram
 
-let findAnagrams sources target = failwith "You need to implement this function."
+let sort s = s |> Seq.sortBy id |> Seq.toList
+let lower (s: string) = s.ToLower()
+
+let findAnagrams candidates target =
+    let lc = lower target
+    let sorted = sort lc
+    
+    candidates
+    |> Seq.filter (fun (c) -> c |> lower |> sort = sorted && c |> lower <> lc)
+    |> Seq.toList
