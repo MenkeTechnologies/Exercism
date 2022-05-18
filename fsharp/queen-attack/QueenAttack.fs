0e6bc0f3fc20dab38cdf09375e368f37ac247807
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-18 12:10:06 -0400

    qa

--- fsharp/queen-attack/QueenAttack.fs
+++ fsharp/queen-attack/QueenAttack.fs
@@ -1,5 +1,14 @@
 module QueenAttack
 
-let create (position: int * int) = failwith "You need to implement this function."
+open System
 
-let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
+let valid x = x >= 0 && x <= 7
+
+let create (position: int * int) =
+    position |> fst |> valid
+    && position |> snd |> valid
+
+let canAttack (queen1: int * int) (queen2: int * int) =
+    let dx = fst queen1 - fst queen2 |> Math.Abs
+    let dy = snd queen1 - snd queen2 |> Math.Abs
+    dx = 0 || dy = 0 || dx = dy
