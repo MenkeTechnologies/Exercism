e26c8dd55c851fc0079b1aceefa63508c5b597f5
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-18 00:29:07 -0400

    trx

--- fsharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.fs
+++ fsharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.fs
@@ -1,14 +1,18 @@
 module TracksOnTracksOnTracks
 
-let newList: string list = failwith "Please implement the 'newList' value"
+let newList: string list = []
 
-let existingList: string list = failwith "Please implement the 'existingList' value"
+let existingList: string list = [ "F#"; "Clojure"; "Haskell" ]
 
-let addLanguage (language: string) (languages: string list): string list =
-    failwith "Please implement the 'addLanguage' function"
+let addLanguage (language: string) (languages: string list) : string list = language :: languages
 
-let countLanguages (languages: string list): int = failwith "Please implement the 'countLanguages' function"
+let countLanguages (languages: string list) : int = List.length languages
 
-let reverseList(languages: string list): string list = failwith "Please implement the 'reverseList' function"
+let reverseList (languages: string list) : string list = List.rev languages
 
-let excitingList (languages: string list): bool = failwith "Please implement the 'excitingList' function"
+let excitingList (languages: string list) : bool =
+    match languages with
+    | "F#" :: _ -> true
+    | [ _; "F#" ]
+    | [ _; "F#"; _ ] -> true
+    | _ -> false
