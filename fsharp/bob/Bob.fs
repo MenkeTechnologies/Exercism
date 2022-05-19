a6ac1d001cb574b7639969f250b699388b2c1286
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-19 00:21:10 -0400

    bob

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -1,4 +1,18 @@
 module Bob
 
+open System
+open System.Text.RegularExpressions
 
-let response (input: string): string = failwith "You need to implement this function."
+let response (s : string) =
+    let nows = s.ToCharArray() |> Array.filter (fun c -> Char.IsWhiteSpace c |> not) |> String.Concat
+    let isSilent = nows.Length = 0
+    let isShouting = nows.ToCharArray() |> Array.exists Char.IsLetter && nows.ToUpper() = nows
+    let isQuestion = nows.EndsWith("?")
+    
+    match (isSilent, isShouting, isQuestion) with
+    | true,    _,    _ -> "Fine. Be that way!"
+    |    _, true, true -> "Calm down, I know what I'm doing!"
+    |    _, true,    _ -> "Whoa, chill out!"
+    |    _,    _, true -> "Sure."
+    | _                  -> "Whatever."
+    
