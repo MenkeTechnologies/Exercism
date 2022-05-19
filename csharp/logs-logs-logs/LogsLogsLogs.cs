d59e04c5832bef52369f5b83f1c51ae752d0094a
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-19 15:02:46 -0400

    logs

--- csharp/logs-logs-logs/LogsLogsLogs.cs
+++ csharp/logs-logs-logs/LogsLogsLogs.cs
@@ -1,16 +1,29 @@
 using System;
 
-// TODO: define the 'LogLevel' enum
+enum LogLevel
+{
+    Unknown = 0,
+    Trace = 1,
+    Debug = 2,
+    Info = 4,
+    Warning = 5,
+    Error = 6,
+    Fatal = 42
+}
 
 static class LogLine
 {
-    public static LogLevel ParseLogLevel(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.ParseLogLevel() method");
-    }
+    public static LogLevel ParseLogLevel(string logLine) =>
+        logLine.Substring(1, 3) switch
+        {
+            "TRC" => LogLevel.Trace,
+            "DBG" => LogLevel.Debug,
+            "INF" => LogLevel.Info,
+            "WRN" => LogLevel.Warning,
+            "ERR" => LogLevel.Error,
+            "FTL" => LogLevel.Fatal,
+            _ => LogLevel.Unknown,
+        };
 
-    public static string OutputForShortLog(LogLevel logLevel, string message)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.OutputForShortLog() method");
-    }
+    public static string OutputForShortLog(LogLevel logLevel, string message) => $"{(int)logLevel}:{message}";
 }
