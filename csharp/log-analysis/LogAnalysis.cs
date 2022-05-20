1056762e249d07a0430d23ad7ad91a110b9d5bd6
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-20 09:40:08 -0400

    la

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -1,12 +1,9 @@
-using System;
+using System.Linq;
 
-public static class LogAnalysis 
+public static class LogAnalysis
 {
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
-
-    // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
-    // TODO: define the 'Message()' extension method on the `string` type
-
-    // TODO: define the 'LogLevel()' extension method on the `string` type
+    public static string SubstringAfter(this string s, string d) => s.Split(d).Last();
+    public static string SubstringBetween(this string s, string c1, string c2) => s.Split(c2).First().Split(c1).Last();
+    public static string Message(this string s) => s.SubstringAfter(": ");
+    public static string LogLevel(this string s) => s.SubstringBetween("[", "]");
 }
