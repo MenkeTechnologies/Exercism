3a15ae1f3d95121c790ffbdd4a55be1f6635581d
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-20 18:14:32 -0400

    wc

--- csharp/word-count/WordCount.cs
+++ csharp/word-count/WordCount.cs
@@ -1,10 +1,10 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class WordCount
 {
-    public static IDictionary<string, int> CountWords(string phrase)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static IDictionary<string, int> CountWords(string phrase) =>
+        Regex.Matches(phrase, @"\b['\w]+\b").Select(m => m.Value)
+            .GroupBy(w => w.ToLowerInvariant()).ToDictionary(g => g.Key, g => g.Count());
 }
