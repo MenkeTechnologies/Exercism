909e45e43a5bbd316d2a1881daaa5c3c1ce63b7e
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-20 10:17:55 -0400

    lo

--- fsharp/list-ops/ListOps.fs
+++ fsharp/list-ops/ListOps.fs
@@ -1,17 +1,17 @@
 module ListOps
 
-let rec foldl folder state list = failwith "You need to implement this function."
-
-let rec foldr folder state list = failwith "You need to implement this function."
-
-let length list = failwith "You need to implement this function."
-
-let reverse list = failwith "You need to implement this function."
-
-let map f list = failwith "You need to implement this function."
-
-let filter f list = failwith "You need to implement this function."
-
-let append xs ys = failwith "You need to implement this function."
-
-let concat xs = failwith "You need to implement this function."
+let rec foldl fn init list =
+    match list with
+    | [] -> init
+    | x::xs -> foldl fn (fn init x) xs
+let rec foldr fn init list =
+    list
+    |> List.rev
+    |> foldl (fun acc n -> fn n acc) init
+    
+let length list = foldl (fun acc _ -> acc + 1) 0 list
+let reverse list = foldl (fun acc n -> n :: acc) [] list
+let map f list = foldr (fun n acc -> f n :: acc) [] list
+let filter f list = foldr (fun n acc -> if f n then n :: acc else acc) [] list
+let append xs ys = foldr (fun n acc -> n :: acc) ys xs
+let concat xs = foldr append [] xs
