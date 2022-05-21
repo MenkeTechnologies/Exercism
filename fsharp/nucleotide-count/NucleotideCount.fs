d480f4d090484b21ceb6aaf85e5795fe4b8da197
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-21 02:59:09 -0400

    nt

--- fsharp/nucleotide-count/NucleotideCount.fs
+++ fsharp/nucleotide-count/NucleotideCount.fs
@@ -1,3 +1,9 @@
 module NucleotideCount
 
-let nucleotideCounts (strand: string): Option<Map<char, int>> =  failwith "You need to implement this function."
+let nucleotideCounts (strand: string): Option<Map<char, int>> =
+    let init = [('A',0);('C',0);('G',0);('T',0)] |> Map.ofList
+    if strand |> Seq.forall (fun c -> Seq.contains c "ATCG") then
+        strand |> Seq.fold (fun acc n -> Map.add n (acc[n] + 1) acc) init
+        |> Some
+    else
+        None
