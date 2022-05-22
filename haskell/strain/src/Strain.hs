aa56228992acb88972cf7e47eb3c7d8c03d561cb
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-22 00:24:32 -0400

    strain

--- haskell/strain/src/Strain.hs
+++ haskell/strain/src/Strain.hs
@@ -1,7 +1,8 @@
 module Strain (keep, discard) where
 
-discard :: (a -> Bool) -> [a] -> [a]
-discard p xs = error "You need to implement this function."
-
 keep :: (a -> Bool) -> [a] -> [a]
-keep p xs = error "You need to implement this function."
+keep p = foldr(\n -> if p n then (n:) else id) []
+
+discard :: (a -> Bool) -> [a] -> [a]
+discard p = keep (not . p)
+
