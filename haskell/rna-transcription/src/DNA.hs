cf76866962f1c6a70be8a4a8943eb3120b59bc80
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-23 12:57:18 -0400

    dna

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,4 +1,12 @@
+{-# LANGUAGE LambdaCase #-}
+
 module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA = traverse $ \case
+  'G' -> Right 'C'
+  'C' -> Right 'G'
+  'T' -> Right 'A'
+  'A' -> Right 'U'
+  c -> Left c
+
