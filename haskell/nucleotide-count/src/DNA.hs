94f02339c53ef96ab5e4813d079b9cca8ffb8f43
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-23 13:26:57 -0400

    nt

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,8 +1,16 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
-import Data.Map (Map)
+import Data.Map
+import Data.Char
 
-data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Read)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts xs
+  | isValid xs = Right $ count xs
+  | otherwise = Left xs
+  where
+    isValid = all (`elem` "ACGT")
+count :: String -> Map Nucleotide Int
+count xs = fromListWith (+) [(read [toUpper c], 1) | c <- xs]
+
