da83cfc16e6fb30d80cb2c188c5c04d19f8b286d
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-23 12:51:02 -0400

    acro

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,4 +1,14 @@
 module Acronym (abbreviate) where
 
+
+import Data.Char
 abbreviate :: String -> String
-abbreviate xs = error "You need to implement this function."
+abbreviate = map (toUpper . head) . words . wordify
+
+wordify :: String -> String
+wordify (x : y : xs)
+  | isLower x && isUpper y = x : ' ' : y : wordify xs
+wordify ('_' : xs) = ' ' : xs
+wordify ('-' : xs) = ' ' : xs
+wordify (x : xs) = x : wordify xs
+wordify [] = []
