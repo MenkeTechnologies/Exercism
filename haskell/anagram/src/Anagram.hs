adab47ddd1977a395a016983f2e2a88965d62b45
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-23 13:30:53 -0400

    ana

--- haskell/anagram/src/Anagram.hs
+++ haskell/anagram/src/Anagram.hs
@@ -1,4 +1,15 @@
 module Anagram (anagramsFor) where
 
+import Data.List (sort)
+import Data.Char (toLower)
+
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = error "You need to implement this function."
+anagramsFor a = filter (anagram a)
+
+anagram :: [Char] -> [Char] -> Bool
+anagram s1 s2 = lc1 /= lc2 && sort lc1 == sort lc2
+    where
+        lc1 = map toLower s1
+        lc2 = map toLower s2
+
+
