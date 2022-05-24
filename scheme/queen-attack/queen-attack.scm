42e9116e15ebf5d0dff245f49d8eda0fbf992cdb
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-24 08:45:21 -0400

    qa

--- scheme/queen-attack/queen-attack.scm
+++ scheme/queen-attack/queen-attack.scm
@@ -1,5 +1,20 @@
 (import (rnrs))
 
 (define (attacking? white black)
-  'implement-me!)
+  (let* (
+    (x1 (car white))
+    (y1 (cadr white))
+    (x2 (car black))
+    (y2 (cadr black))
+    (dx (abs (- x1 x2)))
+    (dy (abs (- y1 y2)))
+    )
+   (cond
+    ((= dx dy) #t)
+    ((= dx 0) #t)
+    ((= dy 0) #t)
+    (#t #f)
+    )
+   )
+ )
 
