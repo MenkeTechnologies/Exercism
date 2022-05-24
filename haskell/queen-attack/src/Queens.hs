2bd4ebef45f8368cccef6e617dffc1a690921abb
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-24 08:14:15 -0400

    qa

--- haskell/queen-attack/src/Queens.hs
+++ haskell/queen-attack/src/Queens.hs
@@ -1,7 +1,16 @@
 module Queens (boardString, canAttack) where
 
 boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
-boardString white black = error "You need to implement this function."
+boardString white black = unlines
+    [ unwords [ board i j | j <- [0 .. 7] ] | i <- [0 .. 7] ]
+  where
+    board i j | Just (i, j) == white = "W"
+              | Just (i, j) == black = "B"
+              | otherwise            = "_"
 
 canAttack :: (Int, Int) -> (Int, Int) -> Bool
-canAttack queenA queenB = error "You need to implement this function."
+canAttack (x1, y1) (x2, y2) =
+        dx == 0 || dy == 0 || dx == dy
+    where
+        dx = abs $ x1 - x2
+        dy = abs $ y1 - y2
