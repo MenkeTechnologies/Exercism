eb4cf29fa213ac7318aeda553517e9d9516d4a6e
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-27 00:37:06 -0400

    kg

--- fsharp/kindergarten-garden/KindergartenGarden.fs
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -1,5 +1,11 @@
 module KindergartenGarden
 
-// TODO: define the Plant type
+type Plant = | Violets | Clover | Grass | Radishes | Unknown
+let children = [ "Alice"; "Bob"; "Charlie"; "David"; "Eve"; "Fred"; "Ginny"; "Harriet"; "Ileana"; "Joseph"; "Kincaid"; "Larry" ]
 
-let plants diagram student = failwith "You need to implement this function."
+let plants (diagram: string) student =
+    let i = children |> List.findIndex(fun n -> n = student)
+    diagram.Split('\n')
+        |> Seq.collect (Seq.skip (i * 2) >> Seq.take 2)
+        |> Seq.map (function | 'C' -> Clover | 'G' -> Grass | 'R' -> Radishes | 'V' -> Violets | _ -> Unknown)
+        |> List.ofSeq
