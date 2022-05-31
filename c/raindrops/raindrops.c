5ae4d481498dd0ec460fff68307213ae51b6a0ca
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-31 09:26:25 -0400

    rd

--- c/raindrops/raindrops.c
+++ c/raindrops/raindrops.c
@@ -1,1 +1,22 @@
 #include "raindrops.h"
+#include <stdio.h>
+#include <string.h>
+
+static const RainDrop dropMap[] = {
+        {3, "Pling"},
+        {5, "Plang"},
+        {7, "Plong"}
+};
+const int DROP_COUNT = sizeof(dropMap) / sizeof(RainDrop);
+
+
+char *convert(char result[], int drops) {
+
+    for (int i = 0; i < DROP_COUNT; i++) {
+        if (!(drops % dropMap[i].divisor))
+            strncat(result, dropMap[i].dropName, 5);
+    }
+    if (!*result) sprintf(result, "%d", drops);
+    return result;
+
+}
--- c/raindrops/raindrops.h
+++ c/raindrops/raindrops.h
@@ -1,6 +1,11 @@
 #ifndef RAINDROPS_H
 #define RAINDROPS_H
 
+typedef struct RainDrop{
+    int divisor;
+    char dropName[5];
+} RainDrop;
+
 char *convert(char result[], int drops);
 
 #endif
