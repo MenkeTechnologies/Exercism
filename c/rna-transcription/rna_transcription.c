3bdf2628b8e279896d9b51bfb0b70a7136dfc38c
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-31 09:39:03 -0400

    rna

--- c/rna-transcription/rna_transcription.c
+++ c/rna-transcription/rna_transcription.c
@@ -1,1 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
 #include "rna_transcription.h"
+
+char convert_nucleotide(char nuc) {
+    switch (nuc) {
+        case 'A':
+            return 'U';
+        case 'C':
+            return 'G';
+        case 'G':
+            return 'C';
+        case 'T':
+            return 'A';
+        default:
+            return '\0';
+    }
+}
+
+char *to_rna(const char *dna) {
+    size_t len = strlen(dna) + 1;
+    char *rna = malloc(len);
+    memset(rna, '\0', len);
+    for (int i = 0; dna[i]; ++i) {
+        rna[i] = convert_nucleotide(dna[i]);
+        if (!rna[i]) {
+            free(rna);
+            return NULL;
+        }
+    }
+    return rna;
+
+}
--- c/rna-transcription/rna_transcription.h
+++ c/rna-transcription/rna_transcription.h
@@ -1,6 +1,8 @@
 #ifndef RNA_TRANSCRIPTION_H
 #define RNA_TRANSCRIPTION_H
 
+char convert_nucleotide(char nuc);
+
 char *to_rna(const char *dna);
 
 #endif
