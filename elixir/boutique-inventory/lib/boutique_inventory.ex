da2315a8401e5d5796f7da202f3585a14013ca0f
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-01 10:12:08 -0400

    boutique

--- elixir/boutique-inventory/lib/boutique_inventory.ex
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -1,17 +1,30 @@
 defmodule BoutiqueInventory do
   def sort_by_price(inventory) do
-    # Please implement the sort_by_price/1 function
+    Enum.sort_by(inventory, & &1.price)
   end
 
   def with_missing_price(inventory) do
-    # Please implement the with_missing_price/1 function
+    Enum.filter(inventory, &(!&1.price))
+  end
+
+  def update_names(inventory, old_word, new_word) do
+    Enum.map(inventory, fn n ->
+      new_name = String.replace(n.name, old_word, new_word)
+      %{n | name: new_name}
+    end)
   end
 
   def increase_quantity(item, count) do
-    # Please implement the increase_quantity/2 function
+    new_sizes =
+      Enum.reduce(item.quantity_by_size, %{}, fn {size, n}, acc ->
+        Map.put(acc, size, n + count)
+      end)
+    Map.put(item, :quantity_by_size, new_sizes)
   end
 
-  def total_quantity(item) do
-    # Please implement the total_quantity/1 function
+  def total_quantity(%{quantity_by_size: sizes}) do
+    Enum.reduce(sizes, 0, fn {_, n}, acc ->
+      n + acc
+    end)
   end
 end
