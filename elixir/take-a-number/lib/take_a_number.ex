9d36400647ea08acc42c372cc96d983ab4ed6fd9
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-01 09:44:59 -0400

    take

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,19 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(fn -> loop(0) end)
+  end
+
+  def loop(state) do
+    receive do
+      {:report_state, pid} ->
+        send(pid, state)
+        loop(state)
+      {:take_a_number, pid} ->
+        send(pid, state + 1)
+        loop(state + 1)
+      :stop -> :ok
+      _ -> loop(state)
+       
+    end
   end
 end
