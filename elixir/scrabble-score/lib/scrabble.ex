35b13d3d2deee650890365cbd3a52727345a5e4a
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-02 09:16:33 -0400

    scrabble

--- elixir/scrabble-score/lib/scrabble.ex
+++ elixir/scrabble-score/lib/scrabble.ex
@@ -1,8 +1,16 @@
 defmodule Scrabble do
-  @doc """
-  Calculate the scrabble score for the word.
-  """
+  @scores %{"e"=> 1, "a"=> 1, "i"=> 1, "o"=> 1, "n"=> 1, "r"=> 1,  "t"=> 1, "l"=> 1, "s"=> 1, "u"=> 1,
+    "d"=>2, "g"=>2,
+    "b"=>3, "c"=>3, "m"=>3, "p"=>3,
+    "f"=>4, "h"=>4, "v"=>4, "w"=>4, "y"=>4,
+    "k"=>5,
+    "j"=>8, "x"=>8,
+    "q"=>10, "z"=>10}
+  
   @spec score(String.t()) :: non_neg_integer
   def score(word) do
+    word |> String.downcase |> String.graphemes
+    |> Enum.map(&(Map.get(@scores, &1, 0)))
+    |> Enum.sum
   end
 end
