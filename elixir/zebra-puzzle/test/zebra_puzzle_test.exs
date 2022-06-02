f2285ce131d7a1fe9f5baeeb550b6fe7c3730a36
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-02 08:55:44 -0400

    redownload elixir

---
+++ elixir/zebra-puzzle/test/zebra_puzzle_test.exs
@@ -0,0 +1,13 @@
+defmodule ZebraPuzzleTest do
+  use ExUnit.Case
+
+  # 
+  test "resident who drinks water" do
+    assert ZebraPuzzle.drinks_water() == :norwegian
+  end
+
+  
+  test "resident who owns zebra" do
+    assert ZebraPuzzle.owns_zebra() == :japanese
+  end
+end
