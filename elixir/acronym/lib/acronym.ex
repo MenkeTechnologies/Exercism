1ddced4425168c056c8dced657f776ae5b752370
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-04 01:17:13 -0400

    acro

--- elixir/acronym/lib/acronym.ex
+++ elixir/acronym/lib/acronym.ex
@@ -1,9 +1,9 @@
 defmodule Acronym do
-  @doc """
-  Generate an acronym from a string.
-  "This is a string" => "TIAS"
-  """
-  @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    string
+    |> String.split( ~r/[\s-_]+/, trim: true)
+    |> Enum.map(&String.first/1)
+    |> Enum.join("")
+    |> String.upcase()
   end
 end
