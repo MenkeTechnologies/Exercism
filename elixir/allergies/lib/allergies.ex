1e8351a9b37f30dde9bd9ba987d9fb05127e6d41
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-04 00:43:36 -0400

    all

--- elixir/allergies/lib/allergies.ex
+++ elixir/allergies/lib/allergies.ex
@@ -1,15 +1,24 @@
 defmodule Allergies do
-  @doc """
-  List the allergies for which the corresponding flag bit is true.
-  """
-  @spec list(non_neg_integer) :: [String.t()]
+  use Bitwise
+
+  @allergies_bits %{
+    "eggs" => 1,
+    "peanuts" => 2,
+    "shellfish" => 4,
+    "strawberries" => 8,
+    "tomatoes" => 16,
+    "chocolate" => 32,
+    "pollen" => 64,
+    "cats" => 128
+  }
+
   def list(flags) do
+    @allergies_bits
+    |> Map.keys
+    |> Enum.filter(&allergic_to?(flags, &1))
   end
 
-  @doc """
-  Returns whether the corresponding flag bit in 'flags' is set for the item.
-  """
-  @spec allergic_to?(non_neg_integer, String.t()) :: boolean
   def allergic_to?(flags, item) do
+    (@allergies_bits[item] &&& flags) > 0
   end
 end
