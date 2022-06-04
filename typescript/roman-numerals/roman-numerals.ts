ae001cdc4a6f1e7990e80a74ec14412cb2f5c2ac
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-03 21:30:12 -0400

    rn

--- typescript/roman-numerals/roman-numerals.ts
+++ typescript/roman-numerals/roman-numerals.ts
@@ -1,3 +1,24 @@
-export const toRoman = () => {
-  throw new Error('Remove this statement and implement this function')
-}
+const ROMAN_NUMERALS: [number, string][] = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+];
+export const toRoman = (n: number) => {
+    let acc = '';
+    for (const [val, s] of ROMAN_NUMERALS) {
+        acc += s.repeat(n / val)
+        n %= val;
+    }
+
+    return acc;
+};
