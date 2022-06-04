5853c0389d0fd85000043b338fe556e122633db8
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-04 11:38:17 -0400

    rn

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,7 +1,14 @@
-=begin
-Write your code for the 'Roman Numerals' exercise in this file. Make the tests in
-`roman_numerals_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/roman-numerals` directory.
-=end
+class Integer
+  def to_roman
+    roman_hash = { 1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD',
+                   100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL',
+                   10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV',
+                   1 => 'I' }
+    num = self
+    roman_hash.inject "" do |acc, (n, s)|
+      acc <<= s * (num / n)
+      num %= n
+      acc
+    end
+  end
+end
