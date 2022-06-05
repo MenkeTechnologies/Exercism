f4392e121037ca9380c61961fd41bf07acc5eafe
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-05 12:07:16 -0400

    secret

--- cpp/secret-handshake/secret_handshake.cpp
+++ cpp/secret-handshake/secret_handshake.cpp
@@ -1,5 +1,15 @@
 #include "secret_handshake.h"
 
-namespace secret_handshake {
+#include <algorithm>
 
-}  // namespace secret_handshake
+namespace secret_handshake {
+    vector <string> commands(int n) {
+        vector<string> r;
+        if (n & 1) r.emplace_back("wink");
+        if (n & 2) r.emplace_back("double blink");
+        if (n & 4) r.emplace_back("close your eyes");
+        if (n & 8) r.emplace_back("jump");
+        if (n & 16) reverse(r.begin(), r.end());
+        return r;
+    }
+}
--- cpp/secret-handshake/secret_handshake.h
+++ cpp/secret-handshake/secret_handshake.h
@@ -1,8 +1,13 @@
 #if !defined(SECRET_HANDSHAKE_H)
 #define SECRET_HANDSHAKE_H
 
+#include <vector>
+#include <string>
+
+using namespace std;
+
 namespace secret_handshake {
+    vector<string> commands(int n);
+}
 
-}  // namespace secret_handshake
-
-#endif // SECRET_HANDSHAKE_H
+#endif
