d58f6e7e0de7b9b797fd85a64443943dd431108c
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-06 09:10:09 -0400

    roman

--- python/roman-numerals/roman_numerals.py
+++ python/roman-numerals/roman_numerals.py
@@ -1,3 +1,21 @@
-def roman(number):
-    pass
-
+VALUE_SYMBOLS = [
+    (1000, 'M'),
+    (900, 'CM'),
+    (500, 'D'),
+    (400, 'CD'),
+    (100, 'C'),
+    (90, 'XC'),
+    (50, 'L'),
+    (40, 'XL'),
+    (10, 'X'),
+    (9, 'IX'),
+    (5, 'V'),
+    (4, 'IV'),
+    (1, 'I')
+]
+def roman(n):
+    r = ''
+    for value, symbol in VALUE_SYMBOLS:
+        r += symbol * (n // value)
+        n %= value
+    return r
