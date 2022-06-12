930124ca48cc1c1659be20b2e9a912dcdbf544e8
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-12 13:05:45 -0400

    sa

--- lua/space-age/space-age.lua
+++ lua/space-age/space-age.lua
@@ -1,3 +1,21 @@
-local SpaceAge = {}
+local space_age = {}
 
-return SpaceAge
+local SPY = 31557600
+
+function space_age:new(seconds)
+  local function f(earth_year)
+    return function() return tonumber(string.format("%.2f", seconds/ SPY / earth_year)) end
+  end
+  return {
+    seconds = seconds,
+    on_earth = f(1),
+    on_mercury = f(0.2408467),
+    on_venus = f(0.61519726),
+    on_mars =  f(1.8808158),
+    on_jupiter = f(11.862615),
+    on_saturn = f(29.447498),
+    on_uranus = f(84.016846),
+    on_neptune = f(164.79132)
+  }
+end
+return space_age
