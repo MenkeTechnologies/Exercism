f80d6936d2ec62edc9691eed74e05e62491ed4ac
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-14 13:10:52 -0400

    rd

--- lua/raindrops/raindrops.lua
+++ lua/raindrops/raindrops.lua
@@ -1,3 +1,16 @@
-return function(n)
+local rainspeak = {
+    { 3, 'Pling' },
+    { 5, 'Plang' },
+    { 7, 'Plong' }
+}
 
+return function(number)
+    local r = ''
+    for _, e in ipairs(rainspeak) do
+        local prime, word = table.unpack(e)
+        if number % prime == 0 then
+            r = r .. word
+        end
+    end
+    return #r > 0 and r or tostring(number)
 end
