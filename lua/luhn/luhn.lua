8690675cb2dfdeea3fb679c2464fd9c43c6c4541
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-15 00:58:33 -0400

    luhn

--- lua/luhn/luhn.lua
+++ lua/luhn/luhn.lua
@@ -1,5 +1,24 @@
-return {
-  valid = function(s)
+local luhn = {}
 
-  end
-}
+function luhn.valid(number_text)
+    local nows = number_text.gsub(number_text, " ", "")
+
+    if #nows <= 1 or nows:find("%D") then
+        return false
+    else
+        local sum = 0
+        for index = #nows, 1, -1 do
+            local digit = tonumber(nows:sub(index, index))
+            if index % 2 == 0 then
+                digit = digit * 2
+                if digit > 9 then
+                    digit = digit - 9
+                end
+            end
+            sum = sum + digit
+        end
+        return sum % 10 == 0
+    end
+end
+
+return luhn
