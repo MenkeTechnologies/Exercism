945c88a03e84969afad722f314a51574fb828955
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-16 12:33:44 -0400

    bob

--- r/bob/bob.R
+++ r/bob/bob.R
@@ -1,3 +1,14 @@
-bob <- function(input) {
+bob = function(input) {
+  nows = trimws(input)
+  has_letters = any(strsplit(nows , "", fixed=TRUE)[[1]] %in% c(letters, LETTERS))
+  yelling = nows == toupper(nows) && has_letters
+  question = substr(nows , start=nchar(nows), stop=nchar(nows)) == "?"
 
+  if (nows == "") return("Fine. Be that way!")
+  if (question && !yelling) return("Sure.")
+  if (question && yelling) return("Calm down, I know what I'm doing!")
+  if (yelling) return("Whoa, chill out!")
+
+  return("Whatever.")
 }
+
