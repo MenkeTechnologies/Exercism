76666dab72d059f23a6158671631bf9a8619cef5
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-16 01:15:20 -0400

    luhn

--- r/luhn/luhn.R
+++ r/luhn/luhn.R
@@ -1,18 +1,14 @@
 # Determine whether the number is valid.
 is_valid <- function(input) {
-  
   nows = gsub(" ", "", input)
-  
-  if (nchar(nows) < 2 || grepl("\\D", nows)){
+  if (nchar(nows) < 2 || grepl(r"{\D}", nows)){
     return(FALSE)
   }
-  
   i = 0; sum = 0
   for (c in as.integer(rev(strsplit(nows,"")[[1]]))){
     if (i %% 2 == 1) c = c * 2
     sum = sum + ifelse(c > 9, c - 9, c)
     i = i + 1
   }
-  
   sum %% 10 == 0
 }
