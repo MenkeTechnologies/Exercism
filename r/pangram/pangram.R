a4e9f89f7344b7fbd0e40e037469c5dc0350d847
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-16 12:25:17 -0400

    pan

--- r/pangram/pangram.R
+++ r/pangram/pangram.R
@@ -1,3 +1,5 @@
-is_pangram <- function(input) {
-
+is_pangram = function(input) {
+    letters = gsub("[^a-z]", "", tolower(input))
+    frequency_table = table(unlist(strsplit(letters , "")))
+    length(frequency_table) == 26
 }
