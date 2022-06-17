f0c5eb35e9384d0e9a4adc1b215ed3fc9c130e4b
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-17 11:06:54 -0400

    tri

--- r/triangle/triangle.R
+++ r/triangle/triangle.R
@@ -1,3 +1,11 @@
-triangle <- function(x, y, z) {
-  
+
+lst_class <- list(c("equilateral", "isosceles"), "isosceles", "scalene")
+
+triangle = function(x, y, z) {
+  sorted = sort(c(x, y, z))
+
+  stopifnot(sorted[1] > 0 && sorted[1] + sorted[2] >= sorted[3])
+
+  class(sorted) = lst_class[[length(unique(sorted))]]
+  sorted
 }
