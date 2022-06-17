5b7ec220fdcaa6b028a73b321956c14a21935d38
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-17 11:22:49 -0400

    pf

--- r/prime-factors/prime-factors.R
+++ r/prime-factors/prime-factors.R
@@ -1,3 +1,14 @@
-prime_factors <- function(number) {
-  
+prime_factors = function(n) {
+  factors = c()
+  cur_prime = 2
+
+  while (n > 1) {
+    if (n %% cur_prime == 0) {
+      n = n / cur_prime
+      factors[length(factors) + 1] = cur_prime
+    } else {
+      cur_prime = cur_prime + 1
+    }
+  }
+  factors
 }
