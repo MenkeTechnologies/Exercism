503c326a67f97d32e9e19145b087e57278bdafa7
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-18 18:20:01 -0400

    ana

--- scala/anagram/src/main/scala/Anagram.scala
+++ scala/anagram/src/main/scala/Anagram.scala
@@ -1,3 +1,5 @@
 object Anagram {
-  def findAnagrams(str: String, candidates: List[String]) = candidates.filter(c => c.toLowerCase != str.toLowerCase && c.toLowerCase.sorted == str.toLowerCase.sorted)
+  def findAnagrams(str: String, candidates: List[String]) = 
+    candidates.filter(c => c.toLowerCase != str.toLowerCase && 
+      c.toLowerCase.sorted == str.toLowerCase.sorted)
 }
