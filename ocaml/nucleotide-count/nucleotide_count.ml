691a04591ab921341302117731e7782375feb06a
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-18 14:57:58 -0400

    nt

--- ocaml/nucleotide-count/nucleotide_count.ml
+++ ocaml/nucleotide-count/nucleotide_count.ml
@@ -2,8 +2,18 @@ open Base
 
 let empty = Map.empty (module Char)
 
+let is_valid_char = function 'A' | 'C' | 'G' | 'T' -> true | _ -> false
+
 let count_nucleotide s c =
-  failwith "'count_nucleotide' is missing"
+  if is_valid_char c then
+    String.fold_result s ~init:0 ~f:(fun count -> function
+      | char when Char.equal c char -> Ok (count + 1)
+      | char when is_valid_char char -> Ok count
+      | char -> Error char)
+  else Error c
 
 let count_nucleotides s =
-  failwith "'count_nucleotides' is missing"
+  String.fold_result s ~init:empty ~f:(fun acc c ->
+      if is_valid_char c then
+        Ok (Map.update acc c ~f:(function None -> 1 | Some n -> n + 1))
+      else Error c)
--- ruby/nucleotide-count/nucleotide_count.rb
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -9,8 +9,8 @@ To get started with TDD, see the `README.md` file in your
 class Nucleotide
 
   def initialize(str)
-    @h = { 'A' => 0, 'C' => 0, 'G' => 0, 'T' => 0 }
     throw ArgumentError if str !~ /^[AGCT]*$/
+    @h = { 'A' => 0, 'C' => 0, 'G' => 0, 'T' => 0 }
     @h.merge!(str.chars.group_by { _1 }.map { [_1, _2.size] }.to_h)
   end
 
