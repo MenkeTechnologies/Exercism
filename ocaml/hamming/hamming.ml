1b1318f7a42507c5ff3e9ae161d1269cc5f63b64
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-18 13:28:42 -0400

    hamm

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -1,4 +1,12 @@
+open List
+
 type nucleotide = A | C | G | T
 
-let hamming_distance _ _ =
-  failwith "'hamming_distance' is missing"
+let combine a b c = a + (if b = c then 0 else 1)
+
+let hamming_distance alpha beta = match length alpha, length beta with
+    | (x, y) when x = y -> Ok (fold_left2 combine 0 alpha beta)
+    | (0, _) -> Error "left strand must not be empty"
+    | (_, 0) -> Error "right strand must not be empty"
+    | (_, _) -> Error "left and right strands must be of equal length"
+
