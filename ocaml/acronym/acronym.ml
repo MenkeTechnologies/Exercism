d7672665ae8664bfd4c993517f11cb89236e6d64
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-19 13:55:23 -0400

    acro

--- ocaml/acronym/acronym.ml
+++ ocaml/acronym/acronym.ml
@@ -1,1 +1,7 @@
-let acronym _ = failwith "'acronym' is missing"
+open Base
+
+let acronym phrase = phrase
+    |> String.split_on_chars ~on:[' '; '_'; '-']
+    |> List.filter ~f:(fun s -> String.length s > 0)
+    |> List.map ~f:(fun s -> Char.uppercase (String.get s 0))
+    |> String.of_char_list
