cfbd78fd6d9155d2685ac900439cc5b9af153310
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-19 14:30:43 -0400

    bob

--- ocaml/bob/bob.ml
+++ ocaml/bob/bob.ml
@@ -1,2 +1,12 @@
-let response_for _ =
-    failwith "'response_for' is missing"
+open Base
+open String
+
+let response_for s =
+  let s = s |> strip in
+  let has_alpha s = s |> exists ~f:Char.is_alpha in
+  if is_empty s then "Fine. Be that way!"
+  else if has_alpha s && uppercase s = s then
+    if s |> is_suffix ~suffix:"?" then "Calm down, I know what I'm doing!"
+    else "Whoa, chill out!"
+  else if s |> is_suffix ~suffix:"?" then "Sure."
+  else "Whatever."
