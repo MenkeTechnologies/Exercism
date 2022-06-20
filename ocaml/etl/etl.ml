25ef41a3cfb7f2b3077d638f37714723bbef981b
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-20 06:15:40 -0400

    etl

--- ocaml/etl/etl.ml
+++ ocaml/etl/etl.ml
@@ -1,2 +1,7 @@
-let transform _ =
-    failwith "'transform' is missing"
+let transform_entry (score, letters) = letters |>
+  List.map (fun c -> Char.lowercase_ascii c, score)
+
+let transform lst = lst |>
+  List.map transform_entry |>
+  List.concat |>
+  List.sort (fun a b -> Char.compare (fst a) (fst b))
