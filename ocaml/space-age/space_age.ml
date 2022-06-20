3836628d4c19cc61ee439644941f817b4f84adea
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-20 06:19:26 -0400

    sa

--- ocaml/space-age/space_age.ml
+++ ocaml/space-age/space_age.ml
@@ -1,5 +1,14 @@
 type planet = Mercury | Venus | Earth | Mars
             | Jupiter | Saturn | Neptune | Uranus
 
-let age_on _ _ =
-    failwith "'age_on' is missing"
+let age_on planet age =
+  float_of_int age /. 31557600. /.
+    match planet with
+    | Mercury ->   0.2408467
+    | Venus ->     0.61519726
+    | Earth ->     1.
+    | Mars ->      1.8808158
+    | Jupiter ->   11.862615
+    | Saturn ->    29.447498
+    | Neptune ->   164.79132
+    | Uranus ->    84.016846
