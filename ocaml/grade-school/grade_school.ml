b6db423c6afcc32c882176e4422b00c4a3c3de11
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-21 09:51:18 -0400

    gs

--- ocaml/grade-school/grade_school.ml
+++ ocaml/grade-school/grade_school.ml
@@ -1,18 +1,21 @@
 open Base
 
 module Int_map = Map.M(Int)
+
 type school = string list Int_map.t
 
 let empty_school = Map.empty (module Int)
 
-let add _ _ _ =
-    failwith "'add' is missing"
+let grade gr roster =
+  match Map.find roster gr with
+      | Some names -> names
+      | None -> []
 
-let grade _ _ =
-    failwith "'grade' is missing"
+let add name gr roster =
+  roster |> Map.set ~key:gr ~data:(name::(grade gr roster))
 
-let sorted _ =
-    failwith "'sorted' is missing"
+let sorted roster =
+  roster |> Map.map ~f:(List.sort ~compare:(String.compare))
 
-let roster _ =
-    failwith "'roster' is missing"
+let roster roster = roster |> sorted |> Map.data |> List.concat
+
