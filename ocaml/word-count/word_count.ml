763e1b2a5de00462257da0b5d8713be4bf9baf83
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-21 10:13:07 -0400

    wc

--- ocaml/word-count/word_count.ml
+++ ocaml/word-count/word_count.ml
@@ -1,2 +1,11 @@
-let word_count _ =
-    failwith "'word_count' is missing"
+open Base
+open String
+open Char
+
+let word_count s = s
+  |> map ~f:(fun c -> if is_alphanum c || equal '\'' c then lowercase c else ' ')
+  |> split ~on:' '
+  |> List.map ~f:(strip ~drop:(equal '\''))
+  |> List.filter ~f:(Fn.non is_empty)
+  |> List.fold ~init:(Map.empty (module String))
+       ~f:(Map.update ~f:(function None -> 1 | Some n -> n + 1))
