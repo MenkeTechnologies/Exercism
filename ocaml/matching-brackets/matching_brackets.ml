baea793f1636a10e52e53f70b1cff0f2b25a8c14
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-21 10:03:14 -0400

    mb

--- ocaml/matching-brackets/matching_brackets.ml
+++ ocaml/matching-brackets/matching_brackets.ml
@@ -1,2 +1,15 @@
-let are_balanced _ =
-    failwith "'are_balanced' is missing"
+let are_balanced s =
+  let bracks = Stack.create() in
+  let compare s c = assert ((s |> Stack.pop) = c) in
+  let parse = function
+    |'(' |'{' |'[' as o -> Stack.push o bracks
+    |')' -> compare bracks '('
+    |'}' -> compare bracks '{'
+    |']' -> compare bracks '['
+    | _ -> ()
+  in try
+    s |> String.iter (fun c -> parse c);
+    if Stack.is_empty bracks then true else false
+  with _ ->
+    false
+
