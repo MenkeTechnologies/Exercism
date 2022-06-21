c883ee93b5e062dba400fc22647010f9295cc225
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-21 10:27:56 -0400

    rn

--- ocaml/roman-numerals/roman_numerals.ml
+++ ocaml/roman-numerals/roman_numerals.ml
@@ -1,2 +1,24 @@
-let to_roman _ =
-    failwith "'to_roman' is missing"
+let roman_list = [
+  (1000, "M");
+  (900, "CM");
+  (500, "D");
+  (400, "CD");
+  (100, "C");
+  (90, "XC");
+  (50, "L");
+  (40, "XL");
+  (10, "X");
+  (9, "IX");
+  (5, "V");
+  (4, "IV");
+  (1, "I")]
+
+let to_roman n =
+  let rec build acc num = function
+    | [] -> acc
+    | (k,v)::tl as t -> if num >= k then
+        build (acc ^ v) (num - k) t
+      else
+        build acc num tl
+  in
+  build "" n roman_list
