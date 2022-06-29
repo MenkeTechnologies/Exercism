33a36963a709e0e855576f571361fd6c6493b506
MenkeTechnologies <linux.dev25@gmail.com>
2022-06-29 12:54:24 -0400

    prim

--- ocaml/prime-factors/prime_factors.ml
+++ ocaml/prime-factors/prime_factors.ml
@@ -1,2 +1,17 @@
-let factors_of _ = 
-    failwith "'factors_of' is missing"
+open Base
+
+let factors_of num =
+  let open Int64.O in
+  let primes = function
+    | 2L -> 3L
+    | m -> m + 2L
+    in
+  let rec aux n m acc =
+    if n = 1L then acc
+    else if n % m = 0L then aux (n / m) m (m :: acc)
+    else aux n (primes m) acc
+    in
+  if num <= 0L then
+    raise (Invalid_argument "factors_of")
+  else List.rev (aux num 2L [])
+
