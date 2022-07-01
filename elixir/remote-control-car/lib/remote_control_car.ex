9c6dda33d8739e047a82a648313a0d1f4ff134a5
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-01 11:29:09 -0400

    rcc

--- elixir/remote-control-car/lib/remote_control_car.ex
+++ elixir/remote-control-car/lib/remote_control_car.ex
@@ -1,23 +1,32 @@
 defmodule RemoteControlCar do
-  # Please implement the struct with the specified fields
+  @enforce_keys [:nickname]
+  defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
 
   def new() do
-    # Please implement the new/0 function
+    new("none")
   end
 
   def new(nickname) do
-    # Please implement the new/1 function
+    %RemoteControlCar{nickname: nickname}
   end
 
-  def display_distance(remote_car) do
-    # Please implement the display_distance/1 function
+  def display_distance(%RemoteControlCar{} = remote_car) do
+    "#{remote_car.distance_driven_in_meters} meters"
   end
 
-  def display_battery(remote_car) do
-    # Please implement the display_battery/1 function
+  def display_battery(%RemoteControlCar{} = remote_car) do
+    case remote_car.battery_percentage do
+      0 -> "Battery empty"
+      n -> "Battery at #{n}%"
+    end
   end
 
-  def drive(remote_car) do
-    # Please implement the drive/1 function
+  def drive(%RemoteControlCar{} = remote_car) do
+    case remote_car.battery_percentage do
+      0 -> remote_car
+      n -> %{remote_car | battery_percentage: n - 1,
+            distance_driven_in_meters: remote_car.distance_driven_in_meters + 20
+        }
+    end
   end
 end
