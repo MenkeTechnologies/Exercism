f0c8ca3d33c853f14bfab1a09f82d025f03e126f
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-02 14:09:15 -0400

    fs

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -1,13 +1,33 @@
 defmodule FileSniffer do
+  @mime_types %{
+    exe: "application/octet-stream",
+    bmp: "image/bmp",
+    png: "image/png",
+    jpg: "image/jpg",
+    gif: "image/gif"
+  }
+
   def type_from_extension(extension) do
-    # Please implement the type_from_extension/1 function
+    Map.get(@mime_types, String.to_atom(extension))
   end
 
   def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
+    case file_binary do
+      <<0x7F, 0x45, 0x4C, 0x46, _::binary>> -> @mime_types.exe
+      <<0x42, 0x4D, _::binary>> -> @mime_types.bmp
+      <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>> -> @mime_types.png
+      <<0xFF, 0xD8, 0xFF, _::binary>> -> @mime_types.jpg
+      <<0x47, 0x49, 0x46, _::binary>> -> @mime_types.gif
+    end
   end
 
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    filetype = type_from_binary(file_binary)
+
+    if filetype == type_from_extension(extension) do
+      {:ok, filetype}
+    else
+      {:error, "Warning, file format and file extension do not match."}
+    end
   end
 end
