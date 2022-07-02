ddf190062cd316ac84a1b557fb27add34889171c
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-02 14:07:16 -0400

    n4s

--- elixir/need-for-speed/lib/need_for_speed.ex
+++ elixir/need-for-speed/lib/need_for_speed.ex
@@ -1,8 +1,9 @@
 defmodule NeedForSpeed do
-  # Add missing aliases and imports here.
-
-  # Do not edit the code below.
-
+  alias NeedForSpeed.Race
+  alias NeedForSpeed.RemoteControlCar, as: Car
+  import IO, only: [puts: 1]
+  import IO.ANSI, except: [color: 1]
+  
   def print_race(%Race{} = race) do
     puts("""
     🏁 #{race.title} 🏁
