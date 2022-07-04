cf0b82dc904f03daba442a7a55ee56b72176c69d
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-04 12:44:09 -0400

    tri

--- scheme/triangle/test.scm
+++ scheme/triangle/test.scm
@@ -1,84 +1,28 @@
-(load "triangle.scm")
+(load "test-util.ss")
 
-(use-modules (srfi srfi-64))
+(define test-cases
+  `((test-success "equilateral: 2 2 2" equal? triangle
+      '(2 2 2) 'equilateral)
+     (test-success "equilateral: 10 10 10" equal? triangle
+       '(10 10 10) 'equilateral)
+     (test-success "isosceles: 3 4 4" equal? triangle '(3 4 4)
+       'isosceles)
+     (test-success "isosceles: 4 3 4" equal? triangle '(4 3 4)
+       'isosceles)
+     (test-success "isosceles: 4 4 3" equal? triangle '(4 4 3)
+       'isosceles)
+     (test-success "isosceles: 10 10 2" equal? triangle
+       '(10 10 2) 'isosceles)
+     (test-success "scalene: 3 4 5" equal? triangle '(3 4 5)
+       'scalene)
+     (test-success "scalene: 10 11 12" equal? triangle
+       '(10 11 12) 'scalene)
+     (test-success "scalene: 5 4 2" equal? triangle '(5 4 2)
+       'scalene)
+     (test-error "invalid: 0 0 0" triangle '(0 0 0))
+     (test-error "invalid: 3 4 -5" triangle '(3 4 -5))
+     (test-error "invalid: 1 1 3" triangle '(1 1 3))
+     (test-error "invalid: 2 4 2" triangle '(2 4 2))))
 
-(test-begin "triangle")
+(run-with-cli "triangle.scm" (list test-cases))
 
-(test-begin "equilateral")
-
-; (test-skip "equilateral: 2 2 2")
-(test-eq "equilateral: 2 2 2"
-  (triangle 2 2 2)
-  'equilateral)
-
-(test-skip "equilateral: 10 10 10")
-(test-eq "equilateral: 10 10 10"
-  (triangle 10 10 10)
-  'equilateral)
-
-(test-end "equilateral")
-
-(test-begin "isosceles")
-
-(test-skip "isosceles: 3 4 4")
-(test-eq "isosceles: 3 4 4"
-  (triangle 3 4 4)
-  'isosceles)
-
-(test-skip "isosceles: 4 3 4")
-(test-eq "isosceles: 4 3 4"
-  (triangle 4 3 4)
-  'isosceles)
-
-(test-skip "isosceles: 4 4 3")
-(test-eq "isosceles: 4 4 3"
-  (triangle 4 4 3)
-  'isosceles)
-
-(test-skip "isosceles: 10 10 2")
-(test-eq "isosceles: 10 10 2"
-  (triangle 10 10 2)
-  'isosceles)
-
-(test-end "isosceles")
-
-(test-begin "scalene")
-
-(test-skip "scalene: 3 4 5")
-(test-eq "scalene: 3 4 5"
-  (triangle 3 4 5)
-  'scalene)
-
-(test-skip "scalene: 10 11 12")
-(test-eq "scalene: 10 11 12"
-  (triangle 10 11 12)
-  'scalene)
-
-(test-skip "scalene: 5 4 2")
-(test-eq "scalene: 5 4 2"
-  (triangle 5 4 2)
-  'scalene)
-
-(test-end "scalene")
-
-(test-begin "invalid")
-
-(test-skip "invalid: 0 0 0")
-(test-error "invalid: 0 0 0" #t
-            (triangle 0 0 0))
-
-(test-skip "invalid: 3 4 -5")
-(test-error "invalid: 3 4 -5" #t
-            (triangle 3 4 -5))
-
-(test-skip "invalid: 1 1 3")
-(test-error "invalid: 1 1 3" #t
-            (triangle 1 1 3))
-
-(test-skip "invalid: 2 4 2")
-(test-error "invalid: 2 4 2" #t
-            (triangle 2 4 2))
-
-(test-end "invalid")
-
-(test-end "triangle")
--- scheme/triangle/triangle.scm
+++ scheme/triangle/triangle.scm
@@ -1,4 +1,19 @@
 (import (rnrs))
 
 (define (triangle a b c)
-  'implement-me!)
+  (when (invalid a b c)
+    (error "invalid: " a b c))
+  (case (length (uniq (list a b c)))
+    ((1) 'equilateral)
+    ((2) 'isosceles)
+    ((3) 'scalene)))
+
+(define (uniq lst)
+  (fold-left
+   (lambda (u x) (if (member x u) u (cons x u)))
+   (list)
+   lst))
+
+(define (invalid a b c)
+  (let-values (((a b c) (apply values (sort (list a b c) <))))
+    (>= c (+ a b))))
