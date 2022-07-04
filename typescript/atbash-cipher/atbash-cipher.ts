df899905a91456635dcc55995681779cd87f8f75
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-04 11:14:34 -0400

    no skip

--- typescript/atbash-cipher/atbash-cipher.ts
+++ typescript/atbash-cipher/atbash-cipher.ts
@@ -1,7 +1,11 @@
-export function encode(plainText: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+const ALPHA = "abcdefghijklmnopqrstuvwxyz";
+export function encode(plainText: string): string {
+  return transcode(plainText).reduce((s, c, i) => s += (i === 0) || i % 5 ? c : " " + c, "");
 }
-
-export function decode(cipherText: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function decode(cipherText: string): string {
+  return transcode(cipherText).join("");
+}
+function transcode(text: string): string[] {
+  return Array.from(text.replace(/\W/g, "").toLowerCase())
+    .map(c => c.match(/\d/) ? c : ALPHA.charAt(25 - ALPHA.indexOf(c)));
 }
