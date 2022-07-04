74d43f70c351642f1b7fca9e820a914640f77131
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-04 11:26:26 -0400

    atbash

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -3,7 +3,7 @@ module Atbash
   DEC = ('a'..'z').to_a.reverse.to_a.zip(('a'..'z').to_a).to_h
 
   def self.common(s, m)
-    s.downcase.scan(/[0-9a-z]/).map { m[_1] || _1 }.join
+    s.downcase.scan(/\w/).map { m[_1] || _1 }.join
   end
 
   def self.encode(s)
--- typescript/atbash-cipher/atbash-cipher.ts
+++ typescript/atbash-cipher/atbash-cipher.ts
@@ -1,11 +1,11 @@
 const ALPHA = "abcdefghijklmnopqrstuvwxyz";
 export function encode(plainText: string): string {
-  return transcode(plainText).reduce((s, c, i) => s += (i === 0) || i % 5 ? c : " " + c, "");
+  return transcode(plainText).reduce((acc, c, i) => acc += i === 0 || i % 5 ? c : " " + c, "");
 }
 export function decode(cipherText: string): string {
   return transcode(cipherText).join("");
 }
 function transcode(text: string): string[] {
-  return Array.from(text.replace(/\W/g, "").toLowerCase())
+  return Array.from(text.toLowerCase().replace(/\W/g, ""))
     .map(c => c.match(/\d/) ? c : ALPHA.charAt(25 - ALPHA.indexOf(c)));
 }
