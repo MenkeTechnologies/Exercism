6d62fafc3964a64f307d2eabff657fb20de8be8f
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-06 08:58:24 -0400

    sa

--- erlang/space-age/src/space_age.erl
+++ erlang/space-age/src/space_age.erl
@@ -2,5 +2,15 @@
 
 -export([age/2]).
 
-
-age(_Planet, _Seconds) -> undefined.
+age(Planet, Seconds) ->
+  Seconds / (earth_year() * case Planet of
+                              mercury -> 0.2408467;
+                              venus -> 0.61519726;
+                              earth -> 1.0;
+                              mars -> 1.8808158;
+                              jupiter -> 11.862615;
+                              saturn -> 29.447498;
+                              uranus -> 84.016846;
+                              neptune -> 164.79132
+                            end).
+earth_year() -> 60 * 60 * 24 * 365.25.
