f51f12852c8abff5423b28cbf88d39c710182953
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-06 08:57:09 -0400

    tri

--- erlang/triangle/src/triangle.erl
+++ erlang/triangle/src/triangle.erl
@@ -1,6 +1,15 @@
 -module(triangle).
-
 -export([kind/3]).
 
+kind(A, B, C) -> kind(lists:sort([A, B, C])).
 
-kind(_A, _B, _C) -> undefined.
+kind([A, _, _]) when A =< 0 ->
+    {error, "all side lengths must be positive"};
+kind([A, B, C]) when A+B =< C ->
+    {error, "side lengths violate triangle inequality"};
+kind([A, A, A]) ->
+    equilateral;
+kind([A, B, C]) when A == B; B == C; A == C ->
+    isosceles;
+kind(_) ->
+    scalene.
