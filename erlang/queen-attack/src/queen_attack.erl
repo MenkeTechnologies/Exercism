5ccf1cf7433b9c10e2b086ce3f0789becd13eae8
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-06 08:59:11 -0400

    qa

--- erlang/queen-attack/src/queen_attack.erl
+++ erlang/queen-attack/src/queen_attack.erl
@@ -2,5 +2,7 @@
 
 -export([can_attack/2]).
 
+can_attack({X, _}, {X, _}) -> true;
+can_attack({_, Y}, {_, Y}) -> true;
+can_attack({X1, Y1}, {X2, Y2}) -> abs(X1 - X2) == abs(Y1 - Y2).
 
-can_attack(_WhiteQueen, _BlackQueen) -> undefined.
