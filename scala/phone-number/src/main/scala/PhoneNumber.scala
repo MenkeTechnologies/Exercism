034920e20595110b371fcdabf46bad40c197bdcd
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-06 16:48:32 -0400

    pn

--- scala/phone-number/src/main/scala/PhoneNumber.scala
+++ scala/phone-number/src/main/scala/PhoneNumber.scala
@@ -1,14 +1,11 @@
-
 object PhoneNumber {
-  def clean(str: String): Option[String] = {
 
-    val regex = """^\s*(\+?1?\s*)?\(?([2-9]\d{2})\)?[-.\s]*([2-9]\d{2})[-.\s]*(\d{4})\s*$""".r
+  val regex = """^\s*(\+?1?\s*)?\(?([2-9]\d{2})\)?[-.\s]*([2-9]\d{2})[-.\s]*(\d{4})\s*$""".r
 
+  def clean(str: String) =
     regex.findFirstMatchIn(str) match {
       case Some(m) => Some(s"${m.group(2)}${m.group(3)}${m.group(4)}")
       case _ => None
     }
-
-  }
 
 }
