af57821515735e632d2890a43fb8b67f1830613c
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-06 14:28:40 -0400

    rn

--- clojure/roman-numerals/src/roman_numerals.clj
+++ clojure/roman-numerals/src/roman_numerals.clj
@@ -1,5 +1,22 @@
 (ns roman-numerals)
 
-(defn numerals [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(def num-roman-pairs [[1000 "M"]
+                      [900 "CM"]
+                      [500 "D"]
+                      [400 "CD"]
+                      [100 "C"]
+                      [90 "XC"]
+                      [50 "L"]
+                      [40 "XL"]
+                      [10 "X"]
+                      [9 "IX"]
+                      [5 "V"]
+                      [4 "IV"]
+                      [1 "I"]])
+
+(defn numerals [init]
+  (first (reduce
+           (fn [[acc num] [k v]] (list (apply str acc (repeat (quot num k) v)) (rem num k)))
+           (list "" init)
+           num-roman-pairs))
+  )
