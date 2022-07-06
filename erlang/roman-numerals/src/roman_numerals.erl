a54d1836fdfe77e761e9c7ff67ac8bb972c444d7
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-06 11:43:04 -0400

    roman numerals

--- erlang/roman-numerals/src/roman_numerals.erl
+++ erlang/roman-numerals/src/roman_numerals.erl
@@ -2,5 +2,27 @@
 
 -export([roman/1]).
 
+-define(NUMERALS,
+        [{1000, "M"},
+         {900, "CM"},
+         {500, "D"},
+         {400, "CD"},
+         {100, "C"},
+         {90, "XC"},
+         {50, "L"},
+         {40, "XL"},
+         {10, "X"},
+         {9, "IX"},
+         {5, "V"},
+         {4, "IV"},
+         {1, "I"}]).
 
-roman(_Number) -> undefined.
+roman(Number) ->
+    roman(Number, ?NUMERALS).
+
+roman(Number, Numerals = [{A, R} | _]) when Number >= A ->
+    R ++ roman(Number - A, Numerals);
+roman(Number, [_ | Numerals]) ->
+    roman(Number, Numerals);
+roman(0, _) ->
+    "".
