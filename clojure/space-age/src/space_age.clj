f89774c74025201ce01db13666c98bb35c52e82d
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-06 22:05:23 -0400

    sa

--- clojure/space-age/src/space_age.clj
+++ clojure/space-age/src/space_age.clj
@@ -1,33 +1,14 @@
 (ns space-age)
 
-(defn on-mercury [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def SPY 31557600)
+(defn space-age [earth-ratio seconds]
+  (/ seconds (* SPY earth-ratio)))
 
-(defn on-venus [] ;; <- arglist goes here
-  ;; your code goes here
-)
-
-(defn on-earth [] ;; <- arglist goes here
-      ;; your code goes here
-)
-
-(defn on-mars [] ;; <- arglist goes here
-  ;; your code goes here
-)
-
-(defn on-jupiter [] ;; <- arglist goes here
-  ;; your code goes here
-)
-
-(defn on-saturn [] ;; <- arglist goes here
-  ;; your code goes here
-)
-
-(defn on-neptune [] ;; <- arglist goes here
-  ;; your code goes here
-)
-
-(defn on-uranus [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def on-mercury (partial space-age 0.2408467))
+(def on-venus (partial space-age 0.61519726))
+(def on-earth (partial space-age 1))
+(def on-mars (partial space-age 1.8808158))
+(def on-jupiter (partial space-age 11.862615))
+(def on-saturn (partial space-age 29.447498))
+(def on-neptune (partial space-age 164.79132))
+(def on-uranus (partial space-age 84.016846))
