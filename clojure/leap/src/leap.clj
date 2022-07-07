9f95963f546960360c0a01dd7301987f71edfc06
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-06 22:49:25 -0400

    leap

--- clojure/leap/src/leap.clj
+++ clojure/leap/src/leap.clj
@@ -1,5 +1,9 @@
 (ns leap)
 
-(defn leap-year? [year] ;; <- argslist goes here
-  ;; your code goes here
-)
+(defn leap-year? [year]
+  (or
+    (and
+      (zero? (mod year 4))
+      (pos? (mod year 100))
+      )
+    (zero? (mod year 400))))
