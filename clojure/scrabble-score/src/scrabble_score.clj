f7639324bcf57d6b445b6d0ad6b14b52568066e4
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-06 22:09:40 -0400

    ss

--- clojure/scrabble-score/src/scrabble_score.clj
+++ clojure/scrabble-score/src/scrabble_score.clj
@@ -1,9 +1,15 @@
 (ns scrabble-score)
 
-(defn score-letter [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn score-letter [l]
+  (let [dict {"a" 1, "e" 1, "i" 1, "o" 1, "u" 1, "l" 1, "n" 1, "r" 1,
+              "s" 1, "t" 1, "d" 2, "g" 2, "b" 3, "c" 3, "m" 3, "p" 3,
+              "f" 4, "h" 4, "v" 4, "w" 4, "y" 4, "k" 5, "j" 8, "x" 8,
+              "q" 10, "z" 10}]
+    (dict (clojure.string/lower-case l))))
 
-(defn score-word [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn score-word [w]
+  (->> w
+       (seq)
+       (map (comp score-letter str))
+       (reduce + 0))
+  )
