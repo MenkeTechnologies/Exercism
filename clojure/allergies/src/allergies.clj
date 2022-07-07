6c0efd41970d63e238d15e85e28ecf492a2adb39
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-07 11:10:28 -0400

    allergies

--- clojure/allergies/src/allergies.clj
+++ clojure/allergies/src/allergies.clj
@@ -1,9 +1,15 @@
 (ns allergies)
 
-(defn allergies [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn allergies [s]
+  (cond-> []
+          (bit-test s 0) (conj :eggs)
+          (bit-test s 1) (conj :peanuts)
+          (bit-test s 2) (conj :shellfish)
+          (bit-test s 3) (conj :strawberries)
+          (bit-test s 4) (conj :tomatoes)
+          (bit-test s 5) (conj :chocolate)
+          (bit-test s 6) (conj :pollen)
+          (bit-test s 7) (conj :cats)))
 
-(defn allergic-to? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn allergic-to? [score substance]
+  (some #{substance} (allergies score)))
