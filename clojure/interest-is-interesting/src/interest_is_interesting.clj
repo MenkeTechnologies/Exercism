908cc665feeb4dd528b57228bd5878028a7d4564
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-06 22:39:21 -0400

    int

--- clojure/interest-is-interesting/src/interest_is_interesting.clj
+++ clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -1,16 +1,34 @@
 (ns interest-is-interesting)
 
+(defn abs [n]
+  (if (neg? n)
+    (- n)
+    n))
+
 (defn interest-rate
-  "TODO: add docstring"
+  "Calculate the interest rate based on the specified balance."
   [balance]
+  (cond
+    (neg? balance) -3.213
+    (< balance 1000.0M) 0.5
+    (< balance 5000.0M) 1.621
+    :else 2.475))
+
+(defn interest [n]
+  (*
+    (bigdec (interest-rate n)) 0.01M (abs n))
   )
 
 (defn annual-balance-update
-  "TODO: add docstring"
+  "Calculate the annual balance update taking into account the interest rate."
   [balance]
-  )
+  (+
+    (interest balance)
+     balance))
 
 (defn amount-to-donate
-  "TODO: add docstring"
+  "Calculate how much money to donate to charities based on the balance and the tax-free percentage that the government allows."
   [balance tax-free-percentage]
-  )
+  (if (pos? balance)
+    (int (* tax-free-percentage 0.01 balance 2))
+    0))
