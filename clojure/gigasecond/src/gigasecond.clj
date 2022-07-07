4dc2e79f93f77593d3f7bda714334f5eeedaeb88
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-06 22:52:14 -0400

    gs

--- clojure/gigasecond/src/gigasecond.clj
+++ clojure/gigasecond/src/gigasecond.clj
@@ -1,5 +1,12 @@
-(ns gigasecond)
+(ns gigasecond
+  (:import (java.time LocalDateTime)))
 
-(defn from []  ;; <- arglist goes here
-    ;; your code goes here
-)
+(def giga 1e9)
+
+(defn from [year month day]
+  (-> (LocalDateTime/of year month day 0 0)
+      (.toInstant java.time.ZoneOffset/UTC)
+      (.plusSeconds giga)
+      (.atOffset java.time.ZoneOffset/UTC)
+      (bean)
+      ((juxt :year :monthValue :dayOfMonth))))
