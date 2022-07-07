00bdb84391fd85db117db4b3944f2bc13b1c4429
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-07 11:00:42 -0400

    hexa

--- clojure/hexadecimal/src/hexadecimal.clj
+++ clojure/hexadecimal/src/hexadecimal.clj
@@ -1,5 +1,14 @@
 (ns hexadecimal)
 
-(defn hex-to-int [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def hex-values '(\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \a \b \c \d \e \f))
+(def parse-hex (zipmap hex-values (range 16)))
+
+(defn hex-to-int
+  [n]
+  (if-not (every? (set hex-values) n)
+    0
+    (->> n
+         seq
+         (map parse-hex)
+         (reduce #(+ (* %1 16) %2)))
+    ))
