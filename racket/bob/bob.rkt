d2da7868cf4dbcc088388ffc46d63ae3e0b3cf29
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-10 10:58:37 -0400

    bob

--- racket/bob/bob.rkt
+++ racket/bob/bob.rkt
@@ -1,3 +1,15 @@
 #lang racket
 
 (provide response-for)
+
+(define (response-for s)
+  (match (list
+          (equal? "" (string-trim s))
+          (equal? s (string-upcase s))
+          (not (false? (regexp-match #px"[[:alpha:]]+" s)))
+          (string-suffix? s "?"))
+    ((list #t _ _ _) "Fine. Be that way!")
+    ((list _ #t #t #t) "Calm down, I know what I'm doing!")
+    ((list _ #t #t #f) "Whoa, chill out!")
+    ((list _ _ _ #t) "Sure.")
+    (_ "Whatever.")))
