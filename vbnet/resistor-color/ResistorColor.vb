c507d9edbc018524401475e4728298e1ba4aa8cf
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-12 09:09:28 -0400

    rc

--- vbnet/resistor-color/ResistorColor.vb
+++ vbnet/resistor-color/ResistorColor.vb
@@ -1,13 +1,15 @@
-Imports System
-
 Public Module ResistorColor
-    
-    Public Function ColorCode(ByVal color As String) As Integer
-        Throw New NotImplementedException("You need to implement this function")
+    Dim lst() As String = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
+
+    Public Function ColorCode(color As String) As Integer
+        For i = 0 To lst.Length - 1
+            If lst(i) = color Then
+                Return i
+            End If
+        Next
     End Function
 
     Public Function Colors() As String()
-        Throw New NotImplementedException("You need to implement this function")
+        Return lst
     End Function
-
 End Module
