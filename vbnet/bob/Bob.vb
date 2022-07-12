6bc1f3515a49078b56491e7b89e4a3451f06b62d
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-12 09:17:52 -0400

    bob

--- vbnet/bob/Bob.vb
+++ vbnet/bob/Bob.vb
@@ -1,6 +1,14 @@
 Public Class Bob
-
     Public Function Hey(statement As String) As String
-        Throw New NotImplementedException("You need to implement this function")
+        Dim isQuestion = statement.EndsWith("?")
+        Dim isUppercase = Text.RegularExpressions.Regex.Match(statement, "[A-Z]").Success AndAlso
+                          (Not Text.RegularExpressions.Regex.Match(statement, "[a-z]").Success)
+        Dim isEmpty = statement.Trim() = String.Empty
+        If isEmpty Then
+            Return "Fine. Be that way!"
+        ElseIf isUppercase Then
+            Return If (isQuestion, "Calm down, I know what I'm doing!", "Whoa, chill out!")
+        End If
+        Return If (isQuestion, "Sure.", "Whatever.")
     End Function
 End Class
