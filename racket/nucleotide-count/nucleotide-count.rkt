abfe86279688ea85ad3a208220c5dc959a17ac57
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-13 09:30:37 -0400

    nt

--- racket/nucleotide-count/nucleotide-count.rkt
+++ racket/nucleotide-count/nucleotide-count.rkt
@@ -1,3 +1,12 @@
 #lang racket
 
 (provide nucleotide-counts)
+
+(define dict '((#\A . 0) (#\C . 0) (#\G . 0) (#\T . 0)))
+
+(define (nucleotide-counts dna)
+  (foldl
+   (lambda (nt acc)
+     (dict-update acc nt add1))
+   dict
+   (string->list dna)))
