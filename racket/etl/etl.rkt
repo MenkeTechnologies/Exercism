38dadd69fdf37de8c677e495d1c08b87f07b70c8
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-13 09:46:35 -0400

    etl

--- racket/etl/etl.rkt
+++ racket/etl/etl.rkt
@@ -1,3 +1,14 @@
 #lang racket
 
 (provide etl)
+
+(define (etl old-hash)
+  (define r (make-hash))
+  (for ([(score letters) (in-hash old-hash)])
+    (if (< score 1)
+        (raise-arguments-error 'etl "Scores must be positive")
+        (for ([letter (in-list letters)])
+          (if (not (string? letter))
+              (raise-arguments-error 'etl "Letters must be strings")
+              (hash-set! r (string-downcase letter) score)))))
+  r)
