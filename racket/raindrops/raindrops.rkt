ab5b24042ddde927e60bf0121ab0b1c024ec9e16
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-13 09:35:50 -0400

    rd

--- racket/raindrops/raindrops.rkt
+++ racket/raindrops/raindrops.rkt
@@ -1,3 +1,13 @@
 #lang racket
 
 (provide convert)
+
+(define dict '((3 . "Pling") (5 . "Plang") (7 . "Plong")))
+
+(define (convert num)
+  (define r (filter-map (lambda (pair)
+                               (and (zero? (remainder num (car pair))) (cdr pair)))
+                             dict))
+  (if (null? r)
+      (number->string num)
+      (string-append* r)))
