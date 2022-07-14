c2c1ba82e1129502eadf078d795d5075afb9e1d8
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-14 11:03:07 -0400

    ayb

--- vbnet/all-your-base/AllYourBase.vb
+++ vbnet/all-your-base/AllYourBase.vb
@@ -1,8 +1,47 @@
 Imports System
 Imports System.Collections.Generic
+Imports System.Linq
 
 Public Module AllYourBase
-    Public Function Rebase(ByVal inputBase As Integer, ByVal inputDigits As Integer(), ByVal outputBase As Integer) As Integer()
-        Throw New NotImplementedException("You need to implement this function")
+    Public Function Rebase(inputBase As Integer, inputDigits As Integer(), outputBase As Integer) As Integer()
+        If inputBase < 2
+            Throw New ArgumentException("Invalid input base.")
+        ElseIf outputBase < 2
+            Throw New ArgumentException("Invalid output base.")
+        End If
+
+        Dim inputDigitsWithoutLeadingZeros = inputDigits.SkipWhile(Function(digit) digit = 0).ToArray()
+        If inputDigitsWithoutLeadingZeros.Length = 0
+            Return {0}
+        End If
+
+        Return ToDigits(outputBase, FromDigits(inputBase, inputDigitsWithoutLeadingZeros))
+    End Function
+
+    Private Function FromDigits(base As Integer, digits As Integer()) As Integer
+        Return digits.Aggregate(0, Function(acc, n) 
+            If n < 0 Or n >= base
+                Throw New ArgumentException("Invalid input digit")
+            End If
+
+            Return acc * base + n
+        End Function)
+    End Function
+
+    Private Function ToDigits(toBase As Integer, n As Integer) As Integer()
+        Dim digits = new List(Of Integer)
+        Dim remainder = n
+        Dim multiplier = 1
+
+        While remainder > 0
+            multiplier *= toBase
+            Dim value = remainder Mod multiplier
+            Dim digit = value * toBase / multiplier
+            digits.Add(digit)
+            remainder -= value
+        End While
+
+        digits.Reverse()
+        Return digits.ToArray()
     End Function
 End Module
