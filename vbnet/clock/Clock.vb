e43d951b874148d69c90f550bf849e32ee05f0f7
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-14 12:19:42 -0400

    clock

--- vbnet/clock/Clock.vb
+++ vbnet/clock/Clock.vb
@@ -1,13 +1,16 @@
-Public Class Clock
-    Public Sub New(ByVal hours As Integer, ByVal minutes As Integer)
-        Throw New NotImplementedException("You need to implement this function")
+Structure Clock
+    Private ReadOnly _clock As DateTime
+    Sub New(hours As Integer, minutes As Integer)
+        Dim t = (New DateTime(2, 1, 1, 0, 0, 0)).AddHours(hours).AddMinutes(minutes)
+        _clock = New DateTime(2, 1, 1, t.Hour, t.Minute, 0)
     End Sub
-
-    Public Function Add(ByVal minutesToAdd As Integer) As Clock
-        Throw New NotImplementedException("You need to implement this function")
+    Function Add(minutesToAdd As Integer) As Clock
+        Return New Clock(_clock.Hour, _clock.Minute + minutesToAdd)
     End Function
-
-    Public Function Subtract(ByVal minutesToSubtract As Integer) As Clock
-        Throw New NotImplementedException("You need to implement this function")
+    Function Subtract(minutesToSubtract As Integer) As Clock
+        Return New Clock(_clock.Hour, _clock.Minute - minutesToSubtract)
     End Function
-End Class
+    Public Overrides Function ToString() As String
+        Return _clock.ToString("HH:mm")
+    End Function
+End Structure
