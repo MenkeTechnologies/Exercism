7882e8c54bfd959e0478afff0448ab3042d0319e
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-15 00:00:16 -0400

    rna

--- racket/rna-transcription/rna-transcription.rkt
+++ racket/rna-transcription/rna-transcription.rkt
@@ -1,3 +1,9 @@
 #lang racket
 
 (provide to-rna)
+
+(define dict (hash #\C #\G #\G #\C #\A #\U #\T #\A))
+
+(define (to-rna s)
+    (list->string (map (lambda (c) (hash-ref dict c))
+                       (string->list s))))
