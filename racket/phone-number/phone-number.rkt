c67470971ccb75ce31f74476f545e33e113fa5a6
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-15 00:05:36 -0400

    pn

--- racket/phone-number/phone-number.rkt
+++ racket/phone-number/phone-number.rkt
@@ -2,5 +2,8 @@
 
 (provide nanp-clean)
 
+(define re #px"^\\s*\\W?1?\\W*([2-9]\\d{2})\\W*([2-9]\\d{2})\\W*(\\d{4})\\s*$")
+
 (define (nanp-clean s)
-  s)
+  (let ([matches (regexp-match re s)])
+    (if matches (string-join (rest matches ) "") (error "Invalid phone number"))))
