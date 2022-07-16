376f793f458c448a58cd25247848ee463417725a
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-16 12:25:33 -0400

    qa

--- lua/queen-attack/queen-attack.lua
+++ lua/queen-attack/queen-attack.lua
@@ -1,3 +1,13 @@
 return function(pos)
-
+  assert(pos.row >= 0 and pos.row < 8, 'row from 0 to 7')
+  assert(pos.column >= 0 and pos.column < 8, 'column from 0 to 7')
+  return {
+    row = pos.row,
+    column = pos.column,
+    can_attack = function(other)
+      local dy = math.abs(pos.row - other.row)
+      local dx = math.abs(pos.column - other.column)
+      return dx == 0 or dy == 0 or dx == dy
+    end
+  }
 end
