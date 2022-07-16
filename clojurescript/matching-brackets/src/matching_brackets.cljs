2a5652b11c10ebc37508f2d70cbe8169a432d198
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-15 22:33:09 -0400

    mb

--- clojurescript/matching-brackets/src/matching_brackets.cljs
+++ clojurescript/matching-brackets/src/matching_brackets.cljs
@@ -1,3 +1,10 @@
-(ns matching-brackets)
+(ns matching-brackets (:require [clojure.string :as str]))
 
-(defn valid? [])
+(defn valid? [s]
+    (let [braces (filter #{"{" "}" "(" ")" "[" "]"} (str/split s #""))
+          remove_pairs (->> braces
+                       (str/join "")
+                       (iterate #(str/replace % #"(\{\}|\[\]|\(\))" "")))]
+      (-> remove_pairs
+          (nth (count s))
+          empty?)))
