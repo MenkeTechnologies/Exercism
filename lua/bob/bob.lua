722b9b3145f84566377f4a0fa72a91a93aaeb72e
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-16 12:02:17 -0400

    bob

--- lua/bob/bob.lua
+++ lua/bob/bob.lua
@@ -1,7 +1,14 @@
-local bob = {}
+return {
+    hey = function(s)
+        local r
+        if s == '' then
+            return 'Fine, be that way.'
+        end
+        if s:upper() == s then
+            return s:find('?') and 'Calm down, I know what I\'m doing!' or 'Whoa, chill out!'
+        end
 
-function bob.hey(say)
+        return s:find('?') and 'Sure' or 'Whatever'
+    end
+}
 
-end
-
-return bob
