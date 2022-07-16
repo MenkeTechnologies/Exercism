e8961bc6c853765037a7f1f592fc459a15dbb818
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-15 21:56:50 -0400

    bob

--- clojurescript/bob/src/bob.cljs
+++ clojurescript/bob/src/bob.cljs
@@ -1,6 +1,15 @@
 (ns bob
   (:require [clojure.string :as str]))
 
-(defn response-for [] ;; <--- arguments comes here...
-  ;; your code goes here...
-  )
+(defn- silence? [s] (str/blank? s))
+(defn- question? [s] (str/ends-with? (str/trim s) "?"))
+(defn- shout? [s] (and (= s (str/upper-case s)) (re-find #"[A-Za-z]" s)))
+
+(defn response-for [s]
+  (cond
+        (silence? s) "Fine. Be that way!"
+        (and (question? s) (shout? s)) "Calm down, I know what I'm doing!"
+        (question? s) "Sure."
+        (shout? s) "Whoa, chill out!"
+        :else "Whatever."
+  ))
