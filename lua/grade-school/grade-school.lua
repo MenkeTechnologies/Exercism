797ccbaab981166ac6cb3fa58631a85773c02e30
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-15 22:37:30 -0400

    gs

--- lua/grade-school/grade-school.lua
+++ lua/grade-school/grade-school.lua
@@ -1,3 +1,22 @@
 local School = {}
+School.__index = School
+
+function School:new()
+  return setmetatable({ students = {} }, School)
+end
+
+function School:roster()
+  return self.students
+end
+
+function School:add(name, grade)
+  self.students[grade] = self.students[grade] or {}
+  table.insert(self.students[grade], name)
+  table.sort(self.students[grade])
+end
+
+function School:grade(gradeNumber)
+  return self.students[gradeNumber] or {}
+end
 
 return School
