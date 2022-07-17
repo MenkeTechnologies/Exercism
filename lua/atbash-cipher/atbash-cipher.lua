ade302f5bb1ba01eb5fc5706c0352e7d9442c50f
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-17 00:46:02 -0400

    atb

--- lua/atbash-cipher/atbash-cipher.lua
+++ lua/atbash-cipher/atbash-cipher.lua
@@ -1,5 +1,16 @@
 return {
-  encode = function(plaintext)
+  encode = function(s)
+  local lc = s:lower()
+  local trans = lc:gsub("%a", function(raw) return string.char(string.byte('z') - raw:byte() + string.byte('a')) end)
+  trans = trans:gsub("%W", '')
+  if #trans <= 5 then return trans end
+  local encoded = ''
+  for i=1, #trans, 5 do
+    local j = i + 4
+    if j > #trans then j = #trans end
+    encoded = encoded .. trans:sub(i,j) .. ' '
+  end
+  return encoded:sub(1,-2)
 
   end
 }
