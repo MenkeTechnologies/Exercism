f46f6c8b49e20de015aa3edf40d5f555d7e0403d
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-17 00:08:16 -0400

    pn

--- lua/phone-number/phone-number.lua
+++ lua/phone-number/phone-number.lua
@@ -1,3 +1,23 @@
-local PhoneNumber = {}
+local phone_number = {}
+phone_number.__index = phone_number
 
-return PhoneNumber
+function phone_number:new(number)
+  local self = setmetatable({}, phone_number)
+  local pattern = '(%d%d%d)[^%d]*(%d%d%d)[^%d]*(%d%d%d%d)$'
+
+  if #number == 11 then pattern = '1' .. pattern end
+    self.area, self.central_office, self.line = number:match(pattern)
+  if not pcall(function() self.number = self.area .. self.central_office .. self.line end) then
+    self.number = "0000000000"
+  end
+  return self
+end
+
+function phone_number:areaCode()
+  return self.area
+end
+
+function phone_number:__tostring()
+  return '(' .. self.area .. ') ' .. self.central_office .. '-' .. self.line
+end
+return phone_number
