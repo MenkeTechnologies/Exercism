263934c3620918a289dd10d7ebbd6b47b6912fa2
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-17 00:09:46 -0400

    etl

--- lua/etl/etl.lua
+++ lua/etl/etl.lua
@@ -1,5 +1,12 @@
 return {
   transform = function(dataset)
+      local r = {}
+      for k, values in pairs(dataset) do
+          for _, v in pairs(values) do
+              r[v:lower()] = k
+          end
+      end
 
+      return r
   end
 }
