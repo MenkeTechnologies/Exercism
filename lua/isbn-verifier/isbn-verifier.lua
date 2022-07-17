12c6397bb288422e5d22248c65ecf31154fba13e
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-17 00:39:50 -0400

    isbn

--- lua/isbn-verifier/isbn-verifier.lua
+++ lua/isbn-verifier/isbn-verifier.lua
@@ -1,5 +1,19 @@
 return {
   valid = function(isbn)
-
+    if not isbn:match("^%d[-]?%d%d%d[-]?%d%d%d%d%d[-]?[%dX]$") then
+        return false
+    end
+    isbn = string.reverse(isbn:gsub("[^%dX]", ""))
+    local sum = 0
+    for i=1, #isbn do
+      local char = isbn:sub(i,i)
+      if char:match("X") then
+          sum = sum + 10
+      end
+      if char:match("%d") then
+        sum = sum + char * i
+      end
+    end
+    return sum % 11 == 0
   end
 }
