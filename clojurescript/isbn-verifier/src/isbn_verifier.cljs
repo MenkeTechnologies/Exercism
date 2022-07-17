b6e842d2f6964f4d0ecb5092a125de7ea688e99c
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-17 11:59:15 -0400

    isbn

--- clojurescript/isbn-verifier/src/isbn_verifier.cljs
+++ clojurescript/isbn-verifier/src/isbn_verifier.cljs
@@ -1,5 +1,12 @@
 (ns isbn-verifier)
 
-(defn isbn? [] ;; <--- argument goes here...
-  ;; your code goes here...
-  )
+(defn isbn? [isbn]
+  (let [isbn-digits (->> isbn
+                         (remove #(= % \-))
+                         (replace {"X" "10"}))
+        isbn-checksum (->> isbn-digits
+                           reverse
+                           (map-indexed #(* %2 (inc %1)))
+                           (reduce +))]
+    (and (= 10 (count isbn-digits))
+         (zero? (mod isbn-checksum 11)))))
