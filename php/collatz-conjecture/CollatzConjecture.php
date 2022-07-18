63ad369abbeff8437d2c03300f7a36e6820b32aa
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-18 05:00:14 -0400

    coll

--- php/collatz-conjecture/CollatzConjecture.php
+++ php/collatz-conjecture/CollatzConjecture.php
@@ -1,30 +1,14 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
-function steps(int $number): int
+function steps(int $n): int
 {
-    throw new \BadFunctionCallException("Implement the steps function");
+    if ($n <= 0) {
+        throw new InvalidArgumentException('Only positive numbers are allowed');
+    }
+    for ($step = 0; $n !== 1; ++$step) {
+        $n = $n % 2 === 0 ? $n / 2 : 3 * $n + 1;
+    }
+    return $step;
 }
