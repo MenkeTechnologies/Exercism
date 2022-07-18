4f17c31f33d9eb5a9763f7cf7588c611d2ab7f6f
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-18 05:13:35 -0400

    gs

--- php/collatz-conjecture/CollatzConjecture.php
+++ php/collatz-conjecture/CollatzConjecture.php
@@ -7,8 +7,10 @@ function steps(int $n): int
     if ($n <= 0) {
         throw new InvalidArgumentException('Only positive numbers are allowed');
     }
-    for ($step = 0; $n !== 1; ++$step) {
+    $step = 0;
+    while ($n > 1) {
         $n = $n % 2 === 0 ? $n / 2 : 3 * $n + 1;
+        ++$step;
     }
     return $step;
 }
