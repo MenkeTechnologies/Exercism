d2e656fd8b4ba4869122a940977404e4d25106d1
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-20 07:10:26 -0400

    nt

--- scheme/nucleotide-count/nucleotide-count.scm
+++ scheme/nucleotide-count/nucleotide-count.scm
@@ -1,5 +1,13 @@
-(import (rnrs))
+(import (rnrs)
+        (except (srfi srfi-1) map))
+
+(define dict '((#\A . 0)
+                (#\C . 0)
+                (#\G . 0)
+                (#\T . 0)))
 
 (define (nucleotide-count dna)
-  'implement-me!)
-
+  (define (update c acc)
+    (let ((matched (assoc c acc)))
+      (alist-cons c (+ 1 (cdr matched)) (alist-delete c acc))))
+  (string-fold update dict dna))
