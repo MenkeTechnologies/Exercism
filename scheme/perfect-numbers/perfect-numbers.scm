08c15fa55727511f9f00bc948491b765259f23c8
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-20 06:55:38 -0400

    pn

--- scheme/perfect-numbers/perfect-numbers.scm
+++ scheme/perfect-numbers/perfect-numbers.scm
@@ -1,5 +1,19 @@
 (import (rnrs))
 
-(define (classify n)
-  'implement-me!)
+(define (classify limit)
+  (let ((aliquot (sum-pos-factors 1 limit)))
+  (cond
+    ((= aliquot limit) 'perfect)
+    ((> aliquot limit) 'abundant)
+    (else 'deficient))))
 
+(define (sum-pos-factors n limit)
+  (when (< limit 1)
+    (assertion-violation 'sum-pos-factors "limit must be a positive natural number" limit))
+  (cond
+    ((> n (/ limit 2)) 0)
+    ((factor? n limit) (+ n (sum-pos-factors (+ n 1) limit)))
+    (else (sum-pos-factors (+ n 1) limit))))
+
+(define (factor? m limit)
+  (zero? (modulo limit m)))
