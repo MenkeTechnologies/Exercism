a08b7dbf96370e42a70ba047b627b57b65b609da
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-21 00:35:39 -0400

    coll

--- common-lisp/collatz-conjecture/collatz-conjecture.lisp
+++ common-lisp/collatz-conjecture/collatz-conjecture.lisp
@@ -4,4 +4,9 @@
 
 (in-package :collatz-conjecture)
 
-(defun collatz (n))
+(defun collatz (n)
+  (labels ((rec (n cnt)
+             (cond ((= n 1) cnt)
+                   ((evenp n) (rec (/ n 2) (1+ cnt)))
+                   (t (rec (1+ (* 3 n)) (1+ cnt))))))
+    (and (plusp n) (rec n 0))))
