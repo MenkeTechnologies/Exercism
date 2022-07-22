0e062370ab5f8424f9dc6ebe5272fcd9699867da
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-22 03:33:23 -0400

    rn

--- common-lisp/roman-numerals/roman-numerals.lisp
+++ common-lisp/roman-numerals/roman-numerals.lisp
@@ -4,6 +4,30 @@
 
 (in-package :roman-numerals)
 
+(setf numerals '((1000 . "M")
+		  (900 . "CM")
+		  (500 . "D")
+		  (400 . "CD")
+		  (100 . "C")
+		  (90 . "XC")
+		  (50 . "L")
+		  (40 . "XL")
+		  (10 . "X")
+		  (9 . "IX")
+		  (5 . "V")
+		  (4 . "IV")
+		  (1 . "I")))
+
+(defun next-numeral (n)
+  (first (remove-if (lambda (x) (> (car x) n)) numerals)))
+
+(defun ->numerals (number)
+  (loop with n = number
+	while (> n 0)
+	collect (let ((pair (next-numeral n)))
+		  (setf n (- n (car pair)))
+		  (cdr pair))))
+
 (defun romanize (number)
-  "Returns the Roman numeral representation for a given number."
-  )
+  (reduce (lambda (acc n) (concatenate 'string acc n))
+	  (->numerals number)))
