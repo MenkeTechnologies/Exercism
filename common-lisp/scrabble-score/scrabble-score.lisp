fb279ad39b22ef7cff11992c721355a4739b34c3
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-22 03:43:05 -0400

    ss

--- common-lisp/scrabble-score/scrabble-score.lisp
+++ common-lisp/scrabble-score/scrabble-score.lisp
@@ -4,6 +4,16 @@
 
 (in-package :scrabble-score)
 
+(defun score-char (c)
+  (case (char-downcase c)
+    ((#\a #\e #\i #\o #\u #\l #\n #\r #\s #\t) 1)
+    ((#\d #\g) 2)
+    ((#\b #\c #\m #\p) 3)
+    ((#\f #\h #\v #\w #\y) 4)
+    ((#\k) 5)
+    ((#\j #\x) 8)
+    ((#\q #\z) 10)
+    (otherwise 0)))
+
 (defun score-word (word)
-  "Computes the score for an entire word."
-  )
+       (reduce #'+ (map 'list #'score-char word)))
