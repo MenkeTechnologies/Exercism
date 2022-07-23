ebd94ba14208d3a9efa6d65c264e50e3a59ae48c
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-23 00:23:30 -0400

    bob

--- common-lisp/bob/bob.lisp
+++ common-lisp/bob/bob.lisp
@@ -3,4 +3,11 @@
   (:export :response))
 (in-package :bob)
 
-(defun response (hey-bob))
+(defun response (s)
+  (when (null s) (setf s ""))
+  (setf trim (string-trim '(#\Space #\Tab #\Newline) s))
+  (if (string= "" trim) "Fine. Be that way!"
+    (let ((all-caps (and (some #'upper-case-p trim) (notany #'lower-case-p trim))))
+      (if (char= #\? (elt trim (1- (length trim))))
+        (if all-caps "Calm down, I know what I'm doing!" "Sure.")
+        (if all-caps "Whoa, chill out!" "Whatever.")))))
