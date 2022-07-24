63ff7c3ee698224d0479061e723f36b280e39177
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-24 10:32:52 -0400

    leap

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -2,23 +2,6 @@ using System;
 
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-
-        if (year % 4 == 0)
-        {
-            if (year % 100 == 0)
-            {
-                if (year % 400 == 0)
-                {
-                    return true;
-                }
-                return false;
-            }
-
-            return true;
-        }
-        
-        return false;
-    }
+    public static bool IsLeapYear(int y) => 
+        y % 4 == 0 && y % 100 != 0 || y % 400 == 0;
 }
