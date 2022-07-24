0e1ca4eb739135a93b8921e0868d3e508186f8d3
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-24 00:21:07 -0400

    darts

--- common-lisp/darts/darts.lisp
+++ common-lisp/darts/darts.lisp
@@ -4,4 +4,12 @@
 
 (in-package :darts)
 
-(defun score (x y))
+(defun dist (x y)
+  (sqrt (+ (expt x 2) (expt y 2))))
+
+(defun score (x y)
+  (let ((d (dist x y)))
+    (cond ((> d 10) 0)
+          ((> d 5) 1)
+          ((> d 1) 5)
+          (t 10))))
