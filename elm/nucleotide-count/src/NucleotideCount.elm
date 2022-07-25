6244f16cc5795caab4a590c89319279245d64295
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-25 04:51:17 -0400

    nt

--- elm/nucleotide-count/src/NucleotideCount.elm
+++ elm/nucleotide-count/src/NucleotideCount.elm
@@ -10,5 +10,12 @@ type alias NucleotideCounts =
 
 
 nucleotideCounts : String -> NucleotideCounts
-nucleotideCounts sequence =
-    Debug.todo "Please implement this function"
+nucleotideCounts s =
+    String.foldl (\c acc ->
+      case c of
+        'A' -> { acc | a = acc.a + 1 }
+        'T' -> { acc | t = acc.t + 1 }
+        'C' -> { acc | c = acc.c + 1 }
+        'G' -> { acc | g = acc.g + 1 }
+        _ -> acc
+      ) (NucleotideCounts 0 0 0 0) s
