8375fe97881c78a64a18e3e6a2c96d9f13152d14
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-25 04:56:55 -0400

    bob

--- elm/bob/src/Bob.elm
+++ elm/bob/src/Bob.elm
@@ -1,6 +1,16 @@
 module Bob exposing (hey)
+import String exposing (toUpper, trim, isEmpty, any, endsWith)
+import Char exposing (isAlpha)
 
-
+isShouting s = (s |> any isAlpha) && (s == (s |> toUpper))
 hey : String -> String
-hey remark =
-    Debug.todo "Please implement this function"
+hey =
+    trim >>
+    (\ s ->
+        case (endsWith "?" s, isShouting s, isEmpty s) of
+            (_, _, True) -> "Fine. Be that way!"
+            (True, False, False) -> "Sure."
+            (False, True, False) -> "Whoa, chill out!"
+            (True, True, False) -> "Calm down, I know what I'm doing!"
+            _ -> "Whatever."
+    )
