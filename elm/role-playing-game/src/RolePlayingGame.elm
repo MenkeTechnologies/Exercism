d3023142411b9fdd9920323a6f4e5fcc5334de99
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-25 04:44:44 -0400

    rpg

--- elm/role-playing-game/src/RolePlayingGame.elm
+++ elm/role-playing-game/src/RolePlayingGame.elm
@@ -11,14 +11,28 @@ type alias Player =
 
 introduce : Player -> String
 introduce { name } =
-    Debug.todo "Please implement this function"
+    name |> Maybe.withDefault "Mighty Magician"
+
 
 
 revive : Player -> Maybe Player
-revive player =
-    Debug.todo "Please implement this function"
+revive ({health,level} as character) =
+    if health > 0 then
+        Nothing
+    else if level >= 10 then
+        Just {character | health = 100, mana = Just 100}
+    else
+        Just {character | health = 100, mana = Nothing}
 
 
 castSpell : Int -> Player -> ( Player, Int )
 castSpell manaCost player =
-    Debug.todo "Please implement this function"
+    case player.mana of
+        Nothing ->
+            ({player | health = max(player.health - manaCost) 0}, 0)
+        Just mana ->
+            if mana >= manaCost then
+                ( {player | mana = Just (mana - manaCost)}
+                , manaCost * 2)
+            else
+                (player, 0)
