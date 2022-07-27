a76a0f7be254bc95ac8afb34afc38baf04f498ac
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-27 00:16:56 -0400

    allergies

--- php/allergies/Allergies.php
+++ php/allergies/Allergies.php
@@ -1,51 +1,43 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
+class Allergen
+{
+    public const EGGS = 1;
+    public const PEANUTS = 2;
+    public const SHELLFISH = 4;
+    public const STRAWBERRIES = 8;
+    public const TOMATOES = 16;
+    public const CHOCOLATE = 32;
+    public const POLLEN = 64;
+    public const CATS = 128;
 
-declare(strict_types=1);
+    public function __construct(private int $score)
+    {
+    }
+    public static function allergenList(): array
+    {
+        return array_map(static fn ($allergen) => new Allergen($allergen),
+            (new ReflectionClass(__CLASS__))->getConstants());
+    }
+    public function getScore(): int
+    {
+        return $this->score;
+    }
+}
 
 class Allergies
 {
-    public function __construct(int $score)
+    public function __construct(private int $mask)
     {
-        throw new \BadMethodCallException("Implement the __construct method");
     }
 
     public function isAllergicTo(Allergen $allergen): bool
     {
-        throw new \BadMethodCallException("Implement the isAllergicTo method");
+        return $this->mask & $allergen->getScore();
     }
-
     public function getList(): array
     {
-        throw new \BadMethodCallException("Implement the getList method");
+        return array_filter(Allergen::allergenList(), fn ($allergen) => $this->isAllergicTo($allergen));
     }
 }
 
-class Allergen
-{
-    public static function allergenList(): array
-    {
-        throw new \BadMethodCallException("Implement the allergenList method");
-    }
-}
