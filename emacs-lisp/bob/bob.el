1b13f86ebbdf34ef25d2a79172236e6a03061588
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-28 02:07:31 -0400

    bob

--- emacs-lisp/bob/bob.el
+++ emacs-lisp/bob/bob.el
@@ -1,9 +1,18 @@
-;;; bob.el --- Bob exercise (exercism)
-
-;;; Commentary:
-
-;;; Code:
-
-
 (provide 'bob)
-;;; bob.el ends here
+
+(require 'subr-x)
+
+(defun response-for (input)
+  ""
+  (let* ((nows (string-trim input))
+        (question? (string-suffix-p "?" nows))
+        (yell? (and (string-match-p "[a-z]\\|[A-Z]" nows)
+                    (string= (upcase nows) nows))))
+    (cond ((string= "" nows)
+           "Fine. Be that way!")
+          ((and question? yell?)
+           "Calm down, I know what I'm doing!")
+          (question? "Sure.")
+          (yell? "Whoa, chill out!")
+          (t "Whatever."))))
+
