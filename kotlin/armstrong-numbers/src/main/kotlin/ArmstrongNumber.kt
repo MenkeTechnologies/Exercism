9be2168dc3b5df5c65ceee38a198f5b0fef944a6
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-28 11:32:52 -0400

    arm

--- kotlin/armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
+++ kotlin/armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
@@ -2,11 +2,11 @@ import kotlin.math.pow
 
 object ArmstrongNumber {
 
-    fun check(input: Int): Boolean {
-        val ary = input.toString().map{ it.toString()}
-
-        return ary.map { it.toDouble().pow(ary.size) }.sum().toInt() == input
-
-    }
+    fun check(input: Int) = input.toString()
+            .map {
+                it.toString().toDouble()
+                    .pow(input.toString().length)
+                    .toInt()
+            }.sum() == input
 
 }
