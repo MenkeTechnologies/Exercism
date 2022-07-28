36d8e964d655e6f22dfc9a2ee583f89d3206693c
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-28 01:29:58 -0400

    acc

--- purescript/accumulate/src/Accumulate.purs
+++ purescript/accumulate/src/Accumulate.purs
@@ -1,3 +1,9 @@
 module Accumulate
   ( accumulate
   ) where
+
+import Data.List (List(..), (:))
+
+accumulate :: forall a b. (a -> b) -> List a -> List b
+accumulate _ Nil = Nil
+accumulate f (x : xs) = f x : accumulate f xs
