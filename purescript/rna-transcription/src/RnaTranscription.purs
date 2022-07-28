a872e4ca6407ab01400428cb64cb3604ddbe7c2e
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-28 01:44:55 -0400

    rna

--- purescript/rna-transcription/src/RnaTranscription.purs
+++ purescript/rna-transcription/src/RnaTranscription.purs
@@ -1,3 +1,20 @@
 module RnaTranscription
   ( toRNA
   ) where
+
+
+import Prelude
+import Data.Maybe (Maybe(..))
+import Data.String.CodeUnits (fromCharArray, toCharArray)
+import Data.Traversable (sequence, traverse)
+
+toRNA :: String -> Maybe String
+toRNA = map fromCharArray <<< traverse toRNAChar <<< toCharArray
+
+toRNAChar :: Char -> Maybe Char
+toRNAChar c = case c of
+      'G' -> Just 'C'
+      'C' -> Just 'G'
+      'T' -> Just 'A'
+      'A' -> Just 'U'
+      _ -> Nothing
