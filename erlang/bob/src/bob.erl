2524ba637371f0892c03a867a26feb33cea46f8a
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-29 00:01:40 -0400

    bob

--- erlang/bob/src/bob.erl
+++ erlang/bob/src/bob.erl
@@ -2,5 +2,20 @@
 
 -export([response/1]).
 
+response(S) ->
+    TRIMMED = string:trim(S, trailing),
+    case TRIMMED of
+      [] -> "Fine. Be that way!";
+      _ -> response(is_question(TRIMMED), is_yelling(TRIMMED))
+    end.
 
-response(_String) -> undefined.
+response(true, true) ->
+    "Calm down, I know what I'm doing!";
+response(true, _) -> "Sure.";
+response(_, true) -> "Whoa, chill out!";
+response(_, _) -> "Whatever.".
+
+is_question(S) -> $? =:= lists:last(S).
+is_yelling(S) ->
+    string:uppercase(S) =:= S andalso
+      not (string:lowercase(S) =:= S).
