9a905354f6257927d8c0dce5f13282118b2d274c
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-29 00:00:57 -0400

    pn

--- erlang/perfect-numbers/src/perfect_numbers.erl
+++ erlang/perfect-numbers/src/perfect_numbers.erl
@@ -3,4 +3,15 @@
 -export([classify/1]).
 
 
-classify(_Number) -> undefined.
+factors(Number) ->
+  [X || X <- lists:seq(1, trunc(Number / 2)), Number rem X == 0].
+
+aliquot_sum(Number) ->
+  lists:sum(factors(Number)).
+
+classify(Number) when Number > 0 -> classify(Number, aliquot_sum(Number)).
+
+classify(Number, Sum) when Number < Sum -> abundant;
+classify(Number, Sum) when Number > Sum -> deficient;
+classify(_, _) -> perfect.
+
