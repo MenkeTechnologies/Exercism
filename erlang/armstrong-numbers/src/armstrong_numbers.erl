ad2ef1549847e73cfb7f1f72a9b80d6cc50516d4
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-29 00:01:12 -0400

    arm

--- erlang/armstrong-numbers/src/armstrong_numbers.erl
+++ erlang/armstrong-numbers/src/armstrong_numbers.erl
@@ -2,5 +2,12 @@
 
 -export([is_armstrong_number/1]).
 
+is_armstrong_number(N) -> N =:= armstrong_sum(N).
 
-is_armstrong_number(_Number) -> undefined.
+armstrong_sum(N) ->
+  Digits = digits(N),
+  Len = length(Digits),
+  lists:foldl(fun(X, Acc) -> Acc + trunc(math:pow(X, Len)) end, 0, Digits).
+
+digits(N) ->
+  [X - $0 || X <- integer_to_list(N)].
