c53cec578f1674aa31ea47277f4fb9088f992c35
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-29 23:44:17 -0400

    rna

--- julia/rna-transcription/rna-transcription.jl
+++ julia/rna-transcription/rna-transcription.jl
@@ -1,4 +1,8 @@
-function to_rna(dna)
-
+function to_rna(dna::AbstractString)
+ dict = Dict('A' => 'U', 'C' => 'G', 'G' => 'C', 'T' => 'A');
+ try
+   map(nucleotide->dict[nucleotide], dna)
+ catch error
+   throw(ErrorException("Invalid nucleotide"));
+ end
 end
-
