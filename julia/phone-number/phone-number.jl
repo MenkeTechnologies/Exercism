17060c83ea23f48448363defaea738e5a9cef1e5
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-30 00:22:04 -0400

    bob

--- julia/phone-number/phone-number.jl
+++ julia/phone-number/phone-number.jl
@@ -1,3 +1,8 @@
 function clean(phone_number)
-    
+    m = match(r"\s*(?:\+?1?\s*)?\(?([2-9]\d{2})\)?[-.\s]*([2-9]\d{2})[-.\s]*(\d{4,})\s*", phone_number)
+    (m === nothing || length(m.captures) != 3) && throw(ArgumentError(""))
+    res = join(m.captures)
+    length(res) != 10 && throw(ArgumentError(""))
+    res
 end
+
