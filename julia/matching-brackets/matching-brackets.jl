9a79559cc79b63405be8a0436ef303d1a5cef3d5
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-30 00:24:58 -0400

    mb

--- julia/matching-brackets/matching-brackets.jl
+++ julia/matching-brackets/matching-brackets.jl
@@ -1,1 +1,14 @@
-
+function matching_brackets(s)
+    bpairs = Dict('[' => ']', '(' => ')', '{' => '}')
+    stk = Char[]
+    for c in s
+        if c ∈ keys(bpairs)
+            push!(stk, c)
+        elseif c ∈ values(bpairs)
+            if isempty(stk) || bpairs[pop!(stk)] != c
+                return false
+            end
+        end
+    end
+    isempty(stk)
+end
