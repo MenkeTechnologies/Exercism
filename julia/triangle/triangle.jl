4db6b1f85657692a97e5bca7e32f5661f56165c0
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-30 00:15:14 -0400

    tri

--- julia/triangle/triangle.jl
+++ julia/triangle/triangle.jl
@@ -1,8 +1,16 @@
+function is_valid(sides)
+    a, b, c = sort(sides)
+    a > 0 && a + b > c
+end
+
 function is_equilateral(sides)
+    Set(sides) |> length == 1 && is_valid(sides)
 end
 
 function is_isosceles(sides)
+    Set(sides) |> length <= 2 && is_valid(sides)
 end
 
 function is_scalene(sides)
+    Set(sides) |> length == 3 && is_valid(sides)
 end
