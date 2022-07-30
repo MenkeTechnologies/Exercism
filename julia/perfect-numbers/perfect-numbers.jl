d2a637c9b43ede36e2af75a20829511e8cf1a80b
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-30 00:07:48 -0400

    pn

--- julia/perfect-numbers/perfect-numbers.jl
+++ julia/perfect-numbers/perfect-numbers.jl
@@ -1,1 +1,7 @@
+aliquotsum(n) = isindomain(n) && sum([x for x in 1:(div(n, 2)) if rem(n, x) == 0])
 
+isindomain(n) = n > 0 || throw(DomainError("argument $n is not a positive natural number"))
+
+isperfect(n) = aliquotsum(n) == n
+isabundant(n) = aliquotsum(n) > n
+isdeficient(n) = aliquotsum(n) < n
