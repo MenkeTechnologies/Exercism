e4d8125745bdb8c74d879ffe7b7560a1760655b5
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-29 23:27:13 -0400

    leap

--- cobol/leap/src/leap.cob
+++ cobol/leap/src/leap.cob
@@ -10,4 +10,4 @@
          IF FUNCTION MOD ( WS-YEAR, 4 ) = 0 AND FUNCTION MOD ( WS-YEAR, 100 ) <> 0 OR FUNCTION MOD ( WS-YEAR, 400 ) = 0
             MOVE 1 TO WS-RESULT
          ELSE
-            MOVE 0 TO WS-RESULT.
+            MOVE 0 TO WS-RESULT. 
