3739ac5df21646f2332dbfda1ab7bb7f324e873f
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-29 23:38:21 -0400

    rd

--- julia/raindrops/raindrops.jl
+++ julia/raindrops/raindrops.jl
@@ -1,3 +1,8 @@
-function raindrops(number)
-
+function raindrops(number::Int)
+    dict = [(3,"Pling"), (5,"Plang"), (7,"Plong")]
+    r = Base.mapfoldl(
+        pair -> number % first(pair) == 0 ? last(pair) : "",
+        *,
+        dict)
+    isempty(r) ? string(number) : r
 end
