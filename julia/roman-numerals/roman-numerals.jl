9af04f22a6de8331b8d25b84d5d83c7e220751d9
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-30 00:29:34 -0400

    rn

--- julia/roman-numerals/roman-numerals.jl
+++ julia/roman-numerals/roman-numerals.jl
@@ -1,3 +1,15 @@
-function to_roman(number)
+nums = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"),
+        (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
 
+function to_roman(number::Integer)
+    if number < 1
+        error("$number must be positive")
+    end
+
+    r = Vector{String}()
+    for (n, s) in nums
+        push!(r, repeat(s, div(number, n)))
+        number = number % n
+    end
+    prod(r)
 end
