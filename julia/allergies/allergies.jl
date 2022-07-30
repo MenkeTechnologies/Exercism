c72fd431cb56a5bf984db5663b19c53c72b1df54
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-29 23:58:46 -0400

    allergies

--- julia/allergies/allergies.jl
+++ julia/allergies/allergies.jl
@@ -1,7 +1,10 @@
+const ALLERGIES = Dict("eggs" => 1, "peanuts" => 2, "shellfish" => 4, "strawberries" => 8,
+  "tomatoes" => 16, "chocolate" => 32, "pollen" => 64, "cats" => 128)
+
 function allergic_to(score, allergen)
-
+  score & ALLERGIES[allergen] > 0
 end
-
 function allergy_list(score)
-    
+    Set(k for (k, v) in ALLERGIES if allergic_to(score, k))
 end
+
