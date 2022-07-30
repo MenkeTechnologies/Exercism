c8d0e8a7be1dec18642028c4790927d789723b90
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-30 00:11:25 -0400

    ana

--- julia/anagram/anagram.jl
+++ julia/anagram/anagram.jl
@@ -1,3 +1,6 @@
-function detect_anagrams(subject, candidates)
-
+function detect_anagrams(target::AbstractString, candidates::AbstractArray)
+    lcsort = s -> s |> lowercase |> collect |> sort
+    sorted = lcsort(target)
+    lc = lowercase(target)
+    filter(s -> lowercase(s) != lc && sorted == lcsort(s), candidates)
 end
