cba37162c26ff3525b4c8d3aebc5d2c5f3a362c9
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-30 00:22:56 -0400

    sh

--- julia/secret-handshake/secret-handshake.jl
+++ julia/secret-handshake/secret-handshake.jl
@@ -1,3 +1,12 @@
-function secret_handshake(code)
+const signals = [
+    "wink" => 1,
+    "double blink" => 2,
+    "close your eyes" => 4,
+    "jump" => 8
+]
+const reverse_signal = 16
 
+function secret_handshake(code::Integer)
+    r = map(p -> p.first, filter(p -> p.second & code > 0, signals))
+    code & reverse_signal != 0 ? reverse(r) : r
 end
