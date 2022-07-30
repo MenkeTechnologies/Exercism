ea5e1618b5c2672b6453ad39051049f65e9c29cc
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-30 00:23:21 -0400

    bob

--- julia/bob/bob.jl
+++ julia/bob/bob.jl
@@ -1,3 +1,18 @@
-function bob(stimulus)
-
+function bob(s::AbstractString)::AbstractString
+    nows = strip(s)
+    silence = isempty(nows)
+    question = endswith(nows, '?')
+    yelling = nows == uppercase(nows) && any(isuppercase, nows)
+    if yelling && question
+        "Calm down, I know what I'm doing!"
+    elseif yelling
+        "Whoa, chill out!"
+    elseif question
+        "Sure."
+    elseif silence
+        "Fine. Be that way!"
+    else
+        "Whatever."
+    end
 end
+
