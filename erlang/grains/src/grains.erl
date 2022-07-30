f06fd0e898c1137c31a4aa1b4bf428a63679473f
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-30 00:44:44 -0400

    grains

--- erlang/grains/src/grains.erl
+++ erlang/grains/src/grains.erl
@@ -1,8 +1,8 @@
 -module(grains).
-
 -export([square/1, total/0]).
 
+square(Square) when 1 =< Square, Square =< 64 -> 1 bsl (Square - 1);
 
-square(_Square) -> undefined.
+square(_) -> {error, "square must be between 1 and 64"}.
 
-total() -> undefined.
+total() -> lists:foldl(fun(N, ACC) -> square(N) + ACC end, 0, lists:seq(1, 64)).
