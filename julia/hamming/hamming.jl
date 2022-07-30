0784850209a5b987dbc2fbdb26638807585d1da6
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-29 23:42:25 -0400

    hamm

--- julia/hamming/hamming.jl
+++ julia/hamming/hamming.jl
@@ -1,4 +1,4 @@
-"Your optional docstring here"
-function distance(a, b)
-
+function distance(s1::AbstractString, s2::AbstractString)
+  length(s1) == length(s2) || throw(ArgumentError("Length mismatch"))
+    map(!=, s1, s2) |> sum
 end
