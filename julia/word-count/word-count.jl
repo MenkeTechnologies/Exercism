a770f62f1eb0db431f8c9ea5df618a4c5dc0656f
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-30 00:32:07 -0400

    wc

--- julia/word-count/word-count.jl
+++ julia/word-count/word-count.jl
@@ -1,3 +1,13 @@
-function wordcount(sentence)
-
+function wordcount(s::AbstractString)
+    lowercase_sentence = lowercase(s)
+    dict = Dict()
+    word = r"(\w+('\w)?)"
+    for w in eachmatch(word, lowercase_sentence)
+        if !haskey(dict, w.match)
+            dict[w.match] = 1
+        else
+            dict[w.match] += 1
+        end
+    end
+    return dict
 end
