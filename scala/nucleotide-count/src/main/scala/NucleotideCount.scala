2a702a549aeb6a726cadf54cc5abc1aa26b76f38
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-29 22:46:48 -0400

    nt

--- scala/nucleotide-count/src/main/scala/NucleotideCount.scala
+++ scala/nucleotide-count/src/main/scala/NucleotideCount.scala
@@ -1,27 +1,12 @@
-import scala.collection.mutable
+class DNA(val s: String) {
 
-class DNA(val str: String) {
-
-  def nucleotideCounts(): Either[Boolean, Map[Char, Int]] = {
-
-    val mm = mutable.Map(
-      'A' -> 0,
-      'T' -> 0,
-      'C' -> 0,
-      'G' -> 0,
-    )
-
-    val kl = mm.keys.toList
-    if (!str.forall(kl.contains(_))) {
-      return Left(false)
-    }
-
-    str.toCharArray.foreach(mm(_) += 1)
-
-
-    Right(Map() ++ mm)
-
-
-  }
+  def nucleotideCounts(): Either[Boolean, Map[Char, Int]] =
+    Right(s.foldLeft(Map('A' -> 0, 'T' -> 0, 'C' -> 0, 'G' -> 0))
+    { (acc, c) =>
+      acc.get(c) match {
+        case None => return Left(false)
+        case Some(v) => acc + (c -> (v + 1))
+      }
+    })
 
 }
