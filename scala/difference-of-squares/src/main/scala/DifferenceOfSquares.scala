a209c755573ab715efbd74bc15e8be12fd739187
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-31 00:19:43 -0400

    sum

--- scala/difference-of-squares/src/main/scala/DifferenceOfSquares.scala
+++ scala/difference-of-squares/src/main/scala/DifferenceOfSquares.scala
@@ -2,9 +2,9 @@ object DifferenceOfSquares {
 
   private def sq(b: Int) = b * b
 
-  def sumOfSquares(n: Int): Int = (1 to n).map(sq).sum
+  def sumOfSquares(n: Int) = (1 to n).map(sq).sum
 
-  def squareOfSum(n: Int): Int = sq((1 to n).sum)
+  def squareOfSum(n: Int) = sq((1 to n).sum)
 
-  def differenceOfSquares(n: Int): Int = squareOfSum(n) - sumOfSquares(n)
+  def differenceOfSquares(n: Int) = squareOfSum(n) - sumOfSquares(n)
 }
