67a99ef3688e2f5b3e3560f925cb5a9411c68397
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-31 00:10:55 -0400

    acro

--- php/acronym/Acronym.php
+++ php/acronym/Acronym.php
@@ -1,30 +1,9 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
-declare(strict_types=1);
-
-function acronym(string $text): string
+function acronym(string $input): string
 {
-    throw new \BadFunctionCallException("Implement the acronym function");
+    $subject = preg_replace('/([a-z])([A-Z])/', '$1 $2', $input);
+    preg_match_all("/\w+/u", $subject, $matches);
+    $acronyms = array_map(fn($s) => mb_strtoupper(mb_substr($s, 0, 1)), $matches[0]);
+    return count($acronyms) > 1 ? implode('', $acronyms) : '';
 }
