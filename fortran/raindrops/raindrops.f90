d3ae119793c428d97a3e4756ba51e8b302040abb
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-01 01:46:58 -0400

    rd

--- fortran/raindrops/raindrops.f90
+++ fortran/raindrops/raindrops.f90
@@ -1,11 +1,21 @@
 module raindrops
   implicit none
 contains
-
   function convert(i)
     integer :: i
-    character(20) :: convert
-
+    character(len=20) :: convert
+    convert=""
+    if (mod(i,3) == 0) then
+      convert = trim(convert) // "Pling"
+    end if
+    if (mod(i,5) == 0) then
+      convert = trim(convert) // "Plang"
+    end if
+    if (mod(i,7) == 0) then
+      convert = trim(convert) // "Plong"
+    end if
+    if (trim(convert) == "") then
+      write(convert,'(i0)') i;
+    end if
   end function convert
-
 end module raindrops
