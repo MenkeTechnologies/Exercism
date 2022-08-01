b539e6e95cc578bc93b9b846a0b38fd1bea223da
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-01 01:48:30 -0400

    dos

--- fortran/difference-of-squares/difference_of_squares.f90
+++ fortran/difference-of-squares/difference_of_squares.f90
@@ -2,9 +2,19 @@ module difference_of_squares
   implicit none
 contains
 
-  integer function difference(n)
-    integer :: n
+  pure integer function sum_of_squares(n)
+    integer, intent(in) :: n
+    sum_of_squares = n * (n + 1) * (2 * n + 1) / 6
+  end function sum_of_squares
 
+  pure integer function square_of_sum(n)
+    integer, intent(in) :: n
+    square_of_sum = (n * (n + 1) / 2) ** 2
+  end function square_of_sum
+
+  pure integer function difference(n)
+    integer, intent(in) :: n
+    difference = square_of_sum(n) - sum_of_squares(n)
   end function difference
 
 end module difference_of_squares
