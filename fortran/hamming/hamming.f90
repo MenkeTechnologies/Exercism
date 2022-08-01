93942b061e98b540b59f128a2272bdd0e9d180a9
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-01 01:41:58 -0400

    hamm

--- fortran/hamming/hamming.f90
+++ fortran/hamming/hamming.f90
@@ -1,12 +1,23 @@
 module hamming
   implicit none
 contains
-
-  function compute(strand1, strand2, distance)
-      character(*) :: strand1, strand2
-      integer :: distance
+  function compute(s1, s2, d)
+      character(*) :: s1, s2
+      integer :: d
       logical :: compute
-
+      integer:: i
+      d = 0
+      if (len(s1(:)) /= len(s2(:))) then
+        compute = .false.
+      else
+        do i=1, len(s1(:))
+          if (s1(i:i) /= s2(i:i)) then
+            d = d + 1
+          end if
+        end do
+        if (d == 0) then
+          compute = .true.
+        end if
+      end if
   end function compute
-
 end module hamming
