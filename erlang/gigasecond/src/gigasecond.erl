fa1e6475dc4f3931d5c2c864b2f5cc775e33c9e1
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-01 02:01:05 -0400

    gs

--- erlang/gigasecond/src/gigasecond.erl
+++ erlang/gigasecond/src/gigasecond.erl
@@ -1,6 +1,9 @@
 -module(gigasecond).
-
--export([from/1]).
-
-
-from(_From) -> undefined.
+-export([from/1, test_version/0]).
+from({Y, M, D}) ->
+    from({{Y, M, D}, {0, 0, 0}});
+from(DateTime) ->
+  Seconds = calendar:datetime_to_gregorian_seconds(DateTime),
+  GS = math:pow(10,9),
+  calendar:gregorian_seconds_to_datetime(Seconds + round(GS)).
+test_version() -> 1.
