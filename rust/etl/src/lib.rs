8f267745282816e8c9d0a44fda6c8d321276c2ee
MenkeTechnologies <linux.dev25@gmail.com>
2022-07-31 20:10:07 -0400

    etl

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,16 +1,14 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    let mut map = BTreeMap::<char, i32>::new();
+    let mut r = BTreeMap::<char, i32>::new();
 
-    for k in h.keys() {
-        if h.contains_key(k) {
-            for v in h.get(k).unwrap() {
-                let f = v.to_ascii_lowercase();
-                map.insert(f, *k);
+    for (&score, letters) in h {
+            for letter in letters {
+                let lc = letter.to_ascii_lowercase();
+                r.insert(lc, score);
             }
-        }
     }
 
-    map
+    r
 }
