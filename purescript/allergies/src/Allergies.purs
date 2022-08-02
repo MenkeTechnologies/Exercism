fb60ec45d6df9adcef91ac41a720a3ab9602a5ec
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-02 09:45:38 -0400

    allergies

--- purescript/allergies/src/Allergies.purs
+++ purescript/allergies/src/Allergies.purs
@@ -2,3 +2,28 @@ module Allergies
   ( allergicTo
   , list
   ) where
+import Data.Array (elemIndex, filter)
+import Data.Int.Bits (and, shl)
+import Data.Maybe (Maybe(..))
+import Prelude
+
+allergens :: Array String
+allergens = [ "eggs"
+            , "peanuts"
+            , "shellfish"
+            , "strawberries"
+            , "tomatoes"
+            , "chocolate"
+            , "pollen"
+            , "cats"]
+
+hasAllergy :: Int -> Int -> Boolean
+hasAllergy mask i = (1 `shl` i) `and` mask > 0
+
+allergicTo :: Int -> String -> Boolean
+allergicTo mask allergy = case allergy `elemIndex` allergens of
+  Nothing -> false
+  Just index -> hasAllergy mask index
+
+list :: Int -> Array String
+list mask = filter (allergicTo mask) allergens
