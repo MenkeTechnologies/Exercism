23f7d559ee66f00c1cacf78a8dc1b76db2b63b52
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-02 09:36:04 -0400

    rd

--- purescript/raindrops/src/Raindrops.purs
+++ purescript/raindrops/src/Raindrops.purs
@@ -1,3 +1,12 @@
 module Raindrops
   ( raindrops
   ) where
+import Prelude (($), (<$>), (==), mod, show)
+import Data.String (joinWith)
+import Data.Tuple (Tuple(..), snd)
+import Data.Array (filter)
+
+raindrops :: Int -> String
+raindrops n = if s == "" then show n else s
+  where s = joinWith "" $ snd <$> filter (\(Tuple d _) -> mod n d == 0)
+              [Tuple 3 "Pling", Tuple 5 "Plang", Tuple 7 "Plong"]
