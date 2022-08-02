91bda5b8aab5aea03a6954ca12da9ff2d676d23f
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-02 09:37:37 -0400

    coll

--- purescript/collatz-conjecture/src/CollatzConjecture.purs
+++ purescript/collatz-conjecture/src/CollatzConjecture.purs
@@ -1,3 +1,18 @@
-module CollatzConjecture
-  ( collatz
-  ) where
+module CollatzConjecture (collatz) where
+import Prelude
+import Data.Int (even)
+import Data.List.Lazy (iterate, length, takeWhile)
+import Data.Maybe (Maybe(..))
+
+collatz ∷ Int -> Maybe Int
+collatz n
+  | n < 1     = Nothing
+  | otherwise = Just
+                <<< length
+                <<< takeWhile (_ > 1)
+                $ iterate collatzStep n
+
+collatzStep :: Int -> Int
+collatzStep n
+  | even n = n / 2
+  | otherwise = 3 * n + 1
