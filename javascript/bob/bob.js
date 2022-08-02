3c6d0944592f7cd381a05544a78fe2fc41eb9540
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-01 22:49:03 -0400

    bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,20 +1,15 @@
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const hey = (s) => {
 
-export const hey = (message) => {
+  s = s.replace(/\s+/g, '')
 
-  message = message.replace(/\s+/g, '')
-
-  if (message.length === 0){
+  if (s.length === 0){
     return 'Fine. Be that way!'
   }
 
-  if (message.match(/[A-Z]/) && !message.match(/[a-z]/)) {
-     return message.match(/\?$/) ? "Calm down, I know what I'm doing!" : 'Whoa, chill out!'
+  if (s.match(/[A-Z]/) && !s.match(/[a-z]/)) {
+     return s.match(/\?$/) ? "Calm down, I know what I'm doing!" : 'Whoa, chill out!'
   }
 
-  return message.match(/\?$/) ? 'Sure.' : 'Whatever.'
+  return s.match(/\?$/) ? 'Sure.' : 'Whatever.'
 
 };
