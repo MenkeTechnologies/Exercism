509efe51b2b269671c99f8263de9ff3712e96d9b
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-03 10:00:27 -0400

    more exe

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,5 +1,16 @@
 defmodule BoutiqueSuggestions do
-  def get_combinations(tops, bottoms, options) do
-    # Please implement the get_combinations/3 function
+  def get_combinations(tops, bottoms, opts \\ []) do
+    do_get_combinations(tops, bottoms, opts)
+  end
+
+  defp do_get_combinations(tops, bottoms, opts) do
+    max_price = Keyword.get(opts, :maximum_price, 100.0)
+    for top <- tops,
+        bottom <- bottoms,
+        top.base_color != bottom.base_color,
+        top.price + bottom.price < max_price,
+        do: {top, bottom},
+        into: []
   end
 end
+
---
+++ elixir/rectangles/test/rectangles_test.exs
@@ -0,0 +1,165 @@
+defmodule RectanglesTest do
+  use ExUnit.Case
+
+  # 
+  test "no rows" do
+    input = ""
+
+    assert Rectangles.count(input) == 0
+  end
+
+  
+  test "no columns" do
+    input = """
+    """
+
+    assert Rectangles.count(input) == 0
+  end
+
+  
+  test "no rectangles" do
+    input = """
+    \s
+    """
+
+    assert Rectangles.count(input) == 0
+  end
+
+  
+  test "one rectangle" do
+    input = """
+    +-+
+    | |
+    +-+
+    """
+
+    assert Rectangles.count(input) == 1
+  end
+
+  
+  test "two rectangles without shared parts" do
+    input = """
+      +-+
+      | |
+    +-+-+
+    | | \s
+    +-+ \s
+    """
+
+    assert Rectangles.count(input) == 2
+  end
+
+  
+  test "five rectangles with shared parts" do
+    input = """
+      +-+
+      | |
+    +-+-+
+    | | |
+    +-+-+
+    """
+
+    assert Rectangles.count(input) == 5
+  end
+
+  
+  test "rectangle of height 1 is counted" do
+    input = """
+    +--+
+    +--+
+    """
+
+    assert Rectangles.count(input) == 1
+  end
+
+  
+  test "rectangle of width 1 is counted" do
+    input = """
+    ++
+    ||
+    ++
+    """
+
+    assert Rectangles.count(input) == 1
+  end
+
+  
+  test "1x1 square is counted" do
+    input = """
+    ++
+    ++
+    """
+
+    assert Rectangles.count(input) == 1
+  end
+
+  
+  test "only complete rectangles are counted" do
+    input = """
+      +-+
+        |
+    +-+-+
+    | | -
+    +-+-+
+    """
+
+    assert Rectangles.count(input) == 1
+  end
+
+  
+  test "rectangles can be of different sizes" do
+    input = """
+    +------+----+
+    |      |    |
+    +---+--+    |
+    |   |       |
+    +---+-------+
+    """
+
+    assert Rectangles.count(input) == 3
+  end
+
+  
+  test "corner is required for a rectangle to be complete" do
+    input = """
+    +------+----+
+    |      |    |
+    +------+    |
+    |   |       |
+    +---+-------+
+    """
+
+    assert Rectangles.count(input) == 2
+  end
+
+  
+  test "large input with many rectangles" do
+    input = """
+    +---+--+----+
+    |   +--+----+
+    +---+--+    |
+    |   +--+----+
+    +---+--+--+-+
+    +---+--+--+-+
+    +------+  | |
+              +-+
+    """
+
+    assert Rectangles.count(input) == 60
+  end
+
+  
+  test "rectangles must have four sides" do
+    input = """
+    +-+ +-+
+    | | | |
+    +-+-+-+
+      | |
+    +-+-+-+
+    | | | |
+    +-+ +-+
+    """
+
+    assert Rectangles.count(input) == 5
+  end
+end
