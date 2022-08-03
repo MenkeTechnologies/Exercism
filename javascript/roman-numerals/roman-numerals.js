1f95be5bc7bdba38d7c5f3e13bc30ec47f44761e
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-03 14:58:02 -0400

    rn

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,24 +1,9 @@
 const ROMAN_NUMERALS = [
-  [1000, 'M'],
-  [900, 'CM'],
-  [500, 'D'],
-  [400, 'CD'],
-  [100, 'C'],
-  [90, 'XC'],
-  [50, 'L'],
-  [40, 'XL'],
-  [10, 'X'],
-  [9, 'IX'],
-  [5, 'V'],
-  [4, 'IV'],
-  [1, 'I']
+  [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'],
+  [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
 ];
-export const toRoman = n => {
-  let acc = '';
-  for (const [val, s] of ROMAN_NUMERALS) {
-    acc += s.repeat(n / val)
-    n %= val;
-  }
-
-  return acc;
-};
+export const toRoman = n =>
+  ROMAN_NUMERALS.reduce(([acc_str, acc_remainder], [val, digits]) =>
+      [acc_str + digits.repeat(acc_remainder / val), acc_remainder % val],
+    ["", n]
+  )[0];
