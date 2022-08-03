7af39bbb215fe05737af7aa6d41bf2f659a2a5d4
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-03 09:58:59 -0400

    gs

---
+++ elixir/gigasecond/lib/gigasecond.ex
@@ -0,0 +1,6 @@
+defmodule Gigasecond do
+  def from({{year, month, day}, {hours, minutes, seconds}}) do
+      with dt <- NaiveDateTime.new!(Date.new!(year, month, day), Time.new!(hours, minutes, seconds)) |> NaiveDateTime.add(Integer.pow(10,9), :second),
+      do: {{dt.year, dt.month, dt.day}, {dt.hour, dt.minute, dt.second}}
+  end
+end
