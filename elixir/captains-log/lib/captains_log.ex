7b8c9d5edf474802ac81e99a24656a4366edc98a
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-03 23:59:22 -0400

    capt

--- elixir/captains-log/lib/captains_log.ex
+++ elixir/captains-log/lib/captains_log.ex
@@ -1,19 +1,21 @@
 defmodule CaptainsLog do
   @planetary_classes ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
+  @start 41000.0
+  @finish 42000.0
 
   def random_planet_class() do
-    # Please implement the random_planet_class/0 function
+    Enum.random(@planetary_classes)
   end
 
   def random_ship_registry_number() do
-    # Please implement the random_ship_registry_number/0 function
+    "NCC-#{Enum.random((1000..9999))}"
   end
 
   def random_stardate() do
-    # Please implement the random_stardate/0 function
+    :random.uniform() * (@finish - @start) + @start
   end
 
   def format_stardate(stardate) do
-    # Please implement the format_stardate/1 function
+    "~.1f" |> :io_lib.format([stardate]) |> to_string()
   end
 end
