d98584d92302f8c480e97ec6b40330d2938f457e
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-04 00:02:37 -0400

    more exe

---
+++ elixir/grains/lib/grains.ex
@@ -0,0 +1,17 @@
+defmodule Grains do
+  import Bitwise
+
+  def square(n) when n <= 64 and n >= 1 do
+    {:ok, 1 <<< (n - 1)}
+  end
+
+  def square(_) do
+    {:error, "The requested square must be between 1 and 64 (inclusive)"}
+  end
+
+  def total do
+    {:ok,
+     1..64
+     |> Enum.reduce(0, &Kernel.elem(square(&1), 1) + &2)}
+  end
+end
