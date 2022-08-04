e242728e0be81d73082422a1e69e5c6eab58639d
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-03 23:58:26 -0400

    etl

--- elixir/etl/lib/etl.ex
+++ elixir/etl/lib/etl.ex
@@ -1,13 +1,7 @@
 defmodule ETL do
-  @doc """
-  Transforms an old Scrabble score system to a new one.
-
-  ## Examples
-
-    iex> ETL.transform(%{1 => ["A", "E"], 2 => ["D", "G"]})
-    %{"a" => 1, "d" => 2, "e" => 1, "g" => 2}
-  """
-  @spec transform(map) :: map
-  def transform(input) do
+ def transform(dict) do
+    dict
+    |> Enum.flat_map(fn {k, v} -> v |> Enum.map(&{String.downcase(&1), k}) end)
+    |> Map.new()
   end
 end
