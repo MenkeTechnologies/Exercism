186df62295a7301f4917a548802c6f6edc4f55c3
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-06 00:18:01 -0400

    log

--- elixir/log-parser/lib/log_parser.ex
+++ elixir/log-parser/lib/log_parser.ex
@@ -1,17 +1,20 @@
 defmodule LogParser do
-  def valid_line?(line) do
-    # Please implement the valid_line?/1 function
+  def valid_line?(s) do
+    s =~ ~r/^\[(DEBUG|INFO|WARNING|ERROR)\]/
   end
-
-  def split_line(line) do
-    # Please implement the split_line/1 function
+  def split_line(s) do
+    String.split(s, ~r/\<[~\*=-]*\>/)
   end
-
-  def remove_artifacts(line) do
-    # Please implement the remove_artifacts/1 function
+  def remove_artifacts(s) do
+    String.replace(s, ~r/end-of-line(\d+)/i, "")
   end
-
-  def tag_with_user_name(line) do
-    # Please implement the tag_with_user_name/1 function
+  def tag_with_user_name(s) do
+    match = Regex.run(~r/User\s+(\S+)/, s)
+    if match do
+      "[USER] #{Enum.at(match, 1)} " <> s
+    else
+      s
+    end
   end
 end
+
