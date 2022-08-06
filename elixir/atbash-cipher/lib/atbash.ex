0c86ed867bde70e342973ee36b235fe15d5f2cb1
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-06 00:14:06 -0400

    atbash

--- elixir/atbash-cipher/lib/atbash.ex
+++ elixir/atbash-cipher/lib/atbash.ex
@@ -1,17 +1,23 @@
 defmodule Atbash do
-  @doc """
-  Encode a given plaintext to the corresponding ciphertext
-
-  ## Examples
-
-  iex> Atbash.encode("completely insecure")
-  "xlnko vgvob rmhvx fiv"
-  """
-  @spec encode(String.t()) :: String.t()
-  def encode(plaintext) do
+  def encode(s) do
+    s
+    |> String.downcase()
+    |> String.replace(~r/\W/, "")
+    |> String.to_charlist()
+    |> Enum.map(&transform_char/1)
+    |> Enum.chunk_every(5)
+    |> Enum.map(&to_string/1)
+    |> Enum.join(" ")
   end
 
-  @spec decode(String.t()) :: String.t()
-  def decode(cipher) do
+  defp transform_char(c) when c in ?0..?9, do: c
+  defp transform_char(c), do: ?z - c + ?a
+
+  def decode(s) do
+    s
+    |> String.replace(" ", "")
+    |> String.to_charlist()
+    |> Enum.map(&transform_char/1)
+    |> to_string()
   end
 end
