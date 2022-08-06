067c521998cab126f2c8fe17ebb7cbce1489e191
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-06 00:05:10 -0400

    dnd

--- elixir/dnd-character/lib/dnd_character.ex
+++ elixir/dnd-character/lib/dnd_character.ex
@@ -1,25 +1,29 @@
 defmodule DndCharacter do
-  @type t :: %__MODULE__{
-          strength: pos_integer(),
-          dexterity: pos_integer(),
-          constitution: pos_integer(),
-          intelligence: pos_integer(),
-          wisdom: pos_integer(),
-          charisma: pos_integer(),
-          hitpoints: pos_integer()
-        }
-
   defstruct ~w[strength dexterity constitution intelligence wisdom charisma hitpoints]a
 
-  @spec modifier(pos_integer()) :: integer()
   def modifier(score) do
+    :math.floor((score - 10) / 2)
+    |> trunc
   end
 
-  @spec ability :: pos_integer()
   def ability do
+    for(_ <- 1..4, do: :rand.uniform(6))
+    |> Enum.sort()
+    |> Enum.drop(1)
+    |> Enum.sum()
   end
 
-  @spec character :: t()
   def character do
+    constitution = ability()
+
+    %DndCharacter{
+      strength: ability(),
+      dexterity: ability(),
+      constitution: constitution,
+      intelligence: ability(),
+      wisdom: ability(),
+      charisma: ability(),
+      hitpoints: 10 + modifier(constitution)
+    }
   end
 end
