b510d01cbd5e0ec350e39d7d7760ddca8d00c55a
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-07 10:32:15 -0400

    etl

--- purescript/etl/src/Etl.purs
+++ purescript/etl/src/Etl.purs
@@ -1,3 +1,20 @@
 module Etl
   ( transform
   ) where
+import Prelude
+import Data.Array (concatMap)
+import Data.Map (Map, fromFoldable, toUnfoldable)
+import Data.String (toLower)
+import Data.String.CodeUnits (fromCharArray, toCharArray)
+import Data.Tuple (Tuple(..))
+
+transform :: Map Int (Array Char) -> Map Char Int
+transform =
+    toUnfoldable >>>
+    concatMap convertOne >>>
+    fromFoldable
+  where
+    convertOne (Tuple n chars) = charArrayToLower chars # map (\c -> Tuple c n)
+
+charArrayToLower :: Array Char -> Array Char
+charArrayToLower = fromCharArray >>> toLower >>> toCharArray
