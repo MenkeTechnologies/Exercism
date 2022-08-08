80289cde95d5e06df802ea916e5c4dc573ca46e5
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-08 10:06:20 -0400

    iso

--- prolog/isogram/isogram.pl
+++ prolog/isogram/isogram.pl
@@ -1,1 +1,7 @@
-isogram(false).
+is_alpha(Char):-
+  char_type(Char, alpha).
+isogram(Str):-
+  string_lower(Str, Lower),
+  string_chars(Lower, Chars),
+  include(is_alpha, Chars, Letters),
+  is_set(Letters).
