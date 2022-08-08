416cff2610dd209407de46710a6144d47459b29d
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-08 09:54:34 -0400

    qa

--- prolog/queen-attack/queen_attack.pl
+++ prolog/queen-attack/queen_attack.pl
@@ -1,11 +1,7 @@
-% Please visit https://exercism.io/tracks/prolog/installation
-% for instructions on setting up prolog.
-% Visit https://exercism.io/tracks/prolog/tests
-% for help running the tests for prolog exercises.
+create((X, Y)) :-
+    maplist(between(0, 7), [X, Y]).
+attack((X, _), (X, _)) :- !.
+attack((_, Y), (_, Y)) :- !.
+attack((X1, Y1), (X2, Y2)) :-
+    abs(X1 - X2) =:= abs(Y1 - Y2).
 
-% Replace the goal below with
-% your implementation.
-
-create(false).
-
-attack(false).
