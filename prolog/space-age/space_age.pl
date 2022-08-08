eb49562d6c902d8598c32cd9828d1c256d867bf8
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-08 10:12:03 -0400

    sa

--- prolog/space-age/space_age.pl
+++ prolog/space-age/space_age.pl
@@ -1,1 +1,11 @@
-space_age(Planet, AgeSec, Years).
+orbital_period("Earth", 1.0).
+orbital_period("Mercury", 0.2408467).
+orbital_period("Venus", 0.61519726).
+orbital_period("Mars", 1.8808158).
+orbital_period("Jupiter", 11.862615).
+orbital_period("Saturn", 29.447498).
+orbital_period("Uranus", 84.016846).
+orbital_period("Neptune", 164.79132).
+space_age(Planet, AgeSec, Years) :-
+	orbital_period(Planet, Period),
+	Years is AgeSec / 31557600 / Period.
