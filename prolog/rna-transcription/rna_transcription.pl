d22a767aed3a2a0630f9173f1047ea33babff4c0
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-08 09:56:58 -0400

    rna

--- prolog/rna-transcription/rna_transcription.pl
+++ prolog/rna-transcription/rna_transcription.pl
@@ -1,1 +1,8 @@
-rna_transcription(Rna, Dna).
+rna_transcription(Dna, Rna) :-
+    string_chars(Dna, Xs),
+    maplist(dna_rna, Xs, Ys),
+    string_chars(Rna, Ys).
+dna_rna('G', 'C').
+dna_rna('C', 'G').
+dna_rna('T', 'A').
+dna_rna('A', 'U').
