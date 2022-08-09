3788f400b88c4f24b3967484aeb69d1cf8f616fe
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-09 00:57:00 -0400

    secret

--- typescript/secret-handshake/secret-handshake.ts
+++ typescript/secret-handshake/secret-handshake.ts
@@ -1,3 +1,13 @@
-export function commands() {
-  throw new Error('Remove this statement and implement this function')
-}
+const dict: Map<number, string> = new Map([
+    [1, 'wink'],
+    [2, 'double blink'],
+    [4, 'close your eyes'],
+    [8, 'jump'],
+]);
+const rvs = 16;
+
+export const commands = (cmd: number = 0): string[] =>
+    [...[...dict.keys()].filter(k => (cmd & k) > 0).map(k => dict.get(k)!), cmd & rvs]
+        .reduce((acc, n) =>
+                typeof n === 'string' ? [...acc, n] : n > 0 ? acc.reverse() : acc
+            , [] as string[])
