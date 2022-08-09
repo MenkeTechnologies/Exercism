65da5c1a14aa5dea38fd56d664ae75c4f249971e
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-09 00:26:50 -0400

    pn

--- typescript/phone-number/phone-number.ts
+++ typescript/phone-number/phone-number.ts
@@ -1,3 +1,22 @@
-export function clean() {
-  throw new Error('Remove this statement and implement this function')
+export const clean = (phone: string) => {
+
+  let m = phone.match(/^\s*\+?(\d)?\s*\(?(\d{3})\)?[-.\s]*(\d{3})[-.\s]*(\d{4})\s*$/);
+
+  if (phone.length < 10) throw new Error("Incorrect number of digits")
+
+  if (m) {
+    if (m[1] && m[1] !== '1') throw new Error("11 digits must start with 1")
+
+    if (m[2][0] === '1') throw new Error('Area code cannot start with one')
+    if (m[2][0] === '0') throw new Error('Area code cannot start with zero')
+    if (m[3][0] === '1') throw new Error('Exchange code cannot start with one')
+    if (m[3][0] === '0') throw new Error('Exchange code cannot start with zero')
+
+    return `${m[2]}${m[3]}${m[4]}`
+  }
+
+  if (phone.toLowerCase().match(/[a-z]/)) throw new Error("Letters not permitted")
+  if (phone.replace(/[\d()+-]/g, '').length > 0) throw new Error("Punctuations not permitted")
+  if (phone.length > 10) throw new Error("More than 11 digits")
+
 }
