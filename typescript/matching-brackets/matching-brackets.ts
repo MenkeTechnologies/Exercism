8ca1d35b70cd2c58d2c370c1388462eeaccd6320
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-09 00:23:56 -0400

    mb

--- typescript/matching-brackets/matching-brackets.ts
+++ typescript/matching-brackets/matching-brackets.ts
@@ -1,3 +1,21 @@
-export function isPaired(input: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+const MAP : {[k: string]: string} = {
+  '[': ']',
+  '(': ')',
+  '{': '}'
 }
+export const isPaired = (str : string) => {
+  const stk = []
+  for (const n of str.split('')) {
+
+    if (MAP[n]) {
+      stk.push(n)
+    } else if (Object.values(MAP).indexOf(n) !== -1) {
+      if (!stk.length || MAP[stk.pop()!] !== n) {
+        return false;
+      }
+    }
+
+  }
+
+  return stk.length === 0;
+};
