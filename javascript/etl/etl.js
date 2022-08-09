26afaf280fdce1502b1011a9fa28abb3c8bedadb
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-09 14:00:18 -0400

    clean

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,9 +1,4 @@
-//
-// This is only a SKELETON file for the 'ETL' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const transform = (dict) =>
+export const transform = dict =>
   Object.entries(dict).reduce((acc1, [num,letters]) =>
       ({...acc1, ...(letters.reduce((acc2, letter) => ({...acc2, [letter.toLowerCase()]: parseInt(num)}), {}))})
     , {});
--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,2 +1,2 @@
-export const isPangram = (str) =>
-  [...Array(26).keys()].map(i => String.fromCharCode('A'.charCodeAt(0) + i)).every(c => str.toUpperCase().includes(c));
+export const isPangram = s =>
+  [...Array(26).keys()].map(i => String.fromCharCode('A'.charCodeAt(0) + i)).every(c => s.toUpperCase().includes(c));
