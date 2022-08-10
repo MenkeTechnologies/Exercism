d5f4e3b35366eca610eb89fc0189b849b7dde141
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-10 09:16:12 -0400

    hamm

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,4 +1,11 @@
 module Hamming (distance) where
 
+boolToInt :: Bool -> Int
+boolToInt b = if b then 1 else 0
+
 distance :: String -> String -> Maybe Int
-distance xs ys = error "You need to implement this function."
+distance a b =
+    if length a /= length b
+        then Nothing
+    else
+        Just $ sum $ map boolToInt $ zipWith (/=) a b
