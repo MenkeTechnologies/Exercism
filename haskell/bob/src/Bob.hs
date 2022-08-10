1020b0d46f269f7aca1b969001d2097c3f4060ac
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-10 09:24:38 -0400

    bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,15 @@
 module Bob (responseFor) where
+import Data.Char
 
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor input
+    | null nows = "Fine. Be that way!"
+    | isShouting && isAsking = "Calm down, I know what I'm doing!"
+    | isShouting = "Whoa, chill out!"
+    | isAsking = "Sure."
+    | otherwise = "Whatever."
+  where
+    nows = filter (not . isSpace) input
+    letters = filter isLetter nows
+    isShouting = all isUpper letters && any isUpper letters
+    isAsking = last nows == '?'
