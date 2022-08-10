02241d1aac6cf6ef3008a3363e14325cea6f7677
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-10 14:35:19 -0400

    secret

--- haskell/secret-handshake/src/SecretHandshake.hs
+++ haskell/secret-handshake/src/SecretHandshake.hs
@@ -1,4 +1,8 @@
 module SecretHandshake (handshake) where
+import Data.Bits (testBit)
 
 handshake :: Int -> [String]
-handshake n = error "You need to implement this function."
+handshake n = (if (testBit n 4) then reverse else id)
+              $ map fst
+              $ filter (\pair -> testBit n $ snd pair)
+              $ zip ["wink","double blink","close your eyes","jump"] [0..]
