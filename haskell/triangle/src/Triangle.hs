4295432d3304ee7897fcbc51a0990d3446f0c2b2
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-11 09:53:21 -0400

    tri

--- haskell/triangle/src/Triangle.hs
+++ haskell/triangle/src/Triangle.hs
@@ -1,4 +1,5 @@
 module Triangle (TriangleType(..), triangleType) where
+import Data.List
 
 data TriangleType = Equilateral
                   | Isosceles
@@ -6,5 +7,14 @@ data TriangleType = Equilateral
                   | Illegal
                   deriving (Eq, Show)
 
-triangleType :: (Num a, Ord a) => a -> a -> a -> TriangleType
-triangleType a b c = error "You need to implement this function."
+triangleType :: Float -> Float -> Float -> TriangleType
+triangleType a b c
+    | x + y < z || x == 0 = Illegal
+    | x == y && y == z = Equilateral
+    | x == y || y == z = Isosceles
+    | otherwise = Scalene
+    where
+      l = sort [a, b, c]
+      x = l !! 0
+      y = l !! 1
+      z = l !! 2
