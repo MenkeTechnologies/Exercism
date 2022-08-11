87bf096669d0f86377dc2dbf8810941074f5f2dc
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-11 10:58:25 -0400

    grains

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -6,4 +6,4 @@ square :: Integer -> Maybe Integer
 square n = if elem n [1..64] then Just $ 2^(n - 1) else Nothing
 
 total :: Integer
-total = sum (map ((fromMaybe 0) . square) [1..64])
+total = sum $ map (fromMaybe 0 . square) [1..64]
