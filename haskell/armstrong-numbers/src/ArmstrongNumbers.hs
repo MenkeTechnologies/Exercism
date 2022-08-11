f14fae483b43a6b641175e12adeda8255f94a0d8
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-11 09:28:17 -0400

    arm

--- haskell/armstrong-numbers/src/ArmstrongNumbers.hs
+++ haskell/armstrong-numbers/src/ArmstrongNumbers.hs
@@ -1,4 +1,10 @@
 module ArmstrongNumbers (armstrong) where
+import Data.Char (digitToInt)
 
 armstrong :: Integral a => a -> Bool
-armstrong = error "You need to implement this function."
+armstrong number = sumOfDigitsToPower == toInteger number
+    where
+        digits = map digitToInt . show $ toInteger number
+        numberOfDigits = length digits
+        digitToPower digit = digit ^ numberOfDigits
+        sumOfDigitsToPower = toInteger . sum $ map digitToPower digits
