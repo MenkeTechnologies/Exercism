741d82be389f8c41b167d6bda02a53afa9917bc1
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-11 09:21:39 -0400

    ss

--- haskell/scrabble-score/src/Scrabble.hs
+++ haskell/scrabble-score/src/Scrabble.hs
@@ -1,7 +1,23 @@
-module Scrabble (scoreLetter, scoreWord) where
+module Scrabble (
+      scoreLetter,
+      scoreWord
+) where
 
-scoreLetter :: Char -> Integer
-scoreLetter letter = error "You need to implement this function."
+import Data.Char (toLower)
 
-scoreWord :: String -> Integer
-scoreWord word = error "You need to implement this function."
+letterValues :: [(Char, Int)]
+letterValues =
+  [ ('a', 1), ('b', 3), ('c', 3), ('d', 2), ('e', 1)
+  , ('f', 4), ('g', 2), ('h', 4), ('i', 1), ('j', 8)
+  , ('k', 5), ('l', 1), ('m', 3), ('n', 1), ('o', 1)
+  , ('p', 3), ('q', 10), ('r', 1), ('s', 1), ('t', 1)
+  , ('u', 1), ('v', 4), ('w', 4), ('x', 8), ('y', 4)
+  , ('z', 10) ]
+
+scoreLetter :: Char -> Int
+scoreLetter l = case lookup (toLower l) letterValues of
+                      Just val -> val
+                      Nothing  -> 0
+
+scoreWord :: String -> Int
+scoreWord = sum . map scoreLetter
