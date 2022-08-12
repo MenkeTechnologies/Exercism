54c87d2f55b56445e31767f4627eb9748f9bd0a5
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-12 01:27:51 -0400

    luhn

--- haskell/luhn/src/Luhn.hs
+++ haskell/luhn/src/Luhn.hs
@@ -1,4 +1,12 @@
 module Luhn (isValid) where
+import Data.Char (isDigit, digitToInt)
 
 isValid :: String -> Bool
-isValid n = error "You need to implement this function."
+isValid n | length (normalize n) < 2 = False
+          | any (not . (\x -> x==' ' || isDigit x)) n = False
+          | otherwise = mod (luhn $ normalize n) 10 == 0
+  where
+    normalize = map digitToInt . reverse . filter isDigit
+    luhn (x:y:xs) = x + (if y<5 then 2*y else 2*y-9) + luhn xs
+    luhn [x] = x
+    luhn [] = 0
