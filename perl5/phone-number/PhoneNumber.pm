c96d15ffcbcc1e2e527a5c64ea47e9da98d9337f
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-13 14:04:55 -0400

    pn use $_

--- perl5/phone-number/PhoneNumber.pm
+++ perl5/phone-number/PhoneNumber.pm
@@ -5,22 +5,22 @@ use Exporter 'import';
 our @EXPORT_OK = qw(clean_number);
 
 sub clean_number {
-    my ($num) = @_;
-    die "letters not permitted\n" if $num =~ /[a-z]/i;
-    die "punctuations not permitted\n" if $num =~ /[^0-9() \.+-]/;
-    $num =~ s/\D//g;
-    my $len = length($num);
+    ($_) = @_;
+    die "letters not permitted\n" if /[a-z]/i;
+    die "punctuations not permitted\n" if /[^0-9() \.+-]/;
+    s/\D//g;
+    my $len = length;
     die "incorrect number of digits\n" if $len < 10;
     die "more than 11 digits\n" if $len > 11;
     if ($len == 11) {
-        $num =~ s/^1// or die "11 digits must start with 1\n";
+        s@^1@@ or die "11 digits must start with 1\n";
     }
-    my ($_cc, $area_code, $exchange_code, $station_code) = $num =~ /^(\d?)(\d{3})(\d{3})(\d{4})$/;
+    my ($area_code, $exchange_code) = $_ =~ /^\d?(\d{3})(\d{3})\d{4}$/;
     die "area code cannot start with zero\n" if $area_code =~ /^0/;
     die "area code cannot start with one\n" if $area_code =~ /^1/;
     die "exchange code cannot start with zero\n" if $exchange_code =~ /^0/;
     die "exchange code cannot start with one\n" if $exchange_code =~ /^1/;
-    $num
+    $_
 }
 
 
