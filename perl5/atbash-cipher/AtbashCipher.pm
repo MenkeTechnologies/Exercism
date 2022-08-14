10a10df4cb593e988ed5a98cf9438756e68cac51
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-14 00:03:11 -0400

    strain

--- perl5/atbash-cipher/AtbashCipher.pm
+++ perl5/atbash-cipher/AtbashCipher.pm
@@ -4,14 +4,18 @@ use warnings;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<encode_atbash decode_atbash>;
 
+my @letters = map { chr($_) } (ord('a')..ord('z'));
+
+my %lookup;
+@lookup{@letters} = reverse @letters;
+
 sub encode_atbash {
-  my ($phrase) = @_;
-  return undef;
+  my ($s) = @_;
+  return decode_atbash($s) =~ s/(?<=\G\w{5})(?=\w)/ /gr;
 }
-
 sub decode_atbash {
-  my ($phrase) = @_;
-  return undef;
+  my ($s) = @_;
+  return join '', map { $lookup{lc($_)} // (/\d/ ? $_ : '') } split //, $s;
 }
-
 1;
+
--- perl5/strain/Strain.pm
+++ perl5/strain/Strain.pm
@@ -3,15 +3,15 @@ use strict;
 use warnings;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<keep discard>;
-
 sub keep {
-  my ( $input, $function ) = @_;
-  return undef;
+  my ($lst, $f) = @_;
+  my @keep;
+  do { push @keep => $_ if &$f($_) } for @$lst;
+  \@keep
 }
-
 sub discard {
-  my ( $input, $function ) = @_;
-  return undef;
+  my ($lst, $f) = @_;
+  keep $lst, sub { !&$f($_) }
 }
 
-1;
+1
