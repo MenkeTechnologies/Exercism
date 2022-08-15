a85a19e1a17ff8dc802145fbc667e4723b136b3a
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-15 16:43:12 -0400

    sum mult

--- perl5/sum-of-multiples/SumOfMultiples.pm
+++ perl5/sum-of-multiples/SumOfMultiples.pm
@@ -3,10 +3,14 @@ use strict;
 use warnings;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<sum_of_multiples>;
-
+use List::Util 'sum';
 sub sum_of_multiples {
-  my ($input) = @_;
-  return undef;
+    my ($input) = @_;
+    my $sum = 0;
+    for my $n (1 .. ($$input{limit} - 1)) {
+        $sum += $n if (grep {$_ != 0 and $n % $_ == 0} $$input{factors}->@*);
+    }
+    $sum
 }
 
-1;
+1
