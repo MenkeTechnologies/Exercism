18b552c8d7ec8aa6bee896c9199547c9940c1a95
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-15 16:54:02 -0400

    listops

--- perl5/list-ops/ListOps.pm
+++ perl5/list-ops/ListOps.pm
@@ -1,24 +1,18 @@
 package ListOps;
-
 use strict;
 use warnings;
 
 sub map {
-  my ( $func, $list ) = @_;
-
-  return undef;
+  $_ = $_[0]->($_) for @{$_[1]};
+  $_[1]
 }
-
-sub reduce {
-  my ( $func, $list ) = @_;
-
-  return undef;
-}
-
 sub length {
-  my $list = shift;
-
-  return undef;
+  scalar @{$_[0]}
+}
+sub reduce {
+  my $acc = shift @{$_[1]};
+  $acc = $_[0]->($acc, $_) for @{$_[1]};
+  $acc
 }
 
-1;
+1
--- perl5/sum-of-multiples/SumOfMultiples.pm
+++ perl5/sum-of-multiples/SumOfMultiples.pm
@@ -7,8 +7,8 @@ use List::Util 'sum';
 sub sum_of_multiples {
     my ($input) = @_;
     my $sum = 0;
-    for my $n (1 .. ($$input{limit} - 1)) {
-        $sum += $n if (grep {$_ != 0 and $n % $_ == 0} $$input{factors}->@*);
+    for my $n (1 .. ($input->{limit} - 1)) {
+        $sum += $n if (grep {$_ != 0 and $n % $_ == 0} $input->{factors}->@*);
     }
     $sum
 }
