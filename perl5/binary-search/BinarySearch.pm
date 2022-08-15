4230c912121c086d15208df7fbff1814cd27a069
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-15 11:03:06 -0400

    bin

--- perl5/binary-search/BinarySearch.pm
+++ perl5/binary-search/BinarySearch.pm
@@ -5,8 +5,22 @@ use Exporter qw<import>;
 our @EXPORT_OK = qw<binary_search>;
 
 sub binary_search {
-  my ($input) = @_;
-  return undef;
+    my ($input) = @_;
+    my @array = $$input{array}->@*;
+    my $value = $$input{value};
+    my ($l, $r) = (0, $#array);
+    while ( $l <= $r ) {
+        my $mid = int( ( $l + $r ) / 2 );
+        if ($value < $array[$mid]) {
+            $r = $mid - 1
+        }
+        elsif ($value > $array[$mid] ) {
+            $l = $mid + 1
+        }
+        else {
+            return $mid
+        }
+    }
+    die 'value not in array';
 }
-
-1;
+1
