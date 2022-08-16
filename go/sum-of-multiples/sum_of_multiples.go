88af316095f5ac0380351e51bc56f72c13e6391b
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-16 10:13:55 -0400

    mult

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -1,1 +1,15 @@
 package summultiples
+
+func SumMultiples(limit int, factors ...int) int {
+	sum := 0
+	for n := 1; n < limit; n++ {
+		for _, f := range factors {
+			if f != 0 && n % f == 0 {
+				sum += n
+				break
+			}
+		}
+	}
+	return sum
+}
+
