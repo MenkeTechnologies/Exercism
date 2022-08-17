b9ccb98aa6119c142ae8df67feeaf889295fdc45
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-17 14:49:39 -0400

    bin

--- go/binary-search/binary_search.go
+++ go/binary-search/binary_search.go
@@ -1,1 +1,19 @@
 package binarysearch
+
+func SearchInts(data []int, search int) int {
+
+	lo, hi := 0, len(data)-1
+	for lo <= hi {
+		mid := lo + (hi-lo)/2
+		d := data[mid]
+		switch {
+		case d < search:
+			lo = mid + 1
+		case d > search:
+			hi = mid - 1
+		default:
+			return mid
+		}
+	}
+	return -1
+}
