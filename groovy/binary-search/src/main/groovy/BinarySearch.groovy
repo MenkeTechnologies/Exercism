d863342e4e865b2da85f9564a24f7c82a1c0710f
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-17 14:36:04 -0400

    bin

--- groovy/binary-search/src/main/groovy/BinarySearch.groovy
+++ groovy/binary-search/src/main/groovy/BinarySearch.groovy
@@ -1,14 +1,22 @@
 class BinarySearch {
-
     List data
-
-    // You may assume for this exercise that the input data is sorted in ascending order.
+    
     BinarySearch(List data) {
         this.data = data
     }
-
     int indexOf(item) {
-        throw new UnsupportedOperationException('indexOf method not implemented')
+        def (low, hi) = [0, data.size - 1]
+        def mid
+        while (low <= hi) {
+            mid = (low + hi).intdiv(2)
+            if (data[mid] < item) {
+                low = mid + 1
+            } else if (data[mid] > item){
+                hi = mid - 1
+            } else {
+                return mid
+            }
+        }
+        return -1
     }
-
 }
--- groovy/binary-search/src/test/groovy/BinarySearchSpec.groovy
+++ groovy/binary-search/src/test/groovy/BinarySearchSpec.groovy
@@ -11,7 +11,7 @@ class BinarySearchSpec extends Specification {
         [6]   | 6     || 0
     }
 
-    @Ignore
+    
     def "Finds a value in the middle of an array"() {
         expect:
         new BinarySearch(array).indexOf(value) == expected
@@ -21,7 +21,7 @@ class BinarySearchSpec extends Specification {
         [1, 3, 4, 6, 8, 9, 11] | 6     || 3
     }
 
-    @Ignore
+    
     def "Finds a value at the beginning of an array"() {
         expect:
         new BinarySearch(array).indexOf(value) == expected
@@ -31,7 +31,7 @@ class BinarySearchSpec extends Specification {
         [1, 3, 4, 6, 8, 9, 11] | 1     || 0
     }
 
-    @Ignore
+    
     def "Finds a value at the end of an array"() {
         expect:
         new BinarySearch(array).indexOf(value) == expected
@@ -41,7 +41,7 @@ class BinarySearchSpec extends Specification {
         [1, 3, 4, 6, 8, 9, 11] | 11    || 6
     }
 
-    @Ignore
+    
     def "Finds a value in an array of odd length"() {
         expect:
         new BinarySearch(array).indexOf(value) == expected
@@ -51,7 +51,7 @@ class BinarySearchSpec extends Specification {
         [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634] | 144   || 9
     }
 
-    @Ignore
+    
     def "Finds a value in an array of even length"() {
         expect:
         new BinarySearch(array).indexOf(value) == expected
@@ -61,7 +61,7 @@ class BinarySearchSpec extends Specification {
         [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377] | 21    || 5
     }
 
-    @Ignore
+    
     def "Identifies that a value is not included in the array"() {
         expect:
         new BinarySearch(array).indexOf(value) == expected
@@ -71,7 +71,7 @@ class BinarySearchSpec extends Specification {
         [1, 3, 4, 6, 8, 9, 11] | 7     || -1
     }
 
-    @Ignore
+    
     def "A value smaller than the arrays smallest value is not included"() {
         expect:
         new BinarySearch(array).indexOf(value) == expected
@@ -81,7 +81,7 @@ class BinarySearchSpec extends Specification {
         [1, 3, 4, 6, 8, 9, 11] | 0     || -1
     }
 
-    @Ignore
+    
     def "A value larger than the arrays largest value is not included"() {
         expect:
         new BinarySearch(array).indexOf(value) == expected
@@ -91,7 +91,7 @@ class BinarySearchSpec extends Specification {
         [1, 3, 4, 6, 8, 9, 11] | 13    || -1
     }
 
-    @Ignore
+    
     def "Nothing is included in an empty array"() {
         expect:
         new BinarySearch(array).indexOf(value) == expected
@@ -101,7 +101,7 @@ class BinarySearchSpec extends Specification {
         []    | 1     || -1
     }
 
-    @Ignore
+    
     def "Nothing is found when the left and right bounds cross"() {
         expect:
         new BinarySearch(array).indexOf(value) == expected
