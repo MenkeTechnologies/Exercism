00e439f67b71cd82700fe5f2f003e29d32aabdae
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-17 14:31:52 -0400

    bin

--- kotlin/binary-search/src/main/kotlin/BinarySearch.kt
+++ kotlin/binary-search/src/main/kotlin/BinarySearch.kt
@@ -1,5 +1,17 @@
 object BinarySearch {
     fun search(list: List<Int>, item: Int): Int {
-        TODO("Implement the function to complete the task. Change the signature if necessary.")
+        var low = 0
+        var high = list.size - 1
+        while (low <= high) {
+            val mid = (low + high) / 2
+            if (list[mid] == item) {
+                return mid
+            } else if (list[mid] < item) {
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+        }
+        throw NoSuchElementException()
     }
 }
