e34bb502ac14b922dad39c0a6b67c62fdf9f1ab6
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-18 11:10:34 -0400

    bin

--- javascript/binary-search/binary-search.js
+++ javascript/binary-search/binary-search.js
@@ -1,3 +1,15 @@
-export const find = () => {
-  throw new Error('Remove this statement and implement this function');
+export const find = (ary, target) => {
+  let lo = 0;
+  let hi = ary.length - 1;
+  while (lo <= hi) {
+    let mid = Math.floor((lo + hi) / 2);
+    if (target === ary[mid]) {
+      return mid;
+    } else if (target < ary[mid]) {
+      hi = mid - 1;
+    } else if (target > ary[mid]) {
+      lo = mid + 1;
+    }
+  }
+  throw new Error('Value not in array');
 };
