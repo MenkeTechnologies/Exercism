d76183d3011d082a519016d865d8981a1d36b31e
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-18 11:13:46 -0400

    bin

--- typescript/binary-search/binary-search.ts
+++ typescript/binary-search/binary-search.ts
@@ -1,3 +1,15 @@
-export function find(haystack: unknown, needle: unknown): number | never {
-  throw new Error('Remove this statement and implement this function')
-}
+export const find = (ary: number[], target: number) => {
+    let lo = 0;
+    let hi = ary.length - 1;
+    while (lo <= hi) {
+        let mid = Math.floor((lo + hi) / 2);
+        if (target === ary[mid]) {
+            return mid;
+        } else if (target < ary[mid]) {
+            hi = mid - 1;
+        } else if (target > ary[mid]) {
+            lo = mid + 1;
+        }
+    }
+    throw new Error('Value not in array');
+};
