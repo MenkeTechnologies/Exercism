1afa08f18877e59ba7d2d2c72ff6609c5fce1076
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-18 19:59:59 -0400

    nth

--- typescript/nth-prime/nth-prime.ts
+++ typescript/nth-prime/nth-prime.ts
@@ -1,3 +1,7 @@
-export function nth() {
-  throw new Error('Remove this statement and implement this function')
-}
+export const nth = (n: number) => {
+    if (n < 1) throw new Error("Prime is not possible")
+    const primes = [2]
+    for (let i = 3; primes.length < n; i += 2)
+        if (primes.every(prime => i % prime !== 0)) primes.push(i)
+    return primes[primes.length - 1]
+};
