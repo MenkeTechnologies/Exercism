769dc3a7a488b07f895c3505cca9df3dc3a5163e
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-19 01:01:18 -0400

    ozans

--- javascript/ozans-playlist/ozans-playlist.js
+++ javascript/ozans-playlist/ozans-playlist.js
@@ -1,18 +1,14 @@
-// @ts-check
 //
 // The line above enables type checking for this file. Various IDEs interpret
 // the @ts-check directive. It will give you helpful autocompletion when
 // implementing this exercise.
-
 /**
  * Removes duplicate tracks from a playlist.
  *
  * @param {string[]} playlist
  * @returns {string[]} new playlist with unique entries
  */
-export function removeDuplicates(playlist) {
-  throw new Error('Please implement the removeDuplicates function');
-}
+export const removeDuplicates = playlist => [...new Set(playlist)];
 
 /**
  * Checks whether a playlist includes a track.
@@ -21,9 +17,7 @@ export function removeDuplicates(playlist) {
  * @param {string} track
  * @returns {boolean} whether the track is in the playlist
  */
-export function hasTrack(playlist, track) {
-  throw new Error('Please implement the hasTrack function');
-}
+export const hasTrack = (playlist, track) => new Set(playlist).has(track);
 
 /**
  * Adds a track to a playlist.
@@ -32,9 +26,11 @@ export function hasTrack(playlist, track) {
  * @param {string} track
  * @returns {string[]} new playlist
  */
-export function addTrack(playlist, track) {
-  throw new Error('Please implement the addTrack function');
-}
+export const addTrack = (playlist, track) => {
+  const set = new Set(playlist)
+  set.add(track)
+  return [...set]
+};
 
 /**
  * Deletes a track from a playlist.
@@ -43,9 +39,11 @@ export function addTrack(playlist, track) {
  * @param {string} track
  * @returns {string[]} new playlist
  */
-export function deleteTrack(playlist, track) {
-  throw new Error('Please implement the deleteTrack function');
-}
+export const deleteTrack = (playlist, track) => {
+  const set = new Set(playlist)
+  set.delete(track)
+  return [...set]
+};
 
 /**
  * Lists the unique artists in a playlist.
@@ -53,6 +51,6 @@ export function deleteTrack(playlist, track) {
  * @param {string[]} playlist
  * @returns {string[]} list of artists
  */
-export function listArtists(playlist) {
-  throw new Error('Please implement the listArtists function');
-}
+export const listArtists = playlist => {
+  return [...new Set(playlist.map(track => track.split(' - ')[1]))]
+};
