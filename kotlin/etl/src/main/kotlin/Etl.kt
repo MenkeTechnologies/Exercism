6864a3d345bc17fc9da218dd45b5efcd9586db0e
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-19 15:20:42 -0400

    etl

--- kotlin/etl/src/main/kotlin/Etl.kt
+++ kotlin/etl/src/main/kotlin/Etl.kt
@@ -1,10 +1,7 @@
 object ETL {
-    fun transform(source: Map<Int, Collection<Char>>): Map<Char, Int> {
-
-        return source.entries.fold(mutableMapOf()) { acc, n ->
+    fun transform(source: Map<Int, Collection<Char>>): Map<Char, Int> =
+        source.entries.fold(mutableMapOf()) { acc, n ->
             n.value.forEach { acc[it.toLowerCase()] = n.key }
             acc
         }
-
-    }
 }
