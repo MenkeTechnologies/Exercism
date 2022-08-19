db94a8b6d200bb0a04a0375d57e553ac5835c09f
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-19 00:53:39 -0400

    nth

--- groovy/nth-prime/src/main/groovy/NthPrime.groovy
+++ groovy/nth-prime/src/main/groovy/NthPrime.groovy
@@ -1,7 +1,14 @@
+import java.util.stream.IntStream
+
 class NthPrime {
-
     static nth(int n) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+        if (n < 1) throw new ArithmeticException()
+        Collection<Integer> primes = [2]
+        IntStream.iterate(3, { it + 2 })
+                .takeWhile { primes.size() < n }
+                .peek { v -> if (primes.every { p -> v % p != 0 })
+                    primes.add v
+                }.collect()
+        primes.last()
     }
-
 }
