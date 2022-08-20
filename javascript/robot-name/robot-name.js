c77320aa3097083741b0fafc2a3f9e446a4b4011
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-19 23:56:07 -0400

    rn

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,6 +1,28 @@
-// This is only a SKELETON file for the 'Robot Name' exercise. It's been
-// provided as a convenience to get your started writing code faster.
+const nameCache = new Set();
+const randLet = (start, range) => String.fromCharCode(start.charCodeAt(0) + range * Math.random());
 
-export class Robot {}
+const makeName = () =>
+  [...Array(5).keys()].map(i => i < 2 ? randLet('A', 26) : randLet('0', 10)).join('')
 
-Robot.releaseNames = () => {};
+export class Robot {
+  constructor() {
+    this.reset();
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  reset() {
+    let name = makeName();
+    while (nameCache.has(name)) {
+      name = makeName();
+    }
+    nameCache.add(name);
+    this._name = name;
+  }
+
+  static releaseNames() {
+    nameCache.clear();
+  }
+}
