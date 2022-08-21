46f9a41ef904b0531ebff1600d9bb2c23b7d3082
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-21 13:02:14 -0400

    pn

--- c/phone-number/phone_number.c
+++ c/phone-number/phone_number.c
@@ -1,1 +1,21 @@
 #include "phone_number.h"
+
+char *phone_number_clean(const char *input) {
+    char *result = calloc(PHONE_NUMBER_LEN + 1, sizeof(char));
+    memset(result, '0', PHONE_NUMBER_LEN);
+    size_t len = strlen(input);
+    size_t rp = 0;
+    for (size_t i = 0; i < len; i++) {
+        char c = input[i];
+        if (!isdigit(c) || c <= '1' && (rp == 0 || rp == 3))
+            continue;
+        if (rp == PHONE_NUMBER_LEN) {
+            memset(result, '0', PHONE_NUMBER_LEN);
+            return result;
+        }
+        result[rp++] = c;
+    }
+    if (rp < PHONE_NUMBER_LEN)
+        memset(result, '0', PHONE_NUMBER_LEN);
+    return result;
+}
--- c/phone-number/phone_number.h
+++ c/phone-number/phone_number.h
@@ -1,6 +1,8 @@
 #ifndef PHONE_NUMBER_H
 #define PHONE_NUMBER_H
-
-char *phone_number_clean(const char *input);
-
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define PHONE_NUMBER_LEN 10
+char *phone_number_clean(const char *);
 #endif
