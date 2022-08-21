417c0978f2a7992a5185744d90d645b88f39801b
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-21 13:06:22 -0400

    bin

--- c/binary-search/binary_search.c
+++ c/binary-search/binary_search.c
@@ -1,1 +1,15 @@
 #include "binary_search.h"
+
+const int *binary_search(int target, const int *arr, size_t n) {
+    int lo = 0, hi = n - 1, mid;
+    while (lo <= hi) {
+        mid = (lo + hi) / 2;
+        if (arr[mid] < target)
+            lo = mid + 1;
+        else if (arr[mid] > target)
+            hi = mid - 1;
+        else
+            return &arr[mid];
+    }
+    return NULL;
+}
