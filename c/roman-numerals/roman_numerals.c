f510d759c3eaa90afcfad4787eb6f2ab3319c9a1
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-21 15:57:36 -0400

    rn

--- c/roman-numerals/roman_numerals.c
+++ c/roman-numerals/roman_numerals.c
@@ -1,1 +1,32 @@
 #include "roman_numerals.h"
+#include <string.h>
+#include <stdlib.h>
+
+static const RomanNumeral lookup[] = {
+        {1000, "M"},
+        {900,  "CM"},
+        {500,  "D"},
+        {400,  "CD"},
+        {100,  "C"},
+        {90,   "XC"},
+        {50,   "L"},
+        {40,   "XL"},
+        {10,   "X"},
+        {9,    "IX"},
+        {5,    "V"},
+        {4,    "IV"},
+        {1,    "I"}};
+
+char *to_roman_numeral(unsigned int number) {
+    char *ptr = calloc(16, sizeof(char));
+    char *r = ptr;
+    for (int i = 0; i < 13; ++i) {
+        RomanNumeral numeral = lookup[i];
+        for (unsigned int j = 0; j < number / numeral.arab; ++j) {
+            memcpy(ptr, numeral.roman, strlen(numeral.roman));
+            ptr += strlen(numeral.roman);
+        }
+        number %= numeral.arab;
+    }
+    return r;
+}
--- c/roman-numerals/roman_numerals.h
+++ c/roman-numerals/roman_numerals.h
@@ -1,5 +1,9 @@
 #ifndef ROMAN_NUMERALS_H
 #define ROMAN_NUMERALS_H
+typedef struct RamenNoodle {
+    unsigned int arab;
+    char *roman;
+} RomanNumeral;
 
 char *to_roman_numeral(unsigned int number);
 
