7972bf4558b537add905d8271866591ee0ae3559
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-21 12:36:29 -0400

    rcd

--- c/resistor-color-duo/resistor_color_duo.c
+++ c/resistor-color-duo/resistor_color_duo.c
@@ -1,1 +1,4 @@
 #include "resistor_color_duo.h"
+int color_code(resistor_band_t color[]) {
+    return 10 * color[0] + color[1];
+}
--- c/resistor-color-duo/resistor_color_duo.h
+++ c/resistor-color-duo/resistor_color_duo.h
@@ -1,4 +1,17 @@
 #ifndef RESISTOR_COLOR_DUO_H
 #define RESISTOR_COLOR_DUO_H
+typedef enum resistor_band_t {
+    BLACK,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE,
+} resistor_band_t;
+int color_code(resistor_band_t []);
 
 #endif
