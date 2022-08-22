fa08506784b0a443677b19c794a2fc5cac052801
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-22 00:53:24 -0400

    nt

--- c/nucleotide-count/nucleotide_count.c
+++ c/nucleotide-count/nucleotide_count.c
@@ -1,1 +1,22 @@
 #include "nucleotide_count.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *count(const char *s) {
+    char *r = calloc(30, sizeof(char));
+    int nums[4] = {0};
+    for (; *s; ++s) {
+        if (*s == 'A') ++nums[0];
+        else if (*s == 'C') ++nums[1];
+        else if (*s == 'G') ++nums[2];
+        else if (*s == 'T') ++nums[3];
+        else {
+            strcpy(r, "");
+            return r;
+        }
+    }
+    sprintf(r, "%c:%d %c:%d %c:%d %c:%d", 'A', nums[0], 'C', nums[1],
+            'G', nums[2], 'T', nums[3]);
+    return r;
+}
--- c/nucleotide-count/nucleotide_count.h
+++ c/nucleotide-count/nucleotide_count.h
@@ -1,6 +1,7 @@
 #ifndef NUCLEOTIDE_COUNT_H
 #define NUCLEOTIDE_COUNT_H
-
-char *count(const char *dna_strand);
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+char* count(const char* s);
 #endif
