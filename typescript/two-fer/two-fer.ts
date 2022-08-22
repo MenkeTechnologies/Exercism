e3d386478e468c7d9696d850ff7f9bb474622583
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-22 10:24:58 -0400

    2fer

--- typescript/two-fer/two-fer.ts
+++ typescript/two-fer/two-fer.ts
@@ -1,9 +1,1 @@
-/**
- * This stub is provided to make it straightforward to get started.
- */
-
-export function twoFer(name: string = "you"): string {
-
-    return `One for ${name}, one for me.`
-
-}
+export const twoFer = (name: string = "you") => `One for ${name}, one for me.`;
