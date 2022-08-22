33e58c2b05b5cdea73680499239e6b4c7b302571
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-22 01:10:15 -0400

    sec

--- c/secret-handshake/secret_handshake.c
+++ c/secret-handshake/secret_handshake.c
@@ -1,1 +1,20 @@
+#include <stdlib.h>
 #include "secret_handshake.h"
+
+const char **commands(size_t number) {
+    const char **commands = calloc(4, sizeof(const char *));
+    size_t len = 0;
+    if (number & 1) commands[len++] = "wink";
+    if (number & 2) commands[len++] = "double blink";
+    if (number & 4) commands[len++] = "close your eyes";
+    if (number & 8) commands[len++] = "jump";
+
+    if (number & 16) {
+        for (size_t i = 0; i < len / 2; ++i) {
+            const char *tmp = commands[i];
+            commands[i] = commands[len - i - 1];
+            commands[len - i - 1] = tmp;
+        }
+    }
+    return commands;
+}
--- c/word-count/word_count.c
+++ c/word-count/word_count.c
@@ -1,1 +1,47 @@
 #include "word_count.h"
+
+int count_words(const char *sentence, word_count_word_t *words) {
+    int counter = 0;
+    char buffer[MAX_WORD_LENGTH + 1];
+    size_t length = 0;
+    for (; *sentence != '\0'; ++sentence) {
+        if (isalnum(*sentence) || (*sentence == '\'' && length && isalnum(*(sentence + 1)))) {
+            if (length == MAX_WORD_LENGTH) return EXCESSIVE_LENGTH_WORD;
+            buffer[length++] = tolower(*sentence);
+        } else if (length) {
+            buffer[length] = '\0';
+            bool hasWord = false;
+            for (int i = 0; i < counter; ++i) {
+                if (strcmp(words[i].text, buffer) == 0) {
+                    ++words[i].count;
+                    hasWord = true;
+                    break;
+                }
+            }
+            if (!hasWord) {
+                if (counter == MAX_WORDS) return EXCESSIVE_NUMBER_OF_WORDS;
+                strcpy(words[counter].text, buffer);
+                ++words[counter++].count;
+            }
+            length = 0;
+        }
+    }
+    if (length) {
+        buffer[length] = '\0';
+        bool hasWord = false;
+        for (int i = 0; i < counter; ++i) {
+            if (strcmp(words[i].text, buffer) == 0) {
+                ++words[i].count;
+                hasWord = true;
+                break;
+            }
+        }
+        if (!hasWord) {
+            if (counter == MAX_WORDS)
+                return EXCESSIVE_NUMBER_OF_WORDS;
+            strcpy(words[counter].text, buffer);
+            ++words[counter++].count;
+        }
+    }
+    return counter;
+}
