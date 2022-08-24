93c00d086b03fb6937747f5d3329d54c10cb46de
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-24 12:56:49 -0400

    atbash

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,1 +1,29 @@
 package atbash
+
+import (
+	"bytes"
+	"regexp"
+	"strings"
+)
+
+var dict = map[rune]rune{}
+var re = regexp.MustCompile(".{5}")
+
+func init() {
+	for i := 0; i <= int('z'-'a'); i++ {
+		dict[rune('a'+i)] = rune('z' - i)
+	}
+	for i := '0'; i <= '9'; i++ {
+		dict[i] = i
+	}
+}
+
+func Atbash(plaintext string) string {
+	builder := new(bytes.Buffer)
+	for _, c := range strings.ToLower(plaintext) {
+		if encoded, ok := dict[c]; ok {
+			builder.WriteRune(encoded)
+		}
+	}
+	return strings.TrimSpace(re.ReplaceAllString(builder.String(), "$0 "))
+}
--- go/phone-number/phone_number.go
+++ go/phone-number/phone_number.go
@@ -1,1 +1,33 @@
 package phonenumber
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+)
+
+func Number(s string) (string, error) {
+	re := regexp.MustCompile(`[^\d]`)
+	s = re.ReplaceAllString(s, "")
+	if match, _ := regexp.MatchString(`^1?[2-9][\d]{2}[2-9][\d]{6}$`, s); !match {
+		return "", errors.New("Poop!")
+	}
+	if len(s) == 11 {
+		return s[1:], nil
+	}
+	return s, nil
+}
+func AreaCode(s string) (string, error) {
+	n, err := Number(s)
+	if err != nil {
+		return "", err
+	}
+	return n[:3], nil
+}
+func Format(s string) (string, error) {
+	n, err := Number(s)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("(%s) %s-%s", n[:3], n[3:6], n[6:]), nil
+}
