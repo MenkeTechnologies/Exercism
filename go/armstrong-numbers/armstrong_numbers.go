10f99e7c7e5803a91692da8a034f59ade502685e
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-24 12:31:18 -0400

    arm

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -1,1 +1,16 @@
 package armstrong
+
+import (
+	"math"
+	"strconv"
+)
+
+func IsNumber(n int) bool {
+	sum := 0.0
+	digits := strconv.Itoa(n)
+	size := float64(len(digits))
+	for _, digit := range digits {
+		sum += math.Pow(float64(digit-'0'), size)
+	}
+	return float64(n) == sum
+}
