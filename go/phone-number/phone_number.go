476a9966a1597a8300e9605854e0d349539e6724
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-24 13:14:46 -0400

    pn

--- go/phone-number/phone_number.go
+++ go/phone-number/phone_number.go
@@ -6,28 +6,35 @@ import (
 	"regexp"
 )
 
+var re, _ = regexp.Compile(`^\s*(?:\+?1?\s*)?\(?([2-9]\d{2})\)?[-.\s]*([2-9]\d{2})[-.\s]*(\d{4})\s*$`)
+
+func GetMatches(s string) ([]string, error) {
+	if !re.MatchString(s) {
+		return nil, errors.New("bad num")
+	}
+	matches := re.FindStringSubmatch(s)
+	return matches, nil
+}
 func Number(s string) (string, error) {
-	re := regexp.MustCompile(`[^\d]`)
-	s = re.ReplaceAllString(s, "")
-	if match, _ := regexp.MatchString(`^1?[2-9][\d]{2}[2-9][\d]{6}$`, s); !match {
-		return "", errors.New("Poop!")
+	matches, err := GetMatches(s)
+	if err != nil {
+		return "", err
 	}
-	if len(s) == 11 {
-		return s[1:], nil
-	}
+	s = matches[1] + matches[2] + matches[3]
+
 	return s, nil
 }
 func AreaCode(s string) (string, error) {
-	n, err := Number(s)
+	matches, err := GetMatches(s)
 	if err != nil {
 		return "", err
 	}
-	return n[:3], nil
+	return matches[1], nil
 }
 func Format(s string) (string, error) {
-	n, err := Number(s)
+	matches, err := GetMatches(s)
 	if err != nil {
 		return "", err
 	}
-	return fmt.Sprintf("(%s) %s-%s", n[:3], n[3:6], n[6:]), nil
+	return fmt.Sprintf("(%s) %s-%s", matches[1], matches[2], matches[3]), nil
 }
