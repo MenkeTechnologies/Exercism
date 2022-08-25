c5ddc4892fb35ce867062acd1c84303ec7f0e7f6
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-25 00:09:46 -0400

    err

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,1 +1,22 @@
 package erratum
+
+func Use(open ResourceOpener, input string) (err error) {
+	var res Resource
+	if res, err = open(); err != nil {
+		if _, ok := err.(TransientError); ok {
+			return Use(open, input)
+		}
+		return
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			if f, ok := r.(FrobError); ok {
+				res.Defrob(f.defrobTag)
+			}
+			err = r.(error)
+		}
+		res.Close()
+	}()
+	res.Frob(input)
+	return
+}
