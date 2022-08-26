e5bba072b2e367275d442ccae136b45d2f2f1eb7
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-26 00:58:18 -0400

    coll

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,1 +1,21 @@
 package collatzconjecture
+
+import (
+	"errors"
+)
+
+func CollatzConjecture(n int) (int, error) {
+	if n < 1 {
+		return 0, errors.New("input must be >= 1")
+	}
+	cnt := 0
+	for n > 1 {
+		if n%2 == 0 {
+			n /= 2
+		} else {
+			n = 3*n + 1
+		}
+		cnt++
+	}
+	return cnt, nil
+}
