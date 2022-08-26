9d2940480ef0185f5963d30c384fa41c7ddf0f03
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-26 01:52:11 -0400

    sh

--- bash/secret-handshake/secret_handshake.sh
+++ bash/secret-handshake/secret_handshake.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+codes=('wink' 'double blink' 'close your eyes' 'jump')
+
+declare -a r
+for ((i = 0; i < ${#codes[@]}; ++i)); do
+    if (( 1 << i & $1 )); then
+        (( 1 << 4 & $1 )) && r=( "${codes[i]}" "${r[@]}" ) || r+=( "${codes[i]}" )
+    fi
+done
+
+IFS=,
+echo "${r[*]}"
