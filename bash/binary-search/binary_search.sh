dd7f965621c46bcd6dc9824d2ab77fab741164f2
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-26 01:57:23 -0400

    bs

--- bash/binary-search/binary_search.sh
+++ bash/binary-search/binary_search.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+target="$1"
+shift
+lst=( "$@" )
+len=${#lst[@]}
+lo=0
+hi=$(( len - 1 ))
+
+while (( lo <= hi )); do
+    mid=$(( ( lo + hi ) / 2 ))
+    if (( lst[mid] < target )); then
+        lo=$(( mid + 1 ))
+    elif (( lst[mid] > target )); then
+        hi=$(( mid - 1 ))
+    else
+        echo $mid
+        exit
+    fi
+done
+
+echo -1
