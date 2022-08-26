cb4bf8afb06a45ea3569368902a6b22abcbc5c51
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-26 01:32:06 -0400

    wc

--- bash/word-count/word_count.sh
+++ bash/word-count/word_count.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-echo -e "$*" | perl -0E '$_=lc<>;s@(?!\w|'"'"'t\b).@ @g;++$h{$_}for split;@k=sort{$h{$b}<=>$h{$a}}keys%h;say"$_: ".$h{$_}for@k'
+echo -e "$*" |
+perl -0E '$_=lc<>;s@(?!\w|'"'"'(t|re)\b).@ @g;++$h{$_}for split;@k=sort{$h{$b}<=>$h{$a}}keys%h;say"$_: ".$h{$_}for@k'
