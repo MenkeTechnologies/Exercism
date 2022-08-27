c22dc96d33a18faf1d4028a1c4697d9d85fcf8c0
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-27 01:47:48 -0400

    rn

--- bash/roman-numerals/roman_numerals.sh
+++ bash/roman-numerals/roman_numerals.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+n="${1:?ERROR: arg must be number between 1 and 3999}"
+r=''
+dict=(
+  M  1000
+  CM 900
+  D  500
+  CD 400
+  C  100
+  XC 90
+  L  50
+  XL 40
+  X  10
+  IX 9
+  V  5
+  IV 4
+  I  1
+)
+for (( i = 0; i < ${#dict[@]}; i += 2 )); do
+  sym=${dict[$i]}
+  val=${dict[((i + 1))]}
+  for (( j = 0; j < n / val; ++j )); do
+    r+=$sym
+  done
+  (( n %= val ))
+done
+
+echo $r
+
