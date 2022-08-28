8cadf7186ac825d06d733dc2a9619f527b1a605c
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-28 15:52:42 -0400

    atbash

--- julia/atbash-cipher/atbash-cipher.jl
+++ julia/atbash-cipher/atbash-cipher.jl
@@ -1,8 +1,17 @@
-function encode(input)
+encode(input) = atbash(input, grouping=true)
+decode(input) = atbash(input, grouping=false)
 
+function atbash(input; grouping =false)
+    input = [c for c in input if !isspace(c) && !ispunct(c)]
+    r = join(map(atbash, input))
+    if grouping
+        join(map(join, Iterators.partition(r, 5)), ' ')
+    else
+        join(r)
+    end
 end
 
-function decode(input)
-
+function atbash(c::Char)
+    isletter(c) || return c
+    'z' - (lowercase(c) - 'a')
 end
-
