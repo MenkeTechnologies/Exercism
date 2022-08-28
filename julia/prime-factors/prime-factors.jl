47d721046d574074561e68cc4993635c82c77521
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-28 15:44:30 -0400

    pf

--- julia/prime-factors/prime-factors.jl
+++ julia/prime-factors/prime-factors.jl
@@ -1,1 +1,13 @@
 
+function prime_factors(start)
+    fac = []
+    divisor = 2
+    while start > 1
+        while start % divisor == 0
+            push!(fac,divisor)
+            start /= divisor
+        end
+        divisor += 1
+    end
+    return fac
+end
