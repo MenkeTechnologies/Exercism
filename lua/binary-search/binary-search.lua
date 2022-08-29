ccc1b8c19ff9b3664b3a87038ce4634632af2e23
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-29 00:04:28 -0400

    bin

--- lua/binary-search/binary-search.lua
+++ lua/binary-search/binary-search.lua
@@ -1,3 +1,16 @@
-return function(array, target)
-
+return function(arr, search)
+    local lo = 1
+    local hi = #arr
+    while lo <= hi do
+        local mid = lo + math.floor((hi - lo) / 2)
+        local tmp = arr[mid]
+        if tmp == search then
+            return mid
+        elseif tmp < search then
+            lo = mid + 1
+        else
+            hi = mid - 1
+        end
+    end
+    return -1
 end
