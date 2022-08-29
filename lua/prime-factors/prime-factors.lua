8d8bfd631c1ab662d9a8162644f467e423e5dfac
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-29 00:00:23 -0400

    pf

--- lua/prime-factors/prime-factors.lua
+++ lua/prime-factors/prime-factors.lua
@@ -1,3 +1,13 @@
-return function(input)
+return function(n)
+  local factor = 2
+  local prime_factors = {}
+  while n > 1 do
+    while n % factor == 0 do
+      table.insert(prime_factors, factor)
+      n = n / factor
+    end
+    factor = factor + 1
+  end
 
+  return prime_factors
 end
