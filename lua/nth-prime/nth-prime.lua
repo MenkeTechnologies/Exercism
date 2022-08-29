3f7077874a4eb00540c0d18c9ee992acc333b50b
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-29 00:11:51 -0400

    nth

--- lua/nth-prime/nth-prime.lua
+++ lua/nth-prime/nth-prime.lua
@@ -1,3 +1,22 @@
 return function(n)
-
+  assert(n > 0, "Error: n must be greater than 0")
+  local primes = {2}
+  local cur = 3
+  while #primes < n do
+    local isprime = true
+    local max = math.sqrt(cur)
+    for _, v in ipairs(primes) do
+      if cur % v == 0 then
+        isprime = false
+        break
+      elseif v > max then
+        break
+      end
+    end
+    if isprime then
+      table.insert(primes, cur)
+    end
+    cur = cur + 2
+  end
+  return primes[#primes]
 end
