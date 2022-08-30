bc413801cae7664bbdd30c8734f806c131ba1db8
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-30 14:06:47 -0400

    bs

--- fsharp/binary-search/BinarySearch.fs
+++ fsharp/binary-search/BinarySearch.fs
@@ -1,3 +1,13 @@
 module BinarySearch
 
-let find input value = failwith "You need to implement this function."
+let find (input: int []) search =
+    let rec aux lo hi =
+        if hi < lo then
+            None
+        else
+            let mid = (lo + hi) / 2
+            let candidate = input[mid]
+            if candidate = search then Some mid
+            elif search < candidate then aux lo (mid - 1)
+            else aux (mid + 1) hi
+    aux 0 (input.Length - 1)
