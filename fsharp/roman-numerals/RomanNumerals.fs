414f97e136665be358e163752132262251c321cc
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-30 14:04:57 -0400

    rn

--- fsharp/roman-numerals/RomanNumerals.fs
+++ fsharp/roman-numerals/RomanNumerals.fs
@@ -1,3 +1,22 @@
 module RomanNumerals
 
-let roman arabicNumeral = failwith "You need to implement this function."
+let roman arabicNumeral =
+    [ (1000, "M")
+      (900, "CM")
+      (500, "D")
+      (400, "CD")
+      (100, "C")
+      (90, "XC")
+      (50, "L")
+      (40, "XL")
+      (10, "X")
+      (9, "IX")
+      (5, "V")
+      (4, "IV")
+      (1, "I") ]
+    |> List.fold (fun (accNum, accStr) (value, sym) ->
+        let quot = accNum / value
+        if quot > 0
+        then (accNum % value, accStr + String.replicate quot sym)
+        else (accNum, accStr)) (arabicNumeral, "")
+    |> snd
