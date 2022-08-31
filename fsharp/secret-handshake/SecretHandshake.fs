03cb31e60c1cf2f72a9cd82298d8bb1e9e665442
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-31 00:37:33 -0400

    sec

--- fsharp/secret-handshake/SecretHandshake.fs
+++ fsharp/secret-handshake/SecretHandshake.fs
@@ -1,3 +1,14 @@
 module SecretHandshake
 
-let commands number = failwith "You need to implement this function."
+let codes =
+    [ (1, "wink")
+      (2, "double blink")
+      (4, "close your eyes")
+      (8, "jump") ]
+
+let commands num =
+    let r =
+        codes
+         |> List.filter (fun (code, _) -> num &&& code > 0)
+         |> List.map snd
+    if num &&& 16 = 16 then List.rev r else r
