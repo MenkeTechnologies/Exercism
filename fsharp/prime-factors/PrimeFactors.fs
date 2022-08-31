d88ec131c0e0d66ffc2cd9e9987b6f59f17366a1
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-31 00:45:20 -0400

    pf

--- fsharp/prime-factors/PrimeFactors.fs
+++ fsharp/prime-factors/PrimeFactors.fs
@@ -1,3 +1,9 @@
 ﻿module PrimeFactors
 
-let factors number = failwith "You need to implement this function."
+let factors start =
+    (start, 2L)
+    |> List.unfold (function
+        | 1L, _ -> None
+        | n, divisor when n % divisor = 0L -> Some(divisor |> int |> Some, (n / divisor, divisor))
+        | n, divisor -> Some(None, (n, divisor + 1L)))
+    |> List.choose id
