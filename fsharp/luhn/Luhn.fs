0401d083e5e7e33da1c89679d74fe3e54edfa15e
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-31 01:13:53 -0400

    luhn

--- fsharp/luhn/Luhn.fs
+++ fsharp/luhn/Luhn.fs
@@ -1,3 +1,16 @@
 module Luhn
 
-let valid number = failwith "You need to implement this function."
+open System
+open System.Text.RegularExpressions
+
+let valid (number: string) =
+    let nows = number.Replace(" ", "")
+    Regex.IsMatch(nows, "\d{2,}$") &&
+    (nows
+       |> Seq.rev
+       |> Seq.map (Char.GetNumericValue >> int)
+       |> Seq.mapi (fun i n ->
+           if i % 2 = 0 then n
+           else if n >= 5 then 2 * n - 9
+           else 2 * n)
+       |> Seq.sum) % 10 = 0
