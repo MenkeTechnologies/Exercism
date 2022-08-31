3cb48008acac8aa04d4e17be9f4008899161bba9
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-31 01:17:04 -0400

    isbn

--- fsharp/isbn-verifier/IsbnVerifier.fs
+++ fsharp/isbn-verifier/IsbnVerifier.fs
@@ -1,3 +1,21 @@
 module IsbnVerifier
 
-let isValid isbn = failwith "You need to implement this function."
+open System.Text.RegularExpressions
+
+let private hasValidCharacters isbn = Regex.IsMatch(isbn, @"^\d{9}[\dX]$")
+
+let private digitToValue index digit =
+    let indexFactor = index + 1
+    match digit with
+    | 'X' -> 10 * indexFactor
+    | _ -> (int digit - int '0') * indexFactor
+
+let private checksum isbn =
+    isbn
+    |> Seq.rev
+    |> Seq.mapi digitToValue
+    |> Seq.sum
+
+let isValid (isbn: string) =
+    let normalizedIsbn = isbn.Replace("-", "")
+    hasValidCharacters normalizedIsbn && checksum normalizedIsbn % 11 = 0
