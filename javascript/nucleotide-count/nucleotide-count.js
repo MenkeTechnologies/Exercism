cff436866ef2fd34a71a052e143f7e369a3ddc85
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-01 15:00:53 -0400

    nt

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,7 +1,6 @@
-export const countNucleotides = strand => {
-  if (strand.match(/[^ACGT]/g) != null ) throw new Error('Invalid nucleotide in strand')
-
-  const count = strand.split('').reduce((acc,n) => ++acc[n] && acc, { A: 0, C: 0, G: 0, T: 0});
-  
-  return Object.values(count).join(' ')
-};
+export const countNucleotides = s =>
+  s.match(/[^ACGT]/g) != null ? (() => {
+    throw new Error('Invalid nucleotide in strand')
+  })() : Object.values(s.split('')
+    .reduce((acc, n) => ++acc[n] && acc, {A: 0, C: 0, G: 0, T: 0}))
+    .join(' ')
