f279676700e1afd964a8baec0153b7b6a70ed8a8
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-01 10:19:47 -0400

    atbash

--- fsharp/atbash-cipher/AtbashCipher.fs
+++ fsharp/atbash-cipher/AtbashCipher.fs
@@ -1,5 +1,20 @@
 module AtbashCipher
 
-let encode str = failwith "You need to implement this function."
+open System
+open System.Text.RegularExpressions
 
-let decode str = failwith "You need to implement this function."
+let cipher =
+    let alpha = [ 'a' .. 'z' ]
+    (alpha, List.rev alpha)
+    ||> List.zip
+    |> Map.ofList
+
+let encode (words: string) =
+    Regex.Replace(words.ToLower(), @"[^\d\w]", "")
+    |> Seq.map (fun c ->
+        if Char.IsDigit c then c else cipher[c])
+    |> Seq.chunkBySize 5
+    |> Seq.map System.String
+    |> String.concat " "
+
+let decode (code: string) = (encode code).Replace(" ", "")
