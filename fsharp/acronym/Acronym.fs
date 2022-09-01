a3933dee4f5e10eeac56627a3e19855f4b12d42e
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-01 09:57:20 -0400

    acro

--- fsharp/acronym/Acronym.fs
+++ fsharp/acronym/Acronym.fs
@@ -1,3 +1,8 @@
 module Acronym
 
-let abbreviate phrase = failwith "You need to implement this function."
+open System
+
+let abbreviate (phrase: string) =
+    phrase.ToUpper().Split([| ' '; '-' |], StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (Seq.filter Char.IsLetter >> Seq.head)
+    |> String.Concat
