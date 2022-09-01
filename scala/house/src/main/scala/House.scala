5e47b3f3495b5726332131334af858e2466e9a57
MenkeTechnologies <linux.dev25@gmail.com>
2022-08-31 21:52:17 -0400

    house

---
+++ scala/house/src/main/scala/House.scala
@@ -0,0 +1,20 @@
+object House {
+  private val segments = Array(
+    "the house that Jack built",
+    "the malt that lay in",
+    "the rat that ate",
+    "the cat that killed",
+    "the dog that worried",
+    "the cow with the crumpled horn that tossed",
+    "the maiden all forlorn that milked",
+    "the man all tattered and torn that kissed",
+    "the priest all shaven and shorn that married",
+    "the rooster that crowed in the morn that woke",
+    "the farmer sowing his corn that kept",
+    "the horse and the hound and the horn that belonged to"
+  )
+
+  private def getVerse(n: Int) = (0 until n).reverse.map(segments(_)).mkString("This is ", " ", ".")
+
+  def recite(start: Int, end: Int) = (start to end).map(getVerse).mkString("\n") + "\n\n"
+}
