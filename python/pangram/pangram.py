7f439b4deb4833f7c53722ad6cc41a481f29b4a4
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-02 00:36:44 -0400

    pan

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,5 @@
+from string import ascii_lowercase
+
+
 def is_pangram(sentence):
-    pass
+    return all(c in sentence.lower() for c in ascii_lowercase)
--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,8 +1,7 @@
 class Isogram
 
   def self.isogram?(input)
-
-    input.downcase.chars.filter { _1.match?(/[a-z]/) }.group_by { _1 }.values.all? { _1.size == 1 }
-
+    input.downcase.chars.filter { _1.match?(/[a-z]/) }
+      .group_by { _1 }.values.all? { _1.size == 1 }
   end
 end
