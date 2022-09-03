4c4763cd78880b2dd1fe44eda6122acade97b235
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-03 00:04:56 -0400

    atbash

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,6 +1,13 @@
-def encode(plain_text):
-    pass
+from string import ascii_lowercase
+
+TRANTAB = str.maketrans(ascii_lowercase, ascii_lowercase[::-1])
 
 
-def decode(ciphered_text):
-    pass
+def encode(s):
+    alnum = "".join([c for c in s.lower() if c.isalnum()])
+    spaced = ' '.join(alnum[i:i + 5] for i in range(0, len(alnum), 5))
+    return spaced.translate(TRANTAB)
+
+
+def decode(string):
+    return string.replace(" ", "").translate(TRANTAB)
