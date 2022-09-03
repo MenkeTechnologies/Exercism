df44d7d030ae48915c6d3f1e964dd5530cd7264e
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-03 00:13:19 -0400

    nth

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,2 +1,10 @@
-def prime(number):
-    pass
+def prime(n):
+    if n < 1:
+        raise ValueError("there is no zeroth prime")
+    curr = 3
+    primes = [2]
+    while len(primes) < n:
+        if all(curr % p != 0 for p in primes):
+            primes.append(curr)
+        curr += 2
+    return primes[-1]
--- rust/rna-transcription/src/lib.rs
+++ rust/rna-transcription/src/lib.rs
@@ -11,9 +11,9 @@ pub struct Rna {
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
         let mut d = Dna { bases: vec![] };
-        for (i, ch) in dna.chars().enumerate() {
-            match ch {
-                'C' | 'G' | 'T' | 'A' => d.bases.push(ch),
+        for (i, c) in dna.chars().enumerate() {
+            match c {
+                'C' | 'G' | 'T' | 'A' => d.bases.push(c),
                 _ => {
                     return Err(i);
                 }
@@ -25,8 +25,8 @@ impl Dna {
 
     pub fn into_rna(self) -> Rna {
         let mut r = Rna { bases: vec![] };
-        for ch in self.bases {
-            match ch {
+        for c in self.bases {
+            match c {
                 'C' => r.bases.push('G'),
                 'G' => r.bases.push('C'),
                 'T' => r.bases.push('A'),
@@ -42,9 +42,9 @@ impl Dna {
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
         let mut r = Rna { bases: vec![] };
-        for (i, ch) in rna.chars().enumerate() {
-            match ch {
-                'C' | 'G' | 'U' | 'A' => r.bases.push(ch),
+        for (i, c) in rna.chars().enumerate() {
+            match c {
+                'C' | 'G' | 'U' | 'A' => r.bases.push(c),
                 _ => {
                     return Err(i);
                 }
