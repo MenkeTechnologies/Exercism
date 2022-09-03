b929df02048a12507ca21ff6341fda00373abec8
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-02 23:50:06 -0400

    bs

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,2 +1,11 @@
-def find(search_list, value):
-    pass
+def find(lst, search):
+    lo, hi = 0, len(lst) - 1
+    while lo <= hi:
+        index = (lo + hi) // 2
+        if lst[index] == search:
+            return index
+        elif lst[index] > search:
+            hi = index - 1
+        else:
+            lo = index + 1
+    raise ValueError("value not in array")
