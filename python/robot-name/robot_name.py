a1398f0504a311247f3335553f616b0528337a5f
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-02 23:55:28 -0400

    rn

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,17 @@
+import string
+import random
+
+
 class Robot:
+    name_list = []
+
     def __init__(self):
-        pass
+        self.name = None
+        self.reset()
+
+    def reset(self):
+        while True:
+            self.name = (''.join(random.choices(string.ascii_uppercase, k=2) + random.choices(string.digits, k=3)))
+            if self.name in Robot.name_list:
+                break
+            Robot.name_list.append(self.name)
