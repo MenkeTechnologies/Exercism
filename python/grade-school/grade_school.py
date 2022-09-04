d4ba382f80f69bde5d0f12ff31032b73385f60d7
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-04 01:48:05 -0400

    gs

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,15 +1,28 @@
+from collections import defaultdict
+
+
 class School:
     def __init__(self):
-        pass
+        self.student_grades = defaultdict(list)
+        self.added_list = []
 
     def add_student(self, name, grade):
-        pass
+        for grade_lst in self.student_grades.values():
+            if name in grade_lst:
+                self.added_list.append(False)
+                break
+        else:
+            self.student_grades[grade].append(name)
+            self.added_list.append(True)
 
     def roster(self):
-        pass
-
-    def grade(self, grade_number):
-        pass
+        r = []
+        for k in sorted(self.student_grades.keys()):
+            r.extend(sorted(self.student_grades[k]))
+        return r
 
     def added(self):
-        pass
+        return self.added_list
+
+    def grade(self, grade_number):
+        return sorted(self.student_grades[grade_number])
