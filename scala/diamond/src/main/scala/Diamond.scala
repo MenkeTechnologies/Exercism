c57557348145a5b7bcd06d684623f3c958889a07
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-04 04:47:26 -0400

    more efficient tri

--- scala/diamond/src/main/scala/Diamond.scala
+++ scala/diamond/src/main/scala/Diamond.scala
@@ -1,18 +1,15 @@
 
 object Diamond {
-  def rows(c: Char): List[String] = {
 
-    val alpha = 'A' to 'Z'
+  val alpha = 'A' to 'Z'
 
+  def rows(c: Char) = {
     val rng = 0 to alpha.indexOf(c)
     val rvs = rng.reverse
-
     (rng ++ rvs.drop(1)).map(r =>
       (rvs ++ rng.drop(1)).foldLeft("") { (acc, c) =>
         if (c == r) acc + alpha(r) else acc + " "
       }
     ).toList
-
   }
-
 }
--- typescript/triangle/triangle.ts
+++ typescript/triangle/triangle.ts
@@ -1,23 +1,23 @@
 export class Triangle {
   private sorted: number[]
   private set: Set<number>;
+  private valid: Boolean;
 
   constructor(...sides: number[]) {
     this.sorted = sides.sort((a,b) => a - b)
     this.set = new Set(sides);
+    this.valid = this.sorted[0] > 0 && this.sorted[0] + this.sorted[1] >= this.sorted[2]
   }
 
-  valid = () => this.sorted[0] > 0 && this.sorted[0] + this.sorted[1] >= this.sorted[2]
-
   get isEquilateral() {
-    return this.valid() && this.set.size === 1
+    return this.valid && this.set.size === 1
   }
 
   get isIsosceles() {
-    return this.valid() && this.set.size <= 2
+    return this.valid && this.set.size <= 2
   }
 
   get isScalene() {
-    return this.valid() && this.set.size === 3
+    return this.valid && this.set.size === 3
   }
 }
