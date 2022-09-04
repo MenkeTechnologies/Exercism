668799bdb56821e7c7ddbf8ebd3ffd2fcb33db6a
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-04 01:53:39 -0400

    ban

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,18 +1,45 @@
+from threading import Lock
+
+
 class BankAccount:
     def __init__(self):
-        pass
+        self.mutex = Lock()
+        self.bal = 0
+        self.locked = False
 
     def get_balance(self):
-        pass
+        with self.mutex:
+            if not self.locked:
+                raise ValueError("account not open")
+            return self.bal
 
     def open(self):
-        pass
+        with self.mutex:
+            if self.locked:
+                raise ValueError("account already open")
+            self.locked = True
 
-    def deposit(self, amount):
-        pass
+    def deposit(self, amt):
+        if amt < 0:
+            raise ValueError("amount must be greater than 0")
+        with self.mutex:
+            if not self.locked:
+                raise ValueError("account not open")
+            self.bal += amt
 
-    def withdraw(self, amount):
-        pass
+    def withdraw(self, amt):
+        with self.mutex:
+            if not self.locked:
+                raise ValueError("account not open")
+            if amt > self.bal:
+                raise ValueError("amount must be less than balance")
+            if amt < 0:
+                raise ValueError("amount must be greater than 0")
+            self.bal -= amt
 
     def close(self):
-        pass
+        with self.mutex:
+            if not self.locked:
+                raise ValueError("account not open")
+            self.locked = False
+            self.bal = 0
