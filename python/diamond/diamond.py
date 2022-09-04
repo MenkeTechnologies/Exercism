dfb60961541254a4a83bfff12f9abd5be8941383
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-04 01:26:11 -0400

    dia

--- python/diamond/diamond.py
+++ python/diamond/diamond.py
@@ -1,2 +1,5 @@
 def rows(letter):
-    pass
+    letters = [chr(k) for k in range(ord('A'), ord(letter) + 1)]
+    alphabet = letters[:-1] + letters[::-1]
+    diamond_line = letters[::-1] + letters[1:]
+    return [''.join(x if x == y else ' ' for y in diamond_line) for x in alphabet]
