0edb4478ade7434a43beb5156cc05a11cb9adab5
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-04 02:18:47 -0400

    gs

--- rust/grade-school/src/lib.rs
+++ rust/grade-school/src/lib.rs
@@ -1,46 +1,36 @@
 use std::collections::HashMap;
 
-// This annotation prevents Clippy from warning us that `School` has a
-// `fn new()` with no arguments, but doesn't implement the `Default` trait.
-//
-// Normally, it's good practice to just do what Clippy tells you, but in this
-// case, we want to keep things relatively simple. The `Default` trait is not the point
-// of this exercise.
 #[allow(clippy::new_without_default)]
 pub struct School {
-    m: HashMap<String, u32>
+    dict: HashMap<u32, Vec<String>>,
 }
 
 impl School {
     pub fn new() -> School {
-        School { m: HashMap::new() }
+        Self { dict: HashMap::new() }
     }
 
-    pub fn add(&mut self, grade: u32, student: &str) {
-        self.m.insert(String::from(student), grade);
+    pub fn add(&mut self, gr: u32, student: &str) {
+        self.dict
+            .entry(gr)
+            .or_insert(Vec::new())
+            .push(String::from(student));
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        let mut values: Vec<u32> = self.m.values().cloned().collect();
-        values.sort();
-        values.dedup();
-        values
+        let mut r: Vec<u32> = self.dict.keys().cloned().collect();
+        r.sort();
+        r.dedup();
+        r
     }
-
-    // If `grade` returned a reference, `School` would be forced to keep a `Vec<String>`
-    // internally to lend out. By returning an owned vector of owned `String`s instead,
-    // the internal structure can be completely arbitrary. The tradeoff is that some data
-    // must be copied each time `grade` is called.
-    pub fn grade(&self, grade: u32) -> Vec<String> {
-        let mut students:Vec<String> = vec![];
-        for (k,v) in &self.m {
-            if *v == grade {
-                students.push(k.clone());
+    pub fn grade(&self, gr: u32) -> Vec<String> {
+        match self.dict.get(&gr) {
+            Some(studs) => {
+                let mut r = studs.to_owned();
+                r.sort();
+                r
             }
+            _ => vec![],
         }
-
-        students.sort();
-
-        students
     }
 }
