dbb44227cdb143161499e03399e4195657d10946
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-05 11:37:28 -0400

    bs

--- php/binary-search/BinarySearch.php
+++ php/binary-search/BinarySearch.php
@@ -1,30 +1,25 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
-function find(int $needle, array $haystack): int
+function find(int $search, array $lst): int
 {
-    throw new \BadFunctionCallException("Implement the find function");
+    $lo = 0;
+    $hi = count($lst);
+
+    while($lo < $hi) {
+
+        $mid = intdiv($lo + $hi, 2);
+        $val = $lst[$mid];
+
+        if ($search > $val) {
+            $lo = $mid + 1;
+        } else if ($search < $val) {
+            $hi = $mid;
+        } else {
+            return $mid;
+        }
+    }
+    return -1;
 }
+
