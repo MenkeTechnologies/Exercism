b503121f4840d7148ce73ae4f9d1379bfad4c0bb
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-06 01:24:17 -0400

    sh

--- dart/secret-handshake/lib/secret_handshake.dart
+++ dart/secret-handshake/lib/secret_handshake.dart
@@ -1,3 +1,7 @@
 class SecretHandshake {
-  // Put your code here
+    final _acts = const {'wink': 1, 'double blink': 2, 'close your eyes': 4, 'jump': 8};
+    List<String> commands(int mask) {
+        List<String> r = _acts.keys.where((key) => mask & _acts[key]! != 0).toList();
+        return mask & 16 == 0 ? r : r.reversed.toList();
+    }
 }
