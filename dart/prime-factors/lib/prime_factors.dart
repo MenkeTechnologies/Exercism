1940a05e3d60c4699e789416942778e492d630c4
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-06 01:22:35 -0400

    pf

--- dart/prime-factors/lib/prime_factors.dart
+++ dart/prime-factors/lib/prime_factors.dart
@@ -1,3 +1,15 @@
 class PrimeFactors {
-  // Put your code here
+    List<int> factors(int number) {
+        int divisor = 2;
+        List<int> r = [];
+        while (number > 1) {
+            while(number % divisor == 0) {
+                r.add(divisor);
+                number ~/= divisor;
+            }
+            ++divisor;
+        }
+        return r;
+    }
 }
+
