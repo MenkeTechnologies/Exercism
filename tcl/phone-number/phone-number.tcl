9ddc47ff0a30ad59224d92c20c61fb7c15ed4d04
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-07 14:49:24 -0400

    pn

--- tcl/phone-number/phone-number.tcl
+++ tcl/phone-number/phone-number.tcl
@@ -1,3 +1,17 @@
-proc clean {phone} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc clean {in} {
+    set num [regsub -all {[+()\s.-]} $in ""]
+    if {[string length $num] > 11} { error "more than 11 digits" }
+    if {[string length $num] < 10} { error "incorrect number of digits" }
+    if [regexp -nocase {[a-z]} $num] { error "letters not permitted" }
+    if [regexp {[^[:digit:]]} $num] { error "punctuations not permitted" }
+    if {[string length $num] == 11} {
+        if {![string match {1*} $num]} { error "11 digits must start with 1" }
+        set num [string range $num 1 end]
+    }
+    if {[string match {0*} $num]} { error "area code cannot start with zero" }
+    if {[string match {1*} $num]} { error "area code cannot start with one" }
+    if {[string match {???0*} $num]} { error "exchange code cannot start with zero" }
+    if {[string match {???1*} $num]} { error "exchange code cannot start with one" }
+    return $num
 }
+
