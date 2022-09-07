eb47e4d923741b8475e040c02387b4c2e7706822
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-07 00:23:25 -0400

    pn

--- tcl/perfect-numbers/perfect-numbers.tcl
+++ tcl/perfect-numbers/perfect-numbers.tcl
@@ -1,3 +1,20 @@
-proc classify {} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc aliquotSum {n} {
+    set sum 1
+    for {set i 2} {[expr $i * $i ] < $n} {incr i} {
+        if {$n % $i == 0} {
+            incr sum $i
+            incr sum [expr $n / $i]
+        }
+    }
+    return $sum
+}
+proc classify {n} {
+    if {![string is integer -strict $n] || $n < 1} {
+        error "Classification is only possible for natural numbers."
+    }
+    if {$n == 1} {return deficient}
+    set sum [aliquotSum $n]
+    if {$sum == $n} { return perfect }
+    if {$sum > $n} { return abundant }
+    if {$sum < $n} { return deficient }
 }
