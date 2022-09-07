c9c46ac670b2a72c0fb71d13857a2e5ce373e43c
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-06 22:23:13 -0400

    nth

--- dart/nth-prime/lib/nth_prime.dart
+++ dart/nth-prime/lib/nth_prime.dart
@@ -1,10 +1,12 @@
 class NthPrime {
   int prime(int n) {
-    if (n == 0) {
-      throw new ArgumentError('There is no zeroth prime');
+    if (n < 1) throw ArgumentError('There is no zeroth prime');
+    var primes = <int>[2];
+    var curr = 3;
+    while (primes.length < n){
+      if (primes.every((p) => curr % p != 0)) primes.add(curr);
+      curr+=2;
     }
-    
-    
-    
+    return primes.last;
   }
 }
