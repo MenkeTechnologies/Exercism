0ddff5e7ebf87186c733d44f6f091bc1d0851091
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-07 00:32:02 -0400

    bob

--- tcl/bob/bob.tcl
+++ tcl/bob/bob.tcl
@@ -1,3 +1,11 @@
-proc heyBob {input} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc heyBob {s} {
+    set nows [string trim $s]
+    if {$nows eq ""} { return "Fine. Be that way!" }
+    set q [string match {*\?} $nows]
+    if {[regexp {[[:upper:]]} $nows] && ![regexp {[[:lower:]]} $nows]} {
+        if {$q} { return "Calm down, I know what I'm doing!" }
+        return "Whoa, chill out!"
+    }
+    if {$q} { return "Sure." }
+    return "Whatever."
 }
--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -2,7 +2,7 @@ export class DnDCharacter {
   public static generateAbilityScore = () => Math.floor(Math.random() * 16) + 3;
 
   public static getModifierFor = (abilityValue: number) => Math.floor((abilityValue - 10) / 2);
-  
+
   readonly strength = DnDCharacter.generateAbilityScore()
   readonly dexterity = DnDCharacter.generateAbilityScore()
   readonly constitution = DnDCharacter.generateAbilityScore()
@@ -10,5 +10,5 @@ export class DnDCharacter {
   readonly wisdom = DnDCharacter.generateAbilityScore()
   readonly charisma = DnDCharacter.generateAbilityScore()
   readonly hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution)
- 
+
 }
