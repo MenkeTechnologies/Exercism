9df44ef4f396a77ef10eb7a59989866dae9eaeed
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-07 14:03:32 -0400

    pf

--- typescript/prime-factors/prime-factors.ts
+++ typescript/prime-factors/prime-factors.ts
@@ -1,3 +1,17 @@
-export function calculatePrimeFactors() {
-  throw new Error('Remove this statement and implement this function')
-}
+export const calculatePrimeFactors = (start: number) => {
+
+    let fac = []
+    let divisor = 2
+
+    while (start > 1) {
+        while (start % divisor === 0) {
+            fac.push(divisor);
+            start /= divisor;
+        }
+        ++divisor;
+    }
+
+    return fac;
+
+
+};
