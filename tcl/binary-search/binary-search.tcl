f366346a526cd904db6c855dbab1ee6c159c6d3e
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-06 23:42:22 -0400

    bs

--- tcl/binary-search/binary-search.tcl
+++ tcl/binary-search/binary-search.tcl
@@ -1,3 +1,19 @@
-proc binarySearch {haystack needle} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc binarySearch {lst search} {
+    set hi [expr {[llength $lst] - 1}]
+    set lo 0
+
+    while { $lo <= $hi } {
+        set mid [expr {($lo + $hi) / 2}]
+        set elem [lindex $lst $mid]
+
+        if {$elem == $search} {
+            return $mid
+        } elseif {$search < $elem} {
+            set hi [expr {$mid - 1}]
+        } else {
+            set lo [expr {$mid + 1}]
+        }
+    }
+    return -1
 }
+
