50c77f19e6e32cbf78eb542456881b0a255c55de
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-07 21:38:35 -0400

    dnd

--- javascript/dnd-character/dnd-character.js
+++ javascript/dnd-character/dnd-character.js
@@ -1,52 +1,19 @@
-export const abilityModifier = (num) => {
-    if (num >= 3 && num <= 18) {
-        return Math.floor((num - 10) / 2);
-    } else if (num < 3) {
-        throw new Error('Ability scores must be at least 3');
-    } else if (num > 18) {
-        throw new Error('Ability scores can be at most 18');
-    }
-};
+export const abilityModifier = n =>
+    n < 3 ? (() => { throw new Error('Ability scores must be at least 3') })() :
+    n > 18 ? (() => { throw new Error('Ability scores can be at most 18'); })() :
+    Math.floor((n - 10) / 2);
+
 
 export class Character {
-    static rollAbility = () => [...Array(4).keys()].map(_ => Math.floor(Math.random() * 6 + 1))
+    characteristics = [
+        'strength', 'dexterity', 'constitution',
+        'intelligence', 'wisdom', 'charisma'
+    ];
+    static rollAbility = () => [...Array(4).keys()].map(_i => Math.floor(Math.random() * 6 + 1))
         .sort().slice(1).reduce((acc, n) => acc + n);
 
     constructor() {
-        this._str = Character.rollAbility();
-        this._dex = Character.rollAbility();
-        this._con = Character.rollAbility();
-        this._int = Character.rollAbility();
-        this._wis = Character.rollAbility();
-        this._cha = Character.rollAbility();
-        this._hit = 10 + abilityModifier(this._con);
-    }
-
-    get strength() {
-        return this._str;
-    }
-
-    get dexterity() {
-        return this._dex;
-    }
-
-    get constitution() {
-        return this._con;
-    }
-
-    get intelligence() {
-        return this._int;
-    }
-
-    get wisdom() {
-        return this._wis;
-    }
-
-    get charisma() {
-        return this._cha;
-    }
-
-    get hitpoints() {
-        return this._hit;
+        this.characteristics.forEach(c => this[c] = Character.rollAbility());
+        this.hitpoints = 10 + abilityModifier(this.constitution);
     }
 }
