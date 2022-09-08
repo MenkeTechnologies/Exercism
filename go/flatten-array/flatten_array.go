482878d3fb36ca75cfaa92d49a6f1f93dbd3a7b9
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-07 21:56:23 -0400

    fa

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,1 +1,14 @@
 package flatten
+
+func Flatten(obj interface{}) (r []interface{}) {
+    r = []interface{}{}
+	switch item := obj.(type) {
+	case []interface{}:
+		for _, val := range item {
+			r = append(r, Flatten(val)...)
+		}
+	case interface{}:
+		r = append(r, item)
+	}
+	return
+}
--- java/micro-blog/src/main/java/MicroBlog.java
+++ java/micro-blog/src/main/java/MicroBlog.java
@@ -2,6 +2,7 @@ class MicroBlog {
     public static final int MAX_SIZE = 5;
 
     public String truncate(String input) {
-        return input.codePoints().limit(MAX_SIZE).collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+        return input.codePoints().limit(MAX_SIZE).collect(StringBuilder::new, 
+                StringBuilder::appendCodePoint, StringBuilder::append).toString();
     }
 }
