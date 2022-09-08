b565c1ae7555d62fbd8260c31b0c67b0eace6a71
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-07 21:53:55 -0400

    rot

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -1,1 +1,19 @@
 package rotationalcipher
+
+func RotationalCipher(s string, rot int) string {
+	var r []rune
+	for _, c := range s {
+		base := 0
+		switch {
+		case c >= 'A' && c <= 'Z':
+			base = int('A')
+		case c >= 'a' && c <= 'z':
+			base = int('a')
+		}
+		if base != 0 {
+			c = rune((int(c) - base + rot) % 26 + base)
+		}
+		r = append(r, c)
+	}
+	return string(r)
+}
