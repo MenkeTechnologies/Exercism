9596eed48483493c3f224fe16a7592553766a565
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-08 16:33:52 -0400

    sh

--- clojure/secret-handshake/src/secret_handshake.clj
+++ clojure/secret-handshake/src/secret_handshake.clj
@@ -1,5 +1,17 @@
 (ns secret-handshake)
+(def dict    {1 "wink"
+              2 "double blink"
+              4 "close your eyes"
+              8 "jump"})
 
-(defn commands [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn commands [n]
+  (let [msg (->> dict
+             (filter #(->> (key %)
+                           (bit-and n)
+                           pos?))
+             (map #(val %))
+             (apply vector))]
+    (if (zero? (bit-and n 16))
+      msg
+      (reverse msg))))
+
