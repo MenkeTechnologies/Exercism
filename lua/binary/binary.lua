395152e6dd7086b537485e889ef41d27d3bc4ee2
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-09 11:17:10 -0400

    bin

--- lua/binary/binary.lua
+++ lua/binary/binary.lua
@@ -1,7 +1,12 @@
-local function to_decimal(input)
-
-end
-
 return {
-  to_decimal = to_decimal
+    to_decimal = function (num)
+        local r = 0
+        for i=1,#num do
+            if num:sub(i,i) ~= '0' and num:sub(i,i) ~= '1' then
+                return 0
+            end
+            r = r * 2 + tonumber(num:sub(i,i))
+        end
+        return r
+    end
 }
