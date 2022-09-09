f553c8a38d4c5de55a21ea8f6eb3c3b5c4c0be96
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-08 23:58:02 -0400

    bs

--- clojure/binary-search/src/binary_search.clj
+++ clojure/binary-search/src/binary_search.clj
@@ -1,9 +1,15 @@
 (ns binary-search)
+(defn middle [items]
+  (quot (count items) 2))
 
-(defn search-for [] ;; <- arglist goes here
-  ;; your code goes here
-)
-
-(defn middle [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn search-for [item items]
+  (loop [lo 0
+         hi (- (count items) 1)]
+    (if (> lo hi)
+      (assert false "not found")
+      (let [mid (quot (+ lo hi) 2)
+            mid-val (nth items mid)]
+        (cond
+          (= mid-val item) mid
+          (< mid-val item) (recur (+ mid 1) hi)
+          (> mid-val item) (recur lo (- mid 1)))))))
