f7231c7f19a7ed55c0eab91e303beb0ee298ac17
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-10 00:07:48 -0400

    sum

--- lua/sum-of-multiples/sum-of-multiples.lua
+++ lua/sum-of-multiples/sum-of-multiples.lua
@@ -1,3 +1,19 @@
-return function(numbers)
-
+return function(factors)
+  return {
+    to = function(limit)
+      local multiples = {}
+      local sum = 0
+      for candidate = 1, limit - 1 do
+        for _, number in ipairs(factors) do
+          if candidate % number == 0 then
+            multiples[candidate] = true
+          end
+        end
+      end
+      for multiple in pairs(multiples) do
+        sum = sum + multiple
+      end
+      return sum
+    end
+  }
 end
