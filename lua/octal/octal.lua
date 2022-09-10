7ca49bb3f9b7b376b0701ac5a4e5ebdb322b6292
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-10 00:02:25 -0400

    octal

--- lua/octal/octal.lua
+++ lua/octal/octal.lua
@@ -1,7 +1,17 @@
-return function(s)
-  return {
-    to_decimal = function()
+return function(input)
+   return {
+      input = input,
+      to_decimal = function()
+         local invalidOctal = "[^0-7]"
+         local res = 0
+         if input:match(invalidOctal) then
+            return res
+         end
 
-    end
-  }
+        for c in input:gmatch(".") do
+            res = res * 8 + c
+        end
+         return res
+      end
+   }
 end
