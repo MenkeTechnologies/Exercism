348dc3d22b07b448dc452e2ef5e1747f8d1cf667
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-12 00:25:18 -0400

    pf

--- bash/prime-factors/prime_factors.sh
+++ bash/prime-factors/prime_factors.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+value=$1
+factors=()
+divisor=2
+while ((value > 1)); do
+    while ((value % divisor == 0)); do
+        factors+=("$divisor")
+        ((value /= divisor))
+    done
+    ((++divisor))
+done && echo ${factors[*]}
