62963242469fd08efa880448e399def52e4c9c2b
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-15 00:46:03 -0400

    etl

--- c/etl/etl.c
+++ c/etl/etl.c
@@ -1,1 +1,24 @@
 #include "etl.h"
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+inline static int cmp(const void *a, const void *b) {
+    return ((new_map *) a)->key - ((new_map *) b)->key;
+}
+
+int convert(const legacy_map *input, const size_t input_len, new_map **result) {
+    int cnt = 0;
+    for (size_t i = 0; i < input_len; ++i) cnt += strlen(input[i].keys);
+    *result = malloc(cnt * sizeof(new_map));
+    size_t k = 0;
+    for (size_t i = 0; i < input_len; ++i) {
+        for (size_t j = 0; j < strlen(input[i].keys); ++j) {
+            (*result)[k].key = tolower(input[i].keys[j]);
+            (*result)[k++].value = input[i].value;
+        }
+    }
+    qsort(*result, cnt, sizeof(new_map), cmp);
+    return cnt;
+}
+
--- c/etl/etl.h
+++ c/etl/etl.h
@@ -1,18 +1,14 @@
-#ifndef ETL_H
-#define ETL_H
+#pragma once
 
 #include <stddef.h>
 
 typedef struct {
-   int value;
-   const char *keys;
+    int value;
+    const char *keys;
 } legacy_map;
-
 typedef struct {
-   char key;
-   int value;
+    char key;
+    int value;
 } new_map;
 
-int convert(const legacy_map *input, const size_t input_len, new_map **output);
-
-#endif
+int convert(const legacy_map *input, const size_t input_len, new_map **result);
