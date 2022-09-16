7b29e8526426a47f3aea0406dd0a07e9de5d3092
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-16 01:01:08 -0400

    bin

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -1,23 +1,17 @@
 defmodule BinarySearch do
-  @doc """
-    Searches for a key in the tuple using the binary search algorithm.
-    It returns :not_found if the key is not in the tuple.
-    Otherwise returns {:ok, index}.
-
-    ## Examples
-
-      iex> BinarySearch.search({}, 2)
-      :not_found
-
-      iex> BinarySearch.search({1, 3, 5}, 2)
-      :not_found
-
-      iex> BinarySearch.search({1, 3, 5}, 5)
-      {:ok, 2}
-
-  """
-
-  @spec search(tuple, integer) :: {:ok, integer} | :not_found
-  def search(numbers, key) do
+  def search(numbers, target) do
+    aux(numbers, 0, tuple_size(numbers) - 1, target)
+  end
+  def aux(_, lo, hi, _) when lo > hi, do: :not_found
+  def aux(numbers, lo, hi, target) do
+    mid = div(lo + hi, 2)
+    cond do
+      elem(numbers, mid) == target ->
+        {:ok, mid}
+      elem(numbers, mid) > target ->
+        aux(numbers, lo, mid - 1, target)
+      true ->
+        aux(numbers, mid + 1, hi, target)
+    end
   end
 end
