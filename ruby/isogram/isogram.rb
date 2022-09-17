5b6ceb899ea985fb60a317312284a640114b4455
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-17 00:36:37 -0400

    iso

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,7 +1,6 @@
 class Isogram
 
   def self.isogram?(input)
-    input.downcase.chars.filter { _1.match?(/[a-z]/) }
-      .group_by { _1 }.values.all? { _1.size == 1 }
+    !input.downcase.match? /([a-z]).*\1/
   end
 end
