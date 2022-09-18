c0a6ea9ee1cc58ce93c076da809ef2b5ac6f002e
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-18 00:12:03 -0400

    sh

--- r/secret-handshake/secret-handshake.R
+++ r/secret-handshake/secret-handshake.R
@@ -1,3 +1,7 @@
-handshake <- function(n) {
-  
+handshake = function(n) {
+  msgs = c("wink", "double blink", "close your eyes", "jump")
+  res = msgs[bitwAnd(n, 2^(0:3)) != 0]
+  if (bitwAnd(n, 16)) res = rev(res)
+  switch(length(res) != 0, res)
 }
+
