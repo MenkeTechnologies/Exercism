28fe9b4ebb4ab6cd6d311cdb5ed469c2cb8b4930
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-19 13:29:59 -0400

    bs

--- ocaml/binary-search/binary_search.ml
+++ ocaml/binary-search/binary_search.ml
@@ -1,2 +1,10 @@
-let find _ _ =
-    failwith "'find' is missing"
+let find ary target =
+    let rec aux lo hi =
+        if lo > hi then Error "value not in array"
+        else let i = (lo + hi) / 2 in
+        match ary.(i) with
+        | x when x = target -> Ok i
+        | x when x > target -> aux lo (i - 1)
+        | _ -> aux (i + 1) hi
+    in
+    aux 0 (Array.length ary - 1)
