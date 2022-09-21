2dd00e0376988c237e1c9a6d89470c5d073271f5
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-21 10:28:45 -0400

    bin

--- clojurescript/binary-search/src/binary_search.cljs
+++ clojurescript/binary-search/src/binary_search.cljs
@@ -1,5 +1,17 @@
 (ns binary-search)
 
-(defn search-for [])
+(defn middle
+  ([vec] (middle 0 (count vec)))
+  ([lo hi] (quot (+ lo hi) 2)))
 
-(defn middle [])
+(defn aux [target vec lo hi]
+  (let [mid-idx (middle lo hi)
+          mid-val (nth vec mid-idx)]
+      (cond
+        (> lo hi) (throw (js/Error. "not found"))
+        (< mid-val target) (recur target vec (inc mid-idx) hi)
+        (> mid-val target) (recur target vec lo (dec mid-idx))
+        :else mid-idx)))
+
+(defn search-for [target vec]
+    (aux target vec 0 (dec (count vec))))
