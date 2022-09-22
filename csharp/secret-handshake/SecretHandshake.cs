de0e04ae6881df83e561db09a75993a8c6c2e4aa
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-22 06:35:32 -0400

    sh

--- csharp/secret-handshake/SecretHandshake.cs
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -1,9 +1,20 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public static class SecretHandshake
 {
+    private static readonly IDictionary<int, string> Orders = new Dictionary<int, string>
+    {
+        { 1, "wink" },
+        { 2, "double blink" },
+        { 4, "close your eyes" },
+        { 8, "jump" }
+    };
+
     public static string[] Commands(int commandValue)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var s = Orders.Where(o => (commandValue & o.Key) > 0).Select(o => o.Value);
+
+        return (commandValue & 16) > 0 ? s.Reverse().ToArray() : s.ToArray();
     }
 }
