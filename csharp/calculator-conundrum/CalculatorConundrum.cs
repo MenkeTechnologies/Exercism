4e661dafba4d07eeb3eae45374c9ac56e2886c72
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-22 08:36:03 -0400

    calc

--- csharp/calculator-conundrum/CalculatorConundrum.cs
+++ csharp/calculator-conundrum/CalculatorConundrum.cs
@@ -4,6 +4,15 @@ public static class SimpleCalculator
 {
     public static string Calculate(int operand1, int operand2, string operation)
     {
-        throw new NotImplementedException("Please implement the SimpleCalculator.Calculate() method");
+        return operation switch
+        {
+            "+" => $"{operand1} + {operand2} = {SimpleOperation.Addition(operand1, operand2)}",
+            "*" => $"{operand1} * {operand2} = {SimpleOperation.Multiplication(operand1, operand2)}",
+            "/" when operand2 == 0 => "Division by zero is not allowed.",
+            "/" => $"{operand1} / {operand2} = {SimpleOperation.Division(operand1, operand2)}",
+            "" => throw new ArgumentException(),
+            null => throw new ArgumentNullException(),
+            _ => throw new ArgumentOutOfRangeException()
+        };
     }
 }
