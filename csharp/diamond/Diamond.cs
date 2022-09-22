2b7e7c11d8364d0c95c11ef1d6bcc115b8be20e1
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-22 06:40:40 -0400

    dia

--- csharp/diamond/Diamond.cs
+++ csharp/diamond/Diamond.cs
@@ -1,9 +1,20 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public static class Diamond
 {
+    private static IEnumerable<T> Reflect<T>(this IEnumerable<T> ie) =>
+        ie.Concat(ie.Reverse().Skip(1));
+
     public static string Make(char target)
     {
-        throw new NotImplementedException("Please implement this function");
+        int width = target - 'A';
+        var lines = Enumerable.Range(0, width + 1).Select(i =>
+        {
+            char letter = (char)('A' + i);
+            string leftHalf = new string(' ', width - i) + letter + new string(' ', i);
+            return string.Concat(leftHalf.Reflect());
+        });
+        return string.Join("\n", lines.Reflect());
     }
 }
