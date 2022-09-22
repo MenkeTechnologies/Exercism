8ab32b2666f0e19be251d695534a8930837ebe74
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-22 06:27:42 -0400

    luhn

--- csharp/luhn/Luhn.cs
+++ csharp/luhn/Luhn.cs
@@ -1,9 +1,22 @@
-using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Luhn
 {
+    private static readonly Regex WsRegex = new(@"\s");
+
     public static bool IsValid(string number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var nows = WsRegex.Replace(number, "");
+
+        if (!nows.All(char.IsDigit) || nows.Length < 2)
+            return false;
+        return nows
+            .Reverse()
+            .Where(char.IsDigit)
+            .Select(c => (int)char.GetNumericValue(c))
+            .Select((n, i) => ((i % 2) == 0) ? n : n * 2)
+            .Select(n => n > 9 ? n - 9 : n)
+            .Sum() % 10 == 0;
     }
 }
