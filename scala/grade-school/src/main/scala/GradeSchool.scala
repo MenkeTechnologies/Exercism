44b7857b4385552c7dc3ffc755a18187c24c504b
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-22 06:12:53 -0400

    gs

--- scala/grade-school/src/main/scala/GradeSchool.scala
+++ scala/grade-school/src/main/scala/GradeSchool.scala
@@ -1,7 +1,7 @@
 import scala.collection.mutable.SortedMap
 
 class School {
-  var db = SortedMap.empty[Int, Seq[String]]
+  val db = SortedMap.empty[Int, Seq[String]]
 
   def add(name: String, g: Int) = db += (g -> (grade(g) :+ name))
 
