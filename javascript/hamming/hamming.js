d115f272d1410062f746f21381503b11a66ac6b1
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-25 01:16:57 -0400

    ham

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,8 +1,6 @@
 export const compute = (s1, s2) =>
-    s1.length !== s2.length ?
+  s1.length !== s2.length ?
     (() => {
-        throw new Error('strands must be of equal length')
+      throw new Error('strands must be of equal length')
     })() :
-    [...s1]
-    .map((c, i) => c !== s2[i] ? 1 : 0)
-    .reduce((acc, n) => acc + n, 0)
+    [...s1].filter((c, i) => c !== s2[i]).length
