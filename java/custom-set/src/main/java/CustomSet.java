9653fc3587c0f38a420e00dd60827c2356b9a92c
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-26 08:49:31 -0400

    set

--- java/custom-set/src/main/java/CustomSet.java
+++ java/custom-set/src/main/java/CustomSet.java
@@ -1,10 +1,68 @@
-/*
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+public class CustomSet<T> {
+    private List<T> lst;
 
-Please remove this comment when submitting your solution.
+    public CustomSet(List<T> lst) {
+        this.lst = new ArrayList<>();
+        this.lst.addAll(lst);
+    }
 
-*/
+    public void add(T element) {
+        if (!contains(element)) {
+            lst.add(element);
+        }
+    }
+
+    public boolean isEmpty() {
+        return lst.size() == 0;
+    }
+
+    public boolean contains(T element) {
+        return lst.contains(element);
+    }
+
+    public boolean isSubset(CustomSet<T> customSet) {
+        return customSet.lst.parallelStream().allMatch(el -> lst.contains(el));
+    }
+
+    public boolean isDisjoint(CustomSet<T> customSet) {
+        return customSet.lst.parallelStream().noneMatch(el -> lst.contains(el));
+    }
+
+    public CustomSet<T> getIntersection(CustomSet<T> customSet) {
+        return new CustomSet<>(customSet.lst.parallelStream()
+                .filter(el -> lst.contains(el)).collect(Collectors.toList()));
+    }
+
+    public CustomSet<T> getDifference(CustomSet<T> customSet) {
+        return new CustomSet<>(lst.parallelStream().filter(el -> !customSet.contains(el)).collect(Collectors.toList()));
+    }
+
+    public CustomSet<T> getUnion(CustomSet<T> customSet) {
+        CustomSet<T> customSet1 = new CustomSet<>(customSet.lst.parallelStream()
+                .filter(el -> !lst.contains(el)).collect(Collectors.toList()));
+        customSet1.lst.addAll(lst);
+        return customSet1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass() || o.getClass() != CustomSet.class) {
+            return false;
+        }
+        CustomSet<T> customSet = (CustomSet<T>) o;
+        return isSubset(customSet) && customSet.isSubset(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lst);
+    }
+}
