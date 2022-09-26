3499513fb29faf599737d256617225ea926087f1
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-26 08:45:03 -0400

    mb

--- java/matching-brackets/src/main/java/BracketChecker.java
+++ java/matching-brackets/src/main/java/BracketChecker.java
@@ -1,10 +1,28 @@
-/*
+import java.util.HashMap;
+import java.util.Stack;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class BracketChecker {
+    private static final HashMap<Character, Character> pairs =
+            new HashMap<>() {{
+                put('(', ')');
+                put('{', '}');
+                put('[', ']');
+            }};
+    private final String s;
 
-Please remove this comment when submitting your solution.
+    BracketChecker(String text) {
+        s = text;
+    }
 
-*/
+    boolean areBracketsMatchedAndNestedCorrectly() {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (pairs.containsKey(c)) {
+                stack.push(c);
+            } else if (pairs.containsValue(c) && (stack.isEmpty() || pairs.get(stack.pop()) != c)) {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+}
