1d8bb289fb7510f9e9cbd145d2ce3532f18188ae
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-27 00:22:39 -0400

    sh

--- crystal/secret-handshake/src/secret_handshake.cr
+++ crystal/secret-handshake/src/secret_handshake.cr
@@ -1,1 +1,19 @@
-# Please implement your solution to secret-handshake in this file
+module SecretHandshake
+  COMMANDS = {
+    1 => "wink",
+    2 => "double blink",
+    4 => "close your eyes",
+    8 => "jump"
+  }
+  REVERSE_MASK = 16
+
+  def self.commands(n)
+    actions = COMMANDS.select { |mask, _| n & mask > 0 }
+                      .values
+    if n & REVERSE_MASK > 0
+      actions.reverse
+    else
+      actions
+    end
+  end
+end
