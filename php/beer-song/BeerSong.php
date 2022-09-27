6c6d07d1469daf665643d5f0cd635208b7cd0509
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-27 00:27:44 -0400

    bs

--- php/beer-song/BeerSong.php
+++ php/beer-song/BeerSong.php
@@ -1,43 +1,34 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
-declare(strict_types=1);
-
 class BeerSong
 {
-    public function verse(int $number): string
-    {
-        throw new \BadMethodCallException("Implement the verse method");
-    }
-
-    public function verses(int $start, int $finish): string
-    {
-        throw new \BadMethodCallException("Implement the verses method");
-    }
-
     public function lyrics(): string
     {
-        throw new \BadMethodCallException("Implement the lyrics method");
+        return $this->verses(99, 0);
+    }
+    public function verse($verse){
+        $minus1 = $verse - 1;
+        switch ($verse){
+            case 2:
+                return "2 bottles of beer on the wall, 2 bottles of beer.\n"
+                                 ."Take one down and pass it around, 1 bottle of beer on the wall.\n";
+            case 1:
+                return  "1 bottle of beer on the wall, 1 bottle of beer.\n"
+                                 ."Take it down and pass it around, no more bottles of beer on the wall.\n";
+            case 0:
+                return "No more bottles of beer on the wall, no more bottles of beer.\n"
+                                 ."Go to the store and buy some more, 99 bottles of beer on the wall.";
+            default:
+                return "$verse bottles of beer on the wall, $verse bottles of beer.\n"
+                                 ."Take one down and pass it around, $minus1 bottles of beer on the wall.\n";
+        }
+    }
+
+    public function verses(int $from, int $to): string
+    {
+        return implode("\n", array_map(
+            fn($number) => $this->verse($number),
+            range($from, $to)
+        ));
     }
 }
