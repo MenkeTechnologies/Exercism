c329a24934b409a0a345d2fab20ae4de7e1661c4
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-27 00:33:28 -0400

    on

--- php/ordinal-number/OrdinalNumber.php
+++ php/ordinal-number/OrdinalNumber.php
@@ -1,30 +1,23 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
-declare(strict_types=1);
-
 function toOrdinal(int $number): string
 {
-    throw new \BadFunctionCallException("Implement the toOrdinal function");
+    $num = strval($number);
+    if ($num == '0') {
+        return '0';
+    }
+    if ($num == '1') {
+        return '1st';
+    }
+    if ($num[0] == '1') {
+        return $num . 'th';
+    }
+    if ($num[-1] == '2') {
+        return $num . 'nd';
+    }
+    if ($num[-1] == '3') {
+        return $num . 'rd';
+    }
+
+    return $num . 'th';
 }
