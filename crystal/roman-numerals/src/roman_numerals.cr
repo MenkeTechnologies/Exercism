09e5193e3acdd8d814e29c49a32422bbe823ff23
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-27 00:21:28 -0400

    rn

--- crystal/roman-numerals/src/roman_numerals.cr
+++ crystal/roman-numerals/src/roman_numerals.cr
@@ -1,1 +1,24 @@
-# Please implement your solution to roman-numerals in this file
+struct Number
+  private DICT = {
+    "M" => 1000,
+    "CM" => 900,
+    "D" => 500,
+    "CD" => 400,
+    "C" => 100,
+    "XC" => 90,
+    "L" => 50,
+    "XL" => 40,
+    "X" => 10,
+    "IX" => 9,
+    "V" => 5,
+    "IV" => 4,
+    "I" => 1
+  }
+  def to_roman
+    remainder = to_i
+    DICT.map do |sigil, value|
+      quotient, remainder = remainder.divmod(value)
+      sigil * quotient
+    end.join
+  end
+end
