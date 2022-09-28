cc50d2f82d818664a99d27812a3ab48ea9038adb
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-28 00:26:27 -0400

    pro

--- crystal/proverb/src/proverb.cr
+++ crystal/proverb/src/proverb.cr
@@ -1,1 +1,8 @@
-# Please implement your solution to proverb in this file
+module Proverb
+  def self.recite(wants)
+    return wants if wants.empty?
+    wants.each.cons_pair.map do |(a, b)|
+      "For want of a #{a} the #{b} was lost."
+    end.to_a << "And all for the want of a #{wants[0]}."
+  end
+end
