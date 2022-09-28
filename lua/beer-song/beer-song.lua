14d97a26147086d113ee8b275bf63c24233f0866
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-28 00:23:53 -0400

    beer

--- lua/beer-song/beer-song.lua
+++ lua/beer-song/beer-song.lua
@@ -1,11 +1,21 @@
-local beer = {}
-
-function beer.verse(number)
-
+local function verse(num)
+    if num == 0 then
+        return 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n'
+    elseif num == 1 then
+        return '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n'
+    elseif num == 2 then
+        return '2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n'
+    else
+        return num..' bottles of beer on the wall, '..num..' bottles of beer.\nTake one down and pass it around, '..(num-1)..' bottles of beer on the wall.\n'
+    end
 end
-
-function beer.sing(start, finish)
-
-end
-
-return beer
+return {
+    verse = verse,
+    sing = function(start, stop)
+        local song = {}
+        for i = start, stop or 0, -1 do
+            table.insert(song, verse(i))
+        end
+        return table.concat(song, '\n')
+    end
+}
