0f4474899e77a4cfc4c5f59854e40712962c32bf
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-28 00:11:34 -0400

    guess

--- haskell/guessing-game/src/GuessingGame.hs
+++ haskell/guessing-game/src/GuessingGame.hs
@@ -1,4 +1,10 @@
 module GuessingGame (reply) where
 
 reply :: Int -> String
-reply guess = error "Implement this function."
+reply guess
+  | guess == answer = "Correct"
+  | guess - 1 == answer || guess + 1 == answer = "So close"
+  | guess < answer = "Too low"
+  | otherwise = "Too high"
+  where answer = 42
+
