a31282f4bde0b7c96126e4586b33169bdb2175a5
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-28 00:17:19 -0400

    sum

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,8 @@
 module SumOfMultiples (sumOfMultiples) where
 
+divides :: Integer -> Integer -> Bool
+divides n m = (n `mod` m) == 0
+
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit = sum [x | x <- [1..limit-1],
+               any (divides x) [f | f <- factors, f > 0]]
