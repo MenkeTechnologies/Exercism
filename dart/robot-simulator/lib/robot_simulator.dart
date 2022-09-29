84ce4ad4a65c74670d20a00cb4d79f493c26b436
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-29 00:16:19 -0400

    rs

--- dart/robot-simulator/lib/robot_simulator.dart
+++ dart/robot-simulator/lib/robot_simulator.dart
@@ -1,3 +1,37 @@
+import 'orientation.dart';
+import 'position.dart';
 class Robot {
-  // Put your code here
+  final int orientation_len = Orientation.values.length;
+  Orientation orientation;
+  Position position;
+  Robot(this.position, this.orientation);
+  void move(String instructions) => instructions.split('').forEach((i) {
+        if (i == 'L') {
+          this.turnLeft();
+        } else if (i == 'R') {
+          this.turnRight();
+        } else if (i == 'A') {
+          this.advance();
+        }
+      });
+  void turnLeft() {
+    this.orientation = Orientation.values[
+        (orientation_len + this.orientation.index - 1) % orientation_len];
+  }
+  void turnRight() {
+    this.orientation =
+        Orientation.values[(this.orientation.index + 1) % orientation_len];
+  }
+  void advance() {
+    if (this.orientation == Orientation.east) {
+      ++this.position.x;
+    } else if (this.orientation == Orientation.west) {
+      --this.position.x;
+    } else if (this.orientation == Orientation.north) {
+      ++this.position.y;
+    } else if (this.orientation == Orientation.south) {
+      --this.position.y;
+    }
+  }
 }
+
