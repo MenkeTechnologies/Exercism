04a3277298ed7d9b98a4cbf02b17982b689619e3
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-29 00:13:20 -0400

    diam

--- dart/diamond/lib/diamond.dart
+++ dart/diamond/lib/diamond.dart
@@ -1,3 +1,13 @@
 class Diamond {
-  // Put your code here
+  List<String> rows(String letter) {
+    var res = <String>[];
+    var a = 'A'.codeUnitAt(0);
+    var diff = letter.codeUnitAt(0) - a;
+    for (var i = 0; i <= diff; ++i) {
+      var halfRow = ' ' * (diff - i) + String.fromCharCode(a + i) + ' ' * i;
+      var list = halfRow.runes.map((r) => String.fromCharCode(r)).toList();
+      res.add(list.followedBy(list.reversed.skip(1)).join());
+    }
+    return res.followedBy(res.reversed.skip(1)).toList();
+  }
 }
