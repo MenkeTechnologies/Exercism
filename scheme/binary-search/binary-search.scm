f28f3aca5e8eb7b7fa50997c3f5807289a5cd9f8
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-30 06:23:43 -0400

    bs

--- scheme/binary-search/binary-search.scm
+++ scheme/binary-search/binary-search.scm
@@ -1,4 +1,14 @@
 (import (rnrs))
 
-(define (binary-search array target)
-  'implement-me!)
+(define (middle a b)
+        (floor (/ (+ a b) 2)))
+
+(define (binary-search ary target)
+        (define (aux lo hi)
+                (let ((mid (middle lo hi)))
+                     (cond
+                           ((>= lo hi) 'not-found)
+                           ((= (vector-ref ary mid) target) mid)
+                           ((< target (vector-ref ary mid)) (aux lo mid))
+                           (#t (aux (+ 1 mid) hi)))))
+        (aux 0 (vector-length ary)))
