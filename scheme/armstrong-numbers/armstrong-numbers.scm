8b91220b7b6f0f4e3e68a39ac1ee969200ce2170
MenkeTechnologies <linux.dev25@gmail.com>
2022-09-30 06:27:13 -0400

    arm

--- scheme/armstrong-numbers/armstrong-numbers.scm
+++ scheme/armstrong-numbers/armstrong-numbers.scm
@@ -1,4 +1,18 @@
 (import (rnrs))
 
-(define (armstrong-numbers? n)
-  'implement-me!)
+(define (digit->number c)
+  (- (char->integer c)
+     (char->integer #\0)))
+
+(define (exponent e)
+  (lambda (b)
+    (expt b e)))
+
+(define (armstrong-number? n)
+  (let* ((num-string (number->string n))
+         (digits (string->list num-string))
+         (len (string-length num-string)))
+    (= n
+     (fold-left + 0
+                (map (exponent len)
+                     (map digit->number digits))))))
--- scheme/armstrong-numbers/test.scm
+++ scheme/armstrong-numbers/test.scm
@@ -1,91 +1,60 @@
-(load "armstrong-numbers.scm")
+(load "test-util.ss")
 
-(use-modules (srfi srfi-64))
+(define test-cases
+  `((test-success "Zero is an Armstrong number" equal?
+      armstrong-number? '(0) #t)
+    (test-success "Single digit numbers are Armstrong numbers"
+      equal? armstrong-number? '(5) #t)
+    (test-success "There are no 2 digit Armstrong numbers"
+      equal? armstrong-number? '(10) #f)
+    (test-success
+      "Three digit number that is an Armstrong number" equal?
+      armstrong-number? '(153) #t)
+    (test-success
+      "Three digit number that is not an Armstrong number" equal?
+      armstrong-number? '(100) #f)
+    (test-success
+      "Four digit number that is an Armstrong number" equal?
+      armstrong-number? '(9474) #t)
+    (test-success
+      "Four digit number that is not an Armstrong number" equal?
+      armstrong-number? '(9475) #f)
+    (test-success
+      "Seven digit number that is an Armstrong number" equal?
+      armstrong-number? '(9926315) #t)
+    (test-success
+      "Seven digit number that is not an Armstrong number" equal?
+      armstrong-number? '(9926314) #f)
+    (test-success "The 25th Armstrong number" equal?
+      armstrong-number? '(24678050) #t)
+    (test-success
+      "Eight digit number that is not an Armstrong number" equal?
+      armstrong-number? '(30852815) #f)
+    (test-success "The 28th Armstrong number" equal?
+      armstrong-number? '(146511208) #t)
+    (test-success
+      "Nine digit number that is not an Armstrong number" equal?
+      armstrong-number? '(927427554) #f)
+    (test-success "The 32nd Armstrong number" equal?
+      armstrong-number? '(4679307774) #t)
+    (test-success
+      "Ten digit number that is not an Armstrong number" equal?
+      armstrong-number? '(8320172640) #f)
+    (test-success "The 34th Armstrong number" equal?
+      armstrong-number? '(32164049651) #t)
+    (test-success
+      "Eleven digit number that is not an Armstrong number" equal?
+      armstrong-number? '(13930642218) #f)
+    (test-success "The 66th Armstrong number" equal?
+      armstrong-number? '(4422095118095899619457938) #t)
+    (test-success "The 77th Armstrong number" equal?
+      armstrong-number? '(1927890457142960697580636236639) #t)
+    (test-success "The 88th Armstrong number" equal?
+      armstrong-number? '(115132219018763992565095597973971522401)
+      #t)
+    (test-success "Thirty-nine digit number that is not an Armstrong number"
+      equal? armstrong-number?
+      '(7744959048678381442547644364350528967165) #f)))
 
-(test-begin "armstrong-numbers")
+(run-with-cli "armstrong-numbers.scm" (list test-cases))
 
-; (test-skip "Zero is an Armstrong number")
-(test-assert "Zero is an Armstrong number"
-  (armstrong-number? 0))
-
-(test-skip "Single digit numbers are Armstrong numbers")
-(test-assert "Single digit numbers are Armstrong numbers"
-  (armstrong-number? 5))
-
-(test-skip "There are no 2 digit Armstrong numbers")
-(test-assert "There are no 2 digit Armstrong numbers"
-  (not (armstrong-number? 10)))
-
-(test-skip "Three digit number that is an Armstrong number")
-(test-assert "Three digit number that is an Armstrong number"
-  (armstrong-number? 153))
-
-(test-skip "Three digit number that is not an Armstrong number")
-(test-assert "Three digit number that is not an Armstrong number"
-  (not (armstrong-number? 100)))
-
-(test-skip "Four digit number that is an Armstrong number")
-(test-assert "Four digit number that is an Armstrong number"
-  (armstrong-number? 9474))
-
-(test-skip "Four digit number that is not an Armstrong number")
-(test-assert "Four digit number that is not an Armstrong number"
-  (not (armstrong-number? 9475)))
-
-(test-skip "Seven digit number that is an Armstrong number")
-(test-assert "Seven digit number that is an Armstrong number"
-  (armstrong-number? 9926315))
-
-(test-skip "Seven digit number that is not an Armstrong number")
-(test-assert "Seven digit number that is not an Armstrong number"
-  (not (armstrong-number? 9926314)))
-
-(test-skip "The 25th Armstrong number")
-(test-assert "The 25th Armstrong number"
-  (armstrong-number? 24678050))
-
-(test-skip "Eight digit number that is not an Armstrong number")
-(test-assert "Eight digit number that is not an Armstrong number"
-  (not (armstrong-number? 30852815)))
-
-(test-skip "The 28th Armstrong number")
-(test-assert "The 28th Armstrong number"
-  (armstrong-number? 146511208))
-
-(test-skip "Nine digit number that is not an Armstrong number")
-(test-assert "Nine digit number that is not an Armstrong number"
-  (not (armstrong-number? 927427554)))
-
-(test-skip "The 32nd Armstrong number")
-(test-assert "The 32nd Armstrong number"
-  (armstrong-number? 4679307774))
-
-(test-skip "Ten digit number that is not an Armstrong number")
-(test-assert "Ten digit number that is not an Armstrong number"
-  (not (armstrong-number? 8320172640)))
-
-(test-skip "The 34th Armstrong number")
-(test-assert "The 34th Armstrong number"
-  (armstrong-number? 32164049651))
-
-(test-skip "Eleven digit number that is not an Armstrong number")
-(test-assert "Eleven digit number that is not an Armstrong number"
-  (not (armstrong-number? 13930642218)))
-
-(test-skip "The 66th Armstrong number")
-(test-assert "The 66th Armstrong number"
-  (armstrong-number? 4422095118095899619457938))
-
-(test-skip "The 77th Armstrong number")
-(test-assert "The 77th Armstrong number"
-  (armstrong-number? 1927890457142960697580636236639))
-
-(test-skip "The 88th Armstrong number")
-(test-assert "The 88th Armstrong number"
-  (armstrong-number? 115132219018763992565095597973971522401))
-
-(test-skip "Thirty-nine digit number that is not an Armstrong number")
-(test-assert "Thirty-nine digit number that is not an Armstrong number"
-  (not (armstrong-number? 7744959048678381442547644364350528967165)))
-
-(test-end "armstrong-numbers")
