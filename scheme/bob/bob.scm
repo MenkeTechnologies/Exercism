128ec626d8c14c8f7cfdb7fe27543786fdcec89f
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-01 12:56:25 -0400

    bob

--- scheme/bob/bob.scm
+++ scheme/bob/bob.scm
@@ -1,4 +1,19 @@
 (import (rnrs))
 
-(define (response-for message)
-  'implement-me!)
+(define (yell? msg)
+    (let ((letters (string-filter char-set:letter msg)))
+        (and (not (string-null? letters))
+            (string-every char-set:upper-case letters))))
+
+(define (question? msg)
+    (let* ((trimmed (string-trim-right msg char-set:whitespace))
+            (found (string-rindex trimmed #\?)))
+        (and found (= found (- (string-length trimmed) 1)))))
+
+(define (response-for msg)
+  (cond
+    ((and (question? msg) (yell? msg)) "Calm down, I know what I'm doing!")
+    ((question? msg) "Sure.")
+    ((yell? msg) "Whoa, chill out!")
+    ((string-every char-set:whitespace msg) "Fine. Be that way!")
+    (else "Whatever.")))
