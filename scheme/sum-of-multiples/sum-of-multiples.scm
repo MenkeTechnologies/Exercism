bda060c668e22e6ec84f8d5ad2fa1ae93c142ed1
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-01 13:25:07 -0400

    sum

--- scheme/sum-of-multiples/sum-of-multiples.scm
+++ scheme/sum-of-multiples/sum-of-multiples.scm
@@ -1,4 +1,14 @@
 (import (rnrs))
 
-(define (sum-of-multiples ints limit)
-  'implement-me!)
+(use-modules ((srfi srfi-1) #:select (any)))
+
+(define (is-multiple? limit factors)
+  (any (lambda (n) (zero? (modulo limit n))) factors))
+
+(define (sum-of-multiples factors limit)
+  (if (zero? limit)
+      0
+      (let ((factors (filter positive? factors)))
+        (fold-left (lambda (total n) (if (is-multiple? n factors)
+                                   (+ n total)
+                                   total)) 0 (iota limit)))))
