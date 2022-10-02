f21b60761ae2f1b09d86e2a42a7d02a088eb9f3c
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-02 00:51:05 -0400

    pf

--- scheme/prime-factors/prime-factors.scm
+++ scheme/prime-factors/prime-factors.scm
@@ -1,5 +1,8 @@
 (import (rnrs))
-
 (define (factorize n)
-  'implement-me!)
+  (let aux ((n n) (d 2) (factors '()))
+    (cond
+      ((= n 1) (reverse factors))
+      ((= 0 (modulo n d)) (aux (/ n d) d (cons d factors)))
+      (else (aux n (if (= d 2) 3 (+ d 2)) factors)))))
 
