8688da38ade7ca146bf4e20c9ce5ac3c5fb8e908
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-02 01:05:03 -0400

    pn

--- scheme/phone-number/phone-number.scm
+++ scheme/phone-number/phone-number.scm
@@ -1,5 +1,18 @@
 (import (rnrs))
 
-(define (clean phone-number)
-  'implement-me!)
+(define (validate-10-digits digits)
+  (and (= (string-length digits) 10)
+       (char>? (string-ref digits 0) #\1)
+       (char>? (string-ref digits 3) #\1)
+       digits))
 
+(define (validate digits)
+  (or (and (= 11 (string-length digits))
+           (char=? #\1 (string-ref digits 0))
+           (validate-10-digits (string-take-right digits 10)))
+      (validate-10-digits digits)))
+
+(define (clean phone-number)
+  (let ([digits (string-filter char-numeric? phone-number)])
+    (or (validate digits)
+        (raise-exception 'invalid))))
