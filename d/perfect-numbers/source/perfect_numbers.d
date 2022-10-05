5d7ab67406b6b9b4391397df4745f890f23a19d6
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-05 10:18:09 -0400

    pn

--- d/perfect-numbers/source/perfect_numbers.d
+++ d/perfect-numbers/source/perfect_numbers.d
@@ -1,11 +1,31 @@
 module perfect_numbers;
 
+import std.exception : enforce;
 enum Classification
 {
     DEFICIENT,
     PERFECT,
     ABUNDANT
 }
+pure Classification classify(immutable int input)
+{
+    enforce(input > 0, "Classification is only possible for natural numbers");
+    int aliquot = 0;
+    for (int i = 1; i < input; ++i) {
+        if (input % i == 0) {
+            aliquot += i;
+        }
+    }
+    if (aliquot == input) {
+        return Classification.PERFECT;
+    }
+    else if (aliquot > input) {
+        return Classification.ABUNDANT;
+    }
+
+    return Classification.DEFICIENT;
+}
+
 
 unittest
 {
