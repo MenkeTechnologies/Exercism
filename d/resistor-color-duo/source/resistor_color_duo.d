d59f59bf72f380e6fb3092b12bde92f357afb0f1
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-06 11:04:53 -0400

    rcd

--- d/resistor-color-duo/source/resistor_color_duo.d
+++ d/resistor-color-duo/source/resistor_color_duo.d
@@ -1,5 +1,21 @@
 module resistor_color_duo;
 
+import std.algorithm.searching : countUntil;
+import std.algorithm.iteration;
+import std.conv;
+
+class ResistorColorDuo
+{
+    private static immutable colors = [
+        "black", "brown", "red", "orange", "yellow", "green", "blue", "violet",
+        "grey", "white"
+    ];
+    static pure long value(immutable string[] colorsInput)
+    {
+        return 10 * colors.countUntil(colorsInput[0]) + colors.countUntil(colorsInput[1]);
+    }
+}
+
 unittest
 {
     immutable int allTestsEnabled = 0;
