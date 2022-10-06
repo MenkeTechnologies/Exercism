2f8e10f0a0fa66db701aafde066e946efa2f0080
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-06 10:59:14 -0400

    rc

--- d/resistor-color/source/resistor_color.d
+++ d/resistor-color/source/resistor_color.d
@@ -1,5 +1,18 @@
 module resistor_color;
 
+import std.algorithm.searching : countUntil;
+class ResistorColor
+{
+    static immutable colors = [
+        "black", "brown", "red", "orange", "yellow", "green", "blue", "violet",
+        "grey", "white"
+    ];
+    static pure long colorCode(immutable string color)
+    {
+        return colors.countUntil(color);
+    }
+}
+
 unittest
 {
     immutable int allTestsEnabled = 0;
