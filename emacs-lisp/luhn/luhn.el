e9ef72a6f742b495c52bf48e67c30be3553fe8e6
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-09 00:17:32 -0400

    luhn

--- emacs-lisp/luhn/luhn.el
+++ emacs-lisp/luhn/luhn.el
@@ -1,11 +1,27 @@
-;;; luhn.el --- Luhn exercise (exercism)
+(require 'cl-lib)
 
-;;; Commentary:
-
-;;; Code:
-
-
-
+(defun luhn-p (card-number)
+  (let ((nows (nreverse (replace-regexp-in-string
+                                        "\s"
+                                        ""
+                                        card-number)))
+        (index 0))
+    (if (string-match-p "[^[:digit:]]" nows)
+        (error "invalid card number")
+      (if (< (length nows) 2)
+          nil
+        (zerop (%
+                (cl-reduce
+                 #'+
+                 (mapcar (lambda (digit)
+                           (let ((num (string-to-number (char-to-string digit))))
+                             (cl-incf index)
+                             (if (zerop (% index 2))
+                                 (if (> (* num 2) 9)
+                                     (- (* num 2) 9)
+                                   (* num 2))
+                               num)))
+                         nows))
+                10))))))
 
 (provide 'luhn)
-;;; luhn.el ends here
