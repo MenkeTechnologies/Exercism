4bee3f328ef7cb8d06a56427df325df66e9ca59a
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-09 00:03:57 -0400

    ba

--- scala/bank-account/src/main/scala/BankAccount.scala
+++ scala/bank-account/src/main/scala/BankAccount.scala
@@ -1,13 +1,16 @@
+import java.util.concurrent.atomic.AtomicInteger
+
 trait BankAccount {
-
   def closeAccount(): Unit
-
   def getBalance: Option[Int]
-
   def incrementBalance(increment: Int): Option[Int]
 }
-
-object Bank {
-  def openAccount(): BankAccount = ???
+class BankAccountImpl() extends BankAccount {
+  private var balance: Option[AtomicInteger] = Some(new AtomicInteger(0))
+  def closeAccount(): Unit = balance = None
+  def getBalance = balance.map(_.intValue())
+  def incrementBalance(increment: Int) = balance.map(_.addAndGet(increment))
 }
-
+object Bank {
+  def openAccount() = new BankAccountImpl()
+}
