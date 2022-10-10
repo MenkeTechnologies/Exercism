0cea93fa1a36735d42bef97f1ed18bb5d1851143
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-10 11:09:07 -0400

    sum mult

--- c/sum-of-multiples/sum_of_multiples.c
+++ c/sum-of-multiples/sum_of_multiples.c
@@ -1,1 +1,16 @@
 #include "sum_of_multiples.h"
+
+unsigned int sum( const unsigned int *factors, const size_t number_of_factors,
+        const unsigned int limit) {
+    unsigned int sum = 0;
+    for (unsigned int n = 1; n < limit; ++n) {
+        for (size_t i = 0; i < number_of_factors; ++i) {
+            if (factors[i] == 0) continue;
+            if (n % factors[i] == 0) {
+                sum += n;
+                break;
+            }
+        }
+    }
+    return sum;
+}
--- c/sum-of-multiples/sum_of_multiples.h
+++ c/sum-of-multiples/sum_of_multiples.h
@@ -1,9 +1,5 @@
-#ifndef SUM_OF_MULTIPLES_H
-#define SUM_OF_MULTIPLES_H
-
+#pragma once
 #include <stddef.h>
 
-unsigned int sum(const unsigned int *factors, const size_t number_of_factors,
-                 const unsigned int limit);
-
-#endif
+unsigned int sum(const unsigned int *factors, size_t number_of_factors,
+                 unsigned int limit);
