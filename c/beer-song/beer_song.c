a63bbb7a0da4a7c2a88a5c7b895cca90d56ce8dc
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-10 11:15:48 -0400

    bs

--- c/beer-song/beer_song.c
+++ c/beer-song/beer_song.c
@@ -1,1 +1,23 @@
 #include "beer_song.h"
+#include <stdio.h>
+
+static const char *FIRST_LINE[] = {
+        "No more bottles of beer on the wall, no more bottles of beer.",
+        "1 bottle of beer on the wall, 1 bottle of beer.",
+        "%u bottles of beer on the wall, %u bottles of beer."
+};
+static const char *SECOND_LINE[] = {
+        "Go to the store and buy some more, 99 bottles of beer on the wall.",
+        "Take it down and pass it around, no more bottles of beer on the wall.",
+        "Take one down and pass it around, 1 bottle of beer on the wall.",
+        "Take one down and pass it around, %u bottles of beer on the wall."
+};
+
+void recite(uint8_t start_bottles, uint8_t take_down, char **song) {
+    uint8_t end_bottle = start_bottles - take_down + 1;
+    for (int bottle = start_bottles; bottle >= end_bottle; --bottle) {
+        sprintf(*song++, FIRST_LINE[bottle > 1 ? 2 : bottle], bottle, bottle);
+        sprintf(*song++, SECOND_LINE[bottle - 1 > 2 ? 3 : bottle], bottle - 1);
+        ++song;
+    }
+}
--- c/beer-song/beer_song.h
+++ c/beer-song/beer_song.h
@@ -1,8 +1,5 @@
-#ifndef BEER_SONG_H
-#define BEER_SONG_H
+#pragma once
 
 #include <stdint.h>
 
 void recite(uint8_t start_bottles, uint8_t take_down, char **song);
-
-#endif
