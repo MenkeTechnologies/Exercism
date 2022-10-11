d1a0665304943b72520fc5f498e706f4752b959d
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-11 10:58:25 -0400

    wc

--- emacs-lisp/word-count/word-count.el
+++ emacs-lisp/word-count/word-count.el
@@ -1,9 +1,11 @@
-;;; word-count.el --- word-count Exercise (exercism)
+(require 'seq)
 
-;;; Commentary:
-
-;;; Code:
-
+(defun word-count (s)
+  (let* ((word-list (mapcar #'downcase (split-string (replace-regexp-in-string
+                                                       "[^a-zA-Z0-9' ]" "" s))))
+         (uniq-list (seq-uniq word-list)))
+    (seq-map (lambda (w) (cons w (seq-count (lambda (w2) (string= w w2))
+                                            word-list)))
+             uniq-list)))
 
 (provide 'word-count)
-;;; word-count.el ends here
