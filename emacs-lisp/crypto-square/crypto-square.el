df25b45f88daf02cced9f54eedc2befd5975c3d4
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-12 00:47:27 -0400

    crypto

--- emacs-lisp/crypto-square/crypto-square.el
+++ emacs-lisp/crypto-square/crypto-square.el
@@ -1,9 +1,15 @@
-;;; crypto-square.el --- Crypto Square (exercism)
+(require 'cl)
+(require 'subr-x)
+(require 'seq)
 
-;;; Commentary:
-
-;;; Code:
-
+(defun encipher (s)
+  (if (zerop (length s)) ""
+    (let* ((normalized (replace-regexp-in-string "[^[:alnum:]]" "" (downcase s)))
+           (size (ceiling (sqrt (length normalized))))
+           (fmt (format "%%-%ds" size)))
+      (string-join (thread-last (seq-partition normalized size)
+                     (mapcar #'(lambda (chunk) (format fmt chunk)))
+                     (apply #'cl-mapcar #'string))
+                   " "))))
 
 (provide 'crypto-square)
-;;; crypto-square.el ends here
