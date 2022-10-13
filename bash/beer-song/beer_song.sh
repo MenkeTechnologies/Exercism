ff2ba193bfeff13c60342ceea3bf515108f0bb39
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-13 10:41:50 -0400

    bs

--- bash/beer-song/beer_song.sh
+++ bash/beer-song/beer_song.sh
@@ -1,24 +1,42 @@
 #!/usr/bin/env bash
+#
+bottles () {
+  if (( $1 > 1 )); then
+      printf '%d bottles' "$1"
+  elif (( $1 == 1 )); then
+      printf '1 bottle'
+  elif ! (( $1 )); then
+      printf 'no more bottles'
+  fi
+}
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+print_verse () {
+    local s
+    if (( $1 )); then
+        printf '%s of beer on the wall, %s of beer.\n' "$(bottles $1)" "$(bottles $1)"
+        (( $1 == 1 )) && s=it || s=one
+        printf 'Take %s down and pass it around, %s of beer on the wall.\n' "$s" "$(bottles $(($1-1)) )"
+    else
+        printf 'No more bottles of beer on the wall, no more bottles of beer.\n'
+        printf 'Go to the store and buy some more, 99 bottles of beer on the wall.\n'
+    fi
+}
+
+if (( !$# || $# > 2 )); then
+    echo '1 or 2 arguments expected'
+    exit 1
+fi
+
+start=$1
+end=${2:-$start}
+
+if (( start < end )); then
+    echo 'Start must be greater than End'
+    exit 1
+fi
+for (( i = start; i > end; --i )); do
+    print_verse "$i"
+    echo
+done
+
+print_verse "$end"
