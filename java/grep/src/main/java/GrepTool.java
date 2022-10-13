b749410d8c978b67a320ad960f56e21e05117df8
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-13 10:30:15 -0400

    grep

--- java/grep/src/main/java/GrepTool.java
+++ java/grep/src/main/java/GrepTool.java
@@ -1,10 +1,90 @@
-/*
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+public class GrepTool {
+    private boolean showLineNum;
+    private boolean caseInsensitive;
+    private boolean printFileNameOnly;
+    private boolean matchEntireLine;
+    private boolean inverted;
+    private boolean multipleFilesMode;
 
-Please remove this comment when submitting your solution.
+    public String grep(String pattern, List<String> flagList, List<String> fileList) {
+        processFlags(flagList);
+        return grepOnMultipleFiles(pattern, fileList);
+    }
 
-*/
+    public void processFlags(List<String> flagList) {
+        showLineNum = flagList.contains("-n");
+        caseInsensitive = flagList.contains("-i");
+        printFileNameOnly = flagList.contains("-l");
+        matchEntireLine = flagList.contains("-x");
+        inverted = flagList.contains("-v");
+    }
+
+    public String grepOnMultipleFiles(String pattern, List<String> fileList) {
+        if (fileList.size() > 1) multipleFilesMode = true;
+        StringBuilder sb = new StringBuilder();
+        List<String> collect = fileList.stream().parallel().map(file -> grepOnSingleFile(pattern, file)).toList();
+        collect.forEach(sb::append);
+        if (sb.length() != 0) trimLastNewLine(sb);
+        return sb.toString();
+    }
+
+    public String grepOnSingleFile(String pattern, String file) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            return grepOnSingleFile(pattern, file, reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public void trimLastNewLine(StringBuilder sb) {
+        sb.setLength(sb.length() - 1);
+    }
+
+    public String grepOnSingleFile(String pattern, String file, BufferedReader reader) throws IOException {
+        Pattern ptn = compilePattern(pattern);
+        StringBuilder sb = new StringBuilder();
+        String line = reader.readLine();
+        for (int lineNum = 1; line != null; lineNum++) {
+            if (match(ptn, line)) {
+                if (printFileNameOnly) return file + "\n";
+                String formatted = formattedLine(file, lineNum, line);
+                sb.append(formatted).append("\n");
+            }
+            line = reader.readLine();
+        }
+        return sb.toString();
+    }
+
+    public Pattern compilePattern(String pattern) {
+        int flag = Pattern.LITERAL | (caseInsensitive ? Pattern.CASE_INSENSITIVE : 0);
+        return Pattern.compile(pattern, flag);
+    }
+
+    public boolean match(Pattern pattern, String toMatch) {
+        Matcher matcher = pattern.matcher(toMatch);
+        boolean ret = matchEntireLine ? matcher.matches() : matcher.find();
+        return inverted != ret;
+    }
+
+    public String formattedLine(String file, int lineNum, String line) {
+        StringBuilder sb = new StringBuilder();
+        if (multipleFilesMode) {
+            sb.append(file);
+            sb.append(":");
+        }
+        if (showLineNum) {
+            sb.append(lineNum);
+            sb.append(":");
+        }
+        sb.append(line);
+        return sb.toString();
+    }
+}
