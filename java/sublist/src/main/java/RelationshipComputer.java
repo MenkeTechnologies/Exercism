b3d3c4f4fae82b440d5f57d788aabc73b8fb0a71
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-13 10:19:44 -0400

    sl

--- java/sublist/src/main/java/RelationshipComputer.java
+++ java/sublist/src/main/java/RelationshipComputer.java
@@ -1,10 +1,14 @@
-/*
+import java.util.Collections;
+import java.util.List;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
+class RelationshipComputer<T> {
+    public Relationship computeRelationship(List<T> ListA, List<T> ListB) {
+        if (ListA.size() > ListB.size() && Collections.indexOfSubList(ListA, ListB) != -1)
+            return Relationship.SUPERLIST;
+        else if (ListA.size() < ListB.size() && Collections.indexOfSubList(ListB, ListA) != -1)
+            return Relationship.SUBLIST;
+        else if (Collections.indexOfSubList(ListA, ListB) != -1)
+            return Relationship.EQUAL;
+        return Relationship.UNEQUAL;
+    }
+}
