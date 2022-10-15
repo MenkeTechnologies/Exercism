63743becc20ae128bb8373d43a86e98b17f01093
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-15 13:07:42 -0400

    allergies

--- tcl/allergies/allergies.tcl
+++ tcl/allergies/allergies.tcl
@@ -1,7 +1,15 @@
+variable allergies {eggs peanuts shellfish strawberries tomatoes chocolate pollen cats}
+
 proc allergicTo {allergen score} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    return [expr {$allergen in [listAllergies $score]}]
 }
 
 proc listAllergies {score} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    variable allergies
+    if {$score == 0} {return [list]}
+    set res [list]
+    for {set i 0} {$i < [llength $allergies]} {incr i} {
+        if {($score & (1 << $i)) > 0} {lappend res [lindex $allergies $i]}
+    }
+    return $res
 }
