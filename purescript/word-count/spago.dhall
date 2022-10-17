e776ede47580df798c965766abf9a7a7ef830a1c
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-17 00:18:12 -0400

    wc

--- purescript/word-count/spago.dhall
+++ purescript/word-count/spago.dhall
@@ -4,18 +4,22 @@ You can edit this file as you like.
 -}
 { name = "word-count"
 , dependencies =
-  [ "console"
+  [ "arrays"
+  , "console"
   , "datetime"
   , "effect"
   , "either"
   , "enums"
+  , "foldable-traversable"
   , "integers"
   , "lists"
+  , "maybe"
   , "ordered-collections"
   , "prelude"
   , "psci-support"
   , "strings"
   , "test-unit"
+  , "tuples"
   , "unicode"
   ]
 , packages = ./packages.dhall
--- purescript/word-count/src/WordCount.purs
+++ purescript/word-count/src/WordCount.purs
@@ -1,3 +1,21 @@
 module WordCount
   ( wordCount
-  ) where
+  )
+  where
+import Prelude
+import Data.Array (foldr)
+import Data.Array.NonEmpty (catMaybes)
+import Data.Map (Map, empty, insertWith)
+import Data.Maybe (fromMaybe)
+import Data.String (toLower)
+import Data.String.Regex (match)
+import Data.String.Regex.Flags (global)
+import Data.String.Regex.Unsafe (unsafeRegex)
+
+wordCount = toLower >>> words >>> foldr accWords empty
+  where accWords w = insertWith (+) w 1
+
+words s = fromMaybe [] do
+  let re = unsafeRegex "(\\w[\\w']+\\w)|([\\w]+)" global
+  matches <- match re s
+  catMaybes matches # pure
