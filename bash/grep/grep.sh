3cc504c5b5d0cfe9dd3e753bd7d39e1bbb1aa05b
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-17 00:37:17 -0400

    grep

--- bash/grep/grep.sh
+++ bash/grep/grep.sh
@@ -1,24 +1,46 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+line_num=0
+case_sensitive=1
+filename=0
+invert=0
+full_line=0
+
+while [[ $1 = -* ]]; do
+    case "$1" in
+        -n) line_num=1;;
+        -l) filename=1;;
+        -i) case_sensitive=0;;
+        -v) invert=1;;
+        -x) full_line=1;;
+        -*) ;;
+    esac
+    shift
+done
+pattern=$1
+shift
+
+(( ! case_sensitive )) && shopt -s nocasematch || shopt -u nocasematch
+
+for file; do
+    line=0 cont=1
+
+    while read -r; do
+
+        (( ++line ))
+
+        [[ $REPLY == $pattern ]] || (( ! full_line )) && [[ $REPLY = *"$pattern"* ]]
+
+        if (( (invert && $?) || ! (invert || $?) )); then
+            if (( filename )); then
+                printf '%s\n' "$file"
+                break
+            fi
+
+            (( $# > 1 )) && printf '%s:' "$file"
+            (( line_num )) && printf '%d:' "$line"
+
+            printf '%s\n' "$REPLY"
+        fi
+    done < "$file"
+done
