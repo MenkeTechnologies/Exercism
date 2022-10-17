2eaf3af9fce2529874fa917f1ce5539b71f84c93
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-16 22:45:05 -0400

    acro

--- purescript/acronym/src/Acronym.purs
+++ purescript/acronym/src/Acronym.purs
@@ -1,3 +1,16 @@
 module Acronym
   ( abbreviate
   ) where
+import Prelude
+import Data.Array (cons, filter)
+import Data.Maybe (Maybe(..))
+import Data.String
+import Data.String.CodePoints
+
+isUpper c = c >= codePointFromChar 'A' && c <= codePointFromChar 'Z'
+
+abbreviate = toUpper <<< joinWith "" <<< map firstAndCaps <<< split (Pattern " ")
+  where firstAndCaps s = case uncons s of
+          Just uc -> fromCodePointArray (cons uc.head (filter isUpper (toCodePointArray uc.tail)))
+          Nothing -> s
+
