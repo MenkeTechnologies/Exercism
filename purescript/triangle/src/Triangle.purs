1b6cc62f86f93c4891c0d8e1a0e001704033bda7
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-16 22:23:52 -0400

    tri

--- purescript/triangle/src/Triangle.purs
+++ purescript/triangle/src/Triangle.purs
@@ -2,3 +2,21 @@ module Triangle
   ( Triangle(Equilateral, Isosceles, Scalene)
   , triangleKind
   ) where
+import Prelude
+import Data.Either
+
+data Triangle = Equilateral | Isosceles | Scalene
+
+derive instance eqTriangle :: Eq Triangle
+
+instance showTriangle :: Show Triangle where
+  show Equilateral = "Equilateral"
+  show Isosceles = "Isosceles"
+  show Scalene  = "Scalene"
+
+triangleKind a b c
+  | a <= 0 || b <= 0 || c <= 0 = Left "Invalid lengths"
+  | a + b < c || a + c < b || b + c < a = Left "Violates inequality"
+  | a == b && a == c = Right Equilateral
+  | a == b || a == c || b == c = Right Isosceles
+  | otherwise = Right Scalene
