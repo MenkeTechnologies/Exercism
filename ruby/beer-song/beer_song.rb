7581250b8acd0bdb34331383f41ac85c87ec1f0b
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-18 11:30:55 -0400

    bs

---
+++ ruby/beer-song/beer_song.rb
@@ -0,0 +1,25 @@
+module BeerSong
+  def self.verse(bottles)
+    if bottles.zero?
+      <<~TEXT
+        No more bottles of beer on the wall, no more bottles of beer.
+        Go to the store and buy some more, 99 bottles of beer on the wall.
+      TEXT
+    elsif bottles > 1
+      <<~TEXT
+        #{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+        Take one down and pass it around, #{bottles - 1} bottle#{'s' if bottles > 2 } of beer on the wall.
+      TEXT
+    else
+      <<~TEXT
+        1 bottle of beer on the wall, 1 bottle of beer.
+        Take it down and pass it around, no more bottles of beer on the wall.
+      TEXT
+    end
+  end
+
+  def self.recite(start, n)
+    start.downto(start + 1 - n).map { verse(_1) }.join("\n")
+  end
+
+end
