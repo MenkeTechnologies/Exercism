5d0c2c6ff5c42e63735fa9a04ee127a9d71fb335
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-19 13:09:08 -0400

    tri

--- common-lisp/triangle/triangle.lisp
+++ common-lisp/triangle/triangle.lisp
@@ -5,5 +5,10 @@
 (in-package :triangle)
 
 (defun triangle-type-p (type a b c)
-  "Deterimines if a triangle (given by side lengths A, B, C) is of the given TYPE"
-  )
+  (let ((sorted (sort (list a b c) #'<)))
+    (when (> (+ (first sorted) (second sorted)) (third sorted))
+      (case type
+        (:equilateral (= a b c))
+        (:isosceles (or (= a b) (= b c) (= c a)))
+        (:scalene (and (/= a b) (/= b c) (/= c a)))))))
+
