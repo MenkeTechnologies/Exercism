e718b5117f486e617cf7fa2ba566c5b9845c54ef
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-19 13:10:38 -0400

    luhn

--- common-lisp/luhn/luhn.lisp
+++ common-lisp/luhn/luhn.lisp
@@ -1,7 +1,25 @@
-(defpackage :luhn
-  (:use :cl)
-  (:export :validp))
+(in-package #:cl-user)
+(defpackage #:luhn
+  (:use #:cl)
+  (:export #:validp))
+(in-package #:luhn)
 
-(in-package :luhn)
+(defun double (digit)
+  (let ((doubled (* digit 2)))
+    (if (> doubled 9)
+        (- doubled 9)
+        doubled)))
 
-(defun validp (input))
+(defun sum (s)
+  (loop
+     for i across s
+     for j from 0
+     summing (if (oddp j)
+                 (double (digit-char-p i))
+                 (digit-char-p i))))
+
+(defun validp (input)
+  (let ((rvs (reverse (remove #\Space input))))
+    (when (and (> (length rvs) 1)
+               (every 'digit-char-p rvs))
+      (zerop (mod (sum rvs) 10)))))
