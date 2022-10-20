2752fcd3d33e744a5fc6a409490c54b5e6402ced
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-20 08:29:17 -0400

    rot

--- typescript/rotational-cipher/rotational-cipher.ts
+++ typescript/rotational-cipher/rotational-cipher.ts
@@ -1,3 +1,12 @@
-export function rotate() {
-  throw new Error('Remove this statement and implement this function')
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+const rotateChar = (c: string, n: number): string => {
+  const lc = c.toLowerCase()
+  const i = ALPHABET.indexOf(lc)
+  if (i === -1) { return c }
+  const d = ALPHABET[ (i + n) % ALPHABET.length ]
+  return c === lc ? d : d.toUpperCase()
 }
+
+export const rotate = (s: string, rot: number = 13): string =>
+  [...s].map((c) => rotateChar(c, rot)).join('')
