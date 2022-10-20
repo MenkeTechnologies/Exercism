bff512d093ab5c3d234b68d66d37a2f5490ebb24
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-20 09:07:09 -0400

    bs

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,8 +1,7 @@
-export const verse = (index) =>
-  index === 0 ?
-    [`No more bottles of beer on the wall, no more bottles of beer.`, `Go to the store and buy some more, 99 bottles of beer on the wall.`] :
-    index === 1 ? [`1 bottle of beer on the wall, 1 bottle of beer.`, `Take it down and pass it around, no more bottles of beer on the wall.`] :
-      [`${index} bottles of beer on the wall, ${index} bottles of beer.`, `Take one down and pass it around, ${index - 1} bottle${index - 1 > 1 ? "s" : ""} of beer on the wall.`]
+export const verse = i =>
+  i === 0 ?
+    [`No more bottles of beer on the wall, no more bottles of beer.`, `Go to the store and buy some more, 99 bottles of beer on the wall.`, ''] :
+    i === 1 ? [`1 bottle of beer on the wall, 1 bottle of beer.`, `Take it down and pass it around, no more bottles of beer on the wall.`, ''] :
+      [`${i} bottles of beer on the wall, ${i} bottles of beer.`, `Take one down and pass it around, ${i - 1} bottle${i - 1 > 1 ? "s" : ""} of beer on the wall.`, '']
 
-export const recite = (initialBottlesCount = 99, takeDownCount= 0) =>
-  [...Array(initialBottlesCount + 1 - takeDownCount).keys()].map(i => initialBottlesCount - i).map(i => verse(i))
+export const recite = (initialBottlesCount = 99, takeDownCount= 0) => [...Array(takeDownCount).keys()].flatMap(i => verse(initialBottlesCount - i)).slice(0, -1)
--- typescript/beer-song/beer-song.ts
+++ typescript/beer-song/beer-song.ts
@@ -4,5 +4,5 @@ export const verse = (index: number): string =>
     index === 1 ? `1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n` :
     `${index} bottles of beer on the wall, ${index} bottles of beer.\nTake one down and pass it around, ${index - 1} bottle${index - 1 > 1 ? "s" : ""} of beer on the wall.\n`
 
-export const sing = (initialBottlesCount: number = 99, takeDownCount: number = 0): string =>
-    [...Array(initialBottlesCount + 1 - takeDownCount).keys()].map(i => verse(initialBottlesCount - i)).join("\n")
+export const sing = (start: number = 99, end: number = 0): string =>
+    [...Array(start + 1 - end).keys()].map(i => verse(start - i)).join("\n")
