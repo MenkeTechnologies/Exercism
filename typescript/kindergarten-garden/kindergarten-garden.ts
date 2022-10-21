75b6e347f00b881010ee014dcae76d639c4c69ec
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-21 11:08:16 -0400

    kg

--- typescript/kindergarten-garden/kindergarten-garden.ts
+++ typescript/kindergarten-garden/kindergarten-garden.ts
@@ -1,38 +1,39 @@
-
-
 const DEFAULT_STUDENTS: Student[] = [
-  'Alice',
-  'Bob',
-  'Charlie',
-  'David',
-  'Eve',
-  'Fred',
-  'Ginny',
-  'Harriet',
-  'Ileana',
-  'Joseph',
-  'Kincaid',
-  'Larry',
+    'Alice',
+    'Bob',
+    'Charlie',
+    'David',
+    'Eve',
+    'Fred',
+    'Ginny',
+    'Harriet',
+    'Ileana',
+    'Joseph',
+    'Kincaid',
+    'Larry',
 ]
-
 const PLANT_CODES = {
-  G: 'grass',
-  V: 'violets',
-  R: 'radishes',
-  C: 'clover',
+    G: 'grass',
+    V: 'violets',
+    R: 'radishes',
+    C: 'clover',
 } as const
-
 type Student = string
 type Plant = typeof PLANT_CODES[keyof typeof PLANT_CODES]
 type Plants = Plant[]
-type Pots = Plants[]
 
 export class Garden {
-  constructor(diagram: string, students = DEFAULT_STUDENTS) {
-    throw new Error('Remove this statement and implement this function')
-  }
+    diagram: String[]
+    allStudents: Student[]
 
-  public plants(student: Student): Plants {
-    throw new Error('Remove this statement and implement this function')
-  }
+    constructor(diagram: string, students = DEFAULT_STUDENTS) {
+        this.diagram = diagram.split("\n")
+        this.allStudents = students.sort()
+    }
+
+    public plants = (student: Student): Plants => {
+        const position = this.allStudents.findIndex(curStudent => student === curStudent) * 2
+        const studentCodes = this.diagram.map(codes => codes.slice(position, position + 2)).join("").split("")
+        return studentCodes.map(code => PLANT_CODES[code as keyof typeof PLANT_CODES])
+    };
 }
