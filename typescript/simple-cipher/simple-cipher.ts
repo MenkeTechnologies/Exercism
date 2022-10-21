324b56ac3bee3d6db73d3dd3eeeff40f272a2846
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-21 11:12:10 -0400

    cipher

--- typescript/simple-cipher/simple-cipher.ts
+++ typescript/simple-cipher/simple-cipher.ts
@@ -1,9 +1,22 @@
 export class SimpleCipher {
-  encode(/* Parameters go here */) {
-    throw new Error('Remove this statement and implement this function')
-  }
+    public key: string
+    private alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-  decode(/* Parameters go here */) {
-    throw new Error('Remove this statement and implement this function')
-  }
+    constructor(key?: string) {
+        this.key = key ? key : this.generateKey()
+    }
+
+    public encode = (s: string) => this.transpose(s, 1);
+
+    public decode = (s: string) => this.transpose(s, -1);
+
+    private generateKey = (length: number = 100) => new Array(length).fill(null)
+        .map(() => this.alphabet.charAt(Math.floor((Math.random() * this.alphabet.length)))).join('');
+
+    private transpose = (text: string, direction: 1 | -1) => Array.from(text).map((c, i) => {
+        const position = this.alphabet.indexOf(c);
+        const shift = this.alphabet.indexOf(this.key[i % this.key.length]);
+        const newPosition = (position + shift * direction + this.alphabet.length) % this.alphabet.length;
+        return this.alphabet.charAt(newPosition);
+    }).join('');
 }
