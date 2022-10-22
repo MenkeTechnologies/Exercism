ee43854dad81acca19dae1f4fc887eda7cae1ded
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-22 13:34:46 -0400

    diamant

--- typescript/diamond/diamond.ts
+++ typescript/diamond/diamond.ts
@@ -1,3 +1,10 @@
-export function makeDiamond(character: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
-}
+export const makeDiamond = (c: string) => {
+    const A = 'A'.charCodeAt(0), len = c.charCodeAt(0) - A, halfRow = []
+    for (let i = 0; i <= len; ++i) {
+        halfRow.push(' '.repeat(len - i) + String.fromCharCode(A + i) + ' '.repeat(i))
+    }
+    const rows = halfRow.map(line => line + [...line]
+        .reverse().slice(1).join(''))
+    const full = [...rows, ...[...rows].reverse().slice(1)]
+    return full.join('\n') + '\n'
+};
