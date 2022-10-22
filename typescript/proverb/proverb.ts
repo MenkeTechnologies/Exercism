675aee32937fc2d71e7ff59b6586101582f4d7cb
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-22 13:32:34 -0400

    proverb

--- typescript/proverb/proverb.ts
+++ typescript/proverb/proverb.ts
@@ -1,3 +1,7 @@
-export function proverb() {
-  throw new Error('Remove this statement and implement this function')
-}
+export const proverb = (...words: string[]) =>
+    words.length < 2 ? (() => { throw new Error(); })() :
+        words.map((_, i) =>
+                i < words.length - 1
+                ? `For want of a ${words[i]} the ${words[i + 1]} was lost.`
+                : `And all for the want of a ${words[0]}.`)
+            .join('\n')
