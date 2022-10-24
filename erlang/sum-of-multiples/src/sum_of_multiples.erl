5fb6de60ec1a270f6c35239c8aad2fede4f548e1
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-24 11:16:33 -0400

    mult

--- erlang/sum-of-multiples/src/sum_of_multiples.erl
+++ erlang/sum-of-multiples/src/sum_of_multiples.erl
@@ -1,6 +1,16 @@
 -module(sum_of_multiples).
-
 -export([sum/2]).
+-spec sum(list(), integer()) -> integer().
+sum(Factors, Limit) ->
+  aux(Factors, Limit - 1, 0).
+aux(_Factors, 0, Sum) ->
+  Sum;
+aux(Factors, Limit, Sum) ->
+  IsMultiple = lists:any(fun(0) -> false;
+                            (I) -> Limit rem I == 0 end,
+                         Factors),
+  case IsMultiple of
+    true  -> aux(Factors, Limit - 1, Sum + Limit);
+    false -> aux(Factors, Limit - 1, Sum)
+  end.
 
-
-sum(_Factors, _Limit) -> undefined.
