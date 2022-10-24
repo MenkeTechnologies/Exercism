f804f73af9dda61634d35adc7e17d47d8306539c
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-24 11:19:28 -0400

    sh

--- erlang/secret-handshake/src/secret_handshake.erl
+++ erlang/secret-handshake/src/secret_handshake.erl
@@ -1,6 +1,15 @@
 -module(secret_handshake).
-
 -export([commands/1]).
 
-
-commands(_Number) -> undefined.
+commands(0) ->
+    [];
+commands(N) when N band 16 > 0 ->
+    lists:reverse(commands(N - 16));
+commands(N) when N band 1 > 0 ->
+    ["wink" | commands(N - 1)];
+commands(N) when N band 2 > 0 ->
+    ["double blink" | commands(N - 2)];
+commands(N) when N band 4 > 0 ->
+    ["close your eyes" | commands(N - 4)];
+commands(N) when N band 8 > 0 ->
+    ["jump" | commands(N - 8)].
