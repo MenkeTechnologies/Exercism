bd23266619ed85de08ffdfec59e6478e3f924d7c
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-28 22:01:21 -0400

    ll

--- perl5/simple-linked-list/LinkedList.pm
+++ perl5/simple-linked-list/LinkedList.pm
@@ -1,31 +1,47 @@
 package LinkedList;
-
 use strict;
 use warnings;
 
 sub new {
-    my $class = shift;
-
-    return undef;
+    my ( $class, $data, $next ) = @_;
+    bless {
+        data => $data,
+        next => $next,
+    }, $class;
 }
 
 sub from_array {
-    my ( $pkg, $array ) = @_;
-
-    return undef;
+    my ( $self, $values ) = @_;
+    my $prev;
+    $prev = $self->new( $_, $prev ) for reverse @$values;
+    $prev;
 }
 
 sub to_array {
-    my $self = shift;
-
-    return undef;
+    my ($self) = @_; 
+    my @values = ();
+    do {
+        push @values, $self->data();
+    } while ( $self = $self->next() );
+    \@values;
 }
 
-sub reverse : method {
-    my $self = shift;
-
-    return undef;
+sub reverse {
+    my ($self) = @_;
+    my $head;
+    do {
+        $head = ( ref $self )->new( $self->data(), $head );
+    } while ( $self = $self->next() );
+    $head;
 }
 
-1;
+sub data {
+    my ($self) = @_;
+    $self->{data};
+}
 
+sub next {
+    my ($self) = @_;
+    $self->{next};
+}
+1
