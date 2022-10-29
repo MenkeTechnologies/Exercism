8ae379d6035ebc2b9edca34877385bf92344da97
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-28 22:31:05 -0400

    qa

--- nim/queen-attack/queen_attack.nim
+++ nim/queen-attack/queen_attack.nim
@@ -1,9 +1,24 @@
-type Queen* = object
-    row*, col*: int
+import std/[strformat, strutils]
+type
+  Queen* = object
+    row*, col*: range[0..7]
 
-proc initQueen*(x, y: int): Queen =
+func initQueen*(row, col: int): Queen =
+  if not (row in 0..7 and col in 0..7):
+    raise newException(ValueError, &"Row & col must be in 0..7 but {row = } & {col = }")
+  result = Queen(row: row, col: col)
 
-proc canAttack*(w, b: Queen): bool =
+func canAttack*(queen1, queen2: Queen): bool =
+  if queen1 == queen2:
+    raise newException(ValueError, "Queens can't have the same position")
+  let dx = abs(queen1.col - queen2.col)
+  let dy = abs(queen1.row - queen2.row)
+  result = dx == 0 or dy == 0 or dx == dy
 
-proc board*(w, b: Queen): string =
+func board*(whiteQueen, blackQueen: Queen): string =
+  if whiteQueen == blackQueen:
+    raise newException(ValueError, "Queens can't have the same position")
+  result = ("_".repeat(8) & '\n').repeat(8)
+  result[whiteQueen.row * 9 + whiteQueen.col] = 'W'
+  result[blackQueen.row * 9 + blackQueen.col] = 'B'
 
