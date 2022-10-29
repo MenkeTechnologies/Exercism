18b6e9e2beac2893f043321498fe11b9f95a5885
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-29 12:46:11 -0400

    ll

--- groovy/linked-list/src/main/groovy/DoubleLinkedList.groovy
+++ groovy/linked-list/src/main/groovy/DoubleLinkedList.groovy
@@ -1,19 +1,42 @@
+import groovy.transform.ToString
+
 class DoubleLinkedList<T> {
+    @ToString
+    private class Node<T> {
+        T value
+        Node previous
+        Node next
+    }
+    
+    private Node<T> first
+    private Node<T> last
 
     void push(T value) {
-        throw new UnsupportedOperationException('Method implementation is missing')
-    }
-
-    T pop() {
-        throw new UnsupportedOperationException('Method implementation is missing')
-    }
-
-    T shift() {
-        throw new UnsupportedOperationException('Method implementation is missing')
+        def newLast = new Node<T>(value: value, previous: last)
+        if (last) last.next = newLast
+        last = newLast
+        if (!first) first = last
     }
 
     void unshift(T value) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+        def newFirst = new Node<T>(value: value, next: first)
+        if (first) first.previous = newFirst
+        first = newFirst
+        if (!last) last = first
+    }
+
+    T pop() {
+        if (!last) throw new RuntimeException('Cannot pop an empty list')
+        def value = last.value
+        last = last.previous
+        value
+    }
+
+    T shift() {
+        if (!first) throw new RuntimeException('Cannot shift an empty list')
+        def value = first.value
+        first = first.next
+        value
     }
 
 }
