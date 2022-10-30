9f86a5771ebbf2df3a36a63681cf035c031a3f27
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-30 10:03:25 -0400

    rs

--- lua/robot-simulator/robot-simulator.lua
+++ lua/robot-simulator/robot-simulator.lua
@@ -1,3 +1,26 @@
-return function(config)
+local directions = {
+  north = { L = 'west', R = 'east', y = 1 },
+  east = { L = 'north', R = 'south', x = 1 },
+  south = { L = 'east', R = 'west', y = -1 },
+  west = { L = 'south', R = 'north', x = -1 }
+  }
 
+return function(p)
+  return {
+    x = p.x,
+    y = p.y,
+    heading = p.heading,
+    move = function (self, command)
+        for i = 1, #command do
+            local c = command:sub(i,i)
+            if c == 'R' or c == 'L' then self.heading = directions[self.heading][c]
+            elseif c == 'A' then
+                self.x = self.x + (directions[self.heading].x or 0)
+                self.y = self.y + (directions[self.heading].y or 0)
+            else
+                assert(false, 'unknown command')
+            end
+        end
+    end
+  }
 end
