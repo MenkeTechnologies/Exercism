f28020347dbd4301612ebb59ff3334dd76485b57
MenkeTechnologies <linux.dev25@gmail.com>
2022-10-31 10:36:54 -0400

    clock

--- c/clock/clock.c
+++ c/clock/clock.c
@@ -1,1 +1,33 @@
 #include "clock.h"
+#include <stdio.h>
+#include <string.h>
+
+clock_t clock_create(int hour, int minute) {
+    hour += minute / 60;
+    minute %= 60;
+    if (minute < 0) {
+        minute += 60;
+        hour -= 1;
+    }
+    hour %= 24;
+    if (hour < 0) {
+        hour += 24;
+    }
+    clock_t clock;
+    sprintf(clock.text, "%02d:%02d", hour, minute);
+    return clock;
+}
+
+clock_t clock_add(clock_t clock, int minute_add) {
+    int hour, minute;
+    sscanf(clock.text, "%d:%d", &hour, &minute);
+    return clock_create(hour, minute + minute_add);
+}
+
+clock_t clock_subtract(clock_t clock, int minute_subtract) {
+    return clock_add(clock, -minute_subtract);
+}
+
+bool clock_is_equal(clock_t a, clock_t b) {
+    return strcmp(a.text, b.text) == 0;
+}
--- c/clock/clock.h
+++ c/clock/clock.h
@@ -1,17 +1,16 @@
-#ifndef CLOCK_H
-#define CLOCK_H
+#pragma once
 
 #include <stdbool.h>
 
-#define MAX_STR_LEN (5 + 1)   // "##:##\0"
-
+#define MAX_STR_LEN (5 + 1)
 typedef struct {
-   char text[MAX_STR_LEN];
+    char text[MAX_STR_LEN];
 } clock_t;
 
 clock_t clock_create(int hour, int minute);
-clock_t clock_add(clock_t clock, int minute_add);
-clock_t clock_subtract(clock_t clock, int minute_subtract);
-bool clock_is_equal(clock_t a, clock_t b);
 
-#endif
+clock_t clock_add(clock_t clock, int minute_add);
+
+clock_t clock_subtract(clock_t clock, int minute_subtract);
+
+bool clock_is_equal(clock_t a, clock_t b);
