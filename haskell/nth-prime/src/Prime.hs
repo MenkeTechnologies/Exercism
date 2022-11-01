c6b0a59ce985e292951946658299e9039f4839b7
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-01 08:43:54 -0400

    nth

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,4 +1,12 @@
 module Prime (nth) where
 
 nth :: Int -> Maybe Integer
-nth n = error "You need to implement this function."
+nth n
+    | n > 0 = Just $ primes !! (n - 1)
+    | otherwise = Nothing
+
+primes :: [Integer]
+primes = aux [2..]
+    where
+        aux [] = []
+        aux (p:ps) = p : aux [x | x <- ps, mod x p /= 0]
