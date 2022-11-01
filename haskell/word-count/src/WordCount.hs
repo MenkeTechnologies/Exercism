64841e809ef1d796b05d8e1cbe96d0f2734d03a8
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-01 08:37:16 -0400

    wc

--- haskell/word-count/src/WordCount.hs
+++ haskell/word-count/src/WordCount.hs
@@ -1,4 +1,16 @@
 module WordCount (wordCount) where
 
+import Data.Bool (bool)
+import Data.Char (isAlphaNum, toLower)
+import Data.List (group, sort)
+import Data.Monoid (All(..), Any(..), getAll, getAny)
+
 wordCount :: String -> [(String, Int)]
-wordCount xs = error "You need to implement this function."
+wordCount = map ((,) <$> head <*> length) . group . sort . fmap removeQuote . words . fmap normalize
+
+normalize :: Char -> Char
+normalize = bool <$> const ' ' <*> toLower <*> getAny . foldMap (Any .) [('\'' ==), isAlphaNum]
+
+removeQuote :: String -> String
+removeQuote = bool <$> id <*> init . tail <*> getAll . foldMap (All .) [('\'' ==) . head, ('\'' ==) . last]
+
