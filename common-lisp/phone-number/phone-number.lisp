d9d92f7f964b6972b9f9908af88b734deede9db5
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-02 19:28:49 -0400

    pn

--- common-lisp/phone-number/phone-number.lisp
+++ common-lisp/phone-number/phone-number.lisp
@@ -1,10 +1,19 @@
 (defpackage :phone-number
   (:use :cl)
   (:export :clean))
-
 (in-package :phone-number)
 
-(defun clean (phrase)
-  "Converts a PHRASE string into a string of digits.
-Will evaluate to \"0000000000\" in case of an invalid input."
-  )
+(defun clean (s)
+  (let* ((digits (remove-if-not #'digit-char-p s))
+         (len (length digits)))
+    (when (or (< len 10) (> len 11))
+      (return-from clean "0000000000"))
+    (when (= len 11)
+      (if (char/= (aref digits 0) #\1)
+        (return-from clean "0000000000"))
+      (setf digits (subseq digits 1)))
+    (when (or
+           (member (aref digits 0) '(#\0 #\1))
+           (member (aref digits 3) '(#\0 #\1)))
+      (return-from clean "0000000000"))
+    digits))
