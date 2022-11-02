60965a3c68d22338611e8c2768a66a39d5d0bb5d
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-02 19:32:27 -0400

    bs

--- common-lisp/beer-song/beer-song.lisp
+++ common-lisp/beer-song/beer-song.lisp
@@ -3,10 +3,15 @@
   (:export :verse :sing))
 
 (in-package :beer-song)
+
 (defun verse (n)
-  "Returns a string verse for a given number."
-  )
+  (concatenate 'string
+               (format nil "~[No more~:;~a~] bottle~:p of beer on the wall, ~[no more~:;~a~] bottle~:p of beer.~%" n n n n)
+               (cond ((> n 0)
+                      (format nil "Take ~[~;it~:;one~] down and pass it around, ~[no more~:;~a~] bottle~:p of beer on the wall.~%" n (1- n) (1- n)))
+                     ((= n 0)
+                      (format nil "Go to the store and buy some more, 99 bottles of beer on the wall.~%")))))
 
 (defun sing (start &optional (end 0))
-  "Returns a string of verses for a given range of numbers."
-  )
+  (format nil "~{~a~%~}"
+          (loop for x from start downto end collect (verse x))))
