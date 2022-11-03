13b18d15eff6cd8a1b92f6a476e585e15a8038bd
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-03 19:23:34 -0400

    pangram

---
+++ fortran/pangram/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wizard/Exercism/fortran/pangram
+# Build directory: /Users/wizard/Exercism/fortran/pangram
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(pangram_test "pangram")
+set_tests_properties(pangram_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/wizard/Exercism/fortran/pangram/CMakeLists.txt;67;add_test;/Users/wizard/Exercism/fortran/pangram/CMakeLists.txt;0;")
+subdirs("testlib")
--- fortran/pangram/pangram.f90
+++ fortran/pangram/pangram.f90
@@ -1,10 +1,18 @@
 module pangram
   implicit none
+  character(len=*), parameter :: &
+    ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 contains
-
-  logical function is_pangram(sentance)
-    character(*) :: sentance
-
-   end function is_pangram
-
+  pure logical function is_pangram(s)
+    character(*), intent(in) :: s
+    logical :: has_letter(26)
+    integer :: i, i_letter
+    has_letter = .false.
+    do i = 1, len(s)
+      i_letter = index(ascii_letters, s(i:i))
+      if (i_letter > 26) i_letter = i_letter - 26
+      if (i_letter > 0) has_letter(i_letter) = .true.
+    end do
+    is_pangram = all(has_letter)
+  end function is_pangram
 end module pangram
