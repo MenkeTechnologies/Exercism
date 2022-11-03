f98f8424cf648de321064b5db6fcf1ef1f3d8b14
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-03 19:54:52 -0400

    rd

--- common-lisp/raindrops/raindrops.lisp
+++ common-lisp/raindrops/raindrops.lisp
@@ -5,16 +5,12 @@
 (in-package :raindrops)
 
 (defparameter raindrops-factors
-    '((3 . "Pling")
+  '((3 . "Pling")
     (5 . "Plang")
     (7 . "Plong")))
 
 (defun convert (n)
-  "Converts a number to a string of raindrop sounds."
   (let ((drops (loop for (factor . drop) in raindrops-factors
-                when (zerop (mod n factor)) collect drop)))
-   (if drops (format nil "~{~a~}" drops)
-    (write-to-string n)
-    )
-   )
-  )
+                     when (zerop (mod n factor)) collect drop)))
+    (if drops (format nil "~{~a~}" drops)
+      (write-to-string n))))
