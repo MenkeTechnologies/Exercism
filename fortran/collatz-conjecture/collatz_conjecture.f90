f4180d3ae56ec780517ca989ab5ed060a76a8b1e
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-03 19:20:45 -0400

    coll

--- fortran/collatz-conjecture/collatz_conjecture.f90
+++ fortran/collatz-conjecture/collatz_conjecture.f90
@@ -1,11 +1,21 @@
-
 module collatz_conjecture
   implicit none
 contains
-
-  integer function steps(i)
-    integer :: i
-    steps = -1
+  integer function steps(limit)
+  integer, intent(in) :: limit
+  integer :: n
+  n = limit
+  steps = -1
+  if (n.lt.1) return
+  steps = 0
+  do while (n.ne.1)
+    if (mod(n,2).eq.0) then
+      n = n / 2
+    else
+      n = n * 3 + 1
+    end if
+    steps = steps + 1
+  end do
   end function
-
 end module
+
