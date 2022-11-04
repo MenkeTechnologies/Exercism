aeece3b17b0b391906ba6f4715a3d401ef50d71c
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-04 11:41:27 -0400

    arm

---
+++ fortran/armstrong-numbers/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wizard/Exercism/fortran/armstrong-numbers
+# Build directory: /Users/wizard/Exercism/fortran/armstrong-numbers
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(armstrong-numbers_test "armstrong-numbers")
+set_tests_properties(armstrong-numbers_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/wizard/Exercism/fortran/armstrong-numbers/CMakeLists.txt;67;add_test;/Users/wizard/Exercism/fortran/armstrong-numbers/CMakeLists.txt;0;")
+subdirs("testlib")
--- fortran/armstrong-numbers/armstrong_numbers.f90
+++ fortran/armstrong-numbers/armstrong_numbers.f90
@@ -1,13 +1,23 @@
-
 module armstrong_numbers
   implicit none
 contains
-
-  logical function isArmstrongNumber(i)
-    integer, intent(in) :: i
-    
+  logical function isArmstrongNumber(num)
+    integer, intent(in) :: num
+    integer :: sum, i, digit, len
+    character(10) :: digits
     isArmstrongNumber = .false.
-
+    write(digits,"(i10)") num
+    digits = trim(adjustl(digits))
+    len = len_trim(digits)
+    sum = 0
+    do i = 1, len
+      read(digits(i:i), *) digit
+      sum = sum + digit ** len
+    end do
+    if (sum == num) then
+      isArmstrongNumber = .true.
+    end if
+    return
   end function
-
 end module
+
