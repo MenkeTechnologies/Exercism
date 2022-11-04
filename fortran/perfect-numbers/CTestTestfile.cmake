04b76c2b8e0d6379b63070f252ce81c57953ed58
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-04 11:47:46 -0400

    pn

---
+++ fortran/perfect-numbers/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wizard/Exercism/fortran/perfect-numbers
+# Build directory: /Users/wizard/Exercism/fortran/perfect-numbers
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(perfect-numbers_test "perfect-numbers")
+set_tests_properties(perfect-numbers_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/wizard/Exercism/fortran/perfect-numbers/CMakeLists.txt;67;add_test;/Users/wizard/Exercism/fortran/perfect-numbers/CMakeLists.txt;0;")
+subdirs("testlib")
--- fortran/perfect-numbers/perfect_numbers.f90
+++ fortran/perfect-numbers/perfect_numbers.f90
@@ -1,12 +1,30 @@
-
 module perfect_numbers
   implicit none
-
 contains
-
   character(len=9) function classify(num)
     integer, intent(in) :: num
-    classify = "ERROR"
-  end function
+    integer, allocatable :: lst(:)
+    integer :: i, mySum
 
+    if (num <= 0) then
+      classify = "ERROR"
+    else
+      allocate(lst(num))
+      lst(:) = 0
+      DO i = 1, num - 1
+        if(mod(num, i) == 0) then
+          lst(i) = i
+        end if
+      end do
+      mySum = sum(lst)
+      if (mySum == num) then
+        classify = "perfect"
+      else if (mySum > num) then
+        classify = "abundant"
+      else if (mySum < num) then
+        classify = "deficient"
+      end if
+    end if
+  end function
 end module
+
