9b1a63a5e01e1f748644decbd777e61aec3b1b7a
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-04 11:31:50 -0400

    leap

---
+++ fortran/leap/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wizard/Exercism/fortran/leap
+# Build directory: /Users/wizard/Exercism/fortran/leap
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(leap_test "leap")
+set_tests_properties(leap_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/wizard/Exercism/fortran/leap/CMakeLists.txt;67;add_test;/Users/wizard/Exercism/fortran/leap/CMakeLists.txt;0;")
+subdirs("testlib")
