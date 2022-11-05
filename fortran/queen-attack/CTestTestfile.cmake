81633f1ca652fc0fab080fde1d506cdeb6465bda
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-05 09:53:12 -0400

    qa

---
+++ fortran/queen-attack/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wizard/Exercism/fortran/queen-attack
+# Build directory: /Users/wizard/Exercism/fortran/queen-attack
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(queen-attack_test "queen-attack")
+set_tests_properties(queen-attack_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/wizard/Exercism/fortran/queen-attack/CMakeLists.txt;67;add_test;/Users/wizard/Exercism/fortran/queen-attack/CMakeLists.txt;0;")
+subdirs("testlib")
--- fortran/queen-attack/queen_attack.f90
+++ fortran/queen-attack/queen_attack.f90
@@ -1,16 +1,18 @@
-
 module queen_attack
-  implicit none
+    implicit none
 contains
 
-  logical function isValid(pos)
-    integer, dimension(2) :: pos
-    isValid = .false.
-  end function
+    logical function isValid(pos)
+        integer, dimension(2), intent(in) :: pos
+        isValid = pos(1) > 0 .and. pos(1) < 8 .and. pos(2) > 0 .and. pos(2) < 8
+    end function
 
-  logical function canAttack(white_pos, black_pos)
-    integer, dimension(2) :: white_pos, black_pos
-    canAttack = .false.
-  end function
-
+    logical function canAttack(white_pos, black_pos)
+        integer, dimension(2), intent(in) :: white_pos, black_pos
+        integer :: dx, dy
+        dx = abs(white_pos(1) - black_pos(1))
+        dy = abs(white_pos(2) - black_pos(2))
+        canAttack = dx == 0 .or. dy == 0 .or. dx == dy
+    end function
 end module
+
