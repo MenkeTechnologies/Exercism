949daf00201ee72442e196aff2a7847e7fea367d
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-05 09:56:06 -0400

    bob

---
+++ fortran/bob/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wizard/Exercism/fortran/bob
+# Build directory: /Users/wizard/Exercism/fortran/bob
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(bob_test "bob")
+set_tests_properties(bob_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/wizard/Exercism/fortran/bob/CMakeLists.txt;67;add_test;/Users/wizard/Exercism/fortran/bob/CMakeLists.txt;0;")
+subdirs("testlib")
--- fortran/bob/bob.f90
+++ fortran/bob/bob.f90
@@ -1,11 +1,36 @@
 module bob
-  implicit none
+    implicit none
 contains
-
-  function hey(statement)
-    character(100) :: hey
-    character(len=*), intent(in) :: statement
-
-  end function hey
-
+    function hey(statement)
+        character(100) :: hey
+        character(len=*), intent(in) :: statement
+        logical :: is_empty, is_question, has_lowcase, has_upcase, is_yelling
+        integer :: i
+        character :: c
+        is_empty = len(trim(statement)) == 0
+        i = index(statement, '?')
+        is_question = i /= 0 .and. i == len(trim(statement))
+        has_lowcase = .false.
+        has_upcase = .false.
+        do i=1,len(statement)
+        c = statement(i:i)
+        has_lowcase = has_lowcase .or. ('a' <= c .and. c <= 'z')
+        has_upcase = has_upcase .or. ('A' <= c .and. c <= 'Z')
+        end do
+        is_yelling = has_upcase .and. .not. has_lowcase
+        if (is_empty) then
+            hey = 'Fine. Be that way!'
+        else if (is_yelling) then
+            if (is_question) then
+                hey = "Calm down, I know what I'm doing!"
+            else
+                hey = 'Whoa, chill out!'
+            end if
+        else if (is_question) then
+            hey = 'Sure.'
+        else
+            hey = 'Whatever.'
+        end if
+    end function hey
 end module bob
+
