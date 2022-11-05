f78d93d5dee8fcaa770ddb35a6682470c8f034b9
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-05 09:48:54 -0400

    hs

---
+++ fortran/high-scores/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wizard/Exercism/fortran/high-scores
+# Build directory: /Users/wizard/Exercism/fortran/high-scores
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(high-scores_test "high-scores")
+set_tests_properties(high-scores_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/wizard/Exercism/fortran/high-scores/CMakeLists.txt;67;add_test;/Users/wizard/Exercism/fortran/high-scores/CMakeLists.txt;0;")
+subdirs("testlib")
--- fortran/high-scores/high_scores.f90
+++ fortran/high-scores/high_scores.f90
@@ -1,11 +1,47 @@
-
 module high_scores
-  implicit none
+    implicit none
 contains
 
-function scores(score_list)
-  integer, dimension(:) :: score_list
+    function scores(score_list)
+        integer, dimension(:) :: score_list
+        integer, dimension(lbound(score_list,dim=1):ubound(score_list,dim=1)) :: scores
+        scores = score_list
+    end function
 
-end function
-  
+    function latest(score_list)
+        integer, dimension(:) :: score_list
+        integer :: latest
+        latest = score_list(size(score_list))
+    end function
+
+    function personalBest(score_list)
+        integer, dimension(:) :: score_list
+        integer :: personalBest
+        personalBest = maxval(score_list)
+    end function
+
+    function personalTopThree(score_list)
+        integer, dimension(:) :: score_list
+        logical, dimension(99) :: not_used
+        integer, dimension(3) :: personalTopThree
+        integer :: i, j, max, maxIdx
+        do i=1,size(score_list)
+        not_used(i) = .true.
+        end do
+        do i=1,3
+        max = 0
+        maxIdx = -1
+        do j=1,size(score_list)
+        if (not_used(j) .and. score_list(j) >= max) then
+            max = score_list(j)
+            maxIdx = j
+        end if
+        end do
+        personalTopThree(i:i) = max
+        if (maxIdx > -1) then
+            not_used(maxIdx) = .false.
+        end if
+        end do
+    end function
+
 end module
