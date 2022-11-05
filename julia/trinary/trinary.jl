2c713aa6ede36abf3a6555965b224fc05503ef75
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-05 10:02:17 -0400

    tri

--- julia/trinary/trinary.jl
+++ julia/trinary/trinary.jl
@@ -1,3 +1,11 @@
-function trinary_to_decimal(str)
-
+function trinary_to_decimal(s::AbstractString)
+    result = 0
+    for c in s
+        if c in '0':'2'
+            result = result * 3 + c - '0'
+        else
+            return 0
+        end
+    end
+    result
 end
