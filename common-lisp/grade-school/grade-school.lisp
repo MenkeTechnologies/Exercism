bb1723efd42c65c3925a87601ebfdc464d6163f6
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-06 15:54:18 -0500

    gs

--- common-lisp/grade-school/grade-school.lisp
+++ common-lisp/grade-school/grade-school.lisp
@@ -1,5 +1,21 @@
-(defpackage :grade-school
-  (:use :cl)
-  (:export :make-school :add :roster :grade))
+(in-package #:cl-user)
 
-(in-package :grade-school)
+(defpackage #:grade-school
+  (:use #:common-lisp)
+  (:export #:make-school #:roster #:add #:grade))
+
+(in-package #:grade-school)
+
+(defun make-school () (make-hash-table))
+
+(defun roster (school)
+  (let ((grades-and-names '()))
+    (maphash #'(lambda (k v) (push (cons k (sort v #'string<)) grades-and-names)) school)
+    (reduce #'append (mapcar #'cdr (sort grades-and-names #'< :key #'car)))))
+
+(defun add (school student grade)
+  (unless (member student (roster school) :test #'string=)
+    (push student (gethash grade school))))
+
+(defun grade (school grade)
+  (sort (gethash grade school) #'string<))
