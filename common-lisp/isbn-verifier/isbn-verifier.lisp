d2c1e68bcccfac2010eee2bb74b2f9cc3e75df3e
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-06 17:50:03 -0500

    isbn

--- common-lisp/isbn-verifier/isbn-verifier.lisp
+++ common-lisp/isbn-verifier/isbn-verifier.lisp
@@ -4,4 +4,18 @@
 
 (in-package :isbn-verifier)
 
-(defun validp (isbn))
+(defun validp (isbn)
+  (let ((digits
+          (loop for c across isbn
+                for d = (cond ((digit-char-p c) (- (char-code c) (char-code #\0)))
+                              ((and (= (length digits) 9) (char-equal c #\X)) 10)
+                              ((char= c #\-) nil)
+                              (t (return-from validp)))
+                if d collect it into digits
+                finally (return digits))))
+    (when (= (length digits) 10)
+      (let ((sum (loop for d in digits
+                       for i downfrom 10
+                       sum (* d i))))
+        (zerop (rem sum 11))))))
+
