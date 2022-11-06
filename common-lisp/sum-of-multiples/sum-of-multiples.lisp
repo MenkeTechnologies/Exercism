20133db426792c4ae9c0e08d68b25884076b0064
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-06 15:21:20 -0500

    sm

--- common-lisp/sum-of-multiples/sum-of-multiples.lisp
+++ common-lisp/sum-of-multiples/sum-of-multiples.lisp
@@ -4,4 +4,8 @@
 
 (in-package :sum-of-multiples)
 
-(defun sum (factors limit))
+(defun sum (factors limit)
+  (reduce #'+ (remove-if-not
+    (lambda (n) (some (lambda (f) (zerop (mod n f))) (remove-if #'zerop factors)))
+      (loop for i from 1 to (1- limit) collect i))))
+
