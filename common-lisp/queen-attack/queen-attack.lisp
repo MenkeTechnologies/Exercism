405c993f0a4839fb36b3b7b7e75ea090281c1a8c
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-06 17:57:32 -0500

    qa

--- common-lisp/queen-attack/queen-attack.lisp
+++ common-lisp/queen-attack/queen-attack.lisp
@@ -5,6 +5,14 @@
 
 (in-package :queen-attack)
 
-(defun valid-position-p (coordinates))
+(defun valid-position-p (coordinates)
+  (let ((x (car coordinates)) (y (cdr coordinates)))
+    (and (and (>= x 0) (< x 8))
+         (and (>= y 0) (< y 8)))))
 
-(defun attackp (white-queen black-queen))
+(defun attackp (white-queen black-queen)
+  (let ((dx (abs (- (car white-queen) (car black-queen))))
+        (dy (abs (- (cdr white-queen) (cdr black-queen)))))
+    (and (and (valid-position-p white-queen)
+              (valid-position-p black-queen))
+         (or (zerop dx) (zerop dy) (= dx dy)))))
