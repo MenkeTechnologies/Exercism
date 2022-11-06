2e547f7c98310ce258bc82340074b088ab93422f
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-06 18:03:55 -0500

    nth

--- common-lisp/nth-prime/nth-prime.lisp
+++ common-lisp/nth-prime/nth-prime.lisp
@@ -4,4 +4,12 @@
 
 (in-package :nth-prime)
 
-(defun find-prime (number))
+(defun find-prime (nth)
+  (unless (zerop nth)
+    (loop :for n :from 2
+          :when (prime? n) :sum 1 :into cnt
+          :when (= cnt nth) :return n)))
+
+(defun prime? (nth)
+  (loop :for n :from 2 :upto (sqrt nth)
+        :never (zerop (rem nth n))))
