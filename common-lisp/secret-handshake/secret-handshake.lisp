0184d9385ab69f63666787cdbfe5b5384316878f
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-07 09:32:58 -0500

    sh

--- common-lisp/secret-handshake/secret-handshake.lisp
+++ common-lisp/secret-handshake/secret-handshake.lisp
@@ -4,4 +4,15 @@
 
 (in-package :secret-handshake)
 
-(defun commands (number))
+(defvar *commands* '((1 . "wink")
+                     (2 . "double blink")
+                     (4 . "close your eyes")
+                     (8 . "jump")))
+
+(defun commands (number)
+  (let ((cmd (loop :for pair :in *commands*
+                   :when (= (car pair) (boole boole-and number (car pair)))
+                   :collect (cdr pair))))
+    (if (= (boole boole-and number 16) 16)
+      (reverse cmd)
+      cmd)))
