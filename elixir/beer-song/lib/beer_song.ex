152e010b0af48b6ca2eb26ffe03193927ac5bca3
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-07 09:24:24 -0500

    bs

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -1,17 +1,27 @@
 defmodule BeerSong do
-  @doc """
-  Get a single verse of the beer song
-  """
-  @spec verse(integer) :: String.t()
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
   def verse(number) do
-    # Your implementation here...
+    """
+    #{number} #{plural("bottle", number)} of beer on the wall, #{number} #{plural("bottle", number)} of beer.
+    #{do_verse(number)}
+    """
+  end
+  defp do_verse(1) do
+    "Take it down and pass it around, no more #{plural("bottle", 0)} of beer on the wall."
+  end
+  defp do_verse(number) do
+    "Take one down and pass it around, #{number-1} #{plural("bottle", number-1)} of beer on the wall."
   end
 
-  @doc """
-  Get the entire beer song for a given range of numbers of bottles.
-  """
-  @spec lyrics(Range.t()) :: String.t()
-  def lyrics(range) do
-    # Your implementation here...
+  defp plural(word, 1), do: word
+  defp plural(word, _number), do: "#{word}s"
+
+  def lyrics(range \\ 99..0) do
+    range |> Enum.map(&verse/1) |> Enum.join("\n")
   end
 end
