2324f6a6a35a2455aa90c40f310e61dc107b40ac
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-08 09:44:21 -0500

    wc

--- scheme/word-count/word-count.scm
+++ scheme/word-count/word-count.scm
@@ -1,5 +1,14 @@
-(import (rnrs))
+(import (rnrs)
+        (srfi srfi-1))
 
-(define (word-count sentence)
-  'implement-me!)
-
+(define (word-count s)
+  (let* ((words (string-tokenize s (char-set-adjoin char-set:letter+digit #\')))
+         (trimmed (map (lambda (word)
+                       (string-downcase
+                         (string-trim-both word (char-set-union char-set:punctuation))))
+                     words))
+         (not-null (filter (compose not string-null?) trimmed)))
+    (fold (lambda (word acc)
+            (assoc-set! acc word (+ 1 (or (assoc-ref acc word) 0))))
+          '()
+          not-null)))
