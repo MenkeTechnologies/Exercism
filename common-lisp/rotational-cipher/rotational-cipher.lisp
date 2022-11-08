4b6a5712cec69ff5de7714ad679ec4ee2a2cdd84
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-08 09:34:49 -0500

    rot

--- common-lisp/rotational-cipher/rotational-cipher.lisp
+++ common-lisp/rotational-cipher/rotational-cipher.lisp
@@ -4,4 +4,19 @@
 
 (in-package :rotational-cipher)
 
-(defun rotate (text key))
+(defconstant *max-s-lower* (char-code #\a))
+(defconstant *max-l-lower* (char-code #\A))
+
+(defun rotate-char (c key)
+  (let ((code (char-code c)))
+    (if (upper-case-p c)
+      (code-char (+ *max-l-lower* (mod (+ key (- code *max-l-lower*)) 26)))
+      (code-char (+ *max-s-lower* (mod (+ key (- code *max-s-lower*)) 26))))))
+
+(defun rotate (s key)
+  (coerce
+    (loop for c across s
+          collect (if (alpha-char-p c)
+                    (rotate-char c key)
+                    c))
+    'string))
