76d21742ace9b4e402b3d8266df8bacb0bcfda31
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-08 08:48:04 -0500

    flatten

--- common-lisp/flatten-array/flatten-array.lisp
+++ common-lisp/flatten-array/flatten-array.lisp
@@ -1,7 +1,9 @@
 (defpackage :flatten-array
   (:use :cl)
   (:export :flatten))
-
 (in-package :flatten-array)
 
-(defun flatten (nested))
+(defun flatten (nested)
+  (cond ((null nested) nil)
+        ((atom nested) (list nested))
+        (t (mapcan #'flatten nested))))
