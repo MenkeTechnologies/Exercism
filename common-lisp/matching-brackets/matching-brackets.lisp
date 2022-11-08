3d05c6944b82cff4e536cc7f85ce29c67fcc4a8f
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-08 09:39:07 -0500

    mb

--- common-lisp/matching-brackets/matching-brackets.lisp
+++ common-lisp/matching-brackets/matching-brackets.lisp
@@ -4,4 +4,20 @@
 
 (in-package :matching-brackets)
 
-(defun pairedp (value))
+(defun opening (char)
+  (case char
+    (#\{ #\})
+    (#\[ #\])
+    (#\( #\))))
+
+(defun pairedp (value)
+  (let ((stk '())
+        (closing '(#\} #\] #\)))
+        (valid T))
+       (loop :for i :from 0 :to (- (length value) 1)
+             :do (if (opening (aref value i))
+                     (push (aref value i) stk)
+                     (when (member (aref value i) closing)
+                       (when (or (= (length stk) 0) (not (equal (aref value i) (opening (pop stk)))))
+                         (setq valid NIL)))))
+       (and valid (= (length stk) 0))))
