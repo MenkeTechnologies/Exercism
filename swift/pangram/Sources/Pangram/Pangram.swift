ba7b81e775669d45ebad79bee6636ff032367ea8
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-10 21:21:16 -0500

    pan

--- swift/pangram/Sources/Pangram/Pangram.swift
+++ swift/pangram/Sources/Pangram/Pangram.swift
@@ -1,1 +1,6 @@
-//Solution goes in Sources
+struct Pangram {
+    static func isPangram(_ s: String) -> Bool {
+        "abcdefghijklmopqrstuvwxyz".allSatisfy({ s.lowercased().contains($0) })
+    }
+}
+
