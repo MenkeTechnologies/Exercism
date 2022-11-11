55525be12a5eec359abcc304d50b18497c53e78a
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-10 21:25:01 -0500

    sh

--- swift/secret-handshake/Sources/SecretHandshake/SecretHandshake.swift
+++ swift/secret-handshake/Sources/SecretHandshake/SecretHandshake.swift
@@ -1,1 +1,14 @@
-//Solution goes in Sources
+let dict = [1:"wink", 2:"double blink", 4:"close your eyes", 8:"jump"]
+
+class SecretHandshake {
+    var commands:[String] = []
+        init(_ mask:Int) {
+            let res = dict.sorted(by:{ $0.key < $1.key }).filter{ mask & $0.key > 0 }.map{$0.value}
+            if mask & 16 > 0 {
+                commands = res.reversed()
+            } else {
+                commands = res
+            }
+        }
+}
+
