5e82e33de531d8e07dab9a35c257392160176aac
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-13 16:49:40 -0500

    kg

--- java/kindergarten-garden/src/main/java/KindergartenGarden.java
+++ java/kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -1,13 +1,32 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class KindergartenGarden {
+    private String garden;
+    private List<String> students = List.of("Alice",
+            "Bob",
+            "Charlie",
+            "David",
+            "Eve",
+            "Fred",
+            "Ginny",
+            "Harriet",
+            "Ileana",
+            "Joseph",
+            "Kincaid",
+            "Larry");
 
     KindergartenGarden(String garden) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.garden = garden;
     }
 
     List<Plant> getPlantsOfStudent(String student) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] rows = garden.split("\n");
+        int index = students.indexOf(student) * 2;
+        return Arrays.stream(rows).flatMap(row ->
+                Stream.of(Plant.getPlant(row.charAt(index)), Plant.getPlant(row.charAt(index + 1)))
+        ).collect(Collectors.toList());
     }
-
 }
