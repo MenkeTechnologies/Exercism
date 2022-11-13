fa5055d28a9c87d8b9091cf9766c28d3c9892782
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-13 13:01:37 -0500

    atb

--- swift/atbash-cipher/Sources/AtbashCipher/AtbashCipher.swift
+++ swift/atbash-cipher/Sources/AtbashCipher/AtbashCipher.swift
@@ -1,1 +1,16 @@
-//Solution goes in Sources
+func encode(_ plaintext: String) -> String {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz",
+            sBox = Dictionary(uniqueKeysWithValues: zip(alphabet, alphabet.reversed()))
+    let ct = plaintext.lowercased().lazy.filter {
+                $0.isLetter || $0.isNumber
+            }
+            .map {
+                sBox[$0] ?? $0
+            }
+    var result = [String](), ctext = ct[...]
+    while !ctext.isEmpty {
+        result.append(String(ctext.prefix(5)))
+        ctext = ctext.dropFirst(5)
+    }
+    return result.joined(separator: " ")
+}
