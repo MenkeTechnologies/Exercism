26f5f84fc12d02bd9c16bf60826488a9907ad185
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-16 13:20:08 -0500

    rot

--- scheme/rotational-cipher/rotational-cipher.scm
+++ scheme/rotational-cipher/rotational-cipher.scm
@@ -1,5 +1,21 @@
 (import (rnrs))
 
-(define (rotate phrase dx)
-  'implement-me!)
+(define (rotate s amt)
+  (string-map (lambda (c)
+                (cond
+                 ((char<=? #\a c #\z)
+                  (integer->char (+ (char->integer #\a)
+                                    (modulo (+ (- (char->integer c)
+                                                  (char->integer #\a))
+                                               amt)
+                                            26))))
+                 ((char<=? #\A c #\Z)
+                  (integer->char (+ (char->integer #\A)
+                                    (modulo (+ (- (char->integer c)
+                                                  (char->integer #\A))
+                                               amt)
+                                            26))))
+                 (#t
+                  c)))
+              s))
 
