d46466cb0b07523b10ed0c77df33c178a8cd8908
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-16 10:41:14 -0500

    trinary

--- scheme/trinary/trinary.scm
+++ scheme/trinary/trinary.scm
@@ -1,4 +1,14 @@
 (import (rnrs))
 
+(define (ascii->integer c)
+  (- (char->integer c) (char->integer #\0)))
+
 (define (to-decimal s)
-  'implement-me!)
+  (let loop ((letters (string->list s))
+             (acc 0))
+    (cond
+     ((null? letters) acc)
+     ((char<? (car letters) #\0) 0)
+     ((char>? (car letters) #\2) 0)
+     (else
+      (loop (cdr letters) (+ (* acc 3) (ascii->integer (car letters))))))))
