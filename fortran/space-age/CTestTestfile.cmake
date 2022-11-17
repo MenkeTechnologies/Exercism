7d4566ac460a27647fef20d47750c6c227defe9c
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-17 13:06:34 -0500

    sa

---
+++ fortran/space-age/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wizard/Exercism/fortran/space-age
+# Build directory: /Users/wizard/Exercism/fortran/space-age
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(space-age_test "space-age")
+set_tests_properties(space-age_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/wizard/Exercism/fortran/space-age/CMakeLists.txt;67;add_test;/Users/wizard/Exercism/fortran/space-age/CMakeLists.txt;0;")
+subdirs("testlib")
--- fortran/space-age/space_age.f90
+++ fortran/space-age/space_age.f90
@@ -1,14 +1,28 @@
-
 module space_age
   implicit none
 contains
-
-  double precision function age_in_years(planet, seconds)
+  pure double precision function age_in_years(planet, seconds)
     character(len=*), intent(in) :: planet
     double precision, intent(in) :: seconds
-    
-    age_in_years = 0.d0
-    
+    double precision :: period
+    select case (planet)
+      case ("Mercury")
+        period = 0.2408467d0
+      case ("Venus")
+        period = 0.61519726d0
+      case ("Earth")
+        period = 1.0
+      case ("Mars")
+        period = 1.8808158d0
+      case ("Jupiter")
+        period = 11.862615d0
+      case ("Saturn")
+        period = 29.447498d0
+      case ("Uranus")
+        period = 84.016846d0
+      case ("Neptune")
+        period = 164.79132d0
+      end select
+    age_in_years = seconds / 31557600.0d0 / period
   end function
-
 end module
