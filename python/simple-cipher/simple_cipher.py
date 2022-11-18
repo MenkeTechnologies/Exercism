4f98251728691c7bab422bc6a19d628fbb416d96
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-18 11:31:09 -0500

    cipher

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -1,9 +1,27 @@
+import random
+from string import ascii_lowercase as letters
+from itertools import cycle
+
+STOP = 100
+A = ord('a')
+ALPHABET_LEN = 26
+
+
 class Cipher:
     def __init__(self, key=None):
-        pass
+        if key is None:
+            self.key = "".join(random.choice(letters) for _ in range(STOP))
+        else:
+            self.key = key
 
     def encode(self, text):
-        pass
+        encoded = []
+        for c1, c2 in zip(text, cycle(self.key)):
+            encoded.append(letters[(ord(c1) % A + ord(c2) % A) % ALPHABET_LEN])
+        return "".join(encoded)
 
     def decode(self, text):
-        pass
+        decoded = []
+        for c1, c2 in zip(text, cycle(self.key)):
+            decoded.append(letters[(ord(c1) % A - ord(c2) % A) % ALPHABET_LEN])
+        return "".join(decoded)
