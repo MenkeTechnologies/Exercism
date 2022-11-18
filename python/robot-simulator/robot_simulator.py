91412e1119573a35a8b3a7e3e87e7d12232be888
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-18 11:25:21 -0500

    rs

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -1,11 +1,25 @@
-# Globals for the directions
-# Change the values as you see fit
-EAST = None
-NORTH = None
-WEST = None
-SOUTH = None
-
+NORTH = (0, 1)
+EAST = (1, 0)
+SOUTH = (0, -1)
+WEST = (-1, 0)
+DIRECTIONS = [NORTH, EAST, SOUTH, WEST]
 
 class Robot:
-    def __init__(self, direction=NORTH, x_pos=0, y_pos=0):
-        pass
+    def __init__(self, direction=NORTH, x=0, y=0):
+        self.direction = direction
+        self.x = x
+        self.y = y
+
+    @property
+    def coordinates(self):
+        return self.x, self.y
+
+    def move(self, path):
+        for move in path:
+            if move == "A":
+                self.x += self.direction[0]
+                self.y += self.direction[1]
+            if move == "L":
+                self.direction = DIRECTIONS[(DIRECTIONS.index(self.direction) + 3) % 4]
+            elif move == "R":
+                self.direction = DIRECTIONS[(DIRECTIONS.index(self.direction) + 1) % 4]
