851d60023d45bef0b2f7efe2e571b9a430b9d2ab
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-18 11:32:56 -0500

    kg

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,20 @@
+DEFAULT_STUDENTS = (
+    'Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred',
+    'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry',
+)
+PLANTS = {
+    'C': 'Clover',
+    'G': 'Grass',
+    'R': 'Radishes',
+    'V': 'Violets',
+}
+
+
 class Garden:
-    def __init__(self, diagram, students):
-        pass
+    def __init__(self, diagram, students=DEFAULT_STUDENTS):
+        self.lines = diagram.splitlines()
+        self.students = sorted(students)
+
+    def plants(self, student):
+        index = self.students.index(student) * 2
+        return [PLANTS[p[i]] for p in self.lines for i in (index, index + 1)]
