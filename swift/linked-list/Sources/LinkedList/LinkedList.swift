536c2550fd43ecdced93406bc9a6d8bfaa42dc3c
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-20 09:17:01 -0500

    ll

--- swift/linked-list/Sources/LinkedList/LinkedList.swift
+++ swift/linked-list/Sources/LinkedList/LinkedList.swift
@@ -1,1 +1,19 @@
-//Solution goes in Sources
+final class Deque<T> {
+    private var values = [T]()
+
+    func push(_ value: T) {
+        values.append(value)
+    }
+
+    func pop() -> T? {
+        values.removeLast()
+    }
+
+    func shift() -> T? {
+        values.removeFirst()
+    }
+
+    func unshift(_ value: T) {
+        values.insert(value, at: 0)
+    }
+}
