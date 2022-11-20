4b14027b8ba6b70a71b48d8b69a1207bd7ff53bc
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-19 23:51:15 -0500

    wc

--- swift/word-count/Sources/WordCount/WordCount.swift
+++ swift/word-count/Sources/WordCount/WordCount.swift
@@ -1,1 +1,10 @@
-//Solution goes in Sources
+struct WordCount {
+    let words: String
+
+    func count() -> [String: Int] {
+        words.split { !$0.isLetter && !$0.isNumber }
+                .reduce(into: [:]) { acc, w in
+                    acc[String(w).lowercased(), default: 0] += 1
+                }
+    }
+}
