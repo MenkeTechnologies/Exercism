67e441c8311737f494b2bc6f18d8a51442c44195
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-20 09:33:21 -0500

    mb

--- swift/bomb-defuser/Sources/BombDefuser/BombDefuser.swift
+++ swift/bomb-defuser/Sources/BombDefuser/BombDefuser.swift
@@ -1,11 +1,17 @@
-//let flip: (String, String, String) -> (String, String, String) = TODO: Please define the flip closure
-//
-//
-//let rotate: (String, String, String) -> (String, String, String) = TODO: Please define the stopAtOne closure
+import Foundation
+
+let flip: ((String, String, String)) -> (String, String, String) = { ($0.1, $0.0, $0.2) }
+let rotate: ((String, String, String)) -> (String, String, String) = { ($0.1, $0.2, $0.0) }
 
 func makeShuffle(
-  flipper: @escaping ((String, String, String)) -> (String, String, String),
-  rotator: @escaping ((String, String, String)) -> (String, String, String)
+    flipper: @escaping ((String, String, String)) -> (String, String, String),
+    rotator: @escaping ((String, String, String)) -> (String, String, String)
 ) -> (UInt8, (String, String, String)) -> (String, String, String) {
-  fatalError("Please implement the makeShuffle(flipper:rotator:) function")
+    { id, items in
+        String(String(String(id, radix: 2).reversed()).padding(toLength: 8, withPad: "0", startingAt: 0))
+            .reduce(items) { (acc, bit) in
+                bit == "0" ? flipper(acc) : rotator(acc)
+            }
+    }
 }
+
--- swift/matching-brackets/Sources/MatchingBrackets/MatchingBrackets.swift
+++ swift/matching-brackets/Sources/MatchingBrackets/MatchingBrackets.swift
@@ -1,1 +1,19 @@
-//Solution goes in Sources
+let dict: [Character: Character] = [
+    "(" : ")",
+    "{" : "}",
+    "[" : "]",
+]
+
+func paired(s: String) -> Bool {
+    var stk = [Character]()
+    for c in s {
+        if dict.keys.contains(c) {
+            stk.append(c)
+        } else if dict.values.contains(c) {
+            guard let opening = stk.popLast(), c == dict[opening] else {
+                return false
+            }
+        }
+    }
+    return stk.isEmpty
+}
