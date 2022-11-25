f53321e88cbc5f296a5983b38605a3ce56979086
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-25 10:31:54 -0500

    bs

--- elm/binary-search/src/BinarySearch.elm
+++ elm/binary-search/src/BinarySearch.elm
@@ -1,8 +1,24 @@
 module BinarySearch exposing (find)
-
 import Array exposing (Array)
 
-
 find : Int -> Array Int -> Maybe Int
-find target xs =
-    Debug.todo "Please implement this function"
+find target lst =
+    aux target 0 (Array.length lst) lst
+
+aux : Int -> Int -> Int -> Array Int -> Maybe Int
+aux target lo hi lst =
+    let
+        mid =
+            (lo + hi) // 2
+        value =
+            Array.get mid lst
+                |> Maybe.withDefault 0
+    in
+    if lo >= hi then
+        Nothing
+    else if target == value then
+        Just mid
+    else if target > value then
+        aux target (mid + 1) hi lst
+    else
+        aux target lo mid lst
