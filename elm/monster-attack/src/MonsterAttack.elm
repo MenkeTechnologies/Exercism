92a69df1b5e695e4533deb6598d27e23f6adc00c
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-25 11:37:21 -0500

    ma

--- elm/monster-attack/src/MonsterAttack.elm
+++ elm/monster-attack/src/MonsterAttack.elm
@@ -4,27 +4,21 @@ import String
 type alias MonsterDamage =
     String
 
-attackWithSword1 : MonsterDamage -> Int -> MonsterDamage
 attackWithSword1 monsterDamage strength =
     monsterDamage ++ "Attacked with sword of strength " ++ String.fromInt strength ++ "."
 
-attackWithClaw1 : MonsterDamage -> Int -> MonsterDamage
 attackWithClaw1 monsterDamage strength =
     monsterDamage ++ "Attacked with claw of strength " ++ String.fromInt strength ++ "."
 
-attack1 : MonsterDamage -> MonsterDamage
 attack1 monsterDamage =
     attackWithSword1 (attackWithClaw1 (attackWithClaw1 (attackWithSword1 monsterDamage 5) 1) 1) 5
 
-attackWithSword2 : Int -> MonsterDamage -> MonsterDamage
 attackWithSword2 strength monsterDamage =
     monsterDamage ++ "Attacked with sword of strength " ++ String.fromInt strength ++ "."
 
-attackWithClaw2 : Int -> MonsterDamage -> MonsterDamage
 attackWithClaw2 strength monsterDamage =
     monsterDamage ++ "Attacked with claw of strength " ++ String.fromInt strength ++ "."
 
-attack2 : MonsterDamage -> MonsterDamage
 attack2 monsterDamage =
     monsterDamage
     |> attackWithSword2 5
@@ -32,7 +26,6 @@ attack2 monsterDamage =
     |> attackWithClaw2 1
     |> attackWithSword2 5
 
-attack3 : MonsterDamage -> MonsterDamage
 attack3 =
     attackWithSword2 5 >> attackWithClaw2 1 >> attackWithClaw2 1 >> attackWithSword2 5
 
