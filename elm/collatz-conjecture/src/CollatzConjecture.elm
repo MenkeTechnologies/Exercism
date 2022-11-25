467545331af16c6ab189dcc2a7eedd42a1b237d4
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-25 10:14:16 -0500

    coll

--- elm/collatz-conjecture/src/CollatzConjecture.elm
+++ elm/collatz-conjecture/src/CollatzConjecture.elm
@@ -1,6 +1,16 @@
 module CollatzConjecture exposing (collatz)
-
-
 collatz : Int -> Result String Int
 collatz start =
-    Debug.todo "Please implement this function"
+  if start < 1 then
+    Err "Only positive integers are allowed"
+  else
+    Ok (aux start)
+
+aux : Int -> Int
+aux n =
+  if n < 2 then
+    0
+  else if modBy 2 n == 0 then
+    1 + aux (n // 2)
+  else
+    1 + aux (3 * n + 1)
