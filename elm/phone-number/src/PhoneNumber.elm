5c27d0b0d2954e4f1c7d8e13320da4041395fece
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-27 13:18:53 -0500

    pn

--- elm/phone-number/src/PhoneNumber.elm
+++ elm/phone-number/src/PhoneNumber.elm
@@ -1,6 +1,26 @@
 module PhoneNumber exposing (getNumber)
+import String exposing (length, slice, filter, fromList, toList)
+import Char exposing (isDigit)
+import Maybe exposing (andThen)
 
+getNumber = digitsStripIntl
+    >> andThen (validateNumber)
+    >> andThen (validateDigitAt 0)
+    >> andThen (validateDigitAt 3)
 
-getNumber : String -> Maybe String
-getNumber phoneNumber =
-    Debug.todo "Please implement this function"
+digitsStripIntl phoneNumber =
+    let digits = phoneNumber |> filter isDigit
+    in case digits |> toList of
+            '1' :: rest -> Just (fromList rest)
+            _ -> Just digits
+
+validateNumber phoneNumber =
+    case length phoneNumber of
+        10 -> Just phoneNumber
+        _  -> Nothing
+
+validateDigitAt pos phoneNumber =
+    case phoneNumber |> slice pos (pos + 1) of
+        "0" -> Nothing
+        "1" -> Nothing
+        _   -> Just phoneNumber
