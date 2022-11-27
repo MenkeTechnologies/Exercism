057c54ea3d6056b57c2d8f6e55975ae513ce9268
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-27 13:14:55 -0500

    rs

--- elm/robot-simulator/src/RobotSimulator.elm
+++ elm/robot-simulator/src/RobotSimulator.elm
@@ -1,49 +1,35 @@
 module RobotSimulator exposing
-    ( Bearing(..)
-    , Robot
-    , advance
-    , defaultRobot
-    , simulate
-    , turnLeft
-    , turnRight
-    )
+    ( Bearing(..) , Robot , advance , defaultRobot , simulate , turnLeft , turnRight)
+type Bearing = North | East | South | West
+type alias Robot = { bearing : Bearing , coordinates : { x : Int, y : Int } }
 
+defaultRobot = { bearing = North , coordinates = { x = 0, y = 0 } }
 
-type Bearing
-    = North
-    | East
-    | South
-    | West
+turnRight robot = case robot.bearing of
+        North -> { robot | bearing = East }
+        East -> { robot | bearing = South }
+        South -> { robot | bearing = West }
+        West -> { robot | bearing = North }
 
+turnLeft robot = case robot.bearing of
+        North -> { robot | bearing = West }
+        West -> { robot | bearing = South }
+        South -> { robot | bearing = East }
+        East -> { robot | bearing = North }
 
-type alias Robot =
-    { bearing : Bearing
-    , coordinates : { x : Int, y : Int }
-    }
+advance { bearing, coordinates } = case bearing of
+        North -> Robot bearing { coordinates | y = coordinates.y + 1 }
+        East -> Robot bearing { coordinates | x = coordinates.x + 1 }
+        South -> Robot bearing { coordinates | y = coordinates.y - 1 }
+        West -> Robot bearing { coordinates | x = coordinates.x - 1 }
 
+simulate directions robot = directions
+        |> String.toList
+        |> List.foldl executeAction robot
 
-defaultRobot : Robot
-defaultRobot =
-    { bearing = North
-    , coordinates = { x = 0, y = 0 }
-    }
+executeAction char robot = case char of
+        'L' -> turnLeft robot
+        'R' -> turnRight robot
+        'A' -> advance robot
+        _ -> robot
 
-
-turnRight : Robot -> Robot
-turnRight robot =
-    Debug.todo "Please implement this function"
-
-
-turnLeft : Robot -> Robot
-turnLeft robot =
-    Debug.todo "Please implement this function"
-
-
-advance : Robot -> Robot
-advance robot =
-    Debug.todo "Please implement this function"
-
-
-simulate : String -> Robot -> Robot
-simulate directions robot =
-    Debug.todo "Please implement this function"
