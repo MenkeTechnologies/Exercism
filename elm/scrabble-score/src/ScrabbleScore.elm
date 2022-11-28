3c76ea8678c0d6a52fb06d4e6004b0f992b47e53
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-28 08:41:58 -0500

    ss

--- elm/scrabble-score/src/ScrabbleScore.elm
+++ elm/scrabble-score/src/ScrabbleScore.elm
@@ -1,6 +1,39 @@
 module ScrabbleScore exposing (scoreWord)
+import Dict exposing (Dict)
 
+scores = Dict.fromList
+        [ ( 'A', 1 )
+        , ( 'E', 1 )
+        , ( 'I', 1 )
+        , ( 'O', 1 )
+        , ( 'U', 1 )
+        , ( 'L', 1 )
+        , ( 'N', 1 )
+        , ( 'R', 1 )
+        , ( 'S', 1 )
+        , ( 'T', 1 )
+        , ( 'D', 2 )
+        , ( 'G', 2 )
+        , ( 'B', 3 )
+        , ( 'C', 3 )
+        , ( 'M', 3 )
+        , ( 'P', 3 )
+        , ( 'F', 4 )
+        , ( 'H', 4 )
+        , ( 'V', 4 )
+        , ( 'W', 4 )
+        , ( 'Y', 4 )
+        , ( 'K', 5 )
+        , ( 'J', 8 )
+        , ( 'X', 8 )
+        , ( 'Q', 10 )
+        , ( 'Z', 10 )
+        ]
 
-scoreWord : String -> Int
-scoreWord x =
-    Debug.todo "Please implement this function"
+scoreWord s = s
+        |> String.toUpper
+        |> String.toList
+        |> List.map (\c -> Dict.get c scores)
+        |> List.filterMap identity
+        |> List.sum
+
