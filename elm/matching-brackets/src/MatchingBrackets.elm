2b7b5684fc2fd7b3253d2e29e765046b97594324
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-29 14:03:34 -0500

    mb

--- elm/matching-brackets/src/MatchingBrackets.elm
+++ elm/matching-brackets/src/MatchingBrackets.elm
@@ -1,6 +1,30 @@
 module MatchingBrackets exposing (isPaired)
 
+import Dict exposing (fromList, get, values)
+import List exposing (member, isEmpty)
 
-isPaired : String -> Bool
+brackets = fromList
+        [ ( '(', ')' )
+        , ( '[', ']' )
+        , ( '{', '}' )
+        ]
+
 isPaired input =
-    Debug.todo "Please implement this function"
+    let
+        aux s stk =
+            case String.uncons s of
+                Just ( head, tail ) ->
+                    case get head brackets of
+                        Just closingBracket ->
+                            aux tail (closingBracket :: stk)
+                        Nothing ->
+                            if member head (values brackets) then
+                                case stk of
+                                    [] -> False
+                                    x :: xs -> if head == x then
+                                            aux tail xs
+                                        else False
+                            else aux tail stk
+                Nothing -> isEmpty stk
+    in aux input []
+
