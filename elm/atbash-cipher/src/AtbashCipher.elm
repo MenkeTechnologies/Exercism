94dfcd202cedd2c02a1880d9fe73d981932c7c97
MenkeTechnologies <linux.dev25@gmail.com>
2022-11-29 15:18:22 -0500

    atb

--- elm/atbash-cipher/src/AtbashCipher.elm
+++ elm/atbash-cipher/src/AtbashCipher.elm
@@ -1,11 +1,22 @@
 module AtbashCipher exposing (decode, encode)
 
+import List exposing (map, length, drop, take)
+import Char exposing (toCode, fromCode, isDigit, toLower, isLower)
 
-encode : String -> String
-encode plain =
-    Debug.todo "Please implement this function"
+encode plain = plain |> digitOrLetter |> String.map transpose |> fragment
+decode cipher = cipher |> digitOrLetter |> String.map transpose
 
+digitOrLetter s = s |> String.map toLower
+        |> String.filter (\c -> isLower c || isDigit c)
 
-decode : String -> String
-decode cipher =
-    Debug.todo "Please implement this function"
+transpose c = if c >= 'a' && c <= 'z' then
+        toCode 'z' - toCode c + toCode 'a' |> fromCode
+    else c
+
+split lst = if length lst <= 5 then [ lst ]
+    else take 5 lst :: split (drop 5 lst)
+
+fragment = String.toList
+        >> split
+        >> map String.fromList
+        >> String.join " "
