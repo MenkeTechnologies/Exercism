6aaef66c11b2444d81912c7ec5803c0f38bf9c9a
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-08 12:26:29 -0500

    rc

--- erlang/resistor-color/src/resistor_color.erl
+++ erlang/resistor-color/src/resistor_color.erl
@@ -1,9 +1,19 @@
 -module(resistor_color).
-
 -export([colors/0, color_code/1]).
-
+-define(COLORS, [
+    {black, 0},
+    {brown, 1},
+    {red, 2},
+    {orange, 3},
+    {yellow, 4},
+    {green, 5},
+    {blue, 6},
+    {violet, 7},
+    {grey, 8},
+    {white, 9}
+]).
 colors() ->
-    undefined.
-
-color_code(_Color) ->
-    undefined.
+    {Colors, _} = lists:unzip(?COLORS),
+    Colors.
+color_code(Color) ->
+    proplists:get_value(Color, ?COLORS).
