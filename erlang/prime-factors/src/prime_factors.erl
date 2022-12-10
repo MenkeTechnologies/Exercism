131be23a8a24a6083ed8c25f0b6aa1e600a310af
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-10 10:51:30 -0500

    pf

--- erlang/prime-factors/src/prime_factors.erl
+++ erlang/prime-factors/src/prime_factors.erl
@@ -1,6 +1,8 @@
 -module(prime_factors).
-
 -export([factors/1]).
 
+factors(Value) -> aux(Value, 2).
 
-factors(_Value) -> undefined.
+aux(1, _) -> [];
+aux(Value, Factor) when Value rem Factor == 0 -> [Factor | aux(Value div Factor, Factor)];
+aux(Value, Factor) -> aux(Value, Factor + 1).
