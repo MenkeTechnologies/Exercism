53031234e1c27d9ef9cb7560cd0ce370d44671e5
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-10 11:04:08 -0500

    wc

--- erlang/word-count/src/word_count.erl
+++ erlang/word-count/src/word_count.erl
@@ -1,6 +1,14 @@
 -module(word_count).
 
 -export([count_words/1]).
+count_words(S) ->
+    {match, WordList} =
+        re:run(
+            string:lowercase(S),
+            "[\\w]+[']*[\\w]*\\b",
+            [unicode, global, {capture, all, list}]),
+    CleanWordList = lists:append(WordList),
+    lists:foldl(fun(Word, Map) -> maps:update_with(Word, fun(V) -> V + 1 end, 1, Map) end,
+                maps:new(),
+                CleanWordList).
 
-
-count_words(_Sentence) -> undefined.
