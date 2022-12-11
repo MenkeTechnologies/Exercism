7b6031314454126578b71f6f8128e3d3e0a17fca
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-11 01:01:32 -0500

    hs

--- dart/high-scores/lib/high_scores.dart
+++ dart/high-scores/lib/high_scores.dart
@@ -1,3 +1,17 @@
 class HighScores {
-  // Put your code here
+  List<int> scores;
+
+  HighScores(this.scores);
+
+  List<int> sort() {
+    var l = scores.toList();
+    l.sort();
+    return l;
+  }
+
+  int latest() => scores.last;
+
+  int personalBest() => sort().last;
+
+  Iterable<int> personalTopThree() => sort().reversed.take(3);
 }
