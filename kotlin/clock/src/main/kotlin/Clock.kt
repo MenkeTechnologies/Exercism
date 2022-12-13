793dabced8cb0a61fcf01cc319ef82c5934235d5
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-13 09:40:59 -0500

    clock

--- kotlin/clock/src/main/kotlin/Clock.kt
+++ kotlin/clock/src/main/kotlin/Clock.kt
@@ -1,13 +1,37 @@
-class Clock {
-    //TODO: implement proper constructor
+data class Clock(private var hours: Int, private var minutes: Int) {
+    private val MINUTES_IN_HOUR = 60
+    private val HOURS_IN_DAY = 24
 
-    //TODO: find a convenient way to take over `toString()` and `equals()`
-
-    fun subtract(minutes: Int) {
-        TODO("Implement the function to complete the task")
+    init {
+        normalize()
     }
 
-    fun add(minutes: Int) {
-        TODO("Implement the function to complete the task")
+    private fun normalize() {
+        while (minutes >= MINUTES_IN_HOUR) {
+            minutes -= MINUTES_IN_HOUR
+            ++hours
+        }
+        while (minutes < 0) {
+            minutes += MINUTES_IN_HOUR
+            --hours
+        }
+        while (hours >= HOURS_IN_DAY) {
+            hours -= HOURS_IN_DAY
+        }
+        while (hours < 0) {
+            hours += HOURS_IN_DAY
+        }
     }
+
+    fun add(m: Int) {
+        minutes += m
+        normalize()
+    }
+
+    fun subtract(m: Int) {
+        minutes -= m
+        normalize()
+    }
+
+    override fun toString() = "%02d:%02d".format(hours, minutes)
 }
