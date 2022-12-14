c230e1ec735bbc2efaf077c2ee0a462a6779d8e4
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-14 01:33:57 -0500

    pf

--- clojure/prime-factors/src/prime_factors.clj
+++ clojure/prime-factors/src/prime_factors.clj
@@ -1,5 +1,12 @@
 (ns prime-factors)
 
-(defn of [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn of [n]
+  (loop [val n
+         divisor 2
+         factors []]
+    (cond
+      (= val 1) factors
+      (zero? (mod val divisor))
+      (recur (/ val divisor) divisor (conj factors divisor))
+      :else
+      (recur val (inc divisor) factors))))
