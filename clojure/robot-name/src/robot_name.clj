41a019f48f89d19c4f8f1b8023d993a242a683ef
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-14 01:21:04 -0500

    rn

--- clojure/robot-name/src/robot_name.clj
+++ clojure/robot-name/src/robot_name.clj
@@ -1,13 +1,15 @@
 (ns robot-name)
 
-(defn robot [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def to-str (partial apply str))
+(def caps (map char (range 65 91)))
 
-(defn robot-name [robot] ;; <- arglist goes here
-      ;; your code goes here
-)
-
-(defn reset-name [robot] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn generate-name []
+  (let [letters (->> caps seq shuffle (take 2) to-str)
+        numbers (->> 10 range shuffle (take 3) to-str)]
+    (to-str letters numbers)))
+(defn robot []
+  (atom {:name (generate-name)}))
+(defn robot-name [robot]
+  (:name @robot))
+(defn reset-name [robot]
+  (swap! robot assoc :name (generate-name)))
