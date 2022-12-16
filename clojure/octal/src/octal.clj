a8f2772ca8eeeaf85be472833e995c0c85f47b91
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-16 14:01:53 -0500

    octal

--- clojure/octal/src/octal.clj
+++ clojure/octal/src/octal.clj
@@ -1,5 +1,12 @@
 (ns octal)
 
-(defn to-decimal [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn octal? [s]
+  (->> s
+       (re-matches #"[0-7]+")
+       (boolean)))
+
+(defn to-decimal [s]
+  (if (not (octal? s)) 0
+    (->> s
+         (map #(Character/digit % 10))
+         (reduce #(+ (* % 8) %2) 0))))
