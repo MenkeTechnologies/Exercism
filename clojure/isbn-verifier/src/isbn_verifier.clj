790dddc008e1269fd29b2a913d200e83b3e06d89
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-16 11:13:03 -0500

    isbn

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,5 +1,13 @@
 (ns isbn-verifier)
 
-(defn isbn? [isbn] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn isbn? [isbn]
+  (->>
+    (some->> (clojure.string/replace isbn "-" "")
+             (re-matches #"^\d{9}[\dX]$")
+             (map #(if (= \X %) 10 (Character/digit % 10)))
+             reverse
+             (map-indexed #(* (inc %1) %2))
+             (apply +)
+             (#(mod % 11))
+             zero?)
+    boolean))
