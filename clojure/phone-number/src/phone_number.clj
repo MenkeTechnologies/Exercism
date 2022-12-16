d19e7fd494cb174b952c975805edb2cdc62f9dcf
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-16 11:34:02 -0500

    pn

--- clojure/phone-number/src/phone_number.clj
+++ clojure/phone-number/src/phone_number.clj
@@ -1,13 +1,22 @@
 (ns phone-number)
 
-(defn number [num-string] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn number [digits]
+  (or
+    (->> digits
+         (re-seq #"\d")
+         (apply str)
+         (re-matches #"1?([2-9]\d\d[2-9]\d{6})")
+         last)
+    "0000000000"))
 
-(defn area-code [num-string] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn area-code [digits]
+  (-> (number digits)
+      (subs 0 3)))
 
-(defn pretty-print [num-string] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn pretty-print [digits]
+  (->> digits
+       (number)
+       (re-matches #"(\d{3})(\d{3})(\d{4})")
+       (rest)
+       (apply format "(%s) %s-%s")))
+
