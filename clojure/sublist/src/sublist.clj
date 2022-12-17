e90a07acf5e202ecab6dddb5534eab93950ecf8c
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-17 11:32:59 -0500

    sl

--- clojure/sublist/src/sublist.clj
+++ clojure/sublist/src/sublist.clj
@@ -1,5 +1,10 @@
 (ns sublist)
 
-(defn classify [list1 list2] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn sublist? [coll1 coll2]
+  (some #{coll1} (partition (count coll1) 1 coll2)))
+
+(defn classify [coll1 coll2]
+  (cond (= coll1 coll2) :equal
+        (sublist? coll1 coll2) :sublist
+        (sublist? coll2 coll1) :superlist
+        :else :unequal))
