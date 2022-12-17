b2d6a5fb18016fa8b8d3c41ec381217557745c57
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-17 11:47:20 -0500

    atb

--- clojure/atbash-cipher/src/atbash_cipher.clj
+++ clojure/atbash-cipher/src/atbash_cipher.clj
@@ -1,5 +1,24 @@
 (ns atbash-cipher)
 
-(defn encode [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn- is-letter? [c]
+  (re-matches #"[a-z]|[A-Z]" c))
+
+(defn- encode-char [c]
+  (if (->> c str (is-letter?))
+    (char (- (int \z) (- (int c) (int \a))))
+    c))
+
+(defn- alnum [w]
+  (->> w (re-seq #"[a-z]+|[A-Z]|[0-9]+") (apply str)))
+
+(defn- split-five [w]
+  (re-seq #".{1,5}" w))
+
+(defn encode [w]
+  (->> w
+    (clojure.string/lower-case)
+    (alnum)
+    (map encode-char)
+    (apply str)
+    (split-five)
+    (clojure.string/join " ")))
