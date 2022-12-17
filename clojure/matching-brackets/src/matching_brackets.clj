3a3de9d876cf1279dddf0d0a59fc9c9683bec411
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-17 11:39:45 -0500

    mb

--- clojure/matching-brackets/src/matching_brackets.clj
+++ clojure/matching-brackets/src/matching_brackets.clj
@@ -1,5 +1,17 @@
 (ns matching-brackets)
 
-(defn valid? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def ^:private matching {\{ \}, \[ \], \( \)})
+
+(def ^:private opening? (set (keys matching)))
+
+(def ^:private bracket? (into opening? (vals matching)))
+
+(defn valid? [s]
+  (let [update-stack (fn [acc c]
+                       (cond (opening? c) (conj acc c)
+                             (= (matching (peek acc)) c) (pop acc)
+                             :else (reduced [::not-empty])))]
+    (->> s
+         (filter bracket?)
+         (reduce update-stack [])
+         (empty?))))
