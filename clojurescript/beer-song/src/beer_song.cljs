fae6c436af98d9560e0ab705254b34380b7d6313
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-18 12:12:13 -0500

    bs

--- clojurescript/beer-song/src/beer_song.cljs
+++ clojurescript/beer-song/src/beer_song.cljs
@@ -1,9 +1,17 @@
 (ns beer-song)
+(defn verse [n]
+  (case n
+    0 (str "No more bottles of beer on the wall, no more bottles of beer.\n"
+           "Go to the store and buy some more, 99 bottles of beer on the wall.\n")
+    1 (str "1 bottle of beer on the wall, 1 bottle of beer.\n"
+           "Take it down and pass it around, no more bottles of beer on the wall.\n")
+    (str n " bottles of beer on the wall, " n " bottles of beer.\n"
+         "Take one down and pass it around, " (dec n) " bottle" (if (> n 2) "s" "") " of beer on the wall.\n")))
 
-(defn verse [] ;; <-- arguments goes here...
-  ;; your code goes here...
-  )
-
-(defn sing [] ;; <-- arguments goes here...
-  ;; your code goes here...
-  )
+(defn sing ([start] (sing start 0))
+  ([start end]
+   (->> (inc start)
+        (range end)
+        reverse
+        (map verse)
+        (clojure.string/join "\n"))))
