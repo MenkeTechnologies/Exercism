e791a9cec96dca6129f53852658cfb50863d9a40
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-18 14:06:28 -0500

    ballerina

--- clojurescript/clock/src/clock.cljs
+++ clojurescript/clock/src/clock.cljs
@@ -1,7 +1,13 @@
-(ns clock)
+(ns clock
+  (:require [clojure.pprint :refer [cl-format]]))
 
-(defn clock [])
+(defn clock->string [c]
+  (cl-format nil "~2,'0D:~2,'0D" (quot c 60) (mod c 60)))
 
-(defn clock->string [])
+(defn add-time [c minutes]
+  (clock 0 (+ c minutes)))
 
-(defn add-time [])
+(defn clock [h m]
+  (mod (+ (* 60 h) m) 1440))
+
+
