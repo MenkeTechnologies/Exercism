f484e934b1aa0b95960ce2b215d47aa2dec0e4ae
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-21 13:18:13 -0500

    proverb

--- common-lisp/proverb/proverb.lisp
+++ common-lisp/proverb/proverb.lisp
@@ -4,4 +4,11 @@
 
 (in-package :proverb)
 
-(defun recite (strings))
+(defun recite (strings)
+  (loop for start on strings
+    if (cdr start)
+        collect (format nil "For want of a ~a the ~a was lost." (car start) (cadr start)) into out
+    else
+        collect (format nil "And all for the want of a ~a." (car strings)) into out
+    end
+    finally (return (format nil "~{~A~^~%~}" out))))
