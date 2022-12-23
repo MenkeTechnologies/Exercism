fbf927d56babb0850fbca02f385d0e63c8b02195
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-23 06:47:22 -0500

    series

--- elm/series/src/Series.elm
+++ elm/series/src/Series.elm
@@ -1,6 +1,22 @@
 module Series exposing (slices)
 
-
-slices : Int -> String -> Result String (List (List Int))
-slices size input =
-    Debug.todo "Please implement this function"
+slices size s =
+    if size < 0 then
+        Err "slice length cannot be negative"
+    else if size == 0 then
+        Err "slice length cannot be zero"
+    else if String.length s < 1 then
+        Err "series cannot be empty"
+    else if String.length s < size then
+        Err "slice length cannot be greater than series length"
+    else
+        String.length s - size
+        |> List.range 0
+        |> List.map
+        (\i ->
+            s
+            |> String.slice i (i + size)
+            |> String.toList
+            |> List.map String.fromChar
+            |> List.filterMap String.toInt)
+            |> Ok
