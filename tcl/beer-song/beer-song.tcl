0f3112aa57896d5097d4404b19f361276351508a
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-24 18:18:04 -0500

    bs

--- tcl/beer-song/beer-song.tcl
+++ tcl/beer-song/beer-song.tcl
@@ -1,3 +1,20 @@
+proc verse {n} {
+	if {$n < 1} {
+		return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+	} elseif {$n == 1} {
+       return  "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall."
+	} else {
+       if {$n > 2} { set plural "s" } else { set plural "" }
+       set next [expr $n - 1]
+       return "$n bottles of beer on the wall, $n bottles of beer.\nTake one down and pass it around, $next bottle$plural of beer on the wall."
+    }
+}
+
 proc beerSong {start take} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set output [list]
+    for {set i 0} {$i < $take} {incr i} {
+        set current [expr $start - $i]
+        lappend output [verse $current]
+    }
+    return [join $output \n]
 }
