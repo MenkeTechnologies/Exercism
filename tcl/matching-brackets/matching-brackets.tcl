14c5f56ccfc2b41602f58981ac7dc8eb493a387b
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-25 12:36:31 -0500

    mb

--- tcl/matching-brackets/matching-brackets.tcl
+++ tcl/matching-brackets/matching-brackets.tcl
@@ -1,3 +1,23 @@
-proc bracketsMatch {input} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc bracketsMatch {s} {
+    set brackets [dict create \{ \} \( \) \[ \] ]
+    set stk {}
+    foreach c [split $s ""] {
+        if {$c in [dict keys $brackets]} {
+            lappend stk $c
+        } elseif {$c in [dict values $brackets]} {
+            if { [llength $stk] == 0 ||
+                $c ne [dict get $brackets [lpop stk]]
+            } then {
+                return false
+            }
+        }
+    }
+    expr [llength $stk] == 0
+}
+
+proc lpop {listname} {
+    upvar 1 $listname lst
+    set value [lindex $lst end]
+    set lst [lrange $lst 0 end-1]
+    return $value
 }
