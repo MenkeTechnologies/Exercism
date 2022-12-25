6971dbfd68650614c1f98051556ff1268a33c127
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-25 12:22:07 -0500

    rn

--- tcl/roman-numerals/roman-numerals.tcl
+++ tcl/roman-numerals/roman-numerals.tcl
@@ -1,3 +1,9 @@
 proc toroman {n} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set romans {1000  M 900 CM 500 D 400 CD 100 C 90 XC 50 L 40 XL 10 X 9 IX 5 V 4 IV 1 I}
+    set res ""
+    foreach {k v} $romans {
+        append res [string repeat $v [expr $n / $k]]
+        set n [expr $n % $k]
+    }
+    return $res
 }
