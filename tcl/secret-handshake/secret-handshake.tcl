15eb46a302e32f1c2251dbdaf3bf8e0233012f0d
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-25 12:47:32 -0500

    sh

--- tcl/secret-handshake/secret-handshake.tcl
+++ tcl/secret-handshake/secret-handshake.tcl
@@ -1,3 +1,11 @@
 proc secretHandshake {code} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set dict {1 wink 2 "double blink" 4 "close your eyes" 8 jump}
+    set output [list]
+
+    foreach {k v} $dict {
+        if {$code & $k} {
+            lappend output $v
+        }
+    }
+    if {$code & 16} { lreverse $output } else { return $output }
 }
