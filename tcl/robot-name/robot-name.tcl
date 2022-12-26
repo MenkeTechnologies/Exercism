afa7a5ee5378c8529365465a3e48c267b188e4df
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-26 11:56:38 -0500

    rn

--- tcl/robot-name/robot-name.tcl
+++ tcl/robot-name/robot-name.tcl
@@ -1,19 +1,29 @@
-oo::class create Robot {
-    constructor {} {
-        throw {NOT_IMPLEMENTED} "Implement this class."
-    }
-
-    method name {} {
-        throw {NOT_IMPLEMENTED} "Implement this method."
-    }
-
-    method reset {} {
-        throw {NOT_IMPLEMENTED} "Implement this method."
-    }
-}
-
+set names [dict create]
 proc resetRobotNames {} {
-    throw {NOT_IMPLEMENTED} {
-        This procedure should reset the available and/or used robot names
+    global names
+    set names [dict create]
+}
+oo::class create Robot {
+    variable name
+    constructor {} {
+        my reset
+    }
+    method name {} {
+        return $name
+    }
+    method reset {} {
+        global names
+        set name [my generateName]
+        while {[dict exists $names $name]} {
+            set name [my generateName]
+        }
+        dict set names $name true
+    }
+    method generateName {} {
+        set number [format %03i [expr {int(rand() * 1000)}]]
+        set c1 [expr {int(rand() * 26) + 65}]
+        set c2 [expr {int(rand() * 26) + 65}]
+        set letters [format %c%c $c1 $c2]
+        return "$letters$number"
     }
 }
