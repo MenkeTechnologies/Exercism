4662bc172698c01629234e2b17701716ddcba8eb
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-26 12:04:02 -0500

    atb

--- tcl/atbash-cipher/atbash-cipher.tcl
+++ tcl/atbash-cipher/atbash-cipher.tcl
@@ -1,12 +1,25 @@
 namespace eval atbash {
     namespace export encode decode
     namespace ensemble create
-
-    proc encode {} {
-        throw {NOT_IMPLEMENTED} "Implement this procedure."
+    variable map {
+        a z   b y   c x   d w   e v   f u   g t   h s   i r
+        j q   k p   l o   m n   n m   o l   p k   q j   r i
+        s h   t g   u f   v e   w d   x c   y b   z a
     }
-
-    proc decode {} {
-        throw {NOT_IMPLEMENTED} "Implement this procedure."
+    proc encode {input} {
+        group [decode $input]
+    }
+    proc decode {input} {
+        encipher [alnum $input]
+    }
+    proc encipher {input} {
+        variable map
+        string map $map $input
+    }
+    proc alnum {input} {
+        regsub -all {[^a-z0-9]} [string tolower $input] ""
+    }
+    proc group {input} {
+        join [regexp -all -inline ".{1,5}" $input] { }
     }
 }
