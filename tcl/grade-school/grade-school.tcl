cf6a4c75ba63e3c72de458f1f6f98b057215e5fc
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-26 12:27:30 -0500

    gs

--- tcl/grade-school/grade-school.tcl
+++ tcl/grade-school/grade-school.tcl
@@ -1,7 +1,32 @@
-proc roster {school} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+oo::class create School {
+    variable studentBody
+    constructor {} {
+        my reset
+    }
+    method reset {} {
+        set studentBody {}
+    }
+    method add {students} {
+        lmap student $students {
+            lassign $student name grade
+            if {[dict exists $studentBody $name]} {
+                string cat "false"
+            } else {
+                dict set studentBody $name $grade
+                string cat "true"
+            }
+        }
+    }
+    method roster {} {
+        set sorted [lsort -stride 2 -index 1 -integer [
+                    lsort -stride 2 -index 0 $studentBody]
+                   ]
+        dict keys $sorted
+    }
+    method grade {gr} {
+        set class [dict filter $studentBody value $gr]
+        lsort [dict keys $class]
+    }
 }
 
-proc grade {school grade} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+School create school
