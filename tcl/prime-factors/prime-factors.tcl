11c9cade7ed4795e68a10bbd49fb645fc84a1ca2
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-27 13:42:22 -0500

    pf

--- tcl/prime-factors/prime-factors.tcl
+++ tcl/prime-factors/prime-factors.tcl
@@ -1,5 +1,17 @@
-#!/usr/bin/env tclsh
-
 proc factors {n} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+
+    set facs {}
+    set divisor 2
+
+    while {$n > 1} {
+
+        while {$n % $divisor == 0} {
+            lappend facs $divisor
+            set n [expr $n / $divisor]
+        }
+        incr divisor
+    }
+
+    return $facs
+
 }
