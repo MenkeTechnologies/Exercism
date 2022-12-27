d9fb75ebfa063bebfe5f4b50f998d0acc7ac63fe
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-27 13:50:06 -0500

    rot

--- tcl/rotational-cipher/rotational-cipher.tcl
+++ tcl/rotational-cipher/rotational-cipher.tcl
@@ -1,3 +1,8 @@
-proc rotate {text shift} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc rotate {s amt} {
+    set alphabet {a b c d e f g h i j k l m n o p q r s t u v w x y z}
+    set shifted [list {*}[lrange $alphabet $amt end] {*}[lrange $alphabet 0 $amt-1]]
+    foreach c $alphabet tr $shifted {
+        lappend map $c $tr [string toupper $c] [string toupper $tr]
+    }
+    string map $map $s
 }
