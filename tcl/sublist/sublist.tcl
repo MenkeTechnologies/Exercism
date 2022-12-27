7c1b6d60b6e38898ed8dca8d681fb28e576e18ea
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-27 12:15:11 -0500

    sublist

--- tcl/sublist/sublist.tcl
+++ tcl/sublist/sublist.tcl
@@ -1,3 +1,28 @@
-proc sublist {list1 list2} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc sublist {lst1 lst2} {
+    set EQ "equal"
+    set SUB "sublist"
+    set SUPER "superlist"
+
+    set len1 [llength $lst1]
+    set len2 [llength $lst2]
+
+    if {!$len1 && !$len2} {
+        return $EQ
+    } elseif {!$len1} {
+        return $SUB
+    } elseif {!$len2} {
+        return $SUPER
+    } elseif {$len1 > $len2} {
+        if {[string first $lst2 $lst1] > -1} {
+            return $SUPER
+        }
+    } elseif {$len1 < $len2} {
+        if {[string first $lst1 $lst2] > -1} {
+            return $SUB
+        }
+    }
+    if {$lst1 eq $lst2} {
+        return $EQ
+    }
+    return un$EQ
 }
