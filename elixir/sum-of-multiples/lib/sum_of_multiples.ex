d1b4318daa7352e4e86b9bd730d685a60249b8e2
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-27 19:20:57 -0500

    sum

--- elixir/sum-of-multiples/lib/sum_of_multiples.ex
+++ elixir/sum-of-multiples/lib/sum_of_multiples.ex
@@ -1,8 +1,12 @@
 defmodule SumOfMultiples do
-  @doc """
-  Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
-  """
-  @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
+
   def to(limit, factors) do
+    1..(limit - 1)
+    |> Enum.filter(fn n -> factors |> Enum.any?(&is_multiple?(n, &1)) end)
+    |> Enum.sum()
   end
+  
+  defp is_multiple?(_, 0), do: false
+  defp is_multiple?(num, factor), do: rem(num, factor) == 0
+  
 end
