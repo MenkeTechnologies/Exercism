c6628a4c9554485c5eea81b0877316c10cefbf30
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-28 13:11:17 -0500

    kg

--- kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
+++ kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
@@ -1,6 +1,9 @@
 class KindergartenGarden(private val diagram: String) {
-
-    fun getPlantsOfStudent(student: String): List<String> {
-        TODO("Implement the function to complete the task")
+    companion object {
+        private val students = listOf( "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry" )
+        private val plants = mapOf( 'G' to "grass", 'C' to "clover", 'R' to "radishes", 'V' to "violets" )
     }
+
+    fun getPlantsOfStudent(student: String) = diagram.split("\n")
+        .map { it.chunked(2)[students.indexOf(student)] }.flatMap { it.toList() }.map { plants[it]!! }
 }
