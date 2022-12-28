19c56a6601e30b39ca83d4d3ad9849b253ba65d3
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-28 13:43:52 -0500

    sieve

--- kotlin/sieve/src/main/kotlin/Sieve.kt
+++ kotlin/sieve/src/main/kotlin/Sieve.kt
@@ -1,6 +1,9 @@
-object Sieve {
+import kotlin.math.sqrt
 
-    fun primesUpTo(upperBound: Int): List<Int> {
-        TODO("Implement this function to complete the task")
+object Sieve {
+    fun primesUpTo(limit: Int): List<Int> {
+        val primes = (2..limit).toMutableList()
+        (2..sqrt(limit.toDouble()).toInt()).forEach { i -> primes.removeAll { it in i * 2..limit step i } }
+        return primes
     }
 }
