fa235728e4ac04d8b520a46a4ac9bd67d863088b
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-29 11:45:34 -0500

    hss

--- csharp/high-school-sweethearts/HighSchoolSweethearts.cs
+++ csharp/high-school-sweethearts/HighSchoolSweethearts.cs
@@ -1,20 +1,33 @@
 using System;
+using System.Globalization;
+using System.Text;
 
 public static class HighSchoolSweethearts
 {
-    public static string DisplaySingleLine(string studentA, string studentB)
-    {
-        throw new NotImplementedException($"Please implement the (static) HighSchoolSweethearts.DisplaySingleLine() method");
-    }
+    public static string DisplaySingleLine(string studentA, string studentB) =>
+        $"{studentA,29} ♡ {studentB,-29}";
 
-    public static string DisplayBanner(string studentA, string studentB)
-    {
-        throw new NotImplementedException($"Please implement the (static) HighSchoolSweethearts.DisplayBanner() method");
-    }
+    public static string DisplayBanner(string studentA, string studentB) =>
+        $@"
+     ******       ******
+   **      **   **      **
+ **         ** **         **
+**            *            **
+**                         **
+**     {studentA.Trim()}  +  {studentB.Trim()}     **
+ **                       **
+   **                   **
+     **               **
+       **           **
+         **       **
+           **   **
+             ***
+              *
+        ";
 
     public static string DisplayGermanExchangeStudents(string studentA
-        , string studentB, DateTime start, float hours)
-    {
-        throw new NotImplementedException($"Please implement the (static) HighSchoolSweethearts.DisplayGermanExchangeStudents() method");
-    }
+        , string studentB, DateTime start, float hours) =>
+        string.Format(new CultureInfo("de-DE"),
+            "{0} and {1} have been dating since {2:d} - that's {3:n2} hours",
+            studentA, studentB, start, hours);
 }
