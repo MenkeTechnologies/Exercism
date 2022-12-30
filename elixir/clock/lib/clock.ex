4d31a91c3be4c543ad18b382b3946d2b540bb6f6
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-30 11:00:41 -0500

    clk

--- elixir/clock/lib/clock.ex
+++ elixir/clock/lib/clock.ex
@@ -1,23 +1,16 @@
 defmodule Clock do
   defstruct hour: 0, minute: 0
 
-  @doc """
-  Returns a clock that can be represented as a string:
-
-      iex> Clock.new(8, 9) |> to_string
-      "08:09"
-  """
-  @spec new(integer, integer) :: Clock
   def new(hour, minute) do
+    %Clock{hour: Integer.mod(hour + floor(minute / 60), 24), minute: Integer.mod(minute, 60)}
   end
 
-  @doc """
-  Adds two clock times:
-
-      iex> Clock.new(10, 0) |> Clock.add(3) |> to_string
-      "10:03"
-  """
-  @spec add(Clock, integer) :: Clock
   def add(%Clock{hour: hour, minute: minute}, add_minute) do
+    new(hour, minute + add_minute)
+  end
+
+  defimpl String.Chars, for: Clock do
+    defp pad(n), do: Kernel.to_string(n) |> String.pad_leading(2, "0")
+    def to_string(%Clock{hour: hour, minute: minute}), do: "#{pad(hour)}:#{pad(minute)}"
   end
 end
