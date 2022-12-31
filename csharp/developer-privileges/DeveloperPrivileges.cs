29f5395fc686a2bf96f4566d1536d505a087ea1b
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-31 12:20:09 -0500

    dev

--- csharp/developer-privileges/DeveloperPrivileges.cs
+++ csharp/developer-privileges/DeveloperPrivileges.cs
@@ -1,22 +1,47 @@
-using System;
 using System.Collections.Generic;
-
 public class Authenticator
-{
-    // TODO: Implement the Authenticator.Admin property
-    public Identity Admin { get; }
-
-    // TODO: Implement the Authenticator.Developers property
-    public IDictionary<string, Identity> Developers { get; }
+{    
+    public Identity Admin { get; } = new()
+    {
+        Email = "admin@ex.ism", 
+        FacialFeatures = new FacialFeatures
+        {
+            EyeColor = "green",
+            PhiltrumWidth = 0.9m
+        },
+        NameAndAddress = new List<string> {"Chanakya", "Mumbai", "India"}
+    }; 
+    public IDictionary<string, Identity> Developers { get; } = new Dictionary<string, Identity>
+    {
+        ["Bertrand"] = new()
+        {
+            Email = "bert@ex.ism", 
+            FacialFeatures = new FacialFeatures
+            {
+                EyeColor = "blue",
+                PhiltrumWidth = 0.8m
+            },
+            NameAndAddress = new List<string> {"Bertrand", "Paris", "France"}    
+        },
+        ["Anders"] = new()
+        {
+            Email = "anders@ex.ism", 
+            FacialFeatures = new FacialFeatures
+            {
+                EyeColor = "brown",
+                PhiltrumWidth = 0.85m
+            },
+            NameAndAddress = new List<string> {"Anders", "Redmond", "USA"}    
+        }
+    };
+    
 }
-
 //**** please do not modify the FacialFeatures class ****
 public class FacialFeatures
 {
     public string EyeColor { get; set; }
     public decimal PhiltrumWidth { get; set; }
 }
-
 //**** please do not modify the Identity class ****
 public class Identity
 {
