3191f875b4221bed88492d8f24001faa12950e7d
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-31 12:12:50 -0500

    sublist

--- csharp/sublist/Sublist.cs
+++ csharp/sublist/Sublist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public enum SublistType
 {
@@ -14,6 +15,15 @@ public static class Sublist
     public static SublistType Classify<T>(List<T> list1, List<T> list2)
         where T : IComparable
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (list1.SequenceEqual(list2))
+            return SublistType.Equal;
+        if (Contains(list1, list2))
+            return SublistType.Sublist;
+        return Contains(list2, list1) ? SublistType.Superlist : SublistType.Unequal;
     }
+
+    private static bool Contains<T>(IReadOnlyCollection<T> list1, IReadOnlyCollection<T> list2) =>
+        !list1.Any() ||
+        list1.Count <= list2.Count && Enumerable.Range(0, list2.Count - list1.Count + 1)
+            .Any(i => list1.SequenceEqual(list2.Skip(i).Take(list1.Count)));
 }
