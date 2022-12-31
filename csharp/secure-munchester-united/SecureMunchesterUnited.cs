65beab993e167487ff24987fdc526da2878bd2ff
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-31 12:15:09 -0500

    smu

--- csharp/secure-munchester-united/SecureMunchesterUnited.cs
+++ csharp/secure-munchester-united/SecureMunchesterUnited.cs
@@ -1,33 +1,29 @@
-using System;
-
 public class SecurityPassMaker
 {
-    public string GetDisplayName(TeamSupport support)
+    public string GetDisplayName(TeamSupport support) => support switch
     {
-        throw new NotImplementedException($"Please implement the SecurityPassMaker.GetDisplayName() method");
-    }
+        Security when support.GetType() == typeof(Security) => $"{support.Title} Priority Personnel",
+        Staff => support.Title,
+        _ => "Too Important for a Security Pass"
+    };
 }
-
-/**** Please do not alter the code below ****/
-
 public interface TeamSupport { string Title { get; } }
-
 public abstract class Staff : TeamSupport { public abstract string Title { get; } }
-
-public class Manager : TeamSupport { public string Title { get; } = "The Manager"; }
-
-public class Chairman : TeamSupport { public string Title { get; } = "The Chairman"; }
-
-public class Physio : Staff { public override string Title { get; } = "The Physio"; }
-
-public class OffensiveCoach : Staff { public override string Title { get; } = "Offensive Coach"; }
-
-public class GoalKeepingCoach : Staff { public override string Title { get; } = "Goal Keeping Coach"; }
-
-public class Security : Staff { public override string Title { get; } = "Security Team Member"; }
-
-public class SecurityJunior : Security { public override string Title { get; } = "Security Junior"; }
-
-public class SecurityIntern : Security { public override string Title { get; } = "Security Intern"; }
-
-public class PoliceLiaison : Security { public override string Title { get; } = "Police Liaison Officer"; }
+public class Manager : TeamSupport { public string Title => "The Manager";
+}
+public class Chairman : TeamSupport { public string Title => "The Manager";
+}
+public class Physio : Staff { public override string Title => "The Physio";
+}
+public class OffensiveCoach : Staff { public override string Title => "Offensive Coach";
+}
+public class GoalKeepingCoach : Staff { public override string Title => "Goal Keeping Coach";
+}
+public class Security : Staff { public override string Title => "Security Team Member";
+}
+public class SecurityJunior : Security { public override string Title => "Security Junior";
+}
+public class SecurityIntern : Security { public override string Title => "Security Intern";
+}
+public class PoliceLiaison : Security { public override string Title => "Police Liaison Officer";
+}
