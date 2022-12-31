44d7d2e6d47c6de80e9151ec8d1be64c6a50221f
MenkeTechnologies <linux.dev25@gmail.com>
2022-12-31 11:58:30 -0500

    net7

--- csharp/kindergarten-garden/KindergartenGarden.cs
+++ csharp/kindergarten-garden/KindergartenGarden.cs
@@ -1,22 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public enum Plant
 {
-    Violets,
-    Radishes,
-    Clover,
-    Grass
+    Violets = 'V',
+    Radishes = 'R',
+    Clover = 'C',
+    Grass = 'G'
 }
 
 public class KindergartenGarden
-{    
+{
+    private readonly string diagram;
+
+    private readonly List<string> studentsName = new List<string>()
+    {
+        "Alice", "Bob", "Charlie", "David",
+        "Eve", "Fred", "Ginny", "Harriet",
+        "Ileana", "Joseph", "Kincaid", "Larry"
+    };
+
     public KindergartenGarden(string diagram)
     {
+        this.diagram = diagram;
     }
 
     public IEnumerable<Plant> Plants(string student)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int index = studentsName.IndexOf(student) * 2;
+        return from line in diagram.Split('\n')
+            from idx in Enumerable.Range(index, 2)
+            select (Plant)line[idx];
     }
 }
