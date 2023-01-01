09ce8bbdcab61960eaee9aa7b0768ef2e92b0bdf
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-01 11:43:53 -0500

    gi

--- elixir/kindergarten-garden/lib/garden.ex
+++ elixir/kindergarten-garden/lib/garden.ex
@@ -1,14 +1,21 @@
 defmodule Garden do
-  @doc """
-    Accepts a string representing the arrangement of cups on a windowsill and a
-    list with names of students in the class. The student names list does not
-    have to be in alphabetical order.
-
-    It decodes that string into the various gardens for each student and returns
-    that information in a map.
-  """
-
-  @spec info(String.t(), list) :: map
-  def info(info_string, student_names) do
+  @default_names [ :alice, :bob, :charlie, :david, :eve, :fred, :ginny, :harriet,
+    :ileana, :joseph, :kincaid, :larry ]
+  
+  @all_plants %{"C" => :clover, "G" => :grass, "R" => :radishes, "V" => :violets}
+  
+  def info(info_string, student_names \\ @default_names) do
+    plants = info_string
+      |> String.split("\n")
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.map(& &1 |> Enum.map(fn c -> @all_plants |> Map.get(c) end))
+      |> Enum.map(&Enum.chunk_every(&1, 2))
+      |> Enum.zip()
+      |> Enum.map(fn {p1, p2} -> (p1 ++ p2) |> List.to_tuple() end)
+    student_names
+    |> Enum.sort()
+    |> Enum.with_index()
+    |> Enum.map(fn {name, i} -> {name, plants |> Enum.at(i, {})} end)
+    |> Map.new()
   end
 end
