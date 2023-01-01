5feb3e2f4ba17097a6e8316d4ca7338e84ac00be
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-01 11:17:09 -0500

    ll

--- python/simple-linked-list/simple_linked_list.py
+++ python/simple-linked-list/simple_linked_list.py
@@ -1,33 +1,52 @@
 class Node:
-    def __init__(self, value):
-        pass
+    def __init__(self, value, next=None):
+        self._value = value
+        self._next = next
 
     def value(self):
-        pass
+        return self._value
 
     def next(self):
-        pass
+        return self._next
 
 
 class LinkedList:
     def __init__(self, values=[]):
-        pass
+        self._head = None
+        self._count = 0
+        for v in values:
+            self.push(v)
 
     def __len__(self):
-        pass
+        return self._count
 
     def head(self):
-        pass
+        if self._head is None:
+            raise EmptyListException('The list is empty.')
+        return self._head
 
     def push(self, value):
-        pass
+        self._head = Node(value, self._head)
+        self._count += 1
 
     def pop(self):
-        pass
+        if self._head is None:
+            raise EmptyListException('The list is empty.')
+        value = self._head.value()
+        self._head = self._head.next()
+        self._count -= 1
+        return value
 
     def reversed(self):
-        pass
+        return LinkedList(self)
+
+    def __iter__(self):
+        node = self._head
+        while node is not None:
+            yield node.value()
+            node = node.next()
 
 
 class EmptyListException(Exception):
-    pass
+    def __init__(self, msg):
+        super().__init__(msg)
