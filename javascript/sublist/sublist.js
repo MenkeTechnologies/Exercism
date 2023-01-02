1785f029f984a8e813fb077ab903e51bffb2fa31
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-02 09:10:00 -0500

    subl

--- javascript/sublist/sublist.js
+++ javascript/sublist/sublist.js
@@ -1,9 +1,13 @@
 export class List {
-    constructor() {
-        throw new Error('Remove this statement and implement this function');
-    }
+  constructor(lst = []) {
+    this.lst = lst;
+  }
 
-    compare() {
-        throw new Error('Remove this statement and implement this function');
-    }
+  compare = other => {
+    const s1 = this.lst.join(','), s2 = other.lst.join(',');
+    let oneInTwo = false, twoInOne = false;
+    if (s1.includes(s2)) twoInOne = true;
+    if (s2.includes(s1)) oneInTwo = true;
+    return oneInTwo && twoInOne ? 'EQUAL' : oneInTwo ? 'SUBLIST' : twoInOne ? 'SUPERLIST' : 'UNEQUAL';
+  };
 }
