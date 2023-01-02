dc29510ce3419e08c4104d269643b824ea6ba40e
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-02 08:56:24 -0500

    promises

--- javascript/promises/promises.js
+++ javascript/promises/promises.js
@@ -1,19 +1,23 @@
-export const promisify = () => {
-    throw new Error('Remove this statement and implement this function');
-};
+export const promisify = f => (...args) =>
+  new Promise((resolve, reject) =>
+    f(...args, (err, result) => err ? reject(err) : resolve(result)));
 
-export const all = () => {
-    throw new Error('Remove this statement and implement this function');
-};
-
-export const allSettled = () => {
-    throw new Error('Remove this statement and implement this function');
-};
-
-export const race = () => {
-    throw new Error('Remove this statement and implement this function');
-};
-
-export const any = () => {
-    throw new Error('Remove this statement and implement this function');
-};
+export const all = promises => promises ?
+  promises.reduce(
+    async (acc, promise) => (await acc).concat(await promise), Promise.resolve([])) : Promise.resolve();
+export const allSettled = promises => promises ? promises.reduce(
+  async (acc, promise) => (await acc).concat(await promise.catch(err => err)),
+  Promise.resolve([])
+) : Promise.resolve();
+export const race = promises => promises ?
+  new Promise((resolve, reject) => promises.length
+    ? promises.forEach(promise => promise.then(resolve, reject))
+    : resolve([]))
+  : Promise.resolve()
+export const any = promises => promises ?
+  new Promise((resolve, reject) => {
+    promises.length
+      ? promises.forEach(promise => promise.then(resolve).catch(() => null))
+      : resolve([]);
+    allSettled(promises).then(reject);
+  }) : Promise.resolve()
