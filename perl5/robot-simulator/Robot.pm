e0bc72c975b26a0b2af264196162942425b4a5b3
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-03 13:36:22 -0500

    robo

--- perl5/robot-simulator/Robot.pm
+++ perl5/robot-simulator/Robot.pm
@@ -1,81 +1,63 @@
-# there are 2 packages in this file; separate them once multiple files are supported
-
+package Robot;
 use strict;
 use warnings;
+no warnings 'experimental';
+use feature 'switch';
 
-{
+my @bearings = ('north', 'east', 'south', 'west');
+sub new {bless { position => [ 0, 0 ], bearing => 'north' }, shift}
+sub bearing {shift->{bearing}}
+sub coordinates {shift->{position}}
 
-    package Robot;
-
-    sub new {
-        return undef;
-    }
-
-    sub bearing {
-        return undef;
-    }
-
-    sub coordinates {
-        return undef;
-    }
-
-    sub orient {
-        my ( $self, $direction ) = @_;
-
-        return undef;
-    }
-
-    sub turn_right {
-        my $self = shift;
-
-        return undef;
-    }
-
-    sub turn_left {
-        my $self = shift;
-
-        return undef;
-    }
-
-    sub at {
-        my ( $self, $x, $y ) = @_;
-
-        return undef;
-    }
-
-    sub advance {
-        my $self = shift;
-
-        return undef;
-    }
-
-}    # end of Package Example;
-
-{
-
-    package Simulator;
-
-    sub new {
-        return undef;
-    }
-
-    sub place {
-        my ( $self, $robot, $options ) = @_;
-
-        return undef;
-    }
-
-    sub evaluate {
-        my ( $self, $robot, $abbreviation ) = @_;
-
-        return undef;
-    }
-
-    sub instructions {
-        my ( $self, $abbreviation ) = @_;
-
-        return undef;
-    }
+sub orient {
+    my ($self, $direction) = @_;
+    die 'ArgumentError' if not grep {/$direction/} @bearings;
+    $self->{bearing} = $direction;
 }
 
+sub turn {
+    my ($self, $dir) = @_;
+    my $amt = $dir eq 'r' ? 1 : -1;
+    my $i = List::Util::first {$bearings[$_] eq $self->{bearing}} 0 .. $#bearings;
+    $bearings[($i + $amt) % 4];
+}
+
+sub turn_right {
+    my $self = shift;
+    $self->{bearing} = $self->turn('r');
+}
+sub turn_left {
+    my $self = shift;
+    $self->{bearing} = $self->turn('l');
+}
+sub at {
+    my ($self, $x, $y) = @_;
+    $self->{position} = [ $x, $y ];
+}
+sub advance {
+    my $self = shift;
+    given ($self->{bearing}) {
+        $self->{position}[0]++ when 'east';
+        $self->{position}[0]-- when 'west';
+        $self->{position}[1]++ when 'north';
+        $self->{position}[1]-- when 'south';
+    }
+}
+package Simulator;
+my %actions = (L => 'turn_left', R => 'turn_right', A => 'advance');
+
+sub new {bless {}, shift}
+sub place {
+    my ($self, $robot, $options) = @_;
+    $robot->at($options->{x}, $options->{y});
+    $robot->orient($options->{direction});
+}
+sub evaluate {
+    my ($self, $robot, $cmds) = @_;
+    $robot->$_ for $self->instructions($cmds)->@*;
+}
+sub instructions {
+    my ($self, $cmds) = @_;
+    [ map {$actions{$_}} split //, $cmds ];
+}
 1;
