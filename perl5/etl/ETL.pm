1ce2ebe72e46dae1484f0147b6d71d1b8a31ef07
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-03 22:11:14 -0500

    etl

--- perl5/etl/ETL.pm
+++ perl5/etl/ETL.pm
@@ -5,17 +5,8 @@ use Exporter qw<import>;
 our @EXPORT_OK = qw<transform>;
 
 sub transform {
-    my ($data) = @_;
-    my %map = %$data;
-    my %out;
-
-    for my $point ( keys %map ) {
-        my @letters = @{ $map{$point} };
-        $out{ lc $_ } = $point for @letters;
-
-    }
-
-    return \%out;
+    my %map = %{(shift)};
+    ({ map {my $score = $_; map {lc, $score} @{$map{$score}}} keys %map })
 }
 
-1;
+1
