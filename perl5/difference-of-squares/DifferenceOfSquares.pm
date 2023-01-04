ab41dbccc651a51c6d4a9968288afe0e126d6c3e
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-03 22:36:42 -0500

    dos

--- perl5/difference-of-squares/DifferenceOfSquares.pm
+++ perl5/difference-of-squares/DifferenceOfSquares.pm
@@ -3,20 +3,16 @@ use strict;
 use warnings;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<square_of_sum sum_of_squares difference_of_squares>;
+use List::Util 'sum';
 
 sub square_of_sum {
-    my ($number) = @_;
-    return undef;
+    sum(1 .. shift) ** 2
 }
-
 sub sum_of_squares {
-    my ($number) = @_;
-    return undef;
+    sum(map {$_ ** 2} 1 .. shift)
 }
-
 sub difference_of_squares {
-    my ($number) = @_;
-    return undef;
+    my $number = shift;
+    square_of_sum($number) - sum_of_squares($number)
 }
-
 1;
--- perl5/difference-of-squares/README.md
+++ perl5/difference-of-squares/README.md
@@ -1,6 +1,6 @@
-# Difference Of Squares
+# Difference of Squares
 
-Welcome to Difference Of Squares on Exercism's Perl 5 Track.
+Welcome to Difference of Squares on Exercism's Perl Track.
 If you need help running the tests or submitting your code, check out `HELP.md`.
 
 ## Instructions
