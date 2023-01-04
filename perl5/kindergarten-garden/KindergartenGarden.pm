1a78a6044fefc21109e5dce143263d2e4610dcae
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-04 02:39:34 -0500

    kg

--- perl5/kindergarten-garden/KindergartenGarden.pm
+++ perl5/kindergarten-garden/KindergartenGarden.pm
@@ -5,28 +5,22 @@ use warnings;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<plants>;
 
-my @children =
-  qw(Alice Bob Charlie David Eve Fred Ginny Harriet Ileana Joseph Kincaid Larry);
-my %plants = qw(G grass C clover R radishes V violets);
+use constant children => [
+    qw(Alice Bob Charlie David Eve Fred Ginny Harriet Ileana Joseph Kincaid Larry)
+];
+use constant dict => {qw(G grass C clover R radishes V violets)};
 
 sub plants {
-    my ($input) = @_;
-
-    my $diagram   = $input->{diagram};
-    my $student   = $input->{student};
+    my ( $diagram, $student ) = @_;
     my @rows      = split /\n/, $diagram;
-    my @positions = map { $_ * 2 } 0 .. scalar(@children) - 1;
+    my @positions = map { $_ * 2 } 0 .. scalar( children->@* ) - 1;
     my %child_pos;
-    @child_pos{@children} = @positions;
-
-    my @student_plants;
-
-    do {
-        push @student_plants, $plants{ substr( $_, $child_pos{$student}, 1 ) };
-        push @student_plants,
-          $plants{ substr( $_, $child_pos{$student} + 1, 1 ) };
-      }
-      for @rows;
-    return \@student_plants;
+    @child_pos{ children->@* } = @positions;
+    [
+        map {
+            dict->{ substr( $_, $child_pos{$student}, 1 ) },
+              dict->{ substr( $_, $child_pos{$student} + 1, 1 ) }
+        } @rows
+    ];
 }
 1;
