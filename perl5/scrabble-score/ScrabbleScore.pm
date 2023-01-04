1ea4a42dab1fdb13e752c1522600317612b1e315
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-04 01:24:28 -0500

    ss

--- perl5/scrabble-score/ScrabbleScore.pm
+++ perl5/scrabble-score/ScrabbleScore.pm
@@ -3,27 +3,22 @@ use strict;
 use warnings;
 use Exporter 'import';
 our @EXPORT_OK = qw(scrabble_score);
+use List::Util 'sum';
 
 my %points = (
-    ( map { $_ => 1 } qw(A E I O U L N R S T) ),
-    ( map { $_ => 2 } qw(D G) ),
-    ( map { $_ => 3 } qw(B C M P) ),
-    ( map { $_ => 4 } qw(F H V W Y) ),
-    ( map { $_ => 5 } qw(K) ),
-    ( map { $_ => 8 } qw(J X) ),
-    ( map { $_ => 10 } qw(Q Z) )
+    (map {$_ => 1} qw(A E I O U L N R S T)),
+    (map {$_ => 2} qw(D G)),
+    (map {$_ => 3} qw(B C M P)),
+    (map {$_ => 4} qw(F H V W Y)),
+    (map {$_ => 5} qw(K)),
+    (map {$_ => 8} qw(J X)),
+    (map {$_ => 10} qw(Q Z))
 );
 
 sub scrabble_score {
-    local $_ = shift;
+    $_ = shift;
 
-    my @c = split //, uc;
-
-    my $sc = 0;
-
-    $sc += $points{$_} for @c;
-
-    $sc;
+    (sum map {$points{$_}} split //, uc) // 0
 }
 
 1;
