78a7d1b988794958a956484753c387217e48ea76
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-03 22:31:53 -0500

    luhn

--- perl5/luhn/Luhn.pm
+++ perl5/luhn/Luhn.pm
@@ -3,24 +3,16 @@ use strict;
 use warnings;
 use Exporter 'import';
 our @EXPORT_OK = qw(is_luhn_valid);
+use List::Util qw/sum/;
 
 sub is_luhn_valid {
-    my ($input) = @_;
-
-    $input =~ s@\s+@@g;
-
-    return 0 if length($input) < 2 || $input =~ /\D/;
-
+    $_ = shift;
+    s@\s+@@g;
     my $cnt = 0;
-    my $sum = 0;
-
-    do {
-        $_   *= 2 if $cnt++ % 2 == 1;
-        $sum += $_ > 9 ? $_ - 9 : $_;
-      }
-      for split( '', reverse($input) );
-
-    $sum % 10 == 0;
+    length >= 2 && !/\D/ && sum(map {
+        $_ *= 2 if $cnt++ % 2 == 1;
+        $_ > 9 ? $_ - 9 : $_;
+    } split '', reverse) % 10 == 0
 }
 
 1;
