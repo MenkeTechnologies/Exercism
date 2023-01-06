c7c997f330321cb11223eaede8ec27ba48101f6d
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-06 12:02:39 -0500

    piglatin

--- perl5/pig-latin/PigLatin.pm
+++ perl5/pig-latin/PigLatin.pm
@@ -1,12 +1,19 @@
 package PigLatin;
 use strict;
 use warnings;
-use Exporter qw<import>;
-our @EXPORT_OK = qw<translate>;
+use Exporter qw/import/;
+our @EXPORT_OK = qw/translate/;
 
-sub translate {
-    my ($phrase) = @_;
-    return undef;
+sub trans {
+    do {
+        if (/^(?: [aeiou] | xr | yt)/x) {return "${_}ay"}
+        if (/^([^aeiouy]+)  (y.+)/x) {return "$2$1ay"}
+        if (/^([^aeiou]*qu) (.+)/x) {return "$2$1ay"}
+        if (/^([^aeiou]+)   (.+)/x) {return "$2$1ay"}
+
+        die "Not translated: $_"
+    } for lc shift;
 }
+sub translate { join ' ', map {trans $_} split ' ', shift; }
 
-1;
+1
