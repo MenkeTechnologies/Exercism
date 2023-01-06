bd4380fa0a71c1085fbc841d4d5b56e1e9cc5a1c
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-05 21:49:46 -0500

    lsp

--- perl5/largest-series-product/LargestSeriesProduct.pm
+++ perl5/largest-series-product/LargestSeriesProduct.pm
@@ -1,12 +1,17 @@
 package LargestSeriesProduct;
+
 use strict;
 use warnings;
-use Exporter qw<import>;
-our @EXPORT_OK = qw<largest_product>;
+use List::Util qw/max product/;
+use Exporter qw/import/;
+our @EXPORT_OK = qw/largest_product/;
 
 sub largest_product {
-    my ($input) = @_;
-    return undef;
+    my ($digits, $window) = @_;
+    my @digits = split //, $digits;
+    die 'span must not be negative' if $window < 0;
+    die 'span must be smaller than string length' if $window > @digits;
+    die 'digits input must only contain digits' if $digits =~ /\D/;
+    max map {product @digits[$_ .. ($_ + $window - 1)]} 0 .. @digits - $window;
 }
-
 1;
