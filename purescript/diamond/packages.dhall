e2c3ec2fc22bbc3481d136551b60893fffcc13ad
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-09 09:20:26 -0500

    diam

--- purescript/diamond/packages.dhall
+++ purescript/diamond/packages.dhall
@@ -99,6 +99,7 @@ in  upstream
 -------------------------------
 -}
 let upstream =
-      https://github.com/purescript/package-sets/releases/download/psc-0.13.8-20210226/packages.dhall sha256:7e973070e323137f27e12af93bc2c2f600d53ce4ae73bb51f34eb7d7ce0a43ea
+      https://github.com/purescript/package-sets/releases/download/psc-0.15.4-20230105/packages.dhall
+        sha256:3e9fbc9ba03e9a1fcfd895f65e2d50ee2f5e86c4cd273f3d5c841b655a0e1bda
 
 in  upstream
--- purescript/diamond/src/Diamond.purs
+++ purescript/diamond/src/Diamond.purs
@@ -1,3 +1,30 @@
 module Diamond
-   (rows
-   ) where
+  ( rows
+  ) where
+
+import Prelude
+import Data.Array (drop, elemIndex, replicate, reverse, (!!), (..))
+import Data.Enum (enumFromTo)
+import Data.Maybe (Maybe, fromMaybe)
+import Data.String.CodeUnits (fromCharArray)
+
+alphabet = enumFromTo 'A' 'Z'
+
+charIndex c = elemIndex c alphabet
+
+charAt n = alphabet !! n
+
+rows c = fromCharArray <$> lines
+  where 
+    rank = fromMaybe 0 (charIndex c)
+    mkRow = mirror <<< row rank
+    lines = mirror <<< map mkRow $ (rank .. 0)
+
+mirror c = reverse c <> drop 1 c
+
+row size rank = before <> [char] <> after
+  where
+    emptyChar = ' '
+    char = fromMaybe emptyChar (charAt rank)
+    before = replicate (rank) emptyChar
+    after = replicate (size - rank) emptyChar
