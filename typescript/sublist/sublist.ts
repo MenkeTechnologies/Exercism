fa3206ad403fdd8c3c6a34d9be8c4a88c330e360
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-09 09:13:45 -0500

    sublist

--- typescript/sublist/sublist.ts
+++ typescript/sublist/sublist.ts
@@ -1,9 +1,21 @@
-export class List {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
-  }
+type Comparison = 'equal' | 'sublist' | 'superlist' | 'unequal';
 
-  compare() {
-    throw new Error('Remove this statement and implement this function')
-  }
+export class List {
+    private _lst: number[];
+
+    constructor(...items: number[]) {
+        this._lst = items;
+    }
+
+    compare(other: List): Comparison {
+        const self = this._lst.join(',');
+        const target = other._lst.join(',');
+        return self === target
+            ? 'equal'
+            : target.indexOf(self) !== -1
+                ? 'sublist'
+                : self.indexOf(target) !== -1
+                    ? 'superlist'
+                    : 'unequal';
+    }
 }
