169702104bc2b88d06b1c670306f37cb564dc26e
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-09 09:26:29 -0500

    ayb

--- typescript/all-your-base/all-your-base.ts
+++ typescript/all-your-base/all-your-base.ts
@@ -1,7 +1,15 @@
-export function convert(
-  digits: unknown,
-  inputBase: unknown,
-  outputBase: unknown
-): unknown {
-  throw new Error('Remove this statement and implement this function')
-}
+export const convert = (digits: number[], inputBase: number, outputBase: number): number[] => {
+    if (inputBase < 2 || !Number.isInteger(inputBase)) throw new Error('Wrong input base');
+    if (outputBase < 2 || !Number.isInteger(outputBase)) throw new Error('Wrong output base');
+    if (digits.length === 1 && digits[0] === 0) return [0];
+    if (digits.length === 0 || digits[0] === 0) throw new Error('Input has wrong format');
+    digits.forEach(d => { if (d < 0 || d >= inputBase) throw new Error('Input has wrong format'); });
+    let num = digits.reduce((acc, n) => acc * inputBase + n, 0)
+
+    const res = [];
+    while (num) {
+        res.unshift(num % outputBase);
+        num = Math.floor(num / outputBase);
+    }
+    return res;
+};
--- typescript/protein-translation/protein-translation.ts
+++ typescript/protein-translation/protein-translation.ts
@@ -1,3 +1,8 @@
-export function translate() {
-  throw new Error('Remove this statement and implement this function')
+const proteins: { [code: string]: string } = {
+    AUG: 'Methionine', UUU: 'Phenylalanine', UUC: 'Phenylalanine', UUA: 'Leucine', UUG: 'Leucine',
+    UCU: 'Serine', UCC: 'Serine', UCA: 'Serine', UCG: 'Serine', UAU: 'Tyrosine',
+    UAC: 'Tyrosine', UGU: 'Cysteine', UGC: 'Cysteine', UGG: 'Tryptophan', UAA: 'STOP',
+    UAG: 'STOP', UGA: 'STOP'
 }
+export const translate = (input: string, matches: string[] = [...input.matchAll(/([UAGC]{3})/g)].map(arr => arr[0])
+    .map(m => proteins[m])) => matches.indexOf('STOP') == -1 ? matches : matches.slice(0, matches.indexOf('STOP'))
