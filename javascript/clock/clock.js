407c47218cfcdc788c28ba13bebbffa5129d8908
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-10 12:33:18 -0500

    clock

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -1,21 +1,21 @@
+const HOUR_MINUTES = 60;
+const DAY_MINUTES = 24 * 60;
+const absModulus = (dividend, divisor) => (divisor + (dividend % divisor)) % divisor;
+
 export class Clock {
-    constructor() {
-        throw new Error('Remove this statement and implement this function');
-    }
+  constructor(hours, minutes = 0) {
+    this._minutes = absModulus(hours * HOUR_MINUTES + minutes, DAY_MINUTES);
+  }
 
-    toString() {
-        throw new Error('Remove this statement and implement this function');
-    }
+  minutes = () => this._minutes % HOUR_MINUTES;
 
-    plus() {
-        throw new Error('Remove this statement and implement this function');
-    }
+  hours = () => Math.floor(this._minutes / HOUR_MINUTES);
 
-    minus() {
-        throw new Error('Remove this statement and implement this function');
-    }
+  toString = () => [this.hours(), this.minutes()].map(time => `${time}`.padStart(2, '0')).join(':');
 
-    equals() {
-        throw new Error('Remove this statement and implement this function');
-    }
+  plus = minutes => new Clock(0, this._minutes + minutes);
+
+  minus = minutes => this.plus(-minutes);
+
+  equals = newClock => this._minutes === newClock._minutes;
 }
