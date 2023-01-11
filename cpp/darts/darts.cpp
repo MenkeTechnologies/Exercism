5cfc3e642cdc2ffd7b5d2c9706033cfcc17eecbd
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-10 22:43:00 -0500

    turn l/r

--- cpp/darts/darts.cpp
+++ cpp/darts/darts.cpp
@@ -4,10 +4,9 @@
 namespace darts {
 
     int score(float xHit, float yHit) {
-        float dist = sqrt(pow(xHit, 2) + pow(yHit, 2));
-        if (dist <= 1) return 10;
-        if (dist <= 5) return 5;
-        if (dist <= 10) return 1;
-        return 0;
+        double dist = sqrt(pow(xHit, 2) + pow(yHit, 2));
+        return dist <= 1 ? 10 :
+        dist <= 5 ? 5 :
+        dist <= 10 ? 1 : 0;
     }
 }
--- javascript/robot-simulator/robot-simulator.js
+++ javascript/robot-simulator/robot-simulator.js
@@ -1,44 +1,51 @@
 export class InvalidInputError extends Error {
-    constructor(message) {
-        super();
-        this.message = message || 'Invalid Input';
-    }
+  constructor(message) {
+    super();
+    this.message = message || 'Invalid Input';
+  }
 }
+
 const DIRECTIONS = ["north", "east", "south", "west"];
+
 export class Robot {
-    x = 0;
-    y = 0;
-    direction = "north";
-    coordChanges = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-    get bearing() {
-        return this.direction;
-    }
-    get coordinates() {
-        return [this.x, this.y];
-    }
-    place = ({ x, y, direction }) => {
-        if (!DIRECTIONS.includes(direction))
-            throw new InvalidInputError()
-        this.direction = direction;
-        this.x = x;
-        this.y = y;
-    };
-    dirIdx = () => DIRECTIONS.indexOf(this.direction);
-    turn = inc => {
-        this.direction = DIRECTIONS[(this.dirIdx() + inc + 4) % 4];
-    };
-    move = c => {
-        const [dx, dy] = this.coordChanges[this.dirIdx()];
-        this.x += c * dx;
-        this.y += c * dy;
-    };
-    evaluate = instructions => {
-        instructions.split('').forEach(cmd =>
-          cmd === "R" ? this.turn(+1) : cmd === "L" ?
-            this.turn(-1) : cmd === "A" ? this.move(1) :
-              (() => {
-                throw new InvalidInputError(`Command ${cmd}`)
-              })()
-        );
-    };
+  x = 0;
+  y = 0;
+  direction = "north";
+  coordChanges = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+
+  get bearing() {
+    return this.direction;
+  }
+
+  get coordinates() {
+    return [this.x, this.y];
+  }
+
+  place = ({x, y, direction}) => {
+    if (!DIRECTIONS.includes(direction))
+      throw new InvalidInputError()
+    this.direction = direction;
+    this.x = x;
+    this.y = y;
+  };
+  dirIdx = () => DIRECTIONS.indexOf(this.direction);
+  turn = inc => {
+    this.direction = DIRECTIONS[(this.dirIdx() + inc + 4) % 4];
+  };
+  turnLeft = () => this.turn(-1)
+  turnRight = () => this.turn(1)
+  advance = () => {
+    const [dx, dy] = this.coordChanges[this.dirIdx()];
+    this.x += dx;
+    this.y += dy;
+  };
+  evaluate = instructions => {
+    instructions.split('').forEach(cmd =>
+      cmd === "R" ? this.turnRight() : cmd === "L" ?
+        this.turnLeft() : cmd === "A" ? this.advance() :
+          (() => {
+            throw new InvalidInputError(`Command ${cmd}`)
+          })()
+    );
+  };
 }
