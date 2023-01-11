1af43f699ca7f7d0caae3e9fecc44ea5654c5ed0
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-11 09:45:46 -0500

    windows

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,9 @@
-def slices(series, length):
-    pass
+def slices(digits, length):
+    if length < 1:
+        raise ValueError(f"slice length cannot be {'zero' if length == 0 else 'negative'}")
+    if not digits:
+        raise ValueError('series cannot be empty')
+    if len(digits) < length:
+        raise ValueError('slice length cannot be greater than series length')
+    number_of_series = len(digits) - length + 1
+    return [digits[n:n + length] for n in range(number_of_series)]
