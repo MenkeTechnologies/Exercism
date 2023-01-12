2ec5c0f6921f0522bd69918843a99e5bb94d9c91
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-12 11:46:42 -0500

    lsp

--- rust/largest-series-product/src/lib.rs
+++ rust/largest-series-product/src/lib.rs
@@ -4,10 +4,17 @@ pub enum Error {
     InvalidDigit(char),
 }
 
-pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
-    unimplemented!(
-        "largest series product of a span of {} digits in {}",
-        span,
-        string_digits
-    );
+pub fn lsp(s: &str, span: usize) -> Result<u64, Error> {
+    if span == 0 {
+        return Ok(1);
+    }
+    let digits = s
+        .chars()
+        .map(|c| c.to_digit(10).ok_or(Error::InvalidDigit(c)))
+        .collect::<Result<Vec<u32>, Error>>()?;
+    digits
+        .windows(span)
+        .map(|win| win.iter().map(|&n| n as u64).product())
+        .max()
+        .ok_or(Error::SpanTooLong)
 }
