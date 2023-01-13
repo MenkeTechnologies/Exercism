de145e7812e8e55f882db33a157c6ef0a10def6a
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-13 10:43:48 -0500

    lsp

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,2 +1,20 @@
+from functools import reduce
+from operator import mul
+
+
+def slices(series, size):
+    if not size <= len(series):
+        raise ValueError('span must be smaller than string length')
+    elif not 0 < size:
+        raise ValueError('span must not be negative')
+    elif not all(item.isdigit() for item in series):
+        raise ValueError('digits input must only contain digits')
+    numbers = [int(digit) for digit in series]
+    return [numbers[i:i + size]
+            for i in range(len(numbers) - size + 1)]
+
+
 def largest_product(series, size):
-    pass
+    if size == 0:
+        return 1
+    return max(reduce(mul, window) for window in slices(series, size))
