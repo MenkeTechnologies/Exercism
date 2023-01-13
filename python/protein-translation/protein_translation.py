ba97aa205fbc97ad0c2d6d54165b7bb10fae139e
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-13 10:32:53 -0500

    prot

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,29 @@
+from itertools import takewhile
+from textwrap import wrap
+
+mapping = {
+    'AUG': 'Methionine',
+    'UUC': 'Phenylalanine',
+    'UUU': 'Phenylalanine',
+    'UUA': 'Leucine',
+    'UUG': 'Leucine',
+    'UCU': 'Serine',
+    'UCC': 'Serine',
+    'UCA': 'Serine',
+    'UCG': 'Serine',
+    'UAC': 'Tyrosine',
+    'UAU': 'Tyrosine',
+    'UGC': 'Cysteine',
+    'UGU': 'Cysteine',
+    'UGG': 'Tryptophan',
+}
+
+
+def is_not_stop(pattern):
+    return pattern not in ('UAG', 'UAA', 'UGA')
+
+
 def proteins(strand):
-    pass
+    return [protein
+            for pattern in takewhile(is_not_stop, wrap(strand, 3))
+            for protein in (mapping[pattern],)]
