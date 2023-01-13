e880a02ee7978b06f9def1937ada14f63d0b34b7
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-13 10:35:24 -0500

    sieve

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,9 @@
 def primes(limit):
-    pass
+    table = set(range(2, limit + 1))
+    result = []
+    while len(table):
+        min_val = min(table)
+        result.append(min_val)
+        to_sub = set(range(min_val, limit + 1, min_val))
+        table -= to_sub
+    return result
