aaf9f72028411cfd6041dfd32b3751e77d89df3c
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-14 10:05:04 -0500

    lsp

--- typescript/largest-series-product/largest-series-product.ts
+++ typescript/largest-series-product/largest-series-product.ts
@@ -1,3 +1,13 @@
-export const largestProduct = () => {
-  throw new Error('Remove this statement and implement this function')
+export const largestProduct = (text: string, winSize: number): number => {
+    if (text.match(/\D/g)) throw('Digits input must only contain digits');
+    if (winSize > text.length) throw('Span must be smaller than string length');
+    if (winSize < 0) throw('Span must be greater than zero');
+    if (winSize === 0) return 1;
+
+    let max = 0;
+    for (let i = 0; i <= text.length - winSize; ++i) {
+        const sub = text.substring(i, i + winSize).split('').map(n => parseInt(n)).reduce((acc, n) => acc * n);
+        max = sub > max ? sub : max;
+    }
+    return max;
 }
