b11ca24cc1c512193d3f334fae915596c76cddd1
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-15 10:42:39 -0500

    ayb

--- julia/all-your-base/all-your-base.jl
+++ julia/all-your-base/all-your-base.jl
@@ -1,5 +1,7 @@
-# Implement number base conversion
-
-function all_your_base(digits, base_in, base_out)
-
+function all_your_base(in, base_in, base_out)
+    (base_in <= 1) | (base_out <= 1) && throw(DomainError("invalid base $base_in"))
+    any(i -> (i < 0) | (i >= base_in), in) && throw(DomainError("invalid digits $in"))
+    n = length(in)
+    sum = reduce((acc, n) -> acc * base_in + n, in, init=0) 
+    reverse!(digits(sum; base=base_out))
 end
