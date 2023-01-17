d11cca7318369b12f1208d9349b815efac63ede1
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-17 12:03:07 -0500

    ayb

--- crystal/all-your-base/src/all_your_base.cr
+++ crystal/all-your-base/src/all_your_base.cr
@@ -1,1 +1,16 @@
-# Please implement your solution to all-your-base in this file
+class AllYourBase
+  def self.rebase(input_base : Int32, digits : Array(Int32), output_base : Int32) : Array(Int32)
+	raise ArgumentError.new if input_base < 2 || output_base < 2 || digits.any? { |d| d.negative? || d >= input_base }
+
+    sum = digits.reduce(0) { |acc, n| acc * input_base + n }
+    return [0] if sum.zero?
+
+    output = [] of Int32
+    while sum.positive?
+      sum, digit = sum.divmod output_base
+      output.unshift digit
+    end
+    output
+
+  end
+end
--- crystal/series/src/series.cr
+++ crystal/series/src/series.cr
@@ -1,6 +1,6 @@
 class Series
   def self.slices(s, len)
     raise ArgumentError.new if len < 1 || len > s.size || s.empty?
-    s.chars.each.cons(len).map(&.join).to_a
+    s.chars.each_cons(len).map(&.join).to_a
   end
 end
