27344bcca2e7ab815f0cca4bf79b400b6d4321dd
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-17 11:44:14 -0500

    clock

--- crystal/clock/src/clock.cr
+++ crystal/clock/src/clock.cr
@@ -1,3 +1,25 @@
 class Clock
-  # Implement the Clock class
+  getter hour : Int32
+  getter minute : Int32
+  def initialize(hour = 0, minute = 0)
+    time = (hour * 60 + minute) % (24 * 60)
+    @hour = time // 60
+    @minute = time % 60
+  end
+ 
+  def +(other : Clock) : Clock
+    Clock.new(@hour + other.hour, @minute + other.minute)
+  end
+ 
+  def -(other : Clock) : Clock
+    Clock.new(@hour - other.hour, @minute - other.minute)
+  end
+ 
+  def ==(other : Clock) : Bool
+    @hour == other.hour && @minute == other.minute
+  end
+ 
+  def to_s : String
+    sprintf("%02d:%02d", @hour, @minute)
+  end
 end
--- crystal/series/src/series.cr
+++ crystal/series/src/series.cr
@@ -1,1 +1,6 @@
-# Please implement your solution to series in this file
+class Series
+  def self.slices(s, len)
+    raise ArgumentError.new if len < 1 || len > s.size || s.empty?
+    s.chars.each.cons(len).map(&.join).to_a
+  end
+end
