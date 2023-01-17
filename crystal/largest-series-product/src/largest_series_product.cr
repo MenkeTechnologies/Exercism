d6fa4c63f48e3efe7a013b9284cc03169bc5fa04
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-17 11:55:44 -0500

    lsp

--- crystal/largest-series-product/src/largest_series_product.cr
+++ crystal/largest-series-product/src/largest_series_product.cr
@@ -1,1 +1,15 @@
-# Please implement your solution to largest-series-product in this file
+class Series
+    def initialize(@s : String)
+    end
+    def largest_product(length : Int32)
+		raise ArgumentError.new if length < 0 || length > @s.size
+		return 1 if @s.size == 0 || length == 0
+
+        @s.chars.each_cons(length, true).map { |slice| UInt128.new(product(slice)) }.max
+
+    end
+
+	private def product(array : Array(Char))
+        array.reduce(1) { |acc, c| UInt128.new(acc * c.to_i) }
+    end
+end
