a09c67f4763af551237dcbdc351b84ce5d7bb978
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-18 10:22:33 -0500

    sieve

--- crystal/sieve/src/sieve.cr
+++ crystal/sieve/src/sieve.cr
@@ -1,1 +1,13 @@
-# Please implement your solution to sieve in this file
+class Primes
+  def self.sieve(n : Int32) : Array(Int32)
+    primes = [] of Int32
+    return primes if n < 2
+    ary = (2..n).to_a
+    until ary.empty?
+      primes << ary[0]
+      ary.reject! { |x| x.divisible_by? primes.last }
+    end
+    primes
+  end
+end
+
