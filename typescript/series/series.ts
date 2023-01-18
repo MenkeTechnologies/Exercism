4a7941f40b7797dbe74c630c46ee0444730dd004
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-18 10:18:57 -0500

    series

--- typescript/series/series.ts
+++ typescript/series/series.ts
@@ -1,9 +1,17 @@
+
 export class Series {
-  constructor(series: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  private readonly series: string;
+
+  constructor(series: string) {
+    if (!series.length) throw new Error('series cannot be empty');
+    this.series = series;
   }
 
-  slices(sliceLength: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  slices(len: number): number[][] {
+    if (!len) throw new Error('slice length cannot be zero');
+    if (len < 0) throw new Error('slice length cannot be negative');
+    if (this.series.length < len) throw new Error('slice length cannot be greater than series length');
+    const numSlices = this.series.length - len + 1;
+    return [...Array(numSlices)].map((_, i) => this.series.slice(i, i + len).split("").map(n => parseInt(n)));
   }
 }
