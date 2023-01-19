173be94fb4aca4c6684774baf69b3434768f0c45
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-19 09:46:21 -0500

    series

--- groovy/series/src/main/groovy/Series.groovy
+++ groovy/series/src/main/groovy/Series.groovy
@@ -1,6 +1,9 @@
 class Series {
-
-    static slices(series, sliceLength) {
-        throw new UnsupportedOperationException('method not implemented.')
+    static slices(String series, int len) {
+        if (len < 1 || len > series.length())
+            throw new ArithmeticException()
+        series.split("")
+                .collate(len, 1, false)
+                .collect { it.join() }
     }
 }
