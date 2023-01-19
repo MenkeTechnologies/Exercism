bccd5462a69c13b8dd915c0a76f06e60d40e0d11
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-19 09:59:38 -0500

    ayb

--- groovy/all-your-base/src/main/groovy/AllYourBase.groovy
+++ groovy/all-your-base/src/main/groovy/AllYourBase.groovy
@@ -1,10 +1,21 @@
 class AllYourBase {
+    private Integer value
 
-    AllYourBase(inputBase, digits) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    AllYourBase(int inputBase, List<Integer> digits) {
+        if (inputBase < 2) throw new ArithmeticException()
+        digits.forEach { if (it < 0 || it >= inputBase) throw new ArithmeticException() }
+        value = digits.inject(0) { acc, n -> acc * inputBase + n }
     }
 
-    def rebase(outputBase) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    Collection<Integer> rebase(int outputBase) {
+        if (outputBase < 2) throw new ArithmeticException()
+        if (value == 0) return [0]
+
+        def res = []
+        while (value > 0) {
+            res.add(0, value % outputBase)
+            value = value.intdiv(outputBase)
+        }
+        res
     }
 }
