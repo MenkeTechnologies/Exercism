3ddc301bbc9a1692156ed6627dd84e52a636ea84
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-19 21:37:20 -0500

    ayb

--- java/all-your-base/src/main/java/BaseConverter.java
+++ java/all-your-base/src/main/java/BaseConverter.java
@@ -1,10 +1,27 @@
-/*
+import java.util.ArrayList;
+import java.util.Arrays;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class BaseConverter {
+    private int sum;
 
-Please remove this comment when submitting your solution.
+    BaseConverter(int inBase, int[] ary) {
+        if (inBase < 2) throw new IllegalArgumentException("Bases must be at least 2.");
+        if (Arrays.stream(ary).anyMatch(n -> n >= inBase)) throw new IllegalArgumentException("All digits must be strictly less than the base.");
+        if (Arrays.stream(ary).anyMatch(n -> n < 0)) throw new IllegalArgumentException("Digits may not be negative.");
+        
+        sum = Arrays.stream(ary).reduce(0, (acc, n) -> acc * inBase + n);
+    }
 
-*/
+    public int[] convertToBase(int outBase) {
+        if (outBase < 2) throw new IllegalArgumentException("Bases must be at least 2.");
+        
+        if (sum < 1 ) return new int[]{0};
+
+        ArrayList<Integer> res = new ArrayList<>();
+        while (sum > 0) {
+            res.add(0, sum % outBase);
+            sum /= outBase;
+        }
+        return res.stream().mapToInt(n -> n).toArray();
+    }
+}
