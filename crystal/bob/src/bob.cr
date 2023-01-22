b9ecc26c8a374f51dad3747d06a6af95b79239c9
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-22 10:31:45 -0500

    bob

--- crystal/bob/src/bob.cr
+++ crystal/bob/src/bob.cr
@@ -1,13 +1,14 @@
 class Bob
     def self.hey(s)
+        nows = s.gsub(/\s/, "")
 
-        return "Fine. Be that way!" if s.blank?
+        return "Fine. Be that way!" if nows.blank?
 
-        if /[A-Z]/ =~ s && /[a-z]/ !~ s
-           return (/\?$/ =~ s ?  "Calm down, I know what I'm doing!" : "Whoa, chill out!")
+        if /[A-Z]/ =~ nows && /[a-z]/ !~ nows
+           return (/\?$/ =~ nows ?  "Calm down, I know what I'm doing!" : "Whoa, chill out!")
         end
 
-        /\?$/ =~ s ?  "Sure." : "Whatever."
+        /\?$/ =~ nows ?  "Sure." : "Whatever."
 
 
     end
