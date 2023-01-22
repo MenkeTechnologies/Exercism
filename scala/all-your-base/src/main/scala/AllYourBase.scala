7beaf3a5a8bbf0e51ed475645fa1e7721de418d5
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-22 10:26:31 -0500

    ayb

---
+++ scala/all-your-base/src/main/scala/AllYourBase.scala
@@ -0,0 +1,17 @@
+import scala.collection.mutable.ListBuffer
+
+object AllYourBase {
+  def rebase(inputBase: Int, digits: List[Int], outputBase: Int): Option[List[Int]] = {
+    if (inputBase <= 1 || outputBase <= 1 || digits.exists(num => num < 0 || num >= inputBase)) None
+    else {
+      var sum = digits .foldLeft(0) { (acc, n) => acc * inputBase + n }
+      if (sum == 0) return Some(List(0))
+      val output = ListBuffer[Int]()
+      while (sum > 0) {
+        output.insert(0, sum % outputBase)
+        sum /= outputBase
+      }
+      Some(output.toList)
+    }
+  }
+}
