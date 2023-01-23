c914f82c62b23b4349e1d4f0e2c4d223f4c912ed
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-23 10:51:01 -0500

    hamm

--- swift/hamming/Sources/Hamming/Hamming.swift
+++ swift/hamming/Sources/Hamming/Hamming.swift
@@ -1,20 +1,6 @@
-//Solution goes in Sources
-
 public class Hamming {
 
     public static func compute(_ s1: String, against s2: String) -> Int? {
-
-        if s1.count != s2.count {
-            return nil
-        }
-
-        var h = 0
-        for (c1, c2) in zip(s1, s2) {
-            if c1 != c2 {
-                h += 1
-            }
-        }
-
-        return h
+        s1.count != s2.count ? nil : zip(s1, s2).filter { $0 != $1 } .count
     }
 }
