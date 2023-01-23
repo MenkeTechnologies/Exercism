6fea6d59eda0c6fdf5a89006d79453cc8841deb4
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-23 10:39:36 -0500

    prov

--- python/proverb/proverb.py
+++ python/proverb/proverb.py
@@ -1,2 +1,11 @@
-def proverb():
-    pass
+def proverb(*args, qualifier=None):
+    res = []
+    if len(args) == 0:
+        return res
+    for num in range(int(len(args) - 1)):
+        res.append(f"For want of a {args[num]} the {args[num + 1]} was lost.")
+    if qualifier:
+        res.append(f"And all for the want of a {qualifier} nail.")
+    else:
+        res.append(f"And all for the want of a {args[0]}.")
+    return res
