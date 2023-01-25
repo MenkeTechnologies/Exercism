1014d251b29daa9c44fb0f894f0371b8ee69c485
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-25 13:02:09 -0500

    ayb

--- csharp/all-your-base/AllYourBase.cs
+++ csharp/all-your-base/AllYourBase.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class AllYourBase
 {
     public static int[] Rebase(int inputBase, int[] inputDigits, int outputBase)
     {
-        throw new NotImplementedException("Please implement this function");
+        List<int> outputDigits = new List<int>();
+        if (inputBase < 2 || outputBase < 2 || inputDigits.Any(c => c < 0 || c >= inputBase))
+        {
+            throw new ArgumentException();
+        }
+
+        int sum = inputDigits.Aggregate(0, (acc, n) => acc * inputBase + n);
+        if (sum == 0)
+        {
+            return new[] { 0 };
+        }
+
+        while (sum > 0)
+        {
+            outputDigits.Insert(0, sum % outputBase);
+            sum /= outputBase;
+        }
+
+        return outputDigits.ToArray();
     }
 }
