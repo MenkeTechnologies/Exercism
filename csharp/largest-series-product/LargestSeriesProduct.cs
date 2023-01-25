b6ca9bb121d94ec93eceaf219741633b64283efc
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-25 12:55:27 -0500

    lsp

--- csharp/largest-series-product/LargestSeriesProduct.cs
+++ csharp/largest-series-product/LargestSeriesProduct.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Linq;
 
 public static class LargestSeriesProduct
 {
-    public static long GetLargestProduct(string digits, int span) 
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static long GetLargestProduct(string digits, int window) =>
+        window < 0 || window > digits.Length || digits.Any(w => !char.IsNumber(w))
+            ? throw new ArgumentException()
+            : Enumerable.Range(0, digits.Length - window + 1)
+                .Select(i => digits.Substring(i, window).Select(c => c - '0').Aggregate(1, (x, y) => x * y)).Max();
 }
