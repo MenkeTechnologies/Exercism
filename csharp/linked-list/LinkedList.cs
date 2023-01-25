25386f4a5b49049757b9988671cb4bc4d60e7da1
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-25 13:12:35 -0500

    ll

--- csharp/linked-list/LinkedList.cs
+++ csharp/linked-list/LinkedList.cs
@@ -1,24 +1,49 @@
-using System;
-
 public class Deque<T>
 {
+    private Node _first;
+    private Node _last;
+
     public void Push(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var previousLast = _last;
+        _last = new Node { Value = value, Prev = previousLast };
+        _first ??= _last;
+
+        if (previousLast != null)
+        {
+            previousLast.Next = _last;
+        }
     }
 
     public T Pop()
     {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public void Unshift(T value)
-    {
-        throw new NotImplementedException("You need to implement this function.");
+        var value = _last.Value;
+        _last = _last.Prev;
+        return value;
     }
 
     public T Shift()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var value = _first.Value;
+        _first = _first.Next;
+        return value;
+    }
+
+    public void Unshift(T value)
+    {
+        var previousFirst = _first;
+        _first = new Node { Value = value, Next = previousFirst };
+        _last ??= _first;
+        if (previousFirst != null)
+        {
+            previousFirst.Prev = _first;
+        }
+    }
+
+    private class Node
+    {
+        public T Value { get; init; }
+        public Node Next { get; set; }
+        public Node Prev { get; set; }
     }
 }
