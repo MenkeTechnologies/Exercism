5f847a6aa17140aad5e170df10327e8e1315dd64
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-27 11:42:14 -0500

    sieve

--- swift/sieve/Sources/Sieve/Sieve.swift
+++ swift/sieve/Sources/Sieve/Sieve.swift
@@ -1,1 +1,16 @@
-//Solution goes in Sources
+struct Sieve {
+    var primes = [Int]()
+
+    init(_ num: Int) {
+        guard num > 2 else {
+            return
+        }
+        var numbers: [Int?] = Array(2...num)
+        (2...num).forEach{
+            stride(from: 2 * $0 - 2, through: num - 2, by: $0).forEach{ numbers[$0] = nil}
+        }
+        
+        primes = numbers.compactMap {$0}
+        
+    }
+}
