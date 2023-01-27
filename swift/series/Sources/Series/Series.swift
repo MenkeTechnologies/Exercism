860012a8050da8a3233f137f272a22d194ec9761
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-27 10:46:57 -0500

    series

--- swift/series/Sources/Series/Series.swift
+++ swift/series/Sources/Series/Series.swift
@@ -1,1 +1,16 @@
-//Solution goes in Sources
+struct Series {
+    var input: [Int]
+
+    init(_ input: String) {
+        self.input = input.map { Int(String($0))! }
+    }
+
+    func slices(_ number: Int) -> [[Int]] {
+        guard number <= input.count else {
+            return []
+        }
+        return (0...input.count - number).map {
+            Array(input[$0..<($0 + number)])
+        }
+    }
+}
