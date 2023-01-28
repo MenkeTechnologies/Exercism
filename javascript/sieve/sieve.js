7eb62588fae8774ce210d263805058bb3e4cf44e
MenkeTechnologies <linux.dev25@gmail.com>
2023-01-28 11:54:32 -0500

    sieve

--- javascript/sieve/sieve.js
+++ javascript/sieve/sieve.js
@@ -1,3 +1,14 @@
-export const primes = () => {
-    throw new Error('Remove this statement and implement this function');
-};
+export function primes(limit) {
+  if (limit < 2) return [];
+  let primeSet = new Set();
+  for (let i = 2; i <= limit; ++i) {
+    primeSet.add(i);
+  }
+  for (let i = 2; i <= limit; ++i) {
+    if (primeSet.has(i)) {
+      for (let j = 2; i * j <= limit; ++j)
+        primeSet.delete(i * j);
+    }
+  }
+  return Array.from(primeSet);
+}
