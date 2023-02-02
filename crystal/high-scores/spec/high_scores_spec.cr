a2dd11eaf9cadcebca5c099b9a3da3d554512bb2
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-02 10:18:54 -0500

    hs

--- crystal/high-scores/spec/high_scores_spec.cr
+++ crystal/high-scores/spec/high_scores_spec.cr
@@ -7,63 +7,63 @@ describe "HighScores" do
     high_scores.scores.should eq([30, 50, 20, 70])
   end
 
-  pending "Latest score" do
+  it "Latest score" do
     high_scores = HighScores.new([100, 0, 90, 30])
     high_scores.latest.should eq(30)
   end
 
-  pending "Personal best" do
+  it "Personal best" do
     high_scores = HighScores.new([40, 100, 70])
     high_scores.personal_best.should eq(100)
   end
 
-  pending "Personal top three from a list of scores" do
+  it "Personal top three from a list of scores" do
     high_scores = HighScores.new([10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70])
     high_scores.personal_top_three.should eq([100, 90, 70])
   end
 
-  pending "Personal top highest to lowest" do
+  it "Personal top highest to lowest" do
     high_scores = HighScores.new([20, 10, 30])
     high_scores.personal_top_three.should eq([30, 20, 10])
   end
 
-  pending "Personal top when there is a tie" do
+  it "Personal top when there is a tie" do
     high_scores = HighScores.new([40, 20, 40, 30])
     high_scores.personal_top_three.should eq([40, 40, 30])
   end
 
-  pending "Personal top when there are less than 3" do
+  it "Personal top when there are less than 3" do
     high_scores = HighScores.new([30, 70])
     high_scores.personal_top_three.should eq([70, 30])
   end
 
-  pending "Personal top when there is only one" do
+  it "Personal top when there is only one" do
     high_scores = HighScores.new([40])
     high_scores.personal_top_three.should eq([40])
   end
 
-  pending "Latest score after personal top scores" do
+  it "Latest score after personal top scores" do
     high_scores = HighScores.new([70, 50, 20, 30])
     expected = 30
     high_scores.personal_top_three
     high_scores.latest.should eq expected
   end
 
-  pending "Scores after personal top scores" do
+  it "Scores after personal top scores" do
     high_scores = HighScores.new([30, 50, 20, 70])
     expected = [30, 50, 20, 70]
     high_scores.personal_top_three
     high_scores.scores.should eq expected
   end
 
-  pending "Latest score after personal best" do
+  it "Latest score after personal best" do
     high_scores = HighScores.new([20, 70, 15, 25, 30])
     expected = 30
     high_scores.personal_best
     high_scores.latest.should eq expected
   end
 
-  pending "Scores after personal best" do
+  it "Scores after personal best" do
     high_scores = HighScores.new([20, 70, 15, 25, 30])
     expected = [20, 70, 15, 25, 30]
     high_scores.personal_best
--- crystal/high-scores/src/high_scores.cr
+++ crystal/high-scores/src/high_scores.cr
@@ -1,3 +1,14 @@
 class HighScores
-  # Write your code for the 'HighScores' exercise in this file.
+  getter scores
+  def initialize(@scores : Array(Int32))
+  end
+  def latest
+    @scores.last
+  end
+  def personal_best
+    @scores.max
+  end
+  def personal_top_three
+    @scores.sort.reverse[0..2]
+  end
 end
