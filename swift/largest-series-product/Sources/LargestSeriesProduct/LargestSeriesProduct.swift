a0742632abf5f8403b168dbb723b371a4527d239
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-03 07:50:10 -0500

    lsp

--- swift/largest-series-product/Sources/LargestSeriesProduct/LargestSeriesProduct.swift
+++ swift/largest-series-product/Sources/LargestSeriesProduct/LargestSeriesProduct.swift
@@ -1,1 +1,45 @@
-//Solution goes in Sources
+struct NumberSeries {
+
+    enum NumberSeriesError: Error {
+        case spanLongerThanStringLength
+        case invalidCharacter
+        case negativeSpan
+    }
+
+    let string: String
+
+    init(_ string: String) {
+        self.string = string
+    }
+
+    func largestProduct(_ span: Int) throws -> Int {
+
+        let windows = try string.windows(span)
+        let largest = windows.map { $0.reduce(1, *) }.max()
+
+        return largest ?? 1
+    }
+}
+
+extension String {
+
+    func windows(_ span: Int) throws -> [[Int]] {
+
+        guard span >= 0 else {
+            throw NumberSeries.NumberSeriesError.negativeSpan
+        }
+        guard span <= self.count else {
+            throw NumberSeries.NumberSeriesError.spanLongerThanStringLength
+        }
+        guard allSatisfy({ $0.isNumber }) else {
+            throw NumberSeries.NumberSeriesError.invalidCharacter
+        }
+        let lst = self.compactMap {
+            $0.wholeNumberValue
+        }
+        return (0...lst.count - span).map {
+            Array(lst[$0..<$0 + span])
+        }
+    }
+
+}
