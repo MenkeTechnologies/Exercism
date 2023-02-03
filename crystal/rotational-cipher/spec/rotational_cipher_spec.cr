7080c7e7aa3d4397b9c3973d425c7828803076cc
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-03 07:57:22 -0500

    rot

--- crystal/rotational-cipher/spec/rotational_cipher_spec.cr
+++ crystal/rotational-cipher/spec/rotational_cipher_spec.cr
@@ -6,39 +6,39 @@ describe "RotationalCipher" do
     RotationalCipher.rotate("a", 0).should eq("a")
   end
 
-  pending "rotate a by 1" do
+  it "rotate a by 1" do
     RotationalCipher.rotate("a", 1).should eq("b")
   end
 
-  pending "rotate a by 26, same output as input" do
+  it "rotate a by 26, same output as input" do
     RotationalCipher.rotate("a", 26).should eq("a")
   end
 
-  pending "rotate m by 13" do
+  it "rotate m by 13" do
     RotationalCipher.rotate("m", 13).should eq("z")
   end
 
-  pending "rotate n by 13 with wrap around alphabet" do
+  it "rotate n by 13 with wrap around alphabet" do
     RotationalCipher.rotate("n", 13).should eq("a")
   end
 
-  pending "rotate capital letters" do
+  it "rotate capital letters" do
     RotationalCipher.rotate("OMG", 5).should eq("TRL")
   end
 
-  pending "rotate spaces" do
+  it "rotate spaces" do
     RotationalCipher.rotate("O M G", 5).should eq("T R L")
   end
 
-  pending "rotate numbers" do
+  it "rotate numbers" do
     RotationalCipher.rotate("Testing 1 2 3 testing", 4).should eq("Xiwxmrk 1 2 3 xiwxmrk")
   end
 
-  pending "rotate punctuation" do
+  it "rotate punctuation" do
     RotationalCipher.rotate("Let's eat, Grandma!", 21).should eq("Gzo'n zvo, Bmviyhv!")
   end
 
-  pending "rotate all letters" do
+  it "rotate all letters" do
     RotationalCipher.rotate("The quick brown fox jumps over the lazy dog.", 13).should eq("Gur dhvpx oebja sbk whzcf bire gur ynml qbt.")
   end
 end
--- crystal/rotational-cipher/src/rotational_cipher.cr
+++ crystal/rotational-cipher/src/rotational_cipher.cr
@@ -1,5 +1,14 @@
 module RotationalCipher
-  def self.rotate(plaintext : String, key : Number) : String
-    # Write your code for the 'Rotational Cipher' exercise in this file.
+  def self.rotate(s : String, key : Number)
+    s.each_char.map do |c|
+      if ('a'..'z').includes?(c)
+        ((c.ord - 'a'.ord + key) % 26 + 'a'.ord).chr
+      elsif ('A'..'Z').includes?(c)
+        ((c.ord - 'A'.ord + key) % 26 + 'A'.ord).chr
+      else
+        c
+      end
+    end.join
   end
 end
+
