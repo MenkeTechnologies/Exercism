9239eeb61f65211db36f0b4cafb2b9c7cc4c67b4
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-03 07:43:12 -0500

    nc

--- vlang/nucleotide-count/nucleotide-count.v
+++ vlang/nucleotide-count/nucleotide-count.v
@@ -1,5 +1,16 @@
 module main
 
 fn count_nucleotides(strand string) !map[string]int {
-
+	mut h := {
+		'A': 0
+		'C': 0
+		'G': 0
+		'T': 0
+	}
+	for c in strand.runes() {
+		v := h[c.str()] or { return error('${c} is not a valid nucleotide!') }
+		h[c.str()] = v + 1
+	}
+	return h
 }
+
