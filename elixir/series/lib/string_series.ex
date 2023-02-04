510fe77847b5c18cd8fc88876c88a37c74db8122
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-04 11:05:04 -0500

    series

--- elixir/series/lib/string_series.ex
+++ elixir/series/lib/string_series.ex
@@ -1,10 +1,9 @@
 defmodule StringSeries do
-  @doc """
-  Given a string `s` and a positive integer `size`, return all substrings
-  of that size. If `size` is greater than the length of `s`, or less than 1,
-  return an empty list.
-  """
-  @spec slices(s :: String.t(), size :: integer) :: list(String.t())
-  def slices(_s, _size) do
+  def slices(s, size) when size > 0 do
+    s
+    |> String.graphemes
+    |> Enum.chunk_every(size, 1, :discard)
+    |> Enum.map(&Enum.join(&1))
   end
+  def slices(_, _) do [] end
 end
