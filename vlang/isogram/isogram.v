12cc1ec78a48bb7a224d98e460db7307d09eb0a0
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-06 11:09:49 -0500

    iso

--- vlang/isogram/isogram.v
+++ vlang/isogram/isogram.v
@@ -1,4 +1,9 @@
 module main
+import regex
+import arrays
 
 fn is_isogram(word string) bool {
+	mut re := regex.regex_opt(r'\W') or { panic(err) }
+	clean_word := re.replace(word, '').to_lower()
+	return arrays.map_of_counts(clean_word.split('')).values().all(it == 1)
 }
