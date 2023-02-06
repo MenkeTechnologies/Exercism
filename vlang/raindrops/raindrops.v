44a4f749f85d32b9bdd9f13cebb5e6625d6e3a9d
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-06 10:58:51 -0500

    rd

--- vlang/raindrops/raindrops.v
+++ vlang/raindrops/raindrops.v
@@ -1,4 +1,10 @@
 module main
 
 fn raindrops(number int) string {
+	mut s := ''
+	if number % 3 == 0 {s += 'Pling'}
+	if number % 5 == 0 {s += 'Plang'}
+	if number % 7 == 0 {s += 'Plong'}
+	if s == '' {s = number.str()}
+	return s
 }
