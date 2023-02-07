5bc4564843c5863eeab272a04d311e200c0c18e3
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-07 11:02:31 -0500

    ayb

--- fsharp/all-your-base/AllYourBase.fs
+++ fsharp/all-your-base/AllYourBase.fs
@@ -1,3 +1,18 @@
 module AllYourBase
 
-let rebase digits inputBase outputBase = failwith "You need to implement this function."
+let fromDecNumber outputBase number =
+    let rec aux n digits =
+        match n / outputBase, n % outputBase with
+        | 0, modulus -> modulus :: digits
+        | x', modulus -> aux x' (modulus :: digits)
+    aux number []
+
+let rebase digits inputBase outputBase =
+    if inputBase <= 1 || outputBase <= 1 || (digits |> List.exists (fun d -> d < 0 || d >= inputBase)) then
+        None
+    elif digits.IsEmpty then
+        Some [ 0 ]
+    else
+        let sum = digits |> List.reduce (fun acc c -> acc * inputBase + c)
+
+        sum |> fromDecNumber outputBase |> Some
