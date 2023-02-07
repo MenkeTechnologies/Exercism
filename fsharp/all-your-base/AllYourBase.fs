e0d3f5c96375e14c741fdf74a47873f31196ec7c
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-07 11:05:40 -0500

    lsp

--- fsharp/all-your-base/AllYourBase.fs
+++ fsharp/all-your-base/AllYourBase.fs
@@ -4,11 +4,11 @@ let fromDecNumber outputBase number =
     let rec aux n digits =
         match n / outputBase, n % outputBase with
         | 0, modulus -> modulus :: digits
-        | x', modulus -> aux x' (modulus :: digits)
+        | remainder, modulus -> aux remainder (modulus :: digits)
     aux number []
 
 let rebase digits inputBase outputBase =
-    if inputBase <= 1 || outputBase <= 1 || (digits |> List.exists (fun d -> d < 0 || d >= inputBase)) then
+    if inputBase < 2 || outputBase < 2 || (digits |> List.exists (fun d -> d < 0 || d >= inputBase)) then
         None
     elif digits.IsEmpty then
         Some [ 0 ]
--- fsharp/largest-series-product/LargestSeriesProduct.fs
+++ fsharp/largest-series-product/LargestSeriesProduct.fs
@@ -1,3 +1,16 @@
 module LargestSeriesProduct
 
-let largestProduct input seriesLength : int option = failwith "You need to implement this function."
+open System
+
+let largestProduct (input: string) len: int option =
+    if not (Seq.forall Char.IsDigit input) || len < 0 || input.Length < len then
+        None
+    elif len = 0 then
+        Some 1
+    else
+        input
+        |> Seq.map (fun c -> c.ToString() |> int)
+        |> Seq.windowed len
+        |> Seq.map (Seq.reduce (*))
+        |> Seq.max
+        |> Some
