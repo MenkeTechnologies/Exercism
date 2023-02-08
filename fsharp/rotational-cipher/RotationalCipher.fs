13e0dba0dd18e1815add1255877dda787edf8388
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-08 10:48:44 -0500

    rotc

--- fsharp/rotational-cipher/RotationalCipher.fs
+++ fsharp/rotational-cipher/RotationalCipher.fs
@@ -1,3 +1,20 @@
 module RotationalCipher
 
-let rotate shiftKey text = failwith "You need to implement this function."
+open System
+
+let shiftChar baseChar (charToShift: char) shiftKey =
+    char ((((int charToShift - int baseChar) + shiftKey) % 26) + int baseChar)
+let shiftUpper = shiftChar 'A'
+let shiftLower = shiftChar 'a'
+
+let rotChar (shiftKey: int) ch =
+    match ch with
+    | c when not (Char.IsLetter c) -> c
+    | c when Char.IsUpper c -> shiftUpper c shiftKey
+    | c -> shiftLower c shiftKey
+
+let rotate shiftKey text =
+    text
+    |> Seq.map (rotChar shiftKey)
+    |> Seq.toArray
+    |> String
