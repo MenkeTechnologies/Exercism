4d14bd79627791578e259b9ad67d01c2565d89e5
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-08 10:31:08 -0500

    mb

--- fsharp/matching-brackets/MatchingBrackets.fs
+++ fsharp/matching-brackets/MatchingBrackets.fs
@@ -1,3 +1,13 @@
 module MatchingBrackets
+let brackets = Map [('(',')');('[',']');('{','}')]
 
-let isPaired input = failwith "You need to implement this function."
+let isPaired input = 
+    let rec aux stk input =
+        match stk, input with
+        | [], [] -> true
+        | _::_, [] -> false
+        | _, x::xs when Map.keys brackets |> Seq.contains x -> aux (x::stk) xs
+        | x::xs, y::ys when Map.values brackets |> Seq.contains y && Map.find x brackets = y -> aux xs ys
+        | _, x::_ when Map.values brackets |> Seq.contains x -> false
+        | _, _::xs -> aux stk xs
+    input |> Seq.toList |> aux []
