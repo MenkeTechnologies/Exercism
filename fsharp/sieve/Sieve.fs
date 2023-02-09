e83663b2b990926587e91e4176124b41496651e5
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-09 11:24:56 -0500

    sieve

--- fsharp/sieve/Sieve.fs
+++ fsharp/sieve/Sieve.fs
@@ -1,3 +1,10 @@
 ﻿module Sieve
 
-let primes limit = failwith "You need to implement this function."
+let filterMultiples xs divisor = xs |> List.filter (fun x -> x % divisor <> 0)
+
+let rec aux listx =
+    match listx with
+    | head :: _ -> head :: aux (filterMultiples listx head)
+    | [] -> []
+
+let primes limit = aux [ 2 .. limit ]
