e187d9627720ac62593b3343d0b7736df91a16dc
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-09 11:16:48 -0500

    sl

--- fsharp/sublist/Sublist.fs
+++ fsharp/sublist/Sublist.fs
@@ -1,5 +1,20 @@
 module Sublist
 
-type SublistType = Equal | Sublist | Superlist | Unequal
+type SublistType =
+    | Equal
+    | Sublist
+    | Superlist
+    | Unequal
 
-let sublist xs ys = failwith "You need to implement this function."
+let isSubList ys xs =
+    xs
+    |> List.isEmpty
+    || ys
+       |> List.windowed xs.Length
+       |> List.exists ((=) xs)
+
+let sublist xs ys =
+    if xs = ys then Equal
+    elif xs |> isSubList ys then Sublist
+    elif ys |> isSubList xs then Superlist
+    else Unequal
