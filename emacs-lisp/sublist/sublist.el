a9792773e38aae065a54f51c0ea212997accffbc
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-10 11:35:57 -0500

    subl

--- emacs-lisp/sublist/sublist.el
+++ emacs-lisp/sublist/sublist.el
@@ -1,11 +1,16 @@
-;;; sublist.el --- Sublist (exercism)  -*- lexical-binding: t; -*-
+(defun is-start-of (lst1 lst2)
+  (or (null lst1)
+      (and (not (null lst1))
+           (equal (car lst1) (car lst2))
+           (is-start-of (cdr lst1) (cdr lst2)))))
 
-;;; Commentary:
+(defun is-sublist-of (lst1 lst2)
+  (if (null lst2) (null lst1)
+    (or (is-start-of lst1 lst2) (is-sublist-of lst1 (cdr lst2)))))
 
-;;; Code:
-
-(defun list-classify (list1 list2)
-  (error "Delete this S-Expression and write your own implementation"))
-
-(provide 'sublist)
-;;; sublist.el ends here
+(defun list-classify (lst1 lst2)
+  (let ((is12 (is-sublist-of lst1 lst2))
+        (is21 (is-sublist-of lst2 lst1)))
+    (if is12
+        (if is21 :equal :sublist)
+      (if is21 :superlist :unequal))))
