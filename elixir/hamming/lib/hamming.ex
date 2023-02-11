57814d23fd09e57ed8e4f0c5428c503450ab8821
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-10 22:15:33 -0500

    ham

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -1,5 +1,4 @@
 defmodule Hamming do
-  @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
   def hamming_distance(s1, s2) do
     if length(s1) != length(s2) do
       {:error, "strands must be of equal length"}
@@ -7,7 +6,7 @@ defmodule Hamming do
       {:ok,
        s1
        |> Enum.zip(s2)
-       |> Enum.count(fn {n1, n2} -> n1 != n2 end)}
+       |> Enum.count(fn {c1, c2} -> c1 != c2 end)}
     end
   end
 end
