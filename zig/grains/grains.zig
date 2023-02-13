0a5f8edcbbf32ec3e482d1cdeb556c6107c7de22
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-13 10:00:12 -0500

    grains

--- zig/grains/grains.zig
+++ zig/grains/grains.zig
@@ -1,7 +1,11 @@
+const math = @import("std").math;
+pub const ChessboardError = error{
+    IndexOutOfBounds,
+};
 pub fn square(index: isize) ChessboardError!u64 {
-    @panic("please implement the square function");
+    if (index < 1 or index > 64) return ChessboardError.IndexOutOfBounds;
+    return math.powi(u64, 2, @intCast(u64, index) - 1) catch unreachable;
 }
-
 pub fn total() u64 {
-    @panic("please implement the total function");
+    return math.maxInt(u64);
 }
