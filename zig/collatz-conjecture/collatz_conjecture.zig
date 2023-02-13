4aaa8abf5a437b196fa0937561ec4b14bd8b87b9
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-13 10:04:29 -0500

    coll

--- zig/collatz-conjecture/collatz_conjecture.zig
+++ zig/collatz-conjecture/collatz_conjecture.zig
@@ -1,3 +1,11 @@
+pub const ComputationError = error{IllegalArgument};
 pub fn steps(number: isize) anyerror!usize {
-    @panic("please implement the steps function");
+    if (number < 1) return ComputationError.IllegalArgument;
+    var cnt: u32 = 0;
+    var dup = number;
+    while (dup != 1) {
+        if (@mod(dup, 2) == 1) dup = dup * 3 + 1 else dup = @divTrunc(dup, 2);
+        cnt += 1;
+    }
+    return cnt;
 }
