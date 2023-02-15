fc5566a85ee1a82084fd38a3859c7c77dc977e94
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-15 10:27:02 -0500

    sieve

--- r/sieve/sieve.R
+++ r/sieve/sieve.R
@@ -1,3 +1,9 @@
-sieve <- function(limit) {
-  
+sieve = function(limit) {
+  if (limit < 2) return(c())
+  primes = 2:limit
+  for (n in primes){
+    primes = primes[primes %% n != 0 | primes == n]
+  }
+  primes
 }
+
