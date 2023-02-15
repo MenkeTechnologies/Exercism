42f2498b8e2f306bbbf38d7d2a10dbfb3ea0c6c6
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-15 10:37:17 -0500

    lsp

--- r/largest-series-product/largest-series-product.R
+++ r/largest-series-product/largest-series-product.R
@@ -1,3 +1,12 @@
-largest_series_product <- function(digits, span){
-
+largest_series_product = function(digits, span){
+  if (span < 0) stop("negative span")
+  if (span == 0) return (1)
+  if (nchar(digits) < span) stop("span is larger than string len")
+  ary = as.integer( unlist(strsplit(digits, '')) )
+  max = 0
+  for (i in seq(length(ary) - span + 1)) {
+    curr = prod(ary[i:(i + span - 1)])
+    if (max < curr) { max = curr }
+  }
+  max
 }
