966255e5c6834a50ff86d08de8ce0a7316fa221a
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-15 22:14:42 -0500

    tri

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -2,19 +2,19 @@ export class Triangle {
     constructor(...sides) {
         this._sorted = sides.sort((a, b) => a - b)
         this._set = new Set(sides);
+        this._valid = this._sorted[0] > 0 && this._sorted[0] + this._sorted[1] >= this._sorted[2]
     }
 
-    valid = () => this._sorted[0] > 0 && this._sorted[0] + this._sorted[1] >= this._sorted[2]
 
     get isEquilateral() {
-        return this.valid() && this._set.size === 1
+        return this._valid && this._set.size === 1
     }
 
     get isIsosceles() {
-        return this.valid() && this._set.size <= 2
+        return this._valid && this._set.size <= 2
     }
 
     get isScalene() {
-        return this.valid() && this._set.size === 3
+        return this._valid && this._set.size === 3
     }
 }
