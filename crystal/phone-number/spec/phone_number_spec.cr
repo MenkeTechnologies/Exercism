7044afb0ce7eb44e8e1e7e1d0bca7a5b4114e628
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-18 09:18:31 -0500

    pn

--- crystal/phone-number/spec/phone_number_spec.cr
+++ crystal/phone-number/spec/phone_number_spec.cr
@@ -6,95 +6,95 @@ describe "PhoneNumber" do
     PhoneNumber.clean("(223) 456-7890").should eq("2234567890")
   end
 
-  pending "cleans numbers with dots" do
+  it "cleans numbers with dots" do
     PhoneNumber.clean("223.456.7890").should eq("2234567890")
   end
 
-  pending "cleans numbers with multiple spaces" do
+  it "cleans numbers with multiple spaces" do
     PhoneNumber.clean("223 456   7890   ").should eq("2234567890")
   end
 
-  pending "invalid when 9 digits" do
+  it "invalid when 9 digits" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("123456789")
     end
   end
 
-  pending "invalid when 11 digits does not start with a 1" do
+  it "invalid when 11 digits does not start with a 1" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("22234567890")
     end
   end
 
-  pending "valid when 11 digits and starting with 1" do
+  it "valid when 11 digits and starting with 1" do
     PhoneNumber.clean("12234567890").should eq("2234567890")
   end
 
-  pending "valid when 11 digits and starting with 1 even with punctuation" do
+  it "valid when 11 digits and starting with 1 even with punctuation" do
     PhoneNumber.clean("+1 (223) 456-7890").should eq("2234567890")
   end
 
-  pending "invalid when more than 11 digits" do
+  it "invalid when more than 11 digits" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("321234567890")
     end
   end
 
-  pending "invalid with letters" do
+  it "invalid with letters" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("523-abc-7890")
     end
   end
 
-  pending "invalid with punctuations" do
+  it "invalid with punctuations" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("523-@:!-7890")
     end
   end
 
-  pending "invalid if area code starts with 0" do
+  it "invalid if area code starts with 0" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("(023) 456-7890")
     end
   end
 
-  pending "invalid if area code starts with 1" do
+  it "invalid if area code starts with 1" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("(123) 456-7890")
     end
   end
 
-  pending "invalid if exchange code starts with 0" do
+  it "invalid if exchange code starts with 0" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("(223) 056-7890")
     end
   end
 
-  pending "invalid if exchange code starts with 1" do
+  it "invalid if exchange code starts with 1" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("(223) 156-7890")
     end
   end
 
-  pending "invalid if area code starts with 0 on valid 11-digit number" do
+  it "invalid if area code starts with 0 on valid 11-digit number" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("1 (023) 456-7890")
     end
   end
 
-  pending "invalid if area code starts with 1 on valid 11-digit number" do
+  it "invalid if area code starts with 1 on valid 11-digit number" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("1 (123) 456-7890")
     end
   end
 
-  pending "invalid if exchange code starts with 0 on valid 11-digit number" do
+  it "invalid if exchange code starts with 0 on valid 11-digit number" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("1 (223) 056-7890")
     end
   end
 
-  pending "invalid if exchange code starts with 1 on valid 11-digit number" do
+  it "invalid if exchange code starts with 1 on valid 11-digit number" do
     expect_raises(ArgumentError) do
       PhoneNumber.clean("1 (223) 156-7890")
     end
--- crystal/phone-number/src/phone_number.cr
+++ crystal/phone-number/src/phone_number.cr
@@ -1,5 +1,9 @@
 module PhoneNumber
-  def self.clean(phrase : String) : String
-    # Write your code for the 'Phone Number' exercise in this file.
+  def self.clean(s : String) : String
+    num = s.delete { |c| !c.ascii_number? }
+    num = num.delete_at 0 if num.size == 11 && num.starts_with?('1')
+    raise ArgumentError.new if num.size != 10 || num[0].to_i < 2 || num[3].to_i < 2
+    num
   end
 end
+
