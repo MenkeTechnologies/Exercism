f9e57799042d6934316534fba72bf7141f831728
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-19 09:32:17 -0500

    bs

--- zig/binary-search/binary_search.zig
+++ zig/binary-search/binary_search.zig
@@ -1,8 +1,27 @@
-// Take a look at the tests, you might have to change the function arguments
+pub const SearchError = error{
+    ValueAbsent,
+    EmptyBuffer,
+};
 
-pub fn binarySearch(
-    target: usize,
-    buffer: ?[]const usize
-) SearchError!usize {
-    @panic("please implement the binarySearch function");
+pub fn binarySearch(comptime T: type, target: usize, buffer: ?[]const T) SearchError!usize {
+    const buff = buffer orelse return SearchError.EmptyBuffer;
+
+    if (buff.len == 0) {
+        return SearchError.EmptyBuffer;
+    }
+
+    var lo: usize = 0;
+    var hi: usize = buff.len;
+
+    while (lo < hi) {
+        const mid = lo + ((hi - lo) / 2);
+        if (buff[mid] == target) {
+            return mid;
+        } else if (buff[mid] > target) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    return SearchError.ValueAbsent;
 }
