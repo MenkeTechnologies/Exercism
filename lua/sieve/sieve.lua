a2aa7f18cfd7e7f9b87fddb590616dd1809db00d
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-21 11:06:05 -0500

    sieve

--- lua/sieve/sieve.lua
+++ lua/sieve/sieve.lua
@@ -1,3 +1,13 @@
 return function(n)
-
+  local composites = {}
+  return coroutine.create(function()
+      for i = 2, n do
+        if not(composites[i]) then
+          coroutine.yield(i)
+          for j = i*i, n, i do
+            composites[j] = true
+          end
+        end
+      end
+  end)
 end
