c2cec159d33d7ef529cd119b8691f252015fea30
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-21 11:18:04 -0500

    subl

--- lua/sublist/sublist.lua
+++ lua/sublist/sublist.lua
@@ -1,3 +1,9 @@
 return function(sublist, list)
-
+    for i = 1, #list - #sublist + 1 do
+        local t = {}
+        for j = i, i + #sublist-1 do table.insert(t, list[j]) end
+        if table.concat(sublist) == table.concat(t) then return true end
+    end
+    return false
 end
+
