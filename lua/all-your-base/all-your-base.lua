256c2c1043c23efbde79322c3ea62d5bb93bb524
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-21 11:20:32 -0500

    ayb

--- lua/all-your-base/all-your-base.lua
+++ lua/all-your-base/all-your-base.lua
@@ -1,7 +1,29 @@
-local all_your_base = {}
-
-all_your_base.convert = function(from_digits, from_base)
-
+local function to_decimal(digits, base)
+  local acc = 0
+  for i, n in ipairs(digits) do
+    assert(n >= 0, 'negative digits are not allowed')
+    assert(n < base, 'digit out of range')
+    acc = acc * base + n
+  end
+  return math.floor(acc)
 end
-
-return all_your_base
+local function from_decimal(decimal, base)
+  local digits = {}
+  while decimal > 0 do
+    table.insert(digits, 1, (decimal % base))
+    decimal = decimal // base
+  end
+  return #digits > 0 and digits or { 0 }
+end
+return {
+  convert = function(from_digits, from_base)
+    return {
+      to = function(to_base)
+        assert(from_base > 1, 'invalid input base')
+        assert(to_base > 1, 'invalid output base')
+        local decimal = to_decimal(from_digits, from_base)
+        return from_decimal(decimal, to_base)
+      end
+    }
+  end
+}
