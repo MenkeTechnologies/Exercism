3746f8a9bc0b0d444781fa7daa597cfc627655e2
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-23 07:36:34 -0500

    sieve

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,1 +1,14 @@
 package sieve
+
+func Sieve(limit int) (primes []int) {
+	bools := make([]bool, limit+1)
+	for i := 2; i <= limit; i++ {
+		if !bools[i] {
+			primes = append(primes, i)
+			for j := i * 2; j <= limit; j = j + i {
+				bools[j] = true
+			}
+		}
+	}
+	return primes
+}
