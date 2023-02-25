e99e95094f6bbfc8036a723a406d8758d5357fd0
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-25 11:34:38 -0500

    series

--- erlang/series/src/series.erl
+++ erlang/series/src/series.erl
@@ -1,6 +1,12 @@
 -module(series).
-
 -export([slices/2]).
 
+slices(Length, Series) ->
+        case Length > length(Series) of
+            true -> error(badarg);
+            _ -> aux(Length, Series)
+    end.
 
-slices(_SliceLength, _Series) -> undefined.
+aux(Length, Series) when Length > length(Series) -> [];
+aux(Length, Series) -> [lists:sublist(Series, Length) | aux(Length, tl(Series))].
+
