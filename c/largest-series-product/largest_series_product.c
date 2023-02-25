6ed60a45d6a52ade57786f0a5d2211dd3bed77e4
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-24 19:34:07 -0500

    lsp

--- c/largest-series-product/largest_series_product.c
+++ c/largest-series-product/largest_series_product.c
@@ -1,1 +1,17 @@
 #include "largest_series_product.h"
+#include <string.h>
+#include <ctype.h>
+
+int64_t largest_series_product(char *series, size_t size) {
+    if (size > strlen(series)) return -1;
+    int64_t max = 0;
+    for (size_t i = 0; i < strlen(series) - size + 1; ++i) {
+        int64_t product = 1;
+        for (size_t j = i; j < i + size; ++j) {
+            if (!isdigit(series[j])) return -1;
+            product *= series[j] - '0';
+        }
+        if (product > max) max = product;
+    }
+    return max;
+}
--- c/largest-series-product/largest_series_product.h
+++ c/largest-series-product/largest_series_product.h
@@ -1,9 +1,6 @@
-#ifndef LARGEST_SERIES_PRODUCT_H
-#define LARGEST_SERIES_PRODUCT_H
+#pragma once
 
 #include <stddef.h>
 #include <stdint.h>
 
 int64_t largest_series_product(char *digits, size_t span);
-
-#endif
