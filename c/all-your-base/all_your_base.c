f18dfbae7b421d8e8525b6e22c353c4eeee2c220
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-24 19:46:44 -0500

    ayb

--- c/all-your-base/all_your_base.c
+++ c/all-your-base/all_your_base.c
@@ -1,1 +1,18 @@
 #include "all_your_base.h"
+#include <string.h>
+
+size_t rebase(int8_t *digits, int16_t input_base, int16_t output_base, size_t input_length) {
+    if (input_base < 2 || output_base < 2 || input_length == 0) return 0;
+    for (size_t i = 0; i < input_length; i++) if (digits[i] < 0 || digits[i] >= input_base) return 0;
+    size_t sum = 0;
+    for (size_t i = 0; i < input_length; ++i) sum = sum * input_base + digits[i];
+    
+    size_t length = 0;
+    do {
+        ++length;
+        memmove(&digits[1], digits, length - 1 * sizeof(int8_t));
+        digits[0] = (int8_t) (sum % (size_t) output_base);
+        sum /= (size_t) output_base;
+    } while (sum > 0);
+    return length;
+}
--- c/all-your-base/all_your_base.h
+++ c/all-your-base/all_your_base.h
@@ -1,6 +1,8 @@
-#ifndef ALL_YOUR_BASE_H
-#define ALL_YOUR_BASE_H
+#pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
 
 #define DIGITS_ARRAY_SIZE 64
 
-#endif
+size_t rebase(int8_t *digits, int16_t input_base, int16_t output_base, size_t input_length);
