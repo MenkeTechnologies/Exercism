1c3c808881fdaaa0ed304cf1949b2e6709484fe6
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-27 10:44:57 -0500

    luhn

--- erlang/luhn/src/luhn.erl
+++ erlang/luhn/src/luhn.erl
@@ -2,5 +2,34 @@
 
 -export([valid/1]).
 
+valid(Value) ->
+    Trimmed = string:trim(Value),
+    if length(Trimmed) > 1 ->
+        Reversed = lists:reverse(Trimmed),
+        aux(Reversed, false, 0);
+    true ->
+        false
+    end.
 
-valid(_Value) -> undefined.
+aux([$\s|Digits], Double, Sum) ->
+    aux(Digits, Double, Sum);
+aux([Digit|_], _, _) when Digit < $0; Digit > $9 ->
+    false;
+aux([Digit|Digits], true, Sum) ->
+    Integer = Digit - 48,
+    Result = double(Integer),
+    aux(Digits, false, Sum + Result);
+aux([Digit|Digits], false, Sum) ->
+    Integer = Digit - 48,
+    aux(Digits, true, Sum + Integer);
+aux([], _, Sum) ->
+    Sum rem 10 == 0.
+
+double(Value) ->
+    Integer = Value * 2,
+    if Integer > 9 ->
+        Integer - 9;
+    true ->
+        Integer
+    end.
+
