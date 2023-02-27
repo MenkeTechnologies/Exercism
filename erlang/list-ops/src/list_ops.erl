09e4e3bf08e70d623959e3ca0c375a89826eb819
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-27 11:46:07 -0500

    lo

--- erlang/list-ops/src/list_ops.erl
+++ erlang/list-ops/src/list_ops.erl
@@ -1,20 +1,36 @@
 -module(list_ops).
 
--export([append/2, concat/1, filter/2, length/1, map/2, foldl/3, foldr/3,
-	 reverse/1]).
+-export([append/2, concat/1, filter/2, length/1, map/2, foldl/3, foldr/3, reverse/1]).
 
-append(_List1, _List2) -> undefined.
+append([], List) -> List;
+append([X | Xs], List) -> [X | append(Xs, List)].
 
-concat(_List) -> undefined.
+concat([]) -> [];
+concat([X | Xs]) -> append(X, concat(Xs)).
 
-filter(_Function, _List) -> undefined.
+filter(_, []) -> [];
+filter(F, [X | Xs]) ->
+    case F(X) of
+      true -> [X | filter(F, Xs)];
+      _ -> filter(F, Xs)
+    end.
 
-length(_List) -> undefined.
+length([]) -> 0;
+length([_ | Xs]) -> 1 + list_ops:length(Xs).
 
-map(_Function, _List) -> undefined.
+map(_, []) -> [];
+map(F, [X | Xs]) ->
+    [F(X) | map(F, Xs)].
 
-foldl(_Function, _Start, _List) -> undefined.
+foldl(_, Acc, []) -> Acc;
+foldl(F, Acc, [X | Xs]) ->
+    foldl(F, F(X, Acc), Xs).
 
-foldr(_Function, _Start, _List) -> undefined.
+foldr(_, Acc, []) -> Acc;
+foldr(F, Acc, [X | Xs]) ->
+    F(X, foldr(F, Acc, Xs)).
 
-reverse(_List) -> undefined.
+reverse(List) -> reverse(List, []).
+reverse([], Acc) -> Acc;
+reverse([X | Xs], Acc) -> reverse(Xs, [X | Acc]).
+
