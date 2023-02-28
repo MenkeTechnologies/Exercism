089433ae7be0109043e248ca7645e4097fccb832
MenkeTechnologies <linux.dev25@gmail.com>
2023-02-28 11:09:08 -0500

    arm

--- zig/armstrong-numbers/armstrong_numbers.zig
+++ zig/armstrong-numbers/armstrong_numbers.zig
@@ -1,4 +1,16 @@
-pub fn isArmstrongNumber(num: u128) bool {
-    _ = num;
-    @compileError("please implement the isArmstrongNumber function");
+const math = @import("std").math;
+pub fn isArmstrongNumber(candidate: u128) bool {
+    var digitCount: u128 = 0;
+    var dup = candidate;
+    while (dup > 0) {
+        digitCount += 1;
+        dup /= 10;
+    }
+    var sum: u128 = 0;
+    dup = candidate;
+    while (dup > 0) {
+        sum += math.pow(u128, dup % 10, digitCount);
+        dup /= 10;
+    }
+    return sum == candidate;
 }
