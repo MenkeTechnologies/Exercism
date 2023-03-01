d1415761f3efb0eab9caad852ec00890d88dc0da
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-01 10:49:41 -0500

    iso

--- zig/isogram/isogram.zig
+++ zig/isogram/isogram.zig
@@ -1,3 +1,17 @@
-pub fn isIsogram(str: []const u8) bool {
-    @panic("please implement the isIsogram function");
+const toUpper = @import("std").ascii.toUpper;
+
+pub fn isIsogram(s: []const u8) bool {
+    var flags: u32 = 0;
+
+    for (s) |c| {
+        const upper = toUpper(c);
+        if ('A' <= upper and upper <= 'Z') {
+            const bit = @as(u32, 1) << @intCast(u5, upper - 'A');
+            if (bit & flags != 0) {
+                return false;
+            }
+            flags |= bit;
+        }
+    }
+    return true;
 }
