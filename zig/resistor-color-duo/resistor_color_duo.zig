255d51a613a2b894c03e80e7ce1b8d38e3482be2
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-01 10:52:46 -0500

    rcd

--- zig/resistor-color-duo/resistor_color_duo.zig
+++ zig/resistor-color-duo/resistor_color_duo.zig
@@ -1,3 +1,16 @@
+pub const ColorBand = enum {
+    black,
+    brown,
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    violet,
+    grey,
+    white,
+};
+
 pub fn colorCode(colors: []const ColorBand) anyerror!isize {
-    @panic("please implement the color_code function");
+    return @as(isize, 10) * @enumToInt(colors[0]) + @enumToInt(colors[1]);
 }
