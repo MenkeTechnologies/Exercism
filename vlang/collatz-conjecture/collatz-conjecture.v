8cdd8dc57d26e7884eb5d33359f5f27a8b6172a3
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-04 10:01:00 -0500

    coll

--- vlang/collatz-conjecture/collatz-conjecture.v
+++ vlang/collatz-conjecture/collatz-conjecture.v
@@ -1,4 +1,14 @@
 module main
 
 fn collatz(number int) !int {
+	if number < 1 {
+		return error('input number must be positive!')
+	}
+	mut counter := 0
+	mut dup := number
+	for dup != 1 {
+		counter++
+		dup = if dup % 2 == 0 { dup / 2 } else { 3 * dup + 1 }
+	}
+	return counter
 }
