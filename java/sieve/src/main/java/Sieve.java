2a66e773c301f97d925d447a9ed1624d6adb7aeb
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-04 09:44:11 -0500

    sieve

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -1,11 +1,27 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Sieve {
+    private final int maxPrime;
+
     Sieve(int maxPrime) {
-        throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+        this.maxPrime = maxPrime + 1;
     }
 
     List<Integer> getPrimes() {
-        throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+        boolean[] sieve = new boolean[maxPrime];
+        Arrays.fill(sieve, true);
+        sieve[0] = false;
+        sieve[1] = false;
+        for (int i = 2; i < Math.sqrt(maxPrime); ++i) {
+            if (sieve[i]) {
+                for (int j = i * i; j < maxPrime; j = j + i) {
+                    sieve[j] = false;
+                }
+            }
+        }
+        return IntStream.range(2, maxPrime).filter(i -> sieve[i]).boxed().collect(Collectors.toList());
     }
 }
