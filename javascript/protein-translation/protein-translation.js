b13b1708ecbcd8646eb771de08af3d717b0fdc10
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-05 10:19:03 -0500

    qa

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -1,3 +1,30 @@
-export const translate = () => {
-    throw new Error('Remove this statement and implement this function');
-};
+const codons = {
+  Methionine: ['AUG'],
+  Phenylalanine: ['UUU', 'UUC'],
+  Leucine: ['UUA', 'UUG'],
+  Serine: ['UCU', 'UCC', 'UCA', 'UCG'],
+  Tyrosine: ['UAU', 'UAC'],
+  Cysteine: ['UGU', 'UGC'],
+  Tryptophan: ['UGG'],
+  STOP: ['UAA', 'UAG', 'UGA']
+}
+
+function find(codon) {
+  for (const name in codons) {
+    if (codons[name].includes(codon)) {
+      return name
+    }
+  }
+  return false
+}
+
+export function translate(s) {
+  if (!s) return []
+  const names = s.match(/.{1,3}/g).map(find)
+  const stop = names.findIndex(n => n === 'STOP')
+  const final = stop === -1 ? names : names.slice(0, stop)
+  if (final.some(n => !n)) {
+    throw new Error('Invalid codon')
+  }
+  return final
+}
--- vlang/queen-attack/queen-attack.v
+++ vlang/queen-attack/queen-attack.v
@@ -1,4 +1,19 @@
 module main
 
 fn can_queen_attack(white string, black string) !bool {
+
+  pos2ints := fn (pos string)![]int {
+    invalid := error("${pos} is not a valid square")
+    if pos.len != 2 { return invalid}
+    r := [int(pos[0]) - 97, int(pos[1]) - 49]
+    if r[0] < 0||r[0] > 7||r[1] < 0||r[1] > 7 {return invalid}
+    return r
+  }
+  w := pos2ints(white) or {return err}
+  b := pos2ints(black) or {return err}
+  if white == black { return error("queens on same square")}
+  dx := w[0] - b[0]
+  dy := w[1] - b[1]
+
+  return dx == 0 || dy == 0 || dx == dy
 }
