c62a10e3c9cafe16b6f549fa654c7b70b24c8694
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-05 10:45:21 -0500

    nth

--- vlang/nth-prime/nth-prime.v
+++ vlang/nth-prime/nth-prime.v
@@ -1,5 +1,25 @@
 module main
 
-fn nth_prime(n int) !int {
+fn is_prime(target int, primes []int) bool {
+	for p in primes {
+		if target % p == 0 {
+			return false
+		}
+	}
+	return true
+}
 
+fn nth_prime(n int) !int {
+	if n < 1 {
+		return error('n must be greater than 0')
+	}
+	mut primes := [2]
+	mut curr := 3
+	for n > primes.len {
+		if is_prime(curr, primes) {
+			primes << curr
+		}
+		curr += 2
+	}
+	return primes[n - 1]
 }
--- vlang/prime-factors/prime-factors.v
+++ vlang/prime-factors/prime-factors.v
@@ -1,4 +1,13 @@
 module main
 
 fn prime_factors(n i64) []i64 {
+	mut dup := n
+	mut factors := []i64{}
+	for candidate := i64(2); dup > 1; candidate++ {
+		for ; dup % candidate == 0; dup /= candidate {
+			factors << candidate
+		}
+	}
+	return factors
 }
+
