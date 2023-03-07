08409e32b126d0a5750d0c7ad5439d0a5513ea4f
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-07 13:21:31 -0500

    sieve

--- vlang/sieve/sieve.v
+++ vlang/sieve/sieve.v
@@ -1,4 +1,15 @@
 module main
-
 fn sieve(limit int) []int {
+	mut primes := []int{len: limit - 1, init: it + 2}
+	for i, n in primes {
+		if n != 0 {
+			mut j := i + n
+			for j < limit - 1 {
+				primes[j] = 0
+				j += n
+			}
+		}
+	}
+	return primes.filter(it > 0)
 }
+
