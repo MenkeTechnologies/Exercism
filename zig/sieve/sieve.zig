9940abec2a0878c245f0292f4c39c5c41de1954d
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-12 12:34:07 -0400

    sieve

--- zig/sieve/sieve.zig
+++ zig/sieve/sieve.zig
@@ -1,5 +1,19 @@
-pub fn primes(buffer: []u32, limit: u32) []u32 {
-    _ = buffer;
-    _ = limit;
-    @compileError("please implement the primes function");
+const std = @import("std");
+
+pub fn primes(buffer: []u32, comptime limit: u32) []u32 {
+    var sieve = std.StaticBitSet(limit + 1).initEmpty();
+    sieve.set(0);
+    sieve.set(1);
+    var idx: u32 = 0;
+    var n: u32 = 2;
+    while (n <= limit) : (n += 1) {
+        if (sieve.isSet(n)) continue;
+        buffer[idx] = n;
+        idx += 1;
+        var j = n * n;
+        while (j <= limit) : (j += n) {
+            sieve.set(j);
+        }
+    }
+    return buffer[0..idx];
 }
