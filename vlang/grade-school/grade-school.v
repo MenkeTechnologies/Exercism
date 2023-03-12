a3f294f83e6565d97fc421ad13c356d71ca8d4e6
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-11 21:14:30 -0500

    gs

--- vlang/grade-school/grade-school.v
+++ vlang/grade-school/grade-school.v
@@ -1,23 +1,37 @@
 module main
 
-// NOTE: there are multiple ways to accomplish this. To allow for automated
-// testing, please fill out whatever type you choose for the type of roster.
-// To make it easy to search, that has been replaced with <TYPE>. Good luck! :)
+type Roster = map[int][]string
 
-fn add_student(roster <TYPE>, name string, grade int) <TYPE> {
-
+fn add_student(roster Roster, name string, grade int) Roster {
+	for _, persons in roster {
+		if name in persons {
+			return roster
+		}
+	}
+	mut dup := roster
+	if name !in dup[grade] {
+		dup[grade] << name
+	}
+	return dup
 }
 
-fn get_students_in_grade(roster <TYPE>, grade int) []string {
-
+fn get_students_in_grade(roster Roster, grade int) []string {
+	mut persons_in_grade := roster[grade]
+	persons_in_grade.sort()
+	return persons_in_grade
+}
+fn get_all_students(roster Roster) []string {
+	mut all_students := []string{}
+	mut grades := roster.keys()
+	grades.sort()
+	for grade in grades {
+		all_students << get_students_in_grade(roster, grade)
+	}
+	return all_students
 }
 
-fn get_all_students(roster <TYPE>) []string {
-
+fn create_new_roster() Roster {
+	roster := Roster(map[int][]string{})
+	return roster
 }
 
-// This is a helper function that should return an 
-// empty roster (type of your choosing)
-fn create_new_roster() <TYPE> {
-	return <TYPE>
-}
