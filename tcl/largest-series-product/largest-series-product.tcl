dd598d20194898bbc887a5dd8b61c25533c1af02
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-13 11:36:44 -0400

    lsp

--- tcl/largest-series-product/largest-series-product.tcl
+++ tcl/largest-series-product/largest-series-product.tcl
@@ -1,3 +1,19 @@
 proc largestSeriesProduct {digits span} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    require {$span <= [string length $digits]} "span must be smaller than string length"
+    require {![regexp {\D} $digits]} "digits input must only contain digits"
+    require {$span >= 0} "span must not be negative"
+    set maxProd -Inf
+    set endIdx [expr {[string length $digits] - $span}]
+    for {set i 0; set j $span} {$i <= $endIdx} {incr i; incr j} {
+        set series [split [string range $digits $i $j-1] ""]
+        set product [::tcl::mathop::* {*}$series]
+        set maxProd [expr {max($maxProd, $product)}]
+    }
+    return $maxProd
 }
+proc require {condition errMsg} {
+    if {![uplevel 1 [list expr $condition]]} {
+        error $errMsg
+    }
+}
+
