ab98f5c247bee9f933816686ce0d820dfa39db5f
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-13 11:39:13 -0400

    prot

--- tcl/protein-translation/protein-translation.tcl
+++ tcl/protein-translation/protein-translation.tcl
@@ -1,3 +1,29 @@
 proc proteins {strand} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set aminoacids {
+        AUG	Methionine
+        UUU	Phenylalanine
+        UUC	Phenylalanine
+        UUA	Leucine
+        UUG	Leucine
+        UCU	Serine
+        UCC	Serine
+        UCA Serine
+        UCG	Serine
+        UAU	Tyrosine
+        UAC	Tyrosine
+        UGU	Cysteine
+        UGC	Cysteine
+        UGG	Tryptophan
+        UAA	STOP
+        UAG	STOP
+        UGA	STOP
+    }
+    set output {}
+    foreach codon [regexp -all -inline {.{1,3}} $strand] {
+        if {![dict exists $aminoacids $codon]} {error "Invalid codon"}
+        set protein [dict get $aminoacids $codon]
+        if {$protein eq "STOP"} {break}
+        lappend output $protein
+    }
+    return $output
 }
