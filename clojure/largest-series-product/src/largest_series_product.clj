cc3ee533cb0f1b99943e7e7094125db900745990
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-14 10:40:05 -0400

    lsp

--- clojure/largest-series-product/src/largest_series_product.clj
+++ clojure/largest-series-product/src/largest_series_product.clj
@@ -1,5 +1,12 @@
 (ns largest-series-product)
 
-(defn largest-product [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn largest-product [n s]
+  (assert (or (not (empty? s))
+              (zero? n)))
+  (if (empty? s)
+    1
+    (->> s
+         (map #(-> % str Integer/parseInt))
+         (partition n 1)
+         (map #(reduce * %))
+         (apply max))))
