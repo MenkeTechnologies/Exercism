0fabffb02b1b9227f80d36e445960535de4008a0
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-15 11:22:36 -0400

    pt

--- lua/protein-translation/protein-translation.lua
+++ lua/protein-translation/protein-translation.lua
@@ -1,12 +1,34 @@
-local function translate_codon(codon)
-
+local codon_pairs = {
+  AUG = 'Methionine',
+  UUU = 'Phenylalanine',
+  UUC = 'Phenylalanine',
+  UUA = 'Leucine',
+  UUG = 'Leucine',
+  UCU = 'Serine',
+  UCC = 'Serine',
+  UCA = 'Serine',
+  UCG = 'Serine',
+  UAU = 'Tyrosine',
+  UAC = 'Tyrosine',
+  UGU = 'Cysteine',
+  UGC = 'Cysteine',
+  UGG = 'Tryptophan',
+  UAA = 'STOP',
+  UAG = 'STOP',
+  UGA = 'STOP',
+}
+local function codon(candidate)
+  return (codon_pairs[candidate] or assert(false, 'unknown codon'))
 end
-
-local function translate_rna_strand(rna_strand)
-
+return{
+  codon = codon,
+  rna_strand = function (sequence)
+  local protein = {}
+  for i=1, #sequence, 3 do
+    local translated = codon(sequence:sub(i, i+2))
+    if translated == 'STOP' then break end
+    table.insert(protein, translated)
+  end
+  return protein
 end
-
-return {
-  codon = translate_codon,
-  rna_strand = translate_rna_strand
 }
