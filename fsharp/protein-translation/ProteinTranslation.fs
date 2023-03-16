433b8b5db0fee1f68c8643190e3dc2233e51dbd8
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-16 10:42:47 -0400

    prot

--- fsharp/protein-translation/ProteinTranslation.fs
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -1,3 +1,19 @@
 module ProteinTranslation
-
-let proteins rna = failwith "You need to implement this function."
+let mapping =
+    function
+    | "AUG"                         -> "Methionine"
+    | "UUU" | "UUC"                 -> "Phenylalanine"
+    | "UUA" | "UUG"                 -> "Leucine"
+    | "UCU" | "UCC" | "UCA" | "UCG" -> "Serine"
+    | "UAU" | "UAC"                 -> "Tyrosine"
+    | "UGU" | "UGC"                 -> "Cysteine"
+    | "UGG"                         -> "Tryptophan"
+    | "UAA" | "UAG" | "UGA"         -> "STOP"
+    | codon                             -> failwith $"Unknown codon {codon}"
+    
+let proteins s =
+    s
+    |> Seq.chunkBySize 3
+    |> Seq.map (System.String >> mapping)
+    |> Seq.takeWhile ((<>) "STOP")
+    |> Seq.toList
