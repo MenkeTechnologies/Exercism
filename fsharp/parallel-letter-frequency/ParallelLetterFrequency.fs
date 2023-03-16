18cd48a2474ae9198ea423f52ce6b0d17f8e4b14
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-16 10:46:44 -0400

    plf

--- fsharp/parallel-letter-frequency/ParallelLetterFrequency.fs
+++ fsharp/parallel-letter-frequency/ParallelLetterFrequency.fs
@@ -1,3 +1,23 @@
 module ParallelLetterFrequency
 
-let frequency texts = failwith "You need to implement this function."
+open System
+
+let private countFrequencies (text: string) =
+    text.ToLowerInvariant()
+    |> Seq.filter Char.IsLetter
+    |> Seq.countBy id
+    |> Map.ofSeq
+
+let mergeMap map =
+    map
+    |> Map.fold (fun acc k v ->
+        match Map.tryFind k acc with
+        | Some v' -> acc.Add(k, v + v')
+        | None -> acc.Add(k, v))
+
+let frequency input =
+    input
+    |> Seq.map (fun text -> async { return countFrequencies text })
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Array.fold mergeMap Map.empty
