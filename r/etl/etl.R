0593bad3cce75cc048acf2330e8234f15c05464a
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-18 18:22:15 -0400

    etl

--- r/etl/etl.R
+++ r/etl/etl.R
@@ -1,3 +1,8 @@
-etl <- function(input) {
-
+etl = function(dict) {
+    res = list()
+    for (k in names(dict)) {
+        res[tolower(dict[[k]])] = as.integer(k)
+    }
+    res[order(names(res))]
 }
+
