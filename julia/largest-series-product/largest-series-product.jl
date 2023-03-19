45b2033377bf31961c05347c5766b03519aa0446
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-19 12:23:03 -0400

    lsp

--- julia/largest-series-product/largest-series-product.jl
+++ julia/largest-series-product/largest-series-product.jl
@@ -1,2 +1,7 @@
-function largest_product(str, span)
+function largest_product(s, window)
+    if window > length(s) || window < 0 || (length(s) == 0 && window > 0)
+        throw(ArgumentError("Invalid window argument"))
+    end
+    lst = [parse(Int, c) for c in s]
+    maximum(prod(lst[i .+ (1:window)]) for i in 0:(length(s) - window))
 end
