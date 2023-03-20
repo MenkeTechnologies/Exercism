c289df08cf85ae0734cc8497e76d9a0e1fb4e2e5
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-20 11:29:23 -0400

    ayb

--- elm/all-your-base/src/AllYourBase.elm
+++ elm/all-your-base/src/AllYourBase.elm
@@ -1,6 +1,23 @@
 module AllYourBase exposing (rebase)
 
-
-rebase : Int -> List Int -> Int -> Maybe (List Int)
+isInvalid inBase digits outBase = 
+    inBase < 2 
+    || outBase < 2
+    || List.any (\d -> d < 0 || d >= inBase) digits
+    || List.all (\d -> d == 0) digits
+    
+digitsToInt inBase digits = digits |> List.foldl (\n acc -> acc * inBase + n) 0
+    
+intToDigits acc base num =
+    if num == 0 then acc
+    else 
+        let reminder = modBy base num
+            quotient = num // base
+        in intToDigits (reminder::acc) base quotient
+        
 rebase inBase digits outBase =
-    Debug.todo "Please implement this function"
+    if isInvalid inBase digits outBase then Nothing
+    else digits
+    |> digitsToInt inBase
+    |> intToDigits [] outBase
+    |> Just
