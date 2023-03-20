ac5cdecd6976e1dcb38599b293753b66daf6d6df
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-20 11:36:12 -0400

    lsp

--- elm/largest-series-product/src/LargestSeriesProduct.elm
+++ elm/largest-series-product/src/LargestSeriesProduct.elm
@@ -1,6 +1,20 @@
 module LargestSeriesProduct exposing (largestProduct)
+import String
+import List
 
-
-largestProduct : Int -> String -> Maybe Int
 largestProduct length series =
-    Debug.todo "Please implement this function"
+    let
+        numbersMaybe = String.toList series |> List.map (String.fromChar >> String.toInt)
+        numbers = List.map (Maybe.withDefault 0) numbersMaybe
+    in
+        if length < 0 || List.any (\n -> n == Nothing) numbersMaybe then
+            Nothing
+        else
+            List.length numbers - length |>
+            List.range 0
+            |> List.map (\i ->
+                List.drop i numbers |> List.take length |> List.foldr (*) 1
+            )
+            |> List.sort
+            |> List.reverse
+            |> List.head
