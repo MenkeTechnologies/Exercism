ff88e667afc778d900e6632f63e1c52381b52dde
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-21 10:49:17 -0400

    sublist

--- elm/sublist/src/Sublist.elm
+++ elm/sublist/src/Sublist.elm
@@ -1,13 +1,23 @@
 module Sublist exposing (ListComparison(..), sublist)
 
-
 type ListComparison
     = Equal
     | Superlist
     | Sublist
     | Unequal
 
+isSublist alist blist =
+    if List.length blist < List.length alist then
+        Unequal
+    else
+        if alist == List.take (List.length alist) blist then
+            Sublist
+        else
+            isSublist alist (List.drop 1 blist)
 
-sublist : List a -> List a -> ListComparison
 sublist alist blist =
-    Debug.todo "Please implement this function"
+    case (isSublist alist blist, isSublist blist alist) of
+            (Unequal, Unequal) -> Unequal
+            (Sublist, Unequal) -> Sublist
+            (Unequal, Sublist) -> Superlist
+            _ -> Equal
