d6abcf2ee54abe8fb2d25338263c0b8baecf8791
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-22 10:53:55 -0400

    plf

--- csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
+++ csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public static class ParallelLetterFrequency
 {
-    public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
-    {
-        throw new NotImplementedException();
-    }
+    public static Dictionary<char, int> Calculate(IEnumerable<string> texts) =>
+        texts
+            .AsParallel()
+            .SelectMany(text => text.ToLower().Select(c => c))
+            .Where(char.IsLetter)
+            .GroupBy(c => c)
+            .ToDictionary(c => c.Key, c => c.Count());
 }
