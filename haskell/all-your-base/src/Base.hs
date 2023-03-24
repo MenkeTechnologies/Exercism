6650c3f5b5b5bed492790d805f5c1781adadc5fb
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-24 11:30:43 -0400

    ayb

--- haskell/all-your-base/src/Base.hs
+++ haskell/all-your-base/src/Base.hs
@@ -1,7 +1,20 @@
-module Base (Error(..), rebase) where
+module Base (Error (..), rebase) where
+import Data.List (find)
 
 data Error a = InvalidInputBase | InvalidOutputBase | InvalidDigit a
-    deriving (Show, Eq)
+  deriving (Show, Eq)
 
 rebase :: Integral a => a -> a -> [a] -> Either (Error a) [a]
-rebase inputBase outputBase inputDigits = error "You need to implement this function."
+rebase inputBase outputBase inputDigits
+  | inputBase < 2 = Left InvalidInputBase
+  | outputBase < 2 = Left InvalidOutputBase
+  | otherwise = case find (\n -> n < 0 || n >= inputBase) inputDigits of
+      Just n -> Left $ InvalidDigit n
+      Nothing -> Right $ to_base (to_int inputDigits inputBase 0) outputBase []
+ where
+  to_base 0 _ _ = []
+  to_base n outputBase acc
+    | n < outputBase = n : acc
+    | otherwise = to_base (n `div` outputBase ) outputBase ((n `rem` outputBase ) : acc)
+  to_int [] _ acc = acc
+  to_int (d : tl) inputBase acc = to_int tl inputBase (acc * inputBase + d)
