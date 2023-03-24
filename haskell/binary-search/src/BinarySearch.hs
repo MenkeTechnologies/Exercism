d9803d8545a008e3968ab0bee440ad0ceacc7de0
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-24 11:43:11 -0400

    bs

--- haskell/binary-search/src/BinarySearch.hs
+++ haskell/binary-search/src/BinarySearch.hs
@@ -1,6 +1,14 @@
 module BinarySearch (find) where
-
 import Data.Array
 
 find :: Ord a => Array Int a -> a -> Maybe Int
-find array x = error "You need to implement this function."
+find ary target = aux (bounds ary) ary target
+
+aux :: Ord a => (Int, Int) -> Array Int a -> a -> Maybe Int
+aux (lo, hi) ary target
+  | lo > hi = Nothing
+  | otherwise = case compare target (ary ! mid) of
+                    GT -> aux (mid + 1, hi) ary target
+                    LT -> aux (lo, mid - 1) ary target
+                    EQ -> Just mid
+  where mid = (lo + hi) `div` 2
