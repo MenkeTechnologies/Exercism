66d04fc35d495d7f58f09003b60ff9dd8a14c57a
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-24 11:57:13 -0400

    prot

--- haskell/protein-translation/src/ProteinTranslation.hs
+++ haskell/protein-translation/src/ProteinTranslation.hs
@@ -1,4 +1,20 @@
 module ProteinTranslation(proteins) where
+import Data.Maybe (isJust)
 
 proteins :: String -> Maybe [String]
-proteins = error "You need to implement this function!"
+proteins = sequence . takeWhile isJust . map translate . take3
+
+translate :: String -> Maybe String
+translate c
+  | c == "AUG"                            = Just "Methionine"
+  | c `elem` ["UUU", "UUC"]               = Just "Phenylalanine"
+  | c `elem` ["UUA", "UUG"]               = Just "Leucine"
+  | c `elem` ["UCU", "UCC", "UCA", "UCG"] = Just "Serine"
+  | c `elem` ["UAU", "UAC"]               = Just "Tyrosine"
+  | c `elem` ["UGU", "UGC"]               = Just "Cysteine"
+  | c == "UGG"                            = Just "Tryptophan"
+  | otherwise                             = Nothing
+
+take3 :: [a] -> [[a]]
+take3 [] = []
+take3 a = take 3 a : take3 (drop 3 a)
