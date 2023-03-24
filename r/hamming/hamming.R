e162b0b03fbbd43ab4004db9352c5113d034949e
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-24 11:14:06 -0400

    ham

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -1,28 +1,7 @@
-# This is a stub function to take two strings
-# and calculate the hamming distance
-hamming =function(strand1, strand2) {
-  
-  
-  if (nchar(strand1) != nchar(strand2)) {
-    stop("This is an error message")
+hamming = function(s1, s2) {
+  if (nchar(s1) != nchar(s2)) {
+    stop("the two strands must be of equal length")
+  } else {
+    sum(charToRaw(s1) != charToRaw(s2))
   }
-  if (nchar(strand1) == 0 || nchar(strand2) == 0) {
-    return(0)
-  }
-  cnt = 0
-  i=1
-  
-  v1 = unlist(strsplit(strand1, ""))
-  v2 = unlist(strsplit(strand2, ""))
-  
-  for (ch1 in v1){
-    ch2 = v2[i]
-    if (ch1 != ch2 ){
-      cnt = cnt + 1
-    }
-    i = i + 1
-  }
-  
-  
-  cnt
 }
