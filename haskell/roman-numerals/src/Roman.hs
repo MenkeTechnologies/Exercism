49158464984552ce06f9c1b8d8ac376941af461f
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-26 15:28:30 -0400

    roman

--- haskell/roman-numerals/src/Roman.hs
+++ haskell/roman-numerals/src/Roman.hs
@@ -1,4 +1,26 @@
 module Roman (numerals) where
 
+nums :: [(String, Integer)]
+nums = [ ("M", 1000)
+       , ("CM", 900)
+       , ("D", 500)
+       , ("CD", 400)
+       , ("C", 100)
+       , ("XC", 90)
+       , ("L", 50)
+       , ("XL", 40)
+       , ("X", 10)
+       , ("IX", 9)
+       , ("V", 5)
+       , ("IV", 4)
+       , ("I", 1)
+       ]
+
+encode :: Integer -> String
+encode 0 = ""
+encode n = k ++ encode (n - v)
+    where (k, v) = head $ filter ((<= n).snd) nums
+
 numerals :: Integer -> Maybe String
-numerals n = error "You need to implement this function."
+numerals n | n < 0 = Nothing
+           | otherwise = Just $ encode n
