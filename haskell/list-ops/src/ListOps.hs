4f0d7a5de81dd8657568f82ee901cbb954416c2f
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-26 15:31:05 -0400

    lo

--- haskell/list-ops/src/ListOps.hs
+++ haskell/list-ops/src/ListOps.hs
@@ -1,37 +1,30 @@
 module ListOps
-  ( length
-  , reverse
-  , map
-  , filter
-  , foldr
-  , foldl'
-  , (++)
-  , concat
-  ) where
-
+  ( length , reverse , map , filter , foldr , foldl' , (++) , concat) where
 import Prelude hiding
   ( length, reverse, map, filter, foldr, (++), concat )
 
 foldl' :: (b -> a -> b) -> b -> [a] -> b
-foldl' f z xs = error "You need to implement this function."
+foldl' _ acc []     = acc
+foldl' f acc (x:xs) = let acc' = f acc x in acc' `seq` foldl' f acc' xs
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr f z xs = error "You need to implement this function."
+foldr _ acc []     = acc
+foldr f acc (x:xs) = f x (foldr f acc xs)
 
 length :: [a] -> Int
-length xs = error "You need to implement this function."
+length = foldl' (\acc _ -> acc + 1) 0
 
 reverse :: [a] -> [a]
-reverse xs = error "You need to implement this function."
+reverse = foldl' (flip (:)) []
 
 map :: (a -> b) -> [a] -> [b]
-map f xs = error "You need to implement this function."
+map f = foldr (\x acc -> f x : acc) []
 
 filter :: (a -> Bool) -> [a] -> [a]
-filter p xs = error "You need to implement this function."
+filter f = foldr (\x acc -> if f x then x:acc else acc) []
 
 (++) :: [a] -> [a] -> [a]
-xs ++ ys = error "You need to implement this function."
+xs ++ ys = foldr (:) ys xs
 
 concat :: [[a]] -> [a]
-concat xss = error "You need to implement this function."
+concat = foldr (++) []
