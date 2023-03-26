b5c1b7463060a60dbb6f02fa6b48ce670dd22234
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-26 14:35:16 -0400

    rn

--- haskell/robot-name/src/Robot.hs
+++ haskell/robot-name/src/Robot.hs
@@ -1,18 +1,38 @@
 module Robot (Robot, initialState, mkRobot, resetName, robotName) where
 
-import Control.Monad.State (StateT)
+import Control.Monad.State (StateT, MonadIO (liftIO), get, put)
+import Data.IORef (IORef, newIORef, readIORef, writeIORef )
+import Data.Set (Set)
+import qualified Data.Set as Set
+import System.Random (randomRIO)
 
-data Robot = Dummy1
-data RunState = Dummy2
+type Robot = IORef String
+type RunState = Set String
 
 initialState :: RunState
-initialState = error "You need to implement this function"
+initialState = Set.empty
 
 mkRobot :: StateT RunState IO Robot
-mkRobot = error "You need to implement this function."
+mkRobot = do
+  name  <- liftIO mkName
+  cache <- get
+  put $ Set.insert name cache
+  if Set.member name cache then mkRobot
+  else liftIO $ newIORef name
 
 resetName :: Robot -> StateT RunState IO ()
-resetName robot = error "You need to implement this function."
+resetName robot = do
+  new <- mkRobot
+  liftIO $ readIORef new >>= writeIORef robot
 
 robotName :: Robot -> IO String
-robotName robot = error "You need to implement this function."
+robotName = readIORef
+
+mkName :: IO String
+mkName = do
+  c1 <- randomRIO ('A', 'Z')
+  c2 <- randomRIO ('A', 'Z')
+  i1 <- randomRIO ('0', '9')
+  i2 <- randomRIO ('0', '9')
+  i3 <- randomRIO ('0', '9')
+  return [c1, c2, i1, i2, i3]
