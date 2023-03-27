bd077e89c54171dc27082759ee55fa1434df3b68
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-27 12:08:18 -0400

    sieve

--- elixir/sieve/lib/sieve.ex
+++ elixir/sieve/lib/sieve.ex
@@ -1,8 +1,14 @@
 defmodule Sieve do
-  @doc """
-  Generates a list of primes up to a given limit.
-  """
-  @spec primes_to(non_neg_integer) :: [non_neg_integer]
+  def primes_to(1), do: []
+
   def primes_to(limit) do
+    2..limit |> Enum.to_list() |> sieve(limit, [])
+  end
+
+  defp sieve([], _, acc), do: acc
+
+  defp sieve([next_prime | rest], limit, acc) do
+    to_remove = next_prime..limit//next_prime |> Enum.to_list()
+    (rest -- to_remove) |> sieve(limit, acc ++ [next_prime])
   end
 end
