1e6b3eeb4a707cc9c8037566f5c80948d91a28e0
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-28 12:54:29 -0400

    subl

--- crystal/sublist/src/sublist.cr
+++ crystal/sublist/src/sublist.cr
@@ -5,10 +5,11 @@ module Sublist
     Sublist
     Superlist
   end
-  def self.classify(list1 : Array(Int32), list2 : Array(Int32)) : Classification
-    list1 = as_string(list1)
-    list2 = as_string(list2)
-    case {list1.includes?(list2), list2.includes?(list1)}
+
+  def self.classify(lst1 : Array(Int32), lst2 : Array(Int32)) : Classification
+    lst1 = as_string(lst1)
+    lst2 = as_string(lst2)
+    case {lst1.includes?(lst2), lst2.includes?(lst1)}
     when {false, false}
       Classification::Unequal
     when {false, true}
@@ -20,6 +21,6 @@ module Sublist
     end
   end
   private def self.as_string(list : Array(Int32)) : String
-    list.map { |x| ":#{x}:" }.join
+    list.map { |x| "#{x}," }.join
   end
 end
