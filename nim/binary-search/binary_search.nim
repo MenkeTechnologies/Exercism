485625053ed7ab0fba4e98fa1ff1fb07ccd55452
MenkeTechnologies <linux.dev25@gmail.com>
2023-03-30 11:50:02 -0400

    bs

--- nim/binary-search/binary_search.nim
+++ nim/binary-search/binary_search.nim
@@ -1,2 +1,9 @@
 proc binarySearch*(a: openArray[int], val: int): int =
-  discard
+  var (lo, hi, mid) = (0, a.len-1 ,0)
+  while lo <= hi:
+    mid = (lo+hi) div 2
+    if a[mid] == val: return mid
+    elif a[mid] < val: lo = mid+1
+    else: hi = mid-1
+  return -1
+
