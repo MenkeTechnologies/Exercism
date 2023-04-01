bc0c56b035a89b99a82a9548a5c5cfbe668aec0b
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-01 12:22:42 -0400

    proverb

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using System.Linq;
 
 public static class Proverb
 {
     public static string[] Recite(string[] subjects)
-    {
-        throw new NotImplementedException();
-    }
+        => subjects.Length == 0
+            ? subjects
+            : Enumerable.Range(0, subjects.Length - 1)
+                .Select(i => string.Format($"For want of a {subjects[i]} the {subjects[i + 1]} was lost."))
+                .Concat(new[] { $"And all for the want of a {subjects[0]}." }).ToArray();
 }
