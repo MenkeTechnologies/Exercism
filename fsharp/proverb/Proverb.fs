b70ef0097e933d721f36b7940f630c70b1ddce59
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-02 12:04:41 -0400

    pro

--- fsharp/proverb/Proverb.fs
+++ fsharp/proverb/Proverb.fs
@@ -1,3 +1,9 @@
 module Proverb
-
-let recite (input: string list): string list = failwith "You need to implement this function."
+let recite (input: string list) = 
+    match input with
+    | [] -> []
+    | [one] -> [$"And all for the want of a {one}."]
+    | _ ->
+        let shiftedInput = input |> List.permute (fun x -> (x + List.length input - 1) % List.length input)
+        let shiftedOutput = shiftedInput |> List.map2 (fun x y -> $"For want of a {x} the {y} was lost.") input |> List.take (List.length input - 1)
+        [$"And all for the want of a {List.head input}."] |> List.append shiftedOutput
