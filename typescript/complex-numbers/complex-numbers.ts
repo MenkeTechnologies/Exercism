aced7772f497048d45ba415a43eab2cdec74d626
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-03 10:34:47 -0400

    cn

--- typescript/complex-numbers/complex-numbers.ts
+++ typescript/complex-numbers/complex-numbers.ts
@@ -1,41 +1,42 @@
 export class ComplexNumber {
-  constructor(real: unknown, imaginary: unknown) {
-    throw new Error('Remove this statement and implement this function')
-  }
+    constructor(private realn: number, private imaginary: number) {
+        this.realn = realn;
+        this.imaginary = imaginary;
+    }
 
-  public get real(): number {
-    throw new Error('Remove this statement and implement this function')
-  }
+    public get real(): number {
+        return this.realn
+    }
 
-  public get imag(): number {
-    throw new Error('Remove this statement and implement this function')
-  }
+    public get imag(): number {
+        return this.imaginary
+    }
 
-  public add(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
-  }
+    public add(o: ComplexNumber): ComplexNumber {
+        return new ComplexNumber(this.realn + o.realn, this.imaginary + o.imaginary)
+    }
 
-  public sub(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
-  }
+    public sub(o: ComplexNumber): ComplexNumber {
+        return new ComplexNumber(this.realn - o.realn, this.imaginary - o.imaginary)
+    }
 
-  public div(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
-  }
+    public div(o: ComplexNumber): ComplexNumber {
+        return new ComplexNumber((this.realn * o.realn + this.imaginary * o.imaginary) / (o.realn * o.realn + o.imaginary * o.imaginary), (this.imaginary * o.realn - this.realn * o.imaginary) / (o.realn * o.realn + o.imaginary * o.imaginary))
+    }
 
-  public mul(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
-  }
+    public mul(o: ComplexNumber): ComplexNumber {
+        return new ComplexNumber(this.realn * o.realn - this.imaginary * o.imaginary, this.imaginary * o.realn + this.realn * o.imaginary)
+    }
 
-  public get abs(): unknown {
-    throw new Error('Remove this statement and implement this function')
-  }
+    public get abs(): number {
+        return Math.sqrt(this.realn * this.realn + this.imaginary * this.imaginary)
+    }
 
-  public get conj(): unknown {
-    throw new Error('Remove this statement and implement this function')
-  }
+    public get conj(): ComplexNumber {
+        return new ComplexNumber(this.realn, this.imaginary ? this.imaginary * -1 : 0)
+    }
 
-  public get exp(): ComplexNumber {
-    throw new Error('Remove this statement and implement this function')
-  }
+    public get exp(): ComplexNumber {
+        return new ComplexNumber(Math.E ** this.realn * Math.cos(this.imaginary), Math.sin(this.imaginary))
+    }
 }
