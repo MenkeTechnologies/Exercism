bb94520e2f214cf842cbca111bbd7695f9a69afd
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-04 13:11:16 -0400

    allergies

--- zig/allergies/allergies.zig
+++ zig/allergies/allergies.zig
@@ -1,24 +1,25 @@
 const std = @import("std");
 const EnumSet = std.EnumSet;
 
-pub const Allergen = enum {
-    eggs,
-    peanuts,
-    shellfish,
-    strawberries,
-    tomatoes,
-    chocolate,
-    pollen,
-    cats,
+pub const Allergen = enum(u8) {
+    eggs = 1,
+    peanuts = 2,
+    shellfish = 4,
+    strawberries = 8,
+    tomatoes = 16,
+    chocolate = 32,
+    pollen = 64,
+    cats = 128,
 };
 
 pub fn isAllergicTo(score: u8, allergen: Allergen) bool {
-    _ = score;
-    _ = allergen;
-    @compileError("please implement the isAllergicTo function");
+    return @enumToInt(allergen) & score > 0;
 }
 
 pub fn initAllergenSet(score: usize) EnumSet(Allergen) {
-    _ = score;
-    @compileError("please implement the initAllergenSet function");
+    const enum_info = @typeInfo(Allergen).Enum;
+    const bit_len = enum_info.fields.len;
+    const tag_type = enum_info.tag_type;
+    const mask = std.bit_set.IntegerBitSet(bit_len){ .mask = @truncate(tag_type, score) };
+    return .{ .bits = mask };
 }
