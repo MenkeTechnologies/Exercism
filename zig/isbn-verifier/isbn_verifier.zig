775044a01cf978c8d0091186c6d2011707fdd0df
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-04 12:49:16 -0400

    isbn

--- zig/isbn-verifier/isbn_verifier.zig
+++ zig/isbn-verifier/isbn_verifier.zig
@@ -1,4 +1,22 @@
+const ascii = @import("std").ascii;
+
 pub fn isValidIsbn10(s: []const u8) bool {
-    _ = s;
-    @compileError("please implement the isValidIsbn10 function");
+    var total: usize = 0;
+    var i: usize = 0;
+    for (s) |c| {
+        if (c == '-') {
+            continue;
+        }
+        if (!ascii.isDigit(c)) {
+            if (i == 9 and c == 'X') {
+                total += 10;
+            } else {
+                return false;
+            }
+        } else {
+            total += (c - '0') * (10 - i);
+        }
+        i += 1;
+    }
+    return total % 11 == 0 and i == 10;
 }
