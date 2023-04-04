f0f4c244bec4cd91c4f4b0fc64980f2797b77206
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-04 13:02:16 -0400

    luhn

--- zig/luhn/luhn.zig
+++ zig/luhn/luhn.zig
@@ -1,4 +1,27 @@
-pub fn isValid(s: []const u8) bool {
-    _ = s;
-    @compileError("please implement the isValid function");
+const std = @import("std");
+
+pub fn isValid(comptime s: []const u8) bool {
+    comptime var replacementSize = std.mem.replacementSize(u8, s, " ", "");
+    var nows: [replacementSize]u8 = undefined;
+    _ = std.mem.replace(u8, s, " ", "", &nows);
+    std.mem.reverse(u8, &nows);
+
+    if (nows.len < 2) {
+        return false;
+    }
+
+    var sum: usize = 0;
+
+    for (nows) |c, i| {
+        var digit = std.fmt.charToDigit(c, 10) catch return false;
+        if (i % 2 == 1) {
+            digit *= 2;
+            if (digit > 9) {
+                digit -= 9;
+            }
+        }
+        sum += digit;
+    }
+
+    return sum % 10 == 0;
 }
