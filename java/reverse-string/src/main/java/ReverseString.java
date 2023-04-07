7a64024cccd4e6cffe70332c78006a5f6490f2e3
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-06 22:39:00 -0400

    rvs

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,10 @@
-class ReverseString {
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
+class ReverseString {
     String reverse(String inputString) {
-        return new StringBuilder(inputString).reverse().toString();
+        return inputString.chars()
+                .collect(() -> new ArrayList<Integer>(), (acc, n) -> acc.add(0, n), ArrayList::addAll)
+                .stream().map(Character::toString).collect(Collectors.joining());
     }
-  
 }
