82c0981eaf7280a09e70c9661fa5839da7b5c955
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-07 11:01:14 -0400

    sll

--- vlang/simple-linked-list/simple-linked-list.v
+++ vlang/simple-linked-list/simple-linked-list.v
@@ -1,37 +1,54 @@
 module main
 
 struct LinkedList {
-	// define your data structure here
+mut:
+    len int
+    head ?&ListNode = none
 }
-
+struct ListNode {
+    val int
+    node ?&ListNode = none
+}
 fn new() LinkedList {
-
+    return LinkedList{}
 }
-
 fn from_array(array []int) LinkedList {
-
+    mut lst := new()
+    for value in array { lst.push(value) }
+    return lst
 }
-
-fn (list LinkedList) is_empty() bool {
-
+fn (lst LinkedList) is_empty() bool {
+    return lst.len == 0
 }
-
-fn (mut list LinkedList) push(data int) {
-
+fn (mut lst LinkedList) push(data int) {
+    lst.head = &ListNode {
+        val: data
+        node: lst.head or { none }
+    }
+    lst.len++
 }
-
-fn (mut list LinkedList) pop() ?int {
-
+fn (mut lst LinkedList) pop() ?int {
+    data := lst.peek()?
+    lst.head = (*lst.head?).node?
+    lst.len--
+    return data
 }
-
-fn (list LinkedList) peek() ?int {
-
+fn (lst LinkedList) peek() ?int {
+    return (*lst.head?).val
 }
-
-fn (list LinkedList) to_array() []int {
-
+fn (lst LinkedList) to_array() []int {
+    mut dup := lst.reverse()
+    mut ary := []int{ cap: dup.len }
+    for _ in 0 .. 2 * dup.len + 1 {
+        ary << dup.pop() or { continue }
+    }
+    return ary
 }
-
-fn (list LinkedList) reverse() LinkedList {
-
+fn (lst LinkedList) reverse() LinkedList {
+    mut dup := lst
+    mut res := new()
+    for _ in 0 .. 2 * dup.len + 1 {
+        res.push(dup.pop() or { continue } )
+    }
+    return res
 }
