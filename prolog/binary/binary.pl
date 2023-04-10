163e236508bfab6c810697e9e2cc6c929a714514
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-10 09:24:08 -0400

    bin

--- prolog/binary/binary.pl
+++ prolog/binary/binary.pl
@@ -1,1 +1,11 @@
-binary(Str, Dec).
+binary("0", 0) :- !.
+binary("1", 1) :- !.
+binary(S, Dec) :-
+    string_length(S, Len),
+    Len >= 2,
+    LenMinusOne is Len - 1,
+    sub_string(S, 0, LenMinusOne, _, Rest),
+    sub_string(S, LenMinusOne, 1, _, Last),
+    binary(Last, LastDec),
+    binary(Rest, RestDec),
+    Dec is RestDec * 2 + LastDec.
