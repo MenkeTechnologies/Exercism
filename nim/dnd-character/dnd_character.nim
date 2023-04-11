6e19bcb68af79c32f9f77ebb7399b396679dfe53
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-11 10:39:09 -0400

    dnd

--- nim/dnd-character/dnd_character.nim
+++ nim/dnd-character/dnd_character.nim
@@ -1,30 +1,30 @@
-import math, random
-type
-  Character* = object
-    strength*, dexterity*, constitution*, intelligence*, wisdom*, charisma*, hitpoints*: int
+import std/[math, random]
+
+type Character* = object
+    strength*: int
+    dexterity*: int
+    wisdom*: int
+    charisma*: int
+    constitution*: int
+    intelligence*: int
+    hitPoints*: int
+
 randomize()
 
 proc ability*: int =
-  var
-    lowest = 7
-    throw: int
-  for i in 1..4:
-    throw = rand(1..6)
-    lowest = min(throw, lowest)
-    result += throw
-  result -= lowest
+  var rolls = [0, 0, 0, 0]
+  for roll in rolls.mitems:
+    roll = rand(1..6)
+  sum(rolls) - min(rolls)
 
-proc modifier*(n: int): int = ((n-10).floorDiv(2))
+func modifier*(n: int): int = floorDiv(n - 10, 2)
 
 proc initCharacter*: Character =
   let constitution = ability()
-  Character(
-              strength:ability(),
-              dexterity: ability(),
-              constitution: constitution,
-              intelligence: ability(),
-              wisdom: ability(),
-              charisma: ability(),
-              hitpoints: 10 + modifier(constitution)
-  )
-
+  Character(strength: ability(),
+    dexterity: ability(),
+    wisdom: ability(),
+    charisma: ability(),
+    intelligence: ability(),
+    constitution: constitution,
+    hitPoints: 10 + modifier(constitution))
