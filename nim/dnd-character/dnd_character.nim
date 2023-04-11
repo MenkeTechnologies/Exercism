b76413670cb09fe419091819312475f5a0d2e171
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-11 10:36:10 -0400

    yacht

--- nim/dnd-character/dnd_character.nim
+++ nim/dnd-character/dnd_character.nim
@@ -1,12 +1,30 @@
+import math, random
 type
   Character* = object
-    strength*: int
+    strength*, dexterity*, constitution*, intelligence*, wisdom*, charisma*, hitpoints*: int
+randomize()
 
 proc ability*: int =
-  discard
+  var
+    lowest = 7
+    throw: int
+  for i in 1..4:
+    throw = rand(1..6)
+    lowest = min(throw, lowest)
+    result += throw
+  result -= lowest
 
-proc modifier*(n: int): int =
-  discard
+proc modifier*(n: int): int = ((n-10).floorDiv(2))
 
 proc initCharacter*: Character =
-  discard
+  let constitution = ability()
+  Character(
+              strength:ability(),
+              dexterity: ability(),
+              constitution: constitution,
+              intelligence: ability(),
+              wisdom: ability(),
+              charisma: ability(),
+              hitpoints: 10 + modifier(constitution)
+  )
+
