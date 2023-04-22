f1db496e981d3134798f662bb17f8724ca103478
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-22 12:30:50 -0400

    pt

--- java/protein-translation/src/main/java/ProteinTranslator.java
+++ java/protein-translation/src/main/java/ProteinTranslator.java
@@ -1,8 +1,34 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 class ProteinTranslator {
+    private static final Map<String, String> CODONS_TO_PROTEIN;
+
+    static {
+        CODONS_TO_PROTEIN = Map.ofEntries(
+                Map.entry("AUG", "Methionine"),
+                Map.entry("UUU", "Phenylalanine"),
+                Map.entry("UUC", "Phenylalanine"),
+                Map.entry("UUA", "Leucine"),
+                Map.entry("UUG", "Leucine"),
+                Map.entry("UCU", "Serine"),
+                Map.entry("UCC", "Serine"),
+                Map.entry("UCA", "Serine"),
+                Map.entry("UCG", "Serine"),
+                Map.entry("UAU", "Tyrosine"),
+                Map.entry("UAC", "Tyrosine"),
+                Map.entry("UGU", "Cysteine"),
+                Map.entry("UGC", "Cysteine"),
+                Map.entry("UGG", "Tryptophan")
+        );
+    }
 
     List<String> translate(String rnaSequence) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Arrays.stream(rnaSequence.split("(?<=\\G.{3})"))
+                .map(CODONS_TO_PROTEIN::get)
+                .takeWhile(Objects::nonNull)
+                .toList();
     }
 }
