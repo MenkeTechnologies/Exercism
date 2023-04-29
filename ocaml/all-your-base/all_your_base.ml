9d21fcbc75adea343610f0eb3c251f6b51c7646a
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-29 01:01:47 -0400

    ayb

--- ocaml/all-your-base/all_your_base.ml
+++ ocaml/all-your-base/all_your_base.ml
@@ -1,4 +1,11 @@
 type base = int
-
+let dec_to_base base n =
+  if n = 0 then [0] else
+    let rec aux acc = function
+      | 0 -> acc
+      | x -> aux ((x mod base) :: acc) (x / base)
+    in aux [] n
+let to_sum base digits = digits |> List.fold_left (fun acc n -> acc * base + n ) 0
 let convert_bases ~from ~digits ~target =
-    failwith "'convert_bases' is missing"
+  if from < 2 || target < 2 || digits |> List.exists (fun n -> n < 0 || n >= from ) then None
+  else Some (digits |> to_sum from |> dec_to_base target)
