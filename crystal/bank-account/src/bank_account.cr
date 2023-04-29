0005dff7ba25cda498ccf12a233adc063e2024f8
MenkeTechnologies <linux.dev25@gmail.com>
2023-04-29 16:00:12 -0400

    ba

--- crystal/bank-account/src/bank_account.cr
+++ crystal/bank-account/src/bank_account.cr
@@ -1,3 +1,29 @@
 class BankAccount
-  # Write your code for the 'Bank Account' exercise in this file.
+  @opened : Bool
+  @balance : Int32
+  def initialize
+    @opened = false
+    @balance = 0
+  end
+  def open
+    raise ArgumentError.new if @opened
+    @opened = true
+    @balance = 0
+  end
+  def close
+    raise ArgumentError.new if !@opened
+    @opened = false
+  end
+  def balance
+    raise ArgumentError.new if !@opened
+    @balance
+  end
+  def deposit(amt : Int32)
+    raise ArgumentError.new if !@opened || amt <= 0
+    @balance += amt
+  end
+  def withdraw(amt : Int32)
+    raise ArgumentError.new if !@opened || @balance - amt < 0 || amt <= 0
+    @balance -= amt
+  end
 end
