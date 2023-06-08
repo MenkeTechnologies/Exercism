69751fc9f10769b1de3146cd03eb49030782ec18
MenkeTechnologies <linux.dev25@gmail.com>
2023-06-07 23:47:53 -0400

    darts

--- typescript/darts/darts.ts
+++ typescript/darts/darts.ts
@@ -1,3 +1,5 @@
-export function score(x: unknown, y: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function score(x: number, y: number): number {
+  let distance = Math.sqrt(x*x + y*y)
+  return distance <= 1 ? 10 : distance <= 5 ? 5 : distance <= 10 ? 1 : 0;
 }
+
