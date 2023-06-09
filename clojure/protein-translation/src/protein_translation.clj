2e49b10be12d6976e581b1ed32133785bed19465
MenkeTechnologies <linux.dev25@gmail.com>
2023-06-08 22:54:50 -0400

    pt

--- clojure/protein-translation/src/protein_translation.clj
+++ clojure/protein-translation/src/protein_translation.clj
@@ -1,9 +1,22 @@
 (ns protein-translation)
 
-(defn translate-codon [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(def codon-to-protein {["AUG"]                      "Methionine"
+                       ["UUU", "UUC"]               "Phenylalanine"
+                       ["UUA", "UUG"]               "Leucine"
+                       ["UCU", "UCC", "UCA", "UCG"] "Serine"
+                       ["UAU", "UAC"]               "Tyrosine"
+                       ["UGU", "UGC"]               "Cysteine"
+                       ["UGG"]                      "Tryptophan"
+                       ["UAA", "UAG" "UGA"]         "STOP"})
 
-(defn translate-rna [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def codon-map
+  (->> (for [[codons protein] codon-to-protein]
+         (map #(array-map % protein) codons))
+       flatten
+       (apply merge)))
+(defn translate-codon [dna] (codon-map dna))
+(defn translate-rna [dna]
+  (->> (partition 3 dna)
+       (map (partial apply str))
+       (map codon-map)
+       (take-while (partial not= "STOP"))))
