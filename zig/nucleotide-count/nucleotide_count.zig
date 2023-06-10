1a38bf21b4adabcaa9c5c3cf1cd1fef6b0f2cdd7
MenkeTechnologies <linux.dev25@gmail.com>
2023-06-09 22:57:27 -0400

    nc

--- zig/nucleotide-count/nucleotide_count.zig
+++ zig/nucleotide-count/nucleotide_count.zig
@@ -1,12 +1,14 @@
 pub const NucleotideError = error{Invalid};
-
-pub const Counts = struct {
-    a: u32,
-    c: u32,
-    g: u32,
-    t: u32,
-};
+pub const Counts = struct { a: u32 = 0, c: u32 = 0, g: u32 = 0, t: u32 = 0 };
 
 pub fn countNucleotides(s: []const u8) NucleotideError!Counts {
-    _ = s;
+    var dict = Counts{};
+    for (s) |c| switch (c) {
+        'A' => dict.a += 1,
+        'C' => dict.c += 1,
+        'G' => dict.g += 1,
+        'T' => dict.t += 1,
+        else => return NucleotideError.Invalid,
+    };
+    return dict;
 }
