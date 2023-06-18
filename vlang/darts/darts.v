0f79dd97e3528fbfda678932aaeffbbca53f6603
MenkeTechnologies <linux.dev25@gmail.com>
2023-06-18 04:52:19 -0400

    darts

--- vlang/darts/darts.v
+++ vlang/darts/darts.v
@@ -1,4 +1,16 @@
 module main
 
-fn score(x f64, y f64) int {
+import math { pow }
+
+pub fn score(x f64, y f64) int {
+	dist := pow(pow(x, 2) + pow(y, 2), 0.5)
+	if dist > 10 {
+		return 0
+	} else if dist > 5 {
+		return 1
+	} else if dist > 1 {
+		return 5
+	}
+	return 10
 }
+
