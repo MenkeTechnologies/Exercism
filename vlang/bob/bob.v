c5a83b55502f9aed6ec32d7039ff86cc8c8d5b84
MenkeTechnologies <linux.dev25@gmail.com>
2023-06-20 11:17:36 -0400

    bob

--- vlang/bob/bob.v
+++ vlang/bob/bob.v
@@ -1,4 +1,20 @@
 module main
 
-fn response(hey_bob string) string {
+pub fn response(s string) string {
+    nows := s.trim_space()
+	is_question := nows.ends_with('?')
+	is_yelling := s.is_upper() && s.to_lower().contains_any('abcdefghijklmnopqrstuvwxyz')
+	if is_yelling {
+		if is_question {
+			return "Calm down, I know what I'm doing!"
+		}
+		return 'Whoa, chill out!'
+	}
+	if is_question {
+		return 'Sure.'
+	}
+	if s.contains_only(' \t\n\r') {
+		return 'Fine. Be that way!'
+	}
+	return 'Whatever.'
 }
