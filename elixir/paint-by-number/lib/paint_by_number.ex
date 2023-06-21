1c2f5c5b71c861806aebc09385ce76d8286aa492
MenkeTechnologies <linux.dev25@gmail.com>
2023-06-21 11:29:30 -0400

    pbn

--- elixir/paint-by-number/lib/paint_by_number.ex
+++ elixir/paint-by-number/lib/paint_by_number.ex
@@ -1,29 +1,38 @@
 defmodule PaintByNumber do
-  def palette_bit_size(color_count) do
-    # Please implement the palette_bit_size/1 function
+
+  def palette_bit_size(color_count), do: aux(1, color_count)
+
+  defp aux(acc, n) do
+    cond do
+      Integer.pow(2, acc) >= n -> acc
+      true -> aux(acc + 1, n)
+    end
   end
 
-  def empty_picture() do
-    # Please implement the empty_picture/0 function
-  end
+  def empty_picture(), do: <<>>
 
-  def test_picture() do
-    # Please implement the test_picture/0 function
-  end
+  def test_picture(), do: <<0::2, 1::2, 2::2, 3::2>>
 
   def prepend_pixel(picture, color_count, pixel_color_index) do
-    # Please implement the prepend_pixel/3 function
+    <<pixel_color_index::size(palette_bit_size(color_count)), picture::bitstring>>
   end
+
+  def get_first_pixel(<<>>, _), do: nil
 
   def get_first_pixel(picture, color_count) do
-    # Please implement the get_first_pixel/2 function
+    bit_size = palette_bit_size(color_count)
+    <<a::size(bit_size), _::bitstring>> = picture
+    a
   end
+
+  def drop_first_pixel(<<>>, _), do: <<>>
 
   def drop_first_pixel(picture, color_count) do
-    # Please implement the drop_first_pixel/2 function
+    bit_size = palette_bit_size(color_count)
+    <<_::size(bit_size), rest::bitstring>> = picture
+    rest
   end
 
-  def concat_pictures(picture1, picture2) do
-    # Please implement the concat_pictures/2 function
-  end
+  def concat_pictures(p1, p2), do: <<p1::bitstring, p2::bitstring>>
 end
+
