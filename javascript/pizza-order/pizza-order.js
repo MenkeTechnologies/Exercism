50f95b17d76a53182ff9d27edbcd3afb92cec5dd
MenkeTechnologies <linux.dev25@gmail.com>
2023-06-25 12:02:12 -0400

    za

--- javascript/pizza-order/pizza-order.js
+++ javascript/pizza-order/pizza-order.js
@@ -1,25 +1,20 @@
-/// <reference path="./global.d.ts" />
-//
-// @ts-check
+const priceLookup = {
+    "Margherita": 7,
+    "Caprese": 9,
+    "Formaggio": 10,
+    "ExtraSauce": 1,
+    "ExtraToppings": 2,
+}
 
-/**
- * Determine the prize of the pizza given the pizza and optional extras
- *
- * @param {Pizza} pizza name of the pizza to be made
- * @param {Extra[]} extras list of extras
- *
- * @returns {number} the price of the pizza
- */
 export function pizzaPrice(pizza, ...extras) {
-  throw new Error('Please implement the pizzaPrice function');
+  return extras.length > 0 ? priceLookup[pizza] + pizzaPrice(...extras) : priceLookup[pizza];
 }
 
-/**
- * Calculate the prize of the total order, given individual orders
- *
- * @param {PizzaOrder[]} pizzaOrders a list of pizza orders
- * @returns {number} the price of the total order
- */
 export function orderPrice(pizzaOrders) {
-  throw new Error('Please implement the orderPrice function');
-}
+  let totalPrice = 0;
+  for (const order of pizzaOrders) {
+    totalPrice += pizzaPrice(order.pizza, ...order.extras);
+  };
+  return totalPrice;
+};
+
