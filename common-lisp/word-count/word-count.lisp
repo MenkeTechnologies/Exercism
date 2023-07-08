bcc03c06164caad37e1d860c52d89d540c27ffff
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-08 13:00:46 -0400

    wc

--- common-lisp/word-count/word-count.lisp
+++ common-lisp/word-count/word-count.lisp
@@ -1,6 +1,23 @@
 (defpackage :word-count
   (:use :cl)
   (:export :count-words))
+
 (in-package :word-count)
 
-(defun count-words (sentence))
+(defun match (char)
+  (find char " ,.:!@#$%^&
+"))
+
+(defun string-split (s)
+  (loop for i = 0 then end
+        as start = (position-if #'alphanumericp s :start i)
+        while start
+        as end = (position-if #'match s :start start)
+        collect (string-right-trim "'" (subseq s start end))
+        while end))
+
+(defun count-words (s)
+  (let* ((words (string-split (string-downcase s)))
+         (keys (remove-duplicates words :test 'equal)))
+    (loop for key in keys
+          collect (cons key (count key words :test 'equal)))))
