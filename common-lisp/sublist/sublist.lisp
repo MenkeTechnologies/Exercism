e6e3b0635ece599bde0246e019b8a19ffaa49ae6
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-09 18:14:39 -0400

    subl

--- common-lisp/sublist/sublist.lisp
+++ common-lisp/sublist/sublist.lisp
@@ -4,5 +4,8 @@
 
 (in-package :sublist)
 
-(defun sublist (list1 list2)
-  "what is list1 of list2 (sublist, superlist, equal or unequal)")
+(defun sublist (lst1 lst2)
+  (cond ((equal lst1 lst2) :equal)
+        ((search lst1 lst2) :sublist)
+        ((search lst2 lst1) :superlist)
+        (t :unequal)))
