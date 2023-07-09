d70315a4e42ee2d05291ffaa79e96e8b38cb0e1b
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-09 18:12:47 -0400

    lsp

--- common-lisp/largest-series-product/largest-series-product.lisp
+++ common-lisp/largest-series-product/largest-series-product.lisp
@@ -4,4 +4,10 @@
 
 (in-package :largest-series-product)
 
-(defun largest-product (digits span))
+(defun largest-product (digits span)
+  (when (and (>= (length digits) span 0)
+             (every #'digit-char-p digits))
+    (loop for start from 0 to (- (length digits) span)
+          for series = (subseq digits start (+ start span))
+          maximize (reduce (lambda (acc c) (* acc (digit-char-p c)))
+                           series :initial-value 1))))
