475451f6f0aefbee53f762553b74557d608f3d28
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-12 08:44:38 -0400

    pt

--- elixir/protein-translation/lib/protein_translation.ex
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -1,33 +1,30 @@
 defmodule ProteinTranslation do
-  @doc """
-  Given an RNA string, return a list of proteins specified by codons, in order.
-  """
-  @spec of_rna(String.t()) :: {:ok, list(String.t())} | {:error, String.t()}
   def of_rna(rna) do
+    rna
+    |> String.graphemes
+    |> Enum.chunk_every(3)
+    |> Enum.map(&List.to_string/1)
+    |> Enum.reduce_while([], fn codon, acc ->
+      case of_codon(codon) do
+        {:ok, "STOP"} -> {:halt, acc}
+        {:ok, protein} -> {:cont, [protein | acc]}
+        {:error, _} = error -> {:halt, error}
+      end
+    end)
+    |> case do
+      {:error, _} -> {:error, "invalid RNA"}
+      proteins -> {:ok, Enum.reverse(proteins)}
+    end
   end
 
-  @doc """
-  Given a codon, return the corresponding protein
-
-  UGU -> Cysteine
-  UGC -> Cysteine
-  UUA -> Leucine
-  UUG -> Leucine
-  AUG -> Methionine
-  UUU -> Phenylalanine
-  UUC -> Phenylalanine
-  UCU -> Serine
-  UCC -> Serine
-  UCA -> Serine
-  UCG -> Serine
-  UGG -> Tryptophan
-  UAU -> Tyrosine
-  UAC -> Tyrosine
-  UAA -> STOP
-  UAG -> STOP
-  UGA -> STOP
-  """
-  @spec of_codon(String.t()) :: {:ok, String.t()} | {:error, String.t()}
-  def of_codon(codon) do
-  end
+  def of_codon(codon) when codon in ["UGU", "UGC"], do: {:ok, "Cysteine"}
+  def of_codon(codon) when codon in ["UUA", "UUG"], do: {:ok, "Leucine"}
+  def of_codon(codon) when codon in ["UUU", "UUC"], do: {:ok, "Phenylalanine"}
+  def of_codon(codon) when codon in ["UAU", "UAC"], do: {:ok, "Tyrosine"}
+  def of_codon(codon) when codon in ["UCU", "UCC", "UCA", "UCG"], do: {:ok, "Serine"}
+  def of_codon(codon) when codon in ["UAA", "UAG", "UGA"], do: {:ok, "STOP"}
+  def of_codon("AUG"), do: {:ok, "Methionine"}
+  def of_codon("UGG"), do: {:ok, "Tryptophan"}
+  def of_codon(_), do: {:error, "invalid codon"}
 end
+
