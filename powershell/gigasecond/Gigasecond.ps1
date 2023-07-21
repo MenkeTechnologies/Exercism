c4413e179b3890fd720a46bfa1d89fe7343caac5
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-21 12:05:51 -0400

    gs

--- powershell/gigasecond/Gigasecond.ps1
+++ powershell/gigasecond/Gigasecond.ps1
@@ -1,21 +1,7 @@
 Function Add-Gigasecond() {
-    <#
-    .SYNOPSIS
-    Add a gigasecond to a date.
-
-    .DESCRIPTION
-    Take a moment and add a gigasecond to it.
-
-    .PARAMETER Time
-    A datetime object, to which a gigasecond will be added.
-
-    .EXAMPLE
-    Add-Gigasecond -Time
-    #>
     [CmdletBinding()]
     Param(
         [DateTime]$Time
     )
-
-    throw "Please implement this function"
+    $Time.AddSeconds(1e9)
 }
