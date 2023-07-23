e0a204603fa476c10592c88f6b98bd6a5b381124
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-23 00:18:29 -0400

    darts

--- powershell/darts/Darts.ps1
+++ powershell/darts/Darts.ps1
@@ -1,27 +1,14 @@
-Function Invoke-Darts() {
-    <#
-    .SYNOPSIS
-    Calculate the earned points in a single toss of a Darts game.
-
-    .DESCRIPTION
-    Take a coordinate of a point and calculate the distance from the center of the dartboard.
-    Then depending on the distance and which concentric circle the point lies in, return the
-    number of points earned.
-
-    .PARAMETER X
-    The X coordinate of the dart.
-
-    .PARAMETER Y
-    The Y coordinate of the dart.
-
-    .EXAMPLE
-    Invoke-Darts -X 0 -Y 10
-    #>
-    [CmdletBinding()]
-    Param(
-        [Double]$X,
-        [Double]$Y
-    )
+Function Invoke-Darts($X, $Y) {
     
-    Throw "Please implement this function"
+    $distance = [Math]::Sqrt([Math]::Pow($X, 2) + [Math]::Pow($Y, 2))
+    if ($distance -le 1) {
+        return 10
+    }
+    if ($distance -le 5) {
+        return 5
+    }
+    if ($distance -le 10) {
+        return 1
+    }
+    return 0
 }
