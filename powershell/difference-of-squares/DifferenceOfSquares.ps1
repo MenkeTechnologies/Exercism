1f77cc5d97f161ac8e9460e51be8880cf6784d1e
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-23 13:40:11 -0400

    dos

--- powershell/difference-of-squares/DifferenceOfSquares.ps1
+++ powershell/difference-of-squares/DifferenceOfSquares.ps1
@@ -1,65 +1,19 @@
-Function Get-SquareOfSum() {
-    <#
-    .SYNOPSIS
-    Get the square of sum of a number.
-
-    .DESCRIPTION
-    Given a number, return the square of sum of all numbers up to and including that number.
-
-    .PARAMETER Number
-    The number to calculate the square of sum.
-    
-    .EXAMPLE
-    Get-SquareOfSum -Number 12
-    #>
-    [CmdletBinding()]
-    Param(
-        [Int64]$Number
-    )
-
-    throw "Please implement this function"
+Function Get-SquareOfSum($Number) {
+    $res = 0;
+    for ($n=1; $n -le $Number; ++$n)
+    {
+       $res += $n;
+    }
+    return [Math]::Pow($res, 2);
 }
-
-Function Get-SumOfSquares() {
-    <#
-    .SYNOPSIS
-    Get the sum of squares of a number.
-
-    .DESCRIPTION
-    Given a number, return the sum of squares of all numbers up to and including that number.
-
-    .PARAMETER Number
-    The number to calculate the sum of squares.
-
-    .EXAMPLE
-    Get-SumOfSquares -Number 12
-    #>
-    [CmdletBinding()]
-    Param(
-        [Int64]$Number
-    )
-
-    throw "Please implement this function"
+Function Get-SumOfSquares($Number) {
+     $res = 0;
+    for ($n=1; $n -le $Number; ++$n)
+    {
+       $res += [Math]::Pow($n, 2);
+    }
+    return $res;
 }
-
-Function Get-DifferenceOfSquares() {
-    <#
-    .SYNOPSIS
-    Get the difference of squares of a number.
-
-    .DESCRIPTION
-    Given a number, return the difference of squares of all numbers up to and including that number.
-
-    .PARAMETER Number
-    The number to calculate the difference of squares.
-
-    .EXAMPLE
-    Get-DifferenceOfSquares -Number 12
-    #>
-    [CmdletBinding()]
-    Param(
-        [Int64]$Number
-    )
-
-    throw "Please implement this function"
+Function Get-DifferenceOfSquares($Number) {
+   return [int](Get-SquareOfSum($Number)) - [int](Get-SumOfSquares($Number));
 }
