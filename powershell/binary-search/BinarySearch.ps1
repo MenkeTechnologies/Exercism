c1eff7d9566747db9877d2dbef485aa1eb42c9c1
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-27 12:31:46 -0400

    bs

--- powershell/binary-search/BinarySearch.ps1
+++ powershell/binary-search/BinarySearch.ps1
@@ -1,25 +1,16 @@
-Function Invoke-BinarySearch() {
-    <#
-    .SYNOPSIS
-    Perform a binary search on a sorted array.
-
-    .DESCRIPTION
-    Take an array of integers and a search value and return the index of the value in the array.
-
-    .PARAMETER Array
-    The array to search.
-
-    .PARAMETER Value
-    The value to search for.
-
-    .EXAMPLE
-    Invoke-BinarySearch -Array @(1, 2, 3, 4, 5) -Value 3
-    #>
-    [CmdletBinding()]
-    Param(
-        [Int64[]]$Array,
-        [Int64]$Value
-    )
-
-    throw "Please implement this function"
+Function Invoke-BinarySearch($Array, $Value) {
+        $lo = 0
+        $hi = $Array.Length - 1
+        while ($lo -le $hi) {
+            [int]$mid = ($lo + $hi) / 2
+            if ($Value -lt $Array[$mid]) {
+                $hi = $mid - 1
+            } elseif ($Value -gt $Array[$mid]) {
+                $lo = $mid + 1
+            } else {
+                 return $mid
+            }
+        }
+        throw "error: value not in array"
 }
+
