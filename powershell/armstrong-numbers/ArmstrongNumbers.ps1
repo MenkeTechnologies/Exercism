a774f9422f39250675e20d98cf46a3ed46a07a71
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-28 11:37:02 -0400

    arm

--- powershell/armstrong-numbers/ArmstrongNumbers.ps1
+++ powershell/armstrong-numbers/ArmstrongNumbers.ps1
@@ -1,21 +1,11 @@
-Function Invoke-ArmstrongNumbers() {
-    <#
-    .SYNOPSIS
-    Determine if a number is an Armstrong number.
-
-    .DESCRIPTION
-    An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
-
-    .PARAMETER Number
-    The number to check.
-
-    .EXAMPLE
-    Invoke-ArmstrongNumbers -Number 12
-    #>
-    [CmdletBinding()]
-    Param(
-        [Int64]$Number
-    )
-
-    Throw "Please implement this function"
+Function Invoke-ArmstrongNumbers($Number)
+{
+    $s = $Number.ToString();
+    $sum = 0;
+    foreach($c in $s.ToCharArray())
+    {
+        $digit = [int]::Parse($c);
+        $sum += [Math]::Pow($digit, $s.Length);
+    }
+    return $sum -eq $Number;
 }
