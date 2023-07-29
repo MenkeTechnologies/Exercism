3d2f516249e1dc7e421281587a97594fdfe9b6db
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-29 12:58:17 -0400

    rna

--- powershell/rna-transcription/RnaTranscription.ps1
+++ powershell/rna-transcription/RnaTranscription.ps1
@@ -1,21 +1,8 @@
-Function Invoke-RnaTranscription() {
-    <#
-    .SYNOPSIS
-    Transcribe a DNA strand into RNA.
-
-    .DESCRIPTION
-    Given a DNA strand, return its RNA complement (per RNA transcription).
-
-    .PARAMETER Strand
-    The DNA strand to transcribe.
-
-    .EXAMPLE
-    Invoke-RnaTranscription -Strand "A"
-    #>
-    [CmdletBinding()]
-    Param(
-        [string]$Strand
-    )
-    
-    throw "Please implement this function"
+Function Invoke-RnaTranscription($Strand) {
+    $dict = @{G = "C"; C = "G"; T = "A"; A = "U"}
+    $res = @()
+    foreach ($c in $Strand.ToCharArray()) {
+        $res += $dict["$c"]
+    }
+    $res -join ""
 }
