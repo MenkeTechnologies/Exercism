659f7fb38d67f95897a5c197156c3ceda32ddc41
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-29 04:25:08 -0400

    ana

--- powershell/anagram/Anagram.ps1
+++ powershell/anagram/Anagram.ps1
@@ -1,26 +1,11 @@
-Function Invoke-Anagram() {
-    <#
-    .SYNOPSIS
-    Determine if a word is an anagram of other words in a list.
-
-    .DESCRIPTION
-    An anagram is a word formed by rearranging the letters of another word, e.g., spar, formed from rasp.
-    Given a word and a list of possible anagrams, find the anagrams in the list.
-
-    .PARAMETER Subject
-    The word to check
-
-    .PARAMETER Candidates
-    The list of possible anagrams
-
-    .EXAMPLE
-    Invoke-Anagram -Subject "listen" -Candidates @("enlists" "google" "inlets" "banana")
-    #>
-    [CmdletBinding()]
-    Param(
-        [string]$Subject,
-        [string[]]$Candidates
-    )
-
-    Throw "Please implement this function"
+Function Invoke-Anagram($Subject, $Candidates) {
+    $sortedTarget = ($Subject.ToLower().ToCharArray() | Sort-Object) -join ""
+    $res = @()
+    foreach ($Candidate in $Candidates) {
+        $sortedCandidate = ($candidate.ToLower().ToCharArray() | Sort-Object) -join ""
+        if ($sortedTarget -eq $sortedCandidate -and $Subject.ToLower() -ne $Candidate.ToLower()) {
+            $res += $Candidate
+        }
+    }
+    return $res
 }
