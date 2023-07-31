bed86408845fa205b5813971cc0dd2bc4adb2de9
MenkeTechnologies <linux.dev25@gmail.com>
2023-07-31 13:31:00 -0400

    tri

--- powershell/triangle/Triangle.ps1
+++ powershell/triangle/Triangle.ps1
@@ -1,22 +1,19 @@
-Function Get-Triangle() {
-    <#
-    .SYNOPSIS
-    Determine if a triangle is equilateral, isosceles, or scalene.
-
-    .DESCRIPTION
-    Given 3 sides of a triangle, return the type of that triangle if it is a valid triangle.
-    
-    .PARAMETER Sides
-    The lengths of a triangle's sides.
-
-    .EXAMPLE
-    Get-Triangle -Sides @(1,2,3)
-    Return: [Triangle]::SCALENE
-    #>
-    
-    [CmdletBinding()]
-    Param (
-        [double[]]$Sides
-    )
-    Throw "Please implement this function"
+Enum Triangle {
+    EQUILATERAL
+    ISOSCELES
+    SCALENE
+}
+Function Get-Triangle($Sides) {
+    if ($Sides | Where-Object {$_ -le 0}) {
+        Throw "All side lengths must be positive."
+    }
+    $sorted = $Sides | Sort-Object
+    if ($sorted[0] + $sorted[1] -lt $sorted[2]) {
+        Throw "Side lengths violate triangle inequality."
+    }
+    switch (($Sides | Select-Object -Unique).Count) {
+        1       { return [Triangle]::EQUILATERAL }
+        2       { return [Triangle]::ISOSCELES }
+        default { return [Triangle]::SCALENE }
+    }
 }
