4fd796bc2b80e14ee9e1e2e30984e4aa81711d87
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-02 10:33:30 -0400

    rcd

--- powershell/resistor-color-duo/ResistorColorDuo.ps1
+++ powershell/resistor-color-duo/ResistorColorDuo.ps1
@@ -1,21 +1,18 @@
-Function Get-ColorCodeValue() {
-    <#
-    .SYNOPSIS
-    Translate a list of colors to their corresponding color code values.
-
-    .DESCRIPTION
-    Given 2 colors, take the first one and times it by 10 and add the second color to it.
-
-    .PARAMETER Colors
-    The colors to translate to their corresponding color codes.
-
-    .EXAMPLE
-    Get-ColorCodeValue -Colors @("black", "brown")
-    #>
-    [CmdletBinding()]
-    Param(
-        [string[]]$Colors
-    )
-    
-    throw "Please implement this function"
+Function Get-ColorCode($Color) {
+    @{
+        'black' = 0
+        'brown' = 1
+        'red' = 2
+        'orange' = 3
+        'yellow' = 4
+        'green' = 5
+        'blue' = 6
+        'violet' = 7
+        'grey' = 8
+        'white' = 9
+    }[$Color]
 }
+Function Get-ColorCodeValue($Colors) {
+    (Get-ColorCode -Color $Colors[0]) * 10 + (Get-ColorCode -Color $Colors[1])
+}
+
