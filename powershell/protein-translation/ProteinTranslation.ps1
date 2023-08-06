54d59710e768bf8a107807f9bc5b5d8d203fda0a
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-06 11:06:37 -0400

    prot

--- powershell/protein-translation/ProteinTranslation.ps1
+++ powershell/protein-translation/ProteinTranslation.ps1
@@ -1,21 +1,31 @@
-Function Invoke-ProteinTranslation() {
-    <#
-    .SYNOPSIS
-    Translate RNA sequences into proteins.
-
-    .DESCRIPTION
-    Take an RNA sequence and convert it into condons and then into the name of the proteins in the form of a list.
-
-    .PARAMETER Strand
-    The RNA sequence to translate.
-
-    .EXAMPLE
-    Invoke-ProteinTranslation -Strand "AUG"
-    #>
-    [CmdletBinding()]
-    Param(
-        [string]$Strand
-    )
-    
-    throw "Please implement this function"
+$Codons = @{
+    "AUG" = "Methionine";
+    "UUU" = "Phenylalanine";
+    "UUC" = "Phenylalanine";
+    "UUA" = "Leucine";
+    "UUG" = "Leucine";
+    "UCU" = "Serine";
+    "UCC" = "Serine";
+    "UCA" = "Serine";
+    "UCG" = "Serine";
+    "UAU" = "Tyrosine";
+    "UAC" = "Tyrosine";
+    "UGU" = "Cysteine";
+    "UGC" = "Cysteine";
+    "UGG" = "Tryptophan";
+    "UAA" = "STOP";
+    "UAG" = "STOP";
+    "UGA" = "STOP";
+}
+Function Invoke-ProteinTranslation($Strand) {
+    $protein = @()
+    :StrandLoop for ($index = 0; $index -lt $Strand.Length; $index += 3) {
+        if ($index + 3 -gt $Strand.Length ) { throw "error: Invalid codon" }
+        $codon = $Strand.Substring($index, 3);
+        if ($codons.containsKey($codon)) {
+            if ($codons[$codon] -eq "STOP") { break; }
+            $protein += $codons[$codon]
+        } else { throw "error: Invalid codon" }
+    }
+    return $protein
 }
