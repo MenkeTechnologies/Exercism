9ce26919008631b547c877805f5496bbcacd039d
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-09 12:58:07 -0400

    Etl

--- powershell/etl/Etl.ps1
+++ powershell/etl/Etl.ps1
@@ -1,21 +1,9 @@
-Function Invoke-Etl() {
-    <#
-    .SYNOPSIS
-    Transforms a set of legacy Lexiconia data stored as letters per score to a set of data stored score per letter.
-
-    .DESCRIPTION
-    Take a hash table and take the values as keys and the keys as values.
-
-    .PARAMETER Legacy
-    The legacy data to transform.
-
-    .EXAMPLE
-    Invoke-Etl -Legacy @{1 = @("A")}
-    #>
-    [CmdletBinding()]
-    Param(
-        [object]$Legacy
-    )
-
-    Throw "Please implement this function"
+Function Invoke-Etl($Legacy) {
+    $Res = @{}
+    foreach ($Key in $Legacy.PSBase.Keys) {
+        foreach ($Letter in $Legacy[$Key]) {
+            $Res[$Letter.ToLower()] = $Key
+        }
+    }
+    $Res
 }
