822745cc665fb1014e8ed49c923f1adcf5519f52
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-11 17:15:42 -0400

    dos

--- vlang/difference-of-squares/difference-of-squares.v
+++ vlang/difference-of-squares/difference-of-squares.v
@@ -1,10 +1,19 @@
 module main
 
 pub fn square_of_sum(n u32) u32 {
+    mut sum := u32(0)
+    for i in 1 .. n + 1 {
+        sum += i
+    }
+    return sum * sum
 }
-
 pub fn sum_of_squares(n u32) u32 {
+    mut sum := u32(0)
+    for i in 1 .. n + 1 {
+        sum += i * i
+    }
+    return sum
 }
-
 pub fn difference(n u32) u32 {
+    return square_of_sum(n) - sum_of_squares(n)
 }
