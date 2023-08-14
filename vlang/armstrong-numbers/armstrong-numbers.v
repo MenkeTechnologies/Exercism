790746778c69804a431674841e7ad330b509b7f7
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-13 21:12:50 -0400

    arm

--- vlang/armstrong-numbers/armstrong-numbers.v
+++ vlang/armstrong-numbers/armstrong-numbers.v
@@ -1,4 +1,12 @@
 module main
 
+import math
+
 pub fn is_armstrong_number(number u32) bool {
+    mut digits := number.str().split("")
+    mut sum := i64(0)
+    for digit in digits {
+        sum += math.powi(digit.u32(), digits.len)
+    }
+    return sum == number
 }
