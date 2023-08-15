bb566a621434d8aac3086d6f2cd276a6ee72b14b
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-15 10:39:29 -0400

    more exe

---
+++ crystal/knapsack/src/knapsack.cr
@@ -0,0 +1,5 @@
+module Knapsack
+  def self.maximum_value(maximum_weight : Int32, items : Array(NamedTuple(weight: Int32, value: Int32)))
+    # Write your code for the 'Knapsack' exercise in this file.
+  end
+end
