774e210bb897fac9800f16fe603e9d2c64cdc9ae
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-17 08:36:37 -0400

    ll

---
+++ perl5/language-list/lib/LanguageList.pm
@@ -0,0 +1,23 @@
+package LanguageList;
+
+use v5.38;
+
+our @Languages;
+
+sub add_language ($language) {
+}
+
+sub remove_language () {
+}
+
+sub first_language () {
+}
+
+sub last_language () {
+}
+
+sub get_languages (@elements) {
+}
+
+sub has_language ($language) {
+}
