c2f6c6e504e0ffaa86c7ebff973b494ccc1deb82
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-17 07:39:10 -0400

    wings

--- elm/magician-in-training/src/MagicianInTraining.elm
+++ elm/magician-in-training/src/MagicianInTraining.elm
@@ -1,7 +1,5 @@
 module MagicianInTraining exposing (..)
-
 import Array exposing (Array)
-
 getCard : Int -> Array Int -> Maybe Int
 getCard = Array.get
 
@@ -12,7 +10,7 @@ addCard : Int -> Array Int -> Array Int
 addCard  = Array.push
 
 removeCard : Int -> Array Int -> Array Int
-removeCard i deck = Array.append (deck |> Array.slice 0 i) (deck |> Array.slice (i + 1) (Array.length deck))
+removeCard i deck = deck |> Array.slice (i + 1) (deck |> Array.length) |> Array.append (deck |> Array.slice 0 i) 
 
 evenCardCount : Array Int -> Int
 evenCardCount deck = deck |> Array.filter (\card -> modBy 2 card == 0) |> Array.length
