736504bf33f75487dd0008314d64e975e5e8fab5
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-19 09:36:14 -0400

    grains

--- powershell/grains/Grains.ps1
+++ powershell/grains/Grains.ps1
@@ -1,36 +1,14 @@
-Function Get-GrainSquare() {
-    <#
-    .SYNOPSIS
-    Get the number of grains on a square.
-
-    .DESCRIPTION
-    Given a number, return the number of grains on that square.
-    
-    .PARAMETER Number
-    Which square to get the number of grains.
-    
-    .EXAMPLE
-    Get-GrainSquare -Number 2
-    #>
-    [CmdletBinding()]
-    Param(
-        [BigInt]$Number
-    )
-
-    throw "Please implement this function"
+Function Get-GrainSquare($Number) {
+    if ($Number -gt 64 -or $Number -lt 1) {
+        throw "square must be between 1 and 64"
+    }
+    [BigInt]::Pow(2, $Number-1)
 }
 
 Function Get-GrainTotal() {
-    <#
-    .SYNOPSIS
-    Get the total number of grains.
-
-    .DESCRIPTION
-    Return the total number of grains on the board.
-
-    .EXAMPLE
-    Get-GrainTotal
-    #>
-    
-    throw "Please implement this function"
+    $total = 0
+    for ($i = 1; $i -le 64; ++$i) {
+        $total += Get-GrainSquare -Number $i
+    }
+    $total
 }
