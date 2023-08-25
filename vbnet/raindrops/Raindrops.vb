1db72d8cec4efb41ed26ffd10c439aa4813611d1
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-25 17:05:59 -0400

    rd

--- vbnet/raindrops/Raindrops.vb
+++ vbnet/raindrops/Raindrops.vb
@@ -1,7 +1,14 @@
 Imports System
+Imports System.Collections.Generic
+Imports System.Linq
 
 Public Module Raindrops
-    Public Function Convert(ByVal number As Integer) As String
-        Throw New NotImplementedException("You need to implement this function.")
+    Private dict As IDictionary(Of Integer, String) = New Dictionary(Of Integer, String) From {
+        {3, "Pling"},
+        {5, "Plang"},
+        {7, "Plong"}
+    }
+    Public Function Convert(number As Integer) As String
+        Return String.Join("", dict.Where(Function(x) number Mod x.Key = 0).Select(Function(x) x.Value).DefaultIfEmpty(number.ToString()))
     End Function
 End Module
