1d6f80f3c2f317d74acd323ecf59956f5cfcaf76
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-27 10:15:57 -0400

    more exe

--- ruby/darts/darts.rb
+++ ruby/darts/darts.rb
@@ -1,19 +1,11 @@
 class Darts
-  def initialize x, y
+  def initialize (x, y)
     @x = x
     @y = y
-    @dist = ((@x - 0)**2 + (@y - 0)**2)**0.5
+    @dist = ( @x * @x + @y * @y ) ** 0.5
   end
 
   def score
-    if @dist > 10
-      0
-    elsif @dist > 5
-      1
-    elsif @dist > 1
-      5
-    else
-      10
-    end
+    @dist > 10 ? 0 : @dist > 5 ? 1 : @dist > 1 ? 5 : 10
   end
 end
