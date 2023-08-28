4e2f41fa28e5ff17b2f23faafda07a58d5ca1b15
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-28 10:42:45 -0400

    lasagna

--- crystal/arys-amazing-lasagna/src/arys_amazing_lasagna.cr
+++ crystal/arys-amazing-lasagna/src/arys_amazing_lasagna.cr
@@ -1,15 +1,16 @@
 class Lasagna
-  # TODO: define the 'EXPECTED_MINUTES_IN_OVEN' constant
+  EXPECTED_MINUTES_IN_OVEN = 40
 
   def remaining_minutes_in_oven(actual_minutes_in_oven)
-    raise "Please implement the Lasagna#remaining_minutes_in_oven method"
+    EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
-    raise "Please implement the Lasagna#preparation_time_in_minutes method"
+    layers * 2
   end
 
   def total_time_in_minutes(number_of_layers, actual_minutes_in_oven)
-    raise "Please implement the Lasagna#total_time_in_minutes method"
+    preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
   end
+
 end
