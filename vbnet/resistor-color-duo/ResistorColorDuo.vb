8ee67e300de6ac80c6f8d427f387c5cd594026bd
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-29 09:42:55 -0400

    rcd

--- vbnet/resistor-color-duo/ResistorColorDuo.vb
+++ vbnet/resistor-color-duo/ResistorColorDuo.vb
@@ -1,7 +1,10 @@
 Imports System
 
 Public Module ResistorColorDuo
-    Public Function Value(ByVal colors As String()) As Integer
-        Throw New NotImplementedException("You need to implement this function.")
+    Dim lst() As String = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
+
+    Public Function Value(colors As String) As Integer
+        Return Integer.Parse(Array.IndexOf(colorList, colors(0)).ToString() & Array.IndexOf(colorList, colors(1)).ToString())
     End Function
+
 End Module
