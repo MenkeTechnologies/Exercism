58286f2eb560c00fe19bd2b498fc5937fb6e7aa4
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-29 09:46:01 -0400

    more exe

---
+++ crystal/wellingtons-weather-station/src/wellingtons_weather_station.cr
@@ -0,0 +1,17 @@
+class Temperature
+  def to_kelvin(celsius)
+    raise "Please implement the Temperature#to_kelvin method"
+  end
+
+  def round(celsius)
+    raise "Please implement the Temperature#round method"
+  end
+
+  def to_fahrenheit(celsius)
+    raise "Please implement the Temperature#to_fahrenheit method"
+  end
+
+  def number_missing_sensors(number_of_sensors)
+    raise "Please implement the Temperature#number_missing_sensors method"
+  end
+end
