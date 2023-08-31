80f59815f96510a95ef6ad9cd871cb4bccf28a4a
MenkeTechnologies <linux.dev25@gmail.com>
2023-08-31 09:10:56 -0400

    rna

--- vbnet/rna-transcription/RnaTranscription.vb
+++ vbnet/rna-transcription/RnaTranscription.vb
@@ -1,7 +1,15 @@
 Imports System
+Imports System.Collections.Generic
+Imports System.Linq
 
 Public Module RnaTranscription
+    Private DnaToRna As Dictionary(Of Char, Char) = New Dictionary(Of Char, Char) From {
+        {"A"c, "U"c},
+        {"C"c, "G"c},
+        {"G"c, "C"c},
+        {"T"c, "A"c}
+    }
     Public Function ToRna(ByVal nucleotide As String) As String
-        Throw New NotImplementedException("You need to implement this function.")
+        Return String.Concat(nucleotide.Select(Function(c) DnaToRna(c)))
     End Function
 End Module
