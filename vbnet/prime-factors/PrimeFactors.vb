db22fdd8d0b57916e75441a9b348864a4ac887c6
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-01 09:40:05 -0400

    pf

--- vbnet/prime-factors/PrimeFactors.vb
+++ vbnet/prime-factors/PrimeFactors.vb
@@ -1,7 +1,18 @@
 Imports System
+Imports System.Collections.Generic
 
 Public Module PrimeFactors
-    Public Function Factors(ByVal number As Long) As Long()
-        Throw New NotImplementedException()
+    Public Function Factors(ByVal start As Long) As Long()
+        Dim longArr As List(Of Long) = New List(Of Long)()
+        Dim divisor = 2L
+
+        While start > 1
+            While start Mod divisor = 0
+                longArr.Add(divisor)
+                start /= divisor
+            End While
+            divisor += 1
+        End While
+        Return longArr.ToArray()
     End Function
 End Module
