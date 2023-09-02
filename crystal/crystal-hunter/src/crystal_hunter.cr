db910f623c3e46e159fb1eb991fdac7efb005168
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-02 11:59:53 -0400

    cry hunt

--- crystal/crystal-hunter/src/crystal_hunter.cr
+++ crystal/crystal-hunter/src/crystal_hunter.cr
@@ -1,17 +1,14 @@
 class Rules
   def bonus_points?(power_up_active, touching_bandit)
-    raise "Please implement the Rules#bonus_points? method"
+    power_up_active && touching_bandit
   end
-
   def score?(touching_power_up, touching_crystal)
-    raise "Please implement the Rules#score? method"
+    touching_power_up || touching_crystal
   end
-
   def lose?(power_up_active, touching_bandit)
-    raise "Please implement the Rules#lose? method"
+    !power_up_active && touching_bandit
   end
-
   def win?(has_picked_up_all_crystals, power_up_active, touching_bandit)
-    raise "Please implement the Rules#win? method"
+    !has_picked_up_all_crystals || !lose? power_up_active, touching_bandit
   end
 end
