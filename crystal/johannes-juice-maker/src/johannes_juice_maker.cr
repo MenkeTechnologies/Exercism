bd1a5e2fdb0e2b98bb58294a714e76f4c5e95c15
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-05 10:37:27 -0400

    joh

--- crystal/johannes-juice-maker/src/johannes_juice_maker.cr
+++ crystal/johannes-juice-maker/src/johannes_juice_maker.cr
@@ -1,1 +1,22 @@
-# To Do: Define the class JuiceMaker
+class JuiceMaker
+  def initialize(@fluid : Int32)
+    @running= false
+  end
+  def start
+    @running = true
+  end
+  def stop(juice_in_cup)
+    @fluid -= juice_in_cup * 5
+    @running = false
+  end
+  def running?
+    @running
+  end
+  def add_fluid(amt)
+    @fluid += amt
+  end
+  def self.debug_light_on?
+    true
+  end
+end
+
