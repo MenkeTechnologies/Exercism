1d4968d358ffe2997ebad64a435e653b4eb3ac72
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-06 19:52:36 -0400

    party robot

---
+++ crystal/party-robot/src/party_robot.cr
@@ -0,0 +1,13 @@
+class PartyRobot
+  def self.welcome(name : String) : String
+    "Welcome to my party, #{name}!"
+  end
+  def self.happy_birthday(name : String, age : Int) : String
+    "Happy birthday #{name}! You are now #{age} years old!"
+  end
+  def self.assign_table(name : String, direction : String, table : Int, distance : Float64, neighbor : String) : String
+    "#{welcome(name)}\n" +
+    "You have been assigned to table #{table}. Your table is #{direction}, exactly #{distance.round(1)} meters from here.\n" + 
+    "You will be sitting next to #{neighbor[0]}#{neighbor[-1]}."
+  end
+end
