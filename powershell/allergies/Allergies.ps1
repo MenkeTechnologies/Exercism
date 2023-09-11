bc221920be2c4a2109d93534a10407fe7d8a74ec
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-10 22:24:52 -0400

    allergies

--- powershell/allergies/Allergies.ps1
+++ powershell/allergies/Allergies.ps1
@@ -1,27 +1,25 @@
-<#
-.SYNOPSIS
-    Given a person's allergy score, determine whether or not they're allergic to a given item, and their full list of allergies.
-
-.DESCRIPTION
-    Given a positive integer to represent the score of a person's allergies, calculate which alleriges they have from the list:
-    - eggs (1)
-    - peanuts (2)
-    - shellfish (4)
-    - strawberries (8)
-    - tomatoes (16)
-    - chocolate (32)
-    - pollen (64)
-    - cats (128)
-    Your task is to write methods that check if the person is allergic to a specific item, and return a list of allergies that they could have.
-
-.EXAMPLE
-    $person = [Allergies]::new(34)
-    $person.IsAllergicTo("chocolate")   => True
-    $person.IsAllergicTo("cats")        => False
-    $person.GetAllergies()              => @("peanuts", "chocolate")
-#>
 Class Allergies {
-    Allergies() {
-        Throw "Please implement this class"
-    }
+  static [string[]]$AllergenNames = @(
+    "eggs"
+    "peanuts"
+    "shellfish"
+    "strawberries"
+    "tomatoes"
+    "chocolate"
+    "pollen"
+    "cats"
+  )
+  [uint] $mask = 0
+  Allergies([uint]$alMask) {
+    $this.mask = $alMask -band 0xff
+  }
+  [bool] IsAllergicTo($Name) {
+    return [Allergies]::Allergens | ? Name -eq $Name | % { $_.Mask -band $this.mask }
+  }
+  [string[]] GetAllergies() {
+    return [Allergies]::Allergens | ? {$_.Mask -band $this.mask} | % Name
+  }
+  static [psobject[]] $Allergens = ([Allergies]::AllergenNames |
+    % { $x = 0 } { [pscustomobject]@{Name=$_;Mask=(1u -shl $x)}; $x += 1 }
+  )
 }
