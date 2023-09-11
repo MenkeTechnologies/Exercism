f4b3769478d096151174b91080f66f19ea153e91
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-11 09:32:39 -0400

    rcd

--- vlang/resistor-color-duo/resistor-color-duo.v
+++ vlang/resistor-color-duo/resistor-color-duo.v
@@ -1,4 +1,23 @@
 module main
 
+const all_colors = [
+	'black',
+	'brown',
+	'red',
+	'orange',
+	'yellow',
+	'green',
+	'blue',
+	'violet',
+	'grey',
+	'white',
+]
+
 fn value(colors []string) int {
+	assert colors.len >= 2
+	first := all_colors.index(colors[0])
+	assert first >= 0
+	second := all_colors.index(colors[1])
+	assert second >= 0
+	return first * 10 + second
 }
