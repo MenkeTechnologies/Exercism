0890c819e00b80b9bdc73f8fe32270d58701896b
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-17 12:37:44 -0400

    sm

--- vbnet/sum-of-multiples/SumOfMultiples.vb
+++ vbnet/sum-of-multiples/SumOfMultiples.vb
@@ -1,8 +1,9 @@
 Imports System
 Imports System.Collections.Generic
+Imports System.Linq
 
 Public Module SumOfMultiples
-    Public Function Sum(ByVal multiples As IEnumerable(Of Integer), ByVal max As Integer) As Integer
-        Throw New NotImplementedException("You need to implement this function.")
+    Public Function Sum(ByVal factors As IEnumerable(Of Integer), ByVal max As Integer) As Integer
+        Return Enumerable.Range(0, max).Where(Function(n) factors.Any(Function(f) (f <> 0) AndAlso n Mod f = 0)).Sum()
     End Function
 End Module
