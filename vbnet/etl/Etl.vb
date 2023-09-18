f50897d8d60a3e139dc1eedd2b80753868aad7a2
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-18 09:44:18 -0400

    etl

--- vbnet/etl/Etl.vb
+++ vbnet/etl/Etl.vb
@@ -1,8 +1,10 @@
-Imports System
 Imports System.Collections.Generic
+Imports System.Linq
 
 Public Module Etl
     Public Function Transform(ByVal old As Dictionary(Of Integer, String())) As Dictionary(Of String, Integer)
-        Throw New NotImplementedException("You need to implement this function.")
+        Return old _
+            .SelectMany(Function(kv) kv.Value, Function(kv, letter) New With {.Letter = letter, .Score = kv.Key}) _
+            .ToDictionary(Function(l) l.Letter.ToLower(), Function(l) l.Score)
     End Function
 End Module
