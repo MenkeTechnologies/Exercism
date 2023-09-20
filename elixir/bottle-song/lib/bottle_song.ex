c1629b38b59129e05dac78c0bc8e83aec729c9de
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-20 10:38:48 -0400

    bs

--- elixir/bottle-song/lib/bottle_song.ex
+++ elixir/bottle-song/lib/bottle_song.ex
@@ -1,9 +1,18 @@
 defmodule BottleSong do
-  @moduledoc """
-  Handles lyrics of the popular children song: Ten Green Bottles
-  """
+  @numbers %{0 => "no", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten"}
 
-  @spec recite(pos_integer, pos_integer) :: String.t()
-  def recite(_start_bottle, _take_down) do
+  def recite(start, take_down) when is_integer(start) and start > 0 and is_integer(take_down) and take_down > 0 do
+    start..(start - take_down + 1)//-1
+    |> Enum.map(&verse/1)
+    |> Enum.join("\n\n")
   end
+
+  defp verse(start) do
+    "#{bottles(start) |> String.capitalize()} hanging on the wall,\n"
+    |> String.duplicate(2)
+    |> Kernel.<>("And if one green bottle should accidentally fall,\n")
+    |> Kernel.<>("There'll be #{bottles(start - 1)} hanging on the wall.")
+  end
+
+  defp bottles(n), do: "#{@numbers[n]} green bottle" <> if(n == 1, do: "", else: "s")
 end
--- python/bottle-song/bottle_song.py
+++ python/bottle-song/bottle_song.py
@@ -5,10 +5,11 @@ def recite(start, take=1):
     output = []
     for i in range(take):
         plural = 's' if start > 1 else ''
+        next = start - 1
         output += [f"{CARDINAL[start].capitalize()} green bottle{plural} hanging on the wall,",
                    f"{CARDINAL[start].capitalize()} green bottle{plural} hanging on the wall,",
                    f"And if one green bottle should accidentally fall,",
-                   f"There'll be {CARDINAL[start - 1]} green bottle{'s' if (start - 1) != 1 else ''} hanging on the wall."]
+                   f"There'll be {CARDINAL[next]} green bottle{'' if next == 1 else 's'} hanging on the wall."]
         if i != take - 1:
             start -= 1
             output.append('')
