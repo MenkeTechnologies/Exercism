5f5e840dca7d9028b61a25c9959942af961f9642
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-22 10:16:29 -0400

    mb

--- vlang/matching-brackets/matching-brackets.v
+++ vlang/matching-brackets/matching-brackets.v
@@ -1,4 +1,11 @@
-module main
-
 fn is_paired(input string) bool {
+	mut stk := []rune{}	
+	pairs := { `]`:`[`, `)`:`(`, `}`:`{` }
+	for c in input.runes() {
+		if c in pairs.values() { stk << c }
+		if c in pairs.keys() && (stk.len == 0 || stk.pop() != pairs[c]) {
+            return false
+		}
+	}
+	return stk.len == 0
 }
