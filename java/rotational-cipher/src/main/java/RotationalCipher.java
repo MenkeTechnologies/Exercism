80fe216dafca4510fa6ff39227fafd50ef40532f
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-22 20:49:00 -0400

    rc

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,21 +1,19 @@
 import java.util.stream.Collectors;
 
 class RotationalCipher {
-    Integer sc;
+    Integer offset;
 
     RotationalCipher(int shiftKey) {
-        sc = shiftKey;
+        offset = shiftKey;
     }
 
     String rotate(String data) {
-
         return data.chars().map(this::rot).mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining(""));
     }
 
     private int rot(int c) {
         if (Character.isLetter(c)) {
-            return c >= 'a' ? (c - 'a' + sc) % 26 + 'a' :
-                    (c - 'A' + sc) % 26 + 'A';
+            return c >= 'a' ? (c - 'a' + offset) % 26 + 'a' : (c - 'A' + offset) % 26 + 'A';
         }
         return c;
     }
