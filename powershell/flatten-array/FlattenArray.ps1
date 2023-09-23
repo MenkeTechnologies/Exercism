9bd82c9ca8e2fdd634882d6500bb1fe930e2c944
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-23 10:29:48 -0400

    fa

--- powershell/flatten-array/FlattenArray.ps1
+++ powershell/flatten-array/FlattenArray.ps1
@@ -1,21 +1,13 @@
-Function Invoke-FlattenArray() {
-    <#
-    .SYNOPSIS
-    Take a nested array and return a single flattened array with all values except null.
-
-    .DESCRIPTION
-    Given an array, flatten it and keep all values execept null.
-
-    .PARAMETER Array
-    The nested array to be flatten.
-
-    .EXAMPLE
-    Invoke-FlattenArray -Array @(1, @(2, 3, $null, 4), @($null), 5)
-    Return: @(1, 2, 3, 4, 5)
-    #>
-    [CmdletBinding()]
-    Param(
-        [System.Object[]]$Array
-    )
-    Throw "Please implement this function"
+Function Invoke-FlattenArray($Array) {
+    $lst = @()
+    ForEach($n in $Array) {
+        If ($n -eq $null) { Continue }
+        ElseIf ($n -is [array]) {
+            $flattened = Invoke-FlattenArray -Array $n
+            If ($flattened -eq $null) { Continue }
+            Else { $lst += $flattened }
+        } Else { $lst += $n }
+    }
+    $lst
 }
+
