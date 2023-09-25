608762b4c3bd493c3672ea09d32ebe4edf8c49a3
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-25 09:53:06 -0400

    ll

--- perl5/language-list/lib/LanguageList.pm
+++ perl5/language-list/lib/LanguageList.pm
@@ -5,19 +5,20 @@ use v5.38;
 our @Languages;
 
 sub add_language ($language) {
+    push(@Languages, $language)
 }
-
 sub remove_language () {
+    pop(@Languages)
 }
-
 sub first_language () {
+    $Languages[0]
 }
-
 sub last_language () {
+    $Languages[-1]
 }
-
 sub get_languages (@elements) {
+    @Languages[map { $_ - 1 } @elements];
 }
-
 sub has_language ($language) {
+    !!(grep { $_ eq $language} @Languages);
 }
