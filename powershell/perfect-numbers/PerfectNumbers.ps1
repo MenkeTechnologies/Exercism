2658e6e80c4dcde3144c28435874a9f62a389327
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-27 10:15:48 -0400

    pn

--- powershell/perfect-numbers/PerfectNumbers.ps1
+++ powershell/perfect-numbers/PerfectNumbers.ps1
@@ -1,23 +1,16 @@
-Function Invoke-PerfectNumbers() {
-    <#
-    .SYNOPSIS
-    Determine if a number is perfect, abundant, or deficient based on Nicomachus' (60 - 120 CE) classification scheme for natural numbers.
+Function Invoke-PerfectNumbers($Number) {
 
-    .DESCRIPTION
-    Calculate the aliquot sum of a number: the sum of its positive divisors not including the number itself.
-    Compare the sum to the original number.
-    Determine the classification: perfect, abundant, or deficient.
-
-    .PARAMETER Number
-    The number to perform the classification on.
-
-    .EXAMPLE
-    Invoke-PerfectNumbers -Number 12
-    #>
-    [CmdletBinding()]
-    Param(
-        [Int64]$Number
-    )
-
-    Throw "Please implement this function"
+    if ($Number -lt 1) {
+        throw "error: Classification is only possible for positive integers."
+    }
+    if ($Number -eq 1) { return "deficient"}
+    $sum = 0
+    foreach($i in 1..($Number / 2)) {
+        if( $Number % $i -eq 0) {
+            $sum += $i
+        }
+    }
+    if ($sum -eq $Number) { "perfect" }
+    elseif ($sum -gt $Number) { "abundant"}
+    else { "deficient" }
 }
