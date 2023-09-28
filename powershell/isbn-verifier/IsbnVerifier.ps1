aafd8cf83e70bad4226c25451ca3b74df6a80d96
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-28 13:47:10 -0400

    isbn

--- powershell/isbn-verifier/IsbnVerifier.ps1
+++ powershell/isbn-verifier/IsbnVerifier.ps1
@@ -1,23 +1,17 @@
-Function Test-Isbn() {
-    <#
-    .SYNOPSIS
-    Determine if an ISBN is valid or not.
-    
-    .DESCRIPTION
-    Given a string the function should check if the provided string is a valid ISBN-10.
-    
-    .PARAMETER Isbn
-    The ISBN to check
-    
-    .EXAMPLE
-    Test-Isbn -Isbn "3-598-21508-8"
-    
-    Retuns: $true
-    #>
-    [CmdletBinding()]
-    Param(
-        [string]$Isbn
-    )
-
-    Throw "Please implement this function"
+Function Test-Isbn($Isbn) {
+    $noWS = $Isbn.Replace(' ','').Replace('-', '').ToCharArray()
+    if ($noWS -join "" -notmatch "^\d\d{3}\d{5}[\dX]$") {
+        return $false
+    }
+    [array]::Reverse($noWS)
+    $sum = 0
+    if ($noWS[0] -eq "X"){
+        $sum += 10
+    } else {
+        $sum += ([int]$noWS[0] - 48)
+    }
+    for ($i = 1 ; $i -le 9 ; $i++) {
+        $sum += ($i + 1) * ([int]$noWS[$i] - 48)
+    }
+    $sum % 11 -eq 0
 }
