93c40117f5cd0718ff9bb0ff642497e0b1d37c2a
MenkeTechnologies <linux.dev25@gmail.com>
2023-09-29 10:38:11 -0400

    hs

--- powershell/high-scores/HighScores.ps1
+++ powershell/high-scores/HighScores.ps1
@@ -1,18 +1,18 @@
-<#
-.SYNOPSIS
-    Manage a game player's High Score list.
-
-.DESCRIPTION
-    Your task is to build a high-score component of the classic Frogger game, one of the highest selling and most addictive games of all time, and a classic of the arcade era.
-    Your task is to write methods that return the highest score from the list, the last added score and the three highest scores.
-
-.EXAMPLE
-    $roster = [HighScores]::new(@(30, 50, 40, 90, 80))
-    $roster.GetTopThree()
-    return : @(90, 80, 50)
-#>
 Class HighScores {
+    [int[]]$scores
     HighScores([int[]]$list){
-        Throw "Please implement this class."
+        $this.scores = $list
+    }
+    [int[]] GetScores(){
+        return $this.scores
+    }
+    [int[]] GetTopThree(){
+        return $this.scores | Sort-Object -Descending | Select-Object -First 3
+    }
+    [int[]] GetLatest() {
+        return $this.scores | select-object -Last 1
+    }
+    [int] GetPersonalBest(){
+        return ($this.scores | Sort-Object -Descending)[0]
     }
 }
