252f3f42ac3fa6a65b3fcf9b9f06af1e886c82f5
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-01 14:08:08 -0400

    pf

--- powershell/prime-factors/PrimeFactors.ps1
+++ powershell/prime-factors/PrimeFactors.ps1
@@ -1,22 +1,13 @@
-Function Invoke-PrimeFactors() {
-    <#
-    .SYNOPSIS
-    Calculate the prime factors of a given natural number.
-
-    .DESCRIPTION
-    Check if the number is divisible by 2. If it is, divide it by 2 and add 2 to the list of factors.
-    If it is not, increment the number by 1 and check if it is divisible by 3. If it is, divide it by 3 and add 3 to the list of factors.
-
-    .PARAMETER Number
-    The number to factorize.
-
-    .EXAMPLE
-    Invoke-PrimeFactors -Number 12
-    #>
-    [CmdletBinding()]
-    Param(
-        [Int64]$Number
-    )
-    
-    throw "Please implement this function"
+Function Invoke-PrimeFactors($Number) {
+   $fac = @()
+   $divisor = 2
+    while ($Number -gt 1) {
+      while ($Number % $divisor -eq 0) {
+        $fac += $divisor
+        $Number /= $divisor
+      }
+      ++$divisor
+    }
+    $fac
 }
+
