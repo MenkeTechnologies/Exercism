0e0b244a37b983de9d346e70cefb8ada2f33fdc0
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-02 09:54:54 -0400

    dnd

--- powershell/dnd-character/DndCharacter.ps1
+++ powershell/dnd-character/DndCharacter.ps1
@@ -1,30 +1,27 @@
-<#
-.SYNOPSIS
-    Generate a DnD character with all the classic abilities by rolling 4 dice.
-
-.DESCRIPTION
-    A DnD character starts has, among other things, six abilities:
-    strength, dexterity, constitution, intelligence, wisdom and charisma.
-    These abilities score are determined randomly by throwing dice.
-    Throw 4 six-sided dice and return the the sum of the largest three.
-    Your character's inittial hitpoints are: 10 + your character's constitution modifier.
-    You find your character's constitution modifier by but this formula : (Constitution - 10) / 2 and then round down.
-    
-.EXAMPLE
-    $character = [Character]::new()
-    $character | Format-Table
-    
-    Strength Dexterity Constitution Intelligence Wisdom Charisma HitPoints
-    -------- --------- ------------ ------------ ------ -------- ---------
-           7        10           15            9     13       13        12
-
-#>
 Class Character {
+    [int] $Strength
+    [int] $Dexterity
+    [int] $Constitution
+    [int] $Intelligence
+    [int] $Wisdom
+    [int] $Charisma
+    [int] $HitPoints
     Character() {
-        Throw "Please implement this class"
+        $this.Strength = [Character]::Generate()
+        $this.Dexterity = [Character]::Generate()
+        $this.Constitution = [Character]::Generate()
+        $this.Intelligence = [Character]::Generate()
+        $this.Wisdom = [Character]::Generate()
+        $this.Charisma = [Character]::Generate()
+        $this.HitPoints = 10 + [Character]::GetModifier($this.Constitution)
     }
-
+    static [int] Generate() {
+        return (1..4 | ForEach-Object {(Get-Random -max 6) + 1} | Sort-Object -Descending | `
+            Select-Object -First 3 | Measure-Object -Sum).Sum
+    }
+    [int] Ability() {
+        return [Character]::Generate()
+    }
     static [int] GetModifier([int]$Score) {
-        Throw "Please implement this function"
+        return [Math]::Floor(($Score - 10) / 2)
     }
-}
