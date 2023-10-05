7a1f48f8e945e1582893add0ca40d8ea0d09b668
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-05 13:59:26 -0400

    sh

--- vbnet/secret-handshake/SecretHandshake.vb
+++ vbnet/secret-handshake/SecretHandshake.vb
@@ -1,7 +1,19 @@
 Imports System
+Imports System.Collections.Generic
 
 Public Module SecretHandshake
+    Private ReadOnly dict As IDictionary(Of Integer, String) = New Dictionary(Of Integer, String) From {
+        {1, "wink"},
+        {2, "double blink"},
+        {4, "close your eyes"},
+        {8, "jump"}
+    }
     Public Function Commands(ByVal commandValue As Integer) As String()
-        Throw New NotImplementedException("You need to implement this function.")
+        Dim lst = New List(Of String)()
+        For Each pair In dict
+            If (commandValue And pair.Key) > 0 Then lst.Add(pair.Value)
+        Next
+        If (commandValue And 16) > 0 Then lst.Reverse()
+        Return lst.ToArray()
     End Function
 End Module
