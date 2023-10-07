31a366205a4c6c21f9601b2745c21b20bc372338
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-07 10:55:53 -0400

    sieve

--- tcl/sieve/sieve.tcl
+++ tcl/sieve/sieve.tcl
@@ -1,3 +1,15 @@
 proc primes {limit} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    for {set i 2} {$i <= $limit} {incr i} { set lst($i) 1 }
+    for {set n 2} {$n <= $limit} {incr n} {
+        if {!$lst($n)} { continue }
+        for {set k [expr {$n * 2}]} {$k <= $limit} {incr k $n} { set lst($k) 0 }
+    }
+    set results {}
+    foreach {n ok} [array get lst] {
+        if $ok {
+            lappend results $n
+        }
+    }
+    lsort -integer $results
 }
+
