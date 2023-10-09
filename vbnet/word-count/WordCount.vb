0f2b8447a26a300ee8fa761f203aa64371b25bd0
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-09 10:43:52 -0400

    wc

--- vbnet/word-count/WordCount.vb
+++ vbnet/word-count/WordCount.vb
@@ -1,8 +1,9 @@
-Imports System
 Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text.RegularExpressions
 
 Public Module WordCount
-    Public Function CountWords(ByVal phrase As String) As IDictionary(Of String, Integer)
-        Throw New NotImplementedException("You need to implement this function.")
+    Public Function CountWords(ByVal s As String) As IDictionary(Of String, Integer)
+        Return Regex.Matches(s, "\b[\w']+\b").Select(Function(m) m.Value).GroupBy(Function(w) w.ToLowerInvariant()).ToDictionary(Function(kv) kv.Key, Function(kv) kv.Count())
     End Function
 End Module
