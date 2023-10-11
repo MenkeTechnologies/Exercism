a671f0141eada6a846b61475f221404fc0df9826
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-11 10:34:51 -0400

    nc

--- scala/etl/src/main/scala/Etl.scala
+++ scala/etl/src/main/scala/Etl.scala
@@ -1,18 +1,8 @@
 
 object Etl {
-  def transform(intToStrings: Map[Int, Seq[String]]): Map[String, Int] = {
-
-    var map = Map[String, Int]()
-
-    for ((k, v) <- intToStrings) {
-      for (elem <- v) {
-        map += elem.toLowerCase -> k
-
-      }
+  def transform(intToStrings: Map[Int, Seq[String]]): Map[String, Int] =
+    intToStrings.flatMap {
+      case (k, v) => v.map(e => (e.toLowerCase, k))
     }
-
-    map
-
-  }
 
 }
