fb680377ffa2f63a5984100b8061599b34efaab9
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-18 09:26:59 -0400

    scrab

--- zig/scrabble-score/scrabble_score.zig
+++ zig/scrabble-score/scrabble_score.zig
@@ -1,4 +1,16 @@
+const std = @import("std");
+
 pub fn score(s: []const u8) u32 {
-    _ = s;
-    @compileError("please implement the score function");
+    var sum: u32 = 0;
+    for (s) |c| sum += switch (std.ascii.toUpper(c)) {
+        'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' => 1,
+        'D', 'G' => 2,
+        'B', 'C', 'M', 'P' => 3,
+        'F', 'H', 'V', 'W', 'Y' => 4,
+        'K' => 5,
+        'J', 'X' => 8,
+        'Q', 'Z' => 10,
+        else => 1,
+    };
+    return sum;
 }
