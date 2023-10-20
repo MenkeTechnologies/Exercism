a0219026b289eb315eb3d1a9d97a9b687d82bbb0
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-20 09:59:07 -0400

    pn

--- zig/perfect-numbers/perfect_numbers.zig
+++ zig/perfect-numbers/perfect_numbers.zig
@@ -1,11 +1,16 @@
+const std = @import("std");
 pub const Classification = enum {
     deficient,
     perfect,
     abundant,
 };
-
 /// Asserts that `n` is nonzero.
 pub fn classify(n: u64) Classification {
-    _ = n;
-    @compileError("please implement the classify function");
+    std.debug.assert(n > 0);
+    var sum: u64 = 0;
+    for (1..n) |i| {
+        if (@mod(n, i) == 0)
+            sum += i;
+    }
+    return if (sum == n) .perfect else if (sum > n) .abundant else .deficient;
 }
