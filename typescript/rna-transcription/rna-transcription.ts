8f3141b65113375a74525384561ffefa7ebc9887
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-21 11:12:46 -0400

    pf

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,8 +1,9 @@
-const dict: {[k:string]:string} = {
-    G: "C", C: "G", T: "A", A: "U"}
+const dict: {[k:string]:string} = { G: "C", C: "G", T: "A", A: "U"}
+
 const getThrow = (c: string) => {
     const v = dict[c];
     if (!v) throw Error('Invalid input DNA.')
     return v;
 }
+
 export const toRna = (s: string) => s.split("").map(getThrow).join("")
--- vlang/perfect-numbers/perfect-numbers.v
+++ vlang/perfect-numbers/perfect-numbers.v
@@ -1,11 +1,25 @@
 module main
-
 pub enum Number {
 	perfect
 	abundant
 	deficient
 }
 
-// returns a `Result` type
 pub fn classify(candidate int) !Number {
+	if candidate < 1 {
+		return error('Classification is only possible for positive integers.')
+	}
+	mut aliquot_sum := 0
+	for i in 1 .. candidate {
+		if candidate % i == 0 {
+			aliquot_sum += i
+		}
+	}
+	if aliquot_sum == candidate {
+		return Number.perfect
+	} else if aliquot_sum > candidate {
+		return Number.abundant
+	} else {
+		return Number.deficient
+	}
 }
