3ba07788d6f53af32699e16d1c99d17cd5332496
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-24 02:24:46 -0400

    wc

--- powershell/word-count/WordCount.ps1
+++ powershell/word-count/WordCount.ps1
@@ -1,22 +1,8 @@
-Function Get-WordCount() {
-    <#
-    .SYNOPSIS
-    Given a phrase, count how many time each word appear.
-
-    .DESCRIPTION
-    Count how many time each word appear in a phrase. Number in string also counted as word, and words are case insensitive.
-
-    .PARAMETER Phrase
-    The phrase to count words.
-
-    .EXAMPLE
-    Get-WordCount -Phrase "Hello, welcome to exercism!"
-    Return: @{ hello = 1; welcome = 1; to = 1; exercism = 1}
-    #>
-    [CmdletBinding()]
-    Param(
-        [string]$Phrase
-    )
-    Throw "Please implement this function"
+function Get-WordCount ($Phrase){
+    $letterApostrophes = $Phrase.ToLower() -replace "(?<!\w)'|'(?!\w)|(?!\w|').", ' '
+    $result = @{}
+    $letterApostrophes -split "[\s_]+" | Where-Object { $_ } |
+    Group-Object -NoElement |
+    ForEach-Object { $result[$_.Name] = $_.Count }
+    $result
 }
-
