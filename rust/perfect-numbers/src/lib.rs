f9ccd8c07d7d1330884edfae7f940ee6f5300d56
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-24 00:03:32 -0400

    pc

--- rust/perfect-numbers/src/lib.rs
+++ rust/perfect-numbers/src/lib.rs
@@ -12,14 +12,6 @@ pub fn classify(num: u64) -> Option<Classification> {
         None
     } else {
         let sum = (1..num).filter(|&f| num % f == 0).sum::<u64>();
-
-        let mut c = Perfect;
-        if sum < num {
-            c = Deficient
-        } else if sum > num {
-            c = Abundant
-        }
-
-        Some(c)
+        Some(if sum < num { Deficient } else if sum > num { Abundant } else { Perfect })
     }
 }
