5b97c5db4acffcd815a2f9dfa4371a78e9f88599
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-25 10:22:12 -0400

    annalyn

--- php/annalyns-infiltration/AnnalynsInfiltration.php
+++ php/annalyns-infiltration/AnnalynsInfiltration.php
@@ -1,33 +1,16 @@
 <?php
-
 class AnnalynsInfiltration
 {
-    public function canFastAttack($is_knight_awake)
-    {
-        throw new \BadFunctionCallException("Implement the function");
+    public function canFastAttack($is_knight_awake){
+        return !$is_knight_awake;
     }
-
-    public function canSpy(
-        $is_knight_awake,
-        $is_archer_awake,
-        $is_prisoner_awake
-    ) {
-        throw new \BadFunctionCallException("Implement the function");
+    public function canSpy($is_knight_awake, $is_archer_awake, $is_prisoner_awake) {
+        return $is_knight_awake || $is_archer_awake || $is_prisoner_awake;
     }
-
-    public function canSignal(
-        $is_archer_awake,
-        $is_prisoner_awake
-    ) {
-        throw new \BadFunctionCallException("Implement the function");
+    public function canSignal($is_archer_awake, $is_prisoner_awake) {
+        return $is_prisoner_awake && !$is_archer_awake;
     }
-
-    public function canLiberate(
-        $is_knight_awake,
-        $is_archer_awake,
-        $is_prisoner_awake,
-        $is_dog_present
-    ) {
-        throw new \BadFunctionCallException("Implement the function");
+    public function canLiberate($is_knight_awake, $is_archer_awake, $is_prisoner_awake, $is_dog_present) {
+        return $is_dog_present ? !$is_archer_awake : $is_prisoner_awake && !$is_archer_awake && !$is_knight_awake;
     }
 }
