8b78ee7b64e8fa4766543f1df667dfe697a2b8ed
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-26 10:41:45 -0400

    rcd

--- racket/resistor-color-duo/resistor-color-duo.rkt
+++ racket/resistor-color-duo/resistor-color-duo.rkt
@@ -2,5 +2,21 @@
 
 (provide color-code)
 
+(define color-values
+  	'(("black" . 0)
+	  ("brown" . 1)
+	  ("red" . 2)
+	  ("orange" . 3)
+	  ("yellow" . 4)
+	  ("green" . 5)
+	  ("blue" . 6)
+	  ("violet" . 7)
+	  ("grey" . 8)
+	  ("white" . 9)))
+
+(define (value color) (cdr (assoc color color-values)))
+
 (define (color-code color)
-  (error "Not implemented yet"))
+  (string->number
+   (string-join (map (lambda (colo) (number->string (value colo))) (take color (min 2 (length color)))) "")))
+
