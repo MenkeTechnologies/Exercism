cd414a7f3ab914186786bbaa8a9af71406f5556c
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-27 13:07:58 -0400

    hs

--- racket/high-scores/high-scores.rkt
+++ racket/high-scores/high-scores.rkt
@@ -4,5 +4,17 @@
 
 (define high-scores%
   (class object%
-    ; implement me!
-    (super-new)))
+    (init values)
+    (super-new)
+
+    (define scores-list values)
+
+    (define/public (scores) scores-list)
+
+    (define/public (latest) (last scores-list))
+
+    (define/public (personal-best) (argmax identity scores-list))
+
+    (define/public (personal-top-three)
+      (take (sort scores-list >)
+            (min (length scores-list) 3)))))
