d02eccded48dd9ed411643257a25852e45998e92
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-27 13:47:21 -0400

    darts

--- racket/darts/darts.rkt
+++ racket/darts/darts.rkt
@@ -3,4 +3,9 @@
 (provide score)
 
 (define (score x y)
-  (error "Not implemented yet"))
+  (define dist (sqrt (+ (sqr x) (sqr y))))
+  (cond
+    [(> dist 10) 0]
+    [(> dist 5) 1]
+    [(> dist 1) 5]
+    [else 10]))
