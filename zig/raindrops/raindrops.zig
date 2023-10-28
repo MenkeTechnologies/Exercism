6dba655ec52a8d851e03c504201bbf5061bda4b7
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-28 14:04:51 -0400

    rd

--- zig/raindrops/raindrops.zig
+++ zig/raindrops/raindrops.zig
@@ -1,5 +1,10 @@
+const fmt = @import("std").fmt;
+
 pub fn convert(buffer: []u8, n: u32) []const u8 {
-    _ = buffer;
-    _ = n;
-    @compileError("please implement the convert function");
+    const res = fmt.bufPrint(buffer, "{s}{s}{s}", .{
+        if (n % 3 == 0) "Pling" else "",
+        if (n % 5 == 0) "Plang" else "",
+        if (n % 7 == 0) "Plong" else "",
+    }) catch unreachable;
+    return if (res.len > 0) res else fmt.bufPrint(buffer, "{d}", .{n}) catch unreachable;
 }
