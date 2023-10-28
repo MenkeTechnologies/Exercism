74ea689b704074e0bc157b53a0b67373be4a880d
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-28 14:08:44 -0400

    more exe

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -6,15 +6,11 @@ function usage(){
 }
 
 
-arg1=$1
-arg2=$2
-
-
 if (( $# < 2 )); then
     usage
 fi
 
-if (( ${#arg1} != ${#arg2} )); then
+if (( ${#1} != ${#2} )); then
 
     echo 'left and right strands must be of equal length'
     exit 1
@@ -22,11 +18,11 @@ fi
 
 cnt=0
 
-for (( i = 0; i < ${#arg1}; i++ )); do
-    a="${arg1:$i:1}"
-    b="${arg2:$i:1}"
+for (( i = 0; i < ${#1}; i++ )); do
+    c1="${1:$i:1}"
+    c2="${2:$i:1}"
 
-    if [[ "$a" != "$b" ]]; then
+    if [[ "$c1" != "$c2" ]]; then
         ((++cnt))
     fi
 done
--- zig/raindrops/raindrops.zig
+++ zig/raindrops/raindrops.zig
@@ -1,10 +1,10 @@
 const fmt = @import("std").fmt;
 
-pub fn convert(buffer: []u8, n: u32) []const u8 {
-    const res = fmt.bufPrint(buffer, "{s}{s}{s}", .{
+pub fn convert(buf: []u8, n: u32) []const u8 {
+    const s = fmt.bufPrint(buf, "{s}{s}{s}", .{
         if (n % 3 == 0) "Pling" else "",
         if (n % 5 == 0) "Plang" else "",
         if (n % 7 == 0) "Plong" else "",
     }) catch unreachable;
-    return if (res.len > 0) res else fmt.bufPrint(buffer, "{d}", .{n}) catch unreachable;
+    return if (s.len > 0) s else fmt.bufPrint(buf, "{d}", .{n}) catch unreachable;
 }
