ba65e2e36d268dda159c8d899351beb7393a5de9
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-29 21:52:57 -0400

    sum mult

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -1,30 +1,26 @@
 #!/usr/bin/env bash
-function usage(){
 
+function usage(){
     echo "Usage: hamming.sh <string1> <string2>"
     exit 1
 }
-
 
 if (( $# < 2 )); then
     usage
 fi
 
 if (( ${#1} != ${#2} )); then
-
     echo 'left and right strands must be of equal length'
     exit 1
 fi
 
 cnt=0
 
-for (( i = 0; i < ${#1}; i++ )); do
+for (( i = 0; i < ${#1}; ++i )); do
     c1="${1:$i:1}"
     c2="${2:$i:1}"
 
-    if [[ "$c1" != "$c2" ]]; then
-        ((++cnt))
-    fi
+    [[ "$c1" != "$c2" ]] && ((++cnt))
 done
 
 
--- zig/sum-of-multiples/sum_of_multiples.zig
+++ zig/sum-of-multiples/sum_of_multiples.zig
@@ -1,9 +1,14 @@
-const std = @import("std");
-const mem = std.mem;
-
-pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) !u64 {
-    _ = allocator;
-    _ = factors;
-    _ = limit;
-    @compileError("please implement the sum function");
+pub fn sum(_: anytype, factors: []const u32, limit: u32) !u64 {
+    var result: u64 = 0;
+    var n: u64 = 1;
+    while (n < limit) {
+        for (factors) |f| {
+            if (f != 0 and n % f == 0) {
+                result += n;
+                break;
+            }
+        }
+        n += 1;
+    }
+    return result;
 }
