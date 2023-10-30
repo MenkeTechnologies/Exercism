4d7ed7c8f82f6f58687506bc0db5cb6e571f758d
MenkeTechnologies <linux.dev25@gmail.com>
2023-10-30 10:54:21 -0400

    ayb

--- zig/all-your-base/all_your_base.zig
+++ zig/all-your-base/all_your_base.zig
@@ -1,23 +1,16 @@
 const std = @import("std");
-const mem = std.mem;
+pub const ConversionError = error{ InvalidInputBase, InvalidOutputBase, InvalidDigit };
 
-pub const ConversionError = error{
-    InvalidInputBase,
-    InvalidOutputBase,
-    InvalidDigit,
-};
+pub fn convert(allocator: std.mem.Allocator, digits: []const u32, in_base: u32, out_base: u32) ![]u32 {
+    if (in_base < 2) return error.InvalidInputBase else if (out_base < 2) return error.InvalidOutputBase;
 
-/// Converts `digits` from `input_base` to `output_base`, returning a slice of digits.
-/// Caller owns the returned memory.
-pub fn convert(
-    allocator: mem.Allocator,
-    digits: []const u32,
-    input_base: u32,
-    output_base: u32,
-) (mem.Allocator.Error || ConversionError)![]u32 {
-    _ = allocator;
-    _ = digits;
-    _ = input_base;
-    _ = output_base;
-    @compileError("please implement the convert function");
+    var acc: u32 = 0;
+
+    for (digits) |d| {
+        if (d >= in_base) return error.InvalidDigit;
+        acc = acc * in_base + d;
+    }
+    var lst = if (acc > 0) std.ArrayList(u32).init(allocator) else return allocator.dupe(u32, &.{0});
+    while (acc > 0) : (acc /= out_base) try lst.insert(0, acc % out_base);
+    return lst.toOwnedSlice();
 }
