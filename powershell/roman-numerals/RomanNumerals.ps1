62b26261c650d02b3cf52b85588daa0e27544546
MenkeTechnologies <linux.dev25@gmail.com>
2023-11-03 13:47:53 -0400

    rn

--- powershell/roman-numerals/RomanNumerals.ps1
+++ powershell/roman-numerals/RomanNumerals.ps1
@@ -1,23 +1,26 @@
-Function Get-RomanNumerals() {
-    <#
-    .SYNOPSIS
-    Given a number, convert it into a roman numeral.
-
-    .DESCRIPTION
-    Convert a positive integer into a string representation of that integer in roman numeral form.
-    
-    .PARAMETER Number
-    The number to turn into roman numeral.
-
-    .EXAMPLE
-    Get-RomanNumerals -Number 1
-    return: 'I'
-    Get-RomanNumerals -Number 3999
-    return: 'MMMCMXCIX'
-    #>
-    [CmdletBinding()]
-    Param(
-        [int]$Number
-    )
-    Throw "Please implement this function."
+$RomanNumerals = @{
+    1000 = 'M'
+    900 = 'CM'
+    500 = 'D'
+    400 = 'CD'
+    100 = 'C'
+    90 = 'XC'
+    50 = 'L'
+    40 = 'XL'
+    10 = 'X'
+    9 = 'IX'
+    5 = 'V'
+    4 = 'IV'
+    1 = 'I'
+}
+Function Get-RomanNumerals($Number) {
+    if ($Number -le 0 -or $Number -ge 4000) {
+        Throw "Number has to be positive integer in range of 1-3999."
+    }
+    $res = ''
+    foreach ($k in $RomanNumerals.Keys | Sort-Object -Descending) {
+        $res += $RomanNumerals[$k] * [Math]::floor($Number / $k)
+        $Number %= $k
+    }
+    $res
 }
