916f9f62e9913a83a104296a2820ee962e11f51a
MenkeTechnologies <linux.dev25@gmail.com>
2023-11-05 10:21:16 -0500

    etl

--- vlang/etl/etl.v
+++ vlang/etl/etl.v
@@ -1,4 +1,12 @@
 module main
 
 fn transform(legacy map[int][]rune) map[rune]int {
+	mut res := map[rune]int{}
+	for score, letters in legacy {
+		for letter in letters {
+			res[letter.str().to_lower()[0]] = score
+		}
+	}
+	return res
 }
+
