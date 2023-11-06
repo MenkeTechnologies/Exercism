2a92a06724709718bfe203a4a66ef205d2c456ae
MenkeTechnologies <linux.dev25@gmail.com>
2023-11-06 11:34:59 -0500

    ayb

--- common-lisp/all-your-base/all-your-base.lisp
+++ common-lisp/all-your-base/all-your-base.lisp
@@ -4,4 +4,16 @@
 
 (in-package :all-your-base)
 
-(defun rebase (list-digits in-base out-base))
+(defun undigits (list-digits base)
+  (reduce (lambda (acc n) (+ (* acc base) n)) list-digits :initial-value 0))
+
+(defun digits (sum base)
+  (if (< sum base)
+      (list sum)
+      (cons (rem sum base) (digits (truncate sum base) base))))
+
+(defun rebase (list-digits in-base out-base)
+  (and (> in-base 1)
+       (> out-base 1)
+       (every (lambda (n) (< -1 n in-base)) list-digits)
+      (nreverse (digits (undigits list-digits in-base) out-base))))
