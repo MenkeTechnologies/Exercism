8876d7e8c7540aa8944d54fe8472b8b3c43ace55
MenkeTechnologies <linux.dev25@gmail.com>
2023-11-13 09:41:33 -0500

    grains

--- vbnet/grains/Grains.vb
+++ vbnet/grains/Grains.vb
@@ -1,11 +1,16 @@
 Imports System
-
 Public Module Grains
     Public Function Square(ByVal n As Integer) As ULong
-        Throw New NotImplementedException("You need to implement this function.")
+        If n < 1 OR n > 64
+          Throw New ArgumentOutOfRangeException()
+        End If
+        Return 2 ^ (n-1)
     End Function
-
     Public Function Total() As ULong
-        Throw New NotImplementedException("You need to implement this function.")
+        Dim totalSum as ULong = 0
+        For n As Integer = 1 To 64
+          totalSum += Square(n)
+        Next
+        Return totalSum
     End Function
 End Module
