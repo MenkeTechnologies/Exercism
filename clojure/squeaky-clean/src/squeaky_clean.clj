e0d15ab5a58346e0444b46263085856559d94ece
MenkeTechnologies <linux.dev25@gmail.com>
2023-11-15 01:09:26 -0500

    sc

--- clojure/squeaky-clean/src/squeaky_clean.clj
+++ clojure/squeaky-clean/src/squeaky_clean.clj
@@ -1,7 +1,9 @@
 (ns squeaky-clean
   (:require [clojure.string :as str]))
-
-(defn clean
-  "TODO: add docstring"
-  [s]
-  )
+(defn clean [s]
+  (-> s
+      (str/replace #"\s" "_")
+      (str/replace #"\p{IsControl}" "CTRL")
+      (str/replace #"-(\p{Ll})" #(str/upper-case (% 1)))
+      (str/replace #"[^_\p{L}]" "")
+      (str/replace #"[\p{IsGreek}&&\p{IsLowercase}]" "")))
