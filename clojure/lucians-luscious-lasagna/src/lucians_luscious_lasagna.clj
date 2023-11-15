cbb0a7a2ab43d2964ed32962e071436968263596
MenkeTechnologies <linux.dev25@gmail.com>
2023-11-15 00:57:24 -0500

    rc

--- clojure/lucians-luscious-lasagna/src/lucians_luscious_lasagna.clj
+++ clojure/lucians-luscious-lasagna/src/lucians_luscious_lasagna.clj
@@ -2,19 +2,8 @@
 
 (def expected-time 40)
 
-(defn remaining-time
-  "Takes the actual time in minutes the lasagna has been in the oven,
-   and returns how many minutes the lasagna still has to remain in the oven."
-  [actual-time] (- expected-time actual-time))
+(defn remaining-time [actual-time] (- expected-time actual-time))
 
-(defn prep-time
-  "Takes the number of layers added to the lasagna,
-   and returns how many minutes you spent preparing the lasagna"
-  [num-layers] (* num-layers 2))
+(defn prep-time [num-layers] (* num-layers 2))
 
-(defn total-time
-  "Takes the number of layers of lasagna and the actual time in minutes it has been in the oven.
-   Returns how many minutes in total you've worked on cooking the lasagna"
-  [num-layers actual-time] (->> num-layers
-                               prep-time
-                               (+ actual-time)))
+(defn total-time [num-layers actual-time] (->> num-layers prep-time (+ actual-time)))
