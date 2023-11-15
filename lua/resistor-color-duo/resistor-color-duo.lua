7a020de1c3c19dfc7fb79a6a77c46d5943c55efb
MenkeTechnologies <linux.dev25@gmail.com>
2023-11-15 11:38:44 -0500

    rcd

--- lua/resistor-color-duo/resistor-color-duo.lua
+++ lua/resistor-color-duo/resistor-color-duo.lua
@@ -1,5 +1,18 @@
-return {
-  value = function(colors)
+local color_codes = {
+    black = 0,
+    brown = 1,
+    red = 2,
+    orange = 3,
+    yellow = 4,
+    green = 5,
+    blue = 6,
+    violet = 7,
+    grey = 8,
+    white = 9
+}
 
-  end
+return {
+    value = function(colors)
+        return color_codes[colors[1]] * 10 + color_codes[colors[2]]
+    end
 }
