b55afba8bd75f2d20972d818e560697eda780f0f
MenkeTechnologies <linux.dev25@gmail.com>
2023-11-21 09:15:09 -0500

    sieve

--- powershell/sieve/Sieve.ps1
+++ powershell/sieve/Sieve.ps1
@@ -1,28 +1,12 @@
-Function Invoke-Sieve() {
-    <#
-    .SYNOPSIS
-    Create a program that implements the Sieve of Eratosthenes algorithm to find prime numbers.
-
-    .DESCRIPTION
-    The function take a limit (inclusive) and use the Sieve of Eratosthenes to find all the prime numbers between 2 and the limit.
-    To use the Sieve of Eratosthenes, you first create a list of all the numbers between 2 and your given number.
-    Then you repeat the following steps:
-    1. Find the next unmarked number in your list. This is a prime number.
-    2. Mark all the multiples of that prime number as composite (not prime).
-
-    Repeating these steps until you've gone through every number in your list.
-    At the end, all the unmarked numbers are prime.
-
-    .PARAMETER Limit
-    The limit (inclusive) to find all the prime numbers.
-
-    .EXAMPLE
-    Invoke-Sieve -Limit 10
-    Return: @(2, 3, 5, 7)
-     #>
-    [CmdletBinding()]
-    Param(
-        [int]$Limit
-    )
-    Throw "Please implement this function"
+Function Invoke-Sieve($Limit) {
+    $lst = 0..$Limit
+    $lst[1] = 0
+    for($i = 2; $i -le [Math]::floor([Math]::sqrt($Limit)) ; ++$i) {
+        if($lst[$i]) {
+            for($j = $i * 2; $j -le $Limit ; $j += $i) {
+                $lst[$j] = 0
+            }
+        }
+    }
+    $lst | ? { $_ }
 }
