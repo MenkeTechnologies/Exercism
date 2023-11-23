103dd2d0df84c13ae01a076ea571e953aeabdc30
MenkeTechnologies <linux.dev25@gmail.com>
2023-11-23 10:39:34 -0500

    gs

--- powershell/grade-school/GradeSchool.ps1
+++ powershell/grade-school/GradeSchool.ps1
@@ -1,21 +1,27 @@
 class Student {
     [int]$Grade
     [string]$Name
-    Student([int]$grade, [string]$name) {
-        $this.Grade = $grade
-        $this.Name = $name
+    Student([int]$Grade, [string]$Name) {
+        $this.Grade = $Grade
+        $this.Name = $Name
     }
 }
 class Roster {
-    [Student[]]$Student = @()
-
-    [void] AddStudent([int]$grade, [string]$name) {
-        $this.Student += [Student]::new($grade, $name)
+    [Student[]]$Students
+    Roster() {
+        $this.Students = @()
+    }
+    [bool] AddStudent([int]$Grade, [string]$Name) {
+        if (-not ($this.Students | ? Name -eq $Name)) {
+            $this.Students += [Student]::new($Grade, $Name)
+            return $true
+        }
+        return $false
     }
     [Student[]] GetRoster() {
-        $this.Student | Sort-Object -Property Grade,Name
+        return $this.Students | Sort-Object Grade, Name
     }
-    [Student[]] GetRoster([int]$grade) {
-        $this.Student | Where-Object -Property Grade -eq $grade | Sort-Object -Property Name
+    [Student[]] GetRoster([int]$Grade) {
+        return $this.Students | ? Grade -eq $Grade | Sort-Object Name
     }
 }
