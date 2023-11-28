e09f2ef21f2466696f6e4c3599374f5111e6b83d
MenkeTechnologies <linux.dev25@gmail.com>
2023-11-28 13:19:52 -0500

    pc

--- lua/pop-count/pop-count.lua
+++ lua/pop-count/pop-count.lua
@@ -1,7 +1,10 @@
-local PopCount = {}
-
-function PopCount.eggCount(number)
-
-end
-
-return PopCount
+return {
+    egg_count = function(n)
+        local cnt = 0
+        while n > 0 do
+            cnt = cnt + n % 2
+            n = n >> 1
+        end
+        return cnt
+    end
+}
