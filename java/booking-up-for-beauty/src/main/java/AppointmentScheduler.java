8811fe8c1fa13021990aba7c9968791d8d312798
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-02 01:04:11 -0500

    bufb

--- java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
+++ java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -1,24 +1,31 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 class AppointmentScheduler {
+    private static final DateTimeFormatter APPT_FMT = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+    private static final DateTimeFormatter DESC_FMT = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy, 'at' h:mm a");
+
     public LocalDateTime schedule(String appointmentDateDescription) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.schedule() method");
+        return LocalDateTime.parse(appointmentDateDescription, APPT_FMT);
     }
 
     public boolean hasPassed(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.hasPassed() method");
+        return LocalDateTime.now().isAfter(appointmentDate);
     }
 
     public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.isAfternoonAppointment() method");
+        int h = appointmentDate.getHour();
+
+        return h > 11 && h < 18;
     }
 
     public String getDescription(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getDescription() method");
+        return "You have an appointment on %s.".formatted(appointmentDate.format(DESC_FMT));
     }
 
     public LocalDate getAnniversaryDate() {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getAnniversaryDate() method");
+        return LocalDate.of(2023, 9, 15);
     }
 }
