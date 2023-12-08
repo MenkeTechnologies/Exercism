f2fe1f44da3d5f5887f7cf8486e12174e0279cbf
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-08 12:21:07 -0500

    nt

--- vbnet/nucleotide-count/NucleotideCount.vb
+++ vbnet/nucleotide-count/NucleotideCount.vb
@@ -1,8 +1,11 @@
-Imports System
-Imports System.Collections.Generic
-
 Public Module NucleotideCount
     Public Function Count(ByVal sequence As String) As IDictionary(Of Char, Integer)
-        Throw New NotImplementedException("You need to implement this function.")
+        If Not sequence.All(Function(c) "ACGT".Contains(c)) Then Throw New ArgumentException()
+        dim dict = New Dictionary(Of Char, Integer) From {{"A", 0}, {"C", 0}, {"G", 0}, {"T", 0}}
+
+        Return sequence.ToCharArray().Aggregate(dict, Function(acc, c)
+                                                    acc(c) += 1
+                                                    Return acc
+                                                End Function)
     End Function
 End Module
