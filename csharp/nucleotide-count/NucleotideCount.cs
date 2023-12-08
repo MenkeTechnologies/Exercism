e6541ceb64d3cbe540c94f375a60f67cb71404e5
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-08 12:52:23 -0500

    nt

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -1,28 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        IDictionary<char, int> map = new Dictionary<char, int>();
-        map['A'] = 0;
-        map['C'] = 0;
-        map['G'] = 0;
-        map['T'] = 0;
-
-        foreach (var ch in sequence.ToCharArray())
+        var map = new Dictionary<char, int> { { 'A', 0 }, { 'G', 0 }, { 'C', 0 }, { 'T', 0 } };
+        return sequence.Aggregate(map, (acc, c) =>
         {
-            if (map.ContainsKey(ch))
-            {
-                ++map[ch];
-            }
-            else
-            {
-                throw new ArgumentException($"invalid char {ch}");
-            }
-        }
-
-        return map;
+            if (!acc.ContainsKey(c)) { throw new ArgumentException("invalid char"); }
+            ++acc[c];
+            return acc;
+        });
     }
 }
