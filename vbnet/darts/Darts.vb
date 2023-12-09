0ad592926f0eb3343aa805e1d3dc10d87b1e9838
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-09 12:33:55 -0500

    darts

--- vbnet/darts/Darts.vb
+++ vbnet/darts/Darts.vb
@@ -1,7 +1,16 @@
 Imports System
 
 Public Module Darts
-    Public Function Score(ByVal x As Double, ByVal y As Double) As Integer
-        Throw New NotImplementedException("You need to implement this function")
-    End Function
+  Public Function Score(ByVal x As Double, ByVal y As Double) As Integer
+    Dim dist = Math.Sqrt(x*x + y*y)
+    If dist <= 1
+      Return 10
+    ElseIf dist <= 5
+      Return 5
+    ElseIf dist <= 10
+      Return 1
+    Else
+      Return 0
+    End If
+  End Function
 End Module
