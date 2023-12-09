63d02bee094edf7947bb2546d21d84d97ad29a09
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-09 05:23:09 -0500

    lsp

--- vbnet/largest-series-product/LargestSeriesProduct.vb
+++ vbnet/largest-series-product/LargestSeriesProduct.vb
@@ -2,6 +2,11 @@ Imports System
 
 Public Module LargestSeriesProduct
     Public Function GetLargestProduct(ByVal digits As String, ByVal span As Integer) As Long
-        Throw New NotImplementedException("You need to implement this function.")
+        If span > digits.Length OrElse span < 0 OrElse digits.Any(Function(c) c > "9"c Or c < "0"c) Then
+            Throw New ArgumentException("invalid arguments")
+        End If
+        Dim ints = digits.Select(Function(c) Convert.ToInt32(c) - Convert.ToInt32("0"c)).ToList()
+        Return Enumerable.Range(0, digits.Length - span + 1).Select(Function(i) ints.Skip(i).Take(span)) _
+            .Max(Function(chunk) chunk.Aggregate(Function(acc, n) acc * n))
     End Function
 End Module
