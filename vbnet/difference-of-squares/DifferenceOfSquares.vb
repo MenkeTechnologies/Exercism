bc380ff5a5a6f02464dd31d4520b4c8358a4790f
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-10 14:22:06 -0500

    dos

--- vbnet/difference-of-squares/DifferenceOfSquares.vb
+++ vbnet/difference-of-squares/DifferenceOfSquares.vb
@@ -2,14 +2,14 @@ Imports System
 
 Public Module DifferenceOfSquares
     Public Function CalculateSquareOfSum(ByVal max As Integer) As Integer
-        Throw New NotImplementedException("You need to implement this function.")
+        Return CInt(Math.Pow(Enumerable.Range(1, max).Aggregate(Function(acc, n) acc + n), 2))
     End Function
 
     Public Function CalculateSumOfSquares(ByVal max As Integer) As Integer
-        Throw New NotImplementedException("You need to implement this function.")
+        Return CInt(Enumerable.Range(1, max).Select(Function(n) Math.Pow(n, 2)).Aggregate(Function(acc, n) acc + n))
     End Function
 
     Public Function CalculateDifferenceOfSquares(ByVal max As Integer) As Integer
-        Throw New NotImplementedException("You need to implement this function.")
+        Return CalculateSquareOfSum(max) - CalculateSumOfSquares(max)
     End Function
 End Module
