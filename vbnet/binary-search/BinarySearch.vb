3078d34901a9225b772e987b650c36cd60399d91
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-11 12:30:38 -0500

    bs

--- vbnet/binary-search/BinarySearch.vb
+++ vbnet/binary-search/BinarySearch.vb
@@ -2,6 +2,18 @@ Imports System
 
 Public Module BinarySearch
     Public Function Find(ByVal input As Integer(), ByVal value As Integer) As Integer
-        Throw New NotImplementedException("You need to implement this function.")
+        Dim lo = 0
+        Dim hi = input.Length - 1
+        While lo <= hi
+            Dim mid = Math.Floor((lo + hi)/2)
+            If input(mid) = value
+                Return mid
+            ElseIf value < input(mid)
+                hi = mid - 1
+            Else
+                lo = mid + 1
+            End If
+        End While
+        Return -1
     End Function
 End Module
