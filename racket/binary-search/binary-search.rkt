5704dc2be84ea45bd55b2af904d0d00aa2023cb6
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-16 01:30:32 -0500

    bs

--- racket/binary-search/binary-search.rkt
+++ racket/binary-search/binary-search.rkt
@@ -3,4 +3,13 @@
 (provide binary-search)
 
 (define (binary-search array value)
-  (error "Not implemented yet"))
+  (define (aux lo hi)
+    (cond
+      [(> lo hi) (error "Value not in array")]
+      [else (define mid (quotient (+ hi lo) 2))
+            (cond
+              [(< value (list-ref array mid)) (aux lo (sub1 mid))]
+              [(< (list-ref array mid) value) (aux (add1 mid) hi)]
+              [else mid])]))
+  (aux 0 (sub1 (length array))))
+
