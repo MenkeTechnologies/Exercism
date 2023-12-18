a75f3834f71b0faff47ee23a44c28c283801b13e
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-18 11:26:28 -0500

    lo

--- powershell/list-ops/ListOps.ps1
+++ powershell/list-ops/ListOps.ps1
@@ -1,94 +1,62 @@
-<#
-.SYNOPSIS
-Implement basic list operations.
-
-.DESCRIPTION
-Implement a series of basic list operations, without using existing functions:
-- `append` (*given two lists, add all items in the second list to the end of the first list*);
-- `concatenate` (*given a series of lists, combine all items in all lists into one flattened list*);
-- `filter` (*given a predicate and a list, return the list of all items for which `predicate(item)` is True*);
-- `length` (*given a list, return the total number of items within it*);
-- `map` (*given a function and a list, return the list of the results of applying `function(item)` on all items*);
-- `foldl` (*given a function, a list, and initial accumulator, fold (reduce) each item into the accumulator from the left*);
-- `foldr` (*given a function, a list, and an initial accumulator, fold (reduce) each item into the accumulator from the right*);
-- `reverse` (*given a list, return a list with all the original items, but in reversed order*).
-
-Note, the ordering in which arguments are passed to the fold functions (`foldl`, `foldr`) is significant.
-
-.EXAMPLE
-Invoke-Append -List1 @(1, 3) -List2 @(2, 4)
-Returns: @(1, 3, 2, 4)
-
-Invoke-Reverse -List @(1, 2, 3, 4, 5)
-Returns: @(5, 4, 3, 2, 1)
-#>
-Function Invoke-Append() {
-    [CmdletBinding()]
-    Param(
-        [object[]]$List1,
-        [object[]]$List2
-    )
-    Throw "Please implement this function"
+Function Invoke-Append($List1, $List2) {
+    $r = $List1 + $List2
+    $r
 }
-
-Function Invoke-Concatenate() {
-    [CmdletBinding()]
-    Param(
-        [object[]]$Lists
-    )
-    Throw "Please implement this function"
+Function Invoke-Concatenate($Lists) {
+    $r = @()
+    foreach ($lst in $Lists){
+        $r += $lst
+    }
+    $r
 }
-
-Function Invoke-Filter() {
-    [CmdletBinding()]
-    Param(
-        [object[]]$List,
-        [scriptblock]$Predicate
-    )
-    Throw "Please implement this function"
+Function Invoke-Filter($List, $Predicate) {
+    $r = @()
+    foreach($n in $List){
+        if ($Predicate.Invoke($n)){
+            $r += $n
+        }
+    }
+    $r
 }
-
-Function Get-Length() {
-    [CmdletBinding()]
-    Param(
-        [object[]]$List
-    )
-    Throw "Please implement this function"
+Function Get-Length($List) {
+    $cnt = 0
+    foreach ($n in $List){
+        $cnt++
+    }
+    $cnt
 }
-
-Function Invoke-Map() {
-    [CmdletBinding()]
-    Param(
-        [object[]]$List,
-        [scriptblock]$Function
-    )
-    Throw "Please implement this function"
+Function Invoke-Map($List, $Function) {
+    $r = @()
+    foreach($n in $List){
+        $r += $Function.Invoke($n)
+    }
+    $r
 }
-
-Function Invoke-Foldl() {
-    [CmdletBinding()]
-    Param(
-        [scriptblock]$Function,
-        [object[]]$List,
-        [object]$Accumulator
-    )
-    Throw "Please implement this function"
+Function Invoke-Foldl($Function, $List, $Accumulator) {
+    $r = $Accumulator
+    foreach($n in $List){
+        $r = &$Function -acc $r -e $n
+    }
+    $r
 }
-
-Function Invoke-Foldr() {
-    [CmdletBinding()]
-    Param(
-        [scriptblock]$Function,
-        [object[]]$List,
-        [object]$Accumulator
-    )
-    Throw "Please implement this function"
+Function Invoke-Foldr($Function, $List, $Accumulator) {
+    $c = (Get-Length $List) - 1
+    $r = $Accumulator
+    for($i=$c; $i -ge 0; $i--){
+        $r = &$Function -e $List[$i] -acc $r
+    }
+    $r
 }
-
-Function Invoke-Reverse() {
-    [CmdletBinding()]
-    Param(
-        [object[]]$List
-    )
-    Throw "Please implement this function"
+Function Invoke-Reverse($List) {
+    $r = @()
+    $c = (Get-Length $List) - 1
+    for($i=$c; $i -ge 0; $i--){
+        if ($List[$i] -is [array]){
+            $r += ,$List[$i]
+        }
+        else{
+            $r += $List[$i]
+        }
+    }
+    $r
 }
