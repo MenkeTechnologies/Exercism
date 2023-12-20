dab594291b0125ee8cb6035b27e4447d2a270a96
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-20 12:21:11 -0500

    acro

--- vbnet/acronym/Acronym.vb
+++ vbnet/acronym/Acronym.vb
@@ -1,7 +1,9 @@
 Imports System
+Imports System.Linq
 
 Public Module Acronym
     Public Function Abbreviate(ByVal phrase As String) As String
-        Throw New NotImplementedException("Please implement this function")
+        Dim separators = {" "c, "-"c, "_"c}
+        Return String.Join("", phrase.Split(separators).Where(Function(s) s.Length).Select(Function(s) s(0))).ToUpper()
     End Function
 End Module
