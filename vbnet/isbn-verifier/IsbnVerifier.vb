05523c5d310a13486b2b2409ea7996f797819bdc
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-21 00:47:21 -0500

    isbn

--- vbnet/isbn-verifier/IsbnVerifier.vb
+++ vbnet/isbn-verifier/IsbnVerifier.vb
@@ -1,7 +1,13 @@
-Imports System
+Imports System.Text.RegularExpressions
 
 Public Module IsbnVerifier
+    Private Const IsbnPattern As String = "^\d(-?)\d{3}\1\d{5}\1[\dX]$"
+
     Public Function IsValid(ByVal number As String) As Boolean
-        Throw New NotImplementedException("You need to implement this function.")
+        Return _
+            Regex.IsMatch(number, IsbnPattern) AndAlso
+            number.Reverse().Where(Function(c) Char.IsLetterOrDigit(c)).Select(
+                Function(c) If(c = "X"c, 10, Integer.Parse(c.ToString()))) _
+                .Select(Function(x, i) x*(i + 1)).Sum() Mod 11 = 0
     End Function
 End Module
