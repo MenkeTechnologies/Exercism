f0c12f8736f492e79d7164a7eed5b80d5dc78d23
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-23 13:54:03 -0500

    qa

--- vbnet/queen-attack/QueenAttack.vb
+++ vbnet/queen-attack/QueenAttack.vb
@@ -12,10 +12,17 @@ End Class
 
 Public Module QueenAttack
     Public Function CanAttack(ByVal white As Queen, ByVal black As Queen) As Boolean
-        Throw New NotImplementedException("You need to implement this function.")
+        Dim dx = Math.Abs(white.Row - black.Row)
+        Dim dy = Math.Abs(white.Column - black.Column)
+
+        Return dx = 0 or dy = 0 or dx = dy
     End Function
 
     Public Function Create(ByVal row As Integer, ByVal column As Integer) As Queen
-        Throw New NotImplementedException("You need to implement this function.")
+        If row < 0 or column < 0 or row > 7 or Column > 7
+            Throw new ArgumentOutOfRangeException()
+        End If
+
+        Return new Queen(row, column)
     End Function
 End Module
