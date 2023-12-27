8b6426bf211c6d7bccb8d3554fe23e14c3230d3c
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-27 09:42:49 -0500

    pt

--- python/plane-tickets/generators.py
+++ python/plane-tickets/generators.py
@@ -1,60 +1,21 @@
-"""Functions to automate Conda airlines ticketing system."""
-
-
 def generate_seat_letters(number):
-    """Generate a series of letters for airline seats.
-
-    :param number: int - total number of seat letters to be generated.
-    :return: generator - generator that yields seat letters.
-
-    Seat letters are generated from A to D.
-    After D it should start again with A.
-
-    Example: A, B, C, D
-
-    """
-
-    pass
+    return ('ABCD'[seat % 4] for seat in range(number))
 
 
 def generate_seats(number):
-    """Generate a series of identifiers for airline seats.
+    seats = range(number + 4 if number >= 13 else number)
+    letters = generate_seat_letters(number)
 
-    :param number: int - total number of seats to be generated.
-    :return: generator - generator that yields seat numbers.
+    return (f'{row}{next(letters)}' for
+            seat in seats if (row := seat // 4 + 1) != 13)
 
-    A seat number consists of the row number and the seat letter.
-
-    There is no row 13.
-    Each row has 4 seats.
-
-    Seats should be sorted from low to high.
-
-    Example: 3C, 3D, 4A, 4B
-
-    """
-
-    pass
 
 def assign_seats(passengers):
-    """Assign seats to passengers.
+    return {passenger: seat for passenger, seat in
+            zip(passengers, generate_seats(len(passengers)))}
 
-    :param passengers: list[str] - a list of strings containing names of passengers.
-    :return: dict - with the names of the passengers as keys and seat numbers as values.
-
-    Example output: {"Adele": "1A", "Björk": "1B"}
-
-    """
-
-    pass
 
 def generate_codes(seat_numbers, flight_id):
-    """Generate codes for a ticket.
-
-    :param seat_numbers: list[str] - list of seat numbers.
-    :param flight_id: str - string containing the flight identifier.
-    :return: generator - generator that yields 12 character long ticket codes.
-
-    """
-
-    pass
+    return (base.ljust(12, '0') for
+            seat in seat_numbers if
+            (base := f'{seat}{flight_id}'))
