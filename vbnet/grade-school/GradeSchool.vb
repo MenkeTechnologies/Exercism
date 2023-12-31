3c1577d5b3331dd506465b37d9ace93d16c9d503
MenkeTechnologies <linux.dev25@gmail.com>
2023-12-31 11:42:01 -0500

    gs

--- vbnet/grade-school/GradeSchool.vb
+++ vbnet/grade-school/GradeSchool.vb
@@ -1,16 +1,25 @@
 Imports System
 Imports System.Collections.Generic
+Imports System.Linq
 
 Public Class GradeSchool
+    Private ReadOnly _roster As New Dictionary(Of String, Integer)()
+
     Public Function Add(ByVal student As String, ByVal grade As Integer) As Boolean
-        Throw New NotImplementedException("You need to implement this function.")
+        If Not _roster.ContainsKey(student) Then
+            _roster.Add(student, grade)
+            Return True
+        Else
+            Return False
+        End If
     End Function
 
     Public Function Roster() As IEnumerable(Of String)
-        Throw New NotImplementedException("You need to implement this function.")
+        Return _roster.OrderBy(Function(kv) kv.Value).ThenBy(Function(kv) kv.Key).Select(Function(kv) kv.Key)
     End Function
 
-    Public Function Grade(ByVal grade As Integer) As IEnumerable(Of String)
-        Throw New NotImplementedException("You need to implement this function.")
+    Public Function Grade(ByVal requestedGrade As Integer) As IEnumerable(Of String)
+        Return _roster.Where(Function(kv) kv.Value = requestedGrade).OrderBy(Function(kv) kv.Key).Select(
+            Function(kv) kv.Key).ToArray()
     End Function
 End Class
