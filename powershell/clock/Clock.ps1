5b18d17fa62bd8b4394c6e407287f85365510c58
MenkeTechnologies <linux.dev25@gmail.com>
2024-01-02 09:38:04 -0500

    clo

--- powershell/clock/Clock.ps1
+++ powershell/clock/Clock.ps1
@@ -1,36 +1,17 @@
-<#
-.SYNOPSIS
-    Implement a clock that handles times without dates.
-
-.DESCRIPTION
-    Implement a clock that handles times without dates in 24 hours format.
-    You should be able to add and subtract minutes to it.
-    Two clocks that represent the same time should be equal to each other.
-    Note: Please try to implement the class and its method instead of using built-in module Datetime.
-
-.EXAMPLE
-    $clock1 = [Clock]::new(5,0)
-    $clock1.ToString()
-    Return: "05:00"
-
-    $clock2 = [Clock]::new(6,-120)
-    $clock2.Add(60).ToString()
-    Return: "05:00"
-
-    $clock1 -eq $clock2
-    Return: $true
-#>
-
 class Clock {
-    Clock() {
-        Throw "Please implement this class"
+    [Int32] $hour = 0
+    [Int32] $minute = 0
+    Clock($h, $m) {
+        $this.hour = (24 + (($h + [Math]::Floor($m / 60)) % 24)) % 24
+        $this.minute = (60 + ($m % 60)) % 60
     }
-
     [string] ToString() {
-        Throw "Please implement this function"
+        return "{0:d2}:{1:d2}" -f $this.hour, $this.minute
     }
-
-    [bool] Equals() {
-        Throw "Please implement this function"
+    [bool] Equals($other) {
+        return $other.hour -eq $this.hour -and $other.minute -eq $this.minute
+    }
+    [Clock] Add($m) {
+        return [Clock]::new($this.hour, $this.minute + $m)
     }
 }
