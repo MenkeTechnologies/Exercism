41a5fc803bf5867b1b08043a0beb94929ea6d362
MenkeTechnologies <linux.dev25@gmail.com>
2024-01-04 09:19:08 -0500

    darts

--- emacs-lisp/darts/darts.el
+++ emacs-lisp/darts/darts.el
@@ -1,14 +1,8 @@
-;;; darts.el --- Darts (exercism)  -*- lexical-binding: t; -*-
-
-;;; Commentary:
-
-;;; Code:
-
+(provide 'darts)
 
 (defun score (x y)
-  (error
-   "Delete this S-Expression and write your own implementation"))
-
-
-(provide 'darts)
-;;; darts.el ends here
+  (let ((dist (sqrt (+ (* x x) (* y y)))))
+	(cond ((> dist 10) 0)
+		  ((> dist 5) 1)
+		  ((> dist 1) 5)
+		  (t 10))))
