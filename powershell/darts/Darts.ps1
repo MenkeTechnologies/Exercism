e478a5135e3046bc44e86519131d8bf6a3a17e9d
MenkeTechnologies <linux.dev25@gmail.com>
2024-01-04 10:41:47 -0500

    darts

--- powershell/darts/Darts.ps1
+++ powershell/darts/Darts.ps1
@@ -1,14 +1,14 @@
 Function Invoke-Darts($X, $Y) {
-    
-    $distance = [Math]::Sqrt([Math]::Pow($X, 2) + [Math]::Pow($Y, 2))
-    if ($distance -le 1) {
-        return 10
+    $dist = [Math]::Sqrt([Math]::Pow($X, 2) + [Math]::Pow($Y, 2))
+
+    if ($dist -gt 10) {
+        return 0
     }
-    if ($distance -le 5) {
-        return 5
-    }
-    if ($distance -le 10) {
+    if ($dist -gt 5) {
         return 1
     }
-    return 0
+    if ($dist -gt 1) {
+        return 5
+    }
+    10
 }
