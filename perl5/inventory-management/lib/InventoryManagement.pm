517d78b6bae944d867e4a2f8dbc7197c46d4339c
MenkeTechnologies <linux.dev25@gmail.com>
2024-01-05 09:19:32 -0500

    im

--- perl5/inventory-management/lib/InventoryManagement.pm
+++ perl5/inventory-management/lib/InventoryManagement.pm
@@ -3,18 +3,21 @@ package InventoryManagement;
 use v5.38;
 
 sub create_inventory ($items) {
-    my %inventory;
-    return \%inventory;
+    add_items({}, $items)
 }
 
-sub add_items ( $inventory, $items ) {
-    return $inventory;
+sub add_items ($inventory, $items) {
+    ++$inventory->{$_} for $items->@*;
+    $inventory
 }
 
-sub remove_items ( $inventory, $items ) {
-    return $inventory;
+sub remove_items ($inventory, $items) {
+    map { --$inventory->{$_} if $inventory->{$_} } $items->@*;
+    $inventory
 }
 
-sub delete_item ( $inventory, $item ) {
-    return $inventory;
+sub delete_item ($inventory, $item) {
+    delete $inventory->{$item};
+    $inventory
 }
+
