42490e0fd5d9a538dc54540cbae6a72137620e64
MenkeTechnologies <linux.dev25@gmail.com>
2024-01-28 12:07:49 -0500

    rvs

--- ruby/reverse-string/reverse_string_test.rb
+++ ruby/reverse-string/reverse_string_test.rb
@@ -8,27 +8,22 @@ class ReverserTest < Minitest::Test
   end
 
   def test_a_word
-    skip
     assert_equal "tobor", Reverser.reverse("robot")
   end
 
   def test_a_capitalized_word
-    skip
     assert_equal "nemaR", Reverser.reverse("Ramen")
   end
 
   def test_a_sentence_with_punctuation
-    skip
     assert_equal "!yrgnuh m'I", Reverser.reverse("I'm hungry!")
   end
 
   def test_a_palindrome
-    skip
     assert_equal "racecar", Reverser.reverse("racecar")
   end
 
   def test_an_even_sized_word
-    skip
     assert_equal "reward", Reverser.reverse("drawer")
   end
 end
