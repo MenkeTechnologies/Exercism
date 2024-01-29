f503aa1e81b7e3d1b65ffac5ad9dc78c85f6122d
MenkeTechnologies <linux.dev25@gmail.com>
2024-01-29 18:44:59 -0500

    ll

--- cpp/log-levels/log_levels.cpp
+++ cpp/log-levels/log_levels.cpp
@@ -1,5 +1,14 @@
 #include <string>
+using std::string;
 
 namespace log_line {
-
-} // namespace log_line
+    string message(const string &msg){
+        return msg.substr(msg.find(':') + 2);
+    }
+    string log_level(const string &msg){
+        return msg.substr(1, msg.find(']')-1);
+    }
+    string reformat(const string &msg){
+        return message(msg) + " (" + log_level(msg) + ")";
+    }
+}
