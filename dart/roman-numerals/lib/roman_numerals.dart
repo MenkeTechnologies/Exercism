610be4826c44a236c99080389bfa1fc30024213b
MenkeTechnologies <linux.dev25@gmail.com>
2024-02-08 00:41:38 -0500

    rn

--- dart/roman-numerals/lib/roman_numerals.dart
+++ dart/roman-numerals/lib/roman_numerals.dart
@@ -1,5 +1,13 @@
+final dict = {"M": 1000, "CM" : 900, "D": 500, "CD": 400, "C": 100, "XC": 90, "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1};
+
 extension ConvertToRomanNumerals on int {
   String toRoman() {
-    // Put your code here
+    var s = "";
+    var dup = this;
+    for (final entry in dict.entries) {
+      s += entry.key * (dup ~/ entry.value);
+      dup %= entry.value;
+    }
+    return s;
   }
 }
