c1efa569f1ab36fb1980b2b8625c47f71c5dbc21
MenkeTechnologies <linux.dev25@gmail.com>
2024-02-11 09:53:44 -0500

    rc

--- julia/resistor-color/resistor-color.jl
+++ julia/resistor-color/resistor-color.jl
@@ -1,7 +1,8 @@
+colorsdict = Dict("black"=> 0,"brown"=> 1,"red"=> 2,"orange"=> 3,"yellow"=> 4,"green"=> 5,"blue"=> 6,"violet"=> 7,"grey"=>8,"white"=>9)
+
 function colorcode(color)
-
+    haskey(colorsdict, color) ? colorsdict[color] : error("Invalid color")
 end
-
 function colors()
-
+    expected_colors = map(pair -> pair[1], sort(collect(colorsdict), by=x -> x[2]))
 end
