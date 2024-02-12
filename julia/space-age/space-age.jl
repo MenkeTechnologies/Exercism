7ee944cebd1a414fe83d95f1214713327c8660f9
MenkeTechnologies <linux.dev25@gmail.com>
2024-02-12 16:21:30 -0500

    sa

--- julia/space-age/space-age.jl
+++ julia/space-age/space-age.jl
@@ -1,31 +1,22 @@
-function onEarth(seconds)
+PLANETS = Dict(
+    "Mercury" => 0.2408467,
+    "Venus" => 0.61519726,
+    "Earth" => 1.0,
+    "Mars" => 1.8808158,
+    "Jupiter" => 11.862615,
+    "Saturn" => 29.447498,
+    "Uranus" => 84.016846,
+    "Neptune" => 164.79132
+)
+SEC_PER_YR = 31557600
 
-end
+onPlanet(seconds, planet) = seconds / PLANETS[planet] / SEC_PER_YR
 
-function onMercury(seconds)
-
-end
-
-function onVenus(seconds)
-
-end
-
-function onMars(seconds)
-
-end
-
-function onJupiter(seconds)
-
-end
-
-function onSaturn(seconds)
-
-end
-
-function onUranus(seconds)
-
-end
-
-function onNeptune(seconds)
-
-end
+onEarth(seconds) = onPlanet(seconds, "Earth")
+onMercury(seconds) = onPlanet(seconds, "Mercury")
+onVenus(seconds) = onPlanet(seconds, "Venus")
+onMars(seconds) = onPlanet(seconds, "Mars")
+onJupiter(seconds) = onPlanet(seconds, "Jupiter")
+onSaturn(seconds) = onPlanet(seconds, "Saturn")
+onUranus(seconds) = onPlanet(seconds, "Uranus")
+onNeptune(seconds) = onPlanet(seconds, "Neptune")
