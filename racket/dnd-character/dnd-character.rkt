32b64e7db21f29f526f4544c4e4dd2e494a10c06
MenkeTechnologies <linux.dev25@gmail.com>
2024-02-17 07:59:02 -0500

    dnd

--- racket/dnd-character/dnd-character.rkt
+++ racket/dnd-character/dnd-character.rkt
@@ -6,13 +6,16 @@
          modifier)
 
 (struct character
-    ()) ; Add necessary struct fields
+  (strength dexterity constitution intelligence wisdom charisma hitpoints))
 
 (define (ability)
-  (error "Not implemented yet"))
+  (let ([dices (for/list ([_ 4]) (add1 (random 6)))])
+    (apply + (rest (sort dices <)))))
 
 (define (modifier value)
-  (error "Not implemented yet"))
+  (floor (/ (- value 10) 2)))
 
 (define (make-character)
-  (error "Not implemented yet"))
+  (let ([constitution (ability)])
+    (character (ability) (ability) constitution (ability) (ability) (ability) (+ 10 (modifier constitution)))))
+
