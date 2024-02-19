f97f6c7902e657c9e595abdf68b325fd20c2bc5e
MenkeTechnologies <linux.dev25@gmail.com>
2024-02-19 10:52:38 -0500

    sweetheart

--- php/sweethearts/HighSchoolSweetheart.php
+++ php/sweethearts/HighSchoolSweetheart.php
@@ -1,24 +1,37 @@
 <?php
-
 class HighSchoolSweetheart
 {
     public function firstLetter(string $name): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        return substr(trim($name),0,1);
     }
-
     public function initial(string $name): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        return strtoupper( $this->firstLetter($name)) . '.';
     }
-
     public function initials(string $name): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        return $this->initial(explode(" ", $name)[0]) . ' ' . $this->initial(explode(" ", $name)[1]);
     }
-
     public function pair(string $sweetheart_a, string $sweetheart_b): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        $heart = <<<EOF
+             ******       ******
+           **      **   **      **
+         **         ** **         **
+        **            *            **
+        **                         **
+        **     %s  +  %s     **
+         **                       **
+           **                   **
+             **               **
+               **           **
+                 **       **
+                   **   **
+                     ***
+                      *
+        EOF;
+        return sprintf($heart, $this->initials($sweetheart_a), $this->initials($sweetheart_b));
     }
 }
+
