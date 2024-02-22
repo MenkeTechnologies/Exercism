95356b905e7b626dd4c8c3cc1dd9b5516e1208f1
MenkeTechnologies <linux.dev25@gmail.com>
2024-02-22 09:23:41 -0500

    rvs

--- haskell/reverse-string/src/ReverseString.hs
+++ haskell/reverse-string/src/ReverseString.hs
@@ -1,4 +1,4 @@
 module ReverseString (reverseString) where
 
 reverseString :: String -> String
-reverseString str = error "You need to implement this function."
+reverseString = foldl (flip (:)) []
