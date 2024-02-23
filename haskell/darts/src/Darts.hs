af28f21d85d2e23f3d22a2545d962953cf79d74b
MenkeTechnologies <linux.dev25@gmail.com>
2024-02-23 10:35:17 -0500

    darts

--- haskell/darts/src/Darts.hs
+++ haskell/darts/src/Darts.hs
@@ -1,4 +1,10 @@
 module Darts (score) where
 
 score :: Float -> Float -> Int
-score x y = error "You need to implement this function."
+score x y
+    | dist > 10 = 0
+    | dist > 5 = 1
+    | dist > 1 = 5
+    | otherwise = 10
+      where
+        dist = (x ** 2 + y ** 2) ** 0.5
