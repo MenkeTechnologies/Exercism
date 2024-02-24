08301968595ea8c8c388e5ae2967a5b70a013433
MenkeTechnologies <linux.dev25@gmail.com>
2024-02-24 16:55:56 -0500

    ll

--- php/language-list/LanguageList.php
+++ php/language-list/LanguageList.php
@@ -1,6 +1,18 @@
 <?php
+function language_list(string ...$lst) {
+    return $lst;
+}
+function add_to_language_list($a, $lst) {
+    return array_merge($a, [$lst]);
+}
+function prune_language_list($lst){
+    return array_slice($lst, 1);
+}
 
-function language_list()
-{
-    // implement the language list function
+function current_language($lst){
+    return $lst[0];
+}
+
+function language_list_length($lst){
+    return count($lst);
 }
