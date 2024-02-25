f7ab6d58949b5440986ed69a50a70336d18d1db5
MenkeTechnologies <linux.dev25@gmail.com>
2024-02-25 12:13:15 -0500

    pi

--- php/pizza-pi/PizzaPi.php
+++ php/pizza-pi/PizzaPi.php
@@ -1,24 +1,16 @@
 <?php
-
-class PizzaPi
-{
-    public function calculateDoughRequirement()
-    {
-        throw new \BadFunctionCallException("Implement the function");
+class PizzaPi {
+    public function calculateDoughRequirement(int $pizzas, int $persons): int {
+        return $pizzas * ($persons * 20 + 200);
     }
-
-    public function calculateSauceRequirement()
-    {
-        throw new \BadFunctionCallException("Implement the function");
+    public function calculateSauceRequirement(int $pizzas, int $sauceCanVolume): int {
+        return $pizzas * 125 / $sauceCanVolume;
     }
-
-    public function calculateCheeseCubeCoverage()
-    {
-        throw new \BadFunctionCallException("Implement the function");
+    public function calculateCheeseCubeCoverage(int $cheeseLength, float $desiredCheeseThickness, int $pizzaDiameter): int {
+        return $cheeseLength ** 3 / ($desiredCheeseThickness * pi() * $pizzaDiameter);
     }
-
-    public function calculateLeftOverSlices()
-    {
-        throw new \BadFunctionCallException("Implement the function");
+    public function calculateLeftOverSlices(int $pizzas, int $friends): int {
+        return $pizzas * 8 % $friends;
     }
 }
+
