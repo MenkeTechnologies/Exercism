a957fe7699e2f8c195af9cc7b71ce12fff8a7e73
MenkeTechnologies <linux.dev25@gmail.com>
2024-02-26 09:17:10 -0500

    sieve

--- dart/sieve/lib/sieve.dart
+++ dart/sieve/lib/sieve.dart
@@ -1,23 +1,15 @@
 class Sieve {
-  final int n;
-  Sieve(this.n);
-  get primes {
-    final lst = List.generate(n+1, (_) => true, growable: false);
-    lst[0] = lst[1] = false;
-    for (int i = 2; i < lst.length; i++) {
-      if (!lst[i]) {
-        continue;
-      }
-      for (int k = i + i; k < lst.length; k += i) {
-        lst[k] = false;
-      }
-    }
-    final ans = [];
-    for (int i = 2; i < lst.length; i++) {
-      if (lst[i]) {
-        ans.add(i);
+  List<int> primes = [];
+
+  Sieve(limit) {
+    final composites = List.generate(limit + 1, (_) => false, growable: false);
+    for (int i = 2; i <= limit; i++) {
+      if (!composites[i]) {
+        primes.add(i);
+        for (int j = i + i; j <= limit; j += i) {
+          composites[j] = true;
+        }
       }
     }
-    return ans;
   }
 }
