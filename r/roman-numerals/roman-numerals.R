1cb08032732f09a7b480a123b7e33d9c307bf131
MenkeTechnologies <linux.dev25@gmail.com>
2024-03-11 16:58:16 -0400

    roman numerals

--- r/roman-numerals/roman-numerals.R
+++ r/roman-numerals/roman-numerals.R
@@ -1,3 +1,12 @@
-roman <- function(arabic) {
-
+roman = function(arabic) {
+  conversion = list(M = 1000, CM = 900, D = 500, CD = 400, 
+                     C = 100, XC = 90, L = 50, XL = 40, 
+                     X = 10, IX = 9, V = 5, IV = 4, I = 1)
+  result = c()
+  for (roman in names(conversion)) {
+    value = conversion[[roman]]
+    result = append(result, replicate(arabic / value, roman))
+    arabic = arabic %% value
+  }
+  paste(result, collapse = "")
 }
