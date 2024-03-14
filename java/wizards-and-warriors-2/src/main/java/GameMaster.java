8ede1cc5528c3f0ab0fd6d30d975a7de26c8d51f
MenkeTechnologies <linux.dev25@gmail.com>
2024-03-14 10:53:21 -0400

    waw2

--- java/wizards-and-warriors-2/src/main/java/GameMaster.java
+++ java/wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -1,12 +1,22 @@
 public class GameMaster {
 
-    // TODO: define a 'describe' method that returns a description of a Character
+    public String describe(Character c) {
+        return "You're a level %d %s with %d hit points.".formatted(c.getLevel(), c.getCharacterClass(), c.getHitPoints());
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Destination
+    public String describe(Destination d) {
+        return "You've arrived at %s, which has %d inhabitants.".formatted(d.getName(), d.getInhabitants());
+    }
 
-    // TODO: define a 'describe' method that returns a description of a TravelMethod
+    public String describe(TravelMethod tm) {
+        return "You're traveling to your destination %s.".formatted(tm == TravelMethod.WALKING ? "by walking" : "on horseback");
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Character, Destination and TravelMethod
+    public String describe(Character c, Destination d, TravelMethod tm) {
+        return "%s %s %s".formatted(describe(c), describe(tm), describe(d));
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Character and Destination
+    public String describe(Character c, Destination d) {
+        return "%s %s %s".formatted(describe(c), describe(TravelMethod.WALKING), describe(d));
+    }
 }
