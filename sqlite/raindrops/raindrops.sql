d162b1153d72be54cd3359c46ea6f0dd6ff9a4c0
MenkeTechnologies <linux.dev25@gmail.com>
2024-04-01 10:19:02 -0400

    rd

--- sqlite/raindrops/raindrops.sql
+++ sqlite/raindrops/raindrops.sql
@@ -1,2 +1,8 @@
 -- Schema: CREATE TABLE "raindrops" ("number" INT, "sound" TEXT);
 -- Task: update the raindrops table and set the sound based on the number.
+
+UPDATE raindrops
+SET sound = iif(number % 3, "", "Pling") || iif(number % 5, "", "Plang") || iif(number % 7, "", "Plong");
+
+UPDATE raindrops
+SET sound = iif(sound == "", number, sound);
