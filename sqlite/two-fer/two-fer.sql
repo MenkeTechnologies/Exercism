b42982701c2d5783370e33eaf96c1de54e0a3b11
MenkeTechnologies <linux.dev25@gmail.com>
2024-04-01 10:16:33 -0400

    2fer

--- sqlite/two-fer/two-fer.sql
+++ sqlite/two-fer/two-fer.sql
@@ -1,2 +1,5 @@
 -- Schema: CREATE TABLE "twofer" ("input" TEXT, "response" TEXT);
 -- Task: update the twofer table and set the response based on the input.
+
+UPDATE twofer
+SET response = 'One for ' || iif(input = '', 'you', input) || ', one for me.';
