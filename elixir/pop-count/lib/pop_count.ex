152c5f9c8a55e28d7abdb9ac59b04194aaf4209d
MenkeTechnologies <linux.dev25@gmail.com>
2024-04-04 10:48:27 -0400

    pc

--- elixir/pop-count/lib/pop_count.ex
+++ elixir/pop-count/lib/pop_count.ex
@@ -1,8 +1,6 @@
 defmodule PopCount do
-  @doc """
-  Given the number, count the number of eggs.
-  """
-  @spec egg_count(number :: integer()) :: non_neg_integer()
-  def egg_count(number) do
+  def egg_count(n) do
+    n |> Integer.digits(2) |> Enum.sum()
   end
 end
+
