6e85d215f60a8acb13877e06153cc9c03260cdb1
MenkeTechnologies <linux.dev25@gmail.com>
2024-04-12 00:37:37 -0400

    dnd

--- ruby/dnd-character/dnd_character.rb
+++ ruby/dnd-character/dnd_character.rb
@@ -1,17 +1,18 @@
-=begin
-Write your code for the 'D&D Character' exercise in this file. Make the tests in
-`dnd_character_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/dnd-character` directory.
-=end
-
 class DndCharacter
-  def self.modifier
-    # Your code here
-  end
+  attr_reader :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :hitpoints
 
   def initialize
-    # Your code here
+    @strength = create_ability
+    @dexterity = create_ability
+    @constitution = create_ability
+    @intelligence = create_ability
+    @wisdom = create_ability
+    @charisma = create_ability
+    @hitpoints = 10 + DndCharacter.modifier(constitution)
   end
+
+  def self.modifier(score) = (score - 10) / 2
+
+  private
+  def create_ability = (1..4).map { rand(6) + 1 }.sort.drop(1).sum
 end
