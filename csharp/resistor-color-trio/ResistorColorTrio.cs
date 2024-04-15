63201b21cac139ca54761d87adf71da632c8a9a3
MenkeTechnologies <linux.dev25@gmail.com>
2024-04-15 07:45:01 -0400

    rct

--- csharp/resistor-color-trio/ResistorColorTrio.cs
+++ csharp/resistor-color-trio/ResistorColorTrio.cs
@@ -2,8 +2,16 @@ using System;
 
 public static class ResistorColorTrio
 {
+    private enum Colors
+    {
+        black, brown, red, orange, yellow, green, blue, violet, grey, white
+    }
+
     public static string Label(string[] colors)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var output = (Value(colors[0]) * 10 + Value(colors[1])) * Math.Pow(10, Value(colors[2]));
+        return output < 1000 ? $"{output} ohms" : $"{output / 1000} kiloohms";
     }
+
+    private static double Value(string color) => (double)Enum.Parse<Colors>(color);
 }
