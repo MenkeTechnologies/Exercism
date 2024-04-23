de3c9b08b2270734ee4a3ec534de6420b5921bfd
MenkeTechnologies <linux.dev25@gmail.com>
2024-04-23 08:02:25 -0400

    more exe

--- php/city-office/Form.php
+++ php/city-office/Form.php
@@ -2,23 +2,23 @@
 
 class Form
 {
-    function blanks($length)
+    function blanks(int $length)
     {
         return str_repeat(" ", $length);
     }
 
-    function letters($word)
+    function letters(string $word)
     {
         return mb_str_split($word);
     }
 
-    function checkLength($word, $max_length)
+    function checkLength(string $word, int $max_length)
     {
         $difference = mb_strlen($word) - $max_length;
         return $difference <= 0;
     }
 
-    function formatAddress($address)
+    function formatAddress(Address $address)
     {
         $formatted_street = mb_strtoupper($address->street);
         $formatted_postal_code = mb_strtoupper($address->postal_code);
