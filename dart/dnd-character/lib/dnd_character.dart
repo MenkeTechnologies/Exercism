e0e0f524c642177b008bbcd84c144353194a7d66
MenkeTechnologies <linux.dev25@gmail.com>
2024-04-25 09:41:50 -0400

    dnd

--- dart/dnd-character/lib/dnd_character.dart
+++ dart/dnd-character/lib/dnd_character.dart
@@ -1,3 +1,32 @@
+import 'dart:math';
+
 class DndCharacter {
-  // Put your code here
+  late int strength;
+  late int constitution;
+  late int dexterity;
+  late int intelligence;
+  late int wisdom;
+  late int charisma;
+  late int hitpoints;
+
+  static int modifier(int score) {
+    return ((score - 10) / 2).floor();
+  }
+
+  static int ability() {
+    var random = Random();
+    var numbers = List.generate(4, (_) => random.nextInt(6) + 1).toList();
+    numbers.sort((a, b) => a - b);
+    return numbers.take(3).fold(0, (sum, score) => sum + score);
+  }
+
+  DndCharacter.create() {
+    strength = ability();
+    constitution = ability();
+    dexterity = ability();
+    intelligence = ability();
+    wisdom = ability();
+    charisma = ability();
+    hitpoints = 10 + modifier(constitution);
+  }
 }
--- dart/dnd-character/test/dnd_character_test.dart
+++ dart/dnd-character/test/dnd_character_test.dart
@@ -9,75 +9,75 @@ void main() {
 void abilityModifier() {
   test('ability modifier for score 3 is -4', () {
     expect(DndCharacter.modifier(3), equals(-4));
-  }, skip: false);
+  }, );
 
   test('ability modifier for score 4 is -3', () {
     expect(DndCharacter.modifier(4), equals(-3));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 5 is -3', () {
     expect(DndCharacter.modifier(5), equals(-3));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 6 is -2', () {
     expect(DndCharacter.modifier(6), equals(-2));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 7 is -2', () {
     expect(DndCharacter.modifier(7), equals(-2));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 8 is -1', () {
     expect(DndCharacter.modifier(8), equals(-1));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 9 is -1', () {
     expect(DndCharacter.modifier(9), equals(-1));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 10 is 0', () {
     expect(DndCharacter.modifier(10), equals(0));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 11 is 0', () {
     expect(DndCharacter.modifier(11), equals(0));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 12 is +1', () {
     expect(DndCharacter.modifier(12), equals(1));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 13 is +1', () {
     expect(DndCharacter.modifier(13), equals(1));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 14 is +2', () {
     expect(DndCharacter.modifier(14), equals(2));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 15 is +2', () {
     expect(DndCharacter.modifier(15), equals(2));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 16 is +3', () {
     expect(DndCharacter.modifier(16), equals(3));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 17 is +3', () {
     expect(DndCharacter.modifier(17), equals(3));
-  }, skip: true);
+  }, );
 
   test('ability modifier for score 18 is +4', () {
     final result = DndCharacter.modifier(18);
     expect(result, equals(4));
-  }, skip: true);
+  }, );
 }
 
 void abilities() {
   test('random ability is within range', () {
     final score = DndCharacter.ability();
     expect(score, inInclusiveRange(3, 18));
-  }, skip: true);
+  }, );
 
   test('random character is valid', () {
     final character = DndCharacter.create();
@@ -88,7 +88,7 @@ void abilities() {
     expect(character.wisdom, inInclusiveRange(3, 18));
     expect(character.charisma, inInclusiveRange(3, 18));
     expect(character.hitpoints, equals(10 + DndCharacter.modifier(character.constitution)));
-  }, skip: true);
+  }, );
 
   test('each ability is only calculated once', () {
     final character = DndCharacter.create();
@@ -116,5 +116,5 @@ void abilities() {
     read1 = character.charisma;
     read2 = character.charisma;
     expect(read1, equals(read2));
-  }, skip: true);
+  }, );
 }
