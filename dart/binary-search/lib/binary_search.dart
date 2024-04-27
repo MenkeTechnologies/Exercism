066d45e7775b7b9ce59ddcc772041ab73b8e6dcc
MenkeTechnologies <linux.dev25@gmail.com>
2024-04-26 20:09:13 -0400

    bs

--- dart/binary-search/lib/binary_search.dart
+++ dart/binary-search/lib/binary_search.dart
@@ -1,5 +1,23 @@
 import 'package:binary_search/value_not_found_exception.dart';
 
 class BinarySearch {
-  // Your code here.
+  final List<int> arr;
+
+  BinarySearch(this.arr);
+  
+  int find(int target) {
+    int lo = 0;
+    int hi = arr.length - 1;
+    while (lo <= hi ) {
+      int mid = (lo + hi) ~/ 2;
+      if (arr[mid] == target) {
+        return mid;
+      } else if (arr[mid] < target) {
+        lo = mid + 1;
+      } else {
+        hi = mid - 1;
+      }
+    }
+    throw ValueNotFoundException('not found');
+  }
 }
