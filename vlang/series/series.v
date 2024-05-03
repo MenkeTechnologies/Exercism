00ff1dece4cb83d248f74c3bd318eead61f3317f
MenkeTechnologies <linux.dev25@gmail.com>
2024-05-03 12:20:34 -0400

    series

---
+++ vlang/series/series.v
@@ -0,0 +1,23 @@
+module main
+
+fn slices(s string, len int) ![]string {
+    if s.len == 0 {
+        return error('series cannot be empty')
+    }
+    if len > s.len {
+        return error('slice length cannot be greater than series length')
+    }
+    if len == 0 {
+        return error('slice length cannot be zero')
+    }
+    if len < 0 {
+        return error('slice length cannot be negative')
+    }
+    mut res := []string{}
+
+    for i := 0; i <= s.len - len; i++ {
+        res << s[i..i + len]
+    }
+
+    return res
+}
