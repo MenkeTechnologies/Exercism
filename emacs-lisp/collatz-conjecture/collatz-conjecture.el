384da0b85d559b9ec5f3282cef0f3570d47bbe6f
MenkeTechnologies <linux.dev25@gmail.com>
2024-05-09 14:11:06 -0400

    coll

--- emacs-lisp/collatz-conjecture/collatz-conjecture.el
+++ emacs-lisp/collatz-conjecture/collatz-conjecture.el
@@ -1,12 +1,11 @@
-;;; collatz-conjecture.el --- Collatz Conjecture (exercism)  -*- lexical-binding: t; -*-
-
-;;; Commentary:
-
-;;; Code:
-
-(defun steps (number)
+(defun steps (n)
   "Count the steps to reach 1 using the Collatz conjecture."
-  (error "Delete this S-Expression and write your own implementation"))
+  (if (< n 1)
+      (error "Only positive numbers are allowed")
+    (let ((cnt 0))
+      (while (> n 1)
+        (setq n (if (= (mod n 2) 0) (/ n 2) (+ (* 3 n) 1)))
+        (setq cnt (+ cnt 1)))
+      cnt)))
 
 (provide 'collatz-conjecture)
-;;; collatz-conjecture.el ends here
