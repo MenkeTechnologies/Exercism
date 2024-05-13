d8d9a8e3f2c12b8655c8dd59ff3c0fff068881d2
MenkeTechnologies <linux.dev25@gmail.com>
2024-05-13 11:28:10 -0400

    tri

--- emacs-lisp/triangle/triangle.el
+++ emacs-lisp/triangle/triangle.el
@@ -1,18 +1,26 @@
-;;; triangle.el --- Triangle (exercism)  -*- lexical-binding: t; -*-
-
-;;; Commentary:
-
-;;; Code:
-
+(defun valid-triangle-p (sides)
+  "Check if the sides form a valid triangle."
+  (let ((a (nth 0 sides))
+        (b (nth 1 sides))
+        (c (nth 2 sides)))
+    (and (> a 0) (> b 0) (> c 0)
+         (>= (+ a b) c)
+         (>= (+ b c) a)
+         (>= (+ a c) b))))
 
 (defun equilateralp (sides)
-  (error "Delete this S-Expression and write your own implementation"))
+  "Check if the triangle is equilateral."
+  (and (valid-triangle-p sides)
+       (= (length (delete-dups sides)) 1)))
 
 (defun isoscelesp (sides)
-  (error "Delete this S-Expression and write your own implementation"))
+  "Check if the triangle is isosceles."
+  (and (valid-triangle-p sides)
+       (<= (length (delete-dups sides)) 2)))
 
 (defun scalenep (sides)
-  (error "Delete this S-Expression and write your own implementation"))
+  "Check if the triangle is scalene."
+  (and (valid-triangle-p sides)
+       (= (length (delete-dups sides)) 3)))
 
 (provide 'triangle)
-;;; triangle.el ends here
