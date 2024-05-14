4d5b3fadc478a09129437855bf94aa5f09e30d0b
MenkeTechnologies <linux.dev25@gmail.com>
2024-05-14 09:48:10 -0400

    lsp

--- dart/largest-series-product/lib/largest_series_product.dart
+++ dart/largest-series-product/lib/largest_series_product.dart
@@ -1,5 +1,20 @@
 import 'dart:math';
 
 class LargestSeriesProduct {
-  // Your code here.
+  int largestProduct(String input, int span) {
+    if (RegExp(r'\D').hasMatch(input))
+      throw ArgumentError('digits input must only contain digits');
+    if (span > input.length)
+      throw ArgumentError('span must be smaller than string length');
+    if (span < 0) throw ArgumentError('span must not be negative');
+    var res = 0;
+    for (var i = 0; i <= input.length - span; ++i) {
+      var product = input
+          .substring(i, i + span)
+          .split('')
+          .fold(1, (acc, n) => acc * int.parse(n));
+      res = max(res, product);
+    }
+    return res;
+  }
 }
