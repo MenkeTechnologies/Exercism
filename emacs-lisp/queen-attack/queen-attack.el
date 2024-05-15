daa53c124a189576df7572170ce2010951e063e0
MenkeTechnologies <linux.dev25@gmail.com>
2024-05-14 22:58:33 -0400

    qa

--- emacs-lisp/queen-attack/queen-attack.el
+++ emacs-lisp/queen-attack/queen-attack.el
@@ -1,18 +1,11 @@
-;;; queen-attack.el --- Queen Attack (exercism)  -*- lexical-binding: t; -*-
+(defun valid-position-p (queen) (and (<= 0 (car queen) 7) (<= 0 (cdr queen) 7)))
 
-;;; Commentary:
-
-;;; Code:
-
-
-(defun valid-position-p (queen)
-  (error
-   "Delete this S-Expression and write your own implementation"))
-
-(defun can-attack-p (white-queen black-queen)
-  (error
-   "Delete this S-Expression and write your own implementation"))
-
+(defun can-attack-p (q1 q2)
+  (cl-destructuring-bind ((x1 . y1) (x2 . y2)) (list q1 q2)
+  (let* ((dx (abs(- x1 x2)))
+        (dy (abs(- y1 y2))))
+    (or (= dx 0)
+	    (= dy 0)
+	    (= dx dy)))))
 
 (provide 'queen-attack)
-;;; queen-attack.el ends here
