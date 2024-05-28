91dc62285f1a740db0741d9bf0acd684f492c565
MenkeTechnologies <linux.dev25@gmail.com>
2024-05-27 23:43:35 -0400

    lo

--- php/list-ops/ListOps.php
+++ php/list-ops/ListOps.php
@@ -1,80 +1,38 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
-declare(strict_types=1);
-
 class ListOps
 {
     public function append(array $list1, array $list2): array
     {
-        throw new \BadMethodCallException("Implement the append function");
+        return [...$list1, ...$list2];
     }
-
-    public function concat(array $list1, array ...$listn): array
+    public function concat(array $list1, array ...$lstn): array
     {
-        throw new \BadMethodCallException("Implement the concat function");
+        return array_reduce($lstn, fn ($acc, $lst) => [...$acc, ...$lst], $list1);
     }
-
-    /**
-     * @param callable(mixed $item): bool $predicate
-     */
-    public function filter(callable $predicate, array $list): array
+    public function filter(callable $f, array $lst): array
     {
-        throw new \BadMethodCallException("Implement the filter function");
+        return array_values(array_filter($lst, $f));
     }
-
-    public function length(array $list): int
+    public function length(array $lst): int
     {
-        throw new \BadMethodCallException("Implement the length function");
+        return count($lst);
     }
-
-    /**
-     * @param callable(mixed $item): mixed $function
-     */
-    public function map(callable $function, array $list): array
+    public function map(callable $f, array $lst): array
     {
-        throw new \BadMethodCallException("Implement the map function");
+        return array_map($f, $lst);
     }
-
-    /**
-     * @param callable(mixed $accumulator, mixed $item): mixed $function
-     */
-    public function foldl(callable $function, array $list, $accumulator)
+    public function foldl(callable $f, array $lst, $acc)
     {
-        throw new \BadMethodCallException("Implement the foldl function");
+        return array_reduce($lst, $f, $acc);
     }
-
-    /**
-     * @param callable(mixed $accumulator, mixed $item): mixed $function
-     */
-    public function foldr(callable $function, array $list, $accumulator)
+    public function foldr(callable $f, array $lst, $acc)
     {
-        throw new \BadMethodCallException("Implement the foldr function");
+        return array_reduce($this->reverse($lst), $f, $acc);
     }
-
-    public function reverse(array $list): array
+    public function reverse(array $lst): array
     {
-        throw new \BadMethodCallException("Implement the reverse function");
+        return array_reverse($lst);
     }
 }
+
