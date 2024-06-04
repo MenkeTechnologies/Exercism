b59b1b6067129f8e1ec15da2f591d4d7b8fb560d
MenkeTechnologies <linux.dev25@gmail.com>
2024-06-04 09:00:28 -0400

    darts

--- cobol/darts/src/darts.cob
+++ cobol/darts/src/darts.cob
@@ -7,4 +7,10 @@
        01 WS-Y PIC 99V9.
        01 WS-RESULT PIC 99.
        PROCEDURE DIVISION.
-       DARTS.
+       darts.
+         evaluate (ws-x ** 2 + ws-y ** 2) ** 0.5
+           when 0 thru 1 move 10 to ws-result
+           when 1 thru 5 move 5 to ws-result
+           when 5 thru 10 move 1 to ws-result
+           when other move 0 to ws-result
+         end-evaluate.
