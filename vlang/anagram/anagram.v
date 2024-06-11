91c3dcff06a4a2da79f0f7bab335c72b869e8ce8
MenkeTechnologies <linux.dev25@gmail.com>
2024-06-11 09:13:38 -0400

    ana

--- vlang/anagram/anagram.v
+++ vlang/anagram/anagram.v
@@ -1,4 +1,18 @@
 module main
 
 fn find_anagrams(subject string, candidates []string) []string {
+	mut ret := []string{}
+	mut target := subject.to_lower().runes()
+	target.sort()
+	for _, cand in candidates {
+		if cand.str().to_lower() == subject.str().to_lower() {
+			continue			
+		}
+		mut lc_sort := cand.str().to_lower().runes()
+		lc_sort.sort()
+		if target == lc_sort {
+			ret << cand
+		}
+	}
+	return ret
 }
