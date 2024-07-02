1ee32ec9201970bcdf746ae993cdbb3827dda39c
MenkeTechnologies <linux.dev25@gmail.com>
2024-07-01 23:55:00 -0400

    lo

--- vlang/list-ops/list-ops.v
+++ vlang/list-ops/list-ops.v
@@ -1,27 +1,57 @@
 module main
 
 pub fn append[T](front []T, back []T) []T {
+	mut res := front.clone()
+	res << back
+	return res
 }
 
 pub fn concat[T](array [][]T) []T {
+	mut res := []T{}
+	for arr in array {
+		res << arr
+	}
+	return res
 }
 
-pub fn foldl[T, U](array []T, initial U, folder fn (acc U, e T) U) U {
+pub fn foldl[T, U](array []T, initial U, f fn (acc U, e T) U) U {
+	mut res := initial
+	for elem in array {
+		res = f(res, elem)
+	}
+	return res
 }
 
-pub fn foldr[T, U](array []T, initial U, folder fn (acc U, e T) U) U {
+pub fn foldr[T, U](array []T, initial U, f fn (acc U, e T) U) U {
+	return foldl(reverse(array), initial, f)
 }
 
 pub fn length[T](array []T) int {
+	return array.len
 }
 
 pub fn reverse[T](array []T) []T {
+	mut res := []T{}
+	for i := array.len - 1; i >= 0; i-- {
+		res << array[i]
+	}
+	return res
 }
 
-pub fn filter[T](array []T, predicate fn (e T) bool) []T {
+pub fn filter[T](array []T, f fn (e T) bool) []T {
+	mut res := []T{}
+	for elem in array {
+		if f(elem) {
+			res << elem
+		}
+	}
+	return res
 }
 
-// renamed 'map_of' as 'map' conflicts with V 'map' datatype
-
-pub fn map_of[T, U](array []T, mapper fn (e T) U) []U {
+pub fn map_of[T, U](array []T, f fn (e T) U) []U {
+	mut res := []U{}
+	for elem in array {
+		res << f(elem)
+	}
+	return res
 }
