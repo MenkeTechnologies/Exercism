f45349076a8f3aed0fd3db4dd903e127cfdec4f2
MenkeTechnologies <linux.dev25@gmail.com>
2024-07-03 00:15:45 -0400

    hs

--- vlang/high-scores/high-scores.v
+++ vlang/high-scores/high-scores.v
@@ -1,20 +1,29 @@
 module main
 
+import arrays
+
 struct HighScores {
+    scores []int
 }
 
-// build a new HighScores
 pub fn HighScores.new(scores []int) HighScores {
+    return HighScores{scores: scores}
 }
 
-pub fn (mut high_scores HighScores) scores() []int {
+pub fn (high_scores &HighScores) scores() []int {
+    return high_scores.scores
 }
 
-pub fn (mut high_scores HighScores) latest() int {
+pub fn (high_scores &HighScores) latest() int {
+    return high_scores.scores.last()
 }
 
-pub fn (mut high_scores HighScores) personal_best() int {
+pub fn (high_scores &HighScores) personal_best() int {
+    return arrays.max(high_scores.scores) or { 0 }
 }
 
-pub fn (mut high_scores HighScores) personal_top_three() []int {
+pub fn (high_scores &HighScores) personal_top_three() []int {
+    mut top_scores := high_scores.scores.sorted(b < a)
+    top_scores.trim(3)
+    return top_scores
 }
