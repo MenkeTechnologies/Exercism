4095c8f1adb05ec72f2f4935ca457faa40427c0e
MenkeTechnologies <linux.dev25@gmail.com>
2024-07-13 05:46:24 -0400

    ptl

--- julia/protein-translation/protein-translation.jl
+++ julia/protein-translation/protein-translation.jl
@@ -1,3 +1,30 @@
+Codon_Protein = Dict(
+	"AUG" => "Methionine",
+	"UUU" => "Phenylalanine",
+	"UUC" => "Phenylalanine",
+	"UUA" => "Leucine",
+	"UUG" => "Leucine",
+	"UCU" => "Serine",
+	"UCC" => "Serine",
+	"UCA" => "Serine",
+	"UCG" => "Serine",
+	"UAU" => "Tyrosine",
+	"UAC" => "Tyrosine",
+	"UGU" => "Cysteine",
+	"UGC" => "Cysteine",
+	"UGG" => "Tryptophan",
+	"UAA" => "STOP",
+	"UAG" => "STOP",
+	"UGA" => "STOP",
+)
 function proteins(strand)
-
+	res = String[]
+	for codon in Iterators.partition(strand, 3)
+        if !haskey(Codon_Protein, codon)
+            throw(DomainError("invalid codon"))
+        end
+		c = Codon_Protein[codon]
+		c == "STOP" ? break : push!(res, c)
+	end
+	return res
 end
