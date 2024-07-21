0a9711cfae948bdfa122a1780b0f336e83299050
MenkeTechnologies <linux.dev25@gmail.com>
2024-07-21 14:49:45 -0400

    bfb

--- perl5/booking-up-for-beauty/lib/BookingUpForBeauty.pm
+++ perl5/booking-up-for-beauty/lib/BookingUpForBeauty.pm
@@ -2,36 +2,23 @@ package BookingUpForBeauty;
 
 use v5.38;
 
-# Suggested datetime modules you can use:
-#use Time::Piece;
-#use DateTime::Tiny;
+use Time::Piece;
+use Exporter 'import';
 
-# Recommended, commented out for portability.
-#use Readonly;
-
-use Exporter ('import');
-our @EXPORT_OK = ();
-
-my $STRPTIME_FORMAT = '%Y-%m-%d' . 'T' . '%H:%M:%S';
-#Readonly::Scalar $STRPTIME_FORMAT => $STRPTIME_FORMAT;
-
-# Private subroutines conventionally start with an underscore.
-# It isn't necessary, but provided for convenience.
-sub _parse_datetime ($date_string) {
-    return $date_string;
-}
+our @EXPORT_OK = qw<appointment_has_passed is_afternoon_appointment describe_appointment>;
+my $STRPTIME_FORMAT = '%Y-%m-%dT%H:%M:%S';
 
 sub appointment_has_passed ($date_string) {
-    my $time = _parse_datetime($date_string);
-    return undef;
+    my $time = Time::Piece->strptime($date_string, $STRPTIME_FORMAT);
+    $time < localtime;
 }
 
 sub is_afternoon_appointment ($date_string) {
-    my $time = _parse_datetime($date_string);
-    return undef;
+    my $time = Time::Piece->strptime($date_string, $STRPTIME_FORMAT);
+    12 <= $time->hour < 18;
 }
 
 sub describe_appointment ($date_string) {
-    my $time = _parse_datetime($date_string);
-    return sprintf('You have an appointment on %02d/%02d/%04d %d:%02d %s', 1, 31, 2019, 6, 4, 'PM');
+    my $time = Time::Piece->strptime($date_string, $STRPTIME_FORMAT);
+    $time->strftime('You have an appointment on %m/%d/%Y %-I:%M %p');
 }
