ec62561ec9ec3c8cc3da3bcb9fd963a63648c96f
MenkeTechnologies <linux.dev25@gmail.com>
2024-07-28 11:44:53 -0400

    rot

--- dart/rotational-cipher/lib/rotational_cipher.dart
+++ dart/rotational-cipher/lib/rotational_cipher.dart
@@ -1,3 +1,10 @@
 class RotationalCipher {
-  // your code here...
+  String rotate({required String text, required int shiftKey}) =>
+      String.fromCharCodes(text.runes.map((rune) => caesar(rune, shiftKey)));
+
+  int caesar(int rune, int rotation) => switch (rune) {
+        >= 65 && <= 90 => 65 + (rune - 65 + rotation) % 26,
+        >= 97 && <= 122 => 97 + (rune - 97 + rotation) % 26,
+        _ => rune,
+      };
 }
