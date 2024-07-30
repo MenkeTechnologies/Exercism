f554041b37d3d86101132e740b3b8f8b48b7d4c1
MenkeTechnologies <linux.dev25@gmail.com>
2024-07-30 11:25:24 -0400

    lsp

--- groovy/largest-series-product/src/main/groovy/LargestSeriesProduct.groovy
+++ groovy/largest-series-product/src/main/groovy/LargestSeriesProduct.groovy
@@ -1,5 +1,13 @@
-class Exercise {
-    static largestProduct(digits, span) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+class LargestSeriesProduct {
+    static largestProduct(digitsString, span) {
+        if (span == 0) return 1
+        if (span < 0) throw new IllegalArgumentException("span must not be negative")
+        if (digitsString.size() < span) throw new IllegalArgumentException("span must be smaller than string length")
+        if (digitsString.toCharArray().any { !Character.isDigit(it) }) throw new IllegalArgumentException("digits input must only contain digits")
+        return digitsString.toCharArray()
+                .collect { Character.getNumericValue(it) }
+                .collate(span, 1, false)
+                .collect { it.inject(1) { acc, n -> acc * n } }
+                .max()
     }
 }
