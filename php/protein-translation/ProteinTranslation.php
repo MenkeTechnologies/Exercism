e210c1e02d0325988c310d894ef0a9e449d94cdf
MenkeTechnologies <linux.dev25@gmail.com>
2024-07-31 10:23:15 -0400

    pt

--- php/protein-translation/ProteinTranslation.php
+++ php/protein-translation/ProteinTranslation.php
@@ -1,33 +1,30 @@
 <?php
-
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 class ProteinTranslation
 {
-    public function getProteins()
+    public function getProteins(string $rna): array
     {
-        throw new \BadMethodCallException(sprintf('Implement the %s method', __FUNCTION__));
+        $res = [];
+        $codons = str_split($rna, 3);
+
+        foreach ($codons as $codon) {
+            $check_for_stop = match ($codon) {
+                'AUG' => $res[] = 'Methionine',
+                'UUU', 'UUC' => $res[] = 'Phenylalanine',
+                'UUA', 'UUG' => $res[] = 'Leucine',
+                'UCU', 'UCC', 'UCA', 'UCG' => $res[] = 'Serine',
+                'UAU', 'UAC' => $res[] = 'Tyrosine',
+                'UGU', 'UGC' => $res[] = 'Cysteine',
+                'UGG' => $res[] = 'Tryptophan',
+                'UAA', 'UAG', 'UGA' => 'STOP',
+                default => throw new InvalidArgumentException('Invalid codon'),
+            };
+            if ($check_for_stop === 'STOP') {
+                break;
+            }
+        }
+
+        return $res;
     }
 }
