f026318e9dcf12f34fe5c4afb43f94e29d76803b
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-05 11:22:44 -0400

    gs

--- dart/grade-school/lib/grade_school.dart
+++ dart/grade-school/lib/grade_school.dart
@@ -1,3 +1,24 @@
+typedef Student = (String name, int grade);
+
 class GradeSchool {
-  // your code here ...
+  List<Student> _db = [];
+
+  List<String> roster() => _db.map((kv) => kv.$1).toList();
+
+  List<bool> add(List<Student> students) => [for (var s in students) _add(s)];
+
+  bool _add(Student student) {
+    final students = roster();
+    final studentAlreadyExists = students.contains(student.$1);
+    if (!studentAlreadyExists) {
+      _db.add(student);
+      _db.sort((a, b) =>
+      a.$2.compareTo(b.$2) == 0
+          ? a.$1.compareTo(b.$1)
+          : a.$2.compareTo(b.$2));
+    }
+    return !studentAlreadyExists;
+  }
+
+  List<String> grade(int grade) => [for (var s in _db) if (s.$2 == grade) s.$1];
 }
