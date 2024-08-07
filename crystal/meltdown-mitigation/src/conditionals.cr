c63b3170cab83df6ebccc650d29215b55f577fbc
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-07 10:40:01 -0400

    meltdown

--- crystal/meltdown-mitigation/src/conditionals.cr
+++ crystal/meltdown-mitigation/src/conditionals.cr
@@ -1,13 +1,15 @@
 class Reactor
-  def self.is_criticality_balanced?(temperature, neutrons_emitted)
-    raise "Please implement the Reactor.is_criticality_balanced? method"
+  def self.criticality_balanced?(temperature, neutrons_emitted)
+    temperature < 800 && neutrons_emitted > 500 && temperature * neutrons_emitted < 500000
   end
-
   def self.reactor_efficiency(voltage, current, theoretical_max_power)
-    raise "Please implement the Reactor.reactor_efficiency method"
+    efficiency = ((voltage * current) / theoretical_max_power) * 100
+    efficiency >= 80 ? "green" : efficiency < 80 && efficiency >= 60 ? "orange" : efficiency < 60 && efficiency >= 30 ? "red" : "black"
   end
-
   def self.fail_safe(temperature, neutrons_produced_per_second, threshold)
-    raise "Please implement the Reactor.fail_safe method"
+    criticality = temperature * neutrons_produced_per_second
+    thresh90 = 0.9 * threshold
+    thresh10 = 0.1 * threshold
+    criticality < thresh90 ? "LOW" : criticality > threshold - thresh10 && criticality < threshold + thresh10 ? "NORMAL" : "DANGER"
   end
 end
