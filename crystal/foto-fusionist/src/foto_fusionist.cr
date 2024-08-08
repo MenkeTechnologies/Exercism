0867f5c71edf8e6771c70fcb9051b9c3dd09a573
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-08 11:23:45 -0400

    ff

--- crystal/foto-fusionist/src/foto_fusionist.cr
+++ crystal/foto-fusionist/src/foto_fusionist.cr
@@ -1,17 +1,14 @@
 class FotoFusionist
   def self.binary_to_int(binary : String) : Int32
-    raise "Please implement the FotoFusionist.binary_to_int method"
+    binary.to_i(2)
   end
-
   def self.least_significant_bit(number : Int32) : Int32
-    raise "Please implement the FotoFusionist.least_significant_bit method"
+    number.bit(0)
   end
-
   def self.hex_value(number : Int32) : String
-    raise "Please implement the FotoFusionist.hex_value method"
+    number.to_s(16, precision: 2)
   end
-
   def self.hex_value_from_rgb(red : Int32, green : Int32, blue : Int32) : String
-    raise "Please implement the FotoFusionist.hex_value_from_rgb method"
+    "#{hex_value(red)}#{hex_value(green)}#{hex_value(blue)}"
   end
 end
