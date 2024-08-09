25ad700b759aa9f0c5a76b0a55af49d34568b784
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-09 10:21:19 -0400

    int

--- crystal/interest-is-interesting/src/interest_is_interesting.cr
+++ crystal/interest-is-interesting/src/interest_is_interesting.cr
@@ -1,17 +1,25 @@
 class SavingsAccount
-  def self.interest_rate(balance)
-    raise "Please implement the SavingsAccount.interest_rate method"
-  end
-
-  def self.interest(balance)
-    raise "Please implement the SavingsAccount.interest method"
-  end
-
-  def self.annual_balance_update(balance)
-    raise "Please implement the SavingsAccount.annual_balance_update method"
-  end
-
-  def self.years_before_desired_balance(current_balance, target_balance)
-    raise "Please implement the SavingsAccount.years_before_desired_balance method"
-  end
+    def self.interest_rate(balance)
+        case balance
+        when 5000.. then 2.475
+        when 1000...5000 then 1.621
+        when 0...1000 then 0.5
+        else 3.213
+        end
+    end
+    def self.interest(balance)
+        balance * interest_rate(balance) / 100
+    end
+    def self.annual_balance_update(balance)
+        balance + interest(balance)
+    end
+    def self.years_before_desired_balance(current_balance, target_balance)
+        cnt = 0
+        while current_balance <= target_balance
+            current_balance = annual_balance_update(current_balance)
+            cnt += 1
+        end
+        cnt
+    end
 end
+
