bc8899583c151e3887c3021ec4b5489de05ca1f9
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-12 11:30:26 -0400

    lib

--- crystal/library-of-luton/src/library_of_luton.cr
+++ crystal/library-of-luton/src/library_of_luton.cr
@@ -1,17 +1,19 @@
 class Library
-  def self.first_letter(title : String) : Char
-    raise "Please implement the Library.first_letter method"
+  def self.first_letter(s : String)
+      s[0]
   end
-
-  def self.initials(first_name : String, last_name : String) : String
-    raise "Please implement the Library.initials method"
+  def self.initials(first_name : String, last_name : String)
+      "#{first_letter(first_name)}.#{first_letter(last_name)}"
   end
-
-  def self.decrypt_character(character : Char) : Char
-    raise "Please implement the Library.decrypt_character method"
+  def self.decrypt_character(c : Char)
+    case c
+        when 'A' then 'Z'
+        when 'a' then 'z'
+        when .letter? then c.pred()
+        else c
+    end
   end
-
-  def self.decrypt_text(text : String) : String
-    raise "Please implement the Library.decrypt_text method"
+  def self.decrypt_text(s : String)
+      s.chars.map{ |c| decrypt_character(c) }.join
   end
 end
