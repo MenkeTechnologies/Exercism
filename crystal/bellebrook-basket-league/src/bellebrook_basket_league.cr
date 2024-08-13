c9bc1ee1a42446827dd6ee8ec7528ca14b3f1a04
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-13 11:48:40 -0400

    bbl

--- crystal/bellebrook-basket-league/src/bellebrook_basket_league.cr
+++ crystal/bellebrook-basket-league/src/bellebrook_basket_league.cr
@@ -1,3 +1,20 @@
-# To Do: Define the module TicketingReservation
+module TicketingReservation
+  def tickets_available
+      @tickets_available
+  end
 
-# To Do: Define the class TicketSystem
+  def order_ticket?
+    return false if tickets_available < 100
+    @tickets_available -= 1
+    true
+  end
+
+  def order_message(name : String)
+      order_ticket? ? "#{name}, your purchase was successful, your ticket number is ##{tickets_available + 1}, and the game is played at the #{@stadium} stadium." : "#{name}, your purchase was unsuccessful, there are not enough tickets available."
+  end
+end
+
+class TicketSystem
+  def initialize(@tickets_available : Int32, @stadium : String) end
+  include TicketingReservation
+end
