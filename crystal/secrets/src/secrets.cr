49cd726501c9b7d4ad658bb376a24f9493a1ec8e
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-14 10:23:09 -0400

    sec

--- crystal/secrets/src/secrets.cr
+++ crystal/secrets/src/secrets.cr
@@ -1,17 +1,14 @@
 module Secrets
-  def self.shift_back(value : UInt8, number_of_places : UInt8) : UInt8
-    raise "Please implement the Secrets.shift_back method"
+  def self.shift_back(n : UInt8, number_of_places : UInt8)
+    n << number_of_places
   end
-
-  def self.apply_mask(value : UInt8, mask : UInt8) : UInt8
-    raise "Please implement the Secrets.apply_mask method"
+  def self.apply_mask(n : UInt8, mask : UInt8)
+    n & mask
   end
-
-  def self.set_bits(value1 : UInt8, value2 : UInt8) : UInt8
-    raise "Please implement the Secrets.set_bits method"
+  def self.set_bits(n1 : UInt8, n2 : UInt8)
+    n1 | n2
   end
-
-  def self.reverse_xor(value : UInt8, agreed_value : UInt8) : UInt8
-    raise "Please implement the Secrets.reverse_xor method"
+  def self.reverse_xor(n : UInt8, agreed_value : UInt8)
+    n ^ ~agreed_value
   end
 end
