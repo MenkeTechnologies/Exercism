5b6cd5e10cc3c62a53c04350f55cdc46f9b32a85
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-15 10:27:04 -0400

    cg

--- crystal/chess-game/src/chess_game.cr
+++ crystal/chess-game/src/chess_game.cr
@@ -1,16 +1,15 @@
 module Chess
-  # TODO: define the 'RANKS' constant
-  # TODO: define the 'FILES' constant
-
-  def self.valid_square?(rank, file)
-    raise "Please implement the Chess.valid_square? method"
+  RANKS = 1..8
+  FILES = 'A'..'H'
+  def self.valid_square?(rank : Int32, file : Char) : Bool
+    RANKS.includes?(rank) && FILES.includes?(file)
   end
-
-  def self.nickname(first_name, last_name)
-    raise "Please implement the Chess.nickname method"
+  def self.nickname(first_name : String, last_name : String) : String
+    "#{first_name[..1]}#{last_name[-2..]}".upcase
   end
-
-  def self.move_message(first_name, last_name, square)
-    raise "Please implement the Chess.move_message method"
+  def self.move_message(first : String, last : String, square : String) : String
+    valid_square?(square[1].to_i, square[0]) ?
+      "#{nickname(first, last)} moved to #{square}" :
+      "#{nickname(first, last)} attempted to move to #{square}, but that is not a valid square"
   end
 end
