6fd91ec93cd8f85bcd84a675e600877913f137f2
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-19 10:48:55 -0400

    calc

--- java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,19 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+        if (operation == null) throw new IllegalArgumentException("Operation cannot be null");
+        switch (operation) {
+            case "":
+                throw new IllegalArgumentException("Operation cannot be empty");
+            case "+":
+                return "%d %s %d = %d".formatted(operand1, operation, operand2, operand1 + operand2);
+            case "*":
+                return "%d %s %d = %d".formatted(operand1, operation, operand2, operand1 * operand2);
+            case "/":
+                if (operand2 == 0)
+                    throw new IllegalOperationException("Division by zero is not allowed", new ArithmeticException());
+                return "%d %s %d = %d".formatted(operand1, operation, operand2, operand1 / operand2);
+            default:
+                throw new IllegalOperationException(String.format("Operation '%s' does not exist", operation));
+        }
     }
 }
