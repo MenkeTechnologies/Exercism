d0acdf0022a14fae8679865848d669d0180809cf
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-20 23:47:41 -0400

    rct

--- java/resistor-color-trio/src/main/java/ResistorColorTrio.java
+++ java/resistor-color-trio/src/main/java/ResistorColorTrio.java
@@ -1,5 +1,16 @@
+import java.util.Map;
+
 class ResistorColorTrio {
+    Map<String, Integer> bandColors = Map.of(
+            "black", 0, "brown", 1, "red", 2, "orange", 3, "yellow", 4,
+            "green", 5, "blue", 6, "violet", 7, "grey", 8, "white", 9);
+
     String label(String[] colors) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        long resistance = ((bandColors.get(colors[0]) * 10) + bandColors.get(colors[1]))
+                * (long) Math.pow(10, bandColors.get(colors[2]));
+        return resistance >= 1_000_000_000 ? "%d gigaohms".formatted(resistance / 1_000_000_000)
+                : resistance >= 1_000_000 ? "%d megaohms".formatted(resistance / 1_000_000)
+                : resistance >= 1_000 ? "%d kiloohms".formatted(resistance / 1_000)
+                : "%d ohms".formatted(resistance);
     }
 }
--- java/resistor-color-trio/src/test/java/ResistorColorTrioTest.java
+++ java/resistor-color-trio/src/test/java/ResistorColorTrioTest.java
@@ -1,6 +1,5 @@
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,79 +14,79 @@ public class ResistorColorTrioTest {
     @Test
     public void testOrangeAndOrangeAndBlack() {
         assertThat(
-            resistorColorTrio.label(new String[]{"orange", "orange", "black"})
+                resistorColorTrio.label(new String[]{"orange", "orange", "black"})
         ).isEqualTo("33 ohms");
     }
-    
-    @Ignore("Remove to run test")
+   
+
     @Test
     public void testBlueAndGreyAndBrown() {
         assertThat(
-            resistorColorTrio.label(new String[]{"blue", "grey", "brown"})
+                resistorColorTrio.label(new String[]{"blue", "grey", "brown"})
         ).isEqualTo("680 ohms");
     }
-    
-    @Ignore("Remove to run test")
+
+
     @Test
     public void testRedAndBlackAndRed() {
         assertThat(
-            resistorColorTrio.label(new String[]{"red", "black", "red"})
+                resistorColorTrio.label(new String[]{"red", "black", "red"})
         ).isEqualTo("2 kiloohms");
     }
-    
-    @Ignore("Remove to run test")
+
+
     @Test
     public void testGreenAndBrownAndOrange() {
         assertThat(
-            resistorColorTrio.label(new String[]{"green", "brown", "orange"})
+                resistorColorTrio.label(new String[]{"green", "brown", "orange"})
         ).isEqualTo("51 kiloohms");
     }
-    
-    @Ignore("Remove to run test")
+
+
     @Test
     public void testYellowAndVioletAndYellow() {
         assertThat(
-            resistorColorTrio.label(new String[]{"yellow", "violet", "yellow"})
+                resistorColorTrio.label(new String[]{"yellow", "violet", "yellow"})
         ).isEqualTo("470 kiloohms");
     }
-    
-    @Ignore("Remove to run test")
+
+
     @Test
     public void testBlueAndVioletAndBlue() {
         assertThat(
-            resistorColorTrio.label(new String[]{"blue", "violet", "blue"})
+                resistorColorTrio.label(new String[]{"blue", "violet", "blue"})
         ).isEqualTo("67 megaohms");
     }
-    
-    @Ignore("Remove to run test")
+
+
     @Test
     public void testBlackAndBlackAndBlack() {
         assertThat(
-            resistorColorTrio.label(new String[]{"black", "black", "black"})
+                resistorColorTrio.label(new String[]{"black", "black", "black"})
         ).isEqualTo("0 ohms");
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void testWhiteAndWhiteAndWhite() {
         assertThat(
-            resistorColorTrio.label(new String[]{"white", "white", "white"})
+                resistorColorTrio.label(new String[]{"white", "white", "white"})
         ).isEqualTo("99 gigaohms");
     }
-    
-    @Ignore("Remove to run test")
+
+
     @Test
     public void testFirstTwoColorsMakeAnInvalidOctalNumber() {
         assertThat(
-            resistorColorTrio.label(new String[]{"black", "grey", "black"})
+                resistorColorTrio.label(new String[]{"black", "grey", "black"})
         ).isEqualTo("8 ohms");
     }
 
-    @Ignore("Remove to run test")
+
     @Test
     public void testIgnoreExtraColors() {
         assertThat(
-            resistorColorTrio.label(new String[]{"blue", "green", "yellow", "orange"})
+                resistorColorTrio.label(new String[]{"blue", "green", "yellow", "orange"})
         ).isEqualTo("650 kiloohms");
     }
 }
