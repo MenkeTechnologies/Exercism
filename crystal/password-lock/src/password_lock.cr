bdde3550feb6347c13bce586373df80726f13d6f
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-21 14:19:19 -0400

    lk

--- crystal/password-lock/src/password_lock.cr
+++ crystal/password-lock/src/password_lock.cr
@@ -1,1 +1,19 @@
-# TODO: Define the class PasswordLock
+class PasswordLock
+  def initialize(@password : (String | Int32 | Float64)) end
+
+  def encrypt
+      @password = logic(@password)
+  end
+
+  def unlock?(key)
+      "Unlocked" if logic(key) == @password
+  end
+
+  def logic(key)
+    case key
+      when Float64 then key * 4
+      when String  then key.reverse
+      else (key / 2).round
+    end
+  end
+end
