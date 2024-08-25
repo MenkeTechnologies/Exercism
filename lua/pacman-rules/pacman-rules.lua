b7785dbb0b219f9ed6767b12c52ee2c15e037fc9
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-25 12:31:33 -0400

    pacm

--- lua/pacman-rules/pacman-rules.lua
+++ lua/pacman-rules/pacman-rules.lua
@@ -1,23 +1,6 @@
-local rules = {}
-
--- returns if Pac-Man was able to eat the ghost or not
-function rules.eat_ghost(power_pellet_active, touching_ghost)
-  -- eat_ghost body
-end
-
--- returns if Pac-Man ate a power pellet or a dot
-function rules.score(touching_power_pellet, touching_dot)
-  -- score body
-end
-
--- returns if Pac-Man lost by touching a ghost without a power pellet
-function rules.lose(power_pellet_active, touching_ghost)
-  -- lose body
-end
-
--- returns if Pac-Man won by eating all dots and has not touched a ghost without a power pellet
-function rules.win(has_eaten_all_dots, power_pellet_active, touching_ghost)
-  -- win body
-end
-
-return rules
+return {
+  eat_ghost = function(a, b) return a and b end,
+  score = function(a, b) return a or b end,
+  lose = function(a, b) return not a and b end,
+  win = function(a, b, c) return a and b or not c end,
+}
