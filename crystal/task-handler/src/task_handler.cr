6a07c90b4c99447a5ad72afe6f8da71d1bb04742
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-27 09:32:37 -0400

    th

--- crystal/task-handler/src/task_handler.cr
+++ crystal/task-handler/src/task_handler.cr
@@ -1,13 +1,13 @@
 class TaskHandler
-  def initialize(tasks : Array(String))
-    raise "Please implement the TaskHandler#initialize method"
-  end
 
+  def initialize(@tasks : Array(String))
+    @task_condition_logic = Proc(Int32, Bool).new {|x| x >= 0}
+  end
   def update_task_condition_logic(&block : Int32 -> Bool)
-    raise "Please implement the TaskHandler#update_task_condition_logic method"
+    @task_condition_logic = block
   end
-
-  def execute(priority : Int32)
-    raise "Please implement the TaskHandler#execute method"
+  def execute(priority : Int32) : String
+    @task_condition_logic.call(priority) ? "Completed #{@tasks.shift}" : "Could not complete #{@tasks.first}"
   end
 end
+
