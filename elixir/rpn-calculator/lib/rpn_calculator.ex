1879a0de853fcbef3d4868ce98a2259707bc835d
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-28 11:05:46 -0400

    rpn

--- elixir/rpn-calculator/lib/rpn_calculator.ex
+++ elixir/rpn-calculator/lib/rpn_calculator.ex
@@ -1,13 +1,14 @@
 defmodule RPNCalculator do
-  def calculate!(stack, operation) do
-    # Please implement the calculate!/2 function
+  def calculate!(stk, op), do: op.(stk)
+
+  def calculate(stk, op, flag \\ :default) do
+    try do
+      {:ok, op.(stk)}
+    rescue
+      e -> if flag == :verbose, do: {:error, e.message}, else: :error
+    end
   end
 
-  def calculate(stack, operation) do
-    # Please implement the calculate/2 function
-  end
+  def calculate_verbose(stk, op), do: calculate(stk, op, :verbose)
 
-  def calculate_verbose(stack, operation) do
-    # Please implement the calculate_verbose/2 function
-  end
 end
