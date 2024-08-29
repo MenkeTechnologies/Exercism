e05e3f17cbdbb8d228cc400fbca09509b98bdbbb
MenkeTechnologies <linux.dev25@gmail.com>
2024-08-29 10:59:32 -0400

    lucas

--- elixir/lucas-numbers/lib/lucas_numbers.ex
+++ elixir/lucas-numbers/lib/lucas_numbers.ex
@@ -1,11 +1,8 @@
 defmodule LucasNumbers do
-  @moduledoc """
-  Lucas numbers are an infinite sequence of numbers which build progressively
-  which hold a strong correlation to the golden ratio (φ or ϕ)
-
-  E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
-  """
-  def generate(count) do
-    # Please implement the generate/1 function
+  def generate(cnt) when is_number(cnt) and cnt > 0 do
+    {-1, 2} |> Stream.iterate(fn {x, y} -> {y, x + y} end)
+      |> Enum.take(cnt)
+      |> Enum.map(&elem(&1, 1))
   end
+  def generate(_), do: raise(ArgumentError, "count must be specified as an integer >= 1")
 end
