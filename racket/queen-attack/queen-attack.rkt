8bbe52e0e3c2dd9c949c0c63233410db4526ca39
MenkeTechnologies <linux.dev25@gmail.com>
2024-09-12 01:45:19 -0400

    qa

--- racket/queen-attack/queen-attack.rkt
+++ racket/queen-attack/queen-attack.rkt
@@ -1,10 +1,15 @@
 #lang racket
-
 (provide create-queen
          can-attack?)
 
-(define (create-queen row column)
-  (error "Not implemented yet"))
+(define (create-queen row col)
+  (if (or (negative? row) (negative? col) (> row 7) (> col 7))
+    (error "invalid coordinates")
+    (cons row col)))
 
 (define (can-attack? white-queen black-queen)
-  (error "Not implemented yet"))
+  (if (equal? white-queen black-queen)
+    (error "both queens on the same square")
+    (let ([dx (abs (- (car white-queen) (car black-queen)))]
+          [dy (abs (- (cdr white-queen) (cdr black-queen)))])
+          (or (zero? dx) (zero? dy) (= dx dy)))))
