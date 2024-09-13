8333b88c3389557e9c1f0cdd3a6ab95fb4fb6599
MenkeTechnologies <linux.dev25@gmail.com>
2024-09-13 09:30:29 -0400

    subl

--- cpp/sublist/sublist.cpp
+++ cpp/sublist/sublist.cpp
@@ -1,5 +1,14 @@
 #include "sublist.h"
+#include <algorithm>
 
 namespace sublist {
-
-}  // namespace sublist
+    List_comparison sublist(const vector<int> &l1, const vector<int> &l2) {
+        if (l1.size() == l2.size()) return l1 == l2 ? List_comparison::equal : List_comparison::unequal;
+        if (l1.size() < l2.size()) {
+            return search(l2.begin(), l2.end(), l1.begin(), l1.end()) != l2.end() ? List_comparison::sublist : List_comparison::unequal;
+        } else {
+            return search(l1.begin(), l1.end(), l2.begin(), l2.end()) != l1.end() ? List_comparison::superlist
+                                                                                  : List_comparison::unequal;
+        }
+    }
+} 
--- cpp/sublist/sublist.h
+++ cpp/sublist/sublist.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <vector>
+using namespace std;
 
 namespace sublist {
-
-}  // namespace sublist
+	enum class List_comparison {equal, sublist, superlist, unequal};
+	List_comparison sublist(const vector<int>& l1, const vector<int>& l2);
+}
