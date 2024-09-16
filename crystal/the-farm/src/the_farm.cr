20373e44028bec31e81015689b3424b87eb0a23a
MenkeTechnologies <linux.dev25@gmail.com>
2024-09-16 12:37:04 -0400

    tf

--- crystal/the-farm/src/the_farm.cr
+++ crystal/the-farm/src/the_farm.cr
@@ -1,15 +1,24 @@
-# Implement the InvalidCowsError here
-
+class InvalidCowsError < Exception
+  def initialize(n : Int32)
+    @message = n < 0 ? "#{n} cows are invalid: there are no negative cows" 
+                     : "#{n} cows are invalid: no cows don't need food"
+  end
+end
 module TheFarm
   def self.divide_food(food : FodderCalculator, number_of_cows : Int32)
-    raise "Please implement the TheFarm.divide_food method"
+    begin
+        (food.fodder_amount! * food.fattening_factor!) / number_of_cows
+    rescue
+        0
+    end
   end
-
+ 
   def self.validate_input_and_divide_food(food : FodderCalculator, number_of_cows : Int32)
-    raise "Please implement the TheFarm.validate_input_and_divide_food method"
+    raise Exception.new("Number of cows must be greater than 0") if number_of_cows < 1
+    divide_food(food, number_of_cows)
   end
-
+ 
   def self.validate_number_of_cows(number_of_cows : Int32)
-    raise "Please implement the TheFarm.validate_number_of_cows method"
+    raise InvalidCowsError.new(number_of_cows) if number_of_cows < 1
   end
 end
