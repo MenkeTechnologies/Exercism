4f527352246ea0aec15c80418d35a500be0aabbd
MenkeTechnologies <linux.dev25@gmail.com>
2024-09-21 00:45:12 -0400

    series

--- r/series/series.R
+++ r/series/series.R
@@ -1,3 +1,4 @@
-slices <- function(series, slice_length) {
-
+slices = function(series, len) {
+    if (nchar(series) < len || len < 1) stop()
+    substring(series, 1:(nchar(series) - len + 1), len:nchar(series))
 }
