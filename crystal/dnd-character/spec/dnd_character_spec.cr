4ebae0cbc99e38bfb1a422d3bb77d13bdba284c8
MenkeTechnologies <linux.dev25@gmail.com>
2024-09-24 11:36:50 -0400

    dnd

--- crystal/dnd-character/spec/dnd_character_spec.cr
+++ crystal/dnd-character/spec/dnd_character_spec.cr
@@ -7,73 +7,73 @@ describe "DndCharacter" do
       DndCharacter.modifier(3).should eq(-4)
     end
 
-    pending "ability modifier for score 4 is -3" do
+    it "ability modifier for score 4 is -3" do
       DndCharacter.modifier(4).should eq(-3)
     end
 
-    pending "ability modifier for score 5 is -3" do
+    it "ability modifier for score 5 is -3" do
       DndCharacter.modifier(5).should eq(-3)
     end
 
-    pending "ability modifier for score 6 is -2" do
+    it "ability modifier for score 6 is -2" do
       DndCharacter.modifier(6).should eq(-2)
     end
 
-    pending "ability modifier for score 7 is -2" do
+    it "ability modifier for score 7 is -2" do
       DndCharacter.modifier(7).should eq(-2)
     end
 
-    pending "ability modifier for score 8 is -1" do
+    it "ability modifier for score 8 is -1" do
       DndCharacter.modifier(8).should eq(-1)
     end
 
-    pending "ability modifier for score 9 is -1" do
+    it "ability modifier for score 9 is -1" do
       DndCharacter.modifier(9).should eq(-1)
     end
 
-    pending "ability modifier for score 10 is 0" do
+    it "ability modifier for score 10 is 0" do
       DndCharacter.modifier(10).should eq(0)
     end
 
-    pending "ability modifier for score 11 is 0" do
+    it "ability modifier for score 11 is 0" do
       DndCharacter.modifier(11).should eq(0)
     end
 
-    pending "ability modifier for score 12 is +1" do
+    it "ability modifier for score 12 is +1" do
       DndCharacter.modifier(12).should eq(1)
     end
 
-    pending "ability modifier for score 13 is +1" do
+    it "ability modifier for score 13 is +1" do
       DndCharacter.modifier(13).should eq(1)
     end
 
-    pending "ability modifier for score 14 is +2" do
+    it "ability modifier for score 14 is +2" do
       DndCharacter.modifier(14).should eq(2)
     end
 
-    pending "ability modifier for score 15 is +2" do
+    it "ability modifier for score 15 is +2" do
       DndCharacter.modifier(15).should eq(2)
     end
 
-    pending "ability modifier for score 16 is +3" do
+    it "ability modifier for score 16 is +3" do
       DndCharacter.modifier(16).should eq(3)
     end
 
-    pending "ability modifier for score 17 is +3" do
+    it "ability modifier for score 17 is +3" do
       DndCharacter.modifier(17).should eq(3)
     end
 
-    pending "ability modifier for score 18 is +4" do
+    it "ability modifier for score 18 is +4" do
       DndCharacter.modifier(18).should eq(4)
     end
   end
 
-  pending "random ability is within range" do
+  it "random ability is within range" do
     DndCharacter.ability.should be >= 3
     DndCharacter.ability.should be <= 18
   end
 
-  pending "random character is valid" do
+  it "random character is valid" do
     character = DndCharacter.new
     character.strength.should be >= 3
     character.strength.should be <= 18
@@ -91,7 +91,7 @@ describe "DndCharacter" do
     character.hitpoints.should eq(10 + DndCharacter.modifier(character.constitution))
   end
 
-  pending "each ability is only calculated once" do
+  it "each ability is only calculated once" do
     character = DndCharacter.new
     character.strength.should eq character.strength
     character.dexterity.should eq character.dexterity
--- crystal/dnd-character/src/dnd_character.cr
+++ crystal/dnd-character/src/dnd_character.cr
@@ -1,6 +1,24 @@
 class DndCharacter
-  # Write your code for the 'DnD Character' exercise in this file.
-  def initialize(name : String = "Blandalf the Beige")
-    @name = name
+  getter name
+  getter strength : Int32 = ability
+  getter dexterity : Int32 = ability
+  getter constitution : Int32 = ability
+  getter intelligence : Int32 = ability
+  getter wisdom : Int32 = ability
+  getter charisma : Int32 = ability
+
+  def initialize(@name = "Blandalf the Beige")
+  end
+
+  def self.ability
+    Array.new(4) { Random.rand(1..6) }.sort.last(3).sum
+  end
+
+  def self.modifier(score)
+    ((score - 10) / 2.0).floor
+  end
+
+  def hitpoints
+      10 + DndCharacter.modifier(@constitution)
   end
 end
