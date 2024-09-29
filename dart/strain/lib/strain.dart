067dbff71274df641403f767593f6072c9dd6357
MenkeTechnologies <linux.dev25@gmail.com>
2024-09-29 08:14:04 -0400

    strain

--- dart/strain/lib/strain.dart
+++ dart/strain/lib/strain.dart
@@ -1,3 +1,12 @@
 class Strain {
-  // your code here...
+  List<T> keep<T>(List<T> lst, bool f(T element)) {
+    final res = <T>[];
+    for (final elem in lst)
+      if (f(elem))
+        res.add(elem);
+    return res;
+  }
+  List<T> discard<T>(List<T> lst, bool f(T element)) {
+    return keep(lst, (elem) => !f(elem));
+  }
 }
