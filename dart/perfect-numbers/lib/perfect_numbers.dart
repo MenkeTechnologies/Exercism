f2920ea299dd89189107c926889191cac4dada8f
MenkeTechnologies <linux.dev25@gmail.com>
2024-09-30 11:16:44 -0400

    pn

--- dart/perfect-numbers/lib/perfect_numbers.dart
+++ dart/perfect-numbers/lib/perfect_numbers.dart
@@ -1,5 +1,21 @@
-// define the Classification enum
+enum Classification {
+  perfect,
+  abundant,
+  deficient,
+}
 
 class PerfectNumbers {
-  // Put your code here
+  Classification classify(int number) {
+    if (number < 1) throw ArgumentError('Number must be greater than 0');
+
+    var sum = List.generate(number - 1, (n) => n + 1)
+        .where((n) => number % n == 0)
+        .fold(0, (acc, n) => acc + n);
+
+    return sum == number
+        ? Classification.perfect
+        : sum > number
+            ? Classification.abundant
+            : Classification.deficient;
+  }
 }
--- dart/perfect-numbers/test/perfect_numbers_test.dart
+++ dart/perfect-numbers/test/perfect_numbers_test.dart
@@ -14,66 +14,68 @@ void main() {
       test('Medium perfect number is classified correctly', () {
         final result = perfectNumbers.classify(28);
         expect(result, equals(Classification.perfect));
-      }, skip: true);
+      }, skip: false);
 
       test('Large perfect number is classified correctly', () {
         final result = perfectNumbers.classify(33550336);
         expect(result, equals(Classification.perfect));
-      }, skip: true);
+      }, skip: false);
     });
 
     group('Abundant numbers', () {
       test('Smallest abundant number is classified correctly', () {
         final result = perfectNumbers.classify(12);
         expect(result, equals(Classification.abundant));
-      }, skip: true);
+      }, skip: false);
 
       test('Medium abundant number is classified correctly', () {
         final result = perfectNumbers.classify(30);
         expect(result, equals(Classification.abundant));
-      }, skip: true);
+      }, skip: false);
 
       test('Large abundant number is classified correctly', () {
         final result = perfectNumbers.classify(33550335);
         expect(result, equals(Classification.abundant));
-      }, skip: true);
+      }, skip: false);
     });
 
     group('Deficient numbers', () {
       test('Smallest prime deficient number is classified correctly', () {
         final result = perfectNumbers.classify(2);
         expect(result, equals(Classification.deficient));
-      }, skip: true);
+      }, skip: false);
 
       test('Smallest non-prime deficient number is classified correctly', () {
         final result = perfectNumbers.classify(4);
         expect(result, equals(Classification.deficient));
-      }, skip: true);
+      }, skip: false);
 
       test('Medium deficient number is classified correctly', () {
         final result = perfectNumbers.classify(32);
         expect(result, equals(Classification.deficient));
-      }, skip: true);
+      }, skip: false);
 
       test('Large deficient number is classified correctly', () {
         final result = perfectNumbers.classify(33550337);
         expect(result, equals(Classification.deficient));
-      }, skip: true);
+      }, skip: false);
 
-      test('Edge case (no factors other than itself) is classified correctly', () {
+      test('Edge case (no factors other than itself) is classified correctly',
+          () {
         final result = perfectNumbers.classify(1);
         expect(result, equals(Classification.deficient));
-      }, skip: true);
+      }, skip: false);
     });
 
     group('Invalid inputs', () {
       test('Zero is rejected (as it is not a positive integer)', () {
         expect(() => perfectNumbers.classify(0), throwsArgumentError);
-      }, skip: true);
+      }, skip: false);
 
-      test('Negative integer is rejected (as it is not a positive integer)', () {
+      test('Negative integer is rejected (as it is not a positive integer)',
+          () {
         expect(() => perfectNumbers.classify(-1), throwsArgumentError);
-      }, skip: true);
+      }, skip: false);
     });
   });
 }
