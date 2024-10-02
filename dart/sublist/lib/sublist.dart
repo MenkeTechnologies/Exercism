727b1910a72ec9578fb302d9bbed4ad3c0616c5e
MenkeTechnologies <linux.dev25@gmail.com>
2024-10-02 11:10:24 -0400

    sublist

--- dart/sublist/lib/sublist.dart
+++ dart/sublist/lib/sublist.dart
@@ -1,5 +1,16 @@
-// Implement the Classification enum.
+enum Classification { equal, unequal, sublist, superlist }
 
 class Sublist {
-  // your code here...
+
+  Classification sublist(List lst1, List lst2) {
+    String s1 = lst1.join(',') + ',';
+    String s2 = lst2.join(',') + ',';
+    return s1 == s2
+        ? Classification.equal
+        : s1.contains(s2)
+            ? Classification.superlist
+            : s2.contains(s1)
+                ? Classification.sublist
+                : Classification.unequal;
+  }
 }
--- dart/sublist/test/sublist_test.dart
+++ dart/sublist/test/sublist_test.dart
@@ -19,7 +19,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.sublist;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('non empty list contains empty list', () {
       List<int> listOne = [1, 2, 3];
@@ -27,7 +27,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.superlist;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('list equals itself', () {
       List<int> listOne = [1, 2, 3];
@@ -35,7 +35,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.equal;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('different lists', () {
       List<int> listOne = [1, 2, 3];
@@ -43,7 +43,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.unequal;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('false start', () {
       List<int> listOne = [1, 2, 5];
@@ -51,7 +51,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.sublist;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('consecutive', () {
       List<int> listOne = [1, 1, 2];
@@ -59,7 +59,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.sublist;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('sublist at start', () {
       List<int> listOne = [0, 1, 2];
@@ -67,7 +67,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.sublist;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('sublist in middle', () {
       List<int> listOne = [2, 3, 4];
@@ -75,7 +75,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.sublist;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('sublist at end', () {
       List<int> listOne = [3, 4, 5];
@@ -83,7 +83,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.sublist;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('at start of superlist', () {
       List<int> listOne = [0, 1, 2, 3, 4, 5];
@@ -91,7 +91,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.superlist;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('in middle of superlist', () {
       List<int> listOne = [0, 1, 2, 3, 4, 5];
@@ -99,7 +99,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.superlist;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('at end of superlist', () {
       List<int> listOne = [0, 1, 2, 3, 4, 5];
@@ -107,7 +107,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.superlist;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('first list missing element from second list', () {
       List<int> listOne = [1, 3];
@@ -115,7 +115,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.unequal;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('second list missing element from first list', () {
       List<int> listOne = [1, 2, 3];
@@ -123,7 +123,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.unequal;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('first list missing additional digits from second list', () {
       List<int> listOne = [1, 2];
@@ -131,7 +131,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.unequal;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('order matters to a list', () {
       List<int> listOne = [1, 2, 3];
@@ -139,7 +139,7 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.unequal;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
 
     test('same digits but different numbers', () {
       List<int> listOne = [1, 0, 1];
@@ -147,6 +147,6 @@ void main() {
       final result = sublist.sublist(listOne, listTwo);
       final expected = Classification.unequal;
       expect(result, equals(expected));
-    }, skip: true);
+    }, skip: false);
   });
 }
