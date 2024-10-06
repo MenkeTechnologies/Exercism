6fbb5f8ca9f73b763a124e3118be7f3458207193
MenkeTechnologies <linux.dev25@gmail.com>
2024-10-05 21:27:37 -0400

    more exe

--- dart/atbash-cipher/lib/atbash_cipher.dart
+++ dart/atbash-cipher/lib/atbash_cipher.dart
@@ -1,3 +1,53 @@
 class AtbashCipher {
-  // Put your code here
+  Map<String,String> encodeChar = {
+    'a': 'z',
+    'b': 'y',
+    'c': 'x',
+    'd': 'w',
+    'e': 'v',
+    'f': 'u',
+    'g': 't',
+    'h': 's',
+    'i': 'r',
+    'j': 'q',
+    'k': 'p',
+    'l': 'o',
+    'm': 'n',
+    'n': 'm',
+    'o': 'l',
+    'p': 'k',
+    'q': 'j',
+    'r': 'i',
+    's': 'h',
+    't': 'g',
+    'u': 'f',
+    'v': 'e',
+    'w': 'd',
+    'x': 'c',
+    'y': 'b',
+    'z': 'a',
+    '0': '0',
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '4': '4',
+    '5': '5',
+    '6': '6',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+  };
+  String encode(String plainText) =>
+      plainText
+          .toLowerCase()
+          .split('')
+          .map((char) => encodeChar[char] ?? '')
+          .join()
+          .replaceAllMapped(RegExp(r'.{5}'), (match) => '${match[0]} ')
+          .trimRight();
+  String decode(String cipherText) =>
+      cipherText
+          .split('')
+          .map((char) => encodeChar[char] ?? '')
+          .join();
 }
