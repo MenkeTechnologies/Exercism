9529ecc89421a9d52a6d417211e4aabc25875fee
MenkeTechnologies <linux.dev25@gmail.com>
2024-10-05 22:09:26 -0400

    gs

--- r/gigasecond/gigasecond.R
+++ r/gigasecond/gigasecond.R
@@ -1,3 +1,4 @@
-add_gigasecond <- function(moment) {
-
+add_gigasecond = function(moment) {
+  seconds = as.numeric(format(moment, "%s")) + 1e9
+  ISOdate(strftime(seconds, "%Y"), strftime(seconds, "%m"), strftime(seconds, "%d"), strftime(seconds, "%H"), strftime(seconds, "%M"), strftime(seconds, "%S"))
 }
