371c3d89456487361f0e8306345d07e9d7e53731
MenkeTechnologies <linux.dev25@gmail.com>
2024-10-11 11:16:28 -0400

    ts

--- javascript/translation-service/service.js
+++ javascript/translation-service/service.js
@@ -1,106 +1,36 @@
-/// <reference path="./global.d.ts" />
-// @ts-check
-//
-// The lines above enable type checking for this file. Various IDEs interpret
-// the @ts-check and reference directives. Together, they give you helpful
-// autocompletion when implementing this exercise. You don't need to understand
-// them in order to use it.
-//
-// In your own projects, files, and code, you can play with @ts-check as well.
-
 export class TranslationService {
-    /**
-     * Creates a new service
-     * @param {ExternalApi} api the original api
-     */
     constructor(api) {
         this.api = api;
     }
 
-    /**
-     * Attempts to retrieve the translation for the given text.
-     *
-     * - Returns whichever translation can be retrieved, regardless the quality
-     * - Forwards any error from the translation api
-     *
-     * @param {string} text
-     * @returns {Promise<string>}
-     */
     free(text) {
-        throw new Error('Implement the free function');
+        return this.api.fetch(text).then(t => t.translation);
     }
 
-    /**
-     * Batch translates the given texts using the free service.
-     *
-     * - Resolves all the translations (in the same order), if they all succeed
-     * - Rejects with the first error that is encountered
-     * - Rejects with a BatchIsEmpty error if no texts are given
-     *
-     * @param {string[]} texts
-     * @returns {Promise<string[]>}
-     */
     batch(texts) {
-        throw new Error('Implement the batch function');
+        if (!texts.length) return Promise.reject(new BatchIsEmpty);
+        return Promise.all(texts.map(this.free, this));
     }
 
-    /**
-     * Requests the service for some text to be translated.
-     *
-     * Note: the request service is flaky, and it may take up to three times for
-     *       it to accept the request.
-     *
-     * @param {string} text
-     * @returns {Promise<void>}
-     */
-    request(text) {
-        throw new Error('Implement the request function');
+    request(text, {api} = this, counter = 0) {
+        return new Promise((solve, fail) => api.request(text, function cb(err) {
+            if (err) ++counter < 3 ? api.request(text, cb) : fail(err); else solve(err);
+        }));
     }
 
-    /**
-     * Retrieves the translation for the given text
-     *
-     * - Rejects with an error if the quality can not be met
-     * - Requests a translation if the translation is not available, then retries
-     *
-     * @param {string} text
-     * @param {number} minimumQuality
-     * @returns {Promise<string>}
-     */
-    premium(text, minimumQuality) {
-        throw new Error('Implement the premium function');
+    premium(text, quality) {
+        return this.api.fetch(text).catch(_ => this.request(text).then(() => this.api.fetch(text))).then(t => t.quality >= quality ? t.translation : Promise.reject(new QualityThresholdNotMet(text)));
     }
 }
 
-/**
- * This error is used to indicate a translation was found, but its quality does
- * not meet a certain threshold. Do not change the name of this error.
- */
 export class QualityThresholdNotMet extends Error {
-    /**
-     * @param {string} text
-     */
     constructor(text) {
-        super(
-            `
-The translation of ${text} does not meet the requested quality threshold.
-    `.trim()
-        );
-
-        this.text = text;
+        super(`${text} translation below requested quality.`);
     }
 }
 
-/**
- * This error is used to indicate the batch service was called without any
- * texts to translate (it was empty). Do not change the name of this error.
- */
 export class BatchIsEmpty extends Error {
     constructor() {
-        super(
-            `
-Requested a batch translation, but there are no texts in the batch.
-    `.trim()
-        );
+        super('No texts found in the batch array.');
     }
 }
