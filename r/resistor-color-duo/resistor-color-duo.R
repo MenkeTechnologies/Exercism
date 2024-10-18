142def2f9de5d817bc59579004a7e9076b1312a6
MenkeTechnologies <linux.dev25@gmail.com>
2024-10-17 22:24:22 -0400

    rcd

--- r/resistor-color-duo/resistor-color-duo.R
+++ r/resistor-color-duo/resistor-color-duo.R
@@ -1,3 +1,5 @@
-value <- function(resistor_colors) {
+colors = c("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white") # nolint
 
+value = function(resistor_colors) {
+  (match(resistor_colors[1], colors) - 1) * 10 + match(resistor_colors[2], colors) - 1
 }
