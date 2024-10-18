9c5564f1b903f670daba11975271f770f6d242ce
MenkeTechnologies <linux.dev25@gmail.com>
2024-10-17 22:24:32 -0400

    more exe

--- r/isbn-verifier/isbn-verifier.R
+++ r/isbn-verifier/isbn-verifier.R
@@ -1,3 +1,8 @@
-is_valid <- function(isbn) {
-
+is_valid = function(isbn) {
+  noDash = gsub(pattern = "-", replacement = "", isbn)
+  if (!grepl("^\\d{9}(\\d|X)$", noDash))  return(FALSE)
+  
+  chars = noDash |> strsplit("") |> unlist() |> rev()
+  if (chars[1] == "X") chars[1] = "10"
+  sum(as.numeric(chars) * 1:10) %% 11 == 0
 }
