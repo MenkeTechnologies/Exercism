0572742fa9d64a43b8754804338cf48e5c7373d1
MenkeTechnologies <linux.dev25@gmail.com>
2024-10-21 15:11:41 -0400

    bs

--- r/binary-search/binary-search.R
+++ r/binary-search/binary-search.R
@@ -1,3 +1,17 @@
-find <- function(haystack, needle) {
+find = function(ary, target) {
+    lo = 1;
+    hi = length(ary);
+
+    while(lo <= hi) {
+        mid = floor((lo + hi) / 2);
+        if (target == ary[[mid]]) {
+            return(mid);
+        } else if (target < ary[[mid]]) {
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;
+        }
+    }
+-1;
 
 }
