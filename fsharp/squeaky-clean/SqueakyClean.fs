853528c355f88e6d3e33936ff5b87fdfa53e230b
MenkeTechnologies <linux.dev25@gmail.com>
2024-10-22 12:24:53 -0400

    sc

--- fsharp/squeaky-clean/SqueakyClean.fs
+++ fsharp/squeaky-clean/SqueakyClean.fs
@@ -2,8 +2,12 @@ module SqueakyClean
 
 open System
 
-let transform (c: char) : string =
-    failwith "Please implement the 'transform' function"
-    
-let clean (identifier: string): string =
-    failwith "Please implement the 'clean' function"
+let transform (c: char) =
+    match c with
+    | '-' -> "_"
+    | x when Char.IsWhiteSpace x || Char.IsDigit x -> ""
+    | x when Char.IsUpper x -> $"-{Char.ToLower x}"
+    | x when 'α' <= x && x <= 'ω' -> "?"
+    | x -> string x
+
+let clean (s: string) = s |> Seq.map transform |> String.Concat
