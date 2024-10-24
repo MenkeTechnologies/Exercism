a4e18c1bb3fb4c881efbf3b2988704880cfe6f18
MenkeTechnologies <linux.dev25@gmail.com>
2024-10-24 12:38:26 -0400

    zebra

--- r/flatten-array/flatten-array.R
+++ r/flatten-array/flatten-array.R
@@ -1,3 +1,12 @@
-flatten <- function(input) {
-
+flatten = function(input) {
+  flattened <- c()
+  for (element in input) {
+    if (is.null(element)) next
+    if (is.list(element)) {
+      flattened = c(flattened, flatten(element))
+    } else {
+      flattened = c(flattened, element)
+    }
+  }
+  flattened
 }
