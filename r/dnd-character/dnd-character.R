1d28741468bb316ca7fcac5d2eb2a98dd80607f6
MenkeTechnologies <linux.dev25@gmail.com>
2024-10-26 00:26:50 -0400

    dnd

--- r/dnd-character/dnd-character.R
+++ r/dnd-character/dnd-character.R
@@ -1,11 +1,21 @@
-modifier <- function(score) {
-
+modifier = function(score) {
+  floor((score - 10) / 2)
 }
 
-ability <- function() {
-
+ability = function() {
+  sum(sort(sample(1:6,4))[-1])
 }
 
-new_character <- function() {
-
+new_character = function() {
+  constitution = ability()
+    structure(list(
+    strength = ability(),
+    dexterity = ability(),
+    constitution = ability(),
+    intelligence = ability(),
+    constiution = constitution,
+    wisdom = ability(),
+    charisma = ability(),
+    hitpoints = 10 + modifier(constitution)
+  ), class = "character")
 }
