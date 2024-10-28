df6d3f48b3a9690de49128f8222b2e7868a6b53a
MenkeTechnologies <linux.dev25@gmail.com>
2024-10-28 11:40:45 -0400

    rcd

--- fsharp/resistor-color-duo/ResistorColorDuo.fs
+++ fsharp/resistor-color-duo/ResistorColorDuo.fs
@@ -1,4 +1,5 @@
 module ResistorColorDuo
+let colors = ["black"; "brown"; "red"; "orange"; "yellow"; "green"; "blue"; "violet"; "grey"; "white"]
 
-let value colors =
-    failwith "Please implement the 'value' function"
+let getIndex (target: string) = colors |> List.findIndex(fun colo -> colo = target)
+let value (colors: List<string>) = (colors[0] |> getIndex) * 10 + (colors[1] |> getIndex)
--- fsharp/resistor-color-duo/ResistorColorDuoTests.fs
+++ fsharp/resistor-color-duo/ResistorColorDuoTests.fs
@@ -9,27 +9,27 @@ open ResistorColorDuo
 let ``Brown and black`` () =
     value [ "brown"; "black" ] |> should equal 10
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Blue and grey`` () =
     value [ "blue"; "grey" ] |> should equal 68
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Yellow and violet`` () =
     value [ "yellow"; "violet" ] |> should equal 47
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``White and red`` () =
     value [ "white"; "red" ] |> should equal 92
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Orange and orange`` () =
     value [ "orange"; "orange" ] |> should equal 33
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Ignore additional colors`` () =
     value [ "green"; "brown"; "orange" ]
     |> should equal 51
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Black and brown, one-digit`` () =
     value [ "black"; "brown" ] |> should equal 1
