856c7c7905e44ab863dc914de0c7fca6dcf97aee
MenkeTechnologies <linux.dev25@gmail.com>
2024-11-04 09:52:09 -0500

    ee

--- common-lisp/eliuds-eggs/eliuds-eggs.lisp
+++ common-lisp/eliuds-eggs/eliuds-eggs.lisp
@@ -4,4 +4,8 @@
 
 (in-package :eliuds-eggs)
 
-(defun egg-count (number))
+(defun egg-count (number)
+    (do ((res 0)
+       (number number (ash number -1)))
+      ((zerop number) res)
+    (when (oddp number) (incf res))))
