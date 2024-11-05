1958d76072a9ade04b48b8b2a6d6e274e68dd6de
MenkeTechnologies <linux.dev25@gmail.com>
2024-11-05 17:44:15 -0500

    rvs

--- batch/reverse-string/ReverseString.bat
+++ batch/reverse-string/ReverseString.bat
@@ -4,7 +4,16 @@ setlocal enabledelayedexpansion
 set "str=%~1"
 set "rev="
 
-REM Your code goes here
+setlocal enabledelayedexpansion
 
+set num=0
+
+:LOOP
+call set tmpa=%%str:~%num%,1%%%
+set /a num+=1
+if not "%tmpa%" equ "" (
+set rev=%tmpa%%rev%
+goto LOOP
+)
 
 echo %rev%
