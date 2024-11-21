e461734b3fa26be399c95ef7a219e5610359c2cb
MenkeTechnologies <linux.dev25@gmail.com>
2024-11-21 10:47:52 -0500

    dos

--- euphoria/difference-of-squares/difference-of-squares.ex
+++ euphoria/difference-of-squares/difference-of-squares.ex
@@ -1,8 +1,19 @@
--- public function squareOfSum(atom n)
--- end function
+public function squareOfSum(integer limit)
+  integer sum = 0
+  for n = 1 to limit do
+      sum += n
+  end for
+  return sum * sum
+end function
 
--- public function sumOfSquares(atom n)
--- end function
+public function sumOfSquares(integer limit)
+  integer sum = 0
+  for n = 1 to limit do
+      sum += n * n
+  end for
+  return sum
+end function
 
--- public function differenceOfSquares(atom n)
--- end function
+public function differenceOfSquares(integer n)
+  return squareOfSum(n) - sumOfSquares(n)
+end function
