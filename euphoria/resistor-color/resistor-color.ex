2e456ae72790cdda5d87849c92edb25572345014
MenkeTechnologies <linux.dev25@gmail.com>
2024-11-23 12:04:28 -0500

    rc

--- euphoria/resistor-color/resistor-color.ex
+++ euphoria/resistor-color/resistor-color.ex
@@ -1,7 +1,9 @@
-public function colorCode( sequence color )
+constant lst = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
 
+public function colorCode(sequence color)
+    return find(color, lst) - 1
 end function
-  
-public function colors()
 
+public function colors()
+    return lst
 end function
