4a81fa01efaa6cfecd7b6697e2c9dd0e2ac080ea
MenkeTechnologies <linux.dev25@gmail.com>
2024-11-22 21:48:32 -0500

    nc

--- euphoria/nucleotide-count/nucleotide-count.ex
+++ euphoria/nucleotide-count/nucleotide-count.ex
@@ -1,3 +1,13 @@
-public function counts(sequence nucleotides)
+include std/map.e
 
+constant dict = {{'A',0},{'C',0},{'G',0},{'T',0}}
+
+public function counts(sequence nucleotides)
+  map:map res = map:new_from_kvpairs(dict)
+  for i=1 to length(nucleotides) do
+    integer c = nucleotides[i]
+    if not map:has(res, c) then return 0 end if
+    map:put(res, c, 1, map:ADD)
+  end for
+  return res
 end function
