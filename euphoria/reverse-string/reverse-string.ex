8125a30e0e9282d1d5acd4256fff1776a0971a10
MenkeTechnologies <linux.dev25@gmail.com>
2024-11-24 07:37:54 -0500

    rvs

--- euphoria/reverse-string/reverse-string.ex
+++ euphoria/reverse-string/reverse-string.ex
@@ -1,2 +1,9 @@
-public function reverse(sequence str)
+public function reverse(sequence s)
+    sequence res = ""
+
+    for i = length(s) to 1 by -1 do
+        res &= s[i]
+    end for
+
+    return res
 end function
