64875612cc15fa11d13fa87b5661ca463f7d28ae
MenkeTechnologies <linux.dev25@gmail.com>
2024-11-25 17:21:30 -0500

    dnd

--- prolog/dnd-character/dnd_character.pl
+++ prolog/dnd-character/dnd_character.pl
@@ -1,3 +1,19 @@
-modifier(Score, Modifier).
-ability(Score).
-create_character(Character).
+package DndCharacter;
+use v5.40;
+use Moo;
+use POSIX qw/floor/;
+use List::Util qw/sum/;
+has strength => (is => 'rwp', default => __PACKAGE__->ability);
+has dexterity => (is => 'rwp', default => __PACKAGE__->ability);
+has constitution => (is => 'rwp', default => __PACKAGE__->ability);
+has intelligence => (is => 'rwp', default => __PACKAGE__->ability);
+has wisdom => (is => 'rwp', default => __PACKAGE__->ability);
+has charisma => (is => 'rwp', default => __PACKAGE__->ability);
+has hitpoints => (is => 'rwp', builder => sub ($self) {10 + __PACKAGE__->modifier($self->constitution)});
+sub ability ($class) {
+    sum((sort map {1 + int rand 6} 1..4)[1..4])
+}
+sub modifier ($class, $score) {
+    floor(($score - 10) / 2);
+}
+1
