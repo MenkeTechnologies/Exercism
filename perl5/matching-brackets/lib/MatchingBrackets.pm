6b2e6aa13cde0c72442dbf6ac20b0a4034d4b2a6
MenkeTechnologies <linux.dev25@gmail.com>
2024-11-26 12:12:01 -0500

    mb

--- perl5/matching-brackets/lib/MatchingBrackets.pm
+++ perl5/matching-brackets/lib/MatchingBrackets.pm
@@ -1,12 +1,26 @@
 package MatchingBrackets;
 
 use v5.40;
-
 use Exporter qw<import>;
 our @EXPORT_OK = qw<has_matching_brackets>;
 
-sub has_matching_brackets ($text) {
-    return undef;
+use constant DICT => {
+    '[' => ']',
+    '(' => ')',
+    '{' => '}'
+};
+
+sub has_matching_brackets ($s) {
+    my @stk;
+     do {
+        if (/[[({]/) {
+            push @stk, $_;
+        } elsif (/[])}]/) {
+            return 0 if !@stk or $_ ne DICT->{pop @stk};
+        }
+    } for split //, $s;
+
+    !@stk;
 }
 
 1;
