e22479ed51d0e58c5fd91be64747a854dc321490
MenkeTechnologies <linux.dev25@gmail.com>
2024-11-30 08:51:23 -0500

    pan

--- euphoria/pangram/pangram.ex
+++ euphoria/pangram/pangram.ex
@@ -1,1 +1,9 @@
--- public function is_pangram(sequence sentence)
+include wildcard.e
+
+public function is_pangram(sequence s)
+    sequence lc = lower(s)
+    for c='a' to 'z' do
+        if not find(c, lc) then return 0 end if
+    end for
+    return 1
+end function
