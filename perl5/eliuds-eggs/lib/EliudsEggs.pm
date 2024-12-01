844a8ba3af4db98e48d194297b51b5eb5fb9d32d
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-01 13:25:41 -0500

    ee

--- perl5/eliuds-eggs/lib/EliudsEggs.pm
+++ perl5/eliuds-eggs/lib/EliudsEggs.pm
@@ -1,12 +1,13 @@
 package EliudsEggs;
-
 use v5.40;
 
 use Exporter qw<import>;
 our @EXPORT_OK = qw<egg_count>;
 
-sub egg_count ($number) {
-    return undef;
+use List::Util qw(sum);
+
+sub egg_count ($n) {
+    sum(split(//, sprintf("%b", $n)));
 }
 
 1;
