44408bf70a8d57e0d16769c2cf2e14caec497ad9
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-01 10:45:18 -0500

    coll

--- perl5/collatz-conjecture/lib/CollatzConjecture.pm
+++ perl5/collatz-conjecture/lib/CollatzConjecture.pm
@@ -1,12 +1,14 @@
 package CollatzConjecture;
-
 use v5.40;
-
 use Exporter qw<import>;
 our @EXPORT_OK = qw<steps>;
-
-sub steps ($number) {
-    return undef;
+sub steps ($n) {
+    die 'Only positive integers are allowed' if $n < 1;
+    my $cnt = 0;
+    while ($n > 1) {
+        $n = $n % 2 == 0 ? $n / 2 : 3 * $n + 1;
+        ++$cnt;
+    }
+    $cnt;
 }
 
-1;
