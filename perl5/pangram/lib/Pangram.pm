9b56cd3d10aba6ee07b00c22c6118c642ac30b33
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-03 11:54:22 -0500

    pan

--- perl5/pangram/lib/Pangram.pm
+++ perl5/pangram/lib/Pangram.pm
@@ -1,12 +1,12 @@
 package Pangram;
-
 use v5.40;
+
+use List::Util qw{all};
 
 use Exporter qw<import>;
 our @EXPORT_OK = qw<is_pangram>;
 
-sub is_pangram ($text) {
-    return undef;
+sub is_pangram ($s) {
+    all { $s =~ /$_/i } 'a' .. 'z';
 }
 
-1;
