3f29fda75e95074498880b68c2c132d49d4944fc
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-04 10:48:08 -0500

    rc

--- perl5/rotational-cipher/lib/RotationalCipher.pm
+++ perl5/rotational-cipher/lib/RotationalCipher.pm
@@ -1,12 +1,15 @@
 package RotationalCipher;
 
 use v5.40;
-
 use Exporter qw<import>;
 our @EXPORT_OK = qw<caesar_cipher>;
 
-sub caesar_cipher ( $text, $shift_key ) {
-    return undef;
+sub caesar_cipher ( $s, $shift_key ) {
+    join '', map {
+            /[A-Z]/ ? chr((ord() - ord('A') + $shift_key) % 26 + ord('A')) :
+            /[a-z]/ ? chr((ord() - ord('a') + $shift_key) % 26 + ord('a')) :
+            $_
+        } split //, $s;
 }
 
 1;
