d6b8e4017c9222103e185ec06ef887f2ae8f473e
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-05 11:58:29 -0500

    leap

--- batch/leap/Leap.bat
+++ batch/leap/Leap.bat
@@ -2,9 +2,9 @@
 setlocal enabledelayedexpansion
 
 set "year=%~1"
-set "result="
+set "result=0"
 
-REM Your code goes here
-
+set /a "_div4=year %% 4,_div100 = year %% 100,_div400 = year %% 400"
+if %_div4% equ 0 ( if %_div100% neq 0 ( set result=1 ) else if %_div400% equ 0 ( set result=1) )
 
 echo %result%
