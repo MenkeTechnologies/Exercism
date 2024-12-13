e3cddcb949840e0622442b6f00dd51df7d35519f
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-13 10:33:36 -0500

    rd

--- euphoria/raindrops/raindrops.ex
+++ euphoria/raindrops/raindrops.ex
@@ -1,3 +1,8 @@
-public function raindrops( atom n )
+include std/math.e
+include std/utils.e
+include std/text.e
 
+public function raindrops(atom n)
+  sequence res = iif(mod(n, 3) = 0, "Pling", "") & iif(mod(n, 5) = 0, "Plang", "") & iif(mod(n, 7) = 0, "Plong", "")
+  return iif(equal(res, ""), sprint(n), res)
 end function
