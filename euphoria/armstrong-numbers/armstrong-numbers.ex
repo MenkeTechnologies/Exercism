b5b4b853cdff92faea2d3886f6c5cab6049ee49f
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-13 10:30:40 -0500

    arm

--- euphoria/armstrong-numbers/armstrong-numbers.ex
+++ euphoria/armstrong-numbers/armstrong-numbers.ex
@@ -1,3 +1,7 @@
-public function is_armstrong_number(integer number)
+include std/convert.e
+include std/math.e
 
+public function is_armstrong_number(integer number)
+  sequence digits = to_string(number) - '0'
+  return number = sum(power(digits, length(digits)))
 end function
