e0e1ba975df404e75332e1b26262005689df6a8b
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-14 11:14:38 -0500

    rcd

--- euphoria/resistor-color-duo/resistor-color-duo.ex
+++ euphoria/resistor-color-duo/resistor-color-duo.ex
@@ -1,3 +1,5 @@
-public function value( sequence colors )
+sequence RESISTOR_COLORS = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
 
+public function value(sequence colors)
+    return (find(colors[1], RESISTOR_COLORS) - 1) * 10 + find(colors[2], RESISTOR_COLORS) - 1
 end function
