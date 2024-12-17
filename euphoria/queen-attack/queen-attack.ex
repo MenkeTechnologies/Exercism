b5a62b65ef3358fcb3a7fc5b93d1747b4ba644c0
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-17 00:22:36 -0500

    qa

--- euphoria/queen-attack/queen-attack.ex
+++ euphoria/queen-attack/queen-attack.ex
@@ -1,11 +1,10 @@
-public type queen(sequence position)
+include std/math.e
 
+public type queen(sequence posn)
+    return length(posn) = 2 and posn[1] >= 0 and posn[1] <= 7 and posn[2] >= 0 and posn[2] <= 7
 end type
 
 public function can_attack(queen white_queen, queen black_queen)
-
-end function
-
-function is_on_diagonal(queen white_queen, queen black_queen)
-
+    integer dx = abs(white_queen[1] - black_queen[1]), dy = abs(white_queen[2] - black_queen[2])
+    return dx = 0 or dy = 0 or dx = dy
 end function
