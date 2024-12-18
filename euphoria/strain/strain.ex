f769b55dbce1997174b6d747c06f165bfc794419
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-18 10:58:03 -0500

    strain

--- euphoria/strain/strain.ex
+++ euphoria/strain/strain.ex
@@ -1,7 +1,19 @@
-public function keep(sequence seq, integer f)
-
+public function keep(sequence seq, atom f)
+    sequence res = {}
+    for i = 1 to length(seq) do
+        if call_func(f, {seq[i]}) then
+            res &= {seq[i]}
+        end if
+    end for
+    return res
 end function
 
-public function discard(sequence seq, integer f)
-
+public function discard(sequence seq, atom f)
+    sequence res = {}
+    for i = 1 to length(seq) do
+        if not call_func(f, {seq[i]}) then
+            res &= {seq[i]}
+        end if
+    end for
+    return res
 end function
