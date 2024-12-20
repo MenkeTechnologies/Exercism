d119b1a429b5a1320250900090f618ddd213b06d
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-20 18:11:12 -0500

    darts

--- swift/darts/Sources/Darts/Darts.swift
+++ swift/darts/Sources/Darts/Darts.swift
@@ -1,3 +1,6 @@
+import Foundation
+
 func dartScore(x: Double, y: Double) -> Int {
-  // Write your code for the 'Darts' exercise here.
+  let distance = pow(x * x + y * y, 0.5)
+  return distance > 10 ? 0 : distance > 5 ? 1 : distance > 1 ? 5 : 10
 }
