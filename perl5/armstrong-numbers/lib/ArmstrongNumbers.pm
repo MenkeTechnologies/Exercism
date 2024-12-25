ff2ea9cadfec1b5c5f25380833b140789a564e69
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-25 02:02:54 -0500

    arm

--- perl5/armstrong-numbers/lib/ArmstrongNumbers.pm
+++ perl5/armstrong-numbers/lib/ArmstrongNumbers.pm
@@ -1,12 +1,12 @@
 package ArmstrongNumbers;
 use v5.40;
 use bigint;
+use List::Util qw<sum0>;
+
 use Exporter qw<import>;
 our @EXPORT_OK = qw<is_armstrong_number>;
 
 sub is_armstrong_number ($number) {
     my @digits = map {0 + $_} split //, $number;
-    my $sum = 0;
-    $sum += $_ ** scalar @digits for @digits;
-    $sum == $number;
+    $number == sum0 map { $_ ** scalar @digits } @digits;
 }
