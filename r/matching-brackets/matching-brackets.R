d931212a9bd975d222375aa55ab6694959a9b7b4
MenkeTechnologies <linux.dev25@gmail.com>
2024-12-31 12:19:30 -0500

    mb

--- r/matching-brackets/matching-brackets.R
+++ r/matching-brackets/matching-brackets.R
@@ -1,3 +1,17 @@
-is_paired <- function(input) {
+dict = list("{" = "}", "[" = "]", "(" = ")")
 
+is_paired = function(s) {
+  stk = list()
+  for (c in s |> strsplit("") |> unlist()) {
+    if (hasName(dict, c)) {
+      stk = append(stk, dict[c])
+    } else if (c %in% dict) {
+      if (!length(stk)|| c != tail(stk, n = 1)) {
+        return(FALSE)
+      }
+      stk = stk[-length(stk)]
+    }
+  }
+  !length(stk)
 }
+
