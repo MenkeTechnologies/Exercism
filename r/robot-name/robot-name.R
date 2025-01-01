3c699a5591f2666d66a4e2555db77ec1dafa74ad
MenkeTechnologies <linux.dev25@gmail.com>
2025-01-01 12:54:30 -0500

    rn

--- r/robot-name/robot-name.R
+++ r/robot-name/robot-name.R
@@ -1,7 +1,9 @@
-new_robot <- function() {
-
+new_robot = function() {
+  robot = structure(list(name=''), class = "robot" )
+  robot$name = paste0(paste0(sample(LETTERS, 2, replace=F), collapse=''), paste0(sample(0:9, 3, replace=F), collapse=''))
+  robot
 }
 
-reset_robot <- function(robot) {
-
+reset_robot = function(robot) {
+  new_robot()
 }
