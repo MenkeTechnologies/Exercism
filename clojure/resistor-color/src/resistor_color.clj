6697dc1de06902bbc6fecb3bc9dfd485a44f7888
MenkeTechnologies <linux.dev25@gmail.com>
2025-01-15 14:12:22 -0500

    rc

--- clojure/resistor-color/src/resistor_color.clj
+++ clojure/resistor-color/src/resistor_color.clj
@@ -1,11 +1,16 @@
 (ns resistor-color)
 
-(def colors
-  ;; definition value
-  )
+(def dict {"black" 0
+             "brown" 1
+             "red" 2
+             "orange" 3
+             "yellow" 4
+             "green" 5
+             "blue" 6
+             "violet" 7
+             "grey" 8
+             "white" 9})
 
-(defn color-code
-  "Returns the numerical value associated with the given color"
-  [color]
-  ;; function body
-  )
+(def colors (keys (into (sorted-map-by (fn [k1 k2] (compare (dict k1) (dict k2)))) dict)))
+
+(defn color-code [color] (dict color))
