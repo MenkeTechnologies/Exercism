99fa0ddd868322b67e4862d26030400be2a8af65
MenkeTechnologies <linux.dev25@gmail.com>
2025-01-24 11:52:01 -0500

    rcd

--- clojure/resistor-color-duo/src/resistor_color_duo.clj
+++ clojure/resistor-color-duo/src/resistor_color_duo.clj
@@ -1,7 +1,15 @@
 (ns resistor-color-duo)
 
-(defn resistor-value
-  "Returns the resistor value based on the given colors"
-  [colors]
-  ;; function body
-  )
+(def dict
+  { "black" 0
+    "brown" 1
+    "red" 2
+    "orange" 3
+    "yellow" 4
+    "green" 5
+    "blue" 6
+    "violet" 7
+    "grey" 8
+    "white" 9 })
+
+(defn resistor-value [colors] (->> colors (take 2) (map dict) (apply str) parse-long))
