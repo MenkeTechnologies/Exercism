e2648ff90e5b92095384d4c3371f5055b4f067d8
MenkeTechnologies <linux.dev25@gmail.com>
2025-01-25 11:28:14 -0500

    cs

--- csharp/custom-set/CustomSet.cs
+++ csharp/custom-set/CustomSet.cs
@@ -1,49 +1,20 @@
-﻿using System;
+﻿using System.Linq;
 
-public class CustomSet
+public class CustomSet(params int[] values)
 {
-    public CustomSet(params int[] values)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    private readonly int[] _values = values.Distinct().ToArray();
 
-    public CustomSet Add(int value)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public CustomSet Add(int value) =>
+        !Contains(value) ? new(_values.Append(value).ToArray()) : new (_values);
 
-    public bool Empty()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public bool Contains(int value)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public bool Subset(CustomSet right)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public bool Disjoint(CustomSet right)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public CustomSet Intersection(CustomSet right)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public CustomSet Difference(CustomSet right)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public CustomSet Union(CustomSet right)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public bool Empty() => _values.Length == 0;
+    public bool Contains(int value) => _values.Contains(value);
+    public bool Subset(CustomSet right) => _values.All(right.Contains);
+    public bool Disjoint(CustomSet right) => _values.All(x => !right.Contains(x));
+    public CustomSet Intersection(CustomSet right) => new(_values.Where(right.Contains).ToArray());
+    public CustomSet Difference(CustomSet right) => new(_values.Where(x => !right.Contains(x)).ToArray());
+    public CustomSet Union(CustomSet right) => new(_values.Union(right._values).ToArray());
+    public override bool Equals(object obj) => obj is CustomSet set && Equals(set);
+    public bool Equals(CustomSet right) => _values.All(right.Contains) && right._values.All(Contains);
+    public override int GetHashCode() => _values.GetHashCode();
 }
