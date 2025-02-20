bf99c271084c627f9e63e15198ebe4092ebb6b33
MenkeTechnologies <linux.dev25@gmail.com>
2025-02-20 12:36:33 -0500

    more exe

---
+++ csharp/state-of-tic-tac-toe/StateOfTicTacToe.cs
@@ -0,0 +1,25 @@
+using System;
+
+public enum State
+{
+    Win,
+    Draw,
+    Ongoing,
+    Invalid
+}
+
+public class TicTacToe
+{
+    public TicTacToe(string[] rows)
+    {
+        throw new NotImplementedException("Please implement this constructor");
+    }
+    
+    public State State
+    {
+        get
+        {
+            throw new NotImplementedException("Please implement this property");
+        }
+    }
+}
---
+++ racket/pythagorean-triplets/README.md
@@ -0,0 +1,12 @@
+# pythagorean-triplets
+
+Welcome to pythagorean-triplets on Exercism's Racket Track.
+If you need help running the tests or submitting your code, check out `HELP.md`.
+
+## Instructions
+
+## Source
+
+### Created by
+
+- @blakelewis
