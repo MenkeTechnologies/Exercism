f48e766e4feabe6c30054b409de490a3983dbcbd
MenkeTechnologies <linux.dev25@gmail.com>
2025-02-22 11:29:49 -0500

    fa

--- vbnet/flatten-array/FlattenArray.vb
+++ vbnet/flatten-array/FlattenArray.vb
@@ -1,8 +1,15 @@
-Imports System
 Imports System.Collections
 
 Public Module FlattenArray
-    Public Function Flatten(ByVal input As IEnumerable) As IEnumerable
-        Throw New NotImplementedException("You need to implement this function.")
+    Public Iterator Function Flatten(lst As IEnumerable) As IEnumerable
+        For Each element In lst
+            If TypeOf element Is IEnumerable Then
+                For Each felement In Flatten(element)
+                    Yield felement
+                Next
+            ElseIf element IsNot Nothing Then
+                Yield element
+            End If
+        Next
     End Function
 End Module
