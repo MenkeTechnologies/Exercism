61f088b9c55818d77420a89fd8f9ba887ac5893f
MenkeTechnologies <linux.dev25@gmail.com>
2025-02-22 00:30:17 -0500

    rc

--- vbnet/rotational-cipher/RotationalCipher.vb
+++ vbnet/rotational-cipher/RotationalCipher.vb
@@ -1,7 +1,15 @@
-Imports System
+Imports System.Linq
 
 Public Module RotationalCipher
-    Public Function Rotate(ByVal text As String, ByVal shiftKey As Integer) As String
-        Throw New NotImplementedException("You need to implement this function.")
+    Private Function RotateChar(c As Char, shiftKey As Integer) As Char
+        If Not Char.IsLetter(c) Then
+            Return c
+        End If
+        Dim offset  = If(Char.IsLower(c), AscW("a"c), AscW("A"c))
+        Return ChrW(offset + (AscW(c) - offset + shiftKey) Mod 26)
+    End Function
+
+    Public Function Rotate(s As String, shiftKey As Integer) As String
+        Return New String(s.Select(Function(c) RotateChar(c, shiftKey)).ToArray())
     End Function
 End Module
