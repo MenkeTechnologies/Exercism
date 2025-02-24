39f8c178c0970a82076f13f40e90a1f2798c7538
MenkeTechnologies <linux.dev25@gmail.com>
2025-02-24 12:43:26 -0500

    ee

--- ocaml/eliuds-eggs/eliuds_eggs.ml
+++ ocaml/eliuds-eggs/eliuds_eggs.ml
@@ -1,2 +1,8 @@
-let egg_count number =
-  failwith "Please implement 'maximum_value'"
+open Int
+
+let egg_count n =
+  let rec aux dup cnt =
+   if dup > 0
+   then aux (shift_right dup 1) (cnt + logand dup 1)
+   else cnt
+  in aux n 0
