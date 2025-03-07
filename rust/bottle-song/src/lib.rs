6a3b02485038528ca66a11c252722e8a434857da
MenkeTechnologies <linux.dev25@gmail.com>
2025-03-07 12:57:33 -0500

    bs

--- rust/bottle-song/src/lib.rs
+++ rust/bottle-song/src/lib.rs
@@ -1,3 +1,26 @@
-pub fn recite(start_bottles: u32, take_down: u32) -> String {
-    todo!("Return the bottle song starting at {start_bottles} and taking down {take_down} bottles")
+static NUMBERS_TO_WORDS: [&str; 11] = [
+    "No", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+];
+fn green_bottles(i: usize) -> String {
+    format!(
+        "{} green bottle{}",
+        NUMBERS_TO_WORDS[i],
+        if i == 1 { "" } else { "s" }
+    )
+}
+pub fn recite(start_bottles: usize, take_down: usize) -> String {
+    ((start_bottles + 1 - take_down)..=start_bottles )
+        .rev()
+        .fold(String::new(), |acc, i| {
+            acc + &format!(
+                "
+{orig} hanging on the wall,
+{orig} hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be {rem} hanging on the wall.
+",
+                rem = green_bottles(i - 1).to_lowercase(),
+                orig = green_bottles(i)
+            )
+        })
 }
--- rust/bottle-song/tests/bottle-song.rs
+++ rust/bottle-song/tests/bottle-song.rs
@@ -14,7 +14,7 @@ fn first_generic_verse() {
 }
 
 #[test]
-#[ignore]
+
 fn last_generic_verse() {
     assert_eq!(
         recite(3, 1).trim(),
@@ -28,7 +28,7 @@ fn last_generic_verse() {
 }
 
 #[test]
-#[ignore]
+
 fn verse_with_2_bottles() {
     assert_eq!(
         recite(2, 1).trim(),
@@ -42,7 +42,7 @@ fn verse_with_2_bottles() {
 }
 
 #[test]
-#[ignore]
+
 fn verse_with_1_bottle() {
     assert_eq!(
         recite(1, 1).trim(),
@@ -56,7 +56,7 @@ fn verse_with_1_bottle() {
 }
 
 #[test]
-#[ignore]
+
 fn first_two_verses() {
     assert_eq!(
         recite(10, 2).trim(),
@@ -75,7 +75,7 @@ fn first_two_verses() {
 }
 
 #[test]
-#[ignore]
+
 fn last_three_verses() {
     assert_eq!(
         recite(3, 3).trim(),
@@ -99,7 +99,7 @@ fn last_three_verses() {
 }
 
 #[test]
-#[ignore]
+
 fn all_verses() {
     assert_eq!(
         recite(10, 10).trim(),
