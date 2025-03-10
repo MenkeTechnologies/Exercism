d7d8585120a2dfb06fed8d69ce0f60142ec5f633
MenkeTechnologies <linux.dev25@gmail.com>
2025-03-10 18:29:25 -0400

    nth prime

--- r/nth-prime/nth-prime.R
+++ r/nth-prime/nth-prime.R
@@ -1,3 +1,10 @@
-prime <- function(n) {
-
+prime = function(n) {
+  stopifnot(n > 0)
+  primes = c(2)
+  current = 3
+  while (length(primes) < n) {
+    if (all(current %% primes != 0)) primes = c(primes, current)
+    current = current + 2
+  }
+  primes[n]
 }
