85670fd4199f504ad78701643c18786af3c5734c
MenkeTechnologies <linux.dev25@gmail.com>
2025-04-09 10:27:44 -0400

    plf

--- vbnet/parallel-letter-frequency/ParallelLetterFrequency.vb
+++ vbnet/parallel-letter-frequency/ParallelLetterFrequency.vb
@@ -1,8 +1,6 @@
-Imports System
 Imports System.Collections.Generic
-
 Public Module ParallelLetterFrequency
     Public Function Calculate(ByVal texts As IEnumerable(Of String)) As Dictionary(Of Char, Integer)
-        Throw New NotImplementedException()
+        Return texts.AsParallel().SelectMany(Function(s) s.ToLower().Select(Function(c) c)).Where(Function(c) Char.IsLetter(c)).GroupBy(Function(c) c).ToDictionary(Function(c) c.Key, Function(c) c.Count())
     End Function
 End Module
