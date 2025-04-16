f608a10eadb664545ce11ede17aefa8995156f7c
MenkeTechnologies <linux.dev25@gmail.com>
2025-04-16 09:43:30 -0400

    lasagna

--- julia/lasagna/lasagna.jl
+++ julia/lasagna/lasagna.jl
@@ -1,8 +1,7 @@
-# Define the `expected_bake_time` constant`
+const expected_bake_time = 60
 
-# Define the `preparation_time(layers)` function.
+const preparation_time(layers) = 2 * layers
 
-# Define the `remaining_time(time_in_oven)` function.
+const remaining_time(time_in_oven) = expected_bake_time - time_in_oven
 
-# Define the `total_working_time(layers, time_in_oven)` function.
-
+const total_working_time(layers, time_in_oven) = preparation_time(layers) + time_in_oven
