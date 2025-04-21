84e5cdd05f9916599aeb237280f5f0ab33ccc618
MenkeTechnologies <linux.dev25@gmail.com>
2025-04-21 11:43:47 -0400

    rcd

--- common-lisp/resistor-color-duo/resistor-color-duo.lisp
+++ common-lisp/resistor-color-duo/resistor-color-duo.lisp
@@ -1,7 +1,11 @@
 (defpackage :resistor-color-duo
   (:use :cl)
   (:export :value))
-
 (in-package :resistor-color-duo)
 
-(defun value (colors))
+(defun value (lst)
+  (let ((first (position (nth 0 lst) (colors) :test #'string=))
+        (second (position (nth 1 lst) (colors) :test #'string=)))
+       (+ (* first 10) second)))
+
+(defun colors() '("black" "brown" "red" "orange" "yellow" "green" "blue" "violet" "grey" "white"))
