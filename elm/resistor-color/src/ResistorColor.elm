012a3e26b43c148d95ee19a5270c616701c6b5d7
MenkeTechnologies <linux.dev25@gmail.com>
2025-04-30 11:24:10 -0400

    rc

--- elm/resistor-color/src/ResistorColor.elm
+++ elm/resistor-color/src/ResistorColor.elm
@@ -1,11 +1,15 @@
 module ResistorColor exposing (colorCode, colors)
 
+colorCode color = findIndex color colors 0
 
-colorCode : String -> Int
-colorCode color =
-    Debug.todo "Please implement colorCode"
+colors = [ "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" ]
 
-
-colors : List String
-colors =
-    Debug.todo "Please implement colors"
+findIndex target lst cnt =
+    case lst of
+        [] ->
+            -1
+        x :: xs ->
+            if target == x then
+                cnt
+            else
+                findIndex target xs (cnt + 1)
