d02c28e067e44316e99c22e96adadeafb86ed472
MenkeTechnologies <linux.dev25@gmail.com>
2025-06-13 11:44:31 -0400

    td

--- javascript/train-driver/train-driver.js
+++ javascript/train-driver/train-driver.js
@@ -1,8 +1,3 @@
-// @ts-check
-//
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
 
 /**
  * Return each wagon's id in form of an array.
@@ -10,31 +5,22 @@
  * @param {number[]} ids
  * @returns {number[]} wagon ids
  */
-export function getListOfWagons(a, b, c, d, e, f, g, h, i, j, k, l, m, n) {
-  throw new Error('Please implement the getListOfWagons function');
-}
-
+export const getListOfWagons = (...ids) => ids;
 /**
  * Reorder the array of wagons by moving the first 2 wagons to the end of the array.
  *
- * @param {Iterable<number>} ids
+ * @param {number[]} ids
  * @returns {number[]} reorderd list of wagons
  */
-export function fixListOfWagons(ids) {
-  throw new Error('Please implement the fixListOfWagons function');
-}
-
+export const fixListOfWagons = ([id1, id2, ...rest]) => [...rest, id1, id2];
 /**
  * Fixes the array of wagons by inserting an array of wagons after the first element in eachWagonsID.
  *
- * @param {Iterable<number>} ids
- * @param {Iterable<number>} missingWagons
+ * @param {number[]} ids
+ * @param {number[]} missingWagons
  * @returns {number[]} corrected list of wagons
  */
-export function correctListOfWagons(ids, missingWagons) {
-  throw new Error('Please implement the correctListOfWagons function');
-}
-
+export const correctListOfWagons = ([id1, ...rest], missingWagons) => [id1, ...missingWagons, ...rest];
 /**
  * Extend route information by adding another object
  *
@@ -42,16 +28,11 @@ export function correctListOfWagons(ids, missingWagons) {
  * @param {Record<string, string>} additional
  * @returns {Record<string, string>} extended route information
  */
-export function extendRouteInformation(information, additional) {
-  throw new Error('Please implement the extendRouteInformation function');
-}
-
+export const extendRouteInformation = (information, additional) => ({ ...information, ...additional })
 /**
  * Separate arrival time from the route information object
  *
  * @param {Record<string, string>} information
  * @returns {[string, Record<string, string>]} array with arrival time and object without arrival time
  */
-export function separateTimeOfArrival(information) {
-  throw new Error('Please implement the separateTimeOfArrival function');
-}
+export const separateTimeOfArrival = ({ timeOfArrival, ...information }) => [timeOfArrival, information];
