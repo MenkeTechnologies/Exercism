95e0f54b4f5b74455cf5be6f151c935643f409a7
MenkeTechnologies <linux.dev25@gmail.com>
2025-06-13 11:19:20 -0400

    more exe

---
+++ javascript/train-driver/train-driver.js
@@ -0,0 +1,57 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Return each wagon's id in form of an array.
+ *
+ * @param {number[]} ids
+ * @returns {number[]} wagon ids
+ */
+export function getListOfWagons(a, b, c, d, e, f, g, h, i, j, k, l, m, n) {
+  throw new Error('Please implement the getListOfWagons function');
+}
+
+/**
+ * Reorder the array of wagons by moving the first 2 wagons to the end of the array.
+ *
+ * @param {Iterable<number>} ids
+ * @returns {number[]} reorderd list of wagons
+ */
+export function fixListOfWagons(ids) {
+  throw new Error('Please implement the fixListOfWagons function');
+}
+
+/**
+ * Fixes the array of wagons by inserting an array of wagons after the first element in eachWagonsID.
+ *
+ * @param {Iterable<number>} ids
+ * @param {Iterable<number>} missingWagons
+ * @returns {number[]} corrected list of wagons
+ */
+export function correctListOfWagons(ids, missingWagons) {
+  throw new Error('Please implement the correctListOfWagons function');
+}
+
+/**
+ * Extend route information by adding another object
+ *
+ * @param {Record<string, string>} information
+ * @param {Record<string, string>} additional
+ * @returns {Record<string, string>} extended route information
+ */
+export function extendRouteInformation(information, additional) {
+  throw new Error('Please implement the extendRouteInformation function');
+}
+
+/**
+ * Separate arrival time from the route information object
+ *
+ * @param {Record<string, string>} information
+ * @returns {[string, Record<string, string>]} array with arrival time and object without arrival time
+ */
+export function separateTimeOfArrival(information) {
+  throw new Error('Please implement the separateTimeOfArrival function');
+}
