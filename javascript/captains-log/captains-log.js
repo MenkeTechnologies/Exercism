34a5af529aaeaa043d50280228abb764567b10de
MenkeTechnologies <linux.dev25@gmail.com>
2025-07-01 23:28:52 -0400

    cl

--- javascript/captains-log/captains-log.js
+++ javascript/captains-log/captains-log.js
@@ -1,34 +1,4 @@
-// @ts-check
-
-/**
- * Generates a random starship registry number.
- *
- * @returns {string} the generated registry number.
- */
-export function randomShipRegistryNumber() {
-  throw new Error(
-    'Please remove this line and implement the randomShipRegistryNumber() function',
-  );
-}
-
-/**
- * Generates a random stardate.
- *
- * @returns {number} a stardate between 41000 (inclusive) and 42000 (exclusive).
- */
-export function randomStardate() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
-}
-
-/**
- * Generates a random planet class.
- *
- * @returns {string} a one-letter planet class.
- */
-export function randomPlanetClass() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
-}
+const PLANET_CLASSES = "DHJKLMNRTY";
+export const randomShipRegistryNumber = () => `NCC-${Math.floor(1000 + 9000 * Math.random())}`;
+export const randomStardate = () => 41000 + 1000 * Math.random();
+export const randomPlanetClass = () => PLANET_CLASSES[Math.floor(Math.random() * PLANET_CLASSES.length)];
