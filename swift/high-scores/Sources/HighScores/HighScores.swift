322f08d734590bb7f3ae4bd2cf750313cbc9e075
MenkeTechnologies <linux.dev25@gmail.com>
2025-07-15 14:21:59 -0400

    hs

--- swift/high-scores/Sources/HighScores/HighScores.swift
+++ swift/high-scores/Sources/HighScores/HighScores.swift
@@ -1,7 +1,8 @@
 import Foundation
-
 struct HighScores {
-
-    // Write your code for the 'High Scores' exercise here.
-
+    let scores: [Int]
+    
+    var latest: Int { scores.last ?? 0 }
+    var personalBest: Int { scores.max() ?? 0 }
+    var topThree: [Int] { Array(scores.sorted(by: >).prefix(3)) }
 }
