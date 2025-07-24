3bf35ce773461801a5b29ca812842c557a37ed03
MenkeTechnologies <linux.dev25@gmail.com>
2025-07-23 23:32:11 -0400

    darts

--- ocaml/darts/darts.ml
+++ ocaml/darts/darts.ml
@@ -1,2 +1,5 @@
+open Float
+
 let score (x: float) (y: float): int =
-  failwith "Please implement 'score'"
+    let dist = pow ((pow x 2.) +. (pow y 2.)) 0.5 in
+    if dist > 10. then 0 else if dist > 5. then 1 else if dist > 1. then 5 else 10
