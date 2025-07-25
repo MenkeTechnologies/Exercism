50d29c28368aab26a5878438e4573e5f4530acd3
MenkeTechnologies <linux.dev25@gmail.com>
2025-07-25 15:04:41 -0400

    ca

--- julia/cars-assemble/cars-assemble.jl
+++ julia/cars-assemble/cars-assemble.jl
@@ -1,11 +1,5 @@
-function success_rate(speed)
-    
-end
+success_rate(speed) = speed == 0 ? 0 : speed <= 4 ? 1 : speed <= 8 ? .9 : speed ==9 ? .8 : .77
 
-function production_rate_per_hour(speed)
-    
-end
+production_rate_per_hour(speed) = 221 * speed * success_rate(speed)
 
-function working_items_per_minute(speed)
-    
-end
+working_items_per_minute(speed) = div(production_rate_per_hour(speed), 60) |> Int
