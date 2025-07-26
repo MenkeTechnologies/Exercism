b08e722f130e0561d87f11cb44625c8215f2ed00
MenkeTechnologies <linux.dev25@gmail.com>
2025-07-26 13:11:14 -0400

    rr

--- javascript/recycling-robot/assembly-line.js
+++ javascript/recycling-robot/assembly-line.js
@@ -1,121 +1,28 @@
-// @ts-check
-//
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
-
 import { ElectronicDevice } from './lib.js';
 
-/**
- * Checks if input is a boolean.
- *
- * @param {unknown} value
- * @returns {boolean} whether the input is a boolean
- */
-export function isBoolean(value) {
-  throw new Error('Remove this line and implement the isBoolean function');
-}
+export const isBoolean = value => typeof value === 'boolean';
 
-/**
- * Checks if input is a finite number or bigint.
- *
- * @param {unknown} value
- * @returns {boolean} whether the input is a finite number or bigint
- */
-export function isNumber(value) {
-  throw new Error('Remove this line and implement the isNumber function');
-}
+export const isNumber = value => typeof value === 'bigint' || (typeof value === 'number' && isFinite(value));
 
-/**
- * Checks if a value is an object.
- *
- * @param {unknown} value
- * @returns {boolean} whether the input is an object.
- */
-export function isObject(value) {
-  throw new Error('Remove this line and implement the isObject function');
-}
+export const isObject = value => Boolean(value && typeof value === 'object');
 
-/**
- * Checks if a value is a numeric string.
- *
- * @param {unknown} value
- * @returns {boolean} whether the input is a numeric string.
- */
-export function isNumericString(value) {
-  throw new Error(
-    'Remove this line and implement the isNumericString function',
-  );
-}
+export const isNumericString = value => typeof value === 'string' && /^-?[\d]+$/.test(value);
 
-/**
- * Checks if an object is an instance of the `ElectronicDevice` class or one of its children.
- *
- * @param {object} object
- * @returns {boolean} whether the object is an instance of the `ElectronicDevice` class or one of its children.
- */
-export function isElectronic(object) {
-  throw new Error('Remove this line and implement the isElectronic function');
-}
+export const isElectronic = object => object instanceof ElectronicDevice;
 
-/**
- * Checks if a value is a non empty array.
- *
- * @param {unknown} value
- * @returns {boolean} whether the input is a non empty array.
- */
-export function isNonEmptyArray(value) {
-  throw new Error(
-    'Remove this line and implement the isNonEmptyArray function',
-  );
-}
+export const isNonEmptyArray = value => Array.isArray(value) && value.length > 0;
 
-/**
- * Checks if a value is an empty array.
- *
- * @param {unknown} value
- * @returns {boolean} whether the input is an empty array.
- */
-export function isEmptyArray(value) {
-  throw new Error('Remove this line and implement the isEmptyArray function');
-}
+export const isEmptyArray = value => Array.isArray(value) && value.length === 0;
 
-/**
- * Checks if a value has a "type" property or method.
- *
- * @param {object} object
- * @returns {boolean} whether the input has a "type" property or method.
- */
-export function hasType(object) {
-  throw new Error('Remove this line and implement the hasType function');
-}
+export const assertHasId = object => {
+  if ('id' in object) {
+    return
+  }
+  throw new Error()
+};
 
-/**
- * Throws an error if an object is missing an "id" property or method.
- *
- * @param {object} object
- * @returns {never|void} undefined if the input has an "id" property or method, otherwise throws an error.
- */
-export function assertHasId(object) {
-  throw new Error('Remove this line and implement the assertHasId function');
-}
+export const hasType = object => 'type' in object;
 
-/**
- * Checks if a value has an "id" property.
- *
- * @param {object} object
- * @returns {boolean} whether the input has an "id" property.
- */
-export function hasIdProperty(object) {
-  throw new Error('Remove this line and implement the hasIdProperty function');
-}
+export const hasIdProperty = object => Object.hasOwn(object, 'id');
 
-/**
- * Checks if a value has a defined "type" property.
- *
- * @param {object} object
- * @returns {boolean} whether the input has a defined "type" property.
- */
-export function hasDefinedType(object) {
-  throw new Error('Remove this line and implement the hasDefinedType function');
-}
+export const hasDefinedType = object => Object.hasOwn(object, 'type') && object.type !== undefined;
