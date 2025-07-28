fde070cad01b37ad5c8208755de01d8e2f10f356
MenkeTechnologies <linux.dev25@gmail.com>
2025-07-28 11:46:13 -0400

    coll

--- ocaml/collatz-conjecture/collatz_conjecture.ml
+++ ocaml/collatz-conjecture/collatz_conjecture.ml
@@ -1,2 +1,14 @@
-let collatz_conjecture _ = 
-  failwith "'collatz_conjecture' is missing"
+open Base
+
+let collatz_conjecture n =
+  let rec aux cnt num =
+    if num < 1 then
+      Error "Only positive integers are allowed"
+    else if num = 1 then
+      Ok cnt
+    else if (num % 2) = 0 then
+      aux (cnt + 1) (num / 2)
+    else
+      aux (cnt + 1) (3 * num + 1)
+  in aux 0 n
+
