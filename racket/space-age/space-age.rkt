e1b62b197478c4dd30ba2b9c73991496d449310c
MenkeTechnologies <linux.dev25@gmail.com>
2025-08-03 15:55:23 -0400

    sa

--- racket/space-age/space-age.rkt
+++ racket/space-age/space-age.rkt
@@ -1,10 +1,21 @@
 #lang racket
 
-(provide on-mercury
+(provide on-earth
+         on-mercury
          on-venus
-         on-earth
          on-mars
          on-jupiter
          on-saturn
          on-uranus
          on-neptune)
+
+(define earth-year 31557600)
+
+(define (on-earth seconds) (/ seconds earth-year))
+(define (on-mercury seconds) (/ (on-earth seconds) 0.2408467))
+(define (on-venus seconds) (/ (on-earth seconds) 0.61519726))
+(define (on-mars seconds) (/ (on-earth seconds) 1.8808158))
+(define (on-jupiter seconds) (/ (on-earth seconds) 11.862615))
+(define (on-saturn seconds) (/ (on-earth seconds) 29.447498))
+(define (on-uranus seconds) (/ (on-earth seconds) 84.016846))
+(define (on-neptune seconds) (/ (on-earth seconds) 164.79132))
