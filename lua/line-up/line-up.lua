677d045c090aeec26521355a83576cf7d731abd2
MenkeTechnologies <linux.dev25@gmail.com>
2025-10-17 11:24:19 -0400

    line up

--- lua/line-up/line-up.lua
+++ lua/line-up/line-up.lua
@@ -1,4 +1,19 @@
 return {
-  format = function(name, number)
+  format = function(name, n)
+    local placement = tostring(n)
+    local placed = ""
+    if placement:sub(-2) == "11" or 
+       placement:sub(-2) == "12" or 
+       placement:sub(-2) == "13" then
+      placed = n .. "th"
+    else 
+      placed = n .. (placement:sub(-1) == "1" and "st" or placement:sub(-1) == "2" and "nd" or placement:sub(-1) == "3" and "rd" or "th")
+    end
+	
+	return string.format(
+      "%s, you are the %s customer we serve today. Thank you!", 
+      name, 
+      placed
+    )
   end
 }
