05f9ac160e2562218a59f3a6b5e2623e0ebc0562
MenkeTechnologies <linux.dev25@gmail.com>
2025-10-18 02:12:22 -0400

    lu

--- powershell/line-up/LineUp.ps1
+++ powershell/line-up/LineUp.ps1
@@ -1,25 +1,15 @@
 Function Get-LineUp() {
-    <#
-    .SYNOPSIS
-    Produce a greeting for customer in a line.
-    
-    .DESCRIPTION
-    Given a name and a number, your task is to produce a sentence using that name and that number as an ordinal numeral.
-
-    .PARAMETER Name
-    String represents name of the customer.
-
-    .PARAMETER Number
-    Integer represents the order of the customer in line. (1 to 999)
-
-    .EXAMPLE
-    Get-LineUp -Name "Exercism" -Number 1
-    Returns: "Exercism, you are the 1st customer we serve today. Thank you!"
-    #>
     [CmdletBinding()]
     Param(
         [string]$Name,
         [int]$Number
     )
-    Throw "Please implement this function"
+    switch([string]$number){
+        {$_ -match "1[123]$"} { $post = "th"; break }
+        {$_ -match "1$"} { $post = "st" }
+        {$_ -match "2$"} { $post = "nd" }
+        {$_ -match "3$"} { $post = "rd" }
+        default { $post = "th" }
+    }
+    "$name, you are the $number$post customer we serve today. Thank you!"
 }
