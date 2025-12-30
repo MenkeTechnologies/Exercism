129cabeec1e52a6938963a4dcbf735fb93ad9f36
MenkeTechnologies <linux.dev25@gmail.com>
2025-12-30 16:51:57 -0500

    elyses

--- julia/elyses-enchantments/elyses-enchantments.jl
+++ julia/elyses-enchantments/elyses-enchantments.jl
@@ -1,31 +1,8 @@
-function get_item(stack, position)
-    
-end
-
-function set_item!(stack, position, replacement_card)
-    
-end
-
-function insert_item_at_top!(stack, new_card)
-    
-end
-
-function remove_item!(stack, position)
-    
-end
-
-function remove_item_from_top!(stack)
-    
-end
-
-function insert_item_at_bottom!(stack, new_card)
-    
-end
-
-function remove_item_at_bottom!(stack)
-    
-end
-
-function check_size_of_stack(stack, stack_size)
-    
-end
+get_item = getindex
+set_item!(stk, idx, card) = setindex!(stk, card, idx)
+insert_item_at_top! = push!
+insert_item_at_bottom! = pushfirst!
+remove_item! = deleteat!
+remove_item_from_top!(stk) = remove_item!(stk, length(stk))
+remove_item_at_bottom!(stk) = remove_item!(stk, 1)
+check_size_of_stack(stk, stack_size) = length(stk) == stack_size
