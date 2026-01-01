569e743c14424e52922cace01a75070217016371
MenkeTechnologies <linux.dev25@gmail.com>
2026-01-01 11:10:28 -0500

    ll

--- cpp/log-levels/log_levels.cpp
+++ cpp/log-levels/log_levels.cpp
@@ -1,14 +1,21 @@
 #include <string>
-using std::string;
+#include <regex>
+
+using namespace std;
+
 
 namespace log_line {
-    string message(const string &msg){
-        return msg.substr(msg.find(':') + 2);
+    string message(const string& line) {
+        regex pattern(R"(\[(\w+)\]:\s*(.*))");
+        smatch m;
+        return regex_search(line, m, pattern) ? string(m[2]) : "";
     }
-    string log_level(const string &msg){
-        return msg.substr(1, msg.find(']')-1);
+    string log_level(const string& line) {
+        regex pattern(R"(\[(\w+)\]:)"); 
+        smatch m;
+        return regex_search(line, m, pattern) ? string(m[1]) : "";
     }
-    string reformat(const string &msg){
-        return message(msg) + " (" + log_level(msg) + ")";
+    string reformat(const string& line) {
+        return message(line) + " (" + log_level(line) + ")";
     }
 }
