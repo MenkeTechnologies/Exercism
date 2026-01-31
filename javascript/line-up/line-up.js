b9100ec0cd714c251271b076aa4077c76593d5f0
MenkeTechnologies <linux.dev25@gmail.com>
2026-01-31 15:31:35 -0500

    lu

--- javascript/line-up/line-up.js
+++ javascript/line-up/line-up.js
@@ -1,8 +1,5 @@
-//
-// This is only a SKELETON file for the 'Line Up' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const ORDINALS = { 1: "st", 2: "nd", 3: "rd" };
 
-export const format = () => {
-  throw new Error('Remove this line and implement the function');
-};
+const suffix = number => ORDINALS[number % 10] && (number % 100 < 11 || number % 100 > 13) ? ORDINALS[number % 10] : "th";
+
+export const format = (name, number) => `${name}, you are the ${number}${suffix(number)} customer we serve today. Thank you!`;
