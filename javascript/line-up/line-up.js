3f57e47326977f7994a5db1d9129a1087079e69c
MenkeTechnologies <linux.dev25@gmail.com>
2026-02-05 19:33:58 -0500

    lu

--- javascript/line-up/line-up.js
+++ javascript/line-up/line-up.js
@@ -1,5 +1,5 @@
 const ORDINALS = { 1: "st", 2: "nd", 3: "rd" };
 
-const suffix = number => ORDINALS[number % 10] && (number % 100 < 11 || number % 100 > 13) ? ORDINALS[number % 10] : "th";
+const suffix = (n) => ORDINALS[n % 10] && (n % 100 < 11 || n % 100 > 13) ? ORDINALS[n % 10] : "th";
 
-export const format = (name, number) => `${name}, you are the ${number}${suffix(number)} customer we serve today. Thank you!`;
+export const format = (s, n) => `${s}, you are the ${n}${suffix(n)} customer we serve today. Thank you!`;
