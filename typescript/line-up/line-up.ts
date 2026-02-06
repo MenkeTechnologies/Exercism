ee5f086e6f2aeaf27d2deb618cf38c204b76f748
MenkeTechnologies <linux.dev25@gmail.com>
2026-02-05 19:32:49 -0500

    lu

--- typescript/line-up/line-up.ts
+++ typescript/line-up/line-up.ts
@@ -1,3 +1,5 @@
-export function format(name: unknown, number: unknown): unknown {
-  throw new Error('Remove this line and implement the function')
-}
+const ORDINALS : {[n: number] : string} = { 1: "st", 2: "nd", 3: "rd" };
+
+const suffix = (n: number) => ORDINALS[n % 10] && (n % 100 < 11 || n % 100 > 13) ? ORDINALS[n % 10] : "th";
+
+export const format = (s: string, n: number) => `${s}, you are the ${n}${suffix(n)} customer we serve today. Thank you!`;
