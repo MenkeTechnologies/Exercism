449449d1f0f174cf5f7f5c5434cea073191ca878
MenkeTechnologies <linux.dev25@gmail.com>
2026-02-12 13:09:35 -0500

    line-up

--- racket/line-up/line-up.rkt
+++ racket/line-up/line-up.rkt
@@ -2,5 +2,14 @@
 
 (provide format-message)
 
-(define (format-message name number)
-  (error "Please implement 'format-message'"))
+(define (suffix n)
+  (let ([units (remainder n 10)]
+        [tens (remainder n 100)])
+    (cond
+      [(= units 1) (if (= tens 11) "th" "st")]
+      [(= units 2) (if (= tens 12) "th" "nd")]
+      [(= units 3) (if (= tens 13) "th" "rd")]
+      [else "th"])))
+
+(define (format-message s n)
+  (format "~a, you are the ~a~a customer we serve today. Thank you!" s n (suffix n)))
