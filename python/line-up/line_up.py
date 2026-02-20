c92ecbb768163d46d814bb5069e1a23788f6ffc5
MenkeTechnologies <linux.dev25@gmail.com>
2026-02-20 12:13:55 -0500

    lu

--- python/line-up/line_up.py
+++ python/line-up/line_up.py
@@ -1,2 +1,4 @@
-def line_up(name, number):
-    pass
+dict = {1: 'st', 2: 'nd', 3: 'rd'}
+def line_up(s, n):
+    ordinal = 'th' if n % 100 in (11, 12, 13) else dict.get(n % 10, 'th')
+    return f'{s}, you are the {n}{ordinal} customer we serve today. Thank you!'
