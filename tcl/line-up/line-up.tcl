87d2b90e99fd1556a257652484c8a1c1035a12fb
MenkeTechnologies <linux.dev25@gmail.com>
2026-02-25 14:12:29 -0500

    lu

--- tcl/line-up/line-up.tcl
+++ tcl/line-up/line-up.tcl
@@ -1,3 +1,10 @@
-proc formatTicket {name number} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc formatTicket {s n} {
+    switch -regexp -- $n {
+        1[123]$ {set suffix th}
+        1$ {set suffix st}
+        2$ {set suffix nd}
+        3$ {set suffix rd}
+        default {set suffix th}
+    }
+    format "%s, you are the %d%s customer we serve today. Thank you!" $s $n $suffix
 }
