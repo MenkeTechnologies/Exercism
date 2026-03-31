25b7c9428f4dcc89f1be79403d34444131ba0482
MenkeTechnologies <linux.dev25@gmail.com>
2026-03-31 16:57:52 -0400

    bs

--- lean/binary-search/BinarySearch.lean
+++ lean/binary-search/BinarySearch.lean
@@ -1,6 +1,14 @@
 namespace BinarySearch
 
-def find (value : Int) (array : Array Int) : Option Nat :=
-  sorry --remove this line and implement the function
+def find (value : Int) (array : Array Int) : Option Nat := Id.run do
+  let mut lo := 0
+  let mut hi := array.size
+
+  while lo < hi do
+    let mid := (lo + hi) / 2
+    if value < array[mid]! then hi := mid
+    else if value > array[mid]! then lo := mid + 1
+    else return some mid
+  none
 
 end BinarySearch
