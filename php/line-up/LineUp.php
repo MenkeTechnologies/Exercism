52bb3766917f0d62c15be5cd888c6fd652c72e97
MenkeTechnologies <linux.dev25@gmail.com>
2026-07-14 17:49:56 -0400

    line-up

--- php/line-up/LineUp.php
+++ php/line-up/LineUp.php
@@ -1,30 +1,12 @@
 <?php
-
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
-declare(strict_types=1);
-
-function format(string $name, int $number): string
-{
-    throw new \BadFunctionCallException(sprintf('Implement the %s function', __FUNCTION__));
+function format($name, $number) {
+    $suffix = match (true) {
+        in_array($number % 100, [11, 12, 13]) => 'th',
+        $number % 10 === 1 => 'st',
+        $number % 10 === 2 => 'nd',
+        $number % 10 === 3 => 'rd',
+        default => 'th',
+    };
+    return "$name, you are the $number$suffix customer we serve today. Thank you!";
 }
+
